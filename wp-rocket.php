7aeb599fc006eeb14aeaf73f48ddd553e6cc7ccb
Julio Potier <juliobosk@gmail.com>
2013-06-17 16:37:19 +0200

    Ajouts de realpath() pour support windows/local dans htaccess

--- wp-rocket.php
+++ wp-rocket.php
@@ -13,7 +13,8 @@ Copyright 2013 WP Rocket
 
 */
 defined( 'ABSPATH' ) or	die( 'Cheatin\' uh?' );
-
+if( !defined( 'DS' ) )
+	define( 'DS', DIRECTORY_SEPARATOR );
 define( 'WP_ROCKET_VERSION'			, '1.0');
 define( 'WP_ROCKET_SLUG'			, 'wp_rocket_settings');
 define( 'WP_ROCKET_WEB_MAIN'		, 'http://wp-rocket.me/');
@@ -22,11 +23,11 @@ define( 'WP_ROCKET_WEB_VALID'		, 'valid_key.php');
 define( 'WP_ROCKET_WEB_INFO'		, 'plugin_information.php');
 define( 'WP_ROCKET_WEB_SUPPORT'		, 'http://support.wp-rocket.me/forum/fr/');
 define( 'WP_ROCKET_FILE'			, __FILE__ );
-define( 'WP_ROCKET_PATH'			, plugin_dir_path(__FILE__) );
-define( 'WP_ROCKET_INC_PATH'		, WP_ROCKET_PATH . 'inc/' );
-define( 'WP_ROCKET_FRONT_PATH'		, WP_ROCKET_INC_PATH . 'front/' );
-define( 'WP_ROCKET_ADMIN_PATH'		, WP_ROCKET_INC_PATH . 'admin/' );
-define( 'WP_ROCKET_CACHE_PATH'		, WP_ROCKET_PATH . 'cache/' );
+define( 'WP_ROCKET_PATH'			, realpath( plugin_dir_path(__FILE__) ).'/' );
+define( 'WP_ROCKET_INC_PATH'		, realpath( WP_ROCKET_PATH . '/inc/' ) );
+define( 'WP_ROCKET_FRONT_PATH'		, realpath( WP_ROCKET_INC_PATH . '/front/' ) );
+define( 'WP_ROCKET_ADMIN_PATH'		, realpath( WP_ROCKET_INC_PATH . '/admin/' ) );
+define( 'WP_ROCKET_CACHE_PATH'		, realpath( WP_ROCKET_PATH . '/cache/' ) );
 define( 'WP_ROCKET_URL'				, plugin_dir_url(__FILE__) );
 define( 'WP_ROCKET_INC_URL'			, WP_ROCKET_URL . 'inc/' );
 define( 'WP_ROCKET_FRONT_URL'		, WP_ROCKET_INC_URL . 'front/' );
@@ -55,36 +56,36 @@ function rocket_init()
 			return;
 
 	// Call all classes and functions
-	require WP_ROCKET_INC_PATH . 'functions.php';
-	require WP_ROCKET_FRONT_PATH . 'htaccess.php';
+	require WP_ROCKET_INC_PATH . '/functions.php';
+	require WP_ROCKET_FRONT_PATH . '/htaccess.php';
 
 	if( rocket_valid_key() ) {
-		require WP_ROCKET_INC_PATH . 'purge.php';
-		require WP_ROCKET_INC_PATH . 'admin-bar.php';
+		require WP_ROCKET_INC_PATH . '/purge.php';
+		require WP_ROCKET_INC_PATH . '/admin-bar.php';
 	}
 
 	$options = get_option( WP_ROCKET_SLUG );
 
 	if( rocket_valid_key() )
 		if( isset( $options['purge_cron_interval'] ) && (int)$options['purge_cron_interval'] > 0 )
-			require  WP_ROCKET_INC_PATH . 'cron.php';
+			require  WP_ROCKET_INC_PATH . '/cron.php';
 
 
 	if( is_admin() )
 	{
-		require WP_ROCKET_ADMIN_PATH . 'upgrader.php';
-		require WP_ROCKET_ADMIN_PATH . 'updater.php';
-		require WP_ROCKET_ADMIN_PATH . 'options.php';
-		require WP_ROCKET_ADMIN_PATH . 'notices.php';
+		require WP_ROCKET_ADMIN_PATH . '/upgrader.php';
+		require WP_ROCKET_ADMIN_PATH . '/updater.php';
+		require WP_ROCKET_ADMIN_PATH . '/options.php';
+		require WP_ROCKET_ADMIN_PATH . '/notices.php';
 	}
 	elseif( rocket_valid_key() )
 	{
-		require WP_ROCKET_FRONT_PATH . 'process.php';
-		require WP_ROCKET_FRONT_PATH . 'minify.php';
-		require WP_ROCKET_FRONT_PATH . 'cookie.php';
+		require WP_ROCKET_FRONT_PATH . '/process.php';
+		require WP_ROCKET_FRONT_PATH . '/minify.php';
+		require WP_ROCKET_FRONT_PATH . '/cookie.php';
 
 		if( isset( $options['lazyload'] ) && $options['lazyload'] == '1' )
-			require WP_ROCKET_FRONT_PATH . 'lazyload.php';
+			require WP_ROCKET_FRONT_PATH . '/lazyload.php';
 	}
 
 	// You can hook this to trigger any action when WP Rocket is correctly loaded, so, not in AJAX or AUTOSAVE mode
