64ef04537b5ea171947856cd80b77fbdf1f2b850
Julio Potier <juliobosk@gmail.com>
2013-06-19 16:18:37 +0200

    Suite 1.0.1 Refonte des crons updates plugins Ajout hook pour update 1.0.1 Ajout de constantes Les options ne sont plus delete si mauvaise clé Uninstall supprime le transient update du plugin

--- uninstall.php
+++ uninstall.php
@@ -1,21 +1,15 @@
 <?php
 
-
-/**
- * If uninstall not called from WordPress exit
- *
- * since 1.0
- *
- */
-
+// If uninstall not called from WordPress exit
 if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 /**
- * Delete option from option table
+ * Delete option and transient from option table
  *
  * since 1.0
  *
  */
 
+delete_site_transient( 'update_rocket' );
 delete_option( 'wp_rocket_settings' );
