f1a268e646e2ba601ceedbfcb4e593a9e85e5ad4
GeekPress <contact@geekpress.fr>
2013-08-07 14:37:28 +0200

    Ajout des constantes dans wp-rocket.php

--- wp-rocket.php
+++ wp-rocket.php
@@ -15,24 +15,27 @@ Copyright 2013 WP Rocket
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 // Rocket defines
-define( 'WP_ROCKET_VERSION'                     , '1.3.0');
-define( 'WP_ROCKET_SLUG'                        , 'wp_rocket_settings');
+define( 'WP_ROCKET_VERSION'             , '1.3.0');
+define( 'WP_ROCKET_SLUG'                , 'wp_rocket_settings');
 define( 'WP_ROCKET_WEB_MAIN'            , 'http://support.wp-rocket.me/');
 define( 'WP_ROCKET_WEB_CHECK'           , WP_ROCKET_WEB_MAIN.'check_update.php');
 define( 'WP_ROCKET_WEB_VALID'           , WP_ROCKET_WEB_MAIN.'valid_key.php');
 define( 'WP_ROCKET_WEB_INFO'            , WP_ROCKET_WEB_MAIN.'plugin_information.php');
 define( 'WP_ROCKET_WEB_SUPPORT'         , WP_ROCKET_WEB_MAIN.'forum/fr/');
-define( 'WP_ROCKET_BOT_URL'                     , 'http://bot.wp-rocket.me/launch.php');
-define( 'WP_ROCKET_FILE'                        , __FILE__ );
-define( 'WP_ROCKET_PATH'                        , realpath( plugin_dir_path( WP_ROCKET_FILE ) ).'/' );
+define( 'WP_ROCKET_BOT_URL'             , 'http://bot.wp-rocket.me/launch.php');
+define( 'WP_ROCKET_FILE'                , __FILE__ );
+define( 'WP_ROCKET_PATH'                , realpath( plugin_dir_path( WP_ROCKET_FILE ) ).'/' );
 define( 'WP_ROCKET_INC_PATH'            , realpath( WP_ROCKET_PATH . 'inc/' ) . '/' );
 define( 'WP_ROCKET_FRONT_PATH'          , realpath( WP_ROCKET_INC_PATH . 'front/' ) . '/' );
 define( 'WP_ROCKET_ADMIN_PATH'          , realpath( WP_ROCKET_INC_PATH . 'admin' ) . '/' );
 define( 'WP_ROCKET_CACHE_PATH'          , WP_CONTENT_DIR . '/wp-rocket-cache/' );
-define( 'WP_ROCKET_URL'                         , plugin_dir_url( WP_ROCKET_FILE ) );
-define( 'WP_ROCKET_INC_URL'                     , WP_ROCKET_URL . 'inc/' );
+define( 'WP_ROCKET_URL'                 , plugin_dir_url( WP_ROCKET_FILE ) );
+define( 'WP_ROCKET_INC_URL'             , WP_ROCKET_URL . 'inc/' );
 define( 'WP_ROCKET_FRONT_URL'           , WP_ROCKET_INC_URL . 'front/' );
-define( 'WP_ROCKET_FRONT_JS_URL'        , WP_ROCKET_FRONT_URL . 'js/' );
+define( 'WP_ROCKET_ADMIN_URL'           , WP_ROCKET_INC_URL . 'admin/' );
+define( 'WP_ROCKET_ADMIN_JS_URL'        , WP_ROCKET_ADMIN_URL . 'js/' );
+define( 'WP_ROCKET_ADMIN_CSS_URL'       , WP_ROCKET_ADMIN_URL . 'css/' );
+define( 'WP_ROCKET_ADMIN_IMG_URL'       , WP_ROCKET_ADMIN_URL . 'img/' );
 define( 'WP_ROCKET_CACHE_URL'           , WP_CONTENT_URL . '/wp-rocket-cache/' );
 if( !defined( 'CHMOD_WP_ROCKET_CACHE_DIRS' ) )
 	define( 'CHMOD_WP_ROCKET_CACHE_DIRS', 0755 );
@@ -54,7 +57,7 @@ if( !defined( 'YEAR_IN_SECONDS' ) )
 /*
  * Tell WP what to do when plugin is loaded
  *
- * since 1.0
+ * @since 1.0
  *
  */
 
@@ -62,57 +65,57 @@ add_action( 'plugins_loaded', 'rocket_init' );
 function rocket_init()
 {
 
-        if( defined( 'DOING_AUTOSAVE' ) )
-                        return;
+    if( defined( 'DOING_AUTOSAVE' ) )
+                    return;
 
-        // Call defines,  classes and functions
-        require WP_ROCKET_INC_PATH . '/functions.php';
-        require WP_ROCKET_FRONT_PATH . '/htaccess.php';
-        require WP_ROCKET_INC_PATH . '/headers.php';
+    // Call defines,  classes and functions
+    require WP_ROCKET_INC_PATH . '/functions.php';
+    require WP_ROCKET_FRONT_PATH . '/htaccess.php';
+    require WP_ROCKET_INC_PATH . '/headers.php';
 
-        if( rocket_valid_key() ) {
-                require WP_ROCKET_INC_PATH . '/purge.php';
-                require WP_ROCKET_INC_PATH . '/admin-bar.php';
-        }
+    if( rocket_valid_key() ) {
+            require WP_ROCKET_INC_PATH . '/purge.php';
+            require WP_ROCKET_INC_PATH . '/admin-bar.php';
+    }
 
-        if( rocket_valid_key() )
-			if( (int)get_rocket_option( 'purge_cron_interval' ) > 0 )
-				require  WP_ROCKET_INC_PATH . '/cron.php';
+    if( rocket_valid_key() )
+		if( (int)get_rocket_option( 'purge_cron_interval' ) > 0 )
+			require  WP_ROCKET_INC_PATH . '/cron.php';
 
 
-        if( is_admin() )
-        {
+    if( is_admin() )
+    {
 
-            require WP_ROCKET_ADMIN_PATH . '/upgrader.php';
-            require WP_ROCKET_ADMIN_PATH . '/updater.php';
-            require WP_ROCKET_ADMIN_PATH . '/options.php';
-            require WP_ROCKET_ADMIN_PATH . '/notices.php';
-            require WP_ROCKET_ADMIN_PATH . '/admin.php';
-        }
-        elseif( rocket_valid_key() )
-        {
-            require WP_ROCKET_FRONT_PATH . '/process.php';
-            require WP_ROCKET_FRONT_PATH . '/minify.php';
-            require WP_ROCKET_FRONT_PATH . '/cookie.php';
-            require WP_ROCKET_FRONT_PATH . '/images.php';
-            require WP_ROCKET_FRONT_PATH . '/enqueue.php';
-            if( get_rocket_option( 'deferred_js_files' ) )
-				require WP_ROCKET_FRONT_PATH . '/deferred-js.php';
+        require WP_ROCKET_ADMIN_PATH . '/upgrader.php';
+        require WP_ROCKET_ADMIN_PATH . '/updater.php';
+        require WP_ROCKET_ADMIN_PATH . '/options.php';
+        require WP_ROCKET_ADMIN_PATH . '/notices.php';
+        require WP_ROCKET_ADMIN_PATH . '/admin.php';
+    }
+    elseif( rocket_valid_key() )
+    {
+        require WP_ROCKET_FRONT_PATH . '/process.php';
+        require WP_ROCKET_FRONT_PATH . '/minify.php';
+        require WP_ROCKET_FRONT_PATH . '/cookie.php';
+        require WP_ROCKET_FRONT_PATH . '/images.php';
+        require WP_ROCKET_FRONT_PATH . '/enqueue.php';
+        if( get_rocket_option( 'deferred_js_files' ) )
+			require WP_ROCKET_FRONT_PATH . '/deferred-js.php';
 
-            if( get_rocket_option( 'lazyload' ) == '1' )
-				require WP_ROCKET_FRONT_PATH . '/lazyload.php';
-        }
+        if( get_rocket_option( 'lazyload' ) == '1' )
+			require WP_ROCKET_FRONT_PATH . '/lazyload.php';
+    }
 
-        // You can hook this to trigger any action when WP Rocket is correctly loaded, so, not in AUTOSAVE mode
-        if( rocket_valid_key() )
-			do_action( 'wp_rocket_loaded' );
+    // You can hook this to trigger any action when WP Rocket is correctly loaded, so, not in AUTOSAVE mode
+    if( rocket_valid_key() )
+		do_action( 'wp_rocket_loaded' );
 
 }
 
 /*
  * Tell WP what to do when plugin is deactivated
  *
- * since 1.0
+ * @since 1.0
  *
  */
 register_deactivation_hook( __FILE__, 'rocket_deactivation' );
@@ -127,7 +130,7 @@ function rocket_deactivation()
 /*
  * Tell WP what to do when plugin is activated
  *
- * since 1.1.0
+ * @since 1.1.0
  *
  */
 register_activation_hook( __FILE__, 'rocket_activation' );
