fd27020fb8f319775eff7cdea45f2ca7d156e985
GeekPress <contact@geekpress.fr>
2013-09-10 01:04:39 +0200

    Nouveau: Ajout du hook do_rocket_lazyload. Cela permet, par exemple, de désactiver le LazyLoad sur certaines pages.

--- inc/front/lazyload.php
+++ inc/front/lazyload.php
@@ -6,6 +6,7 @@ defined( 'ABSPATH' ) or	die( 'Cheatin\' uh?' );
  * Add Lazy Load JavaScript in the header
  * No jQuery or other library is required !!
  *
+ * @since 1.3.5 It's possible to exclude LazyLoad process by used do_rocket_lazyload filter
  * @since 1.1.0 This code is insert in head with inline script for more performance
  * @since 1.0
  *
@@ -14,14 +15,19 @@ defined( 'ABSPATH' ) or	die( 'Cheatin\' uh?' );
 add_action( 'wp_head', 'rocket_lazyload_script', PHP_INT_MAX );
 function rocket_lazyload_script()
 {
+
+	if( !apply_filters( 'do_rocket_lazyload', true ) )
+		return;
+
 	echo '<script type="text/javascript">(function(a,e){function f(){var d=0;if(e.body&&e.body.offsetWidth){d=e.body.offsetHeight}if(e.compatMode=="CSS1Compat"&&e.documentElement&&e.documentElement.offsetWidth){d=e.documentElement.offsetHeight}if(a.innerWidth&&a.innerHeight){d=a.innerHeight}return d}function b(g){var d=ot=0;if(g.offsetParent){do{d+=g.offsetLeft;ot+=g.offsetTop}while(g=g.offsetParent)}return{left:d,top:ot}}function c(){var l=e.querySelectorAll("[data-lazy-original]");var j=a.pageYOffset||e.documentElement.scrollTop||e.body.scrollTop;var d=f();for(var k=0;k<l.length;k++){var h=l[k];var g=b(h).top;if(g<(d+j)){h.src=h.getAttribute("data-lazy-original");h.removeAttribute("data-lazy-original")}}}if(a.addEventListener){a.addEventListener("DOMContentLoaded",c,false);a.addEventListener("scroll",c,false)}else{a.attachEvent("onload",c);a.attachEvent("onscroll",c)}})(window,document);</script>';
 }
 
 
 
 /**
- * Replace Gravatar, thumbnails, images in post content and in widget text by Lazy Load
+ * Replace Gravatar, thumbnails, images in post content and in widget text by LazyLoad
  *
+ * @since 1.3.5 It's possible to exclude LazyLoad process by used do_rocket_lazyload filter
  * @since 1.2.0 It's possible to not lazy load an image with data-no-lazy attribute
  * @since 1.1.0 Don't lazy-load if the thumbnail has already been run through previously
  * @since 1.0.1 Add priority of hooks at maximum later with PHP_INT_MAX
@@ -35,15 +41,17 @@ add_filter( 'the_content', 'rocket_lazyload_images', PHP_INT_MAX );
 add_filter( 'widget_text', 'rocket_lazyload_images', PHP_INT_MAX );
 function rocket_lazyload_images( $html )
 {
+
+	// // Don't LazyLoad if the thumbnail is in a feed or in a post preview
 	if( is_feed() || is_preview() || empty( $html ) )
 		return $html;
-	
-	// Don't lazy-load if the thumbnail has already been run through previously
-	if ( strpos( $html, 'data-lazy-original' ) || strpos( $html, 'data-no-lazy' ) )
+
+	// Don't LazyLoad if the thumbnail has already been run through previously or stop process with a hook
+	if ( strpos( $html, 'data-lazy-original' ) || strpos( $html, 'data-no-lazy' ) || !apply_filters( 'do_rocket_lazyload', true ) )
 		return $html;
-	
+
 	$html = preg_replace( '#<img([^>]+?)src=[\'"]?([^\'"\s>]+)[\'"]?([^>]*)>#', '<img${1}src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-lazy-original="${2}"${3}><noscript><img${1}src="${2}"${3}></noscript>', $html );
-	
+
 	return $html;
 }
 
@@ -52,18 +60,20 @@ function rocket_lazyload_images( $html )
 /**
  * Replace WordPress smilies by Lazy Load
  *
+ * @since 1.3.5 It's possible to exclude LazyLoad process by used do_rocket_lazyload filter
  * @since 1.1.0 Don't lazy-load if the thumbnail has already been run through previously
  * @since 1.0.1 Add priority of hooks at maximum later with PHP_INT_MAX
- * @since 0.1
+ * @since 1.0
  *
  */
 
 add_filter('smilies_src', 'rocket_lazyload_smilies', PHP_INT_MAX );
 function rocket_lazyload_smilies( $src )
 {
-	// Don't lazy-load if the thumbnail has already been run through previously
-	if ( strpos( $src, 'data-lazy-original' ) )
+	// Don't lazy-load if the thumbnail has already been run through previously or stop process with a hook
+
+	if ( strpos( $src, 'data-lazy-original' ) || !apply_filters( 'do_rocket_lazyload', true ) )
 		return $src;
-	
-	return "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' data-lazy-original='".$src;
+
+	return "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' data-lazy-original='" . $src;
 }
