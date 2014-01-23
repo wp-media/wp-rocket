04246a5af3472e8bb943c1971cff53a1dcfd4e91
Fx Bénard <fxbenard@gmail.com>
2014-01-23 15:24:33 +0100

    [i18n]> Tutoriels 100% Traduction 100% - Ready for i18n

--- inc/admin/tutorials.php
+++ inc/admin/tutorials.php
@@ -1,13 +1,13 @@
 <?php defined( 'ABSPATH' ) or	die( __( 'Cheatin&#8217; uh?', 'rocket' ) ); ?>
 
 <h2><?php _e( 'Preload cache', 'rocket' ); ?></h2>
-<p><?php _e ( 'This video gives some explanations about our two crawler robots. They generate several cache files in a few seconds.', 'rocket' );?></p>
+<p><?php _e ( 'This video gives some explanations about our two crawler robots. They generate several cache files in a few seconds.', 'rocket' ); ?></p>
 <p><a href="http://www.youtube.com/embed/9jDcg2f-9yM" class="button-primary fancybox"><?php _e( 'Watch the video', 'rocket' ); ?></a></p>
 
-<h2><?php _e( 'CSS and JavaScript minification', 'rocket' );?></h2>
-<p>Cette vidéo donne quelques explications sur l’utilisation avancée du processus de minification et concaténation des fichiers CSS et JavaScript.</p>
+<h2><?php _e( 'CSS and JavaScript minification', 'rocket' ); ?></h2>
+<p><?php _e( 'This video gives some explanations about how to use the advanced process of minification and concatenation of CSS and JavaScript files.', 'rocket' ); ?></p>
 <p><a href="http://www.youtube.com/embed/iziXSvZgxLk" class="button-primary fancybox"><?php _e( 'Watch the video', 'rocket' ); ?></a></p>
 
-<h2>Pré-chargement des requêtes DNS</h2>
-<p>Cette vidéo permet de comprendre en toute simplicité le fonctionnement de l'option avancée "Pré-chargement des requêtes DNS" et de l'utilisation du filtre <code>rocket_dns_prefetch</code>.</p>
+<h2><?php _e( 'Preloading DNS queries', 'rocket' ); ?></h2>
+<p><?php _e( 'This video helps to understand easily the advanced option of "Preloading DNS queries" and the use of the filter <code>rocket_dns_prefetch</code>.', 'rocket' ); ?></p>
 <p><a href="http://www.youtube.com/embed/ElJCtUidLwc" class="button-primary fancybox"><?php _e( 'Watch the video', 'rocket' ); ?></a></p>
