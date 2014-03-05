fe7469e1caac2151dbabe621d64589fe0461c40d
GeekPress <contact@geekpress.fr>
2014-03-05 17:52:07 +0100

    Mise à jour des noms des options minify_css_pretty_url & minify_js_pretty_url

--- inc/front/minify.php
+++ inc/front/minify.php
@@ -212,7 +212,7 @@ function rocket_minify_css( $buffer )
 	}
 
 	// Insert the minify css file below <head>
-	return array( $buffer, $fonts_tags . $external_tags . get_rocket_minify_files( $internal_files, get_rocket_option( 'minify_pretty_url_css' ) ) . $excluded_tags );
+	return array( $buffer, $fonts_tags . $external_tags . get_rocket_minify_files( $internal_files, get_rocket_option( 'minify_css_pretty_url' ) ) . $excluded_tags );
 
 }
 
@@ -284,7 +284,7 @@ function rocket_minify_js( $buffer )
 		
 	}
     // Insert the minify JS file
-    return array( $buffer, $external_tags . get_rocket_minify_files( $internal_files, get_rocket_option( 'minify_pretty_url_js' ) ) . $excluded_tags );
+    return array( $buffer, $external_tags . get_rocket_minify_files( $internal_files, get_rocket_option( 'minify_js_pretty_url' ) ) . $excluded_tags );
 
 }
 
