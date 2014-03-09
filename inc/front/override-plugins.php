53f7cb9522a76614b2b06312d1d88d1dd51851eb
GeekPress <contact@geekpress.fr>
2014-03-09 01:18:04 +0100

    Support de la version de WP Touch < 3.0 pour le conflit avec notre LazyLoad

--- inc/front/override-plugins.php
+++ inc/front/override-plugins.php
@@ -13,7 +13,7 @@ add_action( 'init', '__deactivate_rocket_lazyload_with_wptouch' );
 function __deactivate_rocket_lazyload_with_wptouch()
 {
 
-    if( function_exists( 'wptouch_is_mobile_theme_showing' ) && wptouch_is_mobile_theme_showing() )
+    if( (function_exists( 'wptouch_is_mobile_theme_showing' ) && wptouch_is_mobile_theme_showing()) || (function_exists( 'bnc_wptouch_is_mobile' ) && bnc_wptouch_is_mobile()) ) 
     {
 		add_filter( 'do_rocket_lazyload', '__return_false' );
     }
