b3aaaa2457afa80d678c2b50b9e76746500558a2
GeekPress <contact@geekpress.fr>
2014-03-23 23:30:05 +0100

    Fixe le doute sur la notice provoquée par $GLOBALS['pagenow']

--- inc/front/cdn.php
+++ inc/front/cdn.php
@@ -114,8 +114,10 @@ add_filter( 'script_loader_src', 'rocket_cdn_enqueue', PHP_INT_MAX );
 function rocket_cdn_enqueue( $src )
 {
 
+	global $pagenow;
+	
 	// Don't use CDN if in admin, in login page, in register page or in a post preview
-	if ( is_admin() || is_preview() || in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php') ) ) {
+	if ( is_admin() || is_preview() || in_array( $pagenow, array('wp-login.php', 'wp-register.php' ) ) ) {
 		return $src;
 	}
 
