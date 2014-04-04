80c633a8ea561ca5041e6b8d9f15c304e5b13af0
Jonathan Brinley <jbrinley@flightless.us>
2014-04-04 10:27:26 -0400

    version number bump

--- action-scheduler.php
+++ action-scheduler.php
@@ -5,24 +5,24 @@ Plugin URI: https://github.com/flightless/action-scheduler
 Description: A robust action scheduler for WordPress
 Author: Flightless
 Author URI: http://flightless.us/
-Version: 1.1
+Version: 1.2-dev
 */
 
-if ( !function_exists('action_scheduler_register_1_dot_1') ) {
+if ( !function_exists('action_scheduler_register_1_dot_2_dev') ) {
 
 	if ( !class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once('classes/ActionScheduler_Versions.php');
 		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
-	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_1', 0, 0 );
+	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_2_dev', 0, 0 );
 
-	function action_scheduler_register_1_dot_1() {
+	function action_scheduler_register_1_dot_2_dev() {
 		$versions = ActionScheduler_Versions::instance();
-		$versions->register( '1.1', 'action_scheduler_initialize_1_dot_1' );
+		$versions->register( '1.2-dev', 'action_scheduler_initialize_1_dot_2_dev' );
 	}
 
-	function action_scheduler_initialize_1_dot_1() {
+	function action_scheduler_initialize_1_dot_2_dev() {
 		require_once('classes/ActionScheduler.php');
 		ActionScheduler::init( __FILE__ );
 	}
