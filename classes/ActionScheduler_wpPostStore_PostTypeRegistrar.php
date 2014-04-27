ada818f9cfac4e4b27c95cbc7feb87a20cf3f283
Jonathan Brinley <jbrinley@flightless.us>
2014-04-27 15:39:49 -0400

    convert claims to posts instead of strings
    actions are claimed by setting the post parent to the claim ID

--- classes/ActionScheduler_wpPostStore_PostTypeRegistrar.php
+++ classes/ActionScheduler_wpPostStore_PostTypeRegistrar.php
@@ -6,15 +6,16 @@
  */
 class ActionScheduler_wpPostStore_PostTypeRegistrar {
 	public function register() {
-		register_post_type( ActionScheduler_wpPostStore::POST_TYPE, $this->post_type_args() );
+		register_post_type( ActionScheduler_wpPostStore::POST_TYPE, $this->action_post_type_args() );
+		register_post_type( ActionScheduler_wpPostStore::CLAIM_POST_TYPE, $this->claim_post_type_args() );
 	}
 
 	/**
-	 * Build the args array for the post type definition
+	 * Build the args array for the action post type definition
 	 *
 	 * @return array
 	 */
-	protected function post_type_args() {
+	protected function action_post_type_args() {
 		$args = array(
 			'label' => __( 'Scheduled Actions', 'action-scheduler' ),
 			'public' => false,
@@ -28,6 +29,28 @@ class ActionScheduler_wpPostStore_PostTypeRegistrar {
 		);
 
 		$args = apply_filters('action_scheduler_post_type_args', $args);
+		return $args;
+	}
+
+	/**
+	 * Build the args array for the action post type definition
+	 *
+	 * @return array
+	 */
+	protected function claim_post_type_args() {
+		$args = array(
+			'label' => __( 'Action Claims', 'action-scheduler' ),
+			'public' => false,
+			'map_meta_cap' => true,
+			'hierarchical' => false,
+			'supports' => false,
+			'rewrite' => false,
+			'query_var' => false,
+			'can_export' => true,
+			'ep_mask' => EP_NONE,
+		);
+
+		$args = apply_filters('action_scheduler_claim_post_type_args', $args);
 		return $args;
 	}
 }
