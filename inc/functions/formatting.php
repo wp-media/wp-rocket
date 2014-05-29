b06b264e3b35eb338028794b00860d836c3451e3
Jonathan Buttigieg <contact@geekpress.fr>
2014-05-29 17:09:57 +0200

    Correction du bug de WP_CACHE a true après activation sans clé API + Coding standard

--- inc/functions/formatting.php
+++ inc/functions/formatting.php
@@ -58,7 +58,7 @@ function get_rocket_cdn_url( $url, $zone = array( 'all' ) )
 		}
 	}
 	
-	$url = rtrim( $cnames[(abs(crc32($path))%count($cnames))] , '/' ) . $path . $query
+	$url = rtrim( $cnames[(abs(crc32($path))%count($cnames))] , '/' ) . $path . $query;
 	return $url;
 }
 
