5bf6df3efa2dfaef0ba845da85c7d4f4f5d7fa50
Jonathan Buttigieg <contact@geekpress.fr>
2014-07-02 15:13:44 +0200

    Add some filters comments

--- inc/front/dns-prefetch.php
+++ inc/front/dns-prefetch.php
@@ -1,31 +1,31 @@
 <?php
 defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
 
-
 /**
  * Launch DNS Prefetching process
  *
  * @since 2.1 Adding CNAMES fo CDN automatically in DNS Prefetch process
  * @since 2.0
- *
  */
-
 add_filter( 'rocket_buffer', 'rocket_dns_prefetch', 12 );
 function rocket_dns_prefetch( $buffer )
 {
 	$dns_link_tags = '';
+	$domains = array_merge( get_rocket_cdn_cnames(), get_rocket_option( 'dns_prefetch', array() ) );
 
-	// Get all domains to prefetch DNS
-	// It's possible to add domains for specific conditions with 'rocket_dns_prefetch' filter
-	$domains = array_merge( get_rocket_cdn_cnames( array( 'all' ) ), get_rocket_option( 'dns_prefetch', array() ) );
+	/**
+	 * Filter list of domains to prefetch DNS
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param array $domains List of domains to prefetch DNS
+	 */
 	$domains = apply_filters( 'rocket_dns_prefetch', $domains );
 
 	if ( count( $domains ) ) {
-		
 		foreach ( $domains as $domain ) {
 			$dns_link_tags .= '<link rel="dns-prefetch" href="' . esc_url( $domain ) . '" />';
 		}
-			
 	}
 
 	// Insert all DNS prefecth tags in head
