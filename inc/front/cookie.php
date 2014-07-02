741b5f7acfc531a8128bad8cb5bf1ee408a64ca4
Jonathan Buttigieg <contact@geekpress.fr>
2014-07-02 11:55:52 +0200

    Add filter rocket_comment_cookie_lifetime

--- inc/front/cookie.php
+++ inc/front/cookie.php
@@ -5,12 +5,19 @@ defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
  * By default, the lifetime of the cookie comment is one year.
  * Life is reduced to 3 minutes so that the visitor can enjoy the cached site.
  *
- * since 1.0
- *
+ * @since 1.0
  */
-
 add_filter( 'comment_cookie_lifetime', 'rocket_comment_cookie_lifetime' );
 function rocket_comment_cookie_lifetime()
 {
-	return 3 * MINUTE_IN_SECONDS;
+	/**
+	 * Filter the lifetime of the cookie comment
+	 *
+	 * @since 2.2
+	 *
+	 * @param int The lifetime of the cookie in seconds
+	 */
+	$cookie_lifetime = apply_filters( 'rocket_comment_cookie_lifetime', 3 * MINUTE_IN_SECONDS );
+
+	return $cookie_lifetime;
 }
