d74e56a4c4d0201ff0be3d8f2f2cd2d9b7514c37
Jonathan Buttigieg <contact@geekpress.fr>
2014-07-02 17:05:04 +0200

    Update functions & filters comments

--- inc/functions/bots.php
+++ inc/functions/bots.php
@@ -5,21 +5,21 @@ defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
  * Launch the Robot
  *
  * @since 1.0
- * @access public
+ *
  * @param string $spider (default: 'cache-preload') The spider name: cache-preload or cache-json
  * @param string $lang (default: '') The language code to preload
  * @return void
  */
 function run_rocket_bot( $spider = 'cache-preload', $lang = '' )
 {
-
 	/**
 	 * Filter to manage the bot job
 	 *
 	 * @since 2.1
-	 * @param bool Do the job or not
+	 *
+	 * @param bool 	 		 Do the job or not
 	 * @param string $spider The spider name
-	 * @param string $lang The language code to preload
+	 * @param string $lang 	 The language code to preload
 	*/
 	if ( ! apply_filters( 'do_run_rocket_bot', true, $spider, $lang ) ) {
 		return false;
@@ -43,16 +43,16 @@ function run_rocket_bot( $spider = 'cache-preload', $lang = '' )
 	}
 
 	foreach ( $urls as $start_url ) {
-
 		/**
 		 * Fires before WP Rocket Bot is called
 		 *
 		 * @since 1.1.0
-		 * @param string $spider The spider name
+		 *
+		 * @param string $spider 	The spider name
 		 * @param string $start_url URL that crawl by the bot
 		*/
 		do_action( 'before_run_rocket_bot', $spider, $start_url );
-		
+
 		add_filter( 'http_headers_useragent', 'rocket_user_agent' );
 		wp_remote_get( WP_ROCKET_BOT_URL . '?spider=' . $spider . '&start_url=' . $start_url );
 		remove_filter( 'http_headers_useragent', 'rocket_user_agent' );
@@ -61,11 +61,10 @@ function run_rocket_bot( $spider = 'cache-preload', $lang = '' )
 		 * Fires after WP Rocket Bot was called
 		 *
 		 * @since 1.1.0
-		 * @param string $spider The spider name
+		 *
+		 * @param string $spider 	The spider name
 		 * @param string $start_url URL that crawl by the bot
 		*/
 		do_action( 'after_run_rocket_bot', $spider, $start_url );
-
 	}
-
 }
