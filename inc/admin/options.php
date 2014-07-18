2cfb17235727053b36dad6822821550a5d052b4a
Jonathan Buttigieg <contact@geekpress.fr>
2014-07-18 16:16:07 +0200

    Fix bug des box CSS et JS déjà cochées -bad Julio =D

--- inc/admin/options.php
+++ inc/admin/options.php
@@ -1184,7 +1184,13 @@ function rocket_settings_callback( $inputs )
 	if ( isset( $_GET['action'] ) && 'purge_cache' == $_GET['action'] ) {
 		return $inputs;
 	}
-
+	
+	/*
+	 * Option : Minification CSS & JS
+	 */
+	$inputs['minify_css'] = ! empty( $inputs['minify_css'] ) ? 1 : 0;
+	$inputs['minify_js']  = ! empty( $inputs['minify_js'] ) ? 1 : 0;
+	
 	/*
 	 * Option : Purge delay
 	 */
