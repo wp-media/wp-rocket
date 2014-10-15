037b67a7640d962b3196e98092af645d6a751e33
Jonathan Buttigieg <contact@geekpress.fr>
2014-10-15 13:17:10 -0700

    Fix & Improve Google Fonts Minification (view comments for more details)
    Improvement: Better Detection for Google Fonts Minification
    Fix: Remove "Array" in source code when Google Fonts Minification is
    activated

--- inc/front/minify.php
+++ inc/front/minify.php
@@ -128,13 +128,13 @@ function __rocket_insert_minify_js_in_footer() {
  */
 function __rocket_concatenate_google_fonts( $buffer ) {
 	// Get all Google Fonts CSS files
-	preg_match_all( '/<link.+href=[\'|"](.+fonts\.googleapis\.com.+)(\'|").+>/iU', $buffer, $matches );
+	preg_match_all( '/<link\s*.+href=[\'|"](.+fonts\.googleapis\.com.+)(\'|").+>/iU', $buffer, $matches );
 
 	$i = 0;
 	$fonts = array();
 
 	if ( ! $matches[1] ) {
-		return array( $buffer, $fonts );
+		return array( $buffer, '' );
 	}
 	
 	foreach ( $matches[1] as $font ) {
