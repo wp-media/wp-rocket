7079e81a0b5f921640e41b661ad197e64f714b59
Jonathan Buttigieg <contact@geekpress.fr>
2014-12-22 14:19:22 +0100

    Bug Fix: Login page & query strings cache issue from 2.3.8

--- inc/admin/plugin-compatibility.php
+++ inc/admin/plugin-compatibility.php
@@ -35,3 +35,20 @@ function __rocket_after_update_edd_options( $old_value, $value ) {
 		rocket_generate_config_file();	
 	}
 }
+
+/**
+ * Compatibility with an usual NGINX configuration which include 
+ * try_files $uri $uri/ /index.php?q=$uri&$args
+ *
+ * @since 2.3.9
+ */
+add_filter( 'rocket_cache_query_strings', '__rocket_better_nginx_compatibility' );
+function __rocket_better_nginx_compatibility( $query_strings ) {
+	global $is_nginx;
+	
+	if ( $is_nginx ) {
+		$query_strings[] = 'q';
+	}
+	
+	return $query_strings;
+}
--- inc/front/process.php
+++ inc/front/process.php
@@ -70,7 +70,6 @@ $request_uri = ( isset( $rocket_cache_query_strings ) && array_intersect( array_
 
 // Don't cache with variables
 // but the cache is enabled if the visitor comes from an RSS feed or an Facebook action
-// @since 2.3.8	Add NGINX compatibility with some configuration includes a "q" in query strings
 // @since 2.3 	Add query strings which can be cached via the options page.
 // @since 2.1 	Add compatibilty with WordPress Landing Pages (permalink_name and lp-variation-id)
 // @since 2.1 	Add compabitiliy with qTranslate and translation plugin with query string "lang"
@@ -82,7 +81,6 @@ if ( ! empty( $_GET )
 	&& ( ! isset( $_GET['lang'] ) )
 	&& ( ! isset( $_GET['s'] ) )
 	&& ( ! isset( $rocket_cache_query_strings ) || ! array_intersect( array_keys( $_GET ), $rocket_cache_query_strings ) )
-	&& ( strpos($_SERVER['SERVER_SOFTWARE'], 'nginx') !== false && ! isset( $_GET['q'] ) )
 )
 	return;
 
