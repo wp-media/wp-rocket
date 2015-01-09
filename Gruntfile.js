b9b1c299033dc399fa0d36e431bb05a241817a0b
fxbenard <fxbenard@gmail.com>
2015-01-09 18:16:29 +0100

    Change pourcentage tx pull à 90%

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ grunt.initConfig({
 
     exec: {
       txpull: { // Pull Transifex translation - grunt exec:txpull
-        cmd: 'tx pull -a --minimum-perc=95' // Change the percentage with --minimum-perc=yourvalue
+        cmd: 'tx pull -a --minimum-perc=90' // Change the percentage with --minimum-perc=yourvalue
       },
       txpush_s: { // Push pot to Transifex - grunt exec:txpush_s
         cmd: 'tx push -s'
