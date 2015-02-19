ec1fd57ee9e4076e426fb5dbc6698361bc587959
Jonathan Buttigieg <contact@geekpress.fr>
2015-02-19 11:57:28 +0100

    Update the "Cache Options" metabox content

--- inc/admin/admin.php
+++ inc/admin/admin.php
@@ -389,7 +389,7 @@ function __rocket_cache_options_meta_boxes() {
 
 	foreach( $cpts as $cpt => $cpt_object ) {
 		$label = $cpt_object->labels->singular_name;
-		add_meta_box( 'rocket_post_exclude', sprintf( __( 'Cache Options for this %s', 'rocket' ), $label ), '__rocket_display_cache_options_meta_boxes', $cpt, 'side', 'core' );		
+		add_meta_box( 'rocket_post_exclude', sprintf( __( 'Cache Options', 'rocket' ), $label ), '__rocket_display_cache_options_meta_boxes', $cpt, 'side', 'core' );
 	}
 
 }
@@ -400,37 +400,41 @@ function __rocket_cache_options_meta_boxes() {
  * @since 2.5
  */
 function __rocket_display_cache_options_meta_boxes() {
-						/** This filter is documented in inc/admin-bar.php */
-	if ( current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) ) { 
+	/** This filter is documented in inc/admin-bar.php */
+	if ( current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) ) {
 		global $post;
 		wp_nonce_field( 'rocket_box_option_' . $post->ID, '_rocketnonce', false, true );
 		?>
-	
+
 		<div class="misc-pub-section">
+			<p><?php _e( 'Activate these options on this page:', 'rocket' ) ;?></p>
 			<?php
-			$fields = array( 
-							'lazyload' 		=> __( 'LazyLoad', 'rocket' ), 
-							'minify_html'	=> __( 'HTML Minification', 'rocket' ), 
-							'minify_css'	=> __( 'CSS Minification', 'rocket' ), 
-							'minify_js' 	=> __( 'JS Minification', 'rocket' ), 
-							'cdn'			=> __( 'CDN', 'rocket' ),
-						);
+			$fields = array(
+				'lazyload' 		=> __( 'LazyLoad', 'rocket' ),
+				'minify_html'	=> __( 'HTML Minification', 'rocket' ),
+				'minify_css'	=> __( 'CSS Minification', 'rocket' ),
+				'minify_js' 	=> __( 'JS Minification', 'rocket' ),
+				'cdn'			=> __( 'CDN', 'rocket' ),
+			);
 
 			foreach ( $fields as $field => $label ) {
 				$disabled = disabled( ! get_rocket_option( $field ), true, false );
-				$title = $disabled ? ' title="' . sprintf( __( 'Activate first the %s option.', 'rocket' ), esc_attr( $label ) ) . '"' : '';
-				$class = $disabled ? ' class="rkt-disabled"' : '';
-				$checked = ! $disabled ? checked( ! get_post_meta( $post->ID, '_rocket_exclude_' . $field, true ), true, false ) : '';
+				$title    = $disabled ? ' title="' . sprintf( __( 'Activate first the %s option.', 'rocket' ), esc_attr( $label ) ) . '"' : '';
+				$class    = $disabled ? ' class="rkt-disabled"' : '';
+				$checked   = ! $disabled ? checked( ! get_post_meta( $post->ID, '_rocket_exclude_' . $field, true ), true, false ) : '';
  				?>
+
 				<input name="rocket_post_exclude_hidden[<?php echo $field; ?>]" type="hidden" value="on">
 				<input name="rocket_post_exclude[<?php echo $field; ?>]" id="rocket_post_exclude_<?php echo $field; ?>" type="checkbox"<?php echo $title; ?><?php echo $checked; ?><?php echo $disabled; ?>>
 				<label for="rocket_post_exclude_<?php echo $field; ?>"<?php echo $title; ?><?php echo $class; ?>><?php echo $label; ?></label><br>
+
 				<?php
 			}
 			?>
-			<p class="rkt-note"><?php _e( 'Note: Minification and CDN options only works for cached page.', 'rocket' ); ?></p>
+
+			<p class="rkt-note"><?php _e( '<strong>Note:</strong> These options aren\'t applied if you added this post in the "Never cache the following pages" option.', 'rocket' ); ?></p>
 		</div>
-	
+
 	<?php
 	}
 }
@@ -442,15 +446,13 @@ function __rocket_display_cache_options_meta_boxes() {
  */
 add_action( 'save_post', '__rocket_save_metabox_options' );
 function __rocket_save_metabox_options() {
-
 	if ( isset( $_POST['post_ID'], $_POST['rocket_post_exclude_hidden'], $_POST['_rocketnonce'] ) ) {
-		
+
 		check_admin_referer( 'rocket_box_option_' . $_POST['post_ID'], '_rocketnonce' );
 
 		$fields = array( 'lazyload', 'minify_html', 'minify_css', 'minify_js', 'cdn' );
 
 		foreach ( $fields as $field ) {
-
 			if ( isset( $_POST['rocket_post_exclude_hidden'][ $field ] ) && $_POST['rocket_post_exclude_hidden'][ $field ] ) {
 				if ( isset( $_POST['rocket_post_exclude'][ $field ] ) ) {
 					delete_post_meta( $_POST['post_ID'], '_rocket_exclude_' . $field );
@@ -460,9 +462,6 @@ function __rocket_save_metabox_options() {
 					}
 				}
 			}
-
 		}
-
 	}
-
 }
