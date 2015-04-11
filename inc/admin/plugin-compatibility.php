116ea95074ffbd263ae6bad0ee688034ba743046
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-04-11 15:17:59 +0200

    Remove one if condition to improve the code

--- inc/admin/plugin-compatibility.php
+++ inc/admin/plugin-compatibility.php
@@ -62,22 +62,20 @@ function __rocket_better_nginx_compatibility( $query_strings ) {
  */
 add_action( 'admin_init', '__rocket_clear_cache_after_studiopress_accelerator' );
 function __rocket_clear_cache_after_studiopress_accelerator() {
-	if ( isset( $GLOBALS['sp_accel_nginx_proxy_cache_purge'] ) && is_a( $GLOBALS['sp_accel_nginx_proxy_cache_purge'], 'SP_Accel_Nginx_Proxy_Cache_Purge' ) ) {
-		if (isset($_REQUEST['_wpnonce'])) {
-			$nonce = $_REQUEST['_wpnonce'];
-			if (wp_verify_nonce($nonce, 'sp-accel-purge-url') && !empty($_REQUEST['cache-purge-url'])) {
-				$submitted_url = $_REQUEST['cache-purge-url'];
-				
-				// Clear the URL
-				rocket_clean_files( array( $submitted_url ) );
-			} else if (wp_verify_nonce($nonce, 'sp-accel-purge-theme')) {
-				// Clear all caching files
-				rocket_clean_domain();
-				
-				// Preload cache
-				run_rocket_bot( 'cache-preload' );
-			}
-		}
+	if ( isset( $GLOBALS['sp_accel_nginx_proxy_cache_purge'] ) && is_a( $GLOBALS['sp_accel_nginx_proxy_cache_purge'], 'SP_Accel_Nginx_Proxy_Cache_Purge' ) && isset( $_REQUEST['_wpnonce'] ) ) {	
+		$nonce = $_REQUEST['_wpnonce'];
+		if (wp_verify_nonce($nonce, 'sp-accel-purge-url') && !empty($_REQUEST['cache-purge-url'])) {
+			$submitted_url = $_REQUEST['cache-purge-url'];
+			
+			// Clear the URL
+			rocket_clean_files( array( $submitted_url ) );
+		} else if (wp_verify_nonce($nonce, 'sp-accel-purge-theme')) {
+			// Clear all caching files
+			rocket_clean_domain();
+			
+			// Preload cache
+			run_rocket_bot( 'cache-preload' );
+		}	
 	}
 }
 
