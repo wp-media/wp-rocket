29d4bae7daed1be6d4297ca2e8ab728976cbb006
Julio Potier <juliobosk@gmail.com>
2015-05-26 15:12:20 +0200

    .dev -> .min
    WP is using a .js / .min.js
    + usage of SCRIPT_DEBUG

--- inc/front/js/lazyload.1.0.1.js
+++ inc/front/js/lazyload.1.0.1.js
@@ -1,9 +1,492 @@
-(function(d,g){var c,M=g.documentElement,h=d.addEventListener,p=d.setTimeout,R=d.requestAnimationFrame||p,V=/^picture$/i,N=["load","error","lazyincluded","_lazyloaded"],G=function(a,e){var c=new RegExp("(\\s|^)"+e+"(\\s|$)");return a.className.match(c)&&c},m=function(a,e){G(a,e)||(a.className+=" "+e)},O=function(a,e){var c;if(c=G(a,e))a.className=a.className.replace(c," ")},C=function(a,c,f){var l=f?"addEventListener":"removeEventListener";f&&C(a,c);N.forEach(function(f){a[l](f,c)})},S=function(a,
-c,f,l,d){var k=g.createEvent("CustomEvent");k.initCustomEvent(c,!l,!d,f);k.details=k.detail;a.dispatchEvent(k);return k},T=function(a,e){var f;d.HTMLPictureElement||((f=d.picturefill||d.respimage||c.pf)?f({reevaluate:!0,reparse:!0,elements:[a]}):e&&e.src&&(a.src=e.src))},E=function(a){a=g.querySelectorAll("[data-lazy-src]:not(.lazyload),[data-lazy-original]:not(.lazyload)");for(var e=0;e<a.length;e++)m(a[e],c.lazyClass)},W=function(a){var e,f=0,l=d.Date,g=function(){e=!1;f=l.now();a()},k=function(){p(g)},
-m=function(){R(k)};return function(a){if(!e){var d=c.throttle-(l.now()-f);e=!0;4>d&&(d=4);!0===a?m():p(m,d)}}},L=function(){var a,e,f,l,D,k,x,y,z,A,B,H,P,L=/^img$/i,N=/^iframe$/i,X="onscroll"in d&&!/glebot/.test(navigator.userAgent),F=0,v=0,u=0,I=function(b){u--;b&&b.target&&C(b.target,I);if(!b||0>u||!b.target)u=0},n=W(function(){var b,t,d,l,g,q,m,J,n;if((D=c.loadMode)&&8>u&&(b=a.length)){t=0;v++;F<P&&1>u&&4<v&&2<D?(F=P,v=0):F=F!=H&&1<D&&3<v?H:0;for(;t<b;t++)if(a[t]&&!a[t]._lazyRace)if(X){(J=a[t].getAttribute("data-expand"))&&
-(q=1*J)||(q=F);n!==q&&(k=innerWidth+q,x=innerHeight+q,m=-1*q,n=q);d=a[t].getBoundingClientRect();var r;if((r=(B=d.bottom)>=m&&(y=d.top)<=x&&(A=d.right)>=m&&(z=d.left)<=k&&(B||A||z||y))&&!(r=f&&3>u&&4>v&&!J&&2<D)){var w=a[t];r=q;var p=void 0,h=w,w="hidden"!=getComputedStyle(w,null).visibility;y-=r;B+=r;z-=r;for(A+=r;w&&(h=h.offsetParent);)(w=0<(getComputedStyle(h,null).opacity||1))&&"visible"!=getComputedStyle(h,null).overflow&&(p=h.getBoundingClientRect(),w=A>p.left&&z<p.right&&B>p.top-1&&y<p.bottom+
-1);r=w}r?(K(a[t],!1,d.width),g=!0):!g&&f&&!l&&3>u&&4>v&&2<D&&(e[0]||c.preloadAfterLoad)&&(e[0]||!J&&(B||A||z||y))&&(l=e[0]||a[t])}else K(a[t]);l&&!g&&K(l)}}),Q=function(b){m(b.target,c.loadedClass);O(b.target,c.loadingClass);C(b.target,Q)},Y=function(){var b,a=[],c=function(){for(;a.length;)a.shift()();b=!1};return function(e){a.push(e);b||(b=!0,R(c))}}(),K=function(b,a,e){var d,g,q,h,k,n=b.currentSrc||b.src,r=L.test(b.nodeName);if(f||!r||!n||b.complete||G(b,c.errorClass))b._lazyRace=!0,u++,Y(function(){b._lazyRace&&
-delete b._lazyRace;O(b,c.lazyClass);if(!(h=S(b,"lazybeforeunveil",{force:!!a})).defaultPrevented){m(b,c.autosizesClass);d=b.getAttribute("data-lazy-src")||b.getAttribute("data-lazy-original");r&&(q=(g=b.parentNode)&&V.test(g.nodeName||""));k=h.detail.firesLoad||"src"in b&&(d||q);h={target:b};k&&(C(b,I,!0),clearTimeout(l),l=p(I,2500),m(b,c.loadingClass),C(b,Q,!0));if(d)if(N.test(b.nodeName)){var e=d;try{b.contentWindow.location.replace(e)}catch(f){b.setAttribute("src",e)}}else b.setAttribute("src",
-d);q&&T(b,{src:d})}if(!k||b.complete&&n==(b.currentSrc||b.src))k?I(h):u--,Q(h)})},U=function(){var b,a=function(){c.loadMode=3;n()};f=!0;v+=8;c.loadMode=3;h("scroll",function(){3==c.loadMode&&(c.loadMode=2);clearTimeout(b);b=p(a,99)},!0)};return{_:function(){a=g.getElementsByClassName(c.lazyClass);e=g.getElementsByClassName(c.lazyClass+" "+c.preloadClass);H=c.expand;P=Math.round(H*c.expFactor);h("scroll",n,!0);h("resize",n,!0);d.MutationObserver?(new MutationObserver(E)).observe(M,{childList:!0,subtree:!0,
-attributes:!0}):(M.addEventListener("DOMNodeInserted",E,!0),M.addEventListener("DOMAttrModified",E,!0),setInterval(E,999));h("hashchange",E,!0);"focus mouseover click load transitionend animationend webkitAnimationEnd".split(" ").forEach(function(a){g.addEventListener(a,n,!0)});(f=/d$|^c/.test(g.readyState))?U():(h("load",U),g.addEventListener("DOMContentLoaded",n));n(0<a.length)},checkElems:n,unveil:K}}(),x=function(){x.i||(x.i=!0,L._())};(function(){var a,e={lazyClass:"lazyload",loadedClass:"lazyloaded",
-loadingClass:"lazyloading",preloadClass:"lazypreload",errorClass:"lazyerror",autosizesClass:"lazyautosizes",init:!0,expFactor:2,expand:359,loadMode:2,throttle:99};c=d.lazyRocketsConfig||d.lazyRocketsConfig||{};for(a in e)a in c||(c[a]=e[a]);d.lazyRocketsConfig=c;p(function(){if(c.init){for(var a=g.querySelectorAll("[data-lazy-src],[data-lazy-original]"),d=0;d<a.length;d++)m(a[d],c.lazyClass);x()}})})();return{cfg:c,loader:L,init:x,uP:T,aC:m,rC:O,hC:G,fire:S}})(window,document);
+(function(window, document) {
+	'use strict';
+
+	var lazyRocketsConfig;
+
+	var docElem = document.documentElement;
+
+	var addEventListener = window.addEventListener;
+
+	var setTimeout = window.setTimeout;
+
+	var rAF = window.requestAnimationFrame || setTimeout;
+
+	var regPicture = /^picture$/i;
+
+	var loadEvents = ['load', 'error', 'lazyincluded', '_lazyloaded'];
+
+	var hasClass = function(ele, cls) {
+		var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+		return ele.className.match(reg) && reg;
+	};
+
+	var addClass = function(ele, cls) {
+		if (!hasClass(ele, cls)){
+			ele.className += ' '+cls;
+		}
+	};
+
+	var removeClass = function(ele, cls) {
+		var reg;
+		if ((reg = hasClass(ele,cls))) {
+			ele.className = ele.className.replace(reg, ' ');
+		}
+	};
+
+	var addRemoveLoadEvents = function(dom, fn, add){
+		var action = add ? 'addEventListener' : 'removeEventListener';
+		if(add){
+			addRemoveLoadEvents(dom, fn);
+		}
+		loadEvents.forEach(function(evt){
+			dom[action](evt, fn);
+		});
+	};
+
+	var triggerEvent = function(elem, name, detail, noBubbles, noCancelable){
+		var event = document.createEvent('CustomEvent');
+
+		event.initCustomEvent(name, !noBubbles, !noCancelable, detail);
+
+		event.details =  event.detail;
+
+		elem.dispatchEvent(event);
+		return event;
+	};
+
+	var updatePolyfill = function (el, full){
+		var polyfill;
+		if(!window.HTMLPictureElement){
+			if( ( polyfill = (window.picturefill || window.respimage || lazyRocketsConfig.pf) ) ){
+				polyfill({reevaluate: true, reparse: true, elements: [el]});
+			} else if(full && full.src){
+				el.src = full.src;
+			}
+		}
+	};
+
+	var getCSS = function (elem, style){
+		return getComputedStyle(elem, null)[style];
+	};
+
+	var prepareThrottledCheckElements = function(fn){
+		var noPreparedElems = document.querySelectorAll('[data-lazy-src]:not(.lazyload),[data-lazy-original]:not(.lazyload)');
+		
+		for (var i = 0; i < noPreparedElems.length; i++) {
+			addClass(noPreparedElems[i], lazyRocketsConfig.lazyClass);
+		}
+	}
+
+	var throttle = function(fn){
+		var running;
+		var lastTime = 0;
+		var Date = window.Date;
+		var run = function(){
+			running = false;
+			lastTime = Date.now();
+			fn();
+		};
+		var afterAF = function(){
+			setTimeout(run);
+		};
+		var getAF = function(){
+			rAF(afterAF);
+		};
+
+		return function(force){
+			if(running){
+				return;
+			}
+			var delay = lazyRocketsConfig.throttle - (Date.now() - lastTime);
+
+			running =  true;
+
+			if(delay < 4){
+				delay = 4;
+			}
+
+			if(force === true){
+				getAF();
+			} else {
+				setTimeout(getAF, delay);
+			}
+		};
+	};
+
+	var loader = (function(){
+		var lazyloadElems, preloadElems, isCompleted, resetPreloadingTimer, loadMode;
+
+		var eLvW, elvH, eLtop, eLleft, eLright, eLbottom;
+
+		var defaultExpand, preloadExpand;
+
+		var regImg = /^img$/i;
+		var regIframe = /^iframe$/i;
+
+		var supportScroll = ('onscroll' in window) && !(/glebot/.test(navigator.userAgent));
+
+		var shrinkExpand = 0;
+		var currentExpand = 0;
+		var lowRuns = 0;
+
+		var isLoading = 0;
+
+		var resetPreloading = function(e){
+			isLoading--;
+			if(e && e.target){
+				addRemoveLoadEvents(e.target, resetPreloading);
+			}
+
+			if(!e || isLoading < 0 || !e.target){
+				isLoading = 0;
+			}
+		};
+
+		var isNestedVisible = function(elem, elemExpand){
+			var outerRect;
+			var parent = elem;
+			var visible = getCSS(elem, 'visibility') != 'hidden';
+
+			eLtop -= elemExpand;
+			eLbottom += elemExpand;
+			eLleft -= elemExpand;
+			eLright += elemExpand;
+
+			while(visible && (parent = parent.offsetParent)){
+				visible = ((getCSS(parent, 'opacity') || 1) > 0);
+
+				if(visible && getCSS(parent, 'overflow') != 'visible'){
+					outerRect = parent.getBoundingClientRect();
+					visible = eLright > outerRect.left &&
+					eLleft < outerRect.right &&
+					eLbottom > outerRect.top - 1 &&
+					eLtop < outerRect.bottom + 1
+					;
+				}
+			}
+
+			return visible;
+		};
+
+		var checkElements = function() {
+			var eLlen, i, rect, autoLoadElem, loadedSomething, elemExpand, elemNegativeExpand, elemExpandVal, beforeExpandVal;
+
+			if((loadMode = lazyRocketsConfig.loadMode) && isLoading < 8 && (eLlen = lazyloadElems.length)){
+
+				i = 0;
+
+				lowRuns++;
+
+				if(currentExpand < preloadExpand && isLoading < 1 && lowRuns > 4 && loadMode > 2){
+					currentExpand = preloadExpand;
+					lowRuns = 0;
+				} else if(currentExpand != defaultExpand && loadMode > 1 && lowRuns > 3){
+					currentExpand = defaultExpand;
+				} else {
+					currentExpand = shrinkExpand;
+				}
+
+				for(; i < eLlen; i++){
+
+					if(!lazyloadElems[i] || lazyloadElems[i]._lazyRace){continue;}
+
+					if(!supportScroll){unveilElement(lazyloadElems[i]);continue;}
+
+					if(!(elemExpandVal = lazyloadElems[i].getAttribute('data-expand')) || !(elemExpand = elemExpandVal * 1)){
+						elemExpand = currentExpand;
+					}
+
+					if(beforeExpandVal !== elemExpand){
+						eLvW = innerWidth + elemExpand;
+						elvH = innerHeight + elemExpand;
+						elemNegativeExpand = elemExpand * -1;
+						beforeExpandVal = elemExpand;
+					}
+
+					rect = lazyloadElems[i].getBoundingClientRect();
+
+					if ((eLbottom = rect.bottom) >= elemNegativeExpand &&
+						(eLtop = rect.top) <= elvH &&
+						(eLright = rect.right) >= elemNegativeExpand &&
+						(eLleft = rect.left) <= eLvW &&
+						(eLbottom || eLright || eLleft || eLtop) &&
+						((isCompleted && isLoading < 3 && lowRuns < 4 && !elemExpandVal && loadMode > 2) || isNestedVisible(lazyloadElems[i], elemExpand))){
+						unveilElement(lazyloadElems[i], false, rect.width);
+						loadedSomething = true;
+					} else if(!loadedSomething && isCompleted && !autoLoadElem &&
+						isLoading < 3 && lowRuns < 4 && loadMode > 2 &&
+						(preloadElems[0] || lazyRocketsConfig.preloadAfterLoad) &&
+						(preloadElems[0] || (!elemExpandVal && ((eLbottom || eLright || eLleft || eLtop) /*|| lazyloadElems[i].getAttribute(lazyRocketsConfig.sizesAttr) != 'auto'*/)))){
+						autoLoadElem = preloadElems[0] || lazyloadElems[i];
+					}
+				}
+
+				if(autoLoadElem && !loadedSomething){
+					unveilElement(autoLoadElem);
+				}
+			}
+		};
+
+		var throttledCheckElements = throttle(checkElements);
+
+		var switchLoadingClass = function(e){
+			addClass(e.target, lazyRocketsConfig.loadedClass);
+			removeClass(e.target, lazyRocketsConfig.loadingClass);
+			addRemoveLoadEvents(e.target, switchLoadingClass);
+		};
+
+		var changeIframeSrc = function(elem, src){
+			try {
+				elem.contentWindow.location.replace(src);
+			} catch(e){
+				elem.setAttribute('src', src);
+			}
+		};
+
+		var rafBatch = (function(){
+			var isRunning;
+			var batch = [];
+			var runBatch = function(){
+				while(batch.length){
+					(batch.shift())();
+				}
+				isRunning = false;
+			};
+			return function(fn){
+				batch.push(fn);
+				if(!isRunning){
+					isRunning = true;
+					rAF(runBatch);
+				}
+			};
+		})();
+
+		var unveilElement = function (elem, force, width){
+			var sources, i, len, sourceSrcset, src, srcset, parent, isPicture, event, firesLoad, customMedia;
+
+			var curSrc = elem.currentSrc || elem.src;
+			var isImg = regImg.test(elem.nodeName);
+
+			var sizes = null;
+			var isAuto = sizes == 'auto';
+
+			if( (isAuto || !isCompleted) && isImg && curSrc && !elem.complete && !hasClass(elem, lazyRocketsConfig.errorClass)){return;}
+
+			elem._lazyRace = true;
+			isLoading++;
+
+			rafBatch(function lazyUnveil(){
+
+				if(elem._lazyRace){
+					delete elem._lazyRace;
+				}
+
+				removeClass(elem, lazyRocketsConfig.lazyClass);
+
+				if(!(event = triggerEvent(elem, 'lazybeforeunveil', {force: !!force})).defaultPrevented){
+
+					/*if(sizes){
+						if(isAuto){*/
+							addClass(elem, lazyRocketsConfig.autosizesClass);
+						//} else {
+							//elem.setAttribute('sizes', sizes);
+						//}
+					//}
+
+					//srcset = elem.getAttribute(lazyRocketsConfig.srcsetAttr);
+					srcset = null;
+					
+					src = elem.getAttribute('data-lazy-src') || elem.getAttribute('data-lazy-original');
+
+					if(isImg) {
+						parent = elem.parentNode;
+						isPicture = parent && regPicture.test(parent.nodeName || '');
+					}
+
+					firesLoad = event.detail.firesLoad || (('src' in elem) && (srcset || src || isPicture));
+
+					event = {target: elem};
+
+					if(firesLoad){
+						addRemoveLoadEvents(elem, resetPreloading, true);
+						clearTimeout(resetPreloadingTimer);
+						resetPreloadingTimer = setTimeout(resetPreloading, 2500);
+
+						addClass(elem, lazyRocketsConfig.loadingClass);
+						addRemoveLoadEvents(elem, switchLoadingClass, true);
+					}
+
+					/*if(isPicture){
+						sources = parent.getElementsByTagName('source');
+						for(i = 0, len = sources.length; i < len; i++){
+							if( (customMedia = lazyRocketsConfig.customMedia[sources[i].getAttribute('data-media') || sources[i].getAttribute('media')]) ){
+								sources[i].setAttribute('media', customMedia);
+							}
+							sourceSrcset = sources[i].getAttribute(lazyRocketsConfig.srcsetAttr);
+							if(sourceSrcset){
+								sources[i].setAttribute('srcset', sourceSrcset);
+							}
+						}
+					}*/
+
+					if(src){
+						if(regIframe.test(elem.nodeName)){
+							changeIframeSrc(elem, src);
+						} else {
+							elem.setAttribute('src', src);
+						}
+					}
+
+					if(isPicture){
+						updatePolyfill(elem, {src: src});
+					}
+				}
+
+				if( !firesLoad || (elem.complete && curSrc == (elem.currentSrc || elem.src)) ){
+					if(firesLoad){
+						resetPreloading(event);
+					} else {
+						isLoading--;
+					}
+					switchLoadingClass(event);
+				}
+			});
+		};
+
+		var onload = function(){
+			var scrollTimer;
+			var afterScroll = function(){
+				lazyRocketsConfig.loadMode = 3;
+				throttledCheckElements();
+			};
+
+			isCompleted = true;
+			lowRuns += 8;
+
+			lazyRocketsConfig.loadMode = 3;
+
+			addEventListener('scroll', function(){
+				if(lazyRocketsConfig.loadMode == 3){
+					lazyRocketsConfig.loadMode = 2;
+				}
+				clearTimeout(scrollTimer);
+				scrollTimer = setTimeout(afterScroll, 99);
+			}, true);
+		};
+
+		return {
+			_: function(){
+
+				lazyloadElems = document.getElementsByClassName(lazyRocketsConfig.lazyClass);
+				preloadElems = document.getElementsByClassName(lazyRocketsConfig.lazyClass + ' ' + lazyRocketsConfig.preloadClass);
+				
+				/*lazyloadElems = document.querySelectorAll('[data-lazy-src],[data-lazy-original]');
+				preloadElems = document.querySelectorAll('[data-lazy-src].' + lazyRocketsConfig.preloadClass + ', [data-lazy-original].' + lazyRocketsConfig.preloadClass);*/
+
+				defaultExpand = lazyRocketsConfig.expand;
+				preloadExpand = Math.round(defaultExpand * lazyRocketsConfig.expFactor);
+
+				addEventListener('scroll', throttledCheckElements, true);
+
+				addEventListener('resize', throttledCheckElements, true);
+
+				if(window.MutationObserver){
+					new MutationObserver( prepareThrottledCheckElements ).observe( docElem, {childList: true, subtree: true, attributes: true} );
+				} else {
+					docElem.addEventListener('DOMNodeInserted', prepareThrottledCheckElements, true);
+					docElem.addEventListener('DOMAttrModified', prepareThrottledCheckElements, true);
+					setInterval(prepareThrottledCheckElements, 999);
+				}
+
+				addEventListener('hashchange', prepareThrottledCheckElements, true);
+
+				/*if(window.MutationObserver){
+					new MutationObserver( throttledCheckElements ).observe( docElem, {childList: true, subtree: true, attributes: true} );
+				} else {
+					docElem.addEventListener('DOMNodeInserted', throttledCheckElements, true);
+					docElem.addEventListener('DOMAttrModified', throttledCheckElements, true);
+					setInterval(throttledCheckElements, 999);
+				}
+
+				addEventListener('hashchange', throttledCheckElements, true);*/
+
+				['focus', 'mouseover', 'click', 'load', 'transitionend', 'animationend', 'webkitAnimationEnd'].forEach(function(name){
+					document.addEventListener(name, throttledCheckElements, true);
+				});
+
+				if(!(isCompleted = /d$|^c/.test(document.readyState))){
+					addEventListener('load', onload);
+					document.addEventListener('DOMContentLoaded', throttledCheckElements);
+				} else {
+					onload();
+				}
+
+				throttledCheckElements(lazyloadElems.length > 0);
+			},
+			checkElems: throttledCheckElements,
+			unveil: unveilElement
+		};
+	})();
+
+	var init = function(){
+		if(!init.i){
+			init.i = true;
+			loader._();
+		}
+	};
+
+	(function(){
+		var prop;
+		var lazyRocketsDefaults = {
+			lazyClass: 'lazyload',
+			loadedClass: 'lazyloaded',
+			loadingClass: 'lazyloading',
+			preloadClass: 'lazypreload',
+			errorClass: 'lazyerror',
+			autosizesClass: 'lazyautosizes',
+			//srcAttr: 'data-src',
+			//srcsetAttr: 'data-srcset',
+			//sizesAttr: 'data-sizes',
+			//minSize: 40,
+			//customMedia: {},
+			init: true,
+			expFactor: 2,
+			expand: 359,
+			loadMode: 2,
+			throttle: 99
+		};
+
+		lazyRocketsConfig = window.lazyRocketsConfig || window.lazyRocketsConfig || {};
+
+		for(prop in lazyRocketsDefaults){
+			if(!(prop in lazyRocketsConfig)){
+				lazyRocketsConfig[prop] = lazyRocketsDefaults[prop];
+			}
+		}
+
+		window.lazyRocketsConfig = lazyRocketsConfig;
+
+		setTimeout(function(){
+			if(lazyRocketsConfig.init){
+
+				var tmpElems = document.querySelectorAll('[data-lazy-src],[data-lazy-original]');
+				for (var i = 0; i < tmpElems.length; i++) {
+					addClass(tmpElems[i], lazyRocketsConfig.lazyClass);
+				};
+
+				init();
+			}
+		});
+	})();
+
+	return {
+		cfg: lazyRocketsConfig,
+		loader: loader,
+		init: init,
+		uP: updatePolyfill,
+		aC: addClass,
+		rC: removeClass,
+		hC: hasClass,
+		fire: triggerEvent
+	};
+})(window, document);
