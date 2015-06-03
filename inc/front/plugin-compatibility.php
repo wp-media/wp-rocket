a74d07053be25931298322fa30d003604791c0b6
Julio Potier <juliobosk@gmail.com>
2015-06-03 17:47:47 +0200

    Rename avada patch callback

--- inc/front/plugin-compatibility.php
+++ inc/front/plugin-compatibility.php
@@ -187,6 +187,8 @@ function __rocket_fix_mailchimp_main_css() {
  *
  * @since 2.6.1
  */
+add_filter( 'less_vars', '__rocket_fix_cdn_for_avada_theme', 11, 2 );
+function __rocket_fix_cdn_for_avada_theme( $vars, $handle ) {
 	if( $handle == 'avada-dynamic' && get_rocket_option( 'cdn' ) ) { 
 		$src = get_rocket_cdn_url( get_template_directory_uri() . '/assets/less/theme/dynamic.less' );
 		$vars['template-directory'] = sprintf( '~"%s"', dirname( dirname( dirname( dirname( $src ) ) ) ) );
