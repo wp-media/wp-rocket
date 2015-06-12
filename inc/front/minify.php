f7ad612579ae089920c9ea5c57822887aabbf786
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-06-12 09:09:05 -0400

    Bug Fix (part #2): Don't duplicate enqueued external JS files when JS minification is enabled

--- inc/front/minify.php
+++ inc/front/minify.php
@@ -554,13 +554,18 @@ function __rocket_extract_js_files_from_footer() {
 	foreach( $wp_scripts->in_footer as $handle ) {
 		$script_src  = $wp_scripts->registered[ $handle ]->src;
 		$script_src  = ( strstr( $script_src, '/wp-includes/js/') ) ? $wp_scripts->base_url . $script_src : $script_src;
-		
 		$script_src_cleaned = str_replace( array( 'http:', 'https:', '//' . $home_host ), '', $script_src );
 
 		if( in_array( $handle, $wp_scripts->queue ) && ! in_array( parse_url( $script_src, PHP_URL_HOST ), $excluded_external_js ) && ! in_array( $script_src, $deferred_js_files ) && ! in_array( parse_url( $script_src, PHP_URL_PATH ), $excluded_js ) && ! in_array( parse_url( $script_src_cleaned, PHP_URL_PATH ), $excluded_js ) ) {			
+			
 			// Dequeue JS files without extension
 			if( pathinfo( $script_src, PATHINFO_EXTENSION ) == '' ) {
 				wp_dequeue_script( $handle );
+			}
+			
+			// Add protocol on external JS to prevent conflict
+			if( $home_host != parse_url( $script_src, PHP_URL_HOST ) && strpos( $script_src, 'http://' ) === false && strpos( $script_src, 'https://' ) === false ) {
+				$script_src = set_url_scheme( $script_src );
 			}
 			
 			// Add dependency enqueued in the footer
