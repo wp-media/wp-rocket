0683bfa4753ecb0f1534c9dbb9edd704190130db
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-08-07 11:47:29 +0200

    Remove an unless check

--- inc/front/process.php
+++ inc/front/process.php
@@ -1,11 +1,6 @@
 <?php
 defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
 
-// Don't cache WP javascript generators
-if ( strstr( $_SERVER['SCRIPT_FILENAME'], 'wp-includes/js' ) ) {
-	return;
-}
-
 // Don't cache robots.txt && .htaccess directory (it's happened sometimes with weird server configuration)
 if ( strstr( $_SERVER['REQUEST_URI'], 'robots.txt' ) || strstr( $_SERVER['REQUEST_URI'], '.htaccess' ) ) {
 	return;
