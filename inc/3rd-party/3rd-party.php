6631e52890f96617796e7dd7b3874b58e05fc9ab
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-08-13 12:59:30 +0200

    Move hostings compatibility in a dedicated folder

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -1,11 +1,11 @@
 <?php 
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
-require( WP_ROCKET_3RD_PARTY_PATH . 'wpengine.php' );
-require( WP_ROCKET_3RD_PARTY_PATH . 'godaddy.php' );
+require( WP_ROCKET_3RD_PARTY_PATH . 'hosting/wpengine.php' );
+require( WP_ROCKET_3RD_PARTY_PATH . 'hosting/godaddy.php' );
 
 if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
-	require( WP_ROCKET_3RD_PARTY_PATH . 'savvii.php' );
+	require( WP_ROCKET_3RD_PARTY_PATH . 'hosting/savvii.php' );
 }
 
 require( WP_ROCKET_3RD_PARTY_PATH . 'kk-star-ratings.php' );
