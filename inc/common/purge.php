623dc3e088740e92db58ff5a6895ded306d82059
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-08-18 10:45:35 +0200

    Fix a possible PHP Notice

--- inc/common/purge.php
+++ inc/common/purge.php
@@ -90,7 +90,7 @@ function rocket_clean_post( $post_id )
 	$post = get_post( $post_id );
 	
 	// No purge for specifics conditions
-	if ( $post->post_status == 'auto-draft' || empty( $post->post_type ) || $post->post_type == 'nav_menu_item' ) {
+	if ( ! is_object( $post ) || $post->post_status == 'auto-draft' || empty( $post->post_type ) || $post->post_type == 'nav_menu_item' ) {
 		return;
 	}
 	
