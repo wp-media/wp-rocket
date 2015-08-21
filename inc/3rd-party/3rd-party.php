4b8f26cfb6390e5a810cf8d7a64e5ef26660ecfb
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-08-21 23:43:04 +0200

    Bug Fix: Clear the homepage when the "Use directory for default language" option is activated on WPML.

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -10,5 +10,6 @@ if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
 	require( WP_ROCKET_3RD_PARTY_PATH . 'hosting/savvii.php' );
 }
 
+require( WP_ROCKET_3RD_PARTY_PATH . 'i18n/wpml.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'kk-star-ratings.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'wp-postratings.php' );
