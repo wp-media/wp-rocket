b6861dc27f287e85a567711690d99d3bd02bcc8c
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-09-29 15:40:52 +0200

    Bug Fix: Auto-deactivate the Inline JS option on websites hosted by WP Serveur to prevent PHP errors issues.

---
+++ inc/3rd-party/hosting/wp-serveur.php
@@ -0,0 +1,15 @@
+<?php 
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+/**
+ * Conflict with WP Serveur hosting: don't apply inline JS on all pages
+ *
+ * @since 2.6.11
+ */
+add_action( 'rocket_minify_html_options', '__deactivate_inline_js_on_wp_serveur' );
+function __deactivate_inline_js_on_wp_serveur( $html_options ) {
+	if ( isset( $html_options['jsMinifier'] ) ) {
+		unset( $html_options['jsMinifier'] );
+	}
+	return $html_options;
+}
