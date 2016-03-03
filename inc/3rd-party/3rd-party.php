57d96a822ce0f3cd295f65fa2c7f49da83162aad
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-03-04 00:50:49 +0100

    Improvement: Compatibility with EDD Software Licensing

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -14,6 +14,7 @@ if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
 require( WP_ROCKET_3RD_PARTY_PATH . 'slider/revslider.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'i18n/wpml.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'i18n/polylang.php' );
+require( WP_ROCKET_3RD_PARTY_PATH . 'ecommerce/edd-software-licencing.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'kk-star-ratings.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'wp-postratings.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'wp-print.php' );
