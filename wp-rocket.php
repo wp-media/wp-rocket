d9940b88eadaf2709d2c199492e5805af4c050b6
Rémi Perona <remperona@gmail.com>
2016-04-21 15:38:47 -0400

    Tagging 2.8

--- wp-rocket.php
+++ wp-rocket.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Rocket
 Plugin URI: http://www.wp-rocket.me
 Description: The best WordPress performance plugin.
-Version: 2.7.4
+Version: 2.8
 Code Name: Hoth
 Author: WP Rocket
 Contributors: Jonathan Buttigieg, Julio Potier
@@ -19,7 +19,7 @@ Copyright 2013-2015 WP Rocket
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 // Rocket defines
-define( 'WP_ROCKET_VERSION'             , '2.7.4' );
+define( 'WP_ROCKET_VERSION'             , '2.8' );
 define( 'WP_ROCKET_PRIVATE_KEY'         , false );
 define( 'WP_ROCKET_SLUG'                , 'wp_rocket_settings' );
 define( 'WP_ROCKET_WEB_MAIN'            , 'http://support.wp-rocket.me/' );
@@ -61,7 +61,7 @@ if ( ! defined( 'CHMOD_WP_ROCKET_CACHE_DIRS' ) ) {
     define( 'CHMOD_WP_ROCKET_CACHE_DIRS', 0755 );
 }
 if ( ! defined( 'WP_ROCKET_LASTVERSION' ) ) {
-    define( 'WP_ROCKET_LASTVERSION', '2.6.17' );
+    define( 'WP_ROCKET_LASTVERSION', '2.7.4' );
 }
 
 require( WP_ROCKET_INC_PATH	. 'compat.php' );
