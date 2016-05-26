fc97f075df8725b4b7eabed54dc7eae451b9aec9
Rémi Perona <remperona@gmail.com>
2016-05-26 09:37:13 -0400

    Missing bracket after merge

--- inc/admin/options.php
+++ inc/admin/options.php
@@ -102,7 +102,7 @@ function rocket_field( $args ) {
 			break;
 
 			case 'checkbox' : 
-				if ( isset( $args['label_screen'] ) ) {
+				if ( isset( $args['label_screen'] ) ) { ?>
                     <legend class="screen-reader-text"><span><?php echo $args['label_screen']; ?></span></legend>
 				<?php } ?>
 					<label><input type="checkbox" id="<?php echo $args['name']; ?>" class="<?php echo $class; ?>" name="wp_rocket_settings[<?php echo $args['name']; ?>]" value="1"<?php echo $readonly; ?> <?php checked( get_rocket_option( $args['name'], $default ), 1 ); ?> <?php echo $parent; ?>/> <?php echo $args['label']; ?>
