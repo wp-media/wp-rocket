ecfe48cfa10e799a42efc790a17ac696e6b19af6
Brent Shepherd <thenbrent@gmail.com>
2016-07-01 16:30:47 -0700

    Add tests for as_get_datetime_object()

--- tests/phpunit/procedural_api/procedural_api_Test.php
+++ tests/phpunit/procedural_api/procedural_api_Test.php
@@ -66,5 +66,68 @@ class procedural_api_Test extends ActionScheduler_UnitTestCase {
 		$this->assertNull($action->get_schedule()->next());
 		$this->assertEmpty($action->get_hook());
 	}
+
+	public function test_as_get_datetime_object_default() {
+
+		$utc_now = new DateTime(null, new DateTimeZone('UTC'));
+		$as_now  = as_get_datetime_object();
+
+		// Don't want to use 'U' as timestamps will always be in UTC
+		$this->assertEquals($utc_now->format('Y-m-d H:i:s'),$as_now->format('Y-m-d H:i:s'));
+	}
+
+	public function test_as_get_datetime_object_relative() {
+
+		$utc_tomorrow = new DateTime('tomorrow', new DateTimeZone('UTC'));
+		$as_tomorrow  = as_get_datetime_object('tomorrow');
+
+		$this->assertEquals($utc_tomorrow->format('Y-m-d H:i:s'),$as_tomorrow->format('Y-m-d H:i:s'));
+
+		$utc_tomorrow = new DateTime('yesterday', new DateTimeZone('UTC'));
+		$as_tomorrow  = as_get_datetime_object('yesterday');
+
+		$this->assertEquals($utc_tomorrow->format('Y-m-d H:i:s'),$as_tomorrow->format('Y-m-d H:i:s'));
+	}
+
+	public function test_as_get_datetime_object_fixed() {
+
+		$utc_tomorrow = new DateTime('29 February 2016', new DateTimeZone('UTC'));
+		$as_tomorrow  = as_get_datetime_object('29 February 2016');
+
+		$this->assertEquals($utc_tomorrow->format('Y-m-d H:i:s'),$as_tomorrow->format('Y-m-d H:i:s'));
+
+		$utc_tomorrow = new DateTime('1st January 2024', new DateTimeZone('UTC'));
+		$as_tomorrow  = as_get_datetime_object('1st January 2024');
+
+		$this->assertEquals($utc_tomorrow->format('Y-m-d H:i:s'),$as_tomorrow->format('Y-m-d H:i:s'));
+	}
+
+	public function test_as_get_datetime_object_timezone() {
+
+		$timezone_au      = 'Australia/Brisbane';
+		$timezone_default = date_default_timezone_get();
+
+		date_default_timezone_set( $timezone_au );
+
+		$au_now = new DateTime(null);
+		$as_now = as_get_datetime_object();
+
+		// Make sure they're for the same time
+		$this->assertEquals($au_now->format('U'),$as_now->format('U'));
+
+		// But not in the same timezone, as $as_now should be using UTC
+		$this->assertNotEquals($au_now->format('Y-m-d H:i:s'),$as_now->format('Y-m-d H:i:s'));
+
+		$au_now    = new DateTime(null);
+		$as_au_now = as_get_datetime_object();
+
+		$this->assertEquals($au_now->format('U'),$as_now->format('U'));
+
+		// But not in the same timezone, as $as_now should be using UTC
+		$this->assertNotEquals($au_now->format('Y-m-d H:i:s'),$as_now->format('Y-m-d H:i:s'));
+
+		// Just in cases
+		date_default_timezone_set( $timezone_default );
+	}
 }
  
