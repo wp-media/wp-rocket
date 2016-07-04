1981812096119505b852969430ed2062963c5780
Rémi Perona <remperona@gmail.com>
2016-07-04 17:30:18 -0400

    Fix issue #95: Automatically cache WooCommerce geolocation query string when the geolocation option is selected

--- inc/3rd-party/ecommerce/woocommerce.php
+++ inc/3rd-party/ecommerce/woocommerce.php
@@ -1,6 +1,46 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
+if ( class_exists( 'WooCommerce' ) ) :
+
+    add_filter( 'rocket_cache_query_strings', '__rocket_cache_v_query_string' );
+    add_filter( 'update_option_woocommerce_default_customer_address', '__rocket_after_update_single_options', 10, 2 );
+
+endif;
+
+/**
+ * Automatically cache v query string when WooCommerce geolocation with cache compatibility option is active
+ *
+ * @since 2.8.6
+ * @author Rémy Perona
+ *
+ * @param array $query_strings list of query strings to cache
+ * @return array Updated list of query strings to cache
+ */
+function __rocket_cache_v_query_string( $query_strings ) {
+    if ( 'geolocation_ajax' === get_option( 'woocommerce_default_customer_address' ) ) {
+        $query_strings[] = 'v';
+    }
+
+    return $query_strings;
+}
+
+add_action( 'activate_woocommerce/woocommerce.php', '__rocket_activate_woocommerce', 11 );
+function __rocket_activate_woocommerce() {
+    add_filter( 'rocket_cache_query_strings', '__rocket_cache_v_query_string' );
+
+    // Regenerate the config file
+    rocket_generate_config_file();
+}
+
+add_action( 'deactivate_woocommerce/woocommerce.php', '__rocket_deactivate_woocommerce', 11 );
+function __rocket_deactivate_woocommerce() {
+    remove_filter( 'rocket_cache_query_strings', '__rocket_cache_v_query_string' );
+
+    // Regenerate the config file
+    rocket_generate_config_file();
+}
+
 if ( class_exists( 'WC_API' ) ) :
 
     add_filter( 'rocket_cache_reject_uri', '__rocket_exclude_wc_rest_api' );
