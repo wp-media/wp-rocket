05a89f723b68bf88ac8cbf4ce6d6a9dbe9d998d6
Rémi Perona <remperona@gmail.com>
2016-07-21 11:03:01 -0400

    Use wp_resource_hints to print the domains to DNS prefetch instead of rocket_buffer for WordPress 4.6 and above

--- inc/front/dns-prefetch.php
+++ inc/front/dns-prefetch.php
@@ -4,33 +4,54 @@ defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
 /**
  * Launch DNS Prefetching process
  *
+ * @since 2.8.9 Use the wp_resource_hints filter introduced in WP 4.6 if possible, fallback to rocket_buffer hook for previous versions
  * @since 2.5 Don't add CNAMES if CDN is disabled HTTPS pages or on specific posts
  * @since 2.1 Adding CNAMES fo CDN automatically in DNS Prefetch process
  * @since 2.0
  */
-add_filter( 'rocket_buffer', 'rocket_dns_prefetch', 12 );
-function rocket_dns_prefetch( $buffer )
+if ( function_exists( 'wp_resource_hints' ) ) {
+    add_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
+} else {
+    add_filter( 'rocket_buffer', 'rocket_dns_prefetch_buffer', 12 );
+}
+
+/**
+ * Adds domain names to the list of DNS Prefetch printed by wp_resource_hints
+ *
+ * @since 2.8.9
+ * @author Remy Perona
+ *
+ * @param Array $hints URLs to print for resource hints
+ * @param String $relation_type The relation type the URL are printed for
+ * @return Array URLs to print
+ */ 
+function rocket_dns_prefetch( $hints, $relation_type ) {
+    $domains = rocket_get_dns_prefetch_domains();
+    if ( (bool)$domains ) {
+        foreach ( $domains as $domain ) {
+            if ( 'dns-prefetch' === $relation_type ) {
+                $hints[] = $domain;
+            }
+        }
+    }
+
+    return $hints;
+}
+
+/**
+ * Inserts html code for domain names to DNS prefetch in the buffer before creating the cache file
+ *
+ * @since 2.0
+ * @author Jonathan Buttigieg
+ *
+ * @param String $buffer
+ * @return String Updated buffer
+ */ 
+function rocket_dns_prefetch_buffer( $buffer )
 {
-	$dns_link_tags = '';
-	$cdn_cnames    = get_rocket_cdn_cnames( array( 'all', 'images', 'css_and_js', 'css', 'js' ) );
+	$domains = rocket_get_dns_prefetch_domains();
 
-	// Don't add CNAMES if CDN is disabled HTTPS pages or on specific posts
-	if( ! is_rocket_cdn_on_ssl() || is_rocket_post_excluded_option( 'cdn' ) ) {
-		$cdn_cnames = array();
-	}
-
-	$domains = array_merge( $cdn_cnames, (array) get_rocket_option( 'dns_prefetch' ) );
-
-	/**
-	 * Filter list of domains to prefetch DNS
-	 *
-	 * @since 1.1.0
-	 *
-	 * @param array $domains List of domains to prefetch DNS
-	 */
-	$domains = apply_filters( 'rocket_dns_prefetch', $domains );
-
-	if ( count( $domains ) ) {
+	if ( (bool)$domains ) {
 		foreach ( $domains as $domain ) {
 			$dns_link_tags .= '<link rel="dns-prefetch" href="' . esc_url( $domain ) . '" />';
 		}
@@ -53,4 +74,33 @@ function rocket_dns_prefetch( $buffer )
 	$buffer = preg_replace( '/<head(.*)>/', '<head$1>' . $old_ie_conditional_tag . $dns_link_tags, $buffer, 1 );
 
 	return $buffer;
+}
+
+/**
+ * Get the domain names to DNS prefetch from WP Rocket options
+ * 
+ * @since 2.8.9
+ * @author Remy Perona
+ *
+ * return Array Array of domain names to DNS prefetch
+ */
+function rocket_get_dns_prefetch_domains() {
+    $dns_link_tags = '';
+	$cdn_cnames    = get_rocket_cdn_cnames( array( 'all', 'images', 'css_and_js', 'css', 'js' ) );
+
+	// Don't add CNAMES if CDN is disabled HTTPS pages or on specific posts
+	if( ! is_rocket_cdn_on_ssl() || is_rocket_post_excluded_option( 'cdn' ) ) {
+		$cdn_cnames = array();
+	}
+
+	$domains = array_merge( $cdn_cnames, (array) get_rocket_option( 'dns_prefetch' ) );
+
+	/**
+	 * Filter list of domains to prefetch DNS
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param array $domains List of domains to prefetch DNS
+	 */
+	return apply_filters( 'rocket_dns_prefetch', $domains );
 }
