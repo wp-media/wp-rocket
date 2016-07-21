e88ccc1261b2c70c3e13dcc48396600f3c522466
Rémi Perona <remperona@gmail.com>
2016-07-21 15:13:36 -0400

    Fix illegal offset type in WPML because $lang is an array instead of a string

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -530,7 +530,7 @@ function rocket_clean_home_feeds() {
  * @return void
  */
 function rocket_clean_domain( $lang = '' ) {
-	$urls = ( ! $lang || is_object( $lang ) ) ? get_rocket_i18n_uri() : get_rocket_i18n_home_url( $lang );
+	$urls = ( ! $lang || is_object( $lang ) || is_array( $lang ) ) ? get_rocket_i18n_uri() : get_rocket_i18n_home_url( $lang );
 	$urls = (array) $urls;
 
 	/**
