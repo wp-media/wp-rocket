719e6036289d7c6fb9bee37134bb3cea0644aab7
Rémi Perona <remperona@gmail.com>
2016-08-12 13:52:34 -0400

    Code improvement

--- inc/3rd-party/page-builder/thrive-visual-editor.php
+++ inc/3rd-party/page-builder/thrive-visual-editor.php
@@ -13,7 +13,4 @@ if ( ! function_exists( 'tve_editor_url' ) ) {
  * @param  boolean|integer $bot_detection 1|0 when crawler has|not been detected, FALSE when user agent string is unavailable
  * @return integer
  */
-add_filter( 'tve_dash_is_crawler', '__rocket_tve_dash_is_crawler', PHP_INT_MAX );
-function __rocket_tve_dash_is_crawler( $bot_detection ) {
-    return 0;
-}
+add_filter( 'tve_dash_is_crawler', '__return_zero', PHP_INT_MAX );
