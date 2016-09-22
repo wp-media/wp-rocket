4a48f1259a7f4b0292ff9fa425fd1d40a52720be
Rémi Perona <remperona@gmail.com>
2016-09-22 16:25:50 -0400

    Update the filter priority and remove the check for profile

--- inc/3rd-party/buddypress.php
+++ inc/3rd-party/buddypress.php
@@ -2,7 +2,6 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 if ( class_exists( 'BuddyPress' )
-	 && function_exists( 'bp_is_my_profile' )
 	 && function_exists( 'bp_is_group_creation_step' )
 	 && function_exists( 'bp_is_group_admin_screen' )
 	 && function_exists( 'bp_is_user_change_avatar' ) ) :
@@ -10,12 +9,12 @@ if ( class_exists( 'BuddyPress' )
 /**
  * Conflict with BuddyPress: don't apply LazyLoad on BuddyPress profil pages
  *
+ * @since 2.8.15 Updated filter priority and removed check for profile
  * @since 2.6.9
  */
-add_filter( 'do_rocket_lazyload', '__deactivate_rocket_lazyload_on_buddypress_profil_pages' );
+add_filter( 'do_rocket_lazyload', '__deactivate_rocket_lazyload_on_buddypress_profil_pages', 12 );
 function __deactivate_rocket_lazyload_on_buddypress_profil_pages( $run_filter ) {
-	if ( bp_is_my_profile()
-		 || bp_is_group_creation_step( 'group-avatar' )
+	if ( bp_is_group_creation_step( 'group-avatar' )
 		 || bp_is_group_admin_screen( 'group-avatar' )
 		 || bp_is_user_change_avatar() ) {
 		$run_filter = false;
