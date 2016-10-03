dc94e984c1f6a7732f316d552d3043d5693e983d
Brent Shepherd <thenbrent@gmail.com>
2016-10-03 16:35:32 -0700

    Manual fixup merge of bleeding and master
    * delete unused ActionScheduler_NullActionClaim.php
    * reinstate tests on master that are also relevant on bleeding and
      fixup hook using in test_changing_batch_count_limit() to use the
      hook used in bleeding

--- tests/phpunit/runner/ActionScheduler_QueueRunner_Test.php
+++ tests/phpunit/runner/ActionScheduler_QueueRunner_Test.php
@@ -111,5 +111,100 @@ class ActionScheduler_QueueRunner_Test extends ActionScheduler_UnitTestCase {
 		$next = wp_next_scheduled( ActionScheduler_QueueRunner::WP_CRON_HOOK );
 		$this->assertNotEmpty($next);
 	}
+
+	public function test_batch_count_limit() {
+		$store = new ActionScheduler_wpPostStore();
+		$runner = new ActionScheduler_QueueRunner( $store );
+
+		$mock = new MockAction();
+		$random = md5(rand());
+		add_action( $random, array( $mock, 'action' ) );
+		$schedule = new ActionScheduler_SimpleSchedule(new DateTime('1 day ago'));
+
+		for ( $i = 0 ; $i < 30 ; $i++ ) {
+			$action = new ActionScheduler_Action( $random, array($random), $schedule );
+			$store->save_action( $action );
+		}
+
+		$claims = array();
+
+		for ( $i = 0 ; $i < 5 ; $i++ ) {
+			$claims[] = $store->stake_claim( 5 );
+		}
+
+		$actions_run = $runner->run();
+
+
+		$this->assertEquals( 0, $mock->get_call_count() );
+		$this->assertEquals( 0, $actions_run );
+
+		$first = reset($claims);
+		$store->release_claim( $first );
+
+		$actions_run = $runner->run();
+		$this->assertEquals( 10, $mock->get_call_count() );
+		$this->assertEquals( 10, $actions_run );
+
+		remove_action( $random, array( $mock, 'action' ) );
+	}
+
+	public function test_changing_batch_count_limit() {
+		$store = new ActionScheduler_wpPostStore();
+		$runner = new ActionScheduler_QueueRunner( $store );
+
+		$random = md5(rand());
+		$schedule = new ActionScheduler_SimpleSchedule(new DateTime('1 day ago'));
+
+		for ( $i = 0 ; $i < 30 ; $i++ ) {
+			$action = new ActionScheduler_Action( $random, array($random), $schedule );
+			$store->save_action( $action );
+		}
+
+		$claims = array();
+
+		for ( $i = 0 ; $i < 5 ; $i++ ) {
+			$claims[] = $store->stake_claim( 5 );
+		}
+
+		$mock1 = new MockAction();
+		add_action( $random, array( $mock1, 'action' ) );
+		$actions_run = $runner->run();
+		remove_action( $random, array( $mock1, 'action' ) );
+
+		$this->assertEquals( 0, $mock1->get_call_count() );
+		$this->assertEquals( 0, $actions_run );
+
+
+		add_filter( 'action_scheduler_queue_runner_concurrent_batches', array( $this, 'return_6' ) );
+
+		$mock2 = new MockAction();
+		add_action( $random, array( $mock2, 'action' ) );
+		$actions_run = $runner->run();
+		remove_action( $random, array( $mock2, 'action' ) );
+
+		$this->assertEquals( 5, $mock2->get_call_count() );
+		$this->assertEquals( 5, $actions_run );
+
+		remove_filter( 'action_scheduler_queue_runner_concurrent_batches', array( $this, 'return_6' ) );
+
+		for ( $i = 0 ; $i < 5 ; $i++ ) { // to make up for the actions we just processed
+			$action = new ActionScheduler_Action( $random, array($random), $schedule );
+			$store->save_action( $action );
+		}
+
+		$mock3 = new MockAction();
+		add_action( $random, array( $mock3, 'action' ) );
+		$actions_run = $runner->run();
+		remove_action( $random, array( $mock3, 'action' ) );
+
+		$this->assertEquals( 0, $mock3->get_call_count() );
+		$this->assertEquals( 0, $actions_run );
+
+		remove_filter( 'action_scheduler_queue_runner_concurrent_batches', array( $this, 'return_6' ) );
+	}
+
+	public function return_6() {
+		return 6;
+	}
 }
  
