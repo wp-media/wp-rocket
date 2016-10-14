023995450dc1a711c7449406c75b179e16b55b6d
Rémi Perona <remperona@gmail.com>
2016-10-14 16:18:22 -0400

    Add IPs fallback if API is not available

--- inc/functions/cloudflare.php
+++ inc/functions/cloudflare.php
@@ -236,9 +236,40 @@ function rocket_get_cloudflare_ips() {
 
             if ( isset( $cf_ips->success ) && $cf_ips->success ) {
                 set_transient(  'rocket_cloudflare_ips', $cf_ips, 2 * WEEK_IN_SECONDS );
+            } else {
+                throw new Exception( 'Error connecting to CloudFlare' );
             }
         } catch ( Exception $e ) {
-            return false;
+            $cf_ips = ( object ) [ 'success' => true, 'result' => ( object ) [] ];
+            $cf_ips->result->ipv4_cidrs = array(
+                '103.21.244.0/22',
+                '103.22.200.0/22',
+                '103.31.4.0/22',
+                '104.16.0.0/12',
+                '108.162.192.0/18',
+                '131.0.72.0/22',
+                '141.101.64.0/18',
+                '162.158.0.0/15',
+                '172.64.0.0/13',
+                '173.245.48.0/20',
+                '188.114.96.0/20',
+                '190.93.240.0/20',
+                '197.234.240.0/22',
+                '198.41.128.0/17',
+                '199.27.128.0/21',
+            );
+
+            $cf_ips->result->ipv6_cidrs = array(
+                '2400:cb00::/32',
+                '2405:8100::/32',
+                '2405:b500::/32',
+                '2606:4700::/32',
+                '2803:f800::/32',
+                '2c0f:f248::/32',
+                '2a06:98c0::/29',
+            );
+
+            return $cf_ips;
         }
     }
 
