e292b13a7a9b56f619f8c05897fc76245601d790
Rémi Perona <remperona@gmail.com>
2016-11-09 08:04:07 -0500

    use empty instead of isset for performance

--- inc/functions/minify.php
+++ inc/functions/minify.php
@@ -80,7 +80,7 @@ function get_rocket_minify_files( $files, $force_pretty_url = true, $pretty_file
 		foreach ( $files as $file ) {
 			$file = parse_url( $file, PHP_URL_PATH );
 
-            if ( ! isset( $file ) ) {
+            if ( empty( trim( $file ) ) ) {
                 continue;
             }
 
