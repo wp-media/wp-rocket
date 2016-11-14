b0b92843612609524d065c53c7edeb3627df5e8e
Rémi Perona <remperona@gmail.com>
2016-11-14 11:11:26 -0500

    Remove double underscores to comply with PHP 7

--- inc/3rd-party/security/secupress.php
+++ inc/3rd-party/security/secupress.php
@@ -1,12 +1,12 @@
 <?php 
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
-add_action( 'update_option_secupress_users-login_settings', '__rocket_after_update_single_options', 10, 2 );
+add_action( 'update_option_secupress_users-login_settings', 'rocket_after_update_single_options', 10, 2 );
 
 
-add_filter( 'rocket_cache_reject_uri', '__rocket_exclude_secupress_move_login' );
+add_filter( 'rocket_cache_reject_uri', 'rocket_exclude_secupress_move_login' );
 
-function __rocket_exclude_secupress_move_login( $urls ) {
+function rocket_exclude_secupress_move_login( $urls ) {
 	if ( ! function_exists( 'secupress_move_login_get_slugs' ) ) {
 		return $urls;
 	}
@@ -22,19 +22,19 @@ function __rocket_exclude_secupress_move_login( $urls ) {
 }
 
 
-add_action( 'secupress.plugins.activation', '__rocket_maybe_activate_secupress', 10001 );
+add_action( 'secupress.plugins.activation', 'rocket_maybe_activate_secupress', 10001 );
 
-function __rocket_maybe_activate_secupress() {
+function rocket_maybe_activate_secupress() {
 	if ( function_exists( 'secupress_move_login_get_slugs' ) ) {
-		__rocket_activate_secupress();
+		rocket_activate_secupress();
 	}
 }
 
 
-add_action( 'secupress.plugin.move_login.activate', '__rocket_activate_secupress' );
+add_action( 'secupress.plugin.move_login.activate', 'rocket_activate_secupress' );
 
-function __rocket_maybe_activate_secupress() {
-	add_filter( 'rocket_cache_reject_uri', '__rocket_exclude_secupress_move_login' );
+function rocket_maybe_activate_secupress() {
+	add_filter( 'rocket_cache_reject_uri', 'rocket_exclude_secupress_move_login' );
 
 	// Update the WP Rocket rules on the .htaccess.
 	flush_rocket_htaccess();
@@ -44,19 +44,19 @@ function __rocket_maybe_activate_secupress() {
 }
 
 
-add_action( 'secupress.deactivation', '__rocket_maybe_deactivate_secupress', 10001 );
+add_action( 'secupress.deactivation', 'rocket_maybe_deactivate_secupress', 10001 );
 
-function __rocket_maybe_deactivate_secupress() {
+function rocket_maybe_deactivate_secupress() {
 	if ( function_exists( 'secupress_move_login_get_slugs' ) ) {
-		__rocket_deactivate_secupress();
+		rocket_deactivate_secupress();
 	}
 }
 
 
-add_action( 'secupress.plugin.move_login.deactivate', '__rocket_deactivate_secupress' );
+add_action( 'secupress.plugin.move_login.deactivate', 'rocket_deactivate_secupress' );
 
-function __rocket_deactivate_secupress() {
-	remove_filter( 'rocket_cache_reject_uri', '__rocket_exclude_secupress_move_login' );
+function rocket_deactivate_secupress() {
+	remove_filter( 'rocket_cache_reject_uri', 'rocket_exclude_secupress_move_login' );
 
 	// Update the WP Rocket rules on the .htaccess.
 	flush_rocket_htaccess();
