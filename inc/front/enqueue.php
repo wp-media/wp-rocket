d4a169854379f1a30bd7db5307762fd727ebeb11
Rémi Perona <remperona@gmail.com>
2016-11-15 14:19:25 -0500

    Create a static file from dynamically generated CSS/JS content using PHP

--- inc/front/enqueue.php
+++ inc/front/enqueue.php
@@ -57,9 +57,7 @@ function rocket_browser_cache_busting( $src ) {
 
     $relative_src_path      = str_replace( home_url( '/' ), '', $full_src );
     $full_src_path          = ABSPATH . dirname( $relative_src_path );
-
     $cache_busting_filename = preg_replace( '/\.(js|css)\?ver=(.+)$/', '-$2.$1', rtrim( str_replace( '/', '-', $relative_src_path ) ) );
-
     $blog_id                = get_current_blog_id();
     $cache_busting_path     = WP_ROCKET_CACHE_BUSTING_PATH . $blog_id . '/';
     $cache_busting_filepath = $cache_busting_path . $cache_busting_filename;
@@ -92,4 +90,71 @@ function rocket_browser_cache_busting( $src ) {
     rocket_put_content( $cache_busting_filepath, $file_content );
 
     return $cache_busting_url;
+}
+
+/**
+ * Create a static file for dynamically generated CSS/JS from PHP
+ *
+ * @since 2.9
+ * @author Remy Perona
+ *
+ * @param string $src dynamic CSS/JS file URL
+ * @return string URL of the generated static file
+ */
+add_filter( 'style_loader_src', 'rocket_cache_dynamic_resource', 16 );
+add_filter( 'script_loader_src', 'rocket_cache_dynamic_resource', 16 );
+function rocket_cache_dynamic_resource( $src ) {
+    global $pagenow;
+	
+	if ( 'wp-login.php' == $pagenow ) {
+    	return $src;
+    }
+
+    if ( false === strpos( $src, '.php' ) ) {
+        return $src;
+    }
+
+    $full_src = ( substr( $src, 0, 2 ) === '//' ) ? rocket_add_url_protocol( $src ) : $src;
+
+    if ( false !== strpos( $full_src, '://' ) && false === strpos( $full_src, home_url() ) ) {
+        return $src;
+    }
+
+    if ( parse_url( $full_src, PHP_URL_HOST ) == '' ) {
+        $full_src = home_url() . $src;
+    }
+
+    if ( 'style_loader_src' == current_filter() ) {
+        $extension = '.css';
+    }
+
+    if ( 'script_loader_src' == current_filter() ) {
+        $extension = '.js';
+    }
+
+    $relative_src_path      = str_replace( home_url( '/' ), '', $full_src );
+    $full_src_path          = ABSPATH . dirname( $relative_src_path );
+    $cache_dynamic_resource_filename = apply_filters( 'rocket_dynamic_resource_cache_filename', preg_replace( '/\.(php)$/', $extension, strtok( rtrim( str_replace( '/', '-', $relative_src_path ) ), '?' ) ) );
+    $blog_id                = get_current_blog_id();
+    $cache_busting_path     = WP_ROCKET_CACHE_BUSTING_PATH . $blog_id . '/';
+    $cache_dynamic_resource_filepath = $cache_busting_path . $cache_dynamic_resource_filename;
+    $cache_dynamic_resource_url      = WP_ROCKET_CACHE_BUSTING_URL . $blog_id . '/' . $cache_dynamic_resource_filename;
+
+    if ( file_exists( $cache_dynamic_resource_filepath ) && is_readable( $cache_dynamic_resource_filepath ) ) {
+    	return $cache_dynamic_resource_url;
+    }
+
+    $response = wp_remote_get( $full_src );
+
+    if ( ! is_array( $response ) || is_wp_error(  $response ) ) {
+        return $src;
+    }
+
+    if ( ! is_dir( $cache_dynamic_resource_filepath ) ) {
+        rocket_mkdir_p( $cache_dynamic_resource_filepath );
+    }
+
+    rocket_put_content( $cache_dynamic_resource_filepath, $response['body'] );
+
+    return $cache_dynamic_resource_url;
 }
