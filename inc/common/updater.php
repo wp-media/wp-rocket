65e139a8205773acbe4fd085219abff26de6cc78
Rémi Perona <remperona@gmail.com>
2016-11-22 10:40:07 -0500

    Add static variable to prevent the function from being executed too many times

--- inc/common/updater.php
+++ inc/common/updater.php
@@ -9,6 +9,14 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 add_filter( 'site_transient_update_plugins', 'rocket_check_update', 1 );
 function rocket_check_update( $value ) {
 
+    static $check_update = 0;
+
+    if ( 1 === $check_update ) {
+        return $value;
+    }
+    
+    $check_update = 1;
+
 	global $pagenow;
 	$timer_update_wprocket = (int) get_site_transient( 'update_wprocket' );
 	$temp_object = get_site_transient( 'update_wprocket_response' );
