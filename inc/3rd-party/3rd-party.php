51f91beb3949048118febc49bff308ab6756635e
Rémi Perona <remperona@gmail.com>
2016-11-29 11:21:50 -0500

    Add Divi to the list of compatibility

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -42,3 +42,5 @@ require( WP_ROCKET_3RD_PARTY_PATH . 'page-builder/thrive-visual-editor.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'page-builder/visual-composer.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'security/secupress.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'simple-custom-css.php' );
+
+require( WP_ROCKET_3RD_PARTY_PATH . 'themes/divi.php' );
