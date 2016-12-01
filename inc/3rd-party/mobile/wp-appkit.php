2c354389431622d987281a7fa9fb1d39d5afba97
Rémi Perona <remperona@gmail.com>
2016-12-01 07:38:22 -0500

    Remove double underscore in functions names to comply with PHP 7

--- inc/3rd-party/mobile/wp-appkit.php
+++ inc/3rd-party/mobile/wp-appkit.php
@@ -3,19 +3,19 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 if ( class_exists( 'WpAppKit' ) ) :
 
-add_filter( 'rocket_cache_reject_uri', '__rocket_add_appkit_exclude_pages' );
+add_filter( 'rocket_cache_reject_uri', 'rocket_add_appkit_exclude_pages' );
 
 endif;
 
-function __rocket_add_appkit_exclude_pages( $urls ) {
+function rocket_add_appkit_exclude_pages( $urls ) {
     $urls[] = '/wp-appkit-api/(.*)';
 
     return $urls;
 }
 
-add_action( 'activate_wp-appkit/wp-appkit.php', '__rocket_activate_wp_appkit', 11 );
-function __rocket_activate_wp_appkit() {
-    add_filter( 'rocket_cache_reject_uri', '__rocket_add_appkit_exclude_pages' );
+add_action( 'activate_wp-appkit/wp-appkit.php', 'rocket_activate_wp_appkit', 11 );
+function rocket_activate_wp_appkit() {
+    add_filter( 'rocket_cache_reject_uri', 'rocket_add_appkit_exclude_pages' );
 
     // Update the WP Rocket rules on the .htaccess
     flush_rocket_htaccess();
@@ -24,9 +24,9 @@ function __rocket_activate_wp_appkit() {
     rocket_generate_config_file();
 }
 
-add_action( 'deactivate_wp-appkit/wp-appkit.php', '__rocket_deactivate_wp_appkit', 11 );
-function __rocket_deactivate_wp_appkit() {
-    remove_filter( 'rocket_cache_reject_uri', '__rocket_add_appkit_exclude_pages' );
+add_action( 'deactivate_wp-appkit/wp-appkit.php', 'rocket_deactivate_wp_appkit', 11 );
+function rocket_deactivate_wp_appkit() {
+    remove_filter( 'rocket_cache_reject_uri', 'rocket_add_appkit_exclude_pages' );
 
     // Update the WP Rocket rules on the .htaccess
     flush_rocket_htaccess();
