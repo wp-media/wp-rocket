af8ba1f05d150ccd08f2196a34d44abd8ec0ac44
thenbrent <thenbrent@gmail.com>
2016-12-05 11:27:54 -0500

    Fix registration function version

--- action-scheduler.php
+++ action-scheduler.php
@@ -8,16 +8,16 @@ Author URI: http://prospress.com/
 Version: 1.5.1
 */
 
-if ( ! function_exists( 'action_scheduler_register_1_dot_5' ) ) {
+if ( ! function_exists( 'action_scheduler_register_1_dot_5_dot_1' ) ) {
 
 	if ( ! class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once( 'classes/ActionScheduler_Versions.php' );
 		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
-	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_5', 0, 0 );
+	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_5_dot_1', 0, 0 );
 
-	function action_scheduler_register_1_dot_5() {
+	function action_scheduler_register_1_dot_5_dot_1() {
 		$versions = ActionScheduler_Versions::instance();
 		$versions->register( '1.5.1', 'action_scheduler_initialize_1_dot_5_dot_1' );
 	}
