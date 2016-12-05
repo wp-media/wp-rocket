f98015913177b5e58cbbee5793c7e949520c3cc5
Rémi Perona <remperona@gmail.com>
2016-12-05 17:59:44 -0500

    Updated to follow WP coding standards

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,9 @@
 <?php
 
-// If uninstall not called from WordPress exit
+// If uninstall not called from WordPress exit.
 defined( 'WP_UNINSTALL_PLUGIN' ) or die( 'Cheatin&#8217; uh?' );
 
-// Delete all transients
+// Delete all transients.
 delete_site_transient( 'update_wprocket' );
 delete_site_transient( 'update_wprocket_response' );
 delete_transient( 'wp_rocket_settings' );
@@ -11,40 +11,40 @@ delete_transient( 'rocket_check_licence_30' );
 delete_transient( 'rocket_check_licence_1' );
 delete_transient( 'rocket_cloudflare_ips' );
 
-// Delete WP Rocket options
+// Delete WP Rocket options.
 delete_option( 'wp_rocket_settings' );
 
-// Delete all user meta related to WP Rocket
+// Delete all user meta related to WP Rocket.
 delete_metadata( 'user', '', 'rocket_boxes', '', true );
 
-// Clear scheduled WP Rocket Cron
+// Clear scheduled WP Rocket Cron.
 wp_clear_scheduled_hook( 'rocket_purge_time_event' );
 wp_clear_scheduled_hook( 'rocket_database_optimization_time_event' );
 
 /**
- * Remove all cache files
+ * Remove all cache files.
  *
  * @since 1.2.0
+ *
+ * @param string $dir Path to directory.
  */
-function __rocket_rrmdir( $dir )
-{
+function rocket_uninstall_rrmdir( $dir ) {
 
-	if( !is_dir( $dir ) ) 
-	{
+	if ( ! is_dir( $dir ) ) {
 		@unlink( $dir );
-		return;	
+		return;
 	}
 
-    if( $globs = glob( $dir . '/*', GLOB_NOSORT ) ) {
-	    foreach( $globs as $file ) {
-			is_dir( $file ) ? __rocket_rrmdir($file) : @unlink( $file );
+	if ( $globs = glob( $dir . '/*', GLOB_NOSORT ) ) {
+	    foreach ( $globs as $file ) {
+			is_dir( $file ) ? __rocket_rrmdir( $file ) : @unlink( $file );
 	    }
 	}
 
-    @rmdir($dir);
+	@rmdir( $dir );
 
 }
 
-__rocket_rrmdir( WP_CONTENT_DIR . '/cache/wp-rocket/' );
-__rocket_rrmdir( WP_CONTENT_DIR . '/cache/min/' );
-__rocket_rrmdir( WP_CONTENT_DIR . '/wp-rocket-config/' );
+rocket_uninstall_rrmdir( WP_CONTENT_DIR . '/cache/wp-rocket/' );
+rocket_uninstall_rrmdir( WP_CONTENT_DIR . '/cache/min/' );
+rocket_uninstall_rrmdir( WP_CONTENT_DIR . '/wp-rocket-config/' );
