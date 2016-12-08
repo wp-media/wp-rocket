eb423b3f3a98d369ccd1ccce21348823380740f1
Rémi Perona <remperona@gmail.com>
2016-12-08 10:36:32 -0500

    Update for WordPress coding standards

--- inc/3rd-party/autoptimize.php
+++ inc/3rd-party/autoptimize.php
@@ -3,11 +3,11 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 if ( function_exists( 'autoptimize_do_cachepurged_action' ) ) :
 
-/**
- * Improvement with Autoptimize: clear the cache when Autoptimize's cache is cleared
- *
- * @since 2.7
- */
-add_action( 'autoptimize_action_cachepurged', 'rocket_clean_domain' );
+	/**
+	 * Improvement with Autoptimize: clear the cache when Autoptimize's cache is cleared
+	 *
+	 * @since 2.7
+	 */
+	add_action( 'autoptimize_action_cachepurged', 'rocket_clean_domain' );
 
 endif;
