a2d0a057a27ce30da93367bd513e6b1bdaef8e3e
Rémi Perona <remperona@gmail.com>
2016-12-08 13:40:43 -0500

    Manually load WP Rocket

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,11 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/wp-rocket.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
 function _manually_load_environment() {
     update_option( 'active_plugins', 'wp-rocket/wp-rocket.php' );
 
