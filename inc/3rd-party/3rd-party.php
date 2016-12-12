d43e332ad268cfecd3f354156c174307366e4f23
Rémi Perona <remperona@gmail.com>
2016-12-12 08:36:14 -0500

    Include WP Retina compatibility code

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -42,5 +42,6 @@ require( WP_ROCKET_3RD_PARTY_PATH . 'page-builder/thrive-visual-editor.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'page-builder/visual-composer.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'security/secupress.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'simple-custom-css.php' );
+require( WP_ROCKET_3RD_PARTY_PATH . 'wp-retina-2x.php' );
 
 require( WP_ROCKET_3RD_PARTY_PATH . 'themes/divi.php' );
