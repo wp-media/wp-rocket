c1b9ae67741533404b9feb093c2240644e1b62af
Nico Mollet <nico.mollet@gmail.com>
2017-01-13 16:59:06 +0100

    Elementor compatibilty file

---
+++ inc/3rd-party/page-builder/elementor.php
@@ -0,0 +1,20 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+if ( defined( 'ELEMENTOR_VERSION' ) ) :
+
+    /**
+     * Excludes Elementor scripts from JS minification
+     *
+     * @since 2.9.x
+     *
+     * @param Array $excluded_handle An array of JS handles enqueued in WordPress
+     * @return Array the updated array of handles
+     */
+    add_filter( 'rocket_exclude_js', 'rocket_exclude_js_elementor' );
+    function rocket_exclude_js_elementor( $excluded_js ) {
+        $excluded_js[] = '/(.*)/elementor/(.*).js';
+        return $excluded_js;
+    }
+
+endif;
