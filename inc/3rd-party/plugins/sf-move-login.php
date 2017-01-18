a68ebd62e5a0c3be9b43a36577a448d9211354a0
Rémi Perona <remperona@gmail.com>
2017-01-18 18:35:19 -0500

    Add a ? at the end of SF Move Login paths to account for both URL with and without trailingslash

--- inc/3rd-party/plugins/sf-move-login.php
+++ inc/3rd-party/plugins/sf-move-login.php
@@ -33,9 +33,13 @@ function rocket_add_sfml_exclude_pages( $urls ) {
 	}
 
 	$sfml_slugs = sfml_get_slugs();
+	error_log( print_r( $sfml_slugs, true ) );
 	$sfml_slugs = array_map( 'home_url', $sfml_slugs );
 	$sfml_slugs = array_map( 'trailingslashit', $sfml_slugs );
 	$sfml_slugs = array_map( 'rocket_clean_exclude_file', $sfml_slugs );
+	$sfml_slugs = array_map( function( $sfml_slug ) {
+		return $sfml_slug . '?';
+	}, $sfml_slugs );
 
 	return array_merge( $urls, $sfml_slugs );
 }
