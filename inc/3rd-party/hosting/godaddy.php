6e8e4f7f1abbd6d084209c17584e7e45b8f3004a
Rémi Perona <remperona@gmail.com>
2017-01-25 14:47:33 -0500

    Remove old GoDaddy compatibility code and add new one

--- inc/3rd-party/hosting/godaddy.php
+++ inc/3rd-party/hosting/godaddy.php
@@ -1,48 +1,16 @@
 <?php 
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
-if ( class_exists( 'GD_System_Plugin_Cache_Purge' ) ) :
+if ( class_exists( 'WPaaS\Plugin' ) ) :
 
-/**
- * Clear WP Rocket cache after purged the Varnish cache via GoDaddy Hosting
- *
- * @since 2.6.5
- *
- * @return void
- */
-add_action( 'init', '__rocket_clear_cache_after_godaddy', 0 );
-function __rocket_clear_cache_after_godaddy() {
-	if ( ! isset( $_REQUEST['GD_COMMAND'] ) || $_REQUEST['GD_COMMAND'] != 'FLUSH_CACHE' ) {
-		return;
-	}
-	
-	if ( wp_verify_nonce( $_REQUEST['GD_NONCE'], 'GD_FLUSH_CACHE' ) ) {
-		// Clear all caching files
-		rocket_clean_domain();
-		
+	add_filter( 'do_rocket_generate_caching_file', '__return_false' );
+	add_filter( 'rocket_display_varnish_options_tab', '__return_false' );
+	add_filter( 'set_rocket_wp_cache_define', '__return_true' );
+
+	add_action( 'wpaas_cache_banned', 'rocket_godaddy_preload_cache' );
+	function rocket_godaddy_preload_cache() {
 		// Preload cache
 		run_rocket_preload_cache( 'cache-preload' );
 	}
-}
-
-/* @since 2.6.5
- * For not conflit with GoDaddy
-*/
-add_action( 'after_rocket_clean_domain', 'rocket_clean_godaddy' );
-
-/**
- * Call the cache server to purge the cache with GoDaddy hosting.
- *
- * @since 2.6.5
- *
- * @return void
- */
-function rocket_clean_godaddy() {	
-	global $gd_cache_purge;
-	
-	if( is_a( $gd_cache_purge, 'GD_System_Plugin_Cache_Purge' ) && method_exists( 'GD_System_Plugin_Cache_Purge', 'ban_cache' ) ) {
-		$gd_cache_purge->ban_cache();	
-	}
-}
 
 endif;
