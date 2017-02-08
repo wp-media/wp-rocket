78af1fe0a8f943148c5cb1041f329a71c89e3b2b
Rémi Perona <remperona@gmail.com>
2017-02-08 15:46:59 -0500

    New function to defer all JS files

--- inc/front/deferred-js.php
+++ inc/front/deferred-js.php
@@ -11,6 +11,10 @@ defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
  * @return string Updated HTML content
  */
 function rocket_insert_deferred_js( $buffer ) {
+	if ( get_rocket_option( 'defer_all_js' ) ) {
+		return $buffer;
+	}
+
 	// Get all JS files with this regex.
 	preg_match_all( '#<script.*src=[\'|"]([^\'|"]+\.js?.+)[\'|"].*></script>#iU', $buffer, $tags_match );
 
@@ -34,3 +38,44 @@ function rocket_insert_deferred_js( $buffer ) {
 	return $buffer;
 }
 add_action( 'rocket_buffer', 'rocket_insert_deferred_js', 11 );
+
+
+/**
+ * Defer all JS files.
+ * 
+ * @since 3.0
+ * @author Remy Perona
+ *
+ * @param string $buffer HTML content.
+ * @return string Updated HTML content
+ */
+function rocket_defer_js( $buffer ) {
+	if ( ! get_rocket_option( 'defer_all_js' ) ) {
+		return $buffer;
+	}
+
+	// Get all JS files with this regex.
+	preg_match_all( '#<script.*src=[\'|"]([^\'|"]+\.js?.+)[\'|"].*></script>#iU', $buffer, $tags_match );
+
+	if ( ! isset( $tags_match[0] ) ) {
+		return $buffer;
+	}
+
+	$exclude_defer_js = array_flip( get_rocket_exlude_defer_js() );
+
+	foreach ( $tags_match[0] as $i => $tag ) {
+		// Strip query args.
+		$url = strtok( $tags_match[1][$i] , '?' );
+
+		// Check if this file should be deferred.
+		if ( isset( $exclude_defer_js[ $url ] ) ) {
+			continue;
+		}
+
+		$deferred_tag = str_replace( '></script>', ' defer></script>', $tag );
+		$buffer = str_replace( $tag, $deferred_tag, $buffer );
+	}
+
+	return $buffer;	
+}
+add_action( 'rocket_buffer', 'rocket_defer_js', 14 );
