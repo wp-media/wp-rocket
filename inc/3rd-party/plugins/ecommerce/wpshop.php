0ebf3ba62284ea07b1a6475752a354581ca6fe8e
Rémi Perona <remperona@gmail.com>
2017-02-27 13:22:25 -0500

    Remove trailing comma

--- inc/3rd-party/plugins/ecommerce/wpshop.php
+++ inc/3rd-party/plugins/ecommerce/wpshop.php
@@ -2,7 +2,7 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 if ( defined( 'WPSHOP_VERSION' ) && class_exists( 'wpshop_tools' ) && method_exists( 'wpshop_tools','get_page_id' ) ) :
-	add_filter( 'rocket_cache_reject_uri', 'rocket_exclude_wpshop_pages', );
+	add_filter( 'rocket_cache_reject_uri', 'rocket_exclude_wpshop_pages' );
 	add_action( 'update_option_wpshop_cart_page_id'			, '__rocket_after_update_single_options', 10, 2 );
 	add_action( 'update_option_wpshop_checkout_page_id'		, '__rocket_after_update_single_options', 10, 2 );
 	add_action( 'update_option_wpshop_payment_return_page_id', '__rocket_after_update_single_options', 10, 2 );
