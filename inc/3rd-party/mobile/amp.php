1e8ebde7c83402bb80bc2f70c467b4b6a6ea50ed
Rémi Perona <remperona@gmail.com>
2017-03-23 14:39:09 -0400

    remove new filters to prevent any issue with amp

--- inc/3rd-party/mobile/amp.php
+++ inc/3rd-party/mobile/amp.php
@@ -11,15 +11,17 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
  */
 function rocket_disable_options_on_amp() {
 	if ( defined( 'AMP_QUERY_VAR' ) && function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
-		remove_filter( 'rocket_buffer', 'rocket_insert_deferred_js', 11 );
-
 		if ( function_exists( 'wp_resource_hints' ) ) {
 			remove_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
 		} else {
-			remove_filter( 'rocket_buffer', '__rocket_dns_prefetch_buffer', 12 );
+			remove_filter( 'rocket_buffer', 'rocket_dns_prefetch_buffer', 12 );
 		}
 
+		remove_filter( 'rocket_buffer', 'rocket_insert_deferred_js', 11 );
 		remove_filter( 'rocket_buffer', 'rocket_minify_process', 13 );
+		remove_filter( 'rocket_buffer', 'rocket_defer_js', 14 );
+		remove_filter( 'rocket_buffer', 'rocket_async_css', 15 );
+		remove_filter( 'rocket_buffer', 'rocket_minify_html', 20 );
 
 		add_filter( 'do_rocket_lazyload', '__return_false' );
 	}
