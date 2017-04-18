730d3c89e2afe05e47d798963ece65773c9f60c3
Brent Shepherd <brent@prospress.com>
2017-04-17 22:14:25 -0700

    Set version to 1.5.3

--- action-scheduler.php
+++ action-scheduler.php
@@ -5,24 +5,24 @@ Plugin URI: https://github.com/prospress/action-scheduler
 Description: A robust action scheduler for WordPress
 Author: Prospress
 Author URI: http://prospress.com/
-Version: 1.5.2
+Version: 1.5.3
 */
 
-if ( ! function_exists( 'action_scheduler_register_1_dot_5_dot_2' ) ) {
+if ( ! function_exists( 'action_scheduler_register_1_dot_5_dot_3' ) ) {
 
 	if ( ! class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once( 'classes/ActionScheduler_Versions.php' );
 		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
-	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_5_dot_2', 0, 0 );
+	add_action( 'plugins_loaded', 'action_scheduler_register_1_dot_5_dot_3', 0, 0 );
 
-	function action_scheduler_register_1_dot_5_dot_2() {
+	function action_scheduler_register_1_dot_5_dot_3() {
 		$versions = ActionScheduler_Versions::instance();
-		$versions->register( '1.5.2', 'action_scheduler_initialize_1_dot_5_dot_2' );
+		$versions->register( '1.5.3', 'action_scheduler_initialize_1_dot_5_dot_3' );
 	}
 
-	function action_scheduler_initialize_1_dot_5_dot_2() {
+	function action_scheduler_initialize_1_dot_5_dot_3() {
 		require_once( 'classes/ActionScheduler.php' );
 		ActionScheduler::init( __FILE__ );
 	}
