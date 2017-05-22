1c2c8a9d4ad79e7fc3d19fd051a0c4b2c580b9f3
Caspar Hübinger <caspar@glueckpress.com>
2017-05-22 19:20:15 +0200

    enhanced license validation
    Added panel caption with docs hint and prompt to contact support.

--- inc/admin/ui/modules/api-key.php
+++ inc/admin/ui/modules/api-key.php
@@ -1,42 +1,82 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
+// Are we white-labeled?
+$rwl = rocket_is_white_label();
+
 add_settings_section( 'rocket_display_apikey_options', __( 'License validation', 'rocket' ), '__return_false', 'rocket_apikey' );
+
+/**
+ * Panel caption
+ */
+if ( ! $rwl ) {
+
+	add_settings_field(
+		'rocket_apikey_options_panel',
+		false,
+		'rocket_field',
+		'rocket_apikey',
+		'rocket_display_apikey_options',
+		array(
+			array(
+				'type'         => 'helper_panel_description',
+				'name'         => 'apikey_options_panel_caption',
+				'description'  => sprintf(
+					'<span class="dashicons dashicons-lock" aria-hidden="true"></span><strong>%1$s</strong>',
+					/* translators: line break recommended, but not mandatory  */
+					__( 'WP Rocket was not able to automatically validate your license.<br>Follow <a href="http://docs.wp-rocket.me/article/100-resolving-problems-with-license-validation" target="_blank">this tutorial</a>, or contact <a href="https://wp-rocket.me/support/" target="_blank">support</a>.', 'rocket' )
+				),
+			),
+		)
+	);
+}
+
+/**
+ * API key
+ */
 add_settings_field(
 	'rocket_api_key',
-	__( 'API Key', 'rocket' ),
+	__( 'API key', 'rocket' ),
 	'rocket_field',
 	'rocket_apikey',
 	'rocket_display_apikey_options',
 	array(
 		array(
-			'type'			=> 'text',
-			'label_for'		=> 'consumer_key',
-			'label_screen'	=> __( 'API Key', 'rocket' ),
+			'type'         => 'helper_help',
+			'name'         => 'consumer_key_help',
+			'description'  => __( 'Enter the API key from your license account', 'rocket' ),
 		),
 		array(
-			'type'			=> 'helper_help',
-			'name'			=> 'consumer_key',
-			'description'	=> __( 'Please enter the API key obtained after your purchase.', 'rocket' ),
+			'type'         => 'text',
+			'label_for'    => 'consumer_key',
+			'label_screen' => __( 'API key', 'rocket' ),
+			'name'         => 'consumer_key',
+			'placeholder'  => '0987654321abcdfghi',
 		),
 	)
 );
+
+/**
+ * Customer email address
+ */
 add_settings_field(
 	'rocket_email',
-	__( 'E-mail Address', 'rocket' ),
+	__( 'Email address', 'rocket' ),
 	'rocket_field',
 	'rocket_apikey',
 	'rocket_display_apikey_options',
 	array(
 		array(
-			'type'         => 'email',
-			'label_for'    => 'consumer_email',
-			'label_screen' => __( 'E-mail Address', 'rocket' ),
+			'type'         => 'helper_help',
+			'name'         => 'consumer_email_help',
+			'description'  => __( 'Enter the email address registered with your license account', 'rocket' ),
 		),
 		array(
-			'type'         => 'helper_help',
+			'type'         => 'email',
+			'label_for'    => 'consumer_email',
+			'label_screen' => __( 'Email address', 'rocket' ),
 			'name'         => 'consumer_email',
-			'description'  => __( 'The one used for the purchase, in your support account.', 'rocket' ),
+			'placeholder'  => 'you@example.com',
 		),
 	)
 );
