78d4ff695f350082c5f56b07e3ceb1354cbf7bf5
Rémi Perona <remperona@gmail.com>
2017-06-06 22:14:13 -0400

    Use the background process to do the database optimization

--- inc/functions/admin.php
+++ inc/functions/admin.php
@@ -332,10 +332,14 @@ function rocket_is_plugin_installed( $plugin ) {
 /**
  * Performs the database optimization
  *
+ * @since Performs the database optimization using a background process
  * @since 2.8
  * @author Remy Perona
  */
 function do_rocket_database_optimization() {
+	global $rocket_database_optimization_process;
+	$rocket_database_optimization_process->cancel_process();
+
 	$options = array(
 	 'revisions',
 	 'auto_drafts',
@@ -349,114 +353,11 @@ function do_rocket_database_optimization() {
 
 	foreach ( $options as $option ) {
 		if ( get_rocket_option( 'database_' . $option, false ) ) {
-			rocket_database_optimize( $option );
+			$rocket_database_optimization_process->push_to_queue( $option );
 		}
 	}
-}
 
-/**
- * Optimizes the database depending on the option
- *
- * @since 2.8
- * @author Remy Perona
- *
- * @param string $type Type of optimization to perform.
- */
-function rocket_database_optimize( $type ) {
-	global $wpdb;
-
-	switch ( $type ) {
-		case 'revisions':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s", 'revision' ) );
-			if ( $query ) {
-				foreach ( $query as $id ) {
-					wp_delete_post_revision( intval( $id ) );
-				}
-			}
-			break;
-		case 'auto_drafts':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_status = %s", 'auto-draft' ) );
-			if ( $query ) {
-				foreach ( $query as $id ) {
-					wp_delete_post( intval( $id ), true );
-				}
-			}
-			break;
-		case 'trashed_posts':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_status = %s", 'trash' ) );
-			if ( $query ) {
-				foreach ( $query as $id ) {
-					wp_delete_post( $id, true );
-				}
-			}
-			break;
-		case 'spam_comments':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT comment_ID FROM $wpdb->comments WHERE comment_approved = %s", 'spam' ) );
-			if ( $query ) {
-				foreach ( $query as $id ) {
-					wp_delete_comment( intval( $id ), true );
-				}
-			}
-			break;
-		case 'trashed_comments':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT comment_ID FROM $wpdb->comments WHERE (comment_approved = %s OR comment_approved = %s)", 'trash', 'post-trashed' ) );
-			if ( $query ) {
-				foreach ( $query as $id ) {
-					wp_delete_comment( intval( $id ), true );
-				}
-			}
-			break;
-		case 'expired_transients':
-			$time = isset( $_SERVER['REQUEST_TIME'] ) ? (int) $_SERVER['REQUEST_TIME'] : time();
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE %s AND option_value < %s", '_transient_timeout%', $time ) );
-
-			if ( $query ) {
-				foreach ( $query as $transient ) {
-					$key = str_replace( '_transient_timeout_', '', $transient );
-					delete_transient( $key );
-				}
-			}
-			break;
-		case 'all_transients':
-			$query = $wpdb->get_col( $wpdb->prepare( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE %s", '%_transient_%' ) );
-			if ( $query ) {
-				foreach ( $query as $transient ) {
-					if ( strpos( $transient, '_site_transient_' ) !== false ) {
-						delete_site_transient( str_replace( '_site_transient_', '', $transient ) );
-					} else {
-						delete_transient( str_replace( '_transient_', '', $transient ) );
-					}
-				}
-			}
-			break;
-		case 'optimize_tables':
-			$query = $wpdb->get_results( $wpdb->prepare( "SELECT table_name, data_free FROM information_schema.tables WHERE table_schema = %s and Engine <> 'InnoDB' and data_free > 0", DB_NAME ) );
-			if ( $query ) {
-				foreach ( $query as $table ) {
-					$wpdb->query( $wpdb->prepare( 'OPTIMIZE TABLE %s', $table->table_name ) );
-				}
-			}
-			break;
-	}
-}
-
-/**
- * Run an async job to preload sitemaps in background
- *
- * @since 2.8
- *
- * @param array $body Contains the usual $_POST.
- **/
-function rocket_do_async_job( $body ) {
-	$args = array(
-		'timeout'   => 0.01,
-		'blocking'  => false,
-		'body'      => $body,
-		'cookies'   => $_COOKIE,
-		'sslverify' => apply_filters( 'https_local_ssl_verify', false ),
-	);
-
-	wp_remote_post( admin_url( 'admin-ajax.php' ), $args );
+	$rocket_database_optimization_process->save()->dispatch();
 }
 
 /**
