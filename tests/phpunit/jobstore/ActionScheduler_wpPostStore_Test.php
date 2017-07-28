8a805134d09cc7c3fb7a305d66ec686e21fb6677
Jonathan Brinley <jbrinley@flightless.us>
2017-07-28 17:11:27 -0400

    add method to the store to get an action's status

--- tests/phpunit/jobstore/ActionScheduler_wpPostStore_Test.php
+++ tests/phpunit/jobstore/ActionScheduler_wpPostStore_Test.php
@@ -195,5 +195,21 @@ class ActionScheduler_wpPostStore_Test extends ActionScheduler_UnitTestCase {
 
 		$this->assertEquals( (int)($now->format('U')) + HOUR_IN_SECONDS, $store->get_date($new_action_id)->format('U') );
 	}
+
+	public function test_get_status() {
+		$time = as_get_datetime_object('-10 minutes');
+		$schedule = new ActionScheduler_IntervalSchedule($time, HOUR_IN_SECONDS);
+		$action = new ActionScheduler_Action('my_hook', array(), $schedule);
+		$store = new ActionScheduler_wpPostStore();
+		$action_id = $store->save_action($action);
+
+		$this->assertEquals( ActionScheduler_Store::STATUS_PENDING, $store->get_status( $action_id ) );
+
+		$store->mark_complete( $action_id );
+		$this->assertEquals( ActionScheduler_Store::STATUS_COMPLETE, $store->get_status( $action_id ) );
+
+		$store->mark_failure( $action_id );
+		$this->assertEquals( ActionScheduler_Store::STATUS_FAILED, $store->get_status( $action_id ) );
+	}
 }
  
