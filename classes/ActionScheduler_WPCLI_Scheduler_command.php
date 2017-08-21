d0d14acab5944732dfa15700d97724556f956114
Jeremy Pry <jeremy.pry@gmail.com>
2017-08-21 12:17:06 -0400

    Wrap most of the command in try/catch statement

--- classes/ActionScheduler_WPCLI_Scheduler_command.php
+++ classes/ActionScheduler_WPCLI_Scheduler_command.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Commands for the Action Scheduler by Prospress.
+ */
 class ActionScheduler_WPCLI_Scheduler_command extends WP_CLI_Command {
 
 	/**
@@ -18,21 +21,34 @@ class ActionScheduler_WPCLI_Scheduler_command extends WP_CLI_Command {
 		$batch = \WP_CLI\Utils\get_flag_value( $assoc_args, 'batch-size', 100 );
 		$force = \WP_CLI\Utils\get_flag_value( $assoc_args, 'force', false );
 
-		// Set up the class instances we'll need
-		$store   = ActionScheduler_Store::instance();
-		$monitor = new ActionScheduler_FatalErrorMonitor();
-		$cleaner = new ActionScheduler_QueueCleaner();
+		$completed = 0;
 
-		// Get the queue runner instance
-		$runner = new ActionScheduler_WPCLI_QueueRunner( $store, $monitor, $cleaner );
+		try {
+			// Set up the class instances we'll need
+			$store   = ActionScheduler_Store::instance();
+			$monitor = new ActionScheduler_FatalErrorMonitor( $store );
+			$cleaner = new ActionScheduler_QueueCleaner( $store );
 
-		// Determine how many tasks will be run.
-		$total = $runner->setup( $batch, $force );
-		\WP_CLI::line(
-			sprintf( _n( 'Found %d scheduled task', 'Found %d scheduled tasks', $total, 'action-scheduler' ), $total )
-		);
+			// Get the queue runner instance
+			$runner = new ActionScheduler_WPCLI_QueueRunner( $store, $monitor, $cleaner );
 
-		$completed = $runner->run();
+			// Determine how many tasks will be run.
+			$total = $runner->setup( $batch, $force );
+			\WP_CLI::line(
+				sprintf( _n( 'Found %d scheduled task', 'Found %d scheduled tasks', $total, 'action-scheduler' ), $total )
+			);
+
+			$completed = $runner->run();
+		} catch ( Exception $e ) {
+			WP_CLI::error(
+				sprintf(
+					/* translators: %s refers to the exception error message. */
+					__( 'There was an error running the action scheduler: %s', 'action-scheduler' ),
+					$e->getMessage()
+				)
+			);
+		}
+
 		\WP_CLI::success(
 			sprintf(
 				_n( '%d scheduled task completed.', '%d scheduled taskes completed.', $completed, 'action-scheduler' ),
