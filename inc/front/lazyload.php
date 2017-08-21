e27ecdab18b3929e4d2fac9611f84fc317528147
Rémi Perona <remperona@gmail.com>
2017-08-21 15:52:28 -0400

    update the way the lazyload script and options are loaded

--- inc/front/lazyload.php
+++ inc/front/lazyload.php
@@ -2,57 +2,93 @@
 defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
 
 /**
- * Add Lazy Load JavaScript in the header
- * No jQuery or other library is required !!
+ * Add lazyload options to the footer
  *
+ * @since 2.11 load options in the footer and add filter for the treshold
  * @since 1.3.5 It's possible to exclude LazyLoad process by used do_rocket_lazyload filter
  * @since 1.1.0 This code is insert in head with inline script for more performance
  * @since 1.0
  */
-function rocket_lazyload_script() {
+function rocket_lazyload_config() {
+	if ( ( ! get_rocket_option( 'lazyload' ) && ! get_rocket_option( 'lazyload_iframes' ) ) || ( ! apply_filters( 'do_rocket_lazyload', true ) && ! apply_filters( 'do_rocket_lazyload_iframes', true ) ) ) {
+		return;
+	}
+
+	$threshold = apply_filters( 'rocket_lazyload_threshold', 300 );
+
+	echo <<<HTML
+	<script>
+	window.lazyLoadOptions = {
+		elements_selector: "img, iframe",
+		data_src: "lazySrc",
+		data_srcset: "lazySrcset",
+		class_loading: "lazyloading",
+		class_loaded: "lazyloaded",
+		threshold: $threshold,
+		callback_set: function(element) {
+			//todo: check fitvids compatibility (class or data-attribute)
+			if (  element.tagName === "IFRAME" && element.classList.contains("fitvidscompatible") ) {
+				if ( element.classList.contains("lazyloaded") ) {
+					//todo: check if $.fn.fitvids() is available
+					if ( typeof $ === "function" ) {
+						$( element ).parent().fitVids();
+					}
+				} else {
+					var temp = setInterval( function() {
+						//todo: check if $.fn.fitvids() is available
+						if ( element.classList.contains("lazyloaded") && typeof $ === "function" ) {
+							$( element ).parent().fitVids();
+							clearInterval( temp );
+						} else {
+							clearInterval( temp );
+						}
+					}, 50 );
+				}
+			} // if element is an iframe
+		}	
+	};
+	</script>
+HTML;
+}
+add_action( 'wp_footer', 'rocket_lazyload_config', 9 );
+
+/**
+ * Enqueue the lazyload script
+ *
+ * @since 2.11
+ * @author Remy Perona
+ */
+function rocket_lazyload_enqueue() {
 	if ( ( ! get_rocket_option( 'lazyload' ) && ! get_rocket_option( 'lazyload_iframes' ) ) || ( ! apply_filters( 'do_rocket_lazyload', true ) && ! apply_filters( 'do_rocket_lazyload_iframes', true ) ) ) {
 		return;
 	}
 
 	$suffix       = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 	$lazyload_url = get_rocket_cdn_url( WP_ROCKET_FRONT_JS_URL . 'lazyload-' . WP_ROCKET_LAZYLOAD_JS_VERSION . $suffix . '.js', array( 'all', 'css_and_js', 'js' ) );
-	$threshold    = apply_filters( 'rocket_lazyload_threshold', 300 );
 
-	echo <<<HTML
-	<script data-cfasync="false">(function(w,d){function loadScript(c,b){var a=d.createElement("script");a.async=!0;a.readyState?a.onreadystatechange=function(){if("loaded"===a.readyState||"complete"===a.readyState)a.onreadystatechange=null,b()}:a.onload=function(){b()};a.src=c;d.getElementsByTagName("head")[0].appendChild(a)}loadScript("$lazyload_url",function(){
-		var rocket_ll = new LazyLoad({
-			elements_selector: "img, iframe",
-			data_src: "lazy-src",
-			data_srcset: "lazy-srcset",
-			class_loading: "lazyloading",
-			class_loaded: "lazyloaded",
-			threshold: $threshold,
-			callback_set: function(element) {
-				//todo: check fitvids compatibility (class or data-attribute)
-				if (  element.tagName === "IFRAME" && element.classList.contains("fitvidscompatible") ) {
-					if ( element.classList.contains("lazyloaded") ) {
-						//todo: check if $.fn.fitvids() is available
-						if ( typeof $ === "function" ) {
-							$( element ).parent().fitVids();
-						}
-					} else {
-						var temp = setInterval( function() {
-							//todo: check if $.fn.fitvids() is available
-							if ( element.classList.contains("lazyloaded") && typeof $ === "function" ) {
-								$( element ).parent().fitVids();
-								clearInterval( temp );
-							} else {
-								clearInterval( temp );
-							}
-						}, 50 );
-					}
-				} // if element is an iframe
-			}	
-		});
-	});})(window,document);</script>
-HTML;
+	wp_enqueue_script( 'rocket-lazyload', $lazyload_url, null, null, true );
 }
-add_action( 'wp_head', 'rocket_lazyload_script', PHP_INT_MAX );
+add_action( 'wp_enqueue_scripts', 'rocket_lazyload_enqueue', PHP_INT_MAX );
+
+/**
+ * Add tags to the lazyload script to async and prevent concatenation
+ *
+ * @since 2.11
+ * @author Remy Perona
+ *
+ * @param string $tag HTML for the script.
+ * @param string $handle Handle for the script.
+ *
+ * @return string Updated HTML
+ */
+function rocket_lazyload_async_script( $tag, $handle ) {
+	if ( 'rocket-lazyload' === $handle ) {
+		return str_replace( '<script', '<script async data-cfasync="false" data-minify="1"', $tag );
+	}
+
+	return $tag;
+}
+add_filter( 'script_loader_tag', 'rocket_lazyload_async_script', 10, 2 );
 
 /**
  * Replace Gravatar, thumbnails, images in post content and in widget text by LazyLoad
