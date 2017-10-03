0035ca6c7dfa4d2243bfab24b741c322b608abc7
Rémi Perona <remperona@gmail.com>
2017-10-03 14:06:16 -0400

    fix PHP warning and notice

--- inc/functions/options.php
+++ inc/functions/options.php
@@ -484,19 +484,16 @@ function get_rocket_exclude_js() {
  * @return array List of JS files.
  */
 function get_rocket_minify_js_in_footer() {
-	global $rocket_enqueue_js_in_footer, $wp_scripts;
-
-	$js_files = array_map( 'rocket_set_internal_url_scheme', $js_files );
-	$js_files = array_unique( array_merge( $js_files, (array) $rocket_enqueue_js_in_footer ) );
+	global $rocket_enqueue_js_in_footer;
 
 	/**
 	 * Filter JS files to move in the footer during the minification.
 	 *
 	 * @since 2.6
 	 *
-	 * @param array $js_files List of JS files.
+	 * @param array $rocket_enqueue_js_in_footer List of JS files.
 	*/
-	$js_files = apply_filters( 'rocket_minify_js_in_footer', $js_files );
+	$js_files = apply_filters( 'rocket_minify_js_in_footer', $rocket_enqueue_js_in_footer );
 
 	return $js_files;
 }
