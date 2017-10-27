0777cb1e5a70a62e7457b433845bbd9e6435739b
Rémi Perona <remperona@gmail.com>
2017-10-27 15:42:52 -0400

    Revert "Remove no longer used files"
    This reverts commit 44c828a55c4a26158e1a859fd521ea206b29397a.

---
+++ inc/classes/background-processing.php
@@ -0,0 +1,55 @@
+<?php
+defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
+
+require( WP_ROCKET_VENDORS_PATH . 'wp-async-request.php' );
+require( WP_ROCKET_VENDORS_PATH . 'wp-background-process.php' );
+
+/**
+ * Extends the background process class for the sitemap preload background process.
+ *
+ * @since 2.7
+ *
+ * @see WP_Background_Process
+ */
+class Rocket_Sitemap_Preload_Process extends WP_Background_Process {
+
+	/**
+	 * Specific action identifier for sitemap preload.
+	 *
+	 * @since 2.7
+	 * @access protected
+	 * @var string Action identifier
+	 */
+	protected $action = 'rocket_sitemap_preload';
+
+	/**
+	 * Preload the URL provided by $item
+	 *
+	 * @param mixed $item Queue item to iterate over.
+	 *
+	 * @return null
+	 */
+	protected function task( $item ) {
+		$args = array(
+			'timeout'    => 0.01,
+			'blocking'   => false,
+			'user-agent' => 'wprocketbot',
+			'sslverify'  => apply_filters( 'https_local_ssl_verify', true ),
+		);
+
+		$tmp = wp_remote_get( esc_url_raw( $item ), $args );
+		usleep( get_rocket_option( 'sitemap_preload_url_crawl', '500000' ) );
+
+		return false;
+	}
+
+	/**
+	 * Complete
+	 */
+	protected function complete() {
+		parent::complete();
+	}
+
+}
+
+$GLOBALS['rocket_sitemap_background_process'] = new Rocket_Sitemap_Preload_Process();
