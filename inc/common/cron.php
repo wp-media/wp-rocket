90f5f87ea3a6047f67f6fbc4e1fef1e877d4fc21
Rémi Perona <remperona@gmail.com>
2017-10-27 15:40:09 -0400

    Revert "Replace preload bot by sitemap preload"
    This reverts commit 384b8f4fe50478246e4510f11955c2c214887f49.

--- inc/common/cron.php
+++ inc/common/cron.php
@@ -70,9 +70,7 @@ function do_rocket_purge_cron() {
 	// Purge domain cache files.
 	rocket_clean_domain();
 
-	// Run WP Rocket sitemap preload if activated.
-	if ( get_rocket_option( 'sitemap_preload', 0 ) ) {
-		run_rocket_sitemap_preload();
-	}
+	// Run WP Rocket Bot for preload cache files.
+	run_rocket_preload_cache( 'cache-preload' );
 }
 add_action( 'rocket_purge_time_event', 'do_rocket_purge_cron' );
