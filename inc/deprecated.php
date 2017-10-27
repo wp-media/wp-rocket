3de041d98bbdc78adde3435ae91be899592d0287
Rémi Perona <remperona@gmail.com>
2017-10-27 19:39:06 -0400

    add old database optimization functions to deprecated

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -542,11 +542,65 @@ function rocket_admin_print_styles() {
  *
  * @since 2.8
  * @deprecated 2.11
- * @see Rocket_Database_Optimisation->optimize()
+ * @see Rocket_Background_Database_Optimisation->task()
  * @author Remy Perona
  *
  * @param string $type Type of optimization to perform.
  */
 function rocket_database_optimize( $type ) {
+	_deprecated_function( __FUNCTION__, '2.11', 'Rocket_Background_Database_Optimisation->task()' );
+}
+
+/**
+ * Launches the database optimization from admin
+ *
+ * @since 2.8
+ * @deprecated 2.11
+ * @see Rocket_Database_Optimisation->optimize()
+ * @author Remy Perona
+ */
+function rocket_optimize_database() {
 	_deprecated_function( __FUNCTION__, '2.11', 'Rocket_Database_Optimisation->optimize()' );
+}
+
+/**
+ * Count the number of items concerned by the database cleanup
+ *
+ * @since 2.8
+ * @deprecated 2.11
+ * @see Rocket_Database_Optimisation->count_cleanup_items()
+ * @author Remy Perona
+ *
+ * @param string $type Item type to count.
+ * @return int Number of items for this type
+ */
+function rocket_database_count_cleanup_items( $type ) {
+	_deprecated_function( __FUNCTION__, '2.11', 'Rocket_Database_Optimisation->count_cleanup_items()' );
+
+	return 0;
+}
+
+/**
+ * Planning database optimization cron
+ * If the task is not programmed, it is automatically triggered
+ *
+ * @since 2.8
+ * @deprecated 2.11
+ * @see Rocket_Database_Optimisation->database_optimization_scheduled()
+ * @author Remy Perona
+ */
+function rocket_database_optimization_scheduled() {
+	_deprecated_function( __FUNCTION__, '2.11', 'Rocket_Database_Optimisation->database_optimization_scheduled()' );
+}
+
+/**
+ * Performs the database optimization
+ *
+ * @since 2.8
+ * @deprecated 2.11
+ * @see Rocket_Database_Optimisation->process_handler()
+ * @author Remy Perona
+ */
+function do_rocket_database_optimization() {
+	_deprecated_function( __FUNCTION__, '2.11', 'Rocket_Database_Optimisation->process_handler()' );
 }
