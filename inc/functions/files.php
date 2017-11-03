0981f97abcb694b2f5fc3e7974735b22ea583efa
Rémi Perona <remperona@gmail.com>
2017-11-03 08:59:38 -0400

    remove deny from all preventing access of cache file from htaccess rewrite rules

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -259,7 +259,7 @@ function rocket_init_cache_dir() {
 
 		if ( ! rocket_direct_filesystem()->is_file( $htaccess_path ) ) {
 			rocket_direct_filesystem()->touch( $htaccess_path );
-			rocket_direct_filesystem()->put_contents( $htaccess_path, "Options -Indexes\ndeny from all" );
+			rocket_direct_filesystem()->put_contents( $htaccess_path, "Options -Indexes" );
 		}
 	}
 
