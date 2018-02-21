bd697c0b06bb6d03a184caa1addff9fc6819fb98
Rémy Perona <remperona@gmail.com>
2018-02-21 15:25:02 -0500

    add addons page view

---
+++ views/settings/page-sections/addons.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Addons section template.
+ *
+ * @since 3.0
+ *
+ * @param array {
+ *     Section arguments.
+ *
+ *     @type string $id    Page section identifier.
+ *     @type string $title Page section title.
+ * }
+ */
+
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+?>
+<h2 id="<?php echo esc_attr( $data['id'] ); ?>"><?php echo esc_html( $data['title'] ); ?></h2>
