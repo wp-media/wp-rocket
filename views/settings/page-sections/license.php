886e266cd2e5511aec81692937474d2a304a636d
Rémy Perona <remperona@gmail.com>
2018-02-22 15:25:39 -0500

    add section description

--- views/settings/page-sections/license.php
+++ views/settings/page-sections/license.php
@@ -15,4 +15,9 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 <h2 id="<?php echo esc_attr( $data['id'] ); ?>"><?php echo esc_html( $data['title'] ); ?></h2>
+<?php _e( 'WP Rocket was not able to automatically validate your license.', 'rocket' ); ?>
+<?php
+// translators: %1$s = tutorial URL, %2$s = support URL.
+printf( __( 'Follow this <a href="%1$s">tutorial</a>, or contact <a href="%2$s">support</a> to get the engine started.', 'rocket' ), '', '' );
+?>
 <?php $this->render_settings_sections( $data['id'] ); ?>
