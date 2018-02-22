77b8d0ccb58d87c1050c8a278325a59500e47079
Rémy Perona <remperona@gmail.com>
2018-02-22 15:25:49 -0500

    move label above input

--- views/settings/fields/text.php
+++ views/settings/fields/text.php
@@ -13,17 +13,11 @@
  *     @type string $value       Field value.
  *     @type string $description Field description.
  *     @type string $input_attr  Attributes for the input field.
- *     @type array  $warning {
- *         Warning panel content.
- *
- *         @type string $title        Warning title.
- *         @type string $description  Warning description.
- *         @type string $button_label Warning Button label.
- *     }
  * }
  */
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
-<input type="text" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="<?php echo esc_attr( $data['value'] ); ?>"<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>> <label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
+<label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
+<input type="text" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="<?php echo esc_attr( $data['value'] ); ?>"<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
 
