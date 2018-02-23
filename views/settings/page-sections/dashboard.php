2304f8388fedf2b9b1c71bf89ebecedd9616339f
Rémy Perona <remperona@gmail.com>
2018-02-23 14:15:45 -0500

    use render documentation block function

--- views/settings/page-sections/dashboard.php
+++ views/settings/page-sections/dashboard.php
@@ -67,13 +67,4 @@ $this->render_action_button( 'button', 'ask_support', [
 	'label' => __( 'Ask support', 'rocket' ),
 	'icon'  => '',
 ] );
-?>
-<h3><?php esc_html_e( 'Documentation', 'rocket' ); ?></h3>
-<p><?php esc_html_e( 'It is a great starting point to fix some of the most common issues.', 'rocket' ); ?></p>
-<?php
-$this->render_action_button( 'link', 'documentation', [
-	'label'      => __( 'Read the documentation', 'rocket' ),
-	'attributes' => [
-		'target' => '_blank',
-	],
-] );
+$this->render_documentation_block(); ?>
