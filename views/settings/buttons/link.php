d98031e8daf7cee553dd9150c2ebe2a72929952b
Rémy Perona <remperona@gmail.com>
2018-02-23 00:16:07 -0500

    add template DocBlock

--- views/settings/buttons/link.php
+++ views/settings/buttons/link.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Action button link template.
+ *
+ * @since 3.0
+ *
+ * @data array {
+ *     Data to populate the template.
+ *
+ *     @type string $label      Link text.
+ *     @type string $url        URL for the href attribute.
+ *     @type string $attributes String of attribute=value for the <a> tag, e.g. class, target, etc.
+ * }
+ */
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
