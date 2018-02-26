9780c54d11fa3ee397405ab4c4b5d2d7d4be4c2e
Thomas Geisen <tom.geisen@gmail.com>
2018-02-26 11:16:16 +0100

    add call to new css/js files and hide old files

--- inc/admin/ui/enqueue.php
+++ inc/admin/ui/enqueue.php
@@ -7,64 +7,67 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
  * @since 1.0.0
  */
 function rocket_add_admin_css_js() {
-	wp_enqueue_script( 'jquery-ui-sortable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
-	wp_enqueue_script( 'jquery-ui-draggable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
-	wp_enqueue_script( 'jquery-ui-droppable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
-	wp_enqueue_script( 'options-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'options.js', array( 'jquery', 'jquery-ui-core' ), WP_ROCKET_VERSION, true );
-	wp_enqueue_script( 'fancybox-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'vendors/jquery.fancybox.pack.js', array( 'options-wp-rocket' ), WP_ROCKET_VERSION, true );
-	wp_enqueue_script( 'sweet-alert-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'vendors/sweetalert2.min.js', array( 'options-wp-rocket' ), WP_ROCKET_VERSION, true );
+	// wp_enqueue_script( 'jquery-ui-sortable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
+	// wp_enqueue_script( 'jquery-ui-draggable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
+	// wp_enqueue_script( 'jquery-ui-droppable', null, array( 'jquery', 'jquery-ui-core' ), null, true );
+	// wp_enqueue_script( 'options-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'options.js', array( 'jquery', 'jquery-ui-core' ), WP_ROCKET_VERSION, true );
+	// wp_enqueue_script( 'fancybox-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'vendors/jquery.fancybox.pack.js', array( 'options-wp-rocket' ), WP_ROCKET_VERSION, true );
+	// wp_enqueue_script( 'sweet-alert-wp-rocket', WP_ROCKET_ADMIN_UI_JS_URL . 'vendors/sweetalert2.min.js', array( 'options-wp-rocket' ), WP_ROCKET_VERSION, true );
+    //
+	// wp_enqueue_style( 'options-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'options.css', array(), WP_ROCKET_VERSION );
+	// wp_enqueue_style( 'fancybox-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'fancybox/jquery.fancybox.css', array( 'options-wp-rocket' ), WP_ROCKET_VERSION );
+    //
+	// $minify_text = __( 'This could break things!', 'rocket' ) . '<br>' .
+	// 	__( 'Before you continue activating this setting, read the documentation:', 'rocket' ) .
+	// 	sprintf(
+	// 		'<br><br><span class="dashicons dashicons-media-text" aria-hidden="true"></span>&#160;<a href="%1$s?utm_source=wp-rocket&utm_medium=wp-admin&utm_term=doc-minification&utm_campaign=plugin" target="_blank">%2$s</a><br/><br/>',
+	// 		/* translators: Documentation exists in EN, DE, FR, ES, IT */
+	// 		__( 'http://docs.wp-rocket.me/article/19-resolving-issues-with-file-optimization', 'rocket' ),
+	// 		/* translators: Translate doc title if you translated doc URL */
+	// 		__( 'Resolving issues with file optimization', 'rocket' )
+	// 	) .
+	// 	__( 'If you notice any errors on your website after having activated this setting, just deactivate it again, and your site will be back to normal.', 'rocket' );
+    //
+	// // Sweet Alert.
+	// $translation_array = array(
+	// 	'warningTitle'     => __( 'Fair warning:', 'rocket' ),
+	// 	'requiredTitle'    => __( 'All fields are required!', 'rocket' ),
+    //
+	// 	'cloudflareTitle'  => __( 'Cloudflare Settings', 'rocket' ),
+	// 	'cloudflareText'   => __( 'Save your changes to activate the Cloudflare tab.', 'rocket' ),
+    //
+	// 	'preloaderTitle' => __( 'Transmitting your message …', 'rocket' ),
+	// 	'preloaderImg'   => WP_ROCKET_ADMIN_UI_IMG_URL . 'preloader.gif',
+    //
+	// 	'badServerConnectionTitle'             => __( 'Unable to transmit', 'rocket' ),
+	// 	'badServerConnectionText'              => __( 'It seems that communications with Mission Control are temporarily down … Please send a support ticket while our Rocketeers fix the issue.', 'rocket' ),
+	// 	'badServerConnectionConfirmButtonText' => __( 'Get help from a Support Rocketeer', 'rocket' ),
+    //
+	// 	'warningSupportTitle' => __( 'Last steps before contacting us', 'rocket' ),
+	// 	// translators: %s is the documentation URL.
+	// 	'warningSupportText'  => sprintf( __( 'You have to read the <a href="%s" target="_blank">documentation</a> and to agree to send informations relative to your website to submit a support ticket.', 'rocket' ), get_rocket_documentation_url() . '?utm_source=wp-rocket&utm_medium=wp-admin&utm_term=doc-support&utm_campaign=plugin' ),
+    //
+	// 	'successSupportTitle' => __( 'Transmission Received!', 'rocket' ),
+	// 	'successSupportText'  => __( 'We have received your ticket and will reply back within a few hours!', 'rocket' ) . '<br/>' . __( 'We answer every ticket, so please check your spam folder if you do not hear from us.', 'rocket' ),
+    //
+	// 	'badSupportTitle'      => __( 'Hmm, that’s problematic …', 'rocket' ),
+	// 	'badSupportText'       => __( 'It looks like you have got yourself a “nulled” version of WP Rocket. In order to activate your plugin and receive support and updates you will need a valid license.', 'rocket' ) . '<br/>' . __( 'Click below to purchase a license with a 20% discount automatically applied.', 'rocket' ),
+	// 	'badConfirmButtonText' => __( 'Buy a valid license now', 'rocket' ),
+    //
+	// 	'expiredSupportTitle'      => __( 'Uh-oh, you’re out of fuel …', 'rocket' ),
+	// 	'expiredSupportText'       => __( 'To keep WP Rocket running with access to support and updates, <strong>you will need to renew your license</strong>.', 'rocket' ) . '<br/><br/>' . __( 'Click below to renew with a <strong>discount of 50%</strong> automatically applied!', 'rocket' ),
+	// 	'expiredConfirmButtonText' => __( 'Buy renewal now', 'rocket' ),
+    //
+	// 	'minifyText' => $minify_text,
+    //
+	// 	'confirmButtonText' => __( 'Activate this setting', 'rocket' ),
+	// 	'cancelButtonText'  => __( 'Cancel', 'rocket' ),
+	// );
+	// wp_localize_script( 'options-wp-rocket', 'sawpr', $translation_array );
+	// wp_enqueue_style( 'sweet-alert-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'sweetalert2.min.css', array( 'options-wp-rocket' ), WP_ROCKET_VERSION );
 
-	wp_enqueue_style( 'options-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'options.css', array(), WP_ROCKET_VERSION );
-	wp_enqueue_style( 'fancybox-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'fancybox/jquery.fancybox.css', array( 'options-wp-rocket' ), WP_ROCKET_VERSION );
-
-	$minify_text = __( 'This could break things!', 'rocket' ) . '<br>' .
-		__( 'Before you continue activating this setting, read the documentation:', 'rocket' ) .
-		sprintf(
-			'<br><br><span class="dashicons dashicons-media-text" aria-hidden="true"></span>&#160;<a href="%1$s?utm_source=wp-rocket&utm_medium=wp-admin&utm_term=doc-minification&utm_campaign=plugin" target="_blank">%2$s</a><br/><br/>',
-			/* translators: Documentation exists in EN, DE, FR, ES, IT */
-			__( 'http://docs.wp-rocket.me/article/19-resolving-issues-with-file-optimization', 'rocket' ),
-			/* translators: Translate doc title if you translated doc URL */
-			__( 'Resolving issues with file optimization', 'rocket' )
-		) .
-		__( 'If you notice any errors on your website after having activated this setting, just deactivate it again, and your site will be back to normal.', 'rocket' );
-
-	// Sweet Alert.
-	$translation_array = array(
-		'warningTitle'     => __( 'Fair warning:', 'rocket' ),
-		'requiredTitle'    => __( 'All fields are required!', 'rocket' ),
-
-		'cloudflareTitle'  => __( 'Cloudflare Settings', 'rocket' ),
-		'cloudflareText'   => __( 'Save your changes to activate the Cloudflare tab.', 'rocket' ),
-
-		'preloaderTitle' => __( 'Transmitting your message …', 'rocket' ),
-		'preloaderImg'   => WP_ROCKET_ADMIN_UI_IMG_URL . 'preloader.gif',
-
-		'badServerConnectionTitle'             => __( 'Unable to transmit', 'rocket' ),
-		'badServerConnectionText'              => __( 'It seems that communications with Mission Control are temporarily down … Please send a support ticket while our Rocketeers fix the issue.', 'rocket' ),
-		'badServerConnectionConfirmButtonText' => __( 'Get help from a Support Rocketeer', 'rocket' ),
-
-		'warningSupportTitle' => __( 'Last steps before contacting us', 'rocket' ),
-		// translators: %s is the documentation URL.
-		'warningSupportText'  => sprintf( __( 'You have to read the <a href="%s" target="_blank">documentation</a> and to agree to send informations relative to your website to submit a support ticket.', 'rocket' ), get_rocket_documentation_url() . '?utm_source=wp-rocket&utm_medium=wp-admin&utm_term=doc-support&utm_campaign=plugin' ),
-
-		'successSupportTitle' => __( 'Transmission Received!', 'rocket' ),
-		'successSupportText'  => __( 'We have received your ticket and will reply back within a few hours!', 'rocket' ) . '<br/>' . __( 'We answer every ticket, so please check your spam folder if you do not hear from us.', 'rocket' ),
-
-		'badSupportTitle'      => __( 'Hmm, that’s problematic …', 'rocket' ),
-		'badSupportText'       => __( 'It looks like you have got yourself a “nulled” version of WP Rocket. In order to activate your plugin and receive support and updates you will need a valid license.', 'rocket' ) . '<br/>' . __( 'Click below to purchase a license with a 20% discount automatically applied.', 'rocket' ),
-		'badConfirmButtonText' => __( 'Buy a valid license now', 'rocket' ),
-
-		'expiredSupportTitle'      => __( 'Uh-oh, you’re out of fuel …', 'rocket' ),
-		'expiredSupportText'       => __( 'To keep WP Rocket running with access to support and updates, <strong>you will need to renew your license</strong>.', 'rocket' ) . '<br/><br/>' . __( 'Click below to renew with a <strong>discount of 50%</strong> automatically applied!', 'rocket' ),
-		'expiredConfirmButtonText' => __( 'Buy renewal now', 'rocket' ),
-
-		'minifyText' => $minify_text,
-
-		'confirmButtonText' => __( 'Activate this setting', 'rocket' ),
-		'cancelButtonText'  => __( 'Cancel', 'rocket' ),
-	);
-	wp_localize_script( 'options-wp-rocket', 'sawpr', $translation_array );
-	wp_enqueue_style( 'sweet-alert-wp-rocket', WP_ROCKET_ADMIN_UI_CSS_URL . 'sweetalert2.min.css', array( 'options-wp-rocket' ), WP_ROCKET_VERSION );
+	wp_enqueue_style( 'wpr-admin-style', WP_ROCKET_ASSETS_CSS_URL . 'wpr-admin.css', null, WP_ROCKET_VERSION );
+	wp_enqueue_script( 'wpr-admin-js', WP_ROCKET_ASSETS_JS_URL . 'wpr-admin.js', null, WP_ROCKET_VERSION, true);
 }
 add_action( 'admin_print_styles-settings_page_' . WP_ROCKET_PLUGIN_SLUG, 'rocket_add_admin_css_js' );
 
