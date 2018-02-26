8f868bc4cb7ac161fb46201bdc4f1fdefcf0540a
Thomas Geisen <tom.geisen@gmail.com>
2018-02-26 12:20:22 +0100

    add the interface basic elements in HTML and CSS

--- views/settings/page.php
+++ views/settings/page.php
@@ -12,19 +12,30 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 settings_errors( $data['slug'] ); ?>
-<div class="wrap">
-	<?php $heading_tag = version_compare( $GLOBALS['wp_version'], '4.3' ) >= 0 ? 'h1' : 'h2'; ?>
-	<<?php echo $heading_tag; ?>><?php echo esc_html( get_admin_page_title() ); ?></<?php echo $heading_tag; ?>>
-	<?php $this->render_navigation(); ?>
-	<?php
-		// translators: %s = Plugin version number.
-		printf( __( 'version %s', 'rocket' ), WP_ROCKET_VERSION );
-	?>
-	<form action="options.php" method="POST" id="<?php echo esc_attr( $data['slug'] ); ?>_options">
-		<?php settings_fields( $data['slug'] ); ?>
-		<?php $this->render_form_sections(); ?>
-		<?php $this->render_hidden_fields(); ?>
-		<input type="submit" value="<?php esc_attr_e( 'Save Changes', 'rocket' ); ?>">
-	</form>
-	<?php $this->render_tools_section(); ?>
+<div class="wpr-wrap wrap">
+	<div class="wpr-body">
+		<header class="wpr-Header">
+			<!-- <?php $heading_tag = version_compare( $GLOBALS['wp_version'], '4.3' ) >= 0 ? 'h1' : 'h2'; ?>
+			<<?php echo $heading_tag; ?>><?php echo esc_html( get_admin_page_title() ); ?></<?php echo $heading_tag; ?>> -->
+			<div class="wpr-logo">
+				<img src="<?php echo WP_ROCKET_ASSETS_IMG_URL; ?>/logo-wprocket-light.svg" width="163" height="44" alt="Logo WP Rocket">
+			</div>
+			<?php $this->render_navigation(); ?>
+			<?php
+			// translators: %s = Plugin version number.
+			printf( __( 'version %s', 'rocket' ), WP_ROCKET_VERSION );
+			?>
+		</header>
+		<div class="wpr-Content">
+			<form action="options.php" method="POST" id="<?php echo esc_attr( $data['slug'] ); ?>_options">
+				<?php settings_fields( $data['slug'] ); ?>
+				<?php $this->render_form_sections(); ?>
+				<?php $this->render_hidden_fields(); ?>
+				<input type="submit" value="<?php esc_attr_e( 'Save Changes', 'rocket' ); ?>">
+			</form>
+		</div>
+		<div class="wpr-Sidebar">
+			<?php $this->render_tools_section(); ?>
+		</div>
+	</div>
 </div>
