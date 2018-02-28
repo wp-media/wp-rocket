0ebfcfb26a07931eae4e998b36b76969c91568c8
Rémy Perona <remperona@gmail.com>
2018-02-28 12:01:28 -0500

    use empty instead of isset for description

--- views/settings/fields/checkbox.php
+++ views/settings/fields/checkbox.php
@@ -26,16 +26,16 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<div class="wpr-field wpr-field--checkbox <?php echo ($data['parent']) ? 'wpr-field--children' : '';?>">
+<div class="wpr-field wpr-field--checkbox <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
 	<div class="wpr-checkbox">
 		<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>
 		<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
 		<label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
 	</div>
 
-	<?php if ( isset( $data['description'] ) ) { ?>
+	<?php if ( ! empty( $data['description'] ) ) { ?>
 	<div class="wpr-field-description">
-		<?php echo esc_html( $data['description'] );?>
+		<?php echo esc_html( $data['description'] ); ?>
 	</div>
 	<?php } ?>
 </div>
