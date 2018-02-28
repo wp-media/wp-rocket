f3a5582e81889e7009dd74b9b5b22d8143795859
Thomas Geisen <tom.geisen@gmail.com>
2018-02-28 16:03:17 +0100

    add HTML and style for .fieldWarning container

--- views/settings/fields/checkbox.php
+++ views/settings/fields/checkbox.php
@@ -25,7 +25,7 @@
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
-<div class="wpr-field">
+<div class="wpr-field wpr-field--checkbox">
 	<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>
 	<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
 	<label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
@@ -35,12 +35,18 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 		<?php echo esc_html( $data['description'] );?>
 	</div>
 	<?php } ?>
-
-	<?php if ( ! empty( $data['warning'] ) ) { ?>
-		<div class="wpr-field-warning">
-			<?php echo esc_html( $data['warning']['title'] ); ?>
-			<?php echo esc_html( $data['warning']['description'] ); ?>
-			<button class="wpr-button wpr-button--small wpr-button--icon wpr-icon-check"><?php echo esc_html( $data['warning']['button_label'] ); ?></button>
-		</div>
-	<?php } ?>
 </div>
+
+<?php if ( ! empty( $data['warning'] ) ) { ?>
+	<div class="wpr-fieldWarning">
+		<div class="wpr-fieldWarning-title wpr-icon-important">
+			<?php echo esc_html( $data['warning']['title'] ); ?>
+		</div>
+		<?php if ( isset( $data['warning']['description'] ) ) { ?>
+			<div class="wpr-fieldWarning-description">
+				<?php echo esc_html( $data['warning']['description'] ); ?>
+			</div>
+		<?php } ?>
+		<button class="wpr-button wpr-button--small wpr-button--icon wpr-icon-check"><?php echo esc_html( $data['warning']['button_label'] ); ?></button>
+	</div>
+<?php } ?>
