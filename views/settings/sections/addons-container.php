9d458fd43d5bd31e4a1bfe45b6a7db5bbceaad7f
Thomas Geisen <tom.geisen@gmail.com>
2018-02-28 13:22:08 +0100

    add common HTML to addons container

--- views/settings/sections/addons-container.php
+++ views/settings/sections/addons-container.php
@@ -17,6 +17,19 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<h3><?php echo esc_html( $data['title'] ); ?></h3>
-<?php echo esc_html( $data['description'] ); ?>
-<?php $this->render_settings_fields( $data['page'], $data['id'] ); ?>
+<div class="wpr-optionHeader">
+	<h3 class="wpr-title2"><?php echo esc_html( $data['title'] ); ?></h3>
+	<?php if ( ! empty( $data['help'] ) ) : ?>
+	<button data-beacon-id="<?php echo esc_attr( $data['help'] ); ?>" class="wpr-infoAction wpr-infoAction--help wpr-icon-help"><?php esc_html_e( 'Need Help?', 'rocket' ); ?></button>
+	<?php endif; ?>
+</div>
+
+<div class="wpr-fieldsContainer">
+	<div class="wpr-fieldsContainer-description">
+		<?php echo esc_html( $data['description'] ); ?>
+	</div>
+
+	<fieldset class="wpr-fieldsContainer-fieldset">
+		<?php $this->render_settings_fields( $data['page'], $data['id'] ); ?>
+	</fieldset>
+</div>
