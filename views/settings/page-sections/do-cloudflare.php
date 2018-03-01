fcfebd503bb528d9074355d6e69466a5a9d825aa
Rémy Perona <remperona@gmail.com>
2018-03-01 17:35:52 -0500

    add action button

--- views/settings/page-sections/do-cloudflare.php
+++ views/settings/page-sections/do-cloudflare.php
@@ -18,10 +18,20 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 <div id="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-Page">
 	<div class="wpr-sectionHeader">
 		<h2 class="wpr-title1">
-	        <img src="<?php echo WP_ROCKET_ASSETS_IMG_URL; ?>/logo-cloudflare.svg" width="153" height="51" alt="Logo WP Rocket">
-	    </h2>
+			<img src="<?php echo WP_ROCKET_ASSETS_IMG_URL; ?>/logo-cloudflare.svg" width="153" height="51" alt="Logo WP Rocket">
+		</h2>
 	</div>
 	<?php $this->render_settings_sections( $data['id'] ); ?>
-	<h3><?php esc_html_e( 'Cloudflare Cache', 'rocket' ); ?></h3>
+	<div class="wpr-optionHeader">
+		<h3 class="wpr-title2"><?php esc_html_e( 'Cloudflare Cache', 'rocket' ); ?></h3>
+	</div>
 	<p><?php esc_html_e( 'Purges cached resources for your website. Learn more', 'rocket' ); ?></p>
+	<?php
+	$this->render_action_button( 'link', 'purge_cloudflare', [
+		'label'      => __( 'Clear all Cloudflare cache files', 'rocket' ),
+		'attributes' => [
+			'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-trash',
+		],
+	]);
+	?>
 </div>
