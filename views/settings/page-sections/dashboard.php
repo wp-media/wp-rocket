294b9627b1a342b10a8ca7bd68e5a5cd2ed5b07f
Thomas Geisen <tom.geisen@gmail.com>
2018-03-01 15:17:20 +0100

    add columns in dashboard

--- views/settings/page-sections/dashboard.php
+++ views/settings/page-sections/dashboard.php
@@ -27,102 +27,105 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 		<h2 class="wpr-title1 wpr-icon-home"><?php echo $data['title']; ?></h2>
 	</div>
 
-	<div class="wpr-page-flex">
-		<div class="wpr-optionHeader">
-			<h3 class="wpr-title2"><?php esc_html_e( 'My account', 'rocket' ); ?></h3>
+	<div class="wpr-Page-row">
+		<div class="wpr-Page-col">
+			<div class="wpr-optionHeader">
+				<h3 class="wpr-title2"><?php esc_html_e( 'My account', 'rocket' ); ?></h3>
+				<?php
+				$this->render_action_button( 'button', 'refresh_account', [
+					'label' => __( 'Refresh info', 'rocket' ),
+					'attributes' => [
+						'class'  => 'wpr-infoAction wpr-icon-refresh',
+					],
+					] );
+					?>
+			</div>
+
+			<?php esc_html_e( 'License' ); ?>
+			<?php esc_html_e( 'Expiration date' ); ?>
 			<?php
-			$this->render_action_button( 'button', 'refresh_account', [
-				'label' => __( 'Refresh info', 'rocket' ),
+			$this->render_action_button( 'link', 'view_account', [
+				'label'      => __( 'View my account', 'rocket' ),
+				'icon'       => '',
 				'attributes' => [
-					'class'  => 'wpr-infoAction wpr-icon-refresh',
+					'target' => '_blank',
+					'class'  => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-user',
 				],
 				] );
-				?>
+			?>
+
+			<?php
+				$this->render_settings_sections( $data['id'] );
+			?>
 		</div>
 
-		<?php esc_html_e( 'License' ); ?>
-		<?php esc_html_e( 'Expiration date' ); ?>
-		<?php
-		$this->render_action_button( 'link', 'view_account', [
-			'label'      => __( 'View my account', 'rocket' ),
-			'icon'       => '',
-			'attributes' => [
-				'target' => '_blank',
-				'class'  => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-user',
-			],
-			] );
-		?>
+		<div class="wpr-Page-col wpr-Page-col--fixed">
+			<div class="wpr-optionHeader">
+				<h3 class="wpr-title2"><?php esc_html_e( 'Quick Actions', 'rocket' ); ?></h3>
+			</div>
 
-		<?php
-			$this->render_settings_sections( $data['id'] );
-		?>
+			<h4><?php esc_html_e( 'Remove all cached files', 'rocket' ); ?></h4>
+			<?php
+			$this->render_action_button( 'link', 'purge_cache', [
+				'label'      => __( 'Clear cache', 'rocket' ),
+				'icon'       => '',
+				'parameters' => [
+					'type' => 'all',
+				],
+				'attributes' => [
+					'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-trash',
+				],
+				] );
+			?>
+
+			<h4><?php esc_html_e( 'Start cache preloading', 'rocket' ); ?></h4>
+			<?php
+			$this->render_action_button( 'link', 'preload', [
+				'label'      => __( 'Preload cache', 'rocket' ),
+				'icon'       => '',
+				'attributes' => [
+					'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-refresh',
+				],
+				] );
+			?>
+
+			<h4><?php esc_html_e( 'Purge OPCache content', 'rocket' ); ?></h4>
+			<?php
+			$this->render_action_button( 'link', 'rocket_purge_opcache', [
+				'label'      => __( 'Purge OPCache', 'rocket' ),
+				'icon'       => '',
+				'attributes' => [
+					'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-trash',
+				],
+				] );
+			?>
+		</div>
 	</div>
-
-	<div class="wpr-page-flex">
-		<div class="wpr-optionHeader">
-			<h3 class="wpr-title2"><?php esc_html_e( 'Quick Actions', 'rocket' ); ?></h3>
+	<div class="wpr-Page-row">
+		<div class="wpr-Page-col">
+			<div class="wpr-optionHeader">
+				<h3 class="wpr-title2"><?php esc_html_e( 'Frequently Asked Questions', 'rocket' ); ?></h3>
+			</div>
+			<ul>
+			<?php foreach ( $data['faq'] as $faq_item ) : ?>
+				<li><a href="<?php echo esc_attr( $faq_item['url'] ); ?>" data-beacon-article="<?php echo esc_attr( $faq_item['id'] ); ?>"><?php echo esc_html( $faq_item['title'] ); ?></a></li>
+			<?php endforeach; ?>
+			</ul>
+			<h4><?php esc_html_e( 'Still can not find a solution?', 'rocket' ); ?></h4>
+			<p><?php esc_html_e( 'Submit a ticket and get help from our friendly and knowledgeable Rocketeers.', 'rocket' ); ?></p>
+			<?php
+			$this->render_action_button( 'button', 'ask_support', [
+				'label'      => __( 'Ask support', 'rocket' ),
+				'icon'       => '',
+				'attributes' => [
+					'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--blue wpr-icon-help',
+				],
+			] ); ?>
 		</div>
 
-		<h4><?php esc_html_e( 'Remove all cached files', 'rocket' ); ?></h4>
-		<?php
-		$this->render_action_button( 'link', 'purge_cache', [
-			'label'      => __( 'Clear cache', 'rocket' ),
-			'icon'       => '',
-			'parameters' => [
-				'type' => 'all',
-			],
-			'attributes' => [
-				'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-trash',
-			],
-			] );
-		?>
-
-		<h4><?php esc_html_e( 'Start cache preloading', 'rocket' ); ?></h4>
-		<?php
-		$this->render_action_button( 'link', 'preload', [
-			'label'      => __( 'Preload cache', 'rocket' ),
-			'icon'       => '',
-			'attributes' => [
-				'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-refresh',
-			],
-			] );
-		?>
-
-		<h4><?php esc_html_e( 'Purge OPCache content', 'rocket' ); ?></h4>
-		<?php
-		$this->render_action_button( 'link', 'rocket_purge_opcache', [
-			'label'      => __( 'Purge OPCache', 'rocket' ),
-			'icon'       => '',
-			'attributes' => [
-				'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-icon-trash',
-			],
-			] );
-		?>
-	</div>
-
-	<div class="wpr-page-flex">
-		<div class="wpr-optionHeader">
-			<h3 class="wpr-title2"><?php esc_html_e( 'Frequently Asked Questions', 'rocket' ); ?></h3>
+		<div class="wpr-Page-col wpr-Page-col--fixed">
+			<?php $this->render_documentation_block(); ?>
 		</div>
-		<ul>
-		<?php foreach ( $data['faq'] as $faq_item ) : ?>
-			<li><a href="<?php echo esc_attr( $faq_item['url'] ); ?>" data-beacon-article="<?php echo esc_attr( $faq_item['id'] ); ?>"><?php echo esc_html( $faq_item['title'] ); ?></a></li>
-		<?php endforeach; ?>
-		</ul>
-		<h4><?php esc_html_e( 'Still can not find a solution?', 'rocket' ); ?></h4>
-		<p><?php esc_html_e( 'Submit a ticket and get help from our friendly and knowledgeable Rocketeers.', 'rocket' ); ?></p>
-		<?php
-		$this->render_action_button( 'button', 'ask_support', [
-			'label'      => __( 'Ask support', 'rocket' ),
-			'icon'       => '',
-			'attributes' => [
-				'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--blue wpr-icon-help',
-			],
-		] ); ?>
-	</div>
-
-	<div class="wpr-page-flex">
-		<?php $this->render_documentation_block(); ?>
 	</div>
 
 	<div id="rocket-analytics-info" class="screen-reader-text">
