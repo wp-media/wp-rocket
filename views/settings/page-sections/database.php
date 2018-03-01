1a680e675b2b1c8016bd41bb1d6326edafc404fb
Rémy Perona <remperona@gmail.com>
2018-03-01 17:36:00 -0500

    add page section description

--- views/settings/page-sections/database.php
+++ views/settings/page-sections/database.php
@@ -18,6 +18,8 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 <div id="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-Page">
 	<div class="wpr-sectionHeader">
 		<h2 class="wpr-title1 wpr-icon-database"><?php echo esc_html( $data['title'] ); ?></h2>
+		<p><?php _e( 'Backup your database before you run a cleanup!', 'rocket' ); ?></p>
+		<p><?php _e( 'Once a database optimization has been performed, there is not way to undo it.', 'rocket' ); ?></p>
 	</div>
 	<?php $this->render_settings_sections( $data['id'] ); ?>
 </div>
