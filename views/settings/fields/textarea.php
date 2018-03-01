2121a431b1a61a54cd325483149f51552e6f7bf1
Rémy Perona <remperona@gmail.com>
2018-03-01 17:36:28 -0500

    add helper data

--- views/settings/fields/textarea.php
+++ views/settings/fields/textarea.php
@@ -11,6 +11,7 @@
  *     @type string $label       Field label.
  *     @type string $value       Field value.
  *     @type string $description Field description.
+ *     @type string $helper      Field helper text.
  * }
  */
 
@@ -19,12 +20,17 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 <div class="wpr-field wpr-field--textarea <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
 	<?php echo $data['label']; ?>
-	<div class="wpr-textarea">
-		<textarea id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]"><?php echo esc_textarea( $data['value'] ); ?></textarea>
-	</div>
 	<?php if ( ! empty( $data['description'] ) ) { ?>
 		<div class="wpr-field-description">
 			<?php echo $data['description']; ?>
+		</div>
+	<?php } ?>
+	<div class="wpr-textarea">
+		<textarea id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]"><?php echo esc_textarea( $data['value'] ); ?></textarea>
+	</div>
+	<?php if ( ! empty( $data['helper'] ) ) { ?>
+		<div class="wpr-field-description">
+			<?php echo $data['helper']; ?>
 		</div>
 	<?php } ?>
 </div>
