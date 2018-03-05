2866699f13e0bc78d90ad82c015a9a908b80cee6
Thomas Geisen <tom.geisen@gmail.com>
2018-03-05 16:58:32 +0100

    add JS for disabled fields

--- src/js/global/main.js
+++ src/js/global/main.js
@@ -1,3 +1,56 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 
+
+    /*
+    ** Warning fields
+    */
+    //
+    // var $warningsTab = document.querySelectorAll('.wpr-field--parent');
+    //
+    // for (var i = 0; i < $warningsTab.length; i++) {
+    //
+    //
+    //     // Input checkbox
+    //     var $checkbox = $prev.querySelector('input[type=checkbox]');
+    //     $checkbox.onchange = function(){
+    //         //$warning.classList.add('wpr-isOpen');
+    //         return false;
+    //     };
+    // }
+
+
+
+
+    /*
+    ** Disabled fields
+    */
+
+    var $disabledFields = document.querySelectorAll('.wpr-isDisabled');
+    var $parentInput = [];
+
+    for (var i = 0; i < $disabledFields.length; i++) {
+        // Get prev element
+        var $prev = $disabledFields[i].parentNode.previousSibling;
+        while($prev && $prev.nodeType != 1) {
+            $prev = $prev.previousSibling;
+        }
+        $parentInput[i] = $prev.querySelector('input[type=checkbox]');
+
+        wprShowDisabled(i);
+    }
+
+    function wprShowDisabled(i){
+        $parentInput[i].onchange = function(){
+             if(this.checked){
+                 $disabledFields[i].classList.remove('wpr-isDisabled');
+                 $disabledFields[i].querySelector('input[type=checkbox]').disabled = false;
+             }
+             else{
+                 $disabledFields[i].classList.add('wpr-isDisabled');
+                 $disabledFields[i].querySelector('input[type=checkbox]').disabled = true;
+                 $disabledFields[i].querySelector('input[type=checkbox]').checked = false;
+             }
+         };
+    }
+
 });
--- views/settings/fields/checkbox.php
+++ views/settings/fields/checkbox.php
@@ -26,19 +26,23 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<div class="wpr-field wpr-field--checkbox <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
-	<div class="wpr-checkbox">
-		<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>
-		<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
-		<label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
-	</div>
+<?php if ( ! empty( $data['warning'] ) ) { ?>
+<div class="wpr-warningContainer">
+<?php } ?>
 
-	<?php if ( ! empty( $data['description'] ) ) { ?>
-	<div class="wpr-field-description">
-		<?php echo $data['description']; ?>
+	<div class="wpr-field wpr-field--checkbox <?php echo $data['class'] ?> <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?> <?php echo isset( $data['warning']['title'] ) ? 'wpr-field--parent' : ''; ?>">
+		<div class="wpr-checkbox">
+			<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>
+			<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
+			<label for="<?php echo esc_attr( $data['id'] ); ?>" class=""><?php echo $data['label']; ?></label>
+		</div>
+
+		<?php if ( ! empty( $data['description'] ) ) { ?>
+		<div class="wpr-field-description">
+			<?php echo $data['description']; ?>
+		</div>
+		<?php } ?>
 	</div>
-	<?php } ?>
-</div>
 
 <?php if ( ! empty( $data['warning'] ) ) { ?>
 	<div class="wpr-fieldWarning">
@@ -52,4 +56,5 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 		<?php } ?>
 		<button class="wpr-button wpr-button--small wpr-button--icon wpr-icon-check"><?php echo esc_html( $data['warning']['button_label'] ); ?></button>
 	</div>
+</div>
 <?php } ?>
