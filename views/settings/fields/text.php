f92386be0ddbe5dec2d5b055c163d9dc3642eca1
Thomas Geisen <tom.geisen@gmail.com>
2018-03-06 17:17:14 +0100

    add styles for description input text

--- views/settings/fields/text.php
+++ views/settings/fields/text.php
@@ -21,13 +21,16 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 <div class="wpr-field wpr-field--text <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
 	<div class="wpr-text">
-        <label for="<?php echo esc_attr( $data['id'] ); ?>"><?php echo $data['label']; ?></label>
+		<?php if ( ! empty( $data['description'] ) ) { ?>
+		<div class="wpr-flex">
+			<label for="<?php echo esc_attr( $data['id'] ); ?>"><?php echo $data['label']; ?></label>
+			<div class="wpr-field-description">
+				<?php echo $data['description']; ?>
+			</div>
+		</div>
+		<?php }else{?>
+			<label for="<?php echo esc_attr( $data['id'] ); ?>"><?php echo $data['label']; ?></label>
+		<?} ?>
 		<input type="text" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="<?php echo esc_attr( $data['value'] ); ?>"<?php echo isset( $data['input_attr'] ) ? $data['input_attr'] : ''; ?>>
 	</div>
-
-	<?php if ( ! empty( $data['description'] ) ) { ?>
-	<div class="wpr-field-description">
-		<?php echo $data['description']; ?>
-	</div>
-	<?php } ?>
 </div>
