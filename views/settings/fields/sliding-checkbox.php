6b76850cb0e16456c53e1efbc5032b041b6ce773
Rémy Perona <remperona@gmail.com>
2018-03-06 17:44:58 -0500

    use container_class

--- views/settings/fields/sliding-checkbox.php
+++ views/settings/fields/sliding-checkbox.php
@@ -17,18 +17,18 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<div class="wpr-field wpr-field--radio <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
+<div class="wpr-field wpr-field--radio <?php echo $data['container_class']; ?>">
 	<div class="wpr-radio">
-        <input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>>
-        <label for="<?php echo esc_attr( $data['id'] ); ?>" class="">
+		<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>>
+		<label for="<?php echo esc_attr( $data['id'] ); ?>" class="">
 			<span class="wpr-radio-ui"></span>
 			<?php echo esc_html( $data['label'] ); ?>
 		</label>
-    </div>
+	</div>
 
-    <?php if ( ! empty( $data['description'] ) ) : ?>
-        <div class="wpr-field-description">
-            <?php echo $data['description']; ?>
-        </div>
-    <?php endif; ?>
+	<?php if ( ! empty( $data['description'] ) ) : ?>
+		<div class="wpr-field-description">
+			<?php echo $data['description']; ?>
+		</div>
+	<?php endif; ?>
 </div>
