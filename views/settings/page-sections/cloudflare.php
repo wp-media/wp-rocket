e26e1e3a9d337325d53635c8c89aec35ba96f86d
Rémy Perona <remperona@gmail.com>
2018-03-06 18:53:28 -0500

    update description content for cloudflare purge

--- views/settings/page-sections/cloudflare.php
+++ views/settings/page-sections/cloudflare.php
@@ -25,7 +25,12 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 	<div class="wpr-optionHeader">
 		<h3 class="wpr-title2"><?php esc_html_e( 'Cloudflare Cache', 'rocket' ); ?></h3>
 	</div><br>
-	<p class="wpr-field-description"><?php esc_html_e( 'Purges cached resources for your website. Learn more', 'rocket' ); ?></p>
+	<p class="wpr-field-description">
+	<?php
+	// translators: %s is the URL to the CloudFlare documentation.
+	printf( __( 'Purges cached resources for your website. <a href="%s" target="_blank">Learn more</a>', 'rocket' ), 'https://support.cloudflare.com/hc/en-us/articles/200169246' );
+	?>
+	</p>
 	<?php
 	$this->render_action_button( 'link', 'purge_cloudflare', [
 		'label'      => __( 'Clear all Cloudflare cache files', 'rocket' ),
