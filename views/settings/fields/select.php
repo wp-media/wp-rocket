08d5166f9a61b9b8d6ee005cbebced81ba5acd0f
Rémy Perona <remperona@gmail.com>
2018-03-06 17:45:14 -0500

    use container_class

--- views/settings/fields/select.php
+++ views/settings/fields/select.php
@@ -7,10 +7,11 @@
  * @param array $data {
  *     Checkbox Field arguments.
  *
- *     @type string $id          Field identifier.
- *     @type string $label       Field label.
- *     @type string $value       Field value.
- *     @type string $description Field description.
+ *     @type string $id              Field identifier.
+ *     @type string $label           Field label.
+ *     @type string $container_class Field container class.
+ *     @type string $value           Field value.
+ *     @type string $description     Field description.
  *     @type array  $choices {
  *          Option choices.
  *
@@ -23,12 +24,12 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<div class="wpr-field wpr-field--select <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
+<div class="wpr-field wpr-field--select <?php echo $data['container_class']; ?>">
 	<div class="wpr-select">
 		<select id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]">
-		<?php foreach ( $data['choices'] as $value => $label ) { ?>
+		<?php foreach ( $data['choices'] as $value => $label ) : ?>
 			<option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, $data['value'] ); ?>><?php echo esc_html( $label ); ?></option>
-		<?php } ?>
+		<?php endforeach; ?>
 		</select>
 	</div>
 </div>
