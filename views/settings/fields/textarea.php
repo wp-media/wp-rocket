e18d2b88f0fa51837f1fb8cc6ab97219a845e875
Rémy Perona <remperona@gmail.com>
2018-03-06 17:36:49 -0500

    use container_class index

--- views/settings/fields/textarea.php
+++ views/settings/fields/textarea.php
@@ -18,21 +18,21 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
-<div class="wpr-field wpr-field--textarea <?php echo isset( $data['parent'] ) ? 'wpr-field--children' : ''; ?>">
-	<?php if ( ! empty( $data['label'] ) ) { ?>
+<div class="wpr-field wpr-field--textarea <?php echo $data['container_class']; ?>">
+	<?php if ( ! empty( $data['label'] ) ) : ?>
 		<label for="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-title3"><?php echo $data['label']; ?></label>
-	<?php } ?>
-	<?php if ( ! empty( $data['description'] ) ) { ?>
+	<?php endif; ?>
+	<?php if ( ! empty( $data['description'] ) ) : ?>
 		<div class="wpr-field-description-label">
 			<?php echo $data['description']; ?>
 		</div>
-	<?php } ?>
+	<?php endif; ?>
 	<div class="wpr-textarea">
 		<textarea id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]"><?php echo esc_textarea( $data['value'] ); ?></textarea>
 	</div>
-	<?php if ( ! empty( $data['helper'] ) ) { ?>
+	<?php if ( ! empty( $data['helper'] ) ) : ?>
 		<div class="wpr-field-description">
 			<?php echo $data['helper']; ?>
 		</div>
-	<?php } ?>
+	<?php endif; ?>
 </div>
