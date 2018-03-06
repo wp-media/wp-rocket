17ce5121f05b8862acf457ae2df37c03c759a2cc
Thomas Geisen <tom.geisen@gmail.com>
2018-03-06 12:48:04 +0100

    add greensock and jquery librairies

--- src/js/global/app.js
+++ src/js/global/app.js
@@ -1,3 +1,13 @@
+// Add jquery
+import '../lib/jquery.min.js';
+
+// Add greensock lib for animations
+import '../lib/greensock/TweenLite.min.js';
+import '../lib/greensock/TimelineLite.min.js';
+import '../lib/greensock/easing/EasePack.min.js';
+import '../lib/greensock/plugins/CSSPlugin.min.js';
+
+// Add scripts
 import '../global/main.js';
 import '../global/pageManager.js';
 import '../global/fields.js';
