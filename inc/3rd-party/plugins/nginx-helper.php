471f730e2ab1f61d69ad6e82d38b47a5ba7ec382
Nikhil <email@nikhilchavan.com>
2018-03-07 22:05:19 +0530

    correctly purge the wp-rocket cache when nginx-helper's cache is purged

--- inc/3rd-party/plugins/nginx-helper.php
+++ inc/3rd-party/plugins/nginx-helper.php
@@ -2,31 +2,30 @@
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 if ( defined( 'rtCamp\WP\Nginx\RT_WP_NGINX_HELPER_PATH' ) ) :
+	add_action( 'admin_init', 'rocket_clear_cache_after_nginx_helper_purge' );
 	/**
-	 * Run WP Rocket preload bot after the cache is purged from Nginx Helper.
+	 * Clear WP Rocket cache after the cache is purged from Nginx Helper.
 	 *
 	 * @since 2.x.x
 	 *
 	 * @return void
 	 */
-	function rocket_run_rocket_bot_after_wpengine() {
-		if ( isset( $_GET['nginx_helper_action'] ) && check_admin_referer( 'nginx_helper-purge_all' ) ) {
+	function rocket_clear_cache_after_nginx_helper_purge() {
+		if ( isset( $_GET['nginx_helper_action'] ) && 'done' == $_GET['nginx_helper_action'] && current_user_can( 'manage_options' ) && check_admin_referer( 'nginx_helper-purge_all' ) ) {
+			// Clear all caching files.
+			rocket_clean_domain();
+
 			// Preload cache.
 			run_rocket_preload_cache( 'cache-preload' );
 		}
 	}
-	add_action( 'rt_nginx_helper_purge_all', 'rocket_run_rocket_bot_after_wpengine' );
-
 	/**
 	 * Clean the cache using Nginx Helper after WP Rocket's cache is purged.
 	 *
 	 * @since 2.x.x
 	 */
 	function rocket_clean_wpengine() {
-		global $rt_wp_nginx_purger;
-		if ( method_exists( $rt_wp_nginx_purger, 'true_purge_all' ) ) {
-			do_action( 'rt_nginx_helper_purge_all' );
-		}
+		do_action( 'rt_nginx_helper_purge_all' );
 	}
 	add_action( 'after_rocket_clean_domain', 'rocket_clean_wpengine' );
 endif;
