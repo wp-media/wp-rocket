c5b01a0c746fd139777567624f839e643b394a8a
Rémy Perona <remperona@gmail.com>
2018-03-07 15:20:36 -0500

    add ajax for safe mode button

--- inc/admin/ui/js/all.js
+++ inc/admin/ui/js/all.js
@@ -4,4 +4,23 @@ jQuery( document ).ready( function( $ ){
 		var url = $( this ).attr( 'href' ).replace( 'admin-post', 'admin-ajax' );
 		$.get( url ).done( $( this ).closest( '.notice' ).hide( 'slow' ) );
 	});
+
+	$('#wpr-action-safe_mode').on('click', function(e) {
+		var button = $(this);
+		e.preventDefault();
+
+		$.post(
+			ajaxurl,
+			{
+				action: 'rocket_safe_mode',
+				_ajax_nonce: ajax_data.nonce,
+			},
+			function(response) {
+				if ( true === response.success ) {
+					button.hide();
+					$('.show-if-safe-mode').show();
+				}
+			}
+		);
+	});
 } );
