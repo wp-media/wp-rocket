d3ebb1aa013599cfa82a03ad57d0a2d154f2328b
Jeremy Pry <jeremy.pry@gmail.com>
2018-03-12 18:30:58 -0400

    Include missing `@throws` tags

--- classes/ActionScheduler_WPCLI_QueueRunner.php
+++ classes/ActionScheduler_WPCLI_QueueRunner.php
@@ -42,6 +42,7 @@ class ActionScheduler_WPCLI_QueueRunner extends ActionScheduler_Abstract_QueueRu
 	 * @param bool $force      Whether to force running even with too many concurrent processes.
 	 *
 	 * @return int The number of actions that will be run.
+	 * @throws \WP_CLI\ExitException When there are too many concurrent batches.
 	 */
 	public function setup( $batch_size, $force = false ) {
 		$this->run_cleanup();
@@ -104,6 +105,7 @@ class ActionScheduler_WPCLI_QueueRunner extends ActionScheduler_Abstract_QueueRu
 	 *
 	 * @author Jeremy Pry
 	 * @return int The number of actions processed.
+	 * @throws \WP_CLI\ExitException When the claim is lost.
 	 */
 	public function run() {
 		do_action( 'action_scheduler_before_process_queue' );
@@ -162,6 +164,7 @@ class ActionScheduler_WPCLI_QueueRunner extends ActionScheduler_Abstract_QueueRu
 	 *
 	 * @param int       $action_id
 	 * @param Exception $exception
+	 * @throws \WP_CLI\ExitException With failure message.
 	 */
 	public function action_failed( $action_id, $exception ) {
 		WP_CLI::error(
