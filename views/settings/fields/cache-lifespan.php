37d8a0b02394f368739eaf3b3a7d76b429bdb00b
Rémy Perona <remperona@gmail.com>
2018-03-12 22:46:35 -0400

    add min value for number

--- views/settings/fields/cache-lifespan.php
+++ views/settings/fields/cache-lifespan.php
@@ -24,7 +24,7 @@ $purge_unit     = get_rocket_option( 'purge_cron_unit', 'HOUR_IN_SECONDS' );
 	<div class="wpr-field wpr-field--text wpr-field--number">
 		<div class="wpr-text wpr-text--number">
 			<label for="purge_cron_interval" class="screen-reader-text"><?php _e( 'Clear cache after', 'rocket' ); ?></label>
-			<input type="number" id="purge_cron_interval" name="wp_rocket_settings[purge_cron_interval]" value="<?php echo esc_attr( $purge_interval ); ?>">
+			<input type="number" min="0" id="purge_cron_interval" name="wp_rocket_settings[purge_cron_interval]" value="<?php echo esc_attr( $purge_interval ); ?>">
 		</div>
 	</div>
 	<div class="wpr-field wpr-field--select">
