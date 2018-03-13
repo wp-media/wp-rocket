1ddc110ece69f032fd346342ad788b0effe3d4c9
Thomas Geisen <tom.geisen@gmail.com>
2018-03-14 00:29:15 +0100

    add missing bold and italic to fonts

--- views/settings/fields/textarea.php
+++ views/settings/fields/textarea.php
@@ -20,10 +20,10 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 <div class="wpr-field wpr-field--textarea <?php echo $data['container_class']; ?>">
 	<?php if ( ! empty( $data['label'] ) ) : ?>
-		<label for="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-title3"><?php echo $data['label']; ?></label>
+		<label for="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-field-description-label"><?php echo $data['label']; ?></label>
 	<?php endif; ?>
 	<?php if ( ! empty( $data['description'] ) ) : ?>
-		<div class="wpr-field-description-label">
+		<div class="wpr-field-description">
 			<?php echo $data['description']; ?>
 		</div>
 	<?php endif; ?>
@@ -31,7 +31,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 		<textarea id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]"><?php echo esc_textarea( $data['value'] ); ?></textarea>
 	</div>
 	<?php if ( ! empty( $data['helper'] ) ) : ?>
-		<div class="wpr-field-description">
+		<div class="wpr-field-description wpr-field-description-helper">
 			<?php echo $data['helper']; ?>
 		</div>
 	<?php endif; ?>
--- views/settings/page-sections/cloudflare.php
+++ views/settings/page-sections/cloudflare.php
@@ -24,22 +24,25 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 	<?php $this->render_settings_sections( $data['id'] ); ?>
 	<div class="wpr-optionHeader">
 		<h3 class="wpr-title2"><?php esc_html_e( 'Cloudflare Cache', 'rocket' ); ?></h3>
-	</div><br>
-	<p class="wpr-field-description">
-	<?php
-	printf(
-		// translators: %s is a "Learn more" link.
-		__( 'Purges cached resources for your website. %s', 'rocket' ),
-		'<a href="' . esc_url( __( 'https://support.cloudflare.com/hc/en-us/articles/200169246', 'rocket' ) ) . '" target="_blank">' . __( 'Learn more', 'rocket' ) . '</a>'
-	);
-	?>
-	</p>
-	<?php
-	$this->render_action_button( 'link', 'rocket_purge_cloudflare', [
-		'label'      => __( 'Clear all Cloudflare cache files', 'rocket' ),
-		'attributes' => [
-			'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-trash',
-		],
-	]);
-	?>
+	</div>
+	<div class="wpr-fieldsContainer">
+		<div class="wpr-fieldsContainer-description">
+			<?php echo $data['description']; ?>
+			<?php
+			printf(
+				// translators: %s is a "Learn more" link.
+				__( 'Purges cached resources for your website. %s', 'rocket' ),
+				'<a href="' . esc_url( __( 'https://support.cloudflare.com/hc/en-us/articles/200169246', 'rocket' ) ) . '" target="_blank">' . __( 'Learn more', 'rocket' ) . '</a>'
+			);
+			?>
+		</div><br>
+		<?php
+		$this->render_action_button( 'link', 'rocket_purge_cloudflare', [
+			'label'      => __( 'Clear all Cloudflare cache files', 'rocket' ),
+			'attributes' => [
+				'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-trash',
+			],
+		]);
+		?>
+	</div>
 </div>
