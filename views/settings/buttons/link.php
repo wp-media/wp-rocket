85c3d3ed3527ca863d58c17c397a1db590a84bd9
Rémy Perona <remperona@gmail.com>
2018-03-12 22:42:32 -0400

    replace esc_attr by esc_url

--- views/settings/buttons/link.php
+++ views/settings/buttons/link.php
@@ -15,4 +15,4 @@
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
-<a href="<?php echo esc_attr( $data['url'] ); ?>" <?php echo $data['attributes']; ?>><?php echo esc_html( $data['label'] ); ?></a>
+<a href="<?php echo esc_url( $data['url'] ); ?>" <?php echo $data['attributes']; ?>><?php echo esc_html( $data['label'] ); ?></a>
