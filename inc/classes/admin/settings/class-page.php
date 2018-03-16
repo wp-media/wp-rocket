aec482c9318f9020f19f14cf9a4daff58189bed3
Rémy Perona <remperona@gmail.com>
2018-03-16 15:39:11 -0400

    update wording for #834

--- inc/classes/admin/settings/class-page.php
+++ inc/classes/admin/settings/class-page.php
@@ -863,7 +863,7 @@ class Page {
 				],
 				'minify_concatenate_js'  => [
 					'type'              => 'checkbox',
-					'label'             => __( 'Combine JavaScript files <em>(Enable Minify JavaScript to select)</em>', 'rocket' ),
+					'label'             => __( 'Combine JavaScript files <em>(Enable Minify JavaScript files to select)</em>', 'rocket' ),
 					// translators: %1$s = opening <a> tag, %2$s = closing </a> tag.
 					'description'       => sprintf( __( 'Combine Javascript files combines your site\'s JS info fewer files, reducing HTTP requests. Not recommended if your site uses HTTP/2. %1$sMore info%2$s', 'rocket' ), '<a href="' . esc_url( $combine_beacon['url'] ) . '" data-beacon-article="' . esc_attr( $combine_beacon['id'] ) . '">', '</a>' ),
 					'container_class'   => [
