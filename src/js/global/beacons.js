4b35620eead6aa025bb6a78465e0f7bd46091562
Rémy Perona <remperona@gmail.com>
2018-03-26 08:40:02 -0400

    update JS to execute it only if beacon is ready

--- src/js/global/beacons.js
+++ src/js/global/beacons.js
@@ -2,43 +2,44 @@ var $ = jQuery;
 $(document).ready(function(){
 
 
-    /***
-    * Show beacons on button "help" click
-    ***/
-
-    var $help = $('.wpr-infoAction--help');
-    $help.on('click', function(e){
-        var ids = $(this).data('beacon-id');
-        wprCallBeacon(ids);
-        return false;
-    });
-
-    function wprCallBeacon(aID){
-        aID = aID.split(',');
-        if ( aID.length === 0 ) {
-            return;
-        }
-        HS.beacon.ready( function(){
-            var refThis = this;
-
-            if ( aID.length > 1 ) {
-                this.suggest(aID);
-
-                setTimeout(function() {
-                    refThis.open();
-                }, 200);
-            } else {
-                this.show(aID);
-            }
+    /**
+     * Show beacons on button "help" click
+     */
+    HS.beacon.ready( function(){
+        var $help = $('.wpr-infoAction--help');
+        $help.on('click', function(e){
+            var ids = $(this).data('beacon-id');
+            wprCallBeacon(ids);
+            return false;
         });
-    }
 
-    // Ask support
-    var $askSupport = $('.wpr-js-askSupport');
-    $askSupport.on('click', function(){
-        HS.beacon.open();
-        return false;
+        function wprCallBeacon(aID){
+            aID = aID.split(',');
+            if ( aID.length === 0 ) {
+                return;
+            }
+            
+                var refThis = HS.beacon;
+
+                if ( aID.length > 1 ) {
+                    refThis.suggest(aID);
+
+                    setTimeout(function() {
+                        refThis.open();
+                    }, 200);
+                } else {
+                    refThis.show(aID);
+                }
+            
+        }
+
+        // Ask support
+        var $askSupport = $('.wpr-js-askSupport');
+        $askSupport.on('click', function(){
+            HS.beacon.open();
+            return false;
+        });
+
     });
-
 
 });
