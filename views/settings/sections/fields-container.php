3de825929caa9c7f5554f7c322d36e5909a8e772
Rémy Perona <remperona@gmail.com>
2018-04-06 15:46:10 -0400

    add custom class to fields container

--- views/settings/sections/fields-container.php
+++ views/settings/sections/fields-container.php
@@ -10,6 +10,7 @@
  *     @type string $id          Section identifier.
  *     @type string $title       Section title.
  *     @type string $description Section description.
+ *     @type string $class       Section classes.
  *     @type string $helper      Section helper text.
  *     @type string $help        Data to pass to beacon.
  *     @type string $page        Page section identifier.
@@ -20,14 +21,14 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
 
 
-<div class="wpr-optionHeader">
+<div class="wpr-optionHeader <?php echo $data['class']; ?>">
 	<h3 class="wpr-title2"><?php echo esc_html( $data['title'] ); ?></h3>
 	<?php if ( ! empty( $data['help'] ) ) : ?>
 	<a href="<?php echo esc_url( $data['help']['url'] ); ?>" data-beacon-id="<?php echo esc_attr( $data['help']['id'] ); ?>" class="wpr-infoAction wpr-infoAction--help wpr-icon-help" target="_blank"><?php esc_html_e( 'Need Help?', 'rocket' ); ?></a>
 	<?php endif; ?>
 </div>
 
-<div class="wpr-fieldsContainer">
+<div class="wpr-fieldsContainer <?php echo $data['class']; ?>">
 	<div class="wpr-fieldsContainer-description">
 		<?php echo $data['description']; ?>
 	</div>
