b7f0c58e63d2a0f6e46b421c2aae4b51e8727dd8
Arun Basil Lal <arunbasillal@gmail.com>
2018-05-24 00:57:23 +0530

    Fix #892 - Polylang language detection compatibility to allow homepage caching

--- inc/3rd-party/plugins/i18n/polylang.php
+++ inc/3rd-party/plugins/i18n/polylang.php
@@ -15,4 +15,103 @@ if ( defined( 'POLYLANG_VERSION' ) && POLYLANG_VERSION ) :
 		}
 	}
 	add_action( 'after_rocket_clean_domain', 'rocket_force_clean_domain_on_polylang' );
+	
+	// Add Polylang's language cookie as a mandatory cookie if Polylang module 'Detect browser language' is enabled.
+	if ( function_exists( 'PLL' ) && PLL()->options['browser'] ) {
+		add_filter( 'rocket_cache_mandatory_cookies', 'rocket_add_polylang_mandatory_cookie' );
+	}
 endif;
+
+/**
+ * Add Polylang's language cookie to the mandatory cookies of WP Rocket.
+ * 
+ * Polylang saves the users preferred language in this cookie by detecting browser language or by user choice
+ * Adding this as a mandatory cookie prevents WP Rocket from serving the cache when the cookie is not set. 
+ * 
+ * @param $cookies (array) Array with mandatory cookies 
+ * @return (array) Array of mandatory cookies with the Polylang cookie appended
+ * 
+ * @author Arun Basil Lal
+ * @since 3.0.5
+ */
+function rocket_add_polylang_mandatory_cookie( $cookies ) {
+	
+	$cookies[] = defined( 'PLL_COOKIE' ) ? PLL_COOKIE : 'pll_language';
+	
+	return $cookies;
+}
+
+/**
+ * Add mandatory cookie to WP Rocket config on Polylang activation. 
+ * 
+ * Add mandatory cookie only if the Polylang module 'Detect browser language' is active. 
+ * Also purge the homepage cache. 
+ *
+ * @author Arun Basil Lal
+ * @since 3.0.5
+ */
+function rocket_activate_polylang() {
+	
+	// Read Polylang settings from db.
+	$polylang_settings = get_option( 'polylang' ); 
+	
+	if ( isset( $polylang_settings['browser'] ) && ( 1 === (int) $polylang_settings['browser'] ) ) {
+		
+		// Add Polylang's language cookie as a mandatory cookie.
+		add_filter( 'rocket_cache_mandatory_cookies', 'rocket_add_polylang_mandatory_cookie' );
+		
+		// Regenerate the config file.
+		rocket_generate_config_file();
+		
+		// Purge homepage cache
+		rocket_clean_home();
+	}
+}
+add_action( 'activate_polylang/polylang.php', 'rocket_activate_polylang', 11 );
+
+/**
+ * Remove mandatory cookie when Polylang is deactivated.
+ *
+ * @author Arun Basil Lal
+ * @since 3.0.5
+ */
+function rocket_deactivate_polylang() {
+	
+	// Remove Polylang's language cookie as a mandatory cookie.
+	remove_filter( 'rocket_cache_mandatory_cookies', 'rocket_add_polylang_mandatory_cookie' ); 
+
+	// Regenerate the config file.
+	rocket_generate_config_file();
+}
+add_action( 'deactivate_polylang/polylang.php', 'rocket_deactivate_polylang', 11 );
+
+/**
+ * Update mandatory cookie in WP Rocket config file when Detect browser language module is enabled / disabled. 
+ * 
+ * @param $value (array) Array containing Polylang settings before its written to db.
+ * @return (array) Array with Polylang settings. Settings are unchanged, we just use the filter as an action. 
+ * 
+ * @author Arun Basil Lal
+ * @since 3.0.5
+ */
+function rocket_detect_browser_language_status_change( $value ) {
+	
+	if ( function_exists( 'PLL' ) && PLL()->options['browser'] ) {
+		
+		// Add Polylang's language cookie as a mandatory cookie.
+		add_filter( 'rocket_cache_mandatory_cookies', 'rocket_add_polylang_mandatory_cookie' );
+		
+		// Regenerate the config file.
+		rocket_generate_config_file();
+	} else {
+		
+		// Remove Polylang's language cookie as a mandatory cookie.
+		remove_filter( 'rocket_cache_mandatory_cookies', 'rocket_add_polylang_mandatory_cookie' );
+		
+		// Regenerate the config file.
+		rocket_generate_config_file();
+	}
+	
+	return $value;
+}
+add_filter( 'pre_update_option_polylang', 'rocket_detect_browser_language_status_change' );
