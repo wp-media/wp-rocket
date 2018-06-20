0558a20b8066d8847d6c6f4b4e1799d766875b40
Rémy Perona <remperona@gmail.com>
2018-06-20 18:31:45 -0400

    add a way to declare multiple callbacks for the same hooks

--- inc/classes/event-management/class-event-manager.php
+++ inc/classes/event-management/class-event-manager.php
@@ -147,6 +147,10 @@ class Event_Manager {
 	private function add_subscriber_callback( Subscriber_Interface $subscriber, $hook_name, $parameters ) {
 		if ( is_string( $parameters ) ) {
 			$this->add_callback( $hook_name, [ $subscriber, $parameters ] );
+		} elseif ( is_array( $parameters ) && count( $parameters ) !== count( $parameters, COUNT_RECURSIVE ) ) {
+			foreach ( $parameters as $parameter ) {
+				$this->add_subscriber_callback( $subscriber, $hook_name, $parameter );
+			}
 		} elseif ( is_array( $parameters ) && isset( $parameters[0] ) ) {
 			$this->add_callback( $hook_name, [ $subscriber, $parameters[0] ], isset( $parameters[1] ) ? $parameters[1] : 10, isset( $parameters[2] ) ? $parameters[2] : 1 );
 		}
