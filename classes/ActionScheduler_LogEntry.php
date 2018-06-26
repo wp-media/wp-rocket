c635e0e8a29a5a77727c215dbbe3972494e06d84
Brent Shepherd <brent@prospress.com>
2018-06-26 16:26:36 +1000

    Remove type declaration for $date
    To avoid fatal errors with Follow-up Emails when running Action Scheduler
    2.0+ due to a comedy of errors.
    
    Fixes #169.

--- classes/ActionScheduler_LogEntry.php
+++ classes/ActionScheduler_LogEntry.php
@@ -28,7 +28,20 @@ class ActionScheduler_LogEntry {
 	 * @param Datetime $date    Datetime object with the time when this log entry was created. If this parameter is
 	 *                          not provided a new Datetime object (with current time) will be created.
 	 */
-	public function __construct( $action_id, $message, Datetime $date = null ) {
+	public function __construct( $action_id, $message, $date = null ) {
+
+		/*
+		 * ActionScheduler_wpCommentLogger::get_entry() previously passed a 3rd param of $comment->comment_type
+		 * to ActionScheduler_LogEntry::__construct(), goodness knows why, and the Follow-up Emails plugin
+		 * hard-codes loading its own version of ActionScheduler_wpCommentLogger without that out-dated method,
+		 * goodness knows why, so we need to guard against that here instead of using a DateTime type declaration
+		 * for the constructor's 3rd param of $date and causing a fatal error with older versions of FUE.
+		 */
+		if ( null !== $date && ! is_a( $date, 'DateTime' ) ) {
+			_deprecated_argument( __METHOD__, '2.0.0', 'The third parameter must be a valid DateTime instance, or null.' );
+			$date = null;
+		}
+
 		$this->action_id = $action_id;
 		$this->message   = $message;
 		$this->date      = $date ? $date : new Datetime;
