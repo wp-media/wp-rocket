74852625ed3d1a854f0f14989cfbd26603458d7e
Jeremy Pry <jeremy.pry@gmail.com>
2018-07-24 13:35:07 -0400

    Include last-ditch attempt to find correct timezone mapping

--- classes/ActionScheduler_TimezoneHelper.php
+++ classes/ActionScheduler_TimezoneHelper.php
@@ -25,6 +25,24 @@ abstract class ActionScheduler_TimezoneHelper {
 						$tzstring = timezone_name_from_abbr( '', $gmt_offset, 0 );
 					}
 
+					// Try mapping to the first abbreviation we can find.
+					if ( false === $tzstring ) {
+						$is_dst = date( 'I' );
+						foreach ( timezone_abbreviations_list() as $abbr ) {
+							foreach ( $abbr as $city ) {
+								if ( $city['dst'] == $is_dst && $city['offset'] == $gmt_offset ) {
+									// If there's no valid timezone ID, keep looking.
+									if ( null === $city['timezone_id'] ) {
+										continue;
+									}
+
+									$tzstring = $city['timezone_id'];
+									break 2;
+								}
+							}
+						}
+					}
+
 					// If we still have no valid string, then fall back to UTC.
 					if ( false === $tzstring ) {
 						$tzstring = 'UTC';
