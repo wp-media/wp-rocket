243c11230e8d5a2a3533e70c48e785fadcf7a8f7
Rémy Perona <remperona@gmail.com>
2018-08-20 18:10:58 -0400

    improve iframe regex

--- inc/front/lazyload.php
+++ inc/front/lazyload.php
@@ -407,8 +407,7 @@ function rocket_lazyload_iframes( $html ) {
 		return $html;
 	}
 
-	$matches = array();
-	preg_match_all( '/<iframe(?:\s.*)?\ssrc=["\'](.*)["\'](.*)><\/iframe>/iU', $html, $matches, PREG_SET_ORDER );
+	preg_match_all( '/<iframe.*\ssrc=["|\'](.+)["|\'].*(>\s*<\/iframe>)/iU', $html, $matches, PREG_SET_ORDER );
 
 	if ( empty( $matches ) ) {
 		return $html;
@@ -458,6 +457,8 @@ function rocket_lazyload_iframes( $html ) {
 		$iframe_noscript = '<noscript>' . $iframe[0] . '</noscript>';
 
 		$iframe_lazyload = str_replace( $iframe[1], $placeholder, $iframe[0] );
+		$iframe_lazyload = str_replace( $iframe[2], ' data-rocket-lazyload="fitvidscompatible" data-lazy-src="' . esc_url( $iframe[1] ) . '"' . $iframe[2], $iframe[0] );
+
 		/**
 		 * Filter the LazyLoad HTML output on iframes
 		 *
@@ -465,7 +466,7 @@ function rocket_lazyload_iframes( $html ) {
 		 *
 		 * @param array $html Output that will be printed.
 		 */
-		$iframe_lazyload  = apply_filters( 'rocket_lazyload_iframe_html', str_replace( $iframe[2], $iframe[2] . ' data-rocket-lazyload="fitvidscompatible" data-lazy-src="' . $iframe[1] . '"', $iframe[0] ) );
+		$iframe_lazyload  = apply_filters( 'rocket_lazyload_iframe_html', $iframe_lazyload );
 		$iframe_lazyload .= $iframe_noscript;
 
 		$html = str_replace( $iframe[0], $iframe_lazyload, $html );
