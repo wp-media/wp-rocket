583aac68d43660b0741ff39ca21d7f2e125cb2b8
Rémy Perona <remperona@gmail.com>
2018-08-21 18:34:07 -0400

    add combine inline JS option

--- inc/admin/upgrader.php
+++ inc/admin/upgrader.php
@@ -173,6 +173,7 @@ function rocket_first_install() {
 				'minify_js'                   => 0,
 				'minify_js_key'               => $minify_js_key,
 				'minify_concatenate_js'       => 0,
+				'combine_inline_js'           => 0,
 				'minify_google_fonts'         => 1,
 				'minify_html'                 => 0,
 				'manual_preload'              => 0,
