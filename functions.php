fb7c07f58223b7f5e354275098a38997d3717b32
Brent Shepherd <brent@prospress.com>
2018-08-28 16:17:07 +1000

    Update docblock on as_unschedule_action()
    To avoid ambiguity about multiple actions.

--- functions.php
+++ functions.php
@@ -59,7 +59,14 @@ function as_schedule_cron_action( $timestamp, $schedule, $hook, $args = array(),
 }
 
 /**
- * Cancel the next occurrence of a job.
+ * Cancel the next occurrence of a scheduled action.
+ *
+ * While only the next instance of a recurring or cron action is unscheduled by this method, that will also prevent
+ * all future instances of that recurring or cron action from being run. Recurring and cron actions are scheduled in
+ * a sequence instead of all being scheduled at once. Each successive occurrence of a recurring action is scheduled
+ * only after the former action is run. If the next instance is never run, because it's unscheduled by this function,
+ * then the following instance will never be scheduled (or exist), which is effectively the same as being unscheduled
+ * by this method also.
  *
  * @param string $hook The hook that the job will trigger
  * @param array $args Args that would have been passed to the job
