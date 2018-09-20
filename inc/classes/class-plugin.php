535cae982d89b2fa2a8e34c474e970ecf78c1478
Grégory Viguier <Screenfeed@users.noreply.github.com>
2018-09-20 17:25:55 +0000

    Fixes #751 #730: Mobile Cache Not Enabled Automatically When Mobile Plugins Are Active (#1179)
    * Introduced the class Mobile_Subscriber
    This class handles events related to plugins that add mobile themes.
    WPR settings filtering from `WP_Rocket\Admin\Settings\Page` is now handled in this class.
    WPR settings sanitization forcing option activation in `WP_Rocket\Admin\Settings\Settings` has been removed, has it doesn't appear to be of any use.
    The function `rocket_is_mobile_plugin_active()` is now a wrapper.

--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -101,6 +101,7 @@ class Plugin {
 			];
 		}
 
+		$subscribers[] = new Plugins\Mobile_Subscriber();
 		$subscribers[] = new Plugins\Ecommerce\WooCommerce_Subscriber();
 		$subscribers[] = new Plugins\Security\Sucuri_Subscriber();
 		$subscribers[] = new Subscriber\Google_Tracking_Cache_Busting_Subscriber( new Busting\Busting_Factory( WP_ROCKET_CACHE_BUSTING_PATH, WP_ROCKET_CACHE_BUSTING_URL ), $this->options );
