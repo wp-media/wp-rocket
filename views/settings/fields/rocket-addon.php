3508668ed68d65c13671cfd256efe6ad7611dd1d
Screenfeed <git@screenfeed.fr>
2018-09-28 12:32:42 +0200

    Display a link to an addon settings only when requested, and not hardcoded to CloudFlare. This follows previous JS changes.

--- views/settings/fields/rocket-addon.php
+++ views/settings/fields/rocket-addon.php
@@ -17,6 +17,8 @@
  */
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+$settings_page = ! empty( $data['settings_page'] ) ? $data['settings_page'] : '';
 ?>
 
 <fieldset class="wpr-fieldsContainer-fieldset">
@@ -26,7 +28,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 			<div class="wpr-radio wpr-radio--reverse">
 				<input type="checkbox" id="<?php echo esc_attr( $data['id'] ); ?>" class="" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="1" <?php checked( $data['value'], 1 ); ?>>
 				<label for="<?php echo esc_attr( $data['id'] ); ?>" class="">
-					<span data-l10n-active="<?php echo esc_attr_x( 'On', 'Active state of checkbox', 'rocket' ); ?>" 
+					<span data-l10n-active="<?php echo esc_attr_x( 'On', 'Active state of checkbox', 'rocket' ); ?>"
   data-l10n-inactive="<?php echo esc_attr_x( 'Off', 'Inactive state of checkbox', 'rocket' ); ?>" class="wpr-radio-ui"></span>
 					<?php esc_html_e( 'Add-on status', 'rocket' ); ?>
 				</label>
@@ -50,7 +52,9 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 						<?php echo $data['description']; ?>
 					</div>
 				<?php endif; ?>
-				<a href="#cloudflare" class="wpr-button wpr-button--small wpr-button--icon wpr-button--purple wpr-icon-chevron-right wpr-cloudflareToggleButton"><?php esc_html_e( 'Modify options', 'rocket' ); ?></a>
+				<?php if ( $settings_page ) : ?>
+					<a href="#<?php echo esc_attr( $settings_page ); ?>" class="wpr-button wpr-button--small wpr-button--icon wpr-button--purple wpr-icon-chevron-right wpr-toggle-button wpr-<?php echo esc_attr( $settings_page ); ?>ToggleButton"><?php esc_html_e( 'Modify options', 'rocket' ); ?></a>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
