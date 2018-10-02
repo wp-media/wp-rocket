89970eaf7e680d3c0c11e454a76f0d96b8be9080
Jordi Tarrida <jordi.tarrida@gmail.com>
2018-10-02 10:40:12 +0200

    change filter to rocket_cache_mandatory_cookies and add gdpr[consent_types] cookie

--- inc/3rd-party/plugins/GDPR.php
+++ inc/3rd-party/plugins/GDPR.php
@@ -6,11 +6,11 @@
  */
 defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 if ( class_exists( 'GDPR' ) ) {
-	// Create cache version based on value set in gdpr[allowed_cookies] cookie
-	add_filter( 'rocket_cache_dynamic_cookies', 'rocket_get_GDPR_cookie' );
+	// Create cache version based on value set in gdpr[] cookies
+	add_filter( 'rocket_cache_mandatory_cookies', 'rocket_get_GDPR_cookies' );
 }
 /**
- * Return the cookie name set by GDPR plugin
+ * Return the cookie names set by GDPR plugin
  *
  * @since 3.1.4
  * @author jorditarrida
@@ -18,8 +18,9 @@ if ( class_exists( 'GDPR' ) ) {
  * @param $cookies array List of dynamic cookies
  * @return array List of dynamic cookies with the GDPR cookie appended
  */
-function rocket_get_GDPR_cookie( $cookies ) {
+function rocket_get_GDPR_cookies( $cookies ) {
 	$cookies[] = 'gdpr[allowed_cookies]';
+	$cookies[] = 'gdpr[consent_types]';
 	return $cookies;
 }
 /**
@@ -28,9 +29,9 @@ function rocket_get_GDPR_cookie( $cookies ) {
  * @since 3.1.4
  * @author jorditarrida
  */
-function rocket_add_GDPR_dynamic_cookie() {
+function rocket_add_GDPR_mandatory_cookies() {
 	// Create cache version based on value set in GDPR cookies
-	add_filter( 'rocket_cache_dynamic_cookies', 'rocket_get_GDPR_cookie' );
+	add_filter( 'rocket_cache_mandatory_cookies', 'rocket_get_GDPR_cookies' );
 	// Update the WP Rocket rules on the .htaccess file.
 	flush_rocket_htaccess();
 	// Regenerate the config file.
@@ -38,16 +39,16 @@ function rocket_add_GDPR_dynamic_cookie() {
 	// Clear WP Rocket cache
 	rocket_clean_domain();
 }
-add_action( 'activate_GDPR/gdpr.php', 'rocket_add_GDPR_dynamic_cookie', 11 );
+add_action( 'activate_GDPR/gdpr.php', 'rocket_add_GDPR_mandatory_cookies', 11 );
 /**
  * Remove dynamic cookie when GDPR plugin is deactivated.
  *
  * @since 3.1.4
  * @author jorditarrida
  */
-function rocket_remove_GDPR_dynamic_cookie() {
+function rocket_remove_GDPR_mandatory_cookies() {
 	// Delete the dynamic cookie filter
-	remove_filter( 'rocket_cache_dynamic_cookies', 'rocket_get_GDPR_cookie' );
+	remove_filter( 'rocket_cache_mandatory_cookies', 'rocket_get_GDPR_cookies' );
 	// Update the WP Rocket rules on the .htaccess file.
 	flush_rocket_htaccess();
 	// Regenerate the config file.
@@ -55,4 +56,4 @@ function rocket_remove_GDPR_dynamic_cookie() {
 	// Clear WP Rocket cache
 	rocket_clean_domain();
 }
-add_action( 'deactivate_GDPR/gdpr.php', 'rocket_remove_GDPR_dynamic_cookie', 11 );
+add_action( 'deactivate_GDPR/gdpr.php', 'rocket_remove_GDPR_mandatory_cookies', 11 );
