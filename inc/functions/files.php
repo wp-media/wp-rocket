e0cc0c8fdfc8ede5c63234250c9562825d12cbd0
Rémy Perona <remperona@gmail.com>
2018-10-03 13:59:10 -0400

    add new variable to handle mobile cache version delivery

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -99,6 +99,17 @@ function get_rocket_config_file() {
 		$buffer .= '$rocket_common_cache_logged_users = 1;' . "\n";
 	}
 
+	/**
+	 * Filters the use of the mobile cache version for tablets
+	 * 0 will serve desktop to tablets, 1 will serve mobile to tablets
+	 *
+	 * @since 3.2
+	 * @author Remy Perona
+	 *
+	 * @param int $mobile_for_tablet valid values are 0 or 1.
+	 */
+	$buffer .= '$rocket_cache_mobile_files_tablet = ' . apply_filters( 'rocket_cache_mobile_files_tablet', 0 ) . ';' . "\n";
+
 	foreach ( $options as $option => $value ) {
 		if ( 'cache_ssl' === $option || 'cache_mobile' === $option || 'do_caching_mobile_files' === $option || 'secret_cache_key' === $option ) {
 			$buffer .= '$rocket_' . $option . ' = \'' . $value . '\';' . "\n";
