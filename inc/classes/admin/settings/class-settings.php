70b52df493a0fab86529d83884bea9504acf3ba6
Git I Hate You <git@screenfeed.fr>
2018-10-05 14:32:39 +0200

    Align values

--- inc/classes/admin/settings/class-settings.php
+++ inc/classes/admin/settings/class-settings.php
@@ -442,10 +442,11 @@ class Settings {
 			'reduce_periodicity' => 1,
 			'disable'            => 1,
 		];
-		$input['control_heartbeat']           = ! empty( $input['control_heartbeat'] ) ? 1 : 0;
-		$input['heartbeat_site_behavior'] = isset( $input['heartbeat_site_behavior'], $choices[ $input['heartbeat_site_behavior'] ] ) ? $input['heartbeat_site_behavior'] : '';
+
+		$input['control_heartbeat']         = ! empty( $input['control_heartbeat'] ) ? 1 : 0;
+		$input['heartbeat_site_behavior']   = isset( $input['heartbeat_site_behavior'], $choices[ $input['heartbeat_site_behavior'] ] ) ? $input['heartbeat_site_behavior'] : '';
 		$input['heartbeat_admin_behavior']  = isset( $input['heartbeat_admin_behavior'], $choices[ $input['heartbeat_admin_behavior'] ] ) ? $input['heartbeat_admin_behavior'] : '';
-		$input['heartbeat_editor_behavior']   = isset( $input['heartbeat_editor_behavior'], $choices[ $input['heartbeat_editor_behavior'] ] ) ? $input['heartbeat_editor_behavior'] : '';
+		$input['heartbeat_editor_behavior'] = isset( $input['heartbeat_editor_behavior'], $choices[ $input['heartbeat_editor_behavior'] ] ) ? $input['heartbeat_editor_behavior'] : '';
 
 		// Option : CDN.
 		$input['cdn'] = ! empty( $input['cdn'] ) ? 1 : 0;
