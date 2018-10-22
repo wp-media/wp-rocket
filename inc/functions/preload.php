eff97f4942baaf20208cd638d9a65b30b1c53b05
Rémy Perona <remperona@gmail.com>
2018-10-22 14:38:12 -0400

    check for current user permissions in admin post action

--- inc/functions/preload.php
+++ inc/functions/preload.php
@@ -80,6 +80,12 @@ function do_admin_post_rocket_preload_cache() {
 		wp_nonce_ays( '' );
 	}
 
+	/** This filter is documented in inc/admin-bar.php */
+	if ( ! current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) ) {
+		wp_safe_redirect( wp_get_referer() );
+		die();
+	}
+
 	$lang = isset( $_GET['lang'] ) && 'all' !== $_GET['lang'] ? sanitize_key( $_GET['lang'] ) : '';
 	run_rocket_bot( 'cache-preload', $lang );
 	run_rocket_sitemap_preload();
