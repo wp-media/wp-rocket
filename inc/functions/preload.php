820bc787d880c944242334008795798615763b20
Rémy Perona <remperona@gmail.com>
2018-11-09 10:13:59 -0500

    remove cancel preload and prevent new preload if one is already running

--- inc/functions/preload.php
+++ inc/functions/preload.php
@@ -26,8 +26,6 @@ function run_rocket_bot( $spider = 'cache-preload', $lang = '' ) {
 
 	$homepage_preload = new WP_Rocket\Preload\Homepage( new WP_Rocket\Preload\Full_Process() );
 
-	$homepage_preload->cancel_preload();
-	usleep( 1000000 );
 	$homepage_preload->preload( $urls );
 }
 
@@ -60,8 +58,6 @@ function run_rocket_sitemap_preload() {
 
 	$sitemap_preload = new WP_Rocket\Preload\Sitemap( new WP_Rocket\Preload\Full_Process() );
 
-	$sitemap_preload->cancel_preload();
-	usleep( 1000000 );
 	$sitemap_preload->run_preload( $sitemaps );
 }
 
@@ -84,6 +80,13 @@ function do_admin_post_rocket_preload_cache() {
 
 	/** This filter is documented in inc/admin-bar.php */
 	if ( ! current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) ) {
+		wp_safe_redirect( wp_get_referer() );
+		die();
+	}
+
+	$preload_process = new WP_Rocket\Preload\Full_Process();
+
+	if ( $preload_process->is_process_running() ) {
 		wp_safe_redirect( wp_get_referer() );
 		die();
 	}
