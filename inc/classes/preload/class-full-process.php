c4771018333b98ddafd91a00eb22b7ad1b116755
Rémy Perona <remperona@gmail.com>
2018-11-09 12:42:28 -0500

    take https into account when checking for the cache file, and remove double slashes

--- inc/classes/preload/class-full-process.php
+++ inc/classes/preload/class-full-process.php
@@ -85,6 +85,12 @@ class Full_Process extends \WP_Background_Process {
 	 * @return bool
 	 */
 	protected function is_already_cached( $item ) {
+		static $https;
+
+		if ( ! isset( $https ) ) {
+			$https = ( is_ssl() && get_rocket_option( 'cache_ssl' ) ) ? '-https' : '';
+		}
+
 		$url = get_rocket_parse_url( $item );
 
 		/** This filter is documented in inc/front/htaccess.php */
@@ -92,7 +98,7 @@ class Full_Process extends \WP_Background_Process {
 			$url['host'] = str_replace( '.', '_', $url['host'] );
 		}
 
-		$file_cache_path = WP_ROCKET_CACHE_PATH . $url['host'] . '/' . strtolower( $url['path'] ) . '/index.html';
+		$file_cache_path = WP_ROCKET_CACHE_PATH . $url['host'] . strtolower( $url['path'] ) . 'index.html';
 
 		return rocket_direct_filesystem()->exists( $file_cache_path );
 	}
