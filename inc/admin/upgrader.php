70d785ef548a03a434fe8b4cf1449a6bb0ceb996
Rémy Perona <remperona@gmail.com>
2018-11-13 12:33:59 -0500

    update htaccess on update to 3.2.2

--- inc/admin/upgrader.php
+++ inc/admin/upgrader.php
@@ -378,5 +378,9 @@ function rocket_new_upgrade( $wp_rocket_version, $actual_version ) {
 	if ( version_compare( $actual_version, '3.2.1.1', '<' ) ) {
 		rocket_generate_config_file();
 	}
+
+	if ( version_compare( $actual_version, '3.2.2', '<' ) ) {
+		flush_rocket_htaccess();
+	}
 }
 add_action( 'wp_rocket_upgrade', 'rocket_new_upgrade', 10, 2 );
