7ad12779705a51dcb2096338a73f7ba78f2c198f
Rémy Perona <remperona@gmail.com>
2018-11-13 10:52:33 -0500

    check for HTTP_USER_AGENT to prevent undefined index

--- inc/front/process.php
+++ inc/front/process.php
@@ -253,7 +253,7 @@ $allowed_ips = array(
 );
 
 // Don't cache page when these cookies don't exist.
-if ( ( ! isset( $allowed_ips[ $ip ] ) && ! preg_match( '#(PingdomPageSpeed|DareBoost|Google|PTST|WP Rocket)#i', $_SERVER['HTTP_USER_AGENT'] ) ) && isset( $rocket_cache_mandatory_cookies ) && ! preg_match( '#(' . $rocket_cache_mandatory_cookies . ')#', var_export( $_COOKIE, true ) ) ) {
+if ( ( ! isset( $allowed_ips[ $ip ] ) && ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) && ! preg_match( '#(PingdomPageSpeed|DareBoost|Google|PTST|WP Rocket)#i', $_SERVER['HTTP_USER_AGENT'] ) ) && isset( $rocket_cache_mandatory_cookies ) && ! preg_match( '#(' . $rocket_cache_mandatory_cookies . ')#', var_export( $_COOKIE, true ) ) ) {
 	rocket_define_donotoptimize_constant( true );
 
 	Logger::debug( 'Missing cookie: page not cached.', [
