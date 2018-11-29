e8be36e6789f99b2bbd22c6b738f9283751faedf
Rémy Perona <remperona@gmail.com>
2018-11-29 14:42:34 -0500

    also merge the images types array when on the all zone

--- inc/front/cdn.php
+++ inc/front/cdn.php
@@ -326,7 +326,7 @@ function rocket_cdn_custom_files( $html ) {
 
 	if ( in_array( 'all', $zones, true ) ) {
 		$cdn_zones[] = 'all';
-		$file_types  = array_merge( $file_types, $other_types );
+		$file_types  = array_merge( $file_types, $image_types, $other_types );
 	}
 
 	$cnames = get_rocket_cdn_cnames( $cdn_zones );
