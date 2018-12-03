5257aca85187f0cddc56c971b764eb04c7cb10db
dotSILENT <dot.silentium@gmail.com>
2018-12-03 16:55:33 +0100

    Add initial invalidation code

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -745,11 +745,20 @@ function rocket_clean_domain( $lang = '' ) {
 		*/
 		do_action( 'before_rocket_clean_domain', $root, $lang, $url );
 
+		$invalidate = 1; //get_rocket_option( 'invalidate_domain_cache' );
+
 		// Delete cache domain files.
 		$dirs = glob( $root . '*', GLOB_NOSORT );
 		if ( $dirs ) {
 			foreach ( $dirs as $dir ) {
-				rocket_rrmdir( $dir, get_rocket_i18n_to_preserve( $lang ) );
+				if( $invalidate) {
+					if( ! rocket_invalidate_dir( $dir ) ) {
+						// Failed to invalidate, fall back to rocket_rrmdir
+						rocket_rrmdir( $dir, get_rocket_i18n_to_preserve( $lang ) );
+					}
+				} else {
+					rocket_rrmdir( $dir, get_rocket_i18n_to_preserve( $lang ) );
+				}
 			}
 		}
 
@@ -931,6 +940,40 @@ function rocket_clean_cache_dir() {
 	 * @since 2.6.8
 	*/
 	do_action( 'after_rocket_clean_cache_dir' );
+}
+
+/**
+ * Invalidate directory by renaming it and queueing the deletion with WP Background Process
+ * 
+ * @author dotSILENT
+ *
+ * @param string $dir File/Directory to invalidate.
+ * @return bool
+ */
+function rocket_invalidate_dir( $dir ) {
+	$dir = untrailingslashit( $dir );
+
+	do_action( 'before_rocket_invalidate_dir', $dir );
+
+	if( ! rocket_direct_filesystem()->is_dir( $dir ) ) {
+		// just delete it since it's a single file
+		rocket_direct_filesystem()->delete( $dir );
+		return true;
+	}
+
+	$parentDir = dirname( $dir );
+	// Format of new directory name: invalid_UNIQIDHASH_dirname
+	$newName = 'invalid_' . uniqid() . '_' . basename( $dir );
+
+	if( ! rename($dir, $parentDir . '/' . $newName) ) {
+		// Failed to rename, fall back to default behaviour
+		return false;
+	}
+
+	// TODO queue the deletion of this invalidated directory
+
+	do_action( 'after_rocket_invalidate_dir', $dir );
+	return true;
 }
 
 /**
