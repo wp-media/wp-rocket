92479b9828f9a3993bc1a432ac1f930ed5d20c0d
Vasilis Manthos <vmanthos@users.noreply.github.com>
2018-12-04 10:41:51 +0200

    Exclude "_thriveCurrentPost" inline JavaScript to avoid the cache directory size issue
    Excluding `_thriveCurrentPost` inline JavaScript to avoid the cache directory size issue.
    
    Diff:
    https://www.diffchecker.com/qQhBPpZp
    
    Related ticket:
    https://secure.helpscout.net/conversation/723392971/89103/?folderId=2135277

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -440,6 +440,7 @@ class Combine extends Abstract_JS_Optimization {
 			'wphc_data',
 			'hc_rand_id',
 			'AfsAnalyticsObject',
+			'_thriveCurrentPost',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
