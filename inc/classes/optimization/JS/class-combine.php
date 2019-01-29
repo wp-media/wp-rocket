df57f8b734a51f21ba83072134addf1a70ef7379
Rémy Perona <remperona@gmail.com>
2019-01-29 09:46:57 -0500

    exclude Insticator pattern from inline JS

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -451,6 +451,7 @@ class Combine extends Abstract_JS_Optimization {
 			'#fancy-',
 			'ult-carousel-',
 			'theChampLJAuthUrl',
+			'Insticator',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
