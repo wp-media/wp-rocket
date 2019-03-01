1b7d0ba86f8c4f4b451783a922ac03c3cc9f292d
Rémy Perona <remperona@gmail.com>
2019-03-01 14:53:09 -0500

    add constructor for dependency injection and update hook to use for optimization

--- inc/classes/subscriber/Optimization/class-buffer-subscriber.php
+++ inc/classes/subscriber/Optimization/class-buffer-subscriber.php
@@ -4,8 +4,6 @@ namespace WP_Rocket\Subscriber\Optimization;
 use WP_Rocket\Buffer\Optimization;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
-defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
-
 /**
  * Event subscriber to buffer and process a page content.
  *
@@ -13,6 +11,21 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
  * @author Grégory Viguier
  */
 class Buffer_Subscriber implements Subscriber_Interface {
+	/**
+	 * Optimization instance
+	 *
+	 * @var Optimization
+	 */
+	private $optimizer;
+
+	/**
+	 * Constructor
+	 *
+	 * @param Optimization $optimizer Optimization instance.
+	 */
+	public function __construct( Optimization $optimizer ) {
+		$this->optimizer = $optimizer;
+	}
 
 	/**
 	 * Return an array of events that this subscriber wants to listen to.
@@ -25,27 +38,18 @@ class Buffer_Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'template_include' => [ 'start_content_process', PHP_INT_MAX - 9 ],
+			'template_redirect' => [ 'start_content_process', PHP_INT_MAX - 9 ],
 		];
 	}
 
 	/**
-	 * Start buffering the page content before including its template.
+	 * Start buffering the page content and apply optimizations if we can.
 	 *
 	 * @since  3.3
 	 * @access public
 	 * @author Grégory Viguier
-	 *
-	 * @param  string $template The path of the template to include.
-	 * @return string
 	 */
-	public function start_content_process( $template ) {
-		( new Optimization(
-			[
-				'config_dir_path' => WP_ROCKET_CONFIG_PATH,
-			]
-		) )->maybe_init_process();
-
-		return $template;
+	public function start_content_process() {
+		$this->optimizer->maybe_init_process();
 	}
 }
