72c1d7e4f89274a8ccb80d77ee5b8ce5796e60cd
Rémy Perona <remperona@gmail.com>
2019-03-07 22:03:41 -0500

    update code for nginx clean

--- inc/classes/subscriber/third-party/Cache/class-nginx-subscriber.php
+++ inc/classes/subscriber/third-party/Cache/class-nginx-subscriber.php
@@ -2,6 +2,7 @@
 namespace WP_Rocket\Subscriber\Third_Party\Cache;
 
 use WP_Rocket\Event_Management\Subscriber_Interface;
+use WP_Rocket\Logger\Logger;
 
 /**
  * Subscriber to sync NGINX cache with WP Rocket cache
@@ -19,6 +20,49 @@ class NGINX_Subscriber implements Subscriber_Interface {
 			'before_rocket_clean_file'   => 'clean_file',
 			'before_rocket_clean_home'   => [ 'clean_home', 10, 2 ],
 		];
+	}
+
+	/**
+	 * Purge all the domain
+	 *
+	 * @since 3.3
+	 * @author Remy Perona
+	 *
+	 * @param string $root The path of home cache file.
+	 * @param string $lang The current lang to purge.
+	 * @param string $url  The home url.
+	 * @return void
+	 */
+	public function clean_domain( $root, $lang, $url ) {
+		$this->send_purge_request( $url );
+	}
+
+	/**
+	 * Purge a specific page
+	 *
+	 * @since 3.3
+	 * @author Remy Perona
+	 *
+	 * @param string $url The url to purge.
+	 */
+	public function clean_file( $url ) {
+		$url = str_replace( '*', '', $url );
+		$this->send_purge_request( $url );
+	}
+
+	/**
+	 * Purge the homepage and its pagination
+	 *
+	 * @since 3.3
+	 * @author Remy Perona
+	 *
+	 * @param string $root The path of home cache file.
+	 * @param string $lang The current lang to purge.
+	 */
+	public function clean_home( $root, $lang ) {
+		$url = trailingslashit( get_rocket_i18n_home_url( $lang ) );
+
+		$this->send_purge_request( $url );
 	}
 
 	/**
@@ -43,6 +87,23 @@ class NGINX_Subscriber implements Subscriber_Interface {
 			$purge_url .= '?' . $parsed_url['query'];
 		}
 
-		wp_remote_get( $purge_url );
+		$purge_request = wp_remote_get( $purge_url );
+
+		if ( is_wp_error( $purge_request ) ) {
+			Logger::error( 'Error while purging NGINX Cache for url: ' . $purge_url, [ 'NGINX Add-on' ] );
+			return;
+		}
+
+		$response_code = wp_remote_retrieve_response_code( $purge_request );
+
+		if ( 200 === $response_code ) {
+			Logger::info( 'NGINX Cache purged for url: ' . $purge_url, [ 'NGINX Add-on' ] );
+			return;
+		}
+
+		if ( 404 === $response_code ) {
+			Logger::error( 'URL currently not NGINX cached: ' . $purge_url, [ 'NGINX Add-on'] );
+			return;
+		}
 	}
 }
