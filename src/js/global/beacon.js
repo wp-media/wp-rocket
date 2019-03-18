1c637c108dca8621428ce6a25dd71ec5dbc1c25a
Rémy Perona <remperona@gmail.com>
2019-03-18 15:18:54 -0400

    update code for Beacon 2.0 JS API

---
+++ src/js/global/beacon.js
@@ -0,0 +1,40 @@
+var $ = jQuery;
+$(document).ready(function(){
+    if ('Beacon' in window) {
+        /**
+         * Show beacons on button "help" click
+         */
+        var $help = $('.wpr-infoAction--help');
+        $help.on('click', function(e){
+            var ids = $(this).data('beacon-id');
+            wprCallBeacon(ids);
+            return false;
+        });
+
+        function wprCallBeacon(aID){
+            aID = aID.split(',');
+            if ( aID.length === 0 ) {
+                return;
+            }
+
+                if ( aID.length > 1 ) {
+                    window.Beacon("suggest", aID);
+
+                    setTimeout(function() {
+                        window.Beacon("open");
+                    }, 200);
+                } else {
+                    window.Beacon("article", aID);
+                }
+            
+        }
+
+        // Ask support
+        var $askSupport = $('.wpr-js-askSupport');
+        $askSupport.on('click', function(){
+            window.Beacon("open");
+            window.Beacon("navigate", "/ask/message/");
+            return false;
+        });
+    }
+});
