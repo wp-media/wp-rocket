e7a438ddfcd150443c6da849e27c9629e9d072f9
Rémy Perona <remperona@gmail.com>
2019-04-02 13:37:23 -0400

    add DocBlock

--- inc/classes/subscriber/third-party/Themes/class-elementor-subscriber.php
+++ inc/classes/subscriber/third-party/Themes/class-elementor-subscriber.php
@@ -21,6 +21,14 @@ class Elementor_Subscriber implements Subscriber_Interface {
         ];
     }
 
+    /**
+     * Remove the callback to clear the cache on widget update
+     *
+     * @since 3.3.1
+     * @author Remy Perona
+     *
+     * @return void
+     */
     public function remove_widget_callback() {
         remove_filter( 'widget_update_callback', 'rocket_widget_update_callback' );
     }
