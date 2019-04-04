180836bf0946ee6d06b5f716d96b009d008ac270
Rémy Perona <remperona@gmail.com>
2019-04-04 17:45:16 -0400

    improve nginx helper compatibility code

--- inc/3rd-party/plugins/nginx-helper.php
+++ inc/3rd-party/plugins/nginx-helper.php
@@ -1,31 +1,53 @@
 <?php
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
-if ( defined( 'rtCamp\WP\Nginx\RT_WP_NGINX_HELPER_PATH' ) ) :
-	add_action( 'admin_init', 'rocket_clear_cache_after_nginx_helper_purge' );
+global $nginx_helper;
+
+if ( isset( $nginx_helper ) ) :
 	/**
-	 * Clear WP Rocket cache after the cache is purged from Nginx Helper.
+	 * Clear WP Rocket cache after the NGINX cache is purged from Nginx Helper.
 	 *
-	 * @since 2.x.x
+	 * @since 3.3.0.1
 	 *
 	 * @return void
 	 */
 	function rocket_clear_cache_after_nginx_helper_purge() {
-		if ( isset( $_GET['nginx_helper_action'] ) && 'done' == $_GET['nginx_helper_action'] && current_user_can( 'manage_options' ) && check_admin_referer( 'nginx_helper-purge_all' ) ) {
-			// Clear all caching files.
-			rocket_clean_domain();
-
-			// Preload cache.
-			run_rocket_preload_cache( 'cache-preload' );
+		if ( ! isset( $_GET['nginx_helper_action'], $_GET['_wpnonce'] ) ) {
+			return;
 		}
+
+		if ( ! wp_verify_nonce( sanitize_key( $_GET['_wpnonce'] ), 'nginx_helper-purge_all' ) ) {
+			return;
+		}
+
+		if ( 'done' !== sanitize_text_field( wp_unslash( $_GET['nginx_helper_action'] ) ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) ) {
+			return;
+		}
+
+		if ( ! check_admin_referer( 'nginx_helper-purge_all' ) ) {
+			return;
+		}
+
+		// Clear all caching files.
+		rocket_clean_domain();
 	}
+	add_action( 'admin_init', 'rocket_clear_cache_after_nginx_helper_purge' );
+
 	/**
-	 * Clean the cache using Nginx Helper after WP Rocket's cache is purged.
+	 * Clean the NGINX cache using Nginx Helper after WP Rocket's cache is purged.
 	 *
-	 * @since 2.x.x
+	 * @since 3.3.0.1
 	 */
 	function rocket_clean_nginx_helper_cache() {
-		do_action( 'rt_nginx_helper_purge_all' );
+		if ( isset( $_GET['nginx_helper_action'] ) ) { // WPCS: csrf ok.
+			return;
+		}
+
+		do_action( 'rt_nginx_helper_purge_all' ); // WPCS: prefix ok.
 	}
 	add_action( 'after_rocket_clean_domain', 'rocket_clean_nginx_helper_cache' );
 endif;
