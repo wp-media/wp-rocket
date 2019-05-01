20ef9031d2301050710c847e35895c62f7d5145a
Rémy Perona <remperona@gmail.com>
2019-05-01 16:05:38 -0400

    add integration test for lighthouse UA

---
+++ tests/Integration/Buffer/Tests/TestIsSpeedTool.php
@@ -0,0 +1,29 @@
+<?php
+namespace WP_Rocket\Tests\Integration\Buffer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use WP_Rocket\Buffer\Tests;
+use WP_Rocket\Buffer\Config;
+
+class TestIsSpeedTool extends TestCase {
+     /**
+     * @covers ::is_speed_tool
+     * @author Remy Perona
+     */
+    public function testShouldReturnTrueWhenLighthouse() {
+        $config = new Config(
+            [
+                'config_dir_path' => 'wp-content/wp-rocket/config',
+                'server'          => [
+                    'HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/61.0.3116.0 Safari/537.36 Chrome-Lighthouse',
+                ]
+            ]
+        );
+
+        $tests = new Tests( $config );
+
+        $this->assertTrue(
+            $tests->is_speed_tool()
+        );
+    }
+}
