837b8a06055193149f2fccef0701bb992dd5efa9
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2019-05-07 22:08:49 +0200

    Add _atrk_opts to the list of auto-excluded inline JS

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -458,6 +458,7 @@ class Combine extends Abstract_JS_Optimization {
 			'ad_block_',
 			'elementorFrontendConfig',
 			'omapi_localized',
+			'_atrk_opts',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
