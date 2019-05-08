9f96b13f746696dcdedff9222c0c6c410cf4ea4c
Rémy Perona <remperona@gmail.com>
2019-05-08 10:11:23 -0400

    exclude Divi main style from async CSS

--- inc/3rd-party/themes/divi.php
+++ inc/3rd-party/themes/divi.php
@@ -3,7 +3,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 $current_theme = wp_get_theme();
 
-if ( 'Divi' === $current_theme->get( 'Name' ) ) :
+if ( 'Divi' === $current_theme->get( 'Name' ) || 'Divi' === $current_theme->get( 'Template' ) ) :
 	/**
 	 * Excludes Divi's Salvatorre script from JS minification
 	 *
@@ -23,4 +23,29 @@ if ( 'Divi' === $current_theme->get( 'Name' ) ) :
 		return $excluded_js;
 	}
 	add_filter( 'rocket_exclude_js', 'rocket_exclude_js_divi' );
+
+	/**
+	 * Excludes Divi main CSS files from Optimize CSS Delivery
+	 *
+	 * Prevents a display issue with the Divi Blog Module
+	 *
+	 * @since 3.3.3
+	 * @author Remy Perona
+	 *
+	 * @param array $excluded_css An array of excluded CSS
+	 * @return array
+	 */
+	function rocket_exclude_async_css_divi( $excluded_css ) {
+		if ( ! get_rocket_option( 'async_css' ) ) {
+			return $excluded_css;
+		}
+
+		$excluded_css[] = rocket_clean_exclude_file( get_stylesheet_uri() );
+		$excluded_css[] = rocket_clean_exclude_file( get_template_directory_uri() . '/style.css' );
+
+		return $excluded_css;
+	}
+	add_filter( 'rocket_exclude_async_css', 'rocket_exclude_async_css_divi' );
+	add_filter( 'rocket_exclude_cache_busting', 'rocket_exclude_async_css_divi' );
+	add_filter( 'rocket_exclude_css', 'rocket_exclude_async_css_divi' );
 endif;
