e4eade111aedb51a51a613dab658f664202ffcf5
Rémy Perona <remperona@gmail.com>
2019-05-17 15:28:27 -0400

    add integration tests for get_rocket_exclude_defer_js()

---
+++ tests/Integration/Functions/Options/TestExcludeDeferJS.php
@@ -0,0 +1,48 @@
+<?php
+namespace WP_Rocket\Tests\Integration\Functions\Options;
+
+use PHPUnit\Framework\TestCase;
+
+class TestExcludeDeferJS extends TestCase {
+    public function testShouldReturnExcludeDeferJSArray() {
+        $exclude_defer_js = [
+            'gist.github.com',
+            'content.jwplatform.com',
+            'js.hsforms.net',
+            'www.uplaunch.com',
+            'google.com/recaptcha',
+            'widget.reviews.co.uk',
+        ];
+
+        $this->assertSame(
+            $exclude_defer_js,
+            get_rocket_exclude_defer_js()
+        );
+    }
+
+    public function testShouldReturnExcludeDeferJSArrayWhenSafeMode() {
+        $options = [
+            'defer_all_js' => 1,
+            'defer_all_js_safe' => 1,
+        ];
+
+        update_option( 'wp_rocket_settings', $options );
+
+        $exclude_defer_js = [
+            'gist.github.com',
+            'content.jwplatform.com',
+            'js.hsforms.net',
+            'www.uplaunch.com',
+            'google.com/recaptcha',
+            'widget.reviews.co.uk',
+            '/wp-includes/js/jquery/jquery.js',
+            'c0.wp.com/c/(?:.+)/wp-includes/js/jquery/jquery.js',
+            'ajax.googleapis.com/ajax/libs/jquery/(?:.+)/jquery(?:\.min)?.js',
+        ];
+
+        $this->assertSame(
+            $exclude_defer_js,
+            get_rocket_exclude_defer_js()
+        );
+    }
+}
