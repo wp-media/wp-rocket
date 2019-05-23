0c2c153f55bb65420c634e511e8b0b4fcef5c929
Rémy Perona <remperona@gmail.com>
2019-05-23 11:36:52 -0400

    remove exclusions because of side effect

--- inc/3rd-party/themes/divi.php
+++ inc/3rd-party/themes/divi.php
@@ -23,29 +23,4 @@ if ( 'Divi' === $current_theme->get( 'Name' ) || 'Divi' === $current_theme->get(
 		return $excluded_js;
 	}
 	add_filter( 'rocket_exclude_js', 'rocket_exclude_js_divi' );
-
-	/**
-	 * Excludes Divi main CSS files from Optimize CSS Delivery
-	 *
-	 * Prevents a display issue with the Divi Blog Module
-	 *
-	 * @since 3.3.3
-	 * @author Remy Perona
-	 *
-	 * @param array $excluded_css An array of excluded CSS
-	 * @return array
-	 */
-	function rocket_exclude_async_css_divi( $excluded_css ) {
-		if ( ! get_rocket_option( 'async_css' ) ) {
-			return $excluded_css;
-		}
-
-		$excluded_css[] = rocket_clean_exclude_file( get_stylesheet_uri() );
-		$excluded_css[] = rocket_clean_exclude_file( get_template_directory_uri() . '/style.css' );
-
-		return $excluded_css;
-	}
-	add_filter( 'rocket_exclude_async_css', 'rocket_exclude_async_css_divi' );
-	add_filter( 'rocket_exclude_cache_busting', 'rocket_exclude_async_css_divi' );
-	add_filter( 'rocket_exclude_css', 'rocket_exclude_async_css_divi' );
 endif;
