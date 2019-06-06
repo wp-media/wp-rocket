47495d73b9f00af216755872b8dbeeeffa61361f
Rémy Perona <remperona@gmail.com>
2019-06-06 08:51:20 -0400

    update htaccess warning message

--- inc/admin/ui/notices.php
+++ inc/admin/ui/notices.php
@@ -395,21 +395,34 @@ function rocket_warning_htaccess_permissions() {
 		return;
 	}
 
-	$message = rocket_notice_writing_permissions( '.htaccess' );
+	$message = sprintf(
+		// translators: %s = plugin name.
+		__( '%s could not modify the .htaccess file due to missing writing permissions.', 'rocket' ),
+		'<strong>' . WP_ROCKET_PLUGIN_NAME . '</strong>'
+	);
+
+	$message .= '<br>' . sprintf(
+		/* translators: This is a doc title! %1$s = opening link; %2$s = closing link */
+		__( 'Troubleshoot: %1$sHow to make system files writeable%2$s', 'rocket' ),
+		/* translators: Documentation exists in EN, DE, FR, ES, IT; use loaclised URL if applicable */
+		'<a href="' . __( 'https://docs.wp-rocket.me/article/626-how-to-make-system-files-htaccess-wp-config-writeable/?utm_source=wp_plugin&utm_medium=wp_rocket', 'rocket' ) . '" target="_blank">',
+		'</a>'
+	);
 
 	add_filter( 'rocket_htaccess_mod_rewrite', '__return_false' );
 
-	rocket_notice_html(
-		[
-			'status'           => 'warning',
-			'dismissible'      => '',
-			'message'          => $message,
-			'dismiss_button'   => __FUNCTION__,
-			'readonly_content' => get_rocket_htaccess_marker(),
-		]
-	);
+	$message .= '<p>' . __( 'Don’t worry, WP Rocket’s page caching and settings will still function correctly.', 'rocket' ) . '<br>' . __( 'For optimal performance, adding the following lines into your .htaccess is recommended (not required):', 'rocket' ) . '<br><textarea readonly="readonly" id="rules" name="rules" class="large-text readonly" rows="6">' . esc_textarea( get_rocket_htaccess_marker() ) . '</textarea></p>';
 
 	remove_filter( 'rocket_htaccess_mod_rewrite', '__return_false' );
+
+	rocket_notice_html(
+		[
+			'status'         => 'warning',
+			'dismissible'    => '',
+			'message'        => $message,
+			'dismiss_button' => __FUNCTION__,
+		]
+	);
 }
 add_action( 'admin_notices', 'rocket_warning_htaccess_permissions' );
 
@@ -854,7 +867,7 @@ function rocket_notice_html( $args ) {
 			echo ( $tag ? '<p>' : '' ) . $args['message'] . ( $tag ? '</p>' : '' );
 		?>
 		<?php if ( ! empty( $args['readonly_content'] ) ) : ?>
-		<p><?php _e( 'The following code should have been written to this file:', 'rocket' ); ?>:
+		<p><?php _e( 'The following code should have been written to this file:', 'rocket' ); ?>
 			<br><textarea readonly="readonly" id="rules" name="rules" class="large-text readonly" rows="6"><?php echo esc_textarea( $args['readonly_content'] ); ?></textarea>
 		</p>
 		<?php
