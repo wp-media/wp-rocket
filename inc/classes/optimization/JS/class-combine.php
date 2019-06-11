c18b7c17b2bfe64d99866656025ae8b0d8fbd516
Rémy Perona <remperona@gmail.com>
2019-06-11 09:50:09 -0400

    Exclude inline JS omapi_data

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -472,6 +472,7 @@ class Combine extends Abstract_JS_Optimization {
 			'ct_checkjs_',
 			'WP_Statistics_http',
 			'penci_block_',
+			'omapi_data',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
