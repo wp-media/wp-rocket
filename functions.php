c702bb38d01c105a02ec321d526f60a38f9e3fe9
Brent Shepherd <thenbrent@gmail.com>
2019-07-02 16:43:18 +1000

    Add as_enqueue_async_action() and async APIs
    For scheduling actions that will always be run as soon as possible,
    rather than a date in the future, or even now, but only after other
    scheduled actions with a date in the past.
    
    This commit makes it acceptable to save an action with a NULL schedule
    by mapping its MySQL datetime string to 0000-00-00 00:00:00. This
    makes sure that any "async" action is given priority in queue processing.

--- functions.php
+++ functions.php
@@ -5,6 +5,18 @@
  */
 
 /**
+ * Enqueue an action to run one time, as soon as possible
+ *
+ * @param string $hook The hook to trigger.
+ * @param array  $args Arguments to pass when the hook triggers.
+ * @param string $group The group to assign this job to.
+ * @return string The action ID.
+ */
+function as_enqueue_async_action( $hook, $args = array(), $group = '' ) {
+	return ActionScheduler::factory()->async( $hook, $args, $group );
+}
+
+/**
  * Schedule an action to run one time
  *
  * @param int $timestamp When the job will run
