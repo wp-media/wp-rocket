0dd0e3975cafeeba6e40c6d48cb675f39f420abf
Rémy Perona <remperona@gmail.com>
2019-07-05 10:17:49 -0400

    finish the CDN class code

--- inc/classes/CDN/CDN.php
+++ inc/classes/CDN/CDN.php
@@ -1,85 +1,117 @@
 <?php
 namespace WP_Rocket\CDN;
 
+use WP_Rocket\Admin\Options_Data;
+
+/**
+ * CDN class
+ *
+ * @since 3.4
+ * @author Remy Perona
+ */
 class CDN {
+	/**
+	 * WP Rocket Options instance
+	 *
+	 * @var Options_Data
+	 */
 	private $options;
 
+	/**
+	 * Constructor
+	 *
+	 * @param Options_Data $options WP Rocket Options instance.
+	 */
 	public function __construct( Options_Data $options ) {
 		$this->options = $options;
 	}
 
+	/**
+	 * Search & Replace URLs with the CDN URLs in the provided content
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @param string $html HTML content.
+	 * @return string
+	 */
 	public function rewrite( $html ) {
-		$pattern = '#(?<=[(\"\'])(?:(?:https?:|)' . $this->get_base_url() . ')?\/(?:(?:(?:' . $this->get_allowed_paths() . ')[^\"\')]+)|(?:[^\/\"\']+\.[^\/\"\')]+))(?=[\"\')])#i';
+		$pattern = '#(?<url>(?<=[(\"\'])(?:(?:https?:|)' . preg_quote( $this->get_base_url(), '#' ) . ')?\/(?:(?:(?:' . $this->get_allowed_paths() . ')[^\"\')]+)|(?:[^\/\"\']+\.[^\/\"\')]+))(?=[\"\')]))#i';
 
-		return preg_replace_callback( $pattern, [ $this, 'rewrite_url' ], $html );
+		return preg_replace_callback(
+			$pattern,
+			function( $matches ) {
+				return $this->rewrite_url( $matches['url'] );
+			},
+			$html
+		);
 	}
 
+	/**
+	 * Rewrites an URL with the CDN URL
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @param string $url Original URL.
+	 * @return string
+	 */
 	public function rewrite_url( $url ) {
+		if ( ! $this->options->get( 'cdn' ) ) {
+			return $url;
+		}
+
 		if ( $this->is_excluded( $url ) ) {
 			return $url;
 		}
 
-	}
+		$cdn_urls = $this->get_cdn_urls( $this->get_zones_for_url( $url ) );
 
-	private function get_base_url() {
-		$home = get_option( 'home' );
-
-		return preg_quote( substr( $home, strpos( $home, '//' ) ), '#' );
-	}
-
-	private function get_allowed_paths() {
-		$wp_content_dirname = wp_parse_url( content_url(), PHP_URL_PATH );
-
-		$custom_upload_dirname = '';
-		$uploads_info          = wp_upload_dir();
-
-		if ( ! empty( $uploads_info['baseurl'] ) ) {
-			$custom_upload_dirname = '|' . trailingslashit( wp_parse_url( $uploads_info['baseurl'], PHP_URL_PATH ) );
+		if ( ! $cdn_urls ) {
+			return $url;
 		}
 
-		return preg_quote( $wp_content_dirname . $custom_upload_dirname . '|wp-includes', '#' );
-	}
+		$parsed_url = wp_parse_url( $url );
+		$cdn_url    = untrailingslashit( $cdn_urls[ ( abs( crc32( $parsed_url['path'] ) ) % count( $cdn_urls ) ) ] );
 
-	private function is_excluded( $url ) {
-		if ( false !== stripos( $url, '.php' ) ) {
-			return true;
+		if ( ! isset( $parsed_url['host'] ) ) {
+			return rocket_add_url_protocol( $cdn_url . '/' . ltrim( $url, '/' ) );
 		}
 
-		$path = wp_parse_url( $url, PHP_URL_PATH );
+		$home      = get_option( 'home' );
+		$home_host = wp_parse_url( $home, PHP_URL_HOST );
 
-		if ( ! isset( $path ) ) {
-			return true;
-		}
-
-		if ( preg_match( '#(' . $this->get_excluded_files() . ')#', $path ) ) {
-			return true;
-		}
-
-		return false;
+		return str_replace( $home_host, $cdn_url, $url );
 	}
 
 	/**
-	 * Get all files we don't allow to get in CDN.
+	 * Rewrites URLs to CDN URLs in CSS content
 	 *
-	 * @since 2.5
+	 * @since 3.4
+	 * @author Remy Perona
 	 *
-	 * @return string A pipe-separated list of excluded files.
+	 * @param string $content CSS content.
+	 * @return string
 	 */
-	private function get_excluded_files() {
-		$files = $this->options->get( 'cdn_reject_files', [] );
+	public function rewrite_css_properties( $content ) {
+		if ( ! preg_match_all( '#url\(\s*(\'|")?\s*(?![\'"]?data)(?<url>(?:https?:|)' . preg_quote( $this->get_base_url(), '#' ) . '\/[^"|\'|\)|\s]+)\s*#i', $content, $matches, PREG_SET_ORDER ) ) {
+			return $content;
+		}
 
-		/**
-			* Filter the excluded files.
-			*
-			* @since 2.5
-			*
-			* @param array $files List of excluded files.
-		*/
-		$files = (array) apply_filters( 'rocket_cdn_reject_files', $files );
-		$files = array_filter( $files );
-		$files = array_flip( array_flip( $files ) );
+		foreach ( $matches as $property ) {
+			/**
+			 * Filters the URL of the CSS property
+			 *
+			 * @since 2.8
+			 *
+			 * @param string $url URL of the CSS property.
+			 */
+			$cdn_url     = $this->rewrite_url( apply_filters( 'rocket_cdn_css_properties_url', $property['url'] ) );
+			$replacement = str_replace( $property['url'], $cdn_url, $property[0] );
+			$content     = str_replace( $property[0], $replacement, $content );
+		}
 
-		return implode( '|', $files );
+		return $content;
 	}
 
 	/**
@@ -91,7 +123,7 @@ class CDN {
 	 * @param  string $zones List of zones. Default is 'all'.
 	 * @return array
 	 */
-	private function get_cdn_urls( $zones = 'all' ) {
+	public function get_cdn_urls( $zones = 'all' ) {
 		$hosts    = [];
 		$cdn_urls = $this->options->get( 'cdn_cnames', [] );
 
@@ -126,5 +158,139 @@ class CDN {
 		$hosts = array_values( $hosts );
 
 		return $hosts;
+	}
+
+	/**
+	 * Gets the base URL for the website
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @return string
+	 */
+	private function get_base_url() {
+		$home = get_option( 'home' );
+
+		return substr( $home, strpos( $home, '//' ) );
+	}
+
+	/**
+	 * Gets the allowed paths as a regex pattern for the CDN rewrite
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @return string
+	 */
+	private function get_allowed_paths() {
+		$wp_content_dirname = ltrim( wp_parse_url( content_url(), PHP_URL_PATH ), '/' );
+
+		$upload_dirname = '';
+		$uploads_info   = wp_upload_dir();
+
+		if ( ! empty( $uploads_info['baseurl'] ) ) {
+			$upload_dirname = '|' . ltrim( trailingslashit( wp_parse_url( $uploads_info['baseurl'], PHP_URL_PATH ) ), '/' );
+		}
+
+		return $wp_content_dirname . $upload_dirname . '|wp-includes';
+	}
+
+	/**
+	 * Checks if the provided URL can be rewritten with the CDN URL
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @param string $url URL to check.
+	 * @return boolean
+	 */
+	private function is_excluded( $url ) {
+		if ( 'php' === pathinfo( strtok( $url, '?' ), PATHINFO_EXTENSION ) ) {
+			return true;
+		}
+
+		$path = wp_parse_url( $url, PHP_URL_PATH );
+
+		if ( ! isset( $path ) ) {
+			return true;
+		}
+
+		if ( '/' === $path ) {
+			return true;
+		}
+
+		if ( preg_match( '#^(' . $this->get_excluded_files() . ')$#', $path ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Gets the CDN zones for the provided URL
+	 *
+	 * @since 3.4
+	 * @author Remy Perona
+	 *
+	 * @param string $url URL to check.
+	 * @return array
+	 */
+	private function get_zones_for_url( $url ) {
+		$zones = [ 'all' ];
+
+		$ext = pathinfo( $url, PATHINFO_EXTENSION );
+
+		$image_types = [
+			'jpg',
+			'jpeg',
+			'jpe',
+			'png',
+			'gif',
+			'webp',
+			'bmp',
+			'tiff',
+		];
+
+		if ( 'css' === $ext || 'js' === $ext ) {
+			$zones[] = 'css_and_js';
+		}
+
+		if ( 'css' === $ext ) {
+			$zones[] = 'css';
+		}
+
+		if ( 'js' === $ext ) {
+			$zones[] = 'js';
+		}
+
+		if ( in_array( $ext, $image_types, true ) ) {
+			$zones[] = 'images';
+		}
+
+		return $zones;
+	}
+
+	/**
+	 * Get all files we don't allow to get in CDN.
+	 *
+	 * @since 2.5
+	 *
+	 * @return string A pipe-separated list of excluded files.
+	 */
+	private function get_excluded_files() {
+		$files = $this->options->get( 'cdn_reject_files', [] );
+
+		/**
+			* Filter the excluded files.
+			*
+			* @since 2.5
+			*
+			* @param array $files List of excluded files.
+		*/
+		$files = (array) apply_filters( 'rocket_cdn_reject_files', $files );
+		$files = array_filter( $files );
+		$files = array_flip( array_flip( $files ) );
+
+		return implode( '|', $files );
 	}
 }
