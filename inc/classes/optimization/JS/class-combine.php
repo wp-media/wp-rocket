6dc1dc3a957d289aedf234b98c9a2acd728b5590
PiotrBak <piotr@wp-media.me>
2019-07-12 22:48:59 +0100

    Add excluded string
    searchlocationHeader
    
    https://www.diffchecker.com/slI2O6UF

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -510,6 +510,7 @@ class Combine extends Abstract_JS_Optimization {
 			'algoliaAutocomplete',
 			'avia_framework_globals',
 			'tabs.easyResponsiveTabs',
+			'searchlocationHeader',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
