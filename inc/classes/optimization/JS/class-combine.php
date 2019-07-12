0cd3746bc6522eb325f53a549e0aa79b96464d1f
PiotrBak <piotr@wp-media.me>
2019-07-12 22:10:26 +0100

    Add excluded string
    pysWooProductData
    
    https://www.diffchecker.com/hvcU1Rn3

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -502,6 +502,7 @@ class Combine extends Abstract_JS_Optimization {
 			'wordpress_page_root',
 			'wcct_info',
 			'Springbot.product_id',
+			'pysWooProductData',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
