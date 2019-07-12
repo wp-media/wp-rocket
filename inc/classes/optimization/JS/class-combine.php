246a9111b8517b71e4e7ed148ebf0c8cc1e19502
PiotrBak <piotr@wp-media.me>
2019-07-12 22:20:07 +0100

    Add excluded string
    owl=$("#
    
    https://www.diffchecker.com/xq3i8kpM

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -504,6 +504,7 @@ class Combine extends Abstract_JS_Optimization {
 			'Springbot.product_id',
 			'pysWooProductData',
 			'dfd-heading',
+			'owl=$("#',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
