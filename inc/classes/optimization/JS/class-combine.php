7bf9726593e86b6bffc427997a0cda3b62b707c9
PiotrBak <piotr@wp-media.me>
2019-07-12 22:34:59 +0100

    Add excluded string ENFOLD
    avia_framework_globals
    
    https://www.diffchecker.com/zWM9RZeL

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -508,6 +508,7 @@ class Combine extends Abstract_JS_Optimization {
 			'penci_megamenu',
 			'fts_security',
 			'algoliaAutocomplete',
+			'avia_framework_globals',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
