8a5e252381d8a6192fdf2ab23be5d33eaa4f5891
PiotrBak <piotr@wp-media.me>
2019-07-16 23:16:06 +0100

    Added excluded JS
    data-parallax-speed
    
    https://www.diffchecker.com/nJQ5NkLR

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -512,6 +512,7 @@ class Combine extends Abstract_JS_Optimization {
 			'tabs.easyResponsiveTabs',
 			'searchlocationHeader',
 			'yithautocomplete',
+			'data-parallax-speed',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
