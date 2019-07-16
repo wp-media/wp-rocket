1006b1f9754704566ed80865f622f3efa048d6e9
PiotrBak <piotr@wp-media.me>
2019-07-16 23:20:30 +0100

    Added excluded JS
    currency_data=
    
    https://www.diffchecker.com/EiB6BiAV

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -513,6 +513,7 @@ class Combine extends Abstract_JS_Optimization {
 			'searchlocationHeader',
 			'yithautocomplete',
 			'data-parallax-speed',
+			'currency_data=',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
