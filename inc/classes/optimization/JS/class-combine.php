6ef0e74eacfe57093268e0e70b4bcdae966304bf
Soponar Cristina <crystinutzaa@gmail.com>
2019-07-26 14:52:41 +0300

    Temp Fix - Problem with big inline scripts #1784

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -217,6 +217,14 @@ class Combine extends Abstract_JS_Optimization {
 			} else {
 				preg_match( '/<script\b(?<attrs>[^>]*)>\s*(?:\/\*\s*<!\[CDATA\[\s*\*\/)?\s*(?<content>[\s\S]*?)\s*(?:\/\*\s*\]\]>\s*\*\/)?\s*<\/script>/msi', $script[0], $matches_inline );
 
+				if (preg_last_error() == PREG_BACKTRACK_LIMIT_ERROR) {
+					Logger::debug( 'PCRE regex execution Catastrophic Backtracking', [
+						'inline JS backtracking error',
+						'content' => $matches_inline['content'],
+					] );
+					return;
+				}
+
 				if ( strpos( $matches_inline['attrs'], 'type' ) !== false && ! preg_match( '/type\s*=\s*["\']?(?:text|application)\/(?:(?:x\-)?javascript|ecmascript)["\']?/i', $matches_inline['attrs'] ) ) {
 					Logger::debug( 'Inline script is not JS.', [
 						'js combine process',
