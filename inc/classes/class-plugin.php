425e11ad6a697d186dd99fadc3d0331eea00d8f5
Git I Hate You <git@screenfeed.fr>
2019-07-30 17:57:20 +0200

    Don't condition cache purge on key validation

--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -127,6 +127,7 @@ class Plugin {
 			'sucuri_subscriber',
 			'facebook_tracking_subscriber',
 			'google_tracking_subscriber',
+			'automatic_cache_purge_subscriber',
 			'preload_subscriber',
 			'sitemap_preload_subscriber',
 			'partial_preload_subscriber',
@@ -145,10 +146,6 @@ class Plugin {
 			'plugin_updater_subscriber',
 			'capabilities_subscriber',
 		];
-
-		if ( \rocket_valid_key() ) {
-			$common_subscribers[] = 'automatic_cache_purge_subscriber';
-		}
 
 		$subscribers = array_merge( $subscribers, $common_subscribers );
 
