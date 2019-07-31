decbffc64e638a892123db0b15e4f62c4d1830cb
PiotrBak <piotr@wp-media.me>
2019-07-31 23:05:06 +0100

    Added excluded JS
    CustomEvent.prototype=window.Event.prototype
    https://www.diffchecker.com/iqVVSBq1

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -504,6 +504,7 @@ class Combine extends Abstract_JS_Optimization {
 			'Springbot.product_id',
 			'et_animation_data=',
 			'current_url="',
+			'CustomEvent.prototype=window.Event.prototype',
 			
 		];
 
