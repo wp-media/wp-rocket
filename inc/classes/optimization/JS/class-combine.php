e277b99aa17a607b6cc9150b22871aef787196d7
PiotrBak <piotr@wp-media.me>
2019-08-01 22:48:09 +0100

    Added excluded JS
    top.location,thispage
    https://www.diffchecker.com/GXczjXge

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -508,6 +508,7 @@ class Combine extends Abstract_JS_Optimization {
 			'electro-wc-product-gallery',
 			'top.location.href=thispage',
 			'woof_is_mobile',
+			'top.location,thispage',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
