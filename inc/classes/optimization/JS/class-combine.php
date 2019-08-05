7de04b0510054ce54f840bd03f38389e696274be
PiotrBak <piotr@wp-media.me>
2019-08-05 22:46:24 +0100

    Added excluded JS
    ic_window_resolution
    https://www.diffchecker.com/r2NXf5PQ

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -516,6 +516,7 @@ class Combine extends Abstract_JS_Optimization {
 			'.ratingbox',
 			'user_rating.prototype.eraseCookie',
 			'test_run_nf_conditional',
+			'ic_window_resolution',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
