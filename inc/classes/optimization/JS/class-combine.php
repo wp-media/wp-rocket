17432c27c526fb874cb8cf21264f3b6ea8df79d4
PiotrBak <piotr@wp-media.me>
2019-08-05 22:38:50 +0100

    Addd excluded JS
    user_rating.prototype.eraseCookie
    https://www.diffchecker.com/4dMAMXUg

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -514,7 +514,7 @@ class Combine extends Abstract_JS_Optimization {
 			'us.templateDirectoryUri=',
 			'.fat-gallery-item',
 			'.ratingbox',
-			
+			'user_rating.prototype.eraseCookie',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
