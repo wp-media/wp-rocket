effa0e9cfc1f218478a29da77e258ba57e7db94a
Brent Shepherd <thenbrent@gmail.com>
2019-08-06 14:16:49 +1000

    Maintain property names for serialized schedules
    Prior to #333, recurring schedules used different property names
    to refer to equivalent data.
    
    For example, IntervalSchedule::start_timestamp was the same as
    SimpleSchedule::timestamp.
    
    PR #333 aligned properties and property names for better inheritance.
    
    To guard against the possibility of infinite loops if downgrading to
    Action Scheduler < 3.0.0, we need to also store the data with the old
    property names so if it's unserialized in AS < 3.0, the schedule
    doesn't end up with a 0 recurrence.
    
    Fixes #340.

--- classes/schedules/ActionScheduler_CronSchedule.php
+++ classes/schedules/ActionScheduler_CronSchedule.php
@@ -56,18 +56,45 @@ class ActionScheduler_CronSchedule extends ActionScheduler_Abstract_RecurringSch
 	}
 
 	/**
+	 * Serialize cron schedules with data required prior to AS 3.0.0
+	 *
+	 * Prior to Action Scheduler 3.0.0, reccuring schedules used different property names to
+	 * refer to equivalent data. For example, ActionScheduler_IntervalSchedule::start_timestamp
+	 * was the same as ActionScheduler_SimpleSchedule::timestamp. Action Scheduler 3.0.0
+	 * aligned properties and property names for better inheritance. To guard against the
+	 * possibility of infinite loops if downgrading to Action Scheduler < 3.0.0, we need to
+	 * also store the data with the old property names so if it's unserialized in AS < 3.0,
+	 * the schedule doesn't end up with a null recurrence.
+	 *
+	 * @return array
+	 */
+	public function __sleep() {
+
+		$sleep_params = parent::__sleep();
+
+		$this->start_timestamp = $this->scheduled_timestamp;
+		$this->cron            = $this->recurrence;
+
+		return array_merge( $sleep_params, array(
+			'start_timestamp',
+			'cron'
+		) );
+	}
+
+	/**
 	 * Unserialize cron schedules serialized/stored prior to AS 3.0.0
 	 *
 	 * For more background, @see ActionScheduler_Abstract_RecurringSchedule::__wakeup().
 	 */
 	public function __wakeup() {
-		if ( ! is_null( $this->start_timestamp ) ) {
+		if ( is_null( $this->scheduled_timestamp ) && ! is_null( $this->start_timestamp ) ) {
 			$this->scheduled_timestamp = $this->start_timestamp;
 			unset( $this->start_timestamp );
 		}
 
-		if ( ! is_null( $this->cron ) ) {
+		if ( is_null( $this->recurrence ) && ! is_null( $this->cron ) ) {
 			$this->recurrence = $this->cron;
+			unset( $this->cron );
 		}
 		parent::__wakeup();
 	}
--- classes/schedules/ActionScheduler_IntervalSchedule.php
+++ classes/schedules/ActionScheduler_IntervalSchedule.php
@@ -36,17 +36,43 @@ class ActionScheduler_IntervalSchedule extends ActionScheduler_Abstract_Recurrin
 	}
 
 	/**
+	 * Serialize interval schedules with data required prior to AS 3.0.0
+	 *
+	 * Prior to Action Scheduler 3.0.0, reccuring schedules used different property names to
+	 * refer to equivalent data. For example, ActionScheduler_IntervalSchedule::start_timestamp
+	 * was the same as ActionScheduler_SimpleSchedule::timestamp. Action Scheduler 3.0.0
+	 * aligned properties and property names for better inheritance. To guard against the
+	 * possibility of infinite loops if downgrading to Action Scheduler < 3.0.0, we need to
+	 * also store the data with the old property names so if it's unserialized in AS < 3.0,
+	 * the schedule doesn't end up with a null/false/0 recurrence.
+	 *
+	 * @return array
+	 */
+	public function __sleep() {
+
+		$sleep_params = parent::__sleep();
+
+		$this->start_timestamp     = $this->scheduled_timestamp;
+		$this->interval_in_seconds = $this->recurrence;
+
+		return array_merge( $sleep_params, array(
+			'start_timestamp',
+			'interval_in_seconds'
+		) );
+	}
+
+	/**
 	 * Unserialize interval schedules serialized/stored prior to AS 3.0.0
 	 *
 	 * For more background, @see ActionScheduler_Abstract_RecurringSchedule::__wakeup().
 	 */
 	public function __wakeup() {
-		if ( ! is_null( $this->start_timestamp ) ) {
+		if ( is_null( $this->scheduled_timestamp ) && ! is_null( $this->start_timestamp ) ) {
 			$this->scheduled_timestamp = $this->start_timestamp;
 			unset( $this->start_timestamp );
 		}
 
-		if ( ! is_null( $this->interval_in_seconds ) ) {
+		if ( is_null( $this->recurrence ) && ! is_null( $this->interval_in_seconds ) ) {
 			$this->recurrence = $this->interval_in_seconds;
 			unset( $this->interval_in_seconds );
 		}
