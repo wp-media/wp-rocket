fea802a5de46022fd30b2eb02abe2b32ffcbfa29
PiotrBak <piotr@wp-media.me>
2019-08-09 20:21:21 +0100

    Added excluded JS
    var sinceID_
    
    https://www.diffchecker.com/MzjCeTa7

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -528,6 +528,7 @@ class Combine extends Abstract_JS_Optimization {
 			'berocket_aapf_time_to_fix_products_style',
 			'ajax_pricing_rules_price_container_is',
 			'window.vc_googleMapsPointer',
+			'var sinceID_',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
