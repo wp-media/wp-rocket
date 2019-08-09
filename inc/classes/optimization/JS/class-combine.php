069c0a279956cfb7e67a99a0910e2045b3ce3ae5
PiotrBak <piotr@wp-media.me>
2019-08-09 19:31:16 +0100

    Added excluded JS
    ajax_pricing_rules_price_container_is
    
    https://www.diffchecker.com/opESLs7T

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -526,6 +526,7 @@ class Combine extends Abstract_JS_Optimization {
 			'GOTMLS_login_offset',
 			'PHP.wp_p_id',
 			'berocket_aapf_time_to_fix_products_style',
+			'ajax_pricing_rules_price_container_is',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
