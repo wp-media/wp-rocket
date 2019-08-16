cc3432c2382adebbdce293f6a39eb898399c4311
Rémy Perona <remperona@gmail.com>
2019-08-16 10:20:52 -0400

    Add unit and integration tests

---
+++ tests/Integration/Subscriber/ExpiredCachePurgeSubscriber/TestGetSubscribedEvents.php
@@ -0,0 +1,20 @@
+<?php
+namespace WP_Rocket\Tests\Integration\Subscriber\ExpiredCachePurgeSubscriber;
+
+use PHPUnit\Framework\TestCase;
+use WP_Rocket\Subscriber\Cache\Expired_Cache_Purge_Subscriber;
+
+class TestGetSubscribedEvents extends TestCase {
+	public function testShouldReturnSubscribedEventsArray() {
+		$events = [
+			'init'                    => 'schedule_event',
+			'rocket_deactivation'     => 'unschedule_event',
+			'rocket_purge_time_event' => 'purge_expired_files',
+		];
+
+		$this->assertSame(
+			$events,
+			Expired_Cache_Purge_Subscriber::get_subscribed_events()
+		);
+	}
+}
