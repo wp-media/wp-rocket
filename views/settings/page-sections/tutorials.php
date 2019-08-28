0976fc5c2de5a9507aada46c0fa99d5aa9d7302e
Rémy Perona <remperona@gmail.com>
2019-08-28 08:50:01 -0400

    add tutorials page

---
+++ views/settings/page-sections/tutorials.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Tutorials section template.
+ *
+ * @since 3.4
+ */
+
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+?>
+<div id="tutorials" class="wpr-Page">
+	<div class="wpr-sectionHeader">
+		<h2 class="wpr-title1 wpr-icon-tutorial"><?php esc_html_e( 'Tutorials', 'rocket' ); ?></h2>
+	</div>
+</div>
