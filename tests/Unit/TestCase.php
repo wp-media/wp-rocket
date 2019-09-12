345f48add70cd9b7b8ccf5a0186e9cf02fcbd14e
Git I Hate You <git@screenfeed.fr>
2019-09-13 01:06:31 +0200

    Added a method to mock common WP functions

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -14,4 +14,41 @@ class TestCase extends PHPUnitTestCase {
 		Monkey\tearDown();
 		parent::tearDown();
 	}
+
+	/**
+	 * Mock common WP functions.
+	 *
+	 * @since  3.4
+	 * @author Grégory Viguier
+	 * @access protected
+	 */
+	protected function mockCommonWpFunctions() {
+		$functions = [
+			'__',
+			'esc_attr__',
+			'esc_html__',
+			'_x',
+			'esc_attr_x',
+			'esc_html_x',
+			'_n',
+			'_nx',
+			'esc_attr',
+			'esc_html',
+		];
+
+		foreach ( $functions as $function ) {
+			Monkey\Functions\when( $function )->returnArg();
+		}
+
+		$functions = [
+			'_e',
+			'esc_attr_e',
+			'esc_html_e',
+			'_ex',
+		];
+
+		foreach ( $functions as $function ) {
+			Monkey\Functions\when( $function )->echoArg();
+		}
+	}
 }
