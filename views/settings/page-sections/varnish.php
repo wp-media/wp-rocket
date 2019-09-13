f03bcbdd0fdc9ef870f8c77f900e28f465960e60
Remy Perona <remy@wp-rocket.me>
2019-09-13 09:28:05 -0400

    add view for varnish addon

---
+++ views/settings/page-sections/varnish.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Varnish Cache section template.
+ *
+ * @since  3.5
+ * @author Remy Perona
+ *
+ * @param array {
+ *     Section arguments.
+ *
+ *     @type string $id    Page section identifier.
+ *     @type string $title Page section title.
+ * }
+ */
+
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+?>
+
+<div id="<?php echo esc_attr( $data['id'] ); ?>" class="wpr-Page">
+	<div class="wpr-sectionHeader">
+		<h2 class="wpr-title1">
+			<img src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL . 'logo-varnish.svg' ); ?>" width="152" height="135" alt="<?php echo esc_attr( $data['title'] ); ?>">
+		</h2>
+	</div>
+	<?php $this->render_settings_sections( $data['id'] ); ?>
+</div>
