b4e29debf45e7f6274830baa524704ecfdad6202
Remy Perona <remy@wp-rocket.me>
2019-09-13 14:22:24 -0400

    remove condition and add_actio ncalls

--- inc/deprecated/3.5.php
+++ inc/deprecated/3.5.php
@@ -41,32 +41,28 @@ function rocket_varnish_clean_home( $root, $lang ) {
     rocket_varnish_http_purge( $home_pagination_url );
 }
 
-if ( get_rocket_option( 'do_cloudflare' ) ) {
-    /**
-     * Sets the Varnish IP to localhost if Cloudflare is active
-     *
-     * @since 3.3.5
-     * @author Remy Perona
-     *
-     * @return string
-     */
-    function rocket_varnish_proxy_host() {
-        return 'localhost';
-    }
-    add_filter( 'rocket_varnish_ip', 'rocket_varnish_proxy_host', 9 );
+/**
+ * Sets the Varnish IP to localhost if Cloudflare is active
+ *
+ * @since 3.3.5
+ * @author Remy Perona
+ *
+ * @return string
+ */
+function rocket_varnish_proxy_host() {
+	return 'localhost';
+}
 
-    /**
-     * Sets the Host header to the website domain if Cloudflare is active
-     *
-     * @since 3.3.5
-     * @author Remy Perona
-     *
-     * @return string
-     */
-    function rocket_varnish_proxy_request_host() {
-        return wp_parse_url( home_url(), PHP_URL_HOST );
-    }
-    add_filter( 'rocket_varnish_purge_request_host', 'rocket_varnish_proxy_request_host', 9 );
+/**
+ * Sets the Host header to the website domain if Cloudflare is active
+ *
+ * @since 3.3.5
+ * @author Remy Perona
+ *
+ * @return string
+ */
+function rocket_varnish_proxy_request_host() {
+	return wp_parse_url( home_url(), PHP_URL_HOST );
 }
 
 /**
