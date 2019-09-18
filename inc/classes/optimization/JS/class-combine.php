1d4c5839f107378b87a5b1a53ba7b131ae8a5ac5
Soponar Cristina <crystinutzaa@gmail.com>
2019-09-18 12:38:04 +0300

    Exclude inline JS: dpsp_pin_button_data & tptn_tracker

--- inc/classes/optimization/JS/class-combine.php
+++ inc/classes/optimization/JS/class-combine.php
@@ -540,6 +540,8 @@ class Combine extends Abstract_JS_Optimization {
 			'advads_has_ads',
 			'wpseo_map_init',
 			'mdf_current_page_url',
+			'tptn_tracker',
+			'dpsp_pin_button_data',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
@@ -708,7 +710,7 @@ class Combine extends Abstract_JS_Optimization {
 			'cb_nombre',
 			'$(\'.fl-node-',
 			'function($){google_maps_',
-      			'$("#myCarousel',
+                '$("#myCarousel',
 		];
 
 		/**
