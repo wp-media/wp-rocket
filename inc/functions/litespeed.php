d5afae2a2415dac2e397b16d752c1be3224ebc3a
Soponar Cristina <crystinutzaa@gmail.com>
2019-10-08 21:33:42 +0300

    Added author and headers_sent; fixed alignment issue

--- inc/functions/litespeed.php
+++ inc/functions/litespeed.php
@@ -7,6 +7,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
  * Purge Litespeed URL
  *
  * @since 3.4.1
+ * @author Soponar Cristina
  *
  * @param  string $url The URL to purge.
  * @return void
@@ -15,8 +16,15 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
  */
 function rocket_litespeed_header_purge_url( $url ) {
     $parse_url      = get_rocket_parse_url( $url );
-	$path           = rtrim($parse_url['path'], '/');
+    $path           = rtrim($parse_url['path'], '/');
     $private_prefix = 'X-LiteSpeed-Purge: ' . $path ;
+
+    if ( headers_sent() ) {
+        Logger::debug( 'X-LiteSpeed Headers already sent', [
+            'headers_sent'
+        ] );
+        return;
+    }
 
     Logger::debug( 'X-LiteSpeed', [
         'rocket_litespeed_header_purge_url',
@@ -30,6 +38,7 @@ function rocket_litespeed_header_purge_url( $url ) {
  * Purge Litespeed Cache
  *
  * @since 3.4.1
+ * @author Soponar Cristina
  *
  * @param  string $url The URL to purge.
  * @return void
@@ -38,5 +47,10 @@ function rocket_litespeed_header_purge_url( $url ) {
  */
 function rocket_litespeed_header_purge_all( ) {
     $private_prefix = 'X-LiteSpeed-Purge: *';
+
+    if ( headers_sent() ) {
+        return;
+    }
+    
     @header( $private_prefix ) ;
 }
