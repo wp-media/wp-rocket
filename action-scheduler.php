e4522d718a4b57903373b755662e4db8c9daacd0
Brent Shepherd <thenbrent@gmail.com>
2019-10-22 14:25:04 +1000

    Tag version 3.0.0-RC-1

--- action-scheduler.php
+++ action-scheduler.php
@@ -5,7 +5,7 @@
  * Description: A robust scheduling library for use in WordPress plugins.
  * Author: Prospress
  * Author URI: https://prospress.com/
- * Version: 3.0.0-beta-1
+ * Version: 3.0.0-RC-1
  * License: GPLv3
  *
  * Copyright 2019 Prospress, Inc.  (email : freedoms@prospress.com)
@@ -25,28 +25,28 @@
  *
  */
 
-if ( ! function_exists( 'action_scheduler_register_3_dot_0_dot_0_beta_1' ) ) {
+if ( ! function_exists( 'action_scheduler_register_3_dot_0_dot_0_rc_1' ) ) {
 
 	if ( ! class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once( 'classes/ActionScheduler_Versions.php' );
 		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
-	add_action( 'plugins_loaded', 'action_scheduler_register_3_dot_0_dot_0_beta_1', 0, 0 );
+	add_action( 'plugins_loaded', 'action_scheduler_register_3_dot_0_dot_0_rc_1', 0, 0 );
 
-	function action_scheduler_register_3_dot_0_dot_0_beta_1() {
+	function action_scheduler_register_3_dot_0_dot_0_rc_1() {
 		$versions = ActionScheduler_Versions::instance();
-		$versions->register( '3.0.0-beta-1', 'action_scheduler_initialize_3_dot_0_dot_0_beta_1' );
+		$versions->register( '3.0.0-RC-1', 'action_scheduler_initialize_3_dot_0_dot_0_rc_1' );
 	}
 
-	function action_scheduler_initialize_3_dot_0_dot_0_beta_1() {
+	function action_scheduler_initialize_3_dot_0_dot_0_rc_1() {
 		require_once( 'classes/abstracts/ActionScheduler.php' );
 		ActionScheduler::init( __FILE__ );
 	}
 
 	// Support usage in themes - load this version if no plugin has loaded a version yet.
 	if ( did_action( 'plugins_loaded' ) && ! class_exists( 'ActionScheduler' ) ) {
-		action_scheduler_register_3_dot_0_dot_0_beta_1();
+		action_scheduler_register_3_dot_0_dot_0_rc_1();
 		do_action( 'action_scheduler_pre_theme_init' );
 		ActionScheduler_Versions::initialize_latest_version();
 	}
