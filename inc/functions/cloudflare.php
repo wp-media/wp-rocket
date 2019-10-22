69bdd0fdb7aa9b43b952415bbb1cc9d3c59f7762
Soponar Cristina <crystinutzaa@gmail.com>
2019-10-22 13:14:46 +0300

    Fixed call to Cloudflare zones - returns an array of objects in result

--- inc/functions/cloudflare.php
+++ inc/functions/cloudflare.php
@@ -47,7 +47,7 @@ function rocket_is_api_keys_valid_cloudflare( $cf_email, $cf_api_key, $cf_zone_i
 	try {
 		$cf_api_instance = new Cloudflare\Api( $cf_email, $cf_api_key );
 		$cf_user         = $cf_api_instance->get( 'user/' );
-		$cf_zone         = $cf_api_instance->get( 'zones/' . $cf_zone_id );
+		$cf_zone         = $cf_api_instance->get( 'zones/' );
 
 		if ( ! isset( $cf_zone->success ) || empty( $cf_zone->success ) ) {
 			$msg = __( 'Incorrect Cloudflare Zone ID.', 'rocket' );
@@ -73,7 +73,7 @@ function rocket_is_api_keys_valid_cloudflare( $cf_email, $cf_api_key, $cf_zone_i
 			if ( ! empty( $cf_zone->result ) ) {
 				foreach ( $cf_zone->result as $zone ) {
 					$parsed_url = wp_parse_url( $site_url );
-					if ( strtolower( $parsed_url['host'] ) === $cf_zone->name ) {
+					if ( strtolower( $parsed_url['host'] ) === $zone->name ) {
 						$zone_found = true;
 						break;
 					}
