682492d1785fbaf7f31165500c6fd23482f598ca
Soponar Cristina <crystinutzaa@gmail.com>
2019-10-23 11:53:08 +0300

    Cloudflare zone validation with zone_id and increased version no to 3.4.1-alpha3

--- inc/functions/cloudflare.php
+++ inc/functions/cloudflare.php
@@ -71,12 +71,9 @@ function rocket_is_api_keys_valid_cloudflare( $cf_email, $cf_api_key, $cf_zone_i
 			}
 
 			if ( ! empty( $cf_zone->result ) ) {
-				foreach ( $cf_zone->result as $zone ) {
-					$parsed_url = wp_parse_url( $site_url );
-					if ( strtolower( $parsed_url['host'] ) === $zone->name ) {
-						$zone_found = true;
-						break;
-					}
+				$parsed_url = wp_parse_url( $site_url );
+				if ( strtolower( $parsed_url['host'] ) === $cf_zone->name ) {
+					$zone_found = true;
 				}
 			}
 
