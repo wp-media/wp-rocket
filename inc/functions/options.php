51ab1b062938f12c62a8733c0f5388d8c570e5be
Vasilis Manthos <vmanthos@users.noreply.github.com>
2019-10-25 16:06:10 +0300

    Update options.php

--- inc/functions/options.php
+++ inc/functions/options.php
@@ -412,7 +412,6 @@ function get_rocket_exclude_defer_js() {
 		'www.uplaunch.com',
 		'google.com/recaptcha',
 		'widget.reviews.co.uk',
-		'app.mailerlite.com',
 	];
 
 	if ( get_rocket_option( 'defer_all_js', 0 ) && get_rocket_option( 'defer_all_js_safe', 0 ) ) {
