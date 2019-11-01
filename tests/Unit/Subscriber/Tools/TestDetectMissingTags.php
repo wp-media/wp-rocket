8bbc31314dee94de56a7f1c05f50a73fdddd43e1
Soponar Cristina <crystinutzaa@gmail.com>
2019-11-01 12:37:50 +0200

    Fixed spacing, changed to tabs

--- tests/Unit/Subscriber/Tools/TestDetectMissingTags.php
+++ tests/Unit/Subscriber/Tools/TestDetectMissingTags.php
@@ -8,14 +8,14 @@ use WP_Rocket\Logger\Logger;
 
 class TestDetectMissingTags extends TestCase {
 
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+	use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
-    protected function setUp() {
-        parent::setUp();
-        if ( ! defined('HOUR_IN_SECONDS') ) {
-            define('HOUR_IN_SECONDS', 60 * 60);
-        }
-    }
+	protected function setUp() {
+		parent::setUp();
+		if ( ! defined('HOUR_IN_SECONDS') ) {
+			define('HOUR_IN_SECONDS', 60 * 60);
+		}
+	}
 
 	/**
 	 * Test Detect_Missing_Tags_Subscriber->maybe_missing_tags() with missing HTML code </html> and </body> are missing.
@@ -23,86 +23,86 @@ class TestDetectMissingTags extends TestCase {
 	public function testShouldIdentifyMissingHtmlAndBodyAndWPFooter() {
 		$missing_tag = new Detect_Missing_Tags_Subscriber();
 
-        $html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_no_html_and_body.html');
+		$html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_no_html_and_body.html');
 
-        // Called did_action('wp_footer'), test also for missing wp_footer()
-        Functions\expect( 'did_action' )
-            ->once()
-            ->andReturn( 0 );
-
-        Functions\expect( 'set_transient' )
+		// Called did_action('wp_footer'), test also for missing wp_footer()
+		Functions\expect( 'did_action' )
 			->once()
-            ->with('rocket_missing_tags', false, HOUR_IN_SECONDS);
+			->andReturn( 0 );
 
-        $missing_tag->maybe_missing_tags( $html );
-
-        $this->assertTrue( true ); // Prevent "risky" warning.
-    }
-
-    /**
-     * Test should identify all elements
-     */
-    public function testShouldNotIdentifyMissingTags() {
-        $missing_tag = new Detect_Missing_Tags_Subscriber();
-
-        $html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_html_and_body.html');
-
-        // Called did_action('wp_footer'), test only for HTML and BODY
-        Functions\expect( 'did_action' )
-            ->once()
-            ->andReturn( true );
-
-        Functions\expect( 'set_transient' )
+		Functions\expect( 'set_transient' )
 			->once()
-            ->with('rocket_missing_tags', true, HOUR_IN_SECONDS);
+			->with('rocket_missing_tags', false, HOUR_IN_SECONDS);
 
-        $missing_tag->maybe_missing_tags( $html );
+		$missing_tag->maybe_missing_tags( $html );
 
-        $this->assertTrue( true ); // Prevent "risky" warning.
-    }
+		$this->assertTrue( true ); // Prevent "risky" warning.
+	}
 
+	/**
+	 * Test should identify all elements
+	 */
+	public function testShouldNotIdentifyMissingTags() {
+		$missing_tag = new Detect_Missing_Tags_Subscriber();
 
-    /**
-     * Test should identify that </html> and </body> are commented
-     */
-    public function testShouldIdentifyHTMLAndBodyAreCommented(){
-        $missing_tag = new Detect_Missing_Tags_Subscriber();
+		$html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_html_and_body.html');
 
-        $html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_commented_html_and_body.html');
-
-        // Called did_action('wp_footer'), test only for HTML and BODY
-        Functions\expect( 'did_action' )
-            ->once()
-            ->andReturn( true );
-
-        Functions\expect( 'set_transient' )
+		// Called did_action('wp_footer'), test only for HTML and BODY
+		Functions\expect( 'did_action' )
 			->once()
-            ->with('rocket_missing_tags', false, HOUR_IN_SECONDS);
+			->andReturn( true );
 
-        $missing_tag->maybe_missing_tags( $html );
-
-        $this->assertTrue( true ); // Prevent "risky" warning.
-    }
-
-    /**
-     * Test should identify that <html> and </body> are fine when are available and also commented
-     */
-    public function testShouldIdentifyFineHTMLAndBodyWhenAreCommented(){
-        $missing_tag = new Detect_Missing_Tags_Subscriber();
-
-        $html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_both_html_and_body_commented.html');
-
-        // Called did_action('wp_footer'), test only for HTML and BODY
-        Functions\expect( 'did_action' )
-            ->once()
-            ->andReturn( true );
-
-        Functions\expect( 'set_transient' )
+		Functions\expect( 'set_transient' )
 			->once()
-            ->with('rocket_missing_tags', true, HOUR_IN_SECONDS);
+			->with('rocket_missing_tags', true, HOUR_IN_SECONDS);
 
-        $missing_tag->maybe_missing_tags( $html );
+		$missing_tag->maybe_missing_tags( $html );
 
-        $this->assertTrue( true ); // Prevent "risky" warning.
-    }
+		$this->assertTrue( true ); // Prevent "risky" warning.
+	}
+
+
+	/**
+	 * Test should identify that </html> and </body> are commented
+	 */
+	public function testShouldIdentifyHTMLAndBodyAreCommented(){
+		$missing_tag = new Detect_Missing_Tags_Subscriber();
+
+		$html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_commented_html_and_body.html');
+
+		// Called did_action('wp_footer'), test only for HTML and BODY
+		Functions\expect( 'did_action' )
+			->once()
+			->andReturn( true );
+
+		Functions\expect( 'set_transient' )
+			->once()
+			->with('rocket_missing_tags', false, HOUR_IN_SECONDS);
+
+		$missing_tag->maybe_missing_tags( $html );
+
+		$this->assertTrue( true ); // Prevent "risky" warning.
+	}
+
+	/**
+	 * Test should identify that <html> and </body> are fine when are available and also commented
+	 */
+	public function testShouldIdentifyFineHTMLAndBodyWhenAreCommented(){
+		$missing_tag = new Detect_Missing_Tags_Subscriber();
+
+		$html = \file_get_contents( WP_ROCKET_PLUGIN_TESTS_ROOT . '/../Fixtures/Subscriber/Tools/original_both_html_and_body_commented.html');
+
+		// Called did_action('wp_footer'), test only for HTML and BODY
+		Functions\expect( 'did_action' )
+			->once()
+			->andReturn( true );
+
+		Functions\expect( 'set_transient' )
+			->once()
+			->with('rocket_missing_tags', true, HOUR_IN_SECONDS);
+
+		$missing_tag->maybe_missing_tags( $html );
+
+		$this->assertTrue( true ); // Prevent "risky" warning.
+	}
 }
