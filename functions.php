83f8a2a4b8218ec15b2aaf9d572a3c19827e4e2b
Brent Shepherd <thenbrent@gmail.com>
2019-11-13 14:20:58 +1000

    Check for in-progress actions as next action
    To avoid scheduling duplicates of long running actions when the
    action is being scheduled on a hook like 'init'.
    
    Fixes #387.
    Fixes #386.

--- functions.php
+++ functions.php
@@ -129,22 +129,36 @@ function as_unschedule_all_actions( $hook, $args = array(), $group = '' ) {
 }
 
 /**
+ * Check if there is an existing action in the queue with a given hook, args and group combination.
+ *
+ * An action in the queue could be pending, in-progress or aysnc. If the is pending for a time in
+ * future, its scheduled date will be returned as a timestamp. If it is currently being run, or an
+ * async action sitting in the queue waiting to be processed, in which case boolean true will be
+ * returned. Or there may be no async, in-progress or pending action for this hook, in which case,
+ * boolean false will be the return value.
+ *
  * @param string $hook
  * @param array $args
  * @param string $group
  *
- * @return int|bool The timestamp for the next occurrence of a scheduled action, true for an async action or false if there is no matching, pending action.
+ * @return int|bool The timestamp for the next occurrence of a pending scheduled action, true for an async or in-progress action or false if there is no matching action.
  */
 function as_next_scheduled_action( $hook, $args = NULL, $group = '' ) {
-	$params = array(
-		'status' => ActionScheduler_Store::STATUS_PENDING,
-	);
+	$params = array();
 	if ( is_array($args) ) {
 		$params['args'] = $args;
 	}
 	if ( !empty($group) ) {
 		$params['group'] = $group;
 	}
+
+	$params['status'] = ActionScheduler_Store::STATUS_RUNNING;
+	$job_id = ActionScheduler::store()->find_action( $hook, $params );
+	if ( ! empty( $job_id ) ) {
+		return true;
+	}
+
+	$params['status'] = ActionScheduler_Store::STATUS_PENDING;
 	$job_id = ActionScheduler::store()->find_action( $hook, $params );
 	if ( empty($job_id) ) {
 		return false;
