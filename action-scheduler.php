573e75a025d0ae62589af817ede77473e4841281
Brent Shepherd <brent@prospress.com>
2019-11-14 14:10:39 +1000

    Tag 3.0.0-RC-4

--- action-scheduler.php
+++ action-scheduler.php
@@ -5,7 +5,7 @@
  * Description: A robust scheduling library for use in WordPress plugins.
  * Author: Automattic
  * Author URI: https://automattic.com/
- * Version: 3.0.0-RC-3
+ * Version: 3.0.0-RC-4
  * License: GPLv3
  *
  * Copyright 2019 Automattic, Inc.  (https://automattic.com/contact/)
@@ -25,28 +25,28 @@
  *
  */
 
-if ( ! function_exists( 'action_scheduler_register_3_dot_0_dot_0_rc_3' ) ) {
+if ( ! function_exists( 'action_scheduler_register_3_dot_0_dot_0_rc_4' ) ) {
 
 	if ( ! class_exists( 'ActionScheduler_Versions' ) ) {
 		require_once( 'classes/ActionScheduler_Versions.php' );
 		add_action( 'plugins_loaded', array( 'ActionScheduler_Versions', 'initialize_latest_version' ), 1, 0 );
 	}
 
-	add_action( 'plugins_loaded', 'action_scheduler_register_3_dot_0_dot_0_rc_3', 0, 0 );
+	add_action( 'plugins_loaded', 'action_scheduler_register_3_dot_0_dot_0_rc_4', 0, 0 );
 
-	function action_scheduler_register_3_dot_0_dot_0_rc_3() {
+	function action_scheduler_register_3_dot_0_dot_0_rc_4() {
 		$versions = ActionScheduler_Versions::instance();
-		$versions->register( '3.0.0-RC-3', 'action_scheduler_initialize_3_dot_0_dot_0_rc_3' );
+		$versions->register( '3.0.0-RC-4', 'action_scheduler_initialize_3_dot_0_dot_0_rc_4' );
 	}
 
-	function action_scheduler_initialize_3_dot_0_dot_0_rc_3() {
+	function action_scheduler_initialize_3_dot_0_dot_0_rc_4() {
 		require_once( 'classes/abstracts/ActionScheduler.php' );
 		ActionScheduler::init( __FILE__ );
 	}
 
 	// Support usage in themes - load this version if no plugin has loaded a version yet.
 	if ( did_action( 'plugins_loaded' ) && ! class_exists( 'ActionScheduler' ) ) {
-		action_scheduler_register_3_dot_0_dot_0_rc_3();
+		action_scheduler_register_3_dot_0_dot_0_rc_4();
 		do_action( 'action_scheduler_pre_theme_init' );
 		ActionScheduler_Versions::initialize_latest_version();
 	}
