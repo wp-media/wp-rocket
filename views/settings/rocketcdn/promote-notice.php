db963f9699cf36c7dfcab74e2513e3d45154e3fa
Rémy Perona <remperona@gmail.com>
2019-12-03 09:34:43 -0500

    add id attribute to notice

--- views/settings/rocketcdn/promote-notice.php
+++ views/settings/rocketcdn/promote-notice.php
@@ -7,7 +7,7 @@
 
 defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 ?>
-<div class="notice notice-alt notice-warning is-dismissible">
+<div class="notice notice-alt notice-warning is-dismissible" id="rocketcdn-promote-notice">
 	<h2 class="notice-title"><?php esc_html_e( 'New!', 'rocket' ); ?></h2>
 	<p><?php esc_html_e( 'Speed up your website with Rocket CDN, WP Rocket’s Content Delivery Network!', 'rocket' ); ?></p>
 	<p><a href="#page_cdn" class="wpr-button"><?php esc_html_e( 'Learn More', 'rocket' ); ?></a></p>
