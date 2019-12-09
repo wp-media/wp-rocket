7d7e779177ad6ff03d864374d583efff013254c4
Rémy Perona <remperona@gmail.com>
2019-12-09 11:58:42 -0500

    add methods to purge RocketCDN cache and display the result

--- inc/classes/subscriber/CDN/RocketCDN/AdminPageSubscriber.php
+++ inc/classes/subscriber/CDN/RocketCDN/AdminPageSubscriber.php
@@ -18,13 +18,17 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'admin_notices'                       => 'promote_rocketcdn_notice',
+			'admin_notices'                       => [
+				[ 'promote_rocketcdn_notice' ],
+				[ 'purge_cache_notice' ],
+			],
 			'rocket_dashboard_after_account_data' => 'display_rocketcdn_status',
 			'rocket_before_cdn_sections'          => 'display_rocketcdn_cta',
 			'rocket_cdn_settings_fields'          => 'rocketcdn_field',
 			'wp_ajax_toggle_rocketcdn_cta'        => 'toggle_cta',
 			'wp_ajax_rocketcdn_dismiss_notice'    => 'dismiss_notice',
 			'admin_footer'                        => 'add_dismiss_script',
+			'admin_post_rocket_purge_rocketcdn'   => 'purge_cdn_cache',
 		];
 	}
 
@@ -38,12 +42,6 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 	 */
 	public function promote_rocketcdn_notice() {
 		if ( ! $this->should_display_notice() ) {
-			return;
-		}
-
-		$subscription_data = $this->get_subscription_data();
-
-		if ( $subscription_data['is_active'] ) {
 			return;
 		}
 
@@ -85,24 +83,6 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 	}
 
 	/**
-	 * Ajax callback to save the dismiss as a user meta
-	 *
-	 * @since 3.5
-	 * @author Remy Perona
-	 *
-	 * @return void
-	 */
-	public function dismiss_notice() {
-		check_ajax_referer( 'rocketcdn_dismiss_notice', 'nonce', true );
-
-		if ( ! isset( $_POST['action'] ) || 'rocketcdn_dismiss_notice' !== $_POST['action'] ) {
-			return;
-		}
-
-		update_user_meta( get_current_user_id(), 'rocketcdn_dismiss_notice', true );
-	}
-
-	/**
 	 * Checks if the promotion notice should be displayed
 	 *
 	 * @since 3.5
@@ -123,7 +103,31 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 			return false;
 		}
 
+		$subscription_data = $this->get_subscription_data();
+
+		if ( $subscription_data['is_active'] ) {
+			return false;
+		}
+
 		return true;
+	}
+
+	/**
+	 * Ajax callback to save the dismiss as a user meta
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 *
+	 * @return void
+	 */
+	public function dismiss_notice() {
+		check_ajax_referer( 'rocketcdn_dismiss_notice', 'nonce', true );
+
+		if ( ! isset( $_POST['action'] ) || 'rocketcdn_dismiss_notice' !== $_POST['action'] ) {
+			return;
+		}
+
+		update_user_meta( get_current_user_id(), 'rocketcdn_dismiss_notice', true );
 	}
 
 	/**
@@ -163,58 +167,6 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 	}
 
 	/**
-	 * Displays the Rocket CDN Call to Action on the CDN tab of WP Rocket settings page
-	 *
-	 * @since 3.5
-	 * @author Remy Perona
-	 *
-	 * @return void
-	 */
-	public function display_rocketcdn_cta() {
-		$subscription_data = $this->get_subscription_data();
-
-		if ( $subscription_data['is_active'] ) {
-			return;
-		}
-
-		$pricing            = $this->get_pricing_data();
-		$current_price      = $pricing['regular_price'];
-		$regular_price      = '';
-		$promotion_campaign = $pricing['discount_campaign_name'];
-		$promotion_end_date = date_i18n( get_option( 'date_format' ), strtotime( $pricing['end_date'] ) );
-		$nopromo_variant    = '--no-promo';
-		$cta_small_class    = 'wpr-isHidden';
-		$cta_big_class      = '';
-
-		if ( get_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden', true ) ) {
-			$cta_small_class = '';
-			$cta_big_class   = 'wpr-isHidden';
-		}
-
-		if ( $pricing['is_discount_active'] ) {
-			$regular_price   = $current_price;
-			$current_price   = $pricing['discounted_price'];
-			$nopromo_variant = '';
-		}
-
-		$small_cta_data = [
-			'container_class' => $cta_small_class,
-		];
-
-		$big_cta_data = [
-			'container_class'    => $cta_big_class,
-			'promotion_campaign' => $promotion_campaign,
-			'promotion_end_date' => $promotion_end_date,
-			'nopromo_variant'    => $nopromo_variant,
-			'regular_price'      => $regular_price,
-			'current_price'      => $current_price,
-		];
-
-		echo $this->generate( 'cta-small', $small_cta_data );
-		echo $this->generate( 'cta-big', $big_cta_data );
-	}
-
-	/**
 	 * Adds the Rocket CDN fields to the CDN section
 	 *
 	 * @since 3.5
@@ -244,25 +196,55 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 	}
 
 	/**
-	 * Toggles display of the Rocket CDN CTAs on the settings page
+	 * Displays the Rocket CDN Call to Action on the CDN tab of WP Rocket settings page
 	 *
 	 * @since 3.5
 	 * @author Remy Perona
 	 *
 	 * @return void
 	 */
-	public function toggle_cta() {
-		check_ajax_referer( 'rocket-ajax', 'nonce', true );
+	public function display_rocketcdn_cta() {
+		$subscription_data = $this->get_subscription_data();
 
-		if ( ! isset( $_POST['action'], $_POST['status'] ) || 'toggle_rocketcdn_cta' !== $_POST['action'] ) {
+		if ( $subscription_data['is_active'] ) {
 			return;
 		}
 
-		if ( 'big' === $_POST['status'] ) {
-			delete_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden' );
-		} elseif ( 'small' === $_POST['status'] ) {
-			update_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden', true );
+		$pricing            = $this->get_pricing_data();
+		$current_price      = $pricing['monthly_price'];
+		$regular_price      = '';
+		$promotion_campaign = $pricing['discount_campaign_name'];
+		$promotion_end_date = date_i18n( get_option( 'date_format' ), strtotime( $pricing['end_date'] ) );
+		$nopromo_variant    = '--no-promo';
+		$cta_small_class    = 'wpr-isHidden';
+		$cta_big_class      = '';
+
+		if ( get_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden', true ) ) {
+			$cta_small_class = '';
+			$cta_big_class   = 'wpr-isHidden';
 		}
+
+		if ( $pricing['is_discount_active'] ) {
+			$regular_price   = $current_price;
+			$current_price   = $pricing['discounted_price_monthly'];
+			$nopromo_variant = '';
+		}
+
+		$small_cta_data = [
+			'container_class' => $cta_small_class,
+		];
+
+		$big_cta_data = [
+			'container_class'    => $cta_big_class,
+			'promotion_campaign' => $promotion_campaign,
+			'promotion_end_date' => $promotion_end_date,
+			'nopromo_variant'    => $nopromo_variant,
+			'regular_price'      => $regular_price,
+			'current_price'      => $current_price,
+		];
+
+		echo $this->generate( 'cta-small', $small_cta_data );
+		echo $this->generate( 'cta-big', $big_cta_data );
 	}
 
 	/**
@@ -281,6 +263,7 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 		}
 
 		$default = [
+			'id'                            => 0,
 			'is_active'                     => false,
 			'subscription_next_date_update' => 0,
 			'subscription_status'           => 'cancelled',
@@ -327,12 +310,13 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 			return $pricing;
 		}
 
-		$default = [
-			'regular_price'          => 79.0,
-			'is_discount_active'     => false,
-			'discounted_price'       => 69.0,
-			'discount_campaign_name' => '',
-			'end_date'               => 0,
+		$transient_duration = 6 * HOUR_IN_SECONDS;
+		$default            = [
+			'monthly_price'            => 7.99,
+			'is_discount_active'       => false,
+			'discounted_price_monthly' => 5.99,
+			'discount_campaign_name'   => '',
+			'end_date'                 => 0,
 		];
 
 		$response = wp_remote_get(
@@ -340,7 +324,7 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 		);
 
 		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
-			set_transient( 'rocketcdn_pricing', $default, WEEK_IN_SECONDS );
+			set_transient( 'rocketcdn_pricing', $default, $transient_duration );
 
 			return $default;
 		}
@@ -348,7 +332,7 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 		$data = wp_remote_retrieve_body( $response );
 
 		if ( empty( $data ) ) {
-			set_transient( 'rocketcdn_pricing', $default, WEEK_IN_SECONDS );
+			set_transient( 'rocketcdn_pricing', $default, $transient_duration );
 
 			return $default;
 		}
@@ -356,8 +340,154 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 		$data = json_decode( $data, true );
 		$data = array_intersect_key( $data, $default );
 
-		set_transient( 'rocketcdn_pricing', $data, WEEK_IN_SECONDS );
+		set_transient( 'rocketcdn_pricing', $data, $transient_duration );
 
 		return $data;
+	}
+
+	/**
+	 * Toggles display of the Rocket CDN CTAs on the settings page
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 *
+	 * @return void
+	 */
+	public function toggle_cta() {
+		check_ajax_referer( 'rocket-ajax', 'nonce', true );
+
+		if ( ! isset( $_POST['action'], $_POST['status'] ) || 'toggle_rocketcdn_cta' !== $_POST['action'] ) {
+			return;
+		}
+
+		if ( 'big' === $_POST['status'] ) {
+			delete_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden' );
+		} elseif ( 'small' === $_POST['status'] ) {
+			update_user_meta( get_current_user_id(), 'rocket_rocketcdn_cta_hidden', true );
+		}
+	}
+
+	/**
+	 * Purges the CDN cache and store the response in a transient.
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 *
+	 * @return void
+	 */
+	public function purge_cdn_cache() {
+		if ( ! isset( $_GET['_wpnonce'] ) || ! wp_verify_nonce( sanitize_key( $_GET['_wpnonce'] ), 'rocket_purge_rocketcdn' ) ) {
+			wp_nonce_ays( '' );
+		}
+
+		if ( ! current_user_can( 'rocket_manage_options' ) ) {
+			wp_die();
+		}
+
+		set_transient( 'rocketcdn_purge_cache_response', $this->purge_cache_request(), HOUR_IN_SECONDS );
+
+		wp_safe_redirect( esc_url_raw( wp_get_referer() ) );
+		die();
+	}
+
+	/**
+	 * Sends a request to purge the CDN cache
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 *
+	 * @return string
+	 */
+	public function purge_cache_request() {
+		$subscription = $this->get_subscription_data();
+		$status       = 'error';
+
+		if ( ! isset( $subscription['id'] ) || 0 === $subscription['id'] ) {
+			return [
+				'status'  => $status,
+				'message' => __( 'RocketCDN cache purge failed: Missing identifier parameter', 'rocket' ),
+			];
+		}
+
+		$response = wp_remote_request(
+			self::ROCKETCDN_API . 'website/' . $subscription['id'] . '/purge/',
+			[
+				'method' => 'DELETE',
+			]
+		);
+
+		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			return [
+				'status'  => $status,
+				'message' => __( 'RocketCDN cache purge failed: The API returned an unexpected response code', 'rocket' ),
+			];
+		}
+
+		$data = wp_remote_retrieve_body( $response );
+
+		if ( empty( $data ) ) {
+			return [
+				'status'  => $status,
+				'message' => __( 'RocketCDN cache purge failed: The API returned an empty response', 'rocket' ),
+			];
+		}
+
+		$data = json_decode( $data );
+
+		if ( ! isset( $data->success ) ) {
+			return [
+				'status'  => $status,
+				'message' => __( 'RocketCDN cache purge failed: The API returned an unexpected response', 'rocket' ),
+			];
+		}
+
+		if ( ! $data->success ) {
+			return [
+				'status'  => $status,
+				'message' => sprintf(
+					// translators: %s = message returned by the API.
+					__( 'RocketCDN cache purge failed: %s', 'rocket' ),
+					$data->message
+				),
+			];
+		}
+
+		$status = 'success';
+
+		return [
+			'status'  => $status,
+			'message' => __( 'RocketCDN cache purge successful.', 'rocket' ),
+		];
+	}
+
+	/**
+	 * Displays a notice after purging the RocketCDN cache.
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 *
+	 * @return void
+	 */
+	public function purge_cache_notice() {
+		if ( ! current_user_can( 'rocket_manage_options' ) ) {
+			return;
+		}
+
+		if ( 'settings_page_wprocket' !== get_current_screen()->id ) {
+			return;
+		}
+
+		$purge_response = get_transient( 'rocketcdn_purge_cache_response' );
+
+		if ( false === $purge_response ) {
+			return;
+		}
+
+		\rocket_notice_html(
+			[
+				'status'  => $purge_response['status'],
+				'message' => $purge_response['message'],
+			]
+		);
 	}
 }
