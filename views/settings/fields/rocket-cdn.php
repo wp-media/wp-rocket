519a49be82654bf0745763599111e159e3ba79fe
Rémy Perona <remperona@gmail.com>
2019-12-09 11:57:17 -0500

    add section to clear RocketCDN cache

--- views/settings/fields/rocket-cdn.php
+++ views/settings/fields/rocket-cdn.php
@@ -39,3 +39,29 @@ $cnames_zone = get_rocket_option( 'cdn_zone' );
 		</div>
 	</div>
 </div>
+<div class="wpr-optionHeader">
+	<h3 class="wpr-title2"><?php esc_html_e( 'Purge RocketCDN cache', 'rocket' ); ?></h3>
+</div>
+<div class="wpr-fieldsContainer">
+	<div class="wpr-fieldsContainer-description">
+		<?php
+		printf(
+			// translators: %s is a "Learn more" link.
+			esc_html__( 'Purges RocketCDN cached resources for your website. %s', 'rocket' ),
+			'<a href="" target="_blank">' . esc_html__( 'Learn more', 'rocket' ) . '</a>'
+		);
+		?>
+	</div><br>
+	<?php
+		$this->render_action_button(
+			'link',
+			'rocket_purge_rocketcdn',
+			[
+				'label'      => __( 'Clear all RocketCDN cache files', 'rocket' ),
+				'attributes' => [
+					'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-trash',
+				],
+			]
+		);
+	?>
+</div>
