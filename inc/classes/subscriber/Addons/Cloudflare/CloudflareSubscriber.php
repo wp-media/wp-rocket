b646f52f8843124dab35b736cff5dd7c34f5230b
Soponar Cristina <crystinutzaa@gmail.com>
2019-12-13 15:29:36 +0200

    Removed duplicate filter documentation

--- inc/classes/subscriber/Addons/Cloudflare/CloudflareSubscriber.php
+++ inc/classes/subscriber/Addons/Cloudflare/CloudflareSubscriber.php
@@ -203,12 +203,7 @@ class CloudflareSubscriber implements Subscriber_Interface {
 		$feed_urls[]  = get_feed_link();
 		$feed_urls[]  = get_feed_link( 'comments_' );
 
-		/**
-		 * Filter the home feeds urls
-		 *
-		 * @since 2.7
-		 * @param array     $urls The urls of the home feeds.
-		*/
+		// this filter is documented in inc/functions/files.php.
 		$feed_urls  = apply_filters( 'rocket_clean_home_feeds', $feed_urls );
 		$purge_urls = array_unique( array_merge( $purge_urls, $feed_urls ) );
 
