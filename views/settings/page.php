212c22c06652653a2d9af4f7eb1216316f2122ef
Rémy Perona <remperona@gmail.com>
2019-12-16 10:59:28 -0500

    add new action rocket_settings_page_footer

--- views/settings/page.php
+++ views/settings/page.php
@@ -13,13 +13,13 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 
 settings_errors( $data['slug'] ); ?>
 <div class="wpr-wrap wrap">
-	<h1 class="screen-reader-text"><?php _e( 'WP Rocket Settings', 'rocket' ); ?></h1>
+	<h1 class="screen-reader-text"><?php esc_html_e( 'WP Rocket Settings', 'rocket' ); ?></h1>
 	<div class="wpr-body">
 
 		<header class="wpr-Header">
 			<div class="wpr-Header-logo">
-				<img src="<?php echo WP_ROCKET_ASSETS_IMG_URL; ?>logo-wprocket-dark.svg" width="163" height="44" alt="Logo WP Rocket" class="wpr-Header-logo-desktop">
-				<img src="<?php echo WP_ROCKET_ASSETS_IMG_URL; ?>picto-wprocket-dark.svg" width="28" height="50" alt="Logo WP Rocket" class="wpr-Header-logo-mobile">
+				<img src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL . 'logo-wprocket-dark.svg' ); ?>" width="163" height="44" alt="Logo WP Rocket" class="wpr-Header-logo-desktop">
+				<img src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL . 'picto-wprocket-dark.svg' ); ?>" width="28" height="50" alt="Logo WP Rocket" class="wpr-Header-logo-mobile">
 			</div>
 			<div class="wpr-Header-nav">
 				<?php $this->render_navigation(); ?>
@@ -27,7 +27,7 @@ settings_errors( $data['slug'] ); ?>
 			<div class="wpr-Header-footer">
 				<?php
 				// translators: %s = Plugin version number.
-				printf( __( 'version %s', 'rocket' ), WP_ROCKET_VERSION );
+				printf( esc_html__( 'version %s', 'rocket' ), WP_ROCKET_VERSION );
 				?>
 			</div>
 		</header>
@@ -47,17 +47,16 @@ settings_errors( $data['slug'] ); ?>
 
 				$this->render_tools_section();
 				$this->render_tutorials_section();
-			?>
+				?>
 			<div class="wpr-Content-tips">
 				<div class="wpr-radio wpr-radio--reverse wpr-radio--tips">
 					<input type="checkbox" class="wpr-js-tips" id="wpr-js-tips" value="1" checked>
 					<label for="wpr-js-tips">
-						<span data-l10n-active="<?php echo esc_attr_x( 'On', 'Active state of checkbox', 'rocket' ); ?>"
-  data-l10n-inactive="<?php echo esc_attr_x( 'Off', 'Inactive state of checkbox', 'rocket' ); ?>" class="wpr-radio-ui"></span>
-						<?php _e( 'Show Sidebar', 'rocket' ); ?></label>
+						<span data-l10n-active="<?php echo esc_attr_x( 'On', 'Active state of checkbox', 'rocket' ); ?>" data-l10n-inactive="<?php echo esc_attr_x( 'Off', 'Inactive state of checkbox', 'rocket' ); ?>" class="wpr-radio-ui"></span>
+						<?php esc_html_e( 'Show Sidebar', 'rocket' ); ?></label>
 				</div>
 			</div>
-			<?php
+				<?php
 			}
 			?>
 		</section>
@@ -94,7 +93,7 @@ settings_errors( $data['slug'] ); ?>
 			<p><?php esc_html_e( 'Below is a detailed view of all data WP Rocket will collect <strong>if granted permission.</strong>', 'rocket' ); ?></p>
 			<?php echo rocket_data_collection_preview_table(); ?>
 			<div class="wpr-Popin-flex">
-				<p><?php _e( 'WP Rocket will never transmit any domain names or email addresses (except for license validation), IP addresses, or third-party API keys.', 'rocket' ); ?></p>
+				<p><?php esc_html_e( 'WP Rocket will never transmit any domain names or email addresses (except for license validation), IP addresses, or third-party API keys.', 'rocket' ); ?></p>
 				<div>
 					<button class="wpr-button wpr-button--small wpr-button--icon wpr-icon-check wpr-button--blue"><?php esc_html_e( 'Activate Rocket analytics', 'rocket' ); ?></button>
 				</div>
@@ -102,19 +101,13 @@ settings_errors( $data['slug'] ); ?>
 		</div>
 	</div>
 	<div class="wpr-Popin-overlay"></div>
-	<div class="wpr-rocketcdn-modal" id="wpr-rocketcdn-modal" aria-hidden="true">
-		<div class="wpr-rocketcdn-modal__overlay" tabindex="-1" data-micromodal-close>
-			<div class="wpr-rocketcdn-modal__container" role="dialog" aria-modal="true" aria-labelledby="wpr-rocketcdn-modal-title" >
-				<header>
-					<h2 id="wpr-rocketcdn-modal-title">
-					Modal Title
-					</h2>
-					<button aria-label="Close modal" data-micromodal-close></button>
-				</header>
-				<div id="wpr-rocketcdn-modal-content">
-					Modal Content
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+	/**
+	 * Fires after the Settings page content
+	 *
+	 * @since 3.5
+	 * @author Remy Perona
+	 */
+	do_action( 'rocket_settings_page_footer' );
+	?>
 </div>
