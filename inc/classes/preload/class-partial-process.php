dbf4b003622ab3838cd9c5e223e2426a71fd61ba
Git I Hate You <git@screenfeed.fr>
2019-12-17 18:13:22 +0100

    Use the new abstract class with full and partial processes

--- inc/classes/preload/class-partial-process.php
+++ inc/classes/preload/class-partial-process.php
@@ -9,14 +9,7 @@ namespace WP_Rocket\Preload;
  *
  * @see WP_Background_Process
  */
-class Partial_Process extends \WP_Background_Process {
-	/**
-	 * Prefix
-	 *
-	 * @since 3.2
-	 * @var string
-	 */
-	protected $prefix = 'rocket';
+class Partial_Process extends Process {
 
 	/**
 	 * Specific action identifier for partial preload
@@ -27,75 +20,22 @@ class Partial_Process extends \WP_Background_Process {
 	protected $action = 'partial_preload';
 
 	/**
-	 * Preload the URL provided by $item
+	 * Preload the URL provided by $item.
 	 *
-	 * @since 3.2
+	 * @since  3.2
+	 * @since  3.6 $item can be an array.
 	 * @author Remy Perona
 	 *
-	 * @param mixed $item Queue item to iterate over.
-	 * @return null
+	 * @param  array|string $item {
+	 *     The item to preload: an array containing the following values.
+	 *     A string is allowed for backward compatibility (for the URL).
+	 *
+	 *     @type string $url    The URL to preload.
+	 *     @type bool   $mobile True when we want to send a "mobile" user agent with the request. Optional.
+	 * }
+	 * @return bool False.
 	 */
 	protected function task( $item ) {
-		if ( $this->is_already_cached( $item ) ) {
-			return false;
-		}
-
-		/**
-		 * Filters the arguments for the partial preload request
-		 *
-		 * @since 3.2
-		 * @author Remy Perona
-		 *
-		 * @param array $args Request arguments.
-		 */
-		$args = apply_filters(
-			'rocket_partial_preload_url_request_args',
-			[
-				'timeout'    => 0.01,
-				'blocking'   => false,
-				'user-agent' => 'WP Rocket/Partial_Preload',
-				'sslverify'  => apply_filters( 'https_local_ssl_verify', false ),
-			]
-		);
-
-		wp_remote_get( esc_url_raw( $item ), $args );
-
-		usleep( absint( get_rocket_option( 'sitemap_preload_url_crawl', 500000 ) ) );
-
-		return false;
-	}
-
-	/**
-	 * Check if the cache file for $item already exists
-	 *
-	 * @since 3.2
-	 * @author Remy Perona
-	 *
-	 * @param string $item Queue item to iterate over.
-	 * @return bool
-	 */
-	protected function is_already_cached( $item ) {
-		static $https;
-
-		if ( ! isset( $https ) ) {
-			$https = ( is_ssl() && get_rocket_option( 'cache_ssl' ) ) ? '-https' : '';
-		}
-
-		$url = get_rocket_parse_url( $item );
-
-		/** This filter is documented in inc/functions/htaccess.php */
-		if ( apply_filters( 'rocket_url_no_dots', false ) ) {
-			$url['host'] = str_replace( '.', '_', $url['host'] );
-		}
-
-		$url['path'] = trailingslashit( $url['path'] );
-
-		if ( '' !== $url['query'] ) {
-			$url['query'] = '#' . $url['query'] . '/';
-		}
-
-		$file_cache_path = WP_ROCKET_CACHE_PATH . $url['host'] . strtolower( $url['path'] . $url['query'] ) . 'index' . $https . '.html';
-
-		return rocket_direct_filesystem()->exists( $file_cache_path );
+		return $this->maybe_preload( $item );
 	}
 }
