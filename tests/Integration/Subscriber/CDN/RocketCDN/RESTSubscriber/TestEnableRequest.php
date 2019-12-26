36da433908d07d7d0e78a2084fad36c3fe6b7e0f
Rémy Perona <remperona@gmail.com>
2019-12-26 16:10:30 -0500

    add tests for the REST requests responses

---
+++ tests/Integration/Subscriber/CDN/RocketCDN/RESTSubscriber/TestEnableRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\Subscriber\CDN\RocketCDN\RESTSubscriber;
+
+use WP_Rocket\Tests\Integration\TestCase;
+use WP_Rest_Request;
+
+/**
+ * Tests for the REST enable request
+ * @group RocketCDN
+ */
+class TestEnableRequest extends TestCase {
+    /**
+     * Test we receive a successful response when providing the correct body params in the request
+     */
+    public function testShouldSuccessWhenCorrectDataProvided() {
+        $request = new WP_Rest_Request( 'PUT', '/wp-rocket/v1/rocketcdn/enable' );
+        $request->set_header('Content-Type', 'application/x-www-form-urlencoded');
+        $request->set_body_params(
+            [
+                'email' => '',
+                'key'   => '',
+                'url'   => 'https://rocketcdn.me',
+            ]
+        );
+
+        $response = rest_do_request( $request );
+        $expected = [
+			'code'    => 'success',
+			'message' => __( 'RocketCDN enabled', 'rocket' ),
+			'data'    => [
+				'status' => 200,
+			],
+		];
+
+        $this->assertSame( $expected, $response->get_data() );
+    }
+}
