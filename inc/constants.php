da28e35a233fa473dadc63005f5470771adcb28d
Tonya Mork <hellofromtonya@knowthecode.io>
2019-12-26 10:49:32 -0600

    Added 2 constants helper functions to abstract constants for testing.
    `rocket_has_constant()` is a wrapper for `defined()`
    `rocket_get_constant()` is a wrapper for invoking the constant.
    
    These 2 functions, when used in the source code, allow tests to mock and override a constant and its value.

---
+++ inc/constants.php
@@ -0,0 +1,38 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Checks if the constant is defined.
+ *
+ * NOTE: This function allows us to mock constants when testing.
+ *
+ * @since 3.5
+ *
+ * @param string $constant_name Name of the constant to check.
+ *
+ * @return bool true when constant is defined; else, false.
+ */
+function rocket_has_constant( $constant_name ) {
+	return defined( $constant_name );
+}
+
+/**
+ * Gets the constant is defined.
+ *
+ * NOTE: This function allows us to mock constants when testing.
+ *
+ * @since 3.5
+ *
+ * @param string $constant_name Name of the constant to check.
+ * @param mixed|null $default Optional. Default value to return if constant is not defined.
+ *
+ * @return bool true when constant is defined; else, false.
+ */
+function rocket_get_constant( $constant_name, $default = null ) {
+	if ( ! rocket_has_constant( $constant_name ) ) {
+		return $default;
+	}
+
+	return constant( $constant_name );
+}
---
+++ tests/Unit/inc/rocketGetConstant.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\Inc;
+
+use WP_Rocket\Tests\Unit\TestCase;
+use Brain\Monkey;
+
+/**
+ * @group Init
+ * @group Constants
+ * @covers ::rocket_get_constant
+ */
+class Test_RocketGetConstant extends TestCase {
+	protected function setUp() {
+		parent::setUp();
+
+		require_once WP_ROCKET_PLUGIN_ROOT . 'inc/constants.php';
+	}
+
+	/**
+	 * Test rocket_get_constant() should mock getting constants, allowing tests to override what gets returned.
+	 */
+	public function testShouldMockConstants() {
+		Monkey\Functions\expect( 'rocket_get_constant' )
+			->ordered()
+			->once()
+			->with( 'THIS_CONSTANT_DOES_NOT_EXIST' )
+			->andReturn( 'Hello World' )
+			->andAlsoExpectIt()
+			->once()
+			->with( 'WP_ROCKET_PLUGIN_ROOT' )
+			->andReturn( 'Hello World' );
+
+		$this->assertSame( 'Hello World', rocket_get_constant( 'THIS_CONSTANT_DOES_NOT_EXIST' ) );
+		$this->assertSame( 'Hello World', rocket_get_constant( 'WP_ROCKET_PLUGIN_ROOT' ) );
+	}
+
+	public function testShouldReturnDefaultWhenConstantNotDefined() {
+		$this->assertNull( rocket_get_constant( 'THIS_CONSTANT_DOES_NOT_EXIST' ) );
+		$this->assertSame( 'Hello World', rocket_get_constant( 'THIS_CONSTANT_DOES_NOT_EXIST', 'Hello World' ) );
+	}
+
+	public function testShouldReturnConstantWhenDefined() {
+		$this->assertSame( WP_ROCKET_PLUGIN_ROOT, rocket_get_constant( 'WP_ROCKET_PLUGIN_ROOT' ) );
+		$this->assertSame( WP_ROCKET_PLUGIN_TESTS_ROOT, rocket_get_constant( 'WP_ROCKET_PLUGIN_TESTS_ROOT' ) );
+	}
+}
