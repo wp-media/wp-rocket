6161e6474a4d027bee1e6c984004fae514c2ec1f
Soponar Cristina <crystinutzaa@gmail.com>
2019-12-30 15:49:53 +0200

    Removed sitemap_preload (double value)

--- inc/admin/options.php
+++ inc/admin/options.php
@@ -53,7 +53,6 @@ function rocket_after_save_options( $oldvalue, $value ) {
 		'sucury_waf_cache_sync'       => true,
 		'sucury_waf_api_key'          => true,
 		'manual_preload'              => true,
-		'sitemap_preload'             => true,
 	];
 
 	// Create 2 arrays to compare.
