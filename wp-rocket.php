6b24bfcc6c0fa102788a9c98a9caa42668eb38bc
Soponar Cristina <crystinutzaa@gmail.com>
2020-01-03 13:51:26 +0200

    Fixed #1550 Update Wordpress Compatibility

--- wp-rocket.php
+++ wp-rocket.php
@@ -20,6 +20,7 @@ defined( 'ABSPATH' ) || exit;
 // Rocket defines.
 define( 'WP_ROCKET_VERSION',               '3.4.3' );
 define( 'WP_ROCKET_WP_VERSION',            '4.9' );
+define( 'WP_ROCKET_WP_VERSION_TESTED',     '5.3.2' );
 define( 'WP_ROCKET_PHP_VERSION',           '5.6' );
 define( 'WP_ROCKET_PRIVATE_KEY',           false );
 define( 'WP_ROCKET_SLUG',                  'wp_rocket_settings' );
