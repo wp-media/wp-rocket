20a8a94bc505c0201fe0ecbb6c51998716d0fa79
Rémy Perona <remperona@gmail.com>
2020-01-09 14:59:46 -0500

    update version and last version

--- wp-rocket.php
+++ wp-rocket.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Rocket
  * Plugin URI: https://wp-rocket.me
  * Description: The best WordPress performance plugin.
- * Version: 3.5-alpha1
+ * Version: 3.5-alpha2
  * Code Name: Coruscant
  * Author: WP Media
  * Author URI: https://wp-media.me
@@ -18,7 +18,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Rocket defines.
-define( 'WP_ROCKET_VERSION',               '3.5-alpha1' );
+define( 'WP_ROCKET_VERSION',               '3.5-alpha2' );
 define( 'WP_ROCKET_WP_VERSION',            '4.9' );
 define( 'WP_ROCKET_PHP_VERSION',           '5.6' );
 define( 'WP_ROCKET_PRIVATE_KEY',           false );
@@ -72,7 +72,7 @@ if ( ! defined( 'CHMOD_WP_ROCKET_CACHE_DIRS' ) ) {
 	define( 'CHMOD_WP_ROCKET_CACHE_DIRS', 0755 ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
 }
 if ( ! defined( 'WP_ROCKET_LASTVERSION' ) ) {
-	define( 'WP_ROCKET_LASTVERSION', '3.3.7' );
+	define( 'WP_ROCKET_LASTVERSION', '3.4.4' );
 }
 
 if ( ! function_exists( 'WP_Filesystem' ) ) {
