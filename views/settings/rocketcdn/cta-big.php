f59c13d2c5c4884b0d79ae06fc5be388130d3902
Rémy Perona <remperona@gmail.com>
2020-01-14 17:12:42 -0500

    add the pricing paragraph inside the div

--- views/settings/rocketcdn/cta-big.php
+++ views/settings/rocketcdn/cta-big.php
@@ -80,8 +80,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 		<a href="https://go.wp-rocket.me/rocket-cdn" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Learn more about Rocket CDN', 'rocket' ); ?></a>
 	</div>
 	<button class="wpr-rocketcdn-cta-close<?php echo esc_attr( $data['nopromo_variant'] ); ?>"  id="wpr-rocketcdn-close-cta"><span class="screen-reader-text"><?php esc_html_e( 'Reduce this banner', 'rocket' ); ?></span></button>
-</div>
-<?php if ( ! empty( $data['promotion_campaign'] ) ) : ?>
+	<?php if ( ! empty( $data['promotion_campaign'] ) ) : ?>
 <p>
 	<?php
 	printf(
@@ -93,3 +92,4 @@ defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
 	?>
 </p>
 <?php endif; ?>
+</div>
