6dc743ec8c26a949fce9654f685dab4f7020b912
Tonya Mork <hellofromtonya@knowthecode.io>
2020-01-20 12:53:41 -0600

    Changed hosting file if wrapper to a guard clause.
    + Added sentence full hardstops (end in a .)
    + Reformatted for consistency.

--- inc/3rd-party/hosting/nginx.php
+++ inc/3rd-party/hosting/nginx.php
@@ -3,13 +3,14 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Compatibility with an usual NGINX configuration which include
- * try_files $uri $uri/ /index.php?q=$uri&$args
+ * Compatibility with an usual NGINX configuration which include:
+ *      try_files $uri $uri/ /index.php?q=$uri&$args
  *
  * @since 2.3.9
  *
  * @param array $query_strings Array of query strings to cache.
- * @return array Updated array of query strings
+ *
+ * @return array Updated array of query strings.
  */
 function rocket_better_nginx_compatibility( $query_strings ) {
 	global $is_nginx;
