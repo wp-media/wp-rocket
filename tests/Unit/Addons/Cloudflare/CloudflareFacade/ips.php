2b3fe6109963be9694804d1da641f292debf0583
Tonya Mork <hellofromtonya@knowthecode.io>
2020-01-21 10:41:19 -0600

    Fixed CloudflareFacade test naming. Doh.

--- tests/Unit/Addons/Cloudflare/CloudflareFacade/ips.php
+++ tests/Unit/Addons/Cloudflare/CloudflareFacade/ips.php
@@ -5,11 +5,11 @@ namespace WP_Rocket\Tests\Unit\Addons\Cloudflare\CloudflareFacade;
 use WP_Rocket\Tests\Unit\TestCase;
 
 /**
- * @covers WP_Rocket\Addons\Cloudflare\CloudflareFacade::settings
+ * @covers WP_Rocket\Addons\Cloudflare\CloudflareFacade::ips
  *
  * @group Cloudflare
  */
-class Test_Settings extends TestCase {
+class Test_Ips extends TestCase {
 
 	public function testShouldReturnSettingsWhenZoneIdIsSet() {
 		$this->assertTrue( true );
