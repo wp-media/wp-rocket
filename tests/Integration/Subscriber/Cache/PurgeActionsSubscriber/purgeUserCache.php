0c7957d8e95eb80bb7dc0b7db04cdde9077153b1
Rémy Perona <remperona@gmail.com>
2020-01-22 09:21:12 -0500

    add integration file for purge_user_cache

---
+++ tests/Integration/Subscriber/Cache/PurgeActionsSubscriber/purgeUserCache.php
@@ -0,0 +1,11 @@
+<?php
+namespace WP_Rocket\Tests\Integration\Subscriber\Cache\PurgeActionsSubscriber;
+
+use WP_Rocket\Tests\Integration\TestCase;
+
+/**
+ * @covers \WP_Rocket\Subscriber\Cache\PurgeActionsSubscriber:purge_user_cache
+ * @group purge_actions
+ */
+class Test_PurgeUserCache extends TestCase {
+}
