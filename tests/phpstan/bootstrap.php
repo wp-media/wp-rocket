791a33f7eeb801bd00dafb8ddc8a35eccb52b373
Viktor Szépe <viktor@szepe.net>
2020-02-03 22:16:58 +0100

    Introduce PHPStan (PR #2236)

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+define( 'DB_NAME', 'local' );
+define( 'WP_ROCKET_VERSION', '3.5' );
+define( 'WP_ROCKET_SLUG', 'wp_rocket_settings' );
+
+/**
+ * @return bool
+ */
+function rocket_avada_maybe_disable_lazyload() {}
