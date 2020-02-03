e46398b4cc176dc66d1c71811ebc5691beb7201c
Rémy Perona <remperona@gmail.com>
2020-02-03 14:43:00 -0500

    add unit tests for CDNOptionsManager methods

---
+++ tests/Unit/CDN/RocketCDN/CDNOptionsManager/enable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\CDN\RocketCDN\CDNOptionsManager;
+
+use WP_Rocket\Tests\Unit\TestCase;
+use WP_Rocket\CDN\RocketCDN\CDNOptionsManager;
+use Brain\Monkey\Functions;
+
+/**
+ * @covers\WP_Rocket\CDN\RocketCDN\CDNOptionsManager::enable
+ * @group RocketCDN
+ */
+class Test_Enable extends TestCase {
+	public function testShouldEnableCDNOptions() {
+		$expected = [
+			'cdn' => 1,
+			'cdn_cnames' => [
+				'https://rocketcdn.me',
+			],
+			'cdn_zone' => [
+				'all',
+			],
+		];
+
+		Functions\expect( 'delete_transient' )
+			->once()
+			->with( 'rocketcdn_status' );
+
+		$options_array = $this->createMock( \WP_Rocket\Admin\Options_Data::class );
+		$options_array->expects( $this->exactly(3))
+			->method( 'set' )
+			->withConsecutive(
+				[ 'cdn', 1 ],
+				[ 'cdn_cnames', [ 'https://rocketcdn.me' ] ],
+				[ 'cdn_zone', [ 'all'] ]
+			);
+		$options_array->method( 'get_options' )
+			->willReturn( $expected );
+
+		$options = $this->createMock( \WP_Rocket\Admin\Options::class );
+		$options->expects( $this->once() )
+			->method( 'set' )
+			->with( 'settings', $expected );
+
+		( new CDNOptionsManager(
+				$options,
+				$options_array
+			) )->enable( 'https://rocketcdn.me' );
+	}
+}
