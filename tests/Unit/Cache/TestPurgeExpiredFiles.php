0636de8a67eb0e91702504e41d817a765d25956e
Tonya Mork <hellofromtonya@knowthecode.io>
2020-02-09 13:27:38 -0600

    Adds Virtual Filesystem for unit tests.

--- tests/Unit/Cache/TestPurgeExpiredFiles.php
+++ tests/Unit/Cache/TestPurgeExpiredFiles.php
@@ -1,159 +1,94 @@
 <?php
+
 namespace WP_Rocket\Tests\Unit\Cache;
 
-use WPMedia\PHPUnit\Unit\TestCase;
-use WP_Rocket\Cache\Expired_Cache_Purge;
 use Brain\Monkey\Functions;
-use Brain\Monkey\Filters;
-use org\bovigo\vfs\vfsStream,
-	org\bovigo\vfs\vfsStreamDirectory;
+use WP_Rocket\Cache\Expired_Cache_Purge;
+use WPMedia\PHPUnit\Unit\VirtualFilesystemTestCase;
 
 /**
  * @group Cache
  */
-class TestPurgeExpiredFiles extends TestCase {
-	private $cache_path;
-	private $mock_fs;
-
-	public function setUp() {
-		parent::setUp();
-
-		$structure = [
-			'wp-rocket' => [
-				'example.org' => [
-					'index.html' => '',
-					'index.html_gzip' => '',
-					'about' => [
-						'index.html'=> '',
-						'index.html_gzip' => '',
-						'index-mobile.html' => '',
-						'index-mobile.html_gzip' => '',
-					],
-					'category' => [
-						'wordpress' => [
-							'index.html' => '',
-							'index.html_gzip' => '',
-						],
-					],
-					'blog' => [
-						'index.html' => '',
-						'index.html_gzip' => '',
-					],
-					'en' => [
-						'index.html' => '',
-						'index.html_gzip' => '',
-					],
-				],
-				'example.org-Greg-594d03f6ae698691165999' => [
-					'index.html' => '',
+class TestPurgeExpiredFiles extends VirtualFilesystemTestCase {
+	protected $wprocket_structure = [
+		'example.org'                             => [
+			'index.html'      => '',
+			'index.html_gzip' => '',
+			'about'           => [
+				'index.html'             => '',
+				'index.html_gzip'        => '',
+				'index-mobile.html'      => '',
+				'index-mobile.html_gzip' => '',
+			],
+			'category'        => [
+				'wordpress' => [
+					'index.html'      => '',
 					'index.html_gzip' => '',
 				],
 			],
+			'blog'            => [
+				'index.html'      => '',
+				'index.html_gzip' => '',
+			],
+			'en'              => [
+				'index.html'      => '',
+				'index.html_gzip' => '',
+			],
+		],
+		'example.org-Greg-594d03f6ae698691165999' => [
+			'index.html'      => '',
+			'index.html_gzip' => '',
+		],
+	];
+	private $cache_path;
+
+	protected function setUp() {
+		$this->structure['wp-rocket'] = $this->wprocket_structure;
+		parent::setUp();
+
+		Functions\When( 'rocket_direct_filesystem' )->justReturn( $this->filesystem );
+		Functions\When( 'get_rocket_parse_url' )->alias( function( $value ) {
+			return parse_url( $value );
+		} );
+
+		// Set file permissions back 11 hours.
+		$files = [
+			'wp-rocket/example.org/blog/index.html',
+			'wp-rocket/example.org/blog/index.html_gzip',
+			'wp-rocket/example.org-Greg-594d03f6ae698691165999/index.html',
+			'wp-rocket/example.org/en/index.html',
 		];
-
-		$this->cache_path = vfsStream::setup( 'cache', null, $structure );
-		$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('blog')->getChild('index.html')->lastAttributeModified( strtotime( '11 hours ago' ) );
-		$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('blog')->getChild('index.html_gzip')->lastAttributeModified( strtotime( '11 hours ago' ) );
-		$this->cache_path->getChild('wp-rocket')->getChild('example.org-Greg-594d03f6ae698691165999')->getChild('index.html')->lastAttributeModified( strtotime( '11 hours ago' ) );
-		$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('en')->getChild('index.html')->lastAttributeModified( strtotime( '11 hours ago' ) );
-
-		$this->mock_fs = $this->getMockBuilder( 'WP_Filesystem_Direct' )
-							->setMethods( [
-								'exists',
-								'delete'
-							])
-							->getMock();
-		$this->mock_fs->method('exists')->will($this->returnCallback('file_exists'));
-		$this->mock_fs->method('delete')->will($this->returnCallback(function($file) {
-			if ( @is_file( $file ) ) {
-				@unlink( $file );
-			} elseif ( @is_dir( $file ) ) {
-				@rmdir( $file );
-			}
-		}));
+		foreach ( $files as $filepath ) {
+			$file = $this->filesystem->getFile( $filepath );
+			$file->lastAttributeModified( strtotime( '11 hours ago' ) );
+		}
+		$this->cache_path = $this->filesystem->getUrl( 'wp-rocket' );
 	}
 
 	public function testShouldReturnNullWhenNoLifespan() {
-		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path->getChild( 'wp-rocket' )->url() );
+		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path );
+		Functions\expect( 'rocket_direct_filesystem' )->never();
 
 		$this->assertNull( $expired_cache_purge->purge_expired_files( 0 ) );
 	}
 
 	public function testShouldReturnNullWhenNoURLs() {
-		Functions\When('get_rocket_i18n_uri')->justReturn(
-			[
-				null,
-				1,
-				'',
-			]
-		);
+		Functions\When( 'get_rocket_i18n_uri' )->justReturn( [ null, 1, '' ] );
+		Functions\expect( 'rocket_direct_filesystem' )->never();
 
-		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path->getChild( 'wp-rocket' )->url() );
-
+		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path );
 		$this->assertNull( $expired_cache_purge->purge_expired_files( 36000 ) );
 	}
 
 	public function testShouldDeleteCacheFilesOlderThanLifespan() {
-		Functions\When('get_rocket_i18n_uri')->justReturn(
-			[
-				'http://example.org/'
-			]
-		);
+		Functions\When( 'get_rocket_i18n_uri' )->justReturn( [ 'http://example.org/' ] );
 
-		Functions\When( 'rocket_direct_filesystem')->alias( function() {
-			return $this->mock_fs;
-		});
-
-		Functions\When('get_rocket_parse_url')->alias( function( $value ) {
-			return parse_url( $value );
-		} );
-
-		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path->getChild( 'wp-rocket' )->url() );
+		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path );
 
 		$expired_cache_purge->purge_expired_files( 36000 );
 
-		$this->assertFalse(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org')->hasChild('blog')
-		);
-		$this->assertTrue(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('about')->hasChild('index.html')
-		);
-		$this->assertFalse(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org-Greg-594d03f6ae698691165999')->hasChild('index.html')
-		);
-	}
-
-	public function testShouldDeleteCacheFilesOlderThanLifespanWhenMultilingual() {
-		Functions\When('get_rocket_i18n_uri')->justReturn(
-			[
-				'http://example.org/en',
-				'http://example.org/',
-			]
-		);
-
-		Functions\When( 'rocket_direct_filesystem')->alias( function() {
-			return $this->mock_fs;
-		});
-
-		Functions\When('get_rocket_parse_url')->alias( function( $value ) {
-			return parse_url( $value );
-		} );
-
-		$expired_cache_purge = new Expired_Cache_Purge( $this->cache_path->getChild( 'wp-rocket' )->url() );
-
-		$expired_cache_purge->purge_expired_files( 36000 );
-
-		$this->assertFalse(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org')->hasChild('blog')
-		);
-		$this->assertTrue(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('about')->hasChild('index.html')
-		);
-		$this->assertFalse(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org-Greg-594d03f6ae698691165999')->hasChild('index.html')
-		);
-		$this->assertFalse(
-			$this->cache_path->getChild('wp-rocket')->getChild('example.org')->getChild('en')->hasChild('index.html')
-		);
+		$this->assertFalse( $this->filesystem->exists( 'wp-rocket/example.org/blog' ) );
+		$this->assertTrue( $this->filesystem->exists( 'wp-rocket/example.org/about/index.html' ) );
+		$this->assertFalse( $this->filesystem->exists( 'wp-rocket/example.org-Greg-594d03f6ae698691165999/index.html' ) );
 	}
 }
