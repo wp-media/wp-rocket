5765b3e4eb97f09999fb7e5ffa130d2a71ddf6ad
Rémy Perona <remperona@gmail.com>
2020-02-11 14:44:52 -0500

    add unit tests for cleaning functions

--- tests/Unit/Functions/rocketCleanCacheBusting.php
+++ tests/Unit/Functions/rocketCleanCacheBusting.php
@@ -1,7 +1,7 @@
 <?php
 namespace WP_Rocket\Tests\Unit\Functions;
 
-use WP_Rocket\Tests\Unit\TestCase;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
 use Brain\Monkey\Functions;
 
 /**
@@ -9,10 +9,33 @@ use Brain\Monkey\Functions;
  * @group Functions
  * @group Files
  */
-class Test_RocketCleanCacheBusting extends TestCase {
-	protected function setUp() {
+class Test_RocketCleanCacheBusting extends FilesystemTestCase {
+	public function setUp() {
 		parent::setUp();
 
-		require_once( WP_ROCKET_PLUGIN_ROOT . 'inc/functions/files.php' );
+		Functions\when( 'get_current_blog_id' )->justReturn( '1' );
+	}
+
+	public function testShouldCleanMinifiedCSS() {
+		rocket_clean_cache_busting( 'css' );
+
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css' ) );
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css.gz' ) );
+	}
+
+	public function testShouldCleanMinifiedJS() {
+		rocket_clean_cache_busting( 'js' );
+
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
+	}
+
+	public function testShouldCleanAllMinified() {
+		rocket_clean_cache_busting();
+
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css' ) );
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css.gz' ) );
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
+		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
 	}
 }
--- tests/Unit/Functions/rocketCleanMinify.php
+++ tests/Unit/Functions/rocketCleanMinify.php
@@ -1,18 +1,41 @@
 <?php
 namespace WP_Rocket\Tests\Unit\Functions;
 
-use WP_Rocket\Tests\Unit\TestCase;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
 use Brain\Monkey\Functions;
 
 /**
  * @covers rocket_clean_minify()
  * @group Functions
- * @group htaccess
+ * @group Files
  */
-class Test_RocketCleanMinify extends TestCase {
-	protected function setUp() {
+class Test_RocketCleanMinify extends FilesystemTestCase {
+	public function setUp() {
 		parent::setUp();
 
-		require( WP_ROCKET_PLUGIN_ROOT . 'inc/functions/files.php' );
+		Functions\when( 'get_current_blog_id' )->justReturn( '1' );
+	}
+
+	public function testShouldCleanMinifiedCSS() {
+		rocket_clean_minify( 'css' );
+
+		$this->assertFalse( $this->filesystem->exists( 'min/1/fa2965d41f1515951de523cecb81f85e.css' ) );
+		$this->assertFalse( $this->filesystem->exists( 'min/1/fa2965d41f1515951de523cecb81f85e.css.gz' ) );
+	}
+
+	public function testShouldCleanMinifiedJS() {
+		rocket_clean_minify( 'js' );
+
+		$this->assertFalse( $this->filesystem->exists( 'min/1/5c795b0e3a1884eec34a989485f863ff.js' ) );
+		$this->assertFalse( $this->filesystem->exists( 'min/1/5c795b0e3a1884eec34a989485f863ff.js.gz' ) );
+	}
+
+	public function testShouldCleanAllMinified() {
+		rocket_clean_minify();
+
+		$this->assertFalse( $this->filesystem->exists( 'min/1/fa2965d41f1515951de523cecb81f85e.css' ) );
+		$this->assertFalse( $this->filesystem->exists( 'min/1/fa2965d41f1515951de523cecb81f85e.css.gz' ) );
+		$this->assertFalse( $this->filesystem->exists( 'min/1/5c795b0e3a1884eec34a989485f863ff.js' ) );
+		$this->assertFalse( $this->filesystem->exists( 'min/1/5c795b0e3a1884eec34a989485f863ff.js.gz' ) );
 	}
 }
