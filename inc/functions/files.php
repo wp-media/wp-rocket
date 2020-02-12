2ba65097d98d752165287a876cec08b0bdf28c06
Rémy Perona <remperona@gmail.com>
2020-02-12 10:51:15 -0500

    wrap constant in rocket_get_constant()

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -481,7 +481,7 @@ function rocket_clean_minify( $extensions = [ 'js', 'css' ] ) {
 		}
 	}
 
-	$third_party = WP_ROCKET_MINIFY_CACHE_PATH . '3rd-party';
+	$third_party = rocket_get_constant( 'WP_ROCKET_MINIFY_CACHE_PATH' ) . '3rd-party';
 
 	try {
 		$files = new FilesystemIterator( $third_party );
