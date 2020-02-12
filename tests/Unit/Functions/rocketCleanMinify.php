62fb016a640447347517ef5c795b62351b05b33f
Rémy Perona <remperona@gmail.com>
2020-02-12 10:52:24 -0500

    define properties and expected values

--- tests/Unit/Functions/rocketCleanMinify.php
+++ tests/Unit/Functions/rocketCleanMinify.php
@@ -10,9 +10,25 @@ use Brain\Monkey\Functions;
  * @group Files
  */
 class Test_RocketCleanMinify extends FilesystemTestCase {
+	protected $rootVirtualDir = 'cache';
+	protected $structure = [
+		'min' => [
+			'1' => [
+				'5c795b0e3a1884eec34a989485f863ff.js'     => '',
+				'5c795b0e3a1884eec34a989485f863ff.js.gz'  => '',
+				'fa2965d41f1515951de523cecb81f85e.css'    => '',
+				'fa2965d41f1515951de523cecb81f85e.css.gz' => '',
+			],
+		],
+	];
+
 	public function setUp() {
 		parent::setUp();
 
+		Functions\expect( 'rocket_get_constant' )
+			->twice()
+			->with( 'WP_ROCKET_MINIFY_CACHE_PATH' )
+			->andReturn( 'vfs://cache/min/' );
 		Functions\when( 'get_current_blog_id' )->justReturn( '1' );
 	}
 
