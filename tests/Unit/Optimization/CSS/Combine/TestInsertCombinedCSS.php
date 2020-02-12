843eb45a38a5e5890d1cb8f77c27518ee1ea1375
Rémy Perona <remperona@gmail.com>
2020-02-12 10:52:42 -0500

    update test after fixtures changes

--- tests/Unit/Optimization/CSS/Combine/TestInsertCombinedCSS.php
+++ tests/Unit/Optimization/CSS/Combine/TestInsertCombinedCSS.php
@@ -6,42 +6,40 @@ use WP_Rocket\Optimization\CSS\Combine;
 use Brain\Monkey\Functions;
 
 /**
+ * @covers WP_Rocket\Optimization\CSS\Combine::insert_combined_css()
  * @group Optimize
  */
 class TestInsertCombinedCSS extends TestCase {
-    public function testShouldInsertCombinedCSS() {
-        Functions\when('create_rocket_uniqid')->justReturn('1234');
-        Functions\when('get_current_blog_id')->justReturn('1');
+	public function testShouldInsertCombinedCSS() {
+		Functions\when('create_rocket_uniqid')->justReturn('123456');
+		Functions\when('get_current_blog_id')->justReturn('1');
 
-        Functions\expect( 'rocket_get_constant' )
-	        ->once()
-	        ->with( 'WP_ROCKET_MINIFY_CACHE_PATH' )
-	        ->andReturn( 'wp-content/cache/' )
-	        ->andAlsoExpectIt()
-	        ->once()
-	        ->with( 'WP_ROCKET_MINIFY_CACHE_URL' )
-	        ->andReturn( 'http://example.com/wp-content/cache/' );
+		Functions\expect( 'rocket_get_constant' )
+			->once()
+			->with( 'WP_ROCKET_MINIFY_CACHE_PATH' )
+			->andReturn( 'wp-content/cache/min/' )
+			->andAlsoExpectIt()
+			->once()
+			->with( 'WP_ROCKET_MINIFY_CACHE_URL' )
+			->andReturn( 'http://example.org/wp-content/cache/min/' );
 
-        $options = $this->createMock('WP_Rocket\Admin\Options_Data');
-        $minify  = $this->createMock('MatthiasMullie\Minify\CSS');
+		$options = $this->createMock('WP_Rocket\Admin\Options_Data');
+		$minify  = $this->createMock('MatthiasMullie\Minify\CSS');
 
-        $combine = new Combine( $options, $minify );
-        $combined_url = 'combined.css';
-        $styles = [
-            [
-                '<link rel="stylesheet" href="style.css" />',
-            ],
-            [
-                '<link rel="stylesheet" href="plugin.css" />',
-            ],
-        ];
+		$combine = new Combine( $options, $minify );
+		$combined_url = 'http://example.org/wp-content/cache/min/1/combined.css';
+		$styles = [
+			[
+				'<link rel="stylesheet" href="http://example.org/wp-content/themes/storefront/style.css" />',
+			],
+		];
 
-        $original = file_get_contents( WP_ROCKET_TESTS_FIXTURES_DIR . '/Optimization/CSS/original.html');
-        $combined = file_get_contents( WP_ROCKET_TESTS_FIXTURES_DIR . '/Optimization/CSS/combined.html');
+		$original = file_get_contents( WP_ROCKET_TESTS_FIXTURES_DIR . '/Optimization/CSS/original.html');
+		$combined = file_get_contents( WP_ROCKET_TESTS_FIXTURES_DIR . '/Optimization/CSS/combined.html');
 
-        $this->assertSame(
-            $combined,
-            $combine->insert_combined_css( $original, $combined_url, $styles )
-        );
-    }
+		$this->assertSame(
+			$combined,
+			$combine->insert_combined_css( $original, $combined_url, $styles )
+		);
+	}
 }
