e4cf2118192c23ae8eb2f523362f957f57c50889
Rémy Perona <remperona@gmail.com>
2020-02-12 10:52:04 -0500

    update to define properties and rename methods

--- tests/Unit/Functions/rocketCleanCacheBusting.php
+++ tests/Unit/Functions/rocketCleanCacheBusting.php
@@ -10,27 +10,53 @@ use Brain\Monkey\Functions;
  * @group Files
  */
 class Test_RocketCleanCacheBusting extends FilesystemTestCase {
+	protected $rootVirtualDir = 'cache';
+	protected $structure = [
+		'busting' => [
+			'1' => [
+				'wp-content' => [
+					'themes' => [
+						'storefront' => [
+							'assets' => [
+								'js' => [
+									'navigation.min-2.5.3.js'    => '',
+									'navigation.min-2.5.3.js.gz' => '',
+								],
+							],
+							'style-2.5.3.css'    => '',
+							'style-2.5.3.css.gz' => '',
+						],
+					],
+				],
+			]
+		]
+	];
+
 	public function setUp() {
 		parent::setUp();
 
+		Functions\expect( 'rocket_get_constant' )
+			->once()
+			->with( 'WP_ROCKET_CACHE_BUSTING_PATH' )
+			->andReturn( 'vfs://cache/busting/' );
 		Functions\when( 'get_current_blog_id' )->justReturn( '1' );
 	}
 
-	public function testShouldCleanMinifiedCSS() {
+	public function testShouldCleanCacheBustingCSS() {
 		rocket_clean_cache_busting( 'css' );
 
 		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css' ) );
 		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css.gz' ) );
 	}
 
-	public function testShouldCleanMinifiedJS() {
+	public function testShouldCleanCacheBustingJS() {
 		rocket_clean_cache_busting( 'js' );
 
 		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
 		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/assets/js/navigation.min-2.5.3.js' ) );
 	}
 
-	public function testShouldCleanAllMinified() {
+	public function testShouldCleanAllCacheBusting() {
 		rocket_clean_cache_busting();
 
 		$this->assertFalse( $this->filesystem->exists( 'busting/1/wp-content/themes/storefront/style-2.5.3.css' ) );
