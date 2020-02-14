7f9434b01e0259dfb5bc78363441a476425abf96
Rémy Perona <remperona@gmail.com>
2020-02-14 13:36:14 -0500

    add hook callback on http_request_args

--- inc/classes/subscriber/CDN/RocketCDN/AdminPageSubscriber.php
+++ inc/classes/subscriber/CDN/RocketCDN/AdminPageSubscriber.php
@@ -62,6 +62,7 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 			'rocket_cdn_settings_fields'          => 'rocketcdn_field',
 			'admin_post_rocket_purge_rocketcdn'   => 'purge_cdn_cache',
 			'rocket_settings_page_footer'         => 'add_subscription_modal',
+			'http_request_args'                   => [ 'preserve_authorization_token', PHP_INT_MAX, 2 ],
 		];
 	}
 
@@ -193,7 +194,7 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 		set_transient( 'rocketcdn_purge_cache_response', $this->api_client->purge_cache_request(), HOUR_IN_SECONDS );
 
 		wp_safe_redirect( esc_url_raw( wp_get_referer() ) );
-		defined( 'WP_ROCKET_IS_TESTING' ) ? wp_die() : exit;
+		rocket_get_constant( 'WP_ROCKET_IS_TESTING', false ) ? wp_die() : exit;
 	}
 
 	/**
@@ -226,5 +227,20 @@ class AdminPageSubscriber extends Abstract_Render implements Subscriber_Interfac
 			</div>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Filter the arguments used in an HTTP request, to make sure our user token has not been overwritten
+	 * by some other plugin.
+	 *
+	 * @since  3.5
+	 * @author Rémy Peorna
+	 *
+	 * @param  array  $args An array of HTTP request arguments.
+	 * @param  string $url  The request URL.
+	 * @return array
+	 */
+	public function preserve_authorization_token( $args, $url ) {
+		return $this->api_client->preserve_authorization_token( $args, $url );
 	}
 }
