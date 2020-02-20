7d4d2d434bf81e7011df8bca6308f7a8f589b902
Soponar Cristina <crystinutzaa@gmail.com>
2020-02-20 09:32:37 +0200

    Added setup theme for installing WooCommerce

--- tests/Integration/bootstrap.php
+++ tests/Integration/bootstrap.php
@@ -14,8 +14,9 @@ tests_add_filter(
 	'muplugins_loaded',
 	function() {
 		// Load WooCommerce.
+		define( 'WC_TAX_ROUNDING_MODE', 'auto' );
+		define( 'WC_USE_TRANSACTIONS', false );
 		require WP_ROCKET_PLUGIN_ROOT . '/vendor/woocommerce/woocommerce/woocommerce.php';
-		\WC_Install::install();
 
 
 		// Overload the license key for testing.
@@ -23,5 +24,29 @@ tests_add_filter(
 
 		// Load the plugin.
 		require WP_ROCKET_PLUGIN_ROOT . '/wp-rocket.php';
+	}
+);
+
+
+// install WC.
+tests_add_filter(
+	'setup_theme',
+	function() {
+		// Clean existing install first.
+		define( 'WP_UNINSTALL_PLUGIN', true );
+		define( 'WC_REMOVE_ALL_DATA', true );
+		include WP_ROCKET_PLUGIN_ROOT . '/vendor/woocommerce/woocommerce/uninstall.php';
+
+		\WC_Install::install();
+
+		// Reload capabilities after install, see https://core.trac.wordpress.org/ticket/28374.
+		if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
+			$GLOBALS['wp_roles']->reinit();
+		} else {
+			$GLOBALS['wp_roles'] = null; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+			wp_roles();
+		}
+
+		echo esc_html( 'Installing WooCommerce...' . PHP_EOL );
 	}
 );
