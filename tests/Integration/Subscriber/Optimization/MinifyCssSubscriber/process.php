3fd4d45b02366819abc1a2ca80c31c08d4717f9e
Rémy Perona <remperona@gmail.com>
2020-02-24 08:27:38 -0500

    remove filters after test

--- tests/Integration/Subscriber/Optimization/MinifyCssSubscriber/process.php
+++ tests/Integration/Subscriber/Optimization/MinifyCssSubscriber/process.php
@@ -33,11 +33,12 @@ class Test_Process extends FilesystemTestCase {
 
 		add_filter( 'pre_get_rocket_option_minify_css', '__return_true' );
 		add_filter( 'pre_get_rocket_option_minify_concatenate_css', '__return_true' );
-		add_filter( 'pre_get_rocket_option_minify_css_key', function() {
-			return '123456';
-		} );
 
-		$callback = function( $file, $url ) {
+		$key_callback = function() {
+			return '123456';
+		};
+
+		$path_callback = function( $file, $url ) {
 			if ( '//example.org/wp-content/themes/storefront/style.css' === $url ) {
 				return 'vfs://wp-content/themes/storefront/style.css';
 			}
@@ -45,13 +46,19 @@ class Test_Process extends FilesystemTestCase {
 			return $file;
 		};
 
-		add_filter( 'rocket_url_to_path', $callback, 10, 2 );
+		add_filter( 'pre_get_rocket_option_minify_css_key', $key_callback );
+		add_filter( 'rocket_url_to_path', $path_callback, 10, 2 );
 
 		$html = file_get_contents( WP_ROCKET_TESTS_FIXTURES_DIR . '/Optimization/CSS/original.html');
 
 		apply_filters( 'rocket_buffer', $html );
 
 		$this->assertTrue( $this->filesystem->exists( 'cache/min/1/3e1ae3552abbc9a53ab11fc7766f5559.css' ) );
-        $this->assertTrue( $this->filesystem->exists( 'cache/min/1/3e1ae3552abbc9a53ab11fc7766f5559.css.gz' ) );
+		$this->assertTrue( $this->filesystem->exists( 'cache/min/1/3e1ae3552abbc9a53ab11fc7766f5559.css.gz' ) );
+
+		remove_filter( 'pre_get_rocket_option_minify_css', '__return_true' );
+		remove_filter( 'pre_get_rocket_option_minify_concatenate_css', '__return_true' );
+		remove_filter( 'pre_get_rocket_option_minify_css_key', $key_callback );
+		remove_filter( 'rocket_url_to_path', $path_callback, 10, 2 );
 	}
 }
