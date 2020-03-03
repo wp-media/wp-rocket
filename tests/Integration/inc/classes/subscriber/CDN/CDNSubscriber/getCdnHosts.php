ebdda1518eb8054ce076660e0d98f62c5797988e
Rémy Perona <remperona@gmail.com>
2020-03-03 16:48:06 -0500

    update test case to add more tests

--- tests/Integration/inc/classes/subscriber/CDN/CDNSubscriber/getCdnHosts.php
+++ tests/Integration/inc/classes/subscriber/CDN/CDNSubscriber/getCdnHosts.php
@@ -7,24 +7,112 @@ namespace WP_Rocket\Tests\Integration\inc\classes\subscriber\CDN\CDNSubscriber;
  * @uses   \WP_Rocket\CDN\CDN::get_cdn_urls
  * @uses   ::rocket_add_url_protocol
  * @group  Subscriber
+ * @group  CDN
  */
 class Test_GetCdnHosts extends TestCase {
+	public function tearDown() {
+		delete_option( 'wp_rocket_settings' );
 
-	public function testShouldReturnCDNHosts() {
-		update_option(
-			'wp_rocket_settings',
-			[
-				'cdn'              => '1',
-				'cdn_cnames'       => [ 'cdn.example.org' ],
-				'cdn_zone'         => [ 'all' ],
-				'cdn_reject_files' => [],
-			]
+		parent::tearDown();
+	}
+
+	public function testShouldReturnOriginalArrayWhenNoCDNURL() {
+		$this->assertSame(
+			[],
+			apply_filters( 'rocket_cdn_hosts', [], [ 'all' ] )
 		);
-		$hosts = [ 'example.org' ];
+	}
+
+	public function testShouldReturnCDNHostsWhenExists() {
+		$callback = function() {
+			return [
+				'http://cdn.example.org',
+				'//cdn2.example.org',
+				'https://cdn3.example.org',
+				'cdn4.example.org',
+			];
+		};
+
+		add_filter( 'rocket_cdn_cnames', $callback );
 
 		$this->assertSame(
-			[ 'example.org', 'cdn.example.org' ],
-			$this->getSubscriberInstance()->get_cdn_hosts( $hosts, 'all' )
+			[
+				'cdn.example.org',
+				'cdn2.example.org',
+				'cdn3.example.org',
+				'cdn4.example.org',
+			],
+			apply_filters( 'rocket_cdn_hosts', [], [ 'all' ] )
 		);
+
+		remove_filter( 'rocket_cdn_cnames', $callback );
+	}
+
+	public function testShouldReturnCDNHostsWhenExistsAndOriginalHasValue() {
+		$callback = function( $hosts ) {
+			return array_merge( $hosts, [
+				'http://cdn.example.org',
+				'//cdn2.example.org',
+				'https://cdn3.example.org',
+				'cdn4.example.org',
+			] );
+		};
+
+		add_filter( 'rocket_cdn_cnames', $callback );
+
+		$this->assertSame(
+			[
+				'cdn5.example.org',
+				'cdn.example.org',
+				'cdn2.example.org',
+				'cdn3.example.org',
+				'cdn4.example.org',
+			],
+			apply_filters( 'rocket_cdn_hosts', [ 'cdn5.example.org' ], [ 'all' ] )
+		);
+
+		remove_filter( 'rocket_cdn_cnames', $callback );
+	}
+
+	public function testShouldReturnCDNHostsWhenCDNHasPath() {
+		$callback = function() {
+			return [
+				'http://cdn.example.org/path',
+				'//cdn2.example.org'
+			];
+		};
+
+		add_filter( 'rocket_cdn_cnames', $callback );
+
+		$this->assertSame(
+			[
+				'cdn.example.org',
+				'cdn2.example.org'
+			],
+			apply_filters( 'rocket_cdn_hosts', [], [ 'all' ] )
+		);
+
+		remove_filter( 'rocket_cdn_cnames', $callback );
+	}
+
+	public function testShouldReturnSpecificCDNHostsWhenSpecificZone() {
+		$callback = function() {
+			return [
+				'http://cdn.example.org/path',
+				'//cdn2.example.org'
+			];
+		};
+
+		add_filter( 'rocket_cdn_cnames', $callback );
+
+		$this->assertSame(
+			[
+				'cdn.example.org',
+				'cdn2.example.org'
+			],
+			apply_filters( 'rocket_cdn_hosts', [], [ 'CSS', 'all' ] )
+		);
+
+		remove_filter( 'rocket_cdn_cnames', $callback );
 	}
 }
