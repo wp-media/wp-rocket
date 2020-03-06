7e94992744a5d29e2630e38ce47dfc9230b824ee
Git I Hate You <git@screenfeed.fr>
2020-03-06 01:02:25 +0100

    Make the user agent used to preload mobile pages detected as mobile by wp_is_mobile() Use a more complex user agent, closer to a real one, that uses `iPhone` (for WPR mobile detect) and `Mobile` (for `wp_is_mobile()`). Also, this new string is prepended to the custom user agent instead of being happened.

--- inc/classes/preload/class-abstract-process.php
+++ inc/classes/preload/class-abstract-process.php
@@ -99,10 +99,38 @@ abstract class Process extends WP_Background_Process {
 	 */
 	public function get_item_user_agent( array $item ) {
 		if ( $item['mobile'] ) {
-			return 'WP Rocket/Preload iPhone';
+			return $this->get_mobile_user_agent_prefix() . ' WP Rocket/Preload';
 		}
 
 		return 'WP Rocket/Preload';
+	}
+
+	/**
+	 * Get the prefix to prepend to the user agent used for preload to make a HTTP request detected as a mobile device.
+	 *
+	 * @since  3.5.0.2
+	 * @author Grégory Viguier
+	 *
+	 * @return string
+	 */
+	public function get_mobile_user_agent_prefix() {
+		$prefix = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+
+		/**
+		 * Filter the prefix to prepend to the user agent used for preload to make a HTTP request detected as a mobile device.
+		 *
+		 * @since  3.5.0.2
+		 * @author Grégory Viguier
+		 *
+		 * @param string $prefix The prefix.
+		 */
+		$new_prefix = apply_filters( 'rocket_mobile_preload_user_agent_prefix', $prefix );
+
+		if ( empty( $new_prefix ) || ! is_string( $new_prefix ) ) {
+			return $prefix;
+		}
+
+		return $new_prefix;
 	}
 
 	/**
