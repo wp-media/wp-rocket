ab009fe1fa17f4615358f51c8f763c717eaac625
Rémy Perona <remperona@gmail.com>
2020-03-09 17:13:18 -0400

    add fixtures for rocket_url_to_path() testing

---
+++ tests/Fixtures/inc/functions/rocket-url-to-path-cdn-subdir.php
@@ -0,0 +1,51 @@
+<?php
+return [
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-includes/js/jquery/jquery.js',
+		'vfs://public_html/wordpress/wp-includes/js/jquery/jquery.js',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-includes/css/dashicons.min.css',
+		'vfs://public_html/wordpress/wp-includes/css/dashicons.min.css',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-content/themes/twentytwenty/style.css',
+		'vfs://public_html/wordpress/wp-content/themes/twentytwenty/style.css',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-content/themes/twentytwenty/assets/script.js',
+		'vfs://public_html/wordpress/wp-content/themes/twentytwenty/assets/script.js',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-content/plugins/hello-dolly/style.css',
+		'vfs://public_html/wordpress/wp-content/plugins/hello-dolly/style.css',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+	[
+		'https://123456.rocketcdn.me/wordpress/wp-content/plugins/hello-dolly/script.js',
+		'vfs://public_html/wordpress/wp-content/plugins/hello-dolly/script.js',
+		[
+			'home' => 0,
+			'123456.rocketcdn.me' => 1,
+		],
+	],
+];
