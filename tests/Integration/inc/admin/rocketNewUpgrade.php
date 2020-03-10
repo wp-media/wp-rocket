d5ed096828e32b79127116fb53617e077f62f907
Rémy Perona <remperona@gmail.com>
2020-03-10 11:27:19 -0400

    add unit & integration test for new update

---
+++ tests/Integration/inc/admin/rocketNewUpgrade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\Inc\admin;
+
+use Brain\Monkey\Functions;
+use WPMedia\PHPUnit\Integration\TestCase;
+
+/**
+ * @covers ::rocket_new_upgrade
+ * @group admin
+ * @group upgrade
+ * @group AdminOnly
+ */
+class Test_RocketNewUpgrade extends TestCase {
+	public function testShouldRegenerateAdvancedCacheFile() {
+		Functions\expect( 'rocket_generate_advanced_cache_file' )
+			->once();
+
+		do_action( 'wp_rocket_upgrade', '3.5.1', '3.4.4' );
+	}
+}
