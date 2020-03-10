d09782795ca0e874f7652b4af8eddc28a445c847
Rémy Perona <remperona@gmail.com>
2020-03-10 16:32:00 -0400

    load formatting.php before unit tests

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -15,10 +15,11 @@ define( 'WP_ROCKET_IS_TESTING', true );
  */
 function load_original_functions_before_mocking() {
 	$originals = [
-		'rocket_get_constant'      => WP_ROCKET_PLUGIN_ROOT . 'inc/constants.php',
-		'rocket_is_live_site'      => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/api.php',
-		'rocket_direct_filesystem' => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/files.php',
-		'get_rocket_option'        => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/options.php',
+		'rocket_get_constant'       => WP_ROCKET_PLUGIN_ROOT . 'inc/constants.php',
+		'rocket_is_live_site'       => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/api.php',
+		'rocket_direct_filesystem'  => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/files.php',
+		'get_rocket_option'         => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/options.php',
+		'rocket_clean_exclude_file' => WP_ROCKET_PLUGIN_ROOT . 'inc/functions/formatting.php'
 	];
 
 	foreach ( $originals as $function_name => $file ) {
