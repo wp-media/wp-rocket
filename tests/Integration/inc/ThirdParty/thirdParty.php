26743c842ad4eb3003e3a000d744fe92fbb74680
Tonya Mork <hellofromtonya@knowthecode.io>
2020-03-12 18:42:36 -0500

    Adds partial integration tests for third party host file loaders.

---
+++ tests/Integration/inc/ThirdParty/thirdParty.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\ThirdParty;
+
+use WPMedia\PHPUnit\Integration\TestCase;
+
+/**
+ * @group ThirdParty
+ */
+class Test_ThirdParty extends TestCase {
+	private static $included_files;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		self::$included_files = get_included_files();
+	}
+
+	public function testShouldNotLoadHostingFilesWhenNotPresent() {
+		$this->assertNotContains( WP_ROCKET_3RD_PARTY_PATH . 'hosting/godaddy.php', self::$included_files );
+		$this->assertNotContains( WP_ROCKET_3RD_PARTY_PATH . 'hosting/wpengine.php', self::$included_files );
+		$this->assertNotContains( WP_ROCKET_3RD_PARTY_PATH . 'hosting/o2switch.php', self::$included_files );
+		$this->assertNotContains( WP_ROCKET_3RD_PARTY_PATH . 'hosting/flywheel.php', self::$included_files );
+		$this->assertNotContains( WP_ROCKET_3RD_PARTY_PATH . 'hosting/siteground.php', self::$included_files );
+	}
+}
