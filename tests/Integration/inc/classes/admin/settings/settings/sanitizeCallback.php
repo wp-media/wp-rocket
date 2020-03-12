e17f1b3192872d6dc1709d0d73b4cfe1fc12b14e
Soponar Cristina <crystinutzaa@gmail.com>
2020-03-12 14:53:18 +0200

    Added remove admin_init action send_frame_options_header

--- tests/Integration/inc/classes/admin/settings/settings/sanitizeCallback.php
+++ tests/Integration/inc/classes/admin/settings/settings/sanitizeCallback.php
@@ -8,7 +8,7 @@ use WPMedia\PHPUnit\Integration\TestCase;
 
 /**
  * @covers \WP_Rocket\Admin\Settings::sanitize_callback
- * @group  AdminOnly
+ * @group  Admin
  * @group  AdminSettings
  */
 class Test_SanitizeCallback extends TestCase {
@@ -16,7 +16,9 @@ class Test_SanitizeCallback extends TestCase {
 	 * @dataProvider addDataProvider
 	 */
 	public function testShouldSanitize( $original, $sanitized ) {
+		remove_action( 'admin_init', 'send_frame_options_header' );
 		do_action('admin_init');
+
 		$sanitize_callback = apply_filters( 'sanitize_option_wp_rocket_settings', $original );
 
 		$this->assertSame(
