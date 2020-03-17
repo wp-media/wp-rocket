c2df1c025fe514651689aefa9a2025633a8ac255
Rémy Perona <remperona@gmail.com>
2020-03-17 17:10:28 -0400

    update fixtures for remove query strings

--- tests/Fixtures/inc/classes/optimization/Remove_Query_String/css/remove-query-strings.php
+++ tests/Fixtures/inc/classes/optimization/Remove_Query_String/css/remove-query-strings.php
@@ -1,7 +1,7 @@
 <?php
 return [
+	// Styles are commented in HTML comments = ignored.
 	[
-		// Styles are commented in HTML comments = ignored.
 		'<html>
 			<head>
 				<title>Page title</title>
@@ -24,9 +24,11 @@ return [
 				<script src="http://example.org/wp-content/plugins/hello-dolly/script.js?ver=3.5"></script>
 			</body>
 		</html>',
+		[],
+		'http://example.org',
 	],
+	// Default domain, CSS files are cached busted. Files with the WordPress version in the query get the ?ver= removed.
 	[
-		// Default domain, CSS files are cached busted. Files with the WordPress version in the query get the ?ver= removed.
 		'<html>
 			<head>
 				<title>Page title</title>
@@ -55,5 +57,42 @@ return [
 				<script src="http://example.org/wp-content/plugins/hello-dolly/script.js?ver=3.5"></script>
 			</body>
 		</html>',
+		[],
+		'http://example.org',
+	],
+	// CDN URL, CSS files are cached busted. Files with the WordPress version in the query get the ?ver= removed.
+	[
+		'<html>
+			<head>
+				<title>Page title</title>
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-includes/css/dashicons.min.css?ver=5.3">
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-content/themes/twentytwenty/style.css?ver=1.0">
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-content/plugins/hello-dolly/style.css?ver=3.5">
+				<link rel="stylesheet" href="https://google.com/external.css?v=20200306">
+				<script src="https://123456.rocketcdn.me/wp-includes/js/jquery/jquery.js?ver=5.3"></script>
+				<script src="https://123456.rocketcdn.me/wp-content/themes/twentytwenty/assets/script.js?ver=1.0"></script>
+			</head>
+			<body>
+				<script src="https://123456.rocketcdn.me/wp-content/plugins/hello-dolly/script.js?ver=3.5"></script>
+			</body>
+		</html>',
+		'<html>
+			<head>
+				<title>Page title</title>
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-includes/css/dashicons.min.css">
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-content/cache/busting/1/wp-content/themes/twentytwenty/style-1.0.css">
+				<link rel="stylesheet" href="https://123456.rocketcdn.me/wp-content/cache/busting/1/wp-content/plugins/hello-dolly/style-3.5.css">
+				<link rel="stylesheet" href="https://google.com/external.css?v=20200306">
+				<script src="https://123456.rocketcdn.me/wp-includes/js/jquery/jquery.js?ver=5.3"></script>
+				<script src="https://123456.rocketcdn.me/wp-content/themes/twentytwenty/assets/script.js?ver=1.0"></script>
+			</head>
+			<body>
+				<script src="https://123456.rocketcdn.me/wp-content/plugins/hello-dolly/script.js?ver=3.5"></script>
+			</body>
+		</html>',
+		[
+			'123456.rocketcdn.me',
+		],
+		'https://123456.rocketcdn.me',
 	],
 ];
