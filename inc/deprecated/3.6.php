f312bf28498769e7c9bc9eb68277698524cfe7e6
Rémy Perona <remperona@gmail.com>
2020-03-19 15:42:47 -0400

    deprecate rocket_exclude_busting_uncode function

--- inc/deprecated/3.6.php
+++ inc/deprecated/3.6.php
@@ -17,3 +17,31 @@ function rocket_exclude_avada_dynamic_css( $excluded_files ) {
 
     return $excluded_files;
 }
+
+/**
+ * Excludes Uncode JS files from remove query strings
+ *
+ * @deprecated 3.6
+ * @since 3.3.3
+ * @author Remy Perona
+ *
+ * @param array $exclude_busting Array of CSS and JS filepaths to be excluded.
+ * @return array
+ */
+function rocket_exclude_busting_uncode( $exclude_busting ) {
+    _deprecated_function( __FUNCTION__ . '()', '3.6' );
+
+    // CSS files.
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/css/style.css' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/css/uncode-icons.css' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/css/style-custom.css' );
+
+    // JS files.
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/init.js' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/min/init.min.js' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/app.js' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/app.min.js' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/plugins.js' );
+    $exclude_busting[] = rocket_clean_exclude_file( get_template_directory_uri() . '/library/js/plugins.min.js' );
+    return $exclude_busting;
+}
