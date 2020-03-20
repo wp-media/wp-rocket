86531191dbc901076c7cb652b0b54699f2e64ec0
Git I Hate You <git@screenfeed.fr>
2020-03-20 15:24:05 +0100

    Improve and DRY unit tests

--- tests/Unit/inc/Engine/Preload/PartialPreloadSubscriber/maybeDispatch.php
+++ tests/Unit/inc/Engine/Preload/PartialPreloadSubscriber/maybeDispatch.php
@@ -12,6 +12,12 @@ use WP_Rocket\Engine\Preload\PartialProcess;
  * @group  Preload
  */
 class Test_MaybePreloadMobileHomepage extends TestCase {
+	private $queue = [];
+
+	protected function tearDown() {
+		parent::tearDown();
+		$this->queue = [];
+	}
 
 	public function testShouldNotDispatchWhenNoUrls() {
 		$options         = $this->createMock( Options_Data::class );
@@ -26,102 +32,29 @@ class Test_MaybePreloadMobileHomepage extends TestCase {
 		$subscriber = new PartialPreloadSubscriber( $partial_process, $options );
 
 		$subscriber->maybe_dispatch();
+
+		$this->assertEmpty( $this->queue );
 	}
 
 	public function testShouldDispatchWhenUrlsAndNoMobilePreload() {
-		$queue           = [];
-		$urls            = [
+		$urls = [
 			'https://example.org/',
 			'https://example.org/test/',
 		];
-		$options         = $this->createMock( Options_Data::class );
-		$partial_process = $this->getMockBuilder( PartialProcess::class )
-		                        ->setMethods( [ 'is_mobile_preload_enabled', 'push_to_queue', 'save', 'dispatch' ] )
-		                        ->getMock();
-		$partial_process
-			->expects( $this->once() )
-			->method( 'is_mobile_preload_enabled' )
-			->willReturn( false );
-		$partial_process
-			->expects( $this->exactly( 2 ) )
-			->method( 'push_to_queue' )
-			->will( $this->returnCallback( function( $item ) use ( &$queue ) {
-				$queue[] = $item;
-			} ) );
-		$partial_process
-			->expects( $this->once() )
-			->method( 'save' )
-			->willReturnSelf();
-		$partial_process
-			->expects( $this->once() )
-			->method( 'dispatch' )
-			->willReturn( null );
 
-		Functions\when( 'wp_doing_ajax' )
-			->justReturn( false );
+		$this->getSubscriber( $urls, false )->maybe_dispatch();
 
-		Functions\when( 'wp_parse_url' )->alias( function( $url, $component = -1 ) {
-			return parse_url( $url, $component );
-		} );
-
-		Functions\when( 'get_rocket_cache_reject_uri' )
-			->justReturn( '/foo/|/bar/|/(?:.+/)?embed/' );
-
-		$subscriber = new PartialPreloadSubscriber( $partial_process, $options );
-
-		$this->set_reflective_property( $urls, 'urls', $subscriber );
-
-		$subscriber->maybe_dispatch();
-
-		$this->assertSame( $urls, $queue );
-		$this->assertCount( 2, $queue );
+		foreach ( $urls as $url ) {
+			$this->assertContains( $url, $this->queue );
+		}
+		$this->assertCount( 2, $this->queue );
 	}
 
 	public function testShouldDispatchWhenUrlsAndMobilePreload() {
-		$queue           = [];
-		$urls            = [
+		$urls     = [
 			'https://example.org/',
 			'https://example.org/test/',
 		];
-		$options         = $this->createMock( Options_Data::class );
-		$partial_process = $this->getMockBuilder( PartialProcess::class )
-		                        ->setMethods( [ 'is_mobile_preload_enabled', 'push_to_queue', 'save', 'dispatch' ] )
-		                        ->getMock();
-		$partial_process
-			->expects( $this->once() )
-			->method( 'is_mobile_preload_enabled' )
-			->willReturn( true );
-		$partial_process
-			->expects( $this->exactly( 4 ) )
-			->method( 'push_to_queue' )
-			->will( $this->returnCallback( function( $item ) use ( &$queue ) {
-				$queue[] = $item;
-			} ) );
-		$partial_process
-			->expects( $this->once() )
-			->method( 'save' )
-			->willReturnSelf();
-		$partial_process
-			->expects( $this->once() )
-			->method( 'dispatch' )
-			->willReturn( null );
-
-		Functions\when( 'wp_doing_ajax' )
-			->justReturn( false );
-
-		Functions\when( 'wp_parse_url' )->alias( function( $url, $component = -1 ) {
-			return parse_url( $url, $component );
-		} );
-
-		Functions\when( 'get_rocket_cache_reject_uri' )
-			->justReturn( '/foo/|/bar/|/(?:.+/)?embed/' );
-
-		$subscriber = new PartialPreloadSubscriber( $partial_process, $options );
-
-		$this->set_reflective_property( $urls, 'urls', $subscriber );
-
-		$subscriber->maybe_dispatch();
-
 		$expected = [
 			'https://example.org/',
 			[ 'url' => 'https://example.org/', 'mobile' => true ],
@@ -129,7 +62,54 @@ class Test_MaybePreloadMobileHomepage extends TestCase {
 			[ 'url' => 'https://example.org/test/', 'mobile' => true ],
 		];
 
-		$this->assertSame( $expected, $queue );
-		$this->assertCount( 4, $queue );
+		$this->getSubscriber( $urls, true )->maybe_dispatch();
+
+		foreach ( $expected as $url ) {
+			$this->assertContains( $url, $this->queue );
+		}
+		$this->assertCount( 4, $this->queue );
+	}
+
+	private function getSubscriber( array $urls, $mobile_preload_enabled ) {
+		$nbr_items_in_queue = $mobile_preload_enabled ? 4 : 2;
+		$this->queue        = [];
+		$options            = $this->createMock( Options_Data::class );
+		$partial_process    = $this->getMockBuilder( PartialProcess::class )
+		                           ->setMethods( [ 'is_mobile_preload_enabled', 'push_to_queue', 'save', 'dispatch' ] )
+		                           ->getMock();
+		$partial_process
+			->expects( $this->once() )
+			->method( 'is_mobile_preload_enabled' )
+			->willReturn( $mobile_preload_enabled );
+		$partial_process
+			->expects( $this->exactly( $nbr_items_in_queue ) )
+			->method( 'push_to_queue' )
+			->will( $this->returnCallback( function( $item ) {
+				$this->queue[] = $item;
+			} ) );
+		$partial_process
+			->expects( $this->once() )
+			->method( 'save' )
+			->willReturnSelf();
+		$partial_process
+			->expects( $this->once() )
+			->method( 'dispatch' )
+			->willReturn( null );
+
+		Functions\when( 'wp_doing_ajax' )
+			->justReturn( false );
+
+		Functions\when( 'wp_parse_url' )->alias( function( $url, $component = -1 ) {
+			return parse_url( $url, $component );
+		} );
+
+		Functions\when( 'get_rocket_cache_reject_uri' )
+			->justReturn( '/foo/|/bar/|/(?:.+/)?embed/' );
+
+		$subscriber = new PartialPreloadSubscriber( $partial_process, $options );
+
+		$this->set_reflective_property( $urls, 'urls', $subscriber );
+
+		return $subscriber;
 	}
 }
