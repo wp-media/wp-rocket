3b4a5c8a9cc09fabf2bbf8276e2fbe077f2c5d21
Soponar Cristina <crystinutzaa@gmail.com>
2020-03-24 17:24:47 +0200

    Fixed Unit and Integration tests ( + changed Path from inc/classes/third-party/plugins to /inc/classes/ThirdParty/Plugins)

--- tests/Integration/inc/classes/ThirdParty/Plugins/Optimization/AMP/isAmpCompatibleCallback.php
+++ tests/Integration/inc/classes/ThirdParty/Plugins/Optimization/AMP/isAmpCompatibleCallback.php
@@ -2,6 +2,8 @@
 
 namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins\Optimization\AMP;
 
+use Brain\Monkey\Functions;
+
 /**
  * @covers \WP_Rocket\ThirdParty\Plugins\Optimization\AMP::is_amp_compatible_callback
  * @group ThirdParty
@@ -9,16 +11,19 @@ namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins\Optimization\AMP;
  */
 class Test_IsAmpCompatibleCallback extends TestCase {
 	public function testShouldAddAmpWhenThemeSupportIsTransitional() {
+		Functions\expect( 'rocket_generate_config_file' )->once();
 		$this->setSettings( 'theme_support', 'transitional' );
 		$this->assertContains( 'amp', apply_filters( 'rocket_cache_query_strings', [] ) );
 	}
 
 	public function testShouldNotAddAmpWhenThemeSupportIsNotTransitional() {
+		Functions\expect( 'rocket_generate_config_file' )->once();
 		$this->setSettings( 'theme_support', 'standard' );
 		$this->assertEquals( [], apply_filters( 'rocket_cache_query_strings', [] ) );
 	}
 
 	public function testShouldNotAddAmpWhenThemeSupportIsNotSet() {
+		Functions\expect( 'rocket_generate_config_file' )->once();
 		$this->setSettings( 'theme_support', null );
 		$this->assertEquals( [], apply_filters( 'rocket_cache_query_strings', [] ) );
 	}
