b233e0c3808b4136109c9b95e024d6083194333c
hellofromtonya <hellofromtonya@knowthecode.io>
2020-03-24 18:15:21 -0500

    Wires delete_licence_data-filed to the new FilesystemTestCase.

---
+++ tests/Fixtures/inc/functions/rocketDeleteLicenceDataFile.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'vfs_dir'   => 'plugins/wp-rocket/',
+
+	// Virtual filesystem structure.
+	'structure' => [
+		'plugins' => [
+			'wp-rocket' => [
+				'licence-data.php' => '',
+			],
+		],
+	],
+
+	// Test data.
+	'test_data' => [],
+];
