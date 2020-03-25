17739b5ec01a9c81159965e0ccbbc297f367d950
Rémy Perona <remperona@gmail.com>
2020-03-25 14:27:06 -0400

    import AMP_Options_Manager

--- tests/Integration/inc/ThirdParty/Plugins/Optimization/AMP/TestCase.php
+++ tests/Integration/inc/ThirdParty/Plugins/Optimization/AMP/TestCase.php
@@ -2,10 +2,11 @@
 
 namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins\Optimization\AMP;
 
+use AMP_Options_Manager;
 use WPMedia\PHPUnit\Integration\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
 	protected function setSettings( $option, $value ) {
-		\AMP_Options_Manager::update_option( $option, $value);
+		AMP_Options_Manager::update_option( $option, $value);
 	}
 }
