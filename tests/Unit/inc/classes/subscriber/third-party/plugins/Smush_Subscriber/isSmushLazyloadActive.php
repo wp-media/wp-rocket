b8fdcb278e506df140d8d00bacb04539663a1f56
Git I Hate You <git@screenfeed.fr>
2020-03-25 02:46:50 +0100

    Use a data provider for the unit tests

--- tests/Unit/inc/classes/subscriber/third-party/plugins/Smush_Subscriber/isSmushLazyloadActive.php
+++ tests/Unit/inc/classes/subscriber/third-party/plugins/Smush_Subscriber/isSmushLazyloadActive.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WP_Rocket\Tests\Unit\inc\classes\third_party\plugins\Smush_Subscriber;
 
 use WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber;
@@ -9,110 +10,35 @@ use WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber;
  * @group Smush
  */
 class Test_IsSmushLazyloadActive extends SmushSubscriberTestCase {
-
-	public function testShouldNotDisableWPRocketLazyLoad() {
+	/**
+	 * @dataProvider addDataProviderThatShouldNotDisableWPRocketLazyLoad
+	 */
+	public function testShouldNotDisableWPRocketLazyLoad( $lazyload_enabled, array $lazyload_formats ) {
 		$subscriber = new Smush_Subscriber( $this->createMock( 'WP_Rocket\Admin\Options' ), $this->createMock( 'WP_Rocket\Admin\Options_Data' ) );
 
-		// Disabled.
-		$this->mock_is_smush_lazyload_enabled(
-			false,
-			[
-				'jpeg'   => true,
-				'png'    => true,
-				'gif'    => true,
-				'svg'    => true,
-				'iframe' => true,
-			]
-		);
-
-		$this->assertNotContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
-
-		// No image formats.
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => false,
-				'png'    => false,
-				'gif'    => false,
-				'svg'    => false,
-				'foo'    => true,
-				'iframe' => true,
-			]
-		);
-
-		$this->assertNotContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
-
-		// Empty formats.
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[]
-		);
+		$this->mock_is_smush_lazyload_enabled( $lazyload_enabled, $lazyload_formats );
 
 		$this->assertNotContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
 	}
 
-	public function testShouldDisableWPRocketLazyLoadWhenAtLeastOneImageFormat() {
+	/**
+	 * @dataProvider addDataProviderThatShouldDisableWPRocketLazyLoad
+	 */
+	public function testShouldDisableWPRocketLazyLoadWhenAtLeastOneImageFormat( $lazyload_enabled, array $lazyload_formats ) {
 		$this->mockCommonWpFunctions();
 
 		$subscriber = new Smush_Subscriber( $this->createMock( 'WP_Rocket\Admin\Options' ), $this->createMock( 'WP_Rocket\Admin\Options_Data' ) );
 
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => true,
-				'png'    => false,
-				'gif'    => false,
-				'svg'    => false,
-				'foo'    => false,
-				'iframe' => false,
-			]
-		);
+		$this->mock_is_smush_lazyload_enabled( $lazyload_enabled, $lazyload_formats );
 
 		$this->assertContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
+	}
 
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg' => false,
-				'png'  => true,
-				'gif'  => true,
-				'svg'  => false,
-			]
-		);
+	public function addDataProviderThatShouldNotDisableWPRocketLazyLoad() {
+		return $this->getTestData( __DIR__, 'isSmushLazyloadActiveNotDisable' );
+	}
 
-		$this->assertContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
-
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg' => true,
-				'png'  => true,
-				'gif'  => true,
-				'svg'  => false,
-			]
-		);
-
-		$this->assertContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
-
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg' => true,
-				'png'  => true,
-				'gif'  => true,
-				'svg'  => true,
-			]
-		);
-
-		$this->assertContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
-
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'png' => true,
-			]
-		);
-
-		$this->assertContains( 'Smush', $subscriber->is_smush_lazyload_active( [] ) );
+	public function addDataProviderThatShouldDisableWPRocketLazyLoad() {
+		return $this->getTestData( __DIR__, 'isSmushLazyloadActiveDisable' );
 	}
 }
--- tests/Unit/inc/classes/subscriber/third-party/plugins/Smush_Subscriber/maybeDeactivateRocketLazyload.php
+++ tests/Unit/inc/classes/subscriber/third-party/plugins/Smush_Subscriber/maybeDeactivateRocketLazyload.php
@@ -1,7 +1,7 @@
 <?php
+
 namespace WP_Rocket\Tests\Unit\inc\classes\third_party\plugins\Smush_Subscriber;
 
-//use Brain\Monkey\Functions;
 use Mockery;
 use WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber;
 
@@ -13,74 +13,30 @@ use WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber;
 class Test_MaybeDeactivateRocketLazyload extends SmushSubscriberTestCase {
 	private $subscriber;
 
-	public function testShouldNotDisableWPRocketLazyLoad() {
-		// Smush not enabled, WPR enabled.
-		$this->mock_is_smush_lazyload_enabled(
-			false,
-			[]
-		);
+	/**
+	 * @dataProvider addDataProviderThatShouldNotDisableWPRocketLazyLoad
+	 */
+	public function testShouldNotDisableWPRocketLazyLoad( $lazyload_enabled, array $lazyload_formats, array $rocketSettings ) {
 
-		$this->setSubscriber( true, true, false, false );
+		$this->mock_is_smush_lazyload_enabled( $lazyload_enabled, $lazyload_formats );
 
-		$this->subscriber->maybe_deactivate_rocket_lazyload();
-
-		// Smush enabled, WPR not enabled.
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => true,
-				'iframe' => true,
-			]
-		);
-
-		$this->setSubscriber( false, false, false, false );
+		call_user_func_array( [ $this, 'setAssertsAndSubscriber' ], $rocketSettings );
 
 		$this->subscriber->maybe_deactivate_rocket_lazyload();
 	}
 
-	public function testShouldDisableWPRocketLazyLoadForImagesWhenSmushLazyLoadForImagesIsEnabled() {
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => true,
-				'iframe' => false,
-			]
-		);
+	/**
+	 * @dataProvider addDataProviderThatShouldDisableWPRocketLazyLoad
+	 */
+	public function testShouldDisableWPRocketLazyLoad( $lazyload_enabled, array $lazyload_formats, array $rocketSettings ) {
+		$this->mock_is_smush_lazyload_enabled( $lazyload_enabled, $lazyload_formats );
 
-		$this->setSubscriber( true, true, true, false );
+		call_user_func_array( [ $this, 'setAssertsAndSubscriber' ], $rocketSettings );
 
 		$this->subscriber->maybe_deactivate_rocket_lazyload();
 	}
 
-	public function testShouldDisableWPRocketLazyLoadForIframesWhenSmushLazyLoadForIframesIsEnabled() {
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => false,
-				'iframe' => true,
-			]
-		);
-
-		$this->setSubscriber( true, true, false, true );
-
-		$this->subscriber->maybe_deactivate_rocket_lazyload();
-	}
-
-	public function testShouldDisableWPRocketBothLazyLoadWhenSmushLazyLoadForImagesAndIframesIsEnabled() {
-		$this->mock_is_smush_lazyload_enabled(
-			true,
-			[
-				'jpeg'   => true,
-				'iframe' => true,
-			]
-		);
-
-		$this->setSubscriber( true, true, true, true );
-
-		$this->subscriber->maybe_deactivate_rocket_lazyload();
-	}
-
-	private function setSubscriber( $getLazyload, $getLazyloadIframes, $setLazyload, $setLazyloadIframes ) {
+	private function setAssertsAndSubscriber( $getLazyload, $getLazyloadIframes, $setLazyload, $setLazyloadIframes ) {
 		$options      = Mockery::mock( 'WP_Rocket\Admin\Options' );
 		$options_data = Mockery::mock( 'WP_Rocket\Admin\Options_Data' );
 		$options_data
@@ -119,5 +75,13 @@ class Test_MaybeDeactivateRocketLazyload extends SmushSubscriberTestCase {
 			->times( $setCount );
 
 		$this->subscriber = new Smush_Subscriber( $options, $options_data );
+	}
+
+	public function addDataProviderThatShouldNotDisableWPRocketLazyLoad() {
+		return $this->getTestData( __DIR__, 'maybeDeactivateRocketLazyloadNotDisable' );
+	}
+
+	public function addDataProviderThatShouldDisableWPRocketLazyLoad() {
+		return $this->getTestData( __DIR__, 'maybeDeactivateRocketLazyloadDisable' );
 	}
 }
