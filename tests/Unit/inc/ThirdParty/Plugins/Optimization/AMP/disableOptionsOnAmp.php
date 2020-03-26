6324fffc0aa36cceb00c318f7038c9002733685d
hellofromtonya <hellofromtonya@knowthecode.io>
2020-03-26 15:46:42 -0500

    Improves the unit tests to use BrainMonkey's filter mocks and expects.

--- tests/Unit/inc/ThirdParty/Plugins/Optimization/AMP/disableOptionsOnAmp.php
+++ tests/Unit/inc/ThirdParty/Plugins/Optimization/AMP/disableOptionsOnAmp.php
@@ -2,15 +2,18 @@
 
 namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Plugins\Optimization\AMP;
 
+use Brain\Monkey\Filters;
 use Brain\Monkey\Functions;
+use Mockery;
 use WPMedia\PHPUnit\Unit\TestCase;
 use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\ThirdParty\Plugins\Optimization\AMP;
 
 /**
  * @covers \WP_Rocket\ThirdParty\Plugins\Optimization\AMP::disable_options_on_amp
- * @group ThirdParty
- * @group WithAmp
+ * @group  ThirdParty
+ * @group  WithAmp
+ * @group  thisone
  */
 class Test_DisableOptionsOnAmp extends TestCase {
 	private $amp;
@@ -19,7 +22,7 @@ class Test_DisableOptionsOnAmp extends TestCase {
 	public function setUp() {
 		parent::setUp();
 
-		$this->options = $this->createMock( Options_Data::class );
+		$this->options = Mockery::mock( Options_Data::class );
 		$this->amp     = new AMP( $this->options );
 	}
 
@@ -29,88 +32,101 @@ class Test_DisableOptionsOnAmp extends TestCase {
 			->andReturn( false );
 
 		Functions\expect( 'remove_filter' )->never();
+		$this->options->shouldReceive( 'get' )->never();
 
 		$this->amp->disable_options_on_amp();
 	}
 
 	public function testShouldDisableOptionForAmpExceptImageSrcSet() {
-		Functions\expect( 'is_amp_endpoint' )
-			->once()
-			->andReturn( true );
+		global $wp_filter;
+		add_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
+		$wp_filter = [ 'rocket_buffer' => '__return_true' ];
+		add_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX );
 
-		Functions\expect( 'remove_filter' )
-			->once()
-			->with( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
+		// Check the hooks before invoking the method.
+		$this->assertTrue( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertFalse( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertTrue( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 
-		Functions\expect( 'add_filter' )
-			->once()
-			->with( 'do_rocket_lazyload', '__return_false' );
+		Functions\expect( 'is_amp_endpoint' )->once()->andReturn( true );
+		$this->options->shouldReceive( 'get' )
+		              ->once()
+		              ->with( 'do_cloudflare', 0 )
+		              ->andReturn( 0 );
+		$this->options->shouldReceive( 'get' )->with( 'cloudflare_protocol_rewrite', 0 )->never();
+		Filters\expectApplied( 'do_rocket_protocol_rewrite' )->with( false )->never();
 
-		$map = [
-			[ 'do_cloudflare', 0, 0, ],
-		];
-
-		$this->options->method( 'get' )->will( $this->returnValueMap( $map ) );
-
+		// Run it.
 		$this->amp->disable_options_on_amp();
+
+		// Check the hooks after invoking the method.
+		$this->assertFalse( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertTrue( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertEmpty( $wp_filter );
+		$this->assertTrue( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 	}
 
 	public function testShouldDisableOptionForAmpWhenCloudflareEnabled() {
-		Functions\expect( 'is_amp_endpoint' )
-			->once()
-			->andReturn( true );
+		global $wp_filter;
+		add_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
+		$wp_filter = [ 'rocket_buffer' => '__return_true' ];
+		add_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX );
 
-		Functions\expect( 'remove_filter' )
-			->ordered()
-			->once()
-			->with( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 )
-			->andAlsoExpectIt()
-			->once()
-			->with( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX );
+		// Check the hooks before invoking the method.
+		$this->assertTrue( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertFalse( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertTrue( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 
-		Functions\expect( 'add_filter' )
-			->once()
-			->with( 'do_rocket_lazyload', '__return_false' );
+		Functions\expect( 'is_amp_endpoint' )->once()->andReturn( true );
+		$this->options->shouldReceive( 'get' )
+		              ->once()
+		              ->with( 'do_cloudflare', 0 )
+		              ->andReturn( 1 );
+		$this->options->shouldReceive( 'get' )
+		              ->once()
+		              ->with( 'cloudflare_protocol_rewrite', 0 )
+		              ->andReturn( 1 );
+		Filters\expectApplied( 'do_rocket_protocol_rewrite' )->with( false )->never();
 
-		$map = [
-			[ 'do_cloudflare', 0, 1, ],
-			[ 'cloudflare_protocol_rewrite', 0, 1, ],
-		];
-
-		$this->options->method( 'get' )->will( $this->returnValueMap( $map ) );
-
+		// Run it.
 		$this->amp->disable_options_on_amp();
+
+		// Check the hooks after invoking the method.
+		$this->assertFalse( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertTrue( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertEmpty( $wp_filter );
+		$this->assertFalse( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 	}
 
 	public function testShouldDisableOptionForAmpWhenCloudflareEnabledAndFilterProtocolRewrite() {
-		Functions\expect( 'is_amp_endpoint' )
-			->once()
-			->andReturn( true );
+		global $wp_filter;
+		add_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 );
+		$wp_filter = [ 'rocket_buffer' => '__return_true' ];
+		add_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX );
 
-		Functions\expect( 'remove_filter' )
-			->ordered()
-			->once()
-			->with( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 )
-			->andAlsoExpectIt()
-			->once()
-			->with( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX );
+		// Check the hooks before invoking the method.
+		$this->assertTrue( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertFalse( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertTrue( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 
-		Functions\expect( 'add_filter' )
-			->once()
-			->with( 'do_rocket_lazyload', '__return_false' );
+		Functions\expect( 'is_amp_endpoint' )->once()->andReturn( true );
+		$this->options->shouldReceive( 'get' )
+		              ->once()
+		              ->with( 'do_cloudflare', 0 )
+		              ->andReturn( 1 );
+		$this->options->shouldReceive( 'get' )
+		              ->once()
+		              ->with( 'cloudflare_protocol_rewrite', 0 )
+		              ->andReturn( 0 );
+		Filters\expectApplied( 'do_rocket_protocol_rewrite' )->once()->with( false )->andReturn( true );
 
-		Functions\expect( 'apply_filters' )
-			->once()
-			->with(  'do_rocket_protocol_rewrite', false )
-			->andReturn( true );
-
-		$map = [
-			[ 'do_cloudflare', 0, 1, ],
-			[ 'cloudflare_protocol_rewrite', 0, 0, ],
-		];
-
-		$this->options->method( 'get' )->will( $this->returnValueMap( $map ) );
-
+		// Run it.
 		$this->amp->disable_options_on_amp();
+
+		// Check the hooks after invoking the method.
+		$this->assertFalse( has_filter( 'wp_resource_hints', 'rocket_dns_prefetch', 10, 2 ) );
+		$this->assertTrue( has_filter( 'do_rocket_lazyload', '__return_false' ) );
+		$this->assertEmpty( $wp_filter );
+		$this->assertFalse( has_filter( 'wp_calculate_image_srcset', 'rocket_protocol_rewrite_srcset', PHP_INT_MAX ) );
 	}
 }
