cfbae5ccbcf3d49706aa9628d77581488caed794
hellofromtonya <hellofromtonya@knowthecode.io>
2020-03-30 16:25:28 -0500

    Moves file load to unit bootstrap to ensure original posts functions are in memory before mocking.

--- tests/Unit/inc/functions/getRocketSamplePermalink.php
+++ tests/Unit/inc/functions/getRocketSamplePermalink.php
@@ -12,12 +12,6 @@ use WPMedia\PHPUnit\Unit\TestCase;
  */
 class Test_GetRocketSamePermalink extends TestCase {
 
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass();
-
-		require_once WP_ROCKET_PLUGIN_ROOT . 'inc/functions/posts.php';
-	}
-
 	public function tearDown() {
 		parent::tearDown();
 
