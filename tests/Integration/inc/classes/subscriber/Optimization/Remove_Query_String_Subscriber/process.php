42bc9f7c69d0225cab850d67c6dab3f0b4691c23
Rémy Perona <remperona@gmail.com>
2020-03-31 18:02:54 -0400

    update test for new filesystem testcase

--- tests/Integration/inc/classes/subscriber/Optimization/Remove_Query_String_Subscriber/process.php
+++ tests/Integration/inc/classes/subscriber/Optimization/Remove_Query_String_Subscriber/process.php
@@ -3,7 +3,6 @@
 namespace WP_Rocket\Tests\Integration\inc\classes\subscriber\Optimization\Remove_Query_String_Subscriber;
 
 use Brain\Monkey\Functions;
-use WP_Rocket\Subscriber\Optimization\Remove_Query_String_Subscriber;
 use WP_Rocket\Tests\Integration\FilesystemTestCase;
 
 /**
@@ -11,71 +10,25 @@ use WP_Rocket\Tests\Integration\FilesystemTestCase;
  * @group  RemoveQueryString
  */
 class Test_Process extends FilesystemTestCase {
-	private static $container;
-	private $subscriber;
-	protected $rootVirtualDir = 'wordpress';
-	protected $structure = [
-        'wp-includes' => [
-            'js' => [
-                'jquery' => [
-                    'jquery.js' => 'jquery',
-                ],
-            ],
-            'css' => [
-                'dashicons.min.css' => '',
-            ],
-        ],
-        'wp-content' => [
-            'cache' => [
-                'busting' => [
-                    '1' => [],
-                ],
-            ],
-            'themes' => [
-                'twentytwenty' => [
-                    'style.css' => 'test',
-                    'assets'    => [
-                        'script.js' => 'test',
-                    ]
-                ]
-            ],
-            'plugins' => [
-                'hello-dolly' => [
-                    'style.css'  => 'test',
-                    'script.js' => 'test',
-                ]
-            ],
-        ],
-	];
-
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass();
-
-		self::$container = apply_filters( 'rocket_container', null );
-	}
-
-	public function setUp() {
-		parent::setUp();
-
-		Functions\expect( 'rocket_get_constant' )->atLeast( 1 )->with( 'WP_ROCKET_CACHE_BUSTING_PATH' )->andReturn( $this->filesystem->getUrl( 'wp-content/cache/busting/' ) );
-		$this->subscriber = new Remove_Query_String_Subscriber( self::$container->get( 'remove_query_string' ) );
-	}
+    protected $path_to_test_data = '/inc/classes/subscriber/Optimization/Remove_Query_String_Subscriber/remove-query-strings.php';
 
 	/**
-	 * @dataProvider addDataProvider
+	 * @dataProvider providerTestData
 	 */
 	public function testShouldRemoveQueryStrings( $original, $expected ) {
-		add_filter( 'pre_get_rocket_option_remove_query_strings', '__return_true' );
+        add_filter( 'pre_get_rocket_option_remove_query_strings', [ $this, 'return_true' ] );
+        add_filter( 'rocket_wp_content_dir', [ $this, 'virtual_wp_content_dir' ] );
 
 		$this->assertSame(
 			$expected,
-			$this->subscriber->process( $original )
+			apply_filters( 'rocket_buffer', $original )
 		);
 
-		remove_filter( 'pre_get_rocket_option_remove_query_strings', '__return_true' );
-	}
+        remove_filter( 'pre_get_rocket_option_remove_query_strings', [ $this, 'return_true' ] );
+        remove_filter( 'rocket_wp_content_dir', [ $this, 'virtual_wp_content_dir' ] );
+    }
 
-	public function addDataProvider() {
-        return $this->getTestData( __DIR__, 'remove-query-strings' );
+    public function virtual_wp_content_dir() {
+        return $this->filesystem->getUrl( 'wp-content' );
     }
 }
