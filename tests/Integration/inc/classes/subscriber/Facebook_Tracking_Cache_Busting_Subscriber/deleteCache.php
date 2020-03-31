81373fddd9a7f5fbcbcc44aada0680746f30510b
Git I Hate You <git@screenfeed.fr>
2020-03-31 15:24:06 +0200

    Use common abstract class for both revious integration tests

--- tests/Integration/inc/classes/subscriber/Facebook_Tracking_Cache_Busting_Subscriber/deleteCache.php
+++ tests/Integration/inc/classes/subscriber/Facebook_Tracking_Cache_Busting_Subscriber/deleteCache.php
@@ -2,86 +2,16 @@
 
 namespace WP_Rocket\Tests\Integration\inc\classes\subscriber\Facebook_Tracking_Cache_Busting_Subscriber;
 
-use \Mockery;
-use WP_Rocket\Busting\Busting_Factory;
-use WP_Rocket\Tests\Integration\FilesystemTestCase;
+use WP_Rocket\Tests\Integration\inc\classes\subscriber\DeleteTrackingCacheTestCase;
 
 /**
  * @covers \WP_Rocket\Subscriber\Facebook_Tracking_Cache_Busting_Subscriber::delete_cache
  * @group  ThirdParty
  * @group  FacebookTracking
  */
-class Test_DeleteCache extends FilesystemTestCase {
+class Test_DeleteCache extends DeleteTrackingCacheTestCase {
 	protected $path_to_test_data = '/inc/classes/subscriber/Facebook_Tracking_Cache_Busting_Subscriber/deleteCache.php';
-	private $subscriber;
-	private $original_factory;
-
-	public function testShouldNotDeleteBustingFilesWhenNotClearingAllCache() {
-		$this->mockFactory();
-
-		// Enable facebook cache.
-		add_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_true' ] );
-
-		do_action( 'rocket_purge_cache', 'post', 123, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_true' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertTrue( $this->filesystem->exists( $file ) );
-		}
-
-		$this->restoreFactory();
-	}
-
-	public function testShouldNotDeleteBustingFilesWhenNotEnabled() {
-		$this->mockFactory();
-
-		// Disable facebook cache.
-		add_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_false' ] );
-
-		do_action( 'rocket_purge_cache', 'all', 0, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_false' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertTrue( $this->filesystem->exists( $file ) );
-		}
-
-		$this->restoreFactory();
-	}
-
-	public function testShouldDeleteBustingFilesWhenClearingAllCacheAndEnabled() {
-		// Enable facebook cache.
-		add_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_true' ] );
-
-		do_action( 'rocket_purge_cache', 'all', 0, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_facebook_pixel_cache', [ $this, 'return_true' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertFalse( $this->filesystem->exists( $file ) );
-		}
-	}
-
-	private function mockFactory() {
-		$container        = apply_filters( 'rocket_container', '' );
-		$this->subscriber = $container->get( 'facebook_tracking_subscriber' );
-
-		$original_factory_ref   = $this->get_reflective_property( 'busting_factory', $this->subscriber );
-		$this->original_factory = $original_factory_ref->getValue( $this->subscriber );
-
-		$mocked_factory = Mockery::mock( Busting_Factory::class );
-		$mocked_factory->shouldReceive( 'type' )
-		               ->never()
-		               ->with( 'fbsdk' );
-		$mocked_factory->shouldReceive( 'type' )
-		               ->never()
-		               ->with( 'fbpix' );
-
-		$original_factory_ref->setValue( $this->subscriber, $mocked_factory );
-	}
-
-	private function restoreFactory() {
-		$this->set_reflective_property( $this->original_factory, 'busting_factory', $this->subscriber );
-	}
+	protected $option_name       = 'facebook_pixel_cache';
+	protected $subscriber_name   = 'facebook_tracking_subscriber';
+	protected $factory_types     = [ 'fbsdk', 'fbpix' ];
 }
--- tests/Integration/inc/classes/subscriber/Google_Tracking_Cache_Busting_Subscriber/deleteTrackingCache.php
+++ tests/Integration/inc/classes/subscriber/Google_Tracking_Cache_Busting_Subscriber/deleteTrackingCache.php
@@ -2,86 +2,16 @@
 
 namespace WP_Rocket\Tests\Integration\inc\classes\subscriber\Google_Tracking_Cache_Busting_Subscriber;
 
-use Mockery;
-use WP_Rocket\Busting\Busting_Factory;
-use WP_Rocket\Tests\Integration\FilesystemTestCase;
+use WP_Rocket\Tests\Integration\inc\classes\subscriber\DeleteTrackingCacheTestCase;
 
 /**
  * @covers \WP_Rocket\Subscriber\Google_Tracking_Cache_Busting_Subscriber::delete_tracking_cache
  * @group  ThirdParty
  * @group  GoogleTracking
  */
-class Test_DeleteTrackingCache extends FilesystemTestCase {
+class Test_DeleteTrackingCache extends DeleteTrackingCacheTestCase {
 	protected $path_to_test_data = '/inc/classes/subscriber/Google_Tracking_Cache_Busting_Subscriber/deleteTrackingCache.php';
-	private $subscriber;
-	private $original_factory;
-
-	public function testShouldNotDeleteBustingFilesWhenNotClearingAllCache() {
-		$this->mockFactory();
-
-		// Enable Google cache.
-		add_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_true' ] );
-
-		do_action( 'rocket_purge_cache', 'post', 123, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_true' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertTrue( $this->filesystem->exists( $file ) );
-		}
-
-		$this->restoreFactory();
-	}
-
-	public function testShouldNotDeleteBustingFilesWhenNotEnabled() {
-		$this->mockFactory();
-
-		// Disable Google cache.
-		add_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_false' ] );
-
-		do_action( 'rocket_purge_cache', 'all', 0, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_false' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertTrue( $this->filesystem->exists( $file ) );
-		}
-
-		$this->restoreFactory();
-	}
-
-	public function testShouldDeleteBustingFilesWhenClearingAllCacheAndEnabled() {
-		// Enable Google cache.
-		add_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_true' ] );
-
-		do_action( 'rocket_purge_cache', 'all', 0, '', '' );
-
-		remove_filter( 'pre_get_rocket_option_google_analytics_cache', [ $this, 'return_true' ] );
-
-		foreach ( $this->original_files as $file ) {
-			$this->assertFalse( $this->filesystem->exists( $file ) );
-		}
-	}
-
-	private function mockFactory() {
-		$container        = apply_filters( 'rocket_container', '' );
-		$this->subscriber = $container->get( 'google_tracking_subscriber' );
-
-		$original_factory_ref   = $this->get_reflective_property( 'busting_factory', $this->subscriber );
-		$this->original_factory = $original_factory_ref->getValue( $this->subscriber );
-
-		$mocked_factory = Mockery::mock( Busting_Factory::class );
-		$mocked_factory->shouldReceive( 'type' )
-		               ->never()
-		               ->with( 'ga' );
-		$mocked_factory->shouldReceive( 'type' )
-		               ->never()
-		               ->with( 'gtm' );
-
-		$original_factory_ref->setValue( $this->subscriber, $mocked_factory );
-	}
-
-	private function restoreFactory() {
-		$this->set_reflective_property( $this->original_factory, 'busting_factory', $this->subscriber );
-	}
+	protected $option_name       = 'google_analytics_cache';
+	protected $subscriber_name   = 'google_tracking_subscriber';
+	protected $factory_types     = [ 'ga', 'gtm' ];
 }
