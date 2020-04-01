65c56f97692407b0e0d89b0ce8c5dc7b6190e200
Rémy Perona <remperona@gmail.com>
2020-04-01 16:32:33 -0400

    add unit tests for get_suggest

---
+++ tests/Fixtures/inc/Engine/Admin/Beacon/Beacon/get-suggest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * - locale
+ * - ID
+ * - expected
+ */
+return [
+	[
+		'en_US',
+		'remove_query_strings',
+		[
+			'id'  => '55231415e4b0221aadf25676',
+			'url' => 'https://docs.wp-rocket.me/article/56-remove-query-string-from-static-resources/?utm_source=wp_plugin&utm_medium=wp_rocket',
+		],
+	],
+	[
+		'fr_FR',
+		'remove_query_strings',
+		[
+			'id'  => '569568269033603f7da30334',
+			'url' => 'https://fr.docs.wp-rocket.me/article/219-supprimer-les-chaines-de-requetes-sur-les-ressources-statiques/?utm_source=wp_plugin&utm_medium=wp_rocket',
+		]
+	],
+	[
+		'en_US',
+		'user_cache',
+		[
+			'id'  => '56b55ba49033600da1c0b687',
+			'url' => 'https://docs.wp-rocket.me/article/313-user-cache/?utm_source=wp_plugin&utm_medium=wp_rocket',
+		],
+	],
+	[
+		'fr_FR',
+		'user_cache',
+		[
+			'id'  => '56cb9ba990336008e9e9e3d9',
+			'url' => 'https://fr.docs.wp-rocket.me/article/333-cache-utilisateurs-connectes/?utm_source=wp_plugin&utm_medium=wp_rocket',
+		],
+	],
+];
