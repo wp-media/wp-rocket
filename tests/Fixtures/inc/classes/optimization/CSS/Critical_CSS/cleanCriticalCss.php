17eb458950ee671c19395a908832eaa19527ef71
Soponar Cristina <crystinutzaa@gmail.com>
2020-04-02 12:35:38 +0300

    Added Unit Tests

---
+++ tests/Fixtures/inc/classes/optimization/CSS/Critical_CSS/cleanCriticalCss.php
@@ -0,0 +1,65 @@
+<?php
+
+
+return [
+	'vfs_dir'   => 'wp-content/cache/critical-css/',
+
+	// Virtual filesystem structure.
+	'structure' => [
+		'wp-content' => [
+			'cache' => [
+				'critical-css' => [
+					'1' => [
+						'.'              => '',
+						'..'             => '',
+						'folder'         => [
+							'.'        => '',
+							'..'       => '',
+							'file.css' => '.p { color: red; }',
+						],
+						'home.css'       => '.p { color: red; }',
+						'front_page.css' => '.p { color: red; }',
+						'category.css'   => '.p { color: red; }',
+						'post_tag.css'   => '.p { color: red; }',
+						'page.css'       => '.p { color: red; }',
+					],
+					'2' => [
+						'.'  => '',
+						'..' => '',
+					],
+				],
+			],
+		],
+	],
+	// Test Data
+	'test_data' => [
+		'testShouldNotDeleteAnything'                            => [
+			// Blog id
+			2,
+			// Deleted Files
+			[],
+			// Available Folders
+			[],
+		],
+		'testShouldDeleteFilesFromRootFolderButKeepChildFolders' => [
+			// Blog id
+			1,
+			// Deleted Files
+			[
+				'home.css'       => '.p { color: red; }',
+				'front_page.css' => '.p { color: red; }',
+				'category.css'   => '.p { color: red; }',
+				'post_tag.css'   => '.p { color: red; }',
+				'page.css'       => '.p { color: red; }',
+			],
+			// Available Folders
+			[
+				'folder' => [
+					'.'        => '',
+					'..'       => '',
+					'file.css' => '.p { color: red; }',
+				],
+			],
+		],
+	],
+];
---
+++ tests/Unit/inc/classes/optimization/CSS/Critical_CSS/cleanCriticalCss.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\classes\optimization\CSS\Critical_CSS;
+
+use Mockery;
+use Brain\Monkey\Functions;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
+use WP_Rocket\Optimization\CSS\Critical_CSS;
+use WP_Rocket\Optimization\CSS\Critical_CSS_Generation;
+
+/**
+ * @covers \WP_Rocket\Optimization\CSS\Critical_CSS::clean_critical_css
+ * @group  CPCSS
+ */
+class Test_CleanCriticalCSS extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/classes/optimization/CSS/Critical_CSS/cleanCriticalCss.php';
+
+	private $critical_css;
+	private $critical_css_path;
+	private $critical_css_generation;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->critical_css_path       = 'wp-content/cache/critical-css/';
+		$this->critical_css_generation = Mockery::mock( Critical_CSS_Generation::class );
+
+		Functions\expect( 'rocket_get_constant' )->with( 'WP_ROCKET_CRITICAL_CSS_PATH' )->andReturn( $this->filesystem->getUrl( $this->critical_css_path ) );
+		Functions\expect( 'home_url' )->with( '/' )->andReturn( 'http://example.org' );
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldDeleteFilesFromRootFolder( $blog_id, $deleted_files, $available_folders ) {
+		Functions\expect( 'get_current_blog_id' )->andReturn( $blog_id );
+
+		$critical_css_path = $this->critical_css_path . $blog_id . '/';
+
+		// Test that deleted Files are available.
+		foreach ( $deleted_files as $file => $content ) {
+			$this->assertTrue( $this->filesystem->exists( $critical_css_path . $file ) );
+		}
+		// Test that Subfolders are available.
+		foreach ( $available_folders as $folder => $content ) {
+			$this->assertTrue( $this->filesystem->exists( $critical_css_path . $folder ) );
+		}
+
+		$this->critical_css = new Critical_CSS( $this->critical_css_generation );
+		$this->critical_css->clean_critical_css();
+
+		// Test that root files are deleted now.
+		foreach ( $deleted_files as $file => $content ) {
+			$this->assertFalse( $this->filesystem->exists( $critical_css_path . $file ) );
+		}
+		// Test that Subfolders are still available.
+		foreach ( $available_folders as $folder => $content ) {
+			$this->assertTrue( $this->filesystem->exists( $critical_css_path . $folder ) );
+		}
+	}
+}
