927900bfdd59d25a6809eaa358b2357886af0220
Rémy Perona <remperona@gmail.com>
2020-04-03 16:29:52 -0400

    update SCSS class instantiation

--- inc/ThirdParty/ServiceProvider.php
+++ inc/ThirdParty/ServiceProvider.php
@@ -67,6 +67,8 @@ class ServiceProvider extends AbstractServiceProvider {
 		$this->getContainer()->share( 'bigcommerce_subscriber', 'WP_Rocket\Subscriber\Third_Party\Plugins\Ecommerce\BigCommerce_Subscriber' );
 		$this->getContainer()->share( 'amp_subscriber', 'WP_Rocket\ThirdParty\Plugins\Optimization\AMP' )
 			->withArgument( $options );
-		$this->getContainer()->share( 'simple_custom_css', 'WP_Rocket\ThirdParty\Plugins\SimpleCustomCss' );
+		$this->getContainer()->share( 'simple_custom_css', 'WP_Rocket\ThirdParty\Plugins\SimpleCustomCss' )
+			->withArgument( WP_ROCKET_CACHE_BUSTING_PATH )
+			->withArgument( WP_ROCKET_CACHE_BUSTING_URL );
 	}
 }
