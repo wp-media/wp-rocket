56053c13fb1302d133490c9068de438eb7f7295e
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-03 13:08:48 -0500

    Moves unit tests to FilesystemTestCase and test data.

--- tests/Fixtures/inc/common/doAdminPostRocketPurgeCache.php
+++ tests/Fixtures/inc/common/doAdminPostRocketPurgeCache.php
@@ -2,4 +2,78 @@
 
 return [
 
+	'vfs_dir'   => 'wp-content/cache/wp-rocket/',
+
+	// Virtual filesystem structure.
+	'structure' => [
+		'wp-content' => [
+			'cache' => [
+				'wp-rocket' => [
+					'example.org' => [
+						'index.html'      => '',
+						'index.html_gzip' => '',
+						'en'              => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+						'de'              => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+						'fr'              => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+					],
+				],
+			],
+		],
+	],
+
+	// Test data.
+	'test_data' => [
+		'purge'     => [
+			[
+				'$_GET'  => [
+					'type'     => 'post-123',
+					'_wpnonce' => 'whatever',
+					'lang'     => 'en',
+				],
+				'config' => [
+					'type'    => 'post',
+					'post_id' => 123,
+					'lang'    => 'en',
+				],
+			],
+		],
+		'wontpurge' => [
+			[
+				'$_GET'  => [
+					'type'     => 'invalid',
+					'_wpnonce' => '',
+				],
+				'config' => [],
+			],
+			[
+				'$_GET'  => [
+					'type'     => 'invalid',
+					'_wpnonce' => '',
+				],
+				'config' => [
+					'current_user_can' => false,
+				],
+			],
+			[
+				'$_GET'  => [
+					'type'     => 'invalid',
+					'_wpnonce' => 'whatever',
+				],
+				'config' => [
+					'current_user_can' => true,
+					'type'    => 'post',
+					'post_id' => 123,
+				],
+			],
+		],
+	],
 ];
