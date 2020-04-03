a3d112fc5a71c84658e5ee99ae6397fedd98a594
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-03 17:29:59 -0500

    Fixes the unit tests for get_rocket_i18n_code().

--- tests/Unit/inc/functions/getRocketI18nCode.php
+++ tests/Unit/inc/functions/getRocketI18nCode.php
@@ -23,15 +23,20 @@ class Test_GetRocketI18nCode extends TestCase {
 	public function tearDown() {
 		parent::tearDown();
 
-		unset( $GLOBALS['sitepress'], $GLOBALS['q_config'] );
+		unset( $GLOBALS['sitepress'], $GLOBALS['q_config'], $GLOBALS['polylang'] );
 	}
 
 	/**
 	 * @dataProvider providerTestData
 	 */
 	public function testShouldReturnExpected( $i18n_plugin, $codes, $expected ) {
+		$i18n_plugin = $this->setUpI18nPlugin( $i18n_plugin, $codes, $expected );
 		Functions\expect( 'rocket_has_i18n' )->once()->andReturn( $i18n_plugin );
 
+		$this->assertSame( $expected, get_rocket_i18n_code() );
+	}
+
+	private function setUpI18nPlugin( $i18n_plugin, $codes, $expect ) {
 		switch ( $i18n_plugin ) {
 			case 'wpml':
 				$GLOBALS['sitepress']                   = new SitePress();
@@ -42,10 +47,17 @@ class Test_GetRocketI18nCode extends TestCase {
 				$GLOBALS['q_config'] = [ 'enabled_languages' => $codes ];
 				break;
 			case 'polylang':
+				$GLOBALS['polylang'] = 'polylang';
+
+				if ( false === $expect ) {
+					Functions\expect( 'pll_languages_list' )->never();
+					return false;
+				}
+
 				Functions\expect( 'pll_languages_list' )->once()->andReturn( $codes );
 		}
 
-		$this->assertSame( $expected, get_rocket_i18n_code() );
+		return $i18n_plugin;
 	}
 
 	public function providerTestData() {
