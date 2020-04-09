17363b5b3f657c35cf04c1ad0d3245d8b4ba9c14
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-09 10:40:36 -0500

    Adds integration tests.

---
+++ tests/Fixtures/inc/functions/rocketCleanDomain.php
@@ -0,0 +1,205 @@
+<?php
+
+$i18n_plugins = require WP_ROCKET_TESTS_FIXTURES_DIR . '/i18n/pluginsData.php';
+
+return [
+	'vfs_dir'   => 'wp-content/cache/',
+
+	// Virtual filesystem structure.
+	'structure' => [
+		'wp-content' => [
+			'cache'            => [
+				'wp-rocket'    => [
+					'example.org'                => [
+						'index.html'      => '',
+						'index.html_gzip' => '',
+						'de'              => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+						'fr'              => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+						'hidden-files'    => [
+							'.mobile-active' => '',
+							'.no-webp'       => '',
+						],
+						'lorem-ipsum'     => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+						'nec-ullamcorper' => [
+							'enim-nunc-faucibus' => [
+								'index.html'      => '',
+								'index.html_gzip' => '',
+							],
+							'index.html'         => '',
+							'index.html_gzip'    => '',
+						],
+					],
+					'example.org-wpmedia-123456' => [
+						'index.html'      => '',
+						'index.html_gzip' => '',
+						'lorem-ipsum'     => [
+							'index.html'      => '',
+							'index.html_gzip' => '',
+						],
+					],
+					'example.org-tester-987654'  => [
+						'index.html'      => '',
+						'index.html_gzip' => '',
+						'nec-ullamcorper' => [
+							'enim-nunc-faucibus' => [
+								'index.html'      => '',
+								'index.html_gzip' => '',
+							],
+							'index.html'         => '',
+							'index.html_gzip'    => '',
+						],
+					],
+					'dots.example.org'           => [
+						'.'               => '',
+						'..'              => '',
+						'index.html'      => '',
+						'index.html_gzip' => '',
+					],
+					'index.html'                 => '',
+				],
+				'min'          => [
+					'1' => [
+						'123456.css' => '',
+						'123456.js'  => '',
+					],
+				],
+				'busting'      => [
+					'1' => [
+						'ga-123456.js' => '',
+					],
+				],
+				'critical-css' => [
+					'1' => [
+						'front-page.php' => '',
+						'blog.php'       => '',
+					],
+				],
+			],
+			'wp-rocket-config' => [
+				'example.org.php' => 'test',
+			],
+		],
+	],
+
+	// Test data.
+	'test_data' => [
+		'shouldDeleteAll_example.org*_whenNoLangGiven' => [
+			'i18n' => [
+				'lang'                        => '',
+				'data'                        => [],
+				'i18n_plugin'                 => false,
+				'rocket_has_i18n'             => false,
+				'get_rocket_i18n_uri'         => [ 'http://example.org' ],
+				'get_rocket_i18n_home_url'    => null,
+				'get_rocket_i18n_to_preserve' => [],
+			],
+
+			'expected' => [
+				'rocket_clean_domain_urls'   => [ 'http://example.org' ],
+				'before_rocket_clean_domain' => 1,
+				'after_rocket_clean_domain'  => 1,
+				'cleaned'                    => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org'                => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456' => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-tester-987654'  => null,
+					'vfs://public/wp-content/cache/wp-rocket/dots.example.org'           => [], // dots prevent it from being deleted. Why?
+				],
+				'non_cleaned' => [
+					// fs entry => should scan the directory and get the file listings.
+					'vfs://public/wp-content/cache/min/' => true,
+					'vfs://public/wp-content/cache/busting/' => true,
+					'vfs://public/wp-content/cache/critical-css/' => true,
+					'vfs://public/wp-content/cache/wp-rocket/' => false,
+					'vfs://public/wp-content/cache/wp-rocket/index.html' => false,
+				]
+			],
+		],
+
+		'shouldDeleteAll_*example.org*_whenLangGiven' => [
+			'i18n' => [
+				'lang'                        => 'fr',
+				'data'                        => [],
+				'i18n_plugin'                 => false,
+				'rocket_has_i18n'             => false,
+				'get_rocket_i18n_uri'         => [ 'http://example.org' ],
+				'get_rocket_i18n_home_url'    => null,
+				'get_rocket_i18n_to_preserve' => [],
+			],
+
+			'expected' => [
+				'rocket_clean_domain_urls'   => [ 'http://example.org' ],
+				'before_rocket_clean_domain' => 1,
+				'after_rocket_clean_domain'  => 1,
+				'cleaned'                    => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org'                => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456' => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-tester-987654'  => null,
+					'vfs://public/wp-content/cache/wp-rocket/dots.example.org'           => [], // dots prevent it from being deleted. Why?
+				],
+				'non_cleaned' => [
+					// fs entry => should scan the directory and get the file listings.
+					'vfs://public/wp-content/cache/min/' => true,
+					'vfs://public/wp-content/cache/busting/' => true,
+					'vfs://public/wp-content/cache/critical-css/' => true,
+					'vfs://public/wp-content/cache/wp-rocket/' => false,
+					'vfs://public/wp-content/cache/wp-rocket/index.html' => false,
+				]
+			],
+		],
+
+		'qtranslate_shouldDeleteAllExceptLangDirs' => [
+			'i18n' => [
+				'lang'                        => 'fr',
+				'data'                        => $i18n_plugins['qtranslate'],
+				'i18n_plugin'                 => 'qtranslate',
+				'rocket_has_i18n'             => 'qtranslate',
+				'get_rocket_i18n_uri'         => [
+					'http://example.org/en',
+					'http://example.org/de',
+				],
+				'get_rocket_i18n_home_url'    => 'http://example.org/fr',
+				'get_rocket_i18n_to_preserve' => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org(.*)/en',
+					'vfs://public/wp-content/cache/wp-rocket/example.org(.*)/de',
+				],
+			],
+
+			'expected' => [
+				'rocket_clean_domain_urls'   => [ 'http://example.org/fr' ],
+				'before_rocket_clean_domain' => 1,
+				'after_rocket_clean_domain'  => 1,
+				'cleaned'                    => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org'                => [
+						'vfs://public/wp-content/cache/wp-rocket/example.org/de/',
+						'vfs://public/wp-content/cache/wp-rocket/example.org/de/index.html',
+						'vfs://public/wp-content/cache/wp-rocket/example.org/de/index.html_gzip',
+					],
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456' => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-tester-987654'  => null,
+					// dots prevent it from being deleted. Why?
+					'vfs://public/wp-content/cache/wp-rocket/dots.example.org'           => [],
+				],
+				'non_cleaned'                => [
+					// fs entry => should scan the directory and get the file listings.
+					'vfs://public/wp-content/cache/min/'                     => true,
+					'vfs://public/wp-content/cache/busting/'                 => true,
+					'vfs://public/wp-content/cache/critical-css/'            => true,
+					'vfs://public/wp-content/cache/wp-rocket/'               => false,
+					'vfs://public/wp-content/cache/wp-rocket/index.html'     => false,
+					'vfs://public/wp-content/cache/wp-rocket/example.org/de' => true,
+				],
+			],
+		],
+
+
+	],
+];
---
+++ tests/Integration/inc/functions/rocketCleanDomain.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\functions;
+
+use Brain\Monkey\Functions;
+use PLL_Frontend;
+use SitePress;
+use WP_Rocket\Tests\Integration\FilesystemTestCase;
+
+/**
+ * @covers ::rocket_clean_domain
+ * @uses  ::get_rocket_i18n_home_url
+ * @uses  ::get_rocket_i18n_to_preserve
+ * @uses  ::get_rocket_i18n_uri
+ * @uses  ::get_rocket_parse_url
+ * @uses  ::rocket_get_constant
+ * @uses  ::rocket_rrmdir
+ *
+ * @group Functions
+ * @group Files
+ * @group vfs
+ */
+class Test_RocketCleanDomain extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/functions/rocketCleanDomain.php';
+	private $stats;
+	private $urlsToClean;
+	private $toPreserve;
+	private $dirsToClean;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/i18n/SitePress.php';
+		require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/i18n/PLL_Frontend.php';
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->urlsToClean = [];
+		$this->toPreserve  = [];
+		$this->dirsToClean = [];
+		$this->stats       = [
+			'before_rocket_clean_domain' => did_action( 'before_rocket_clean_domain' ),
+			'after_rocket_clean_domain'  => did_action( 'after_rocket_clean_domain' ),
+		];
+
+		$this->original_dirs = array_map(
+			function ( $dir ) {
+				return $this->filesystem->getUrl( $dir );
+			},
+			$this->original_dirs
+		);
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		remove_filter( 'rocket_clean_domain_urls', [ $this, 'checkRocketCleaDomainUrls' ], PHP_INT_MIN );
+		remove_action( 'rocket_check_file_to_clean', [ $this, 'checkFilesToClean' ], PHP_INT_MIN, 2 );
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldCleanSingleDomain( $i18n, $expected ) {
+		$this->urlsToClean = $expected['rocket_clean_domain_urls'];
+		$this->toPreserve  = $i18n['get_rocket_i18n_to_preserve'];
+		$this->dirsToClean = $expected['cleaned'];
+
+		$shouldNotClean = $this->getNonCleaned( $expected['non_cleaned'] );
+		$this->setUpI18nPlugin( $i18n['lang'], $i18n );
+
+		add_filter( 'rocket_clean_domain_urls', [ $this, 'checkRocketCleaDomainUrls' ], PHP_INT_MIN );
+		add_action( 'rocket_check_file_to_clean', [ $this, 'checkFilesToClean' ], PHP_INT_MIN, 2 );
+
+		// Run it.
+		rocket_clean_domain( $i18n['lang'] );
+
+		// Check the action events.
+		$this->assertEquals(
+			$expected['before_rocket_clean_domain'],
+			did_action( 'before_rocket_clean_domain' ) - $this->stats['before_rocket_clean_domain']
+		);
+		$this->assertEquals(
+			$expected['after_rocket_clean_domain'],
+			did_action( 'after_rocket_clean_domain' ) - $this->stats['after_rocket_clean_domain']
+		);
+
+		// Check the "cleaned" directories.
+		foreach ( $expected['cleaned'] as $dir => $contents ) {
+			// Deleted.
+			if ( is_null( $contents ) ) {
+				$this->assertFalse( $this->filesystem->exists( $dir ) );
+			} else {
+				$shouldNotClean[] = trailingslashit( $dir );
+				// Emptied, but not deleted.
+				$this->assertSame( $contents, $this->filesystem->getFilesListing( $dir ) );
+			}
+		}
+
+		// Check the non-cleaned files/directories still exist.
+		$entriesAfterCleaning = $this->filesystem->getListing( $this->filesystem->getUrl( $this->config['vfs_dir'] ) );
+		$this->assertSame( sort( $shouldNotClean ), sort( $entriesAfterCleaning ) );
+	}
+
+	private function getNonCleaned( $config ) {
+		$entries = [];
+		foreach( $config as $entry => $scanDir ) {
+			$entries[] = $entry;
+			if ( $scanDir && $this->filesystem->is_dir( $entry ) ) {
+				$entries = array_merge( $entries, $this->filesystem->getListing( $entry ) );
+			}
+		}
+		return $entries;
+	}
+
+	public function checkRocketCleaDomainUrls( $urls ) {
+		$this->assertSame( $this->urlsToClean, $urls );
+
+		return $urls;
+	}
+
+	public function checkFilesToClean( $file, $toPreserve ) {
+		$this->assertArrayHasKey( $file, $this->dirsToClean );
+		$this->assertSame( $this->toPreserve, $toPreserve );
+	}
+
+	private function setUpI18nPlugin( $lang, $config ) {
+		$home_url = home_url();
+		$data     = array_merge(
+			[
+				'codes' => [],
+				'langs' => [],
+				'uris'  => [],
+			],
+			$config['data']
+		);
+		$langs    = $data['langs'];
+
+		switch ( $config['i18n_plugin'] ) {
+			case 'wpml':
+				$GLOBALS['sitepress']                   = new SitePress();
+				$GLOBALS['sitepress']->active_languages = $data['codes'];
+				$GLOBALS['sitepress']->home_root        = $home_url;
+				$GLOBALS['sitepress']->uris_config      = $data['uris'];
+				break;
+
+			case 'qtranslate':
+			case 'qtranslate-x':
+				$GLOBALS['q_config'] = [ 'enabled_languages' => $langs ];
+
+				if ( empty( $lang ) || empty( $langs ) ) {
+					Functions\expect( 'qtranxf_convertURL' )->with( $home_url, $lang, true )->never();
+					Functions\expect( 'qtrans_convertURL' )->with( $home_url, $lang, true )->never();
+
+					return;
+				}
+
+
+				Functions\expect( 'qtranxf_convertURL' )
+//					->atLeast( 1 )
+					->with( $home_url, $lang, true )
+					->andReturnUsing( function ( $home_url, $lang ) use ( $langs ) {
+						if ( empty( $lang ) ) {
+							return $home_url;
+						}
+
+						if ( empty( $langs ) ) {
+							return $home_url;
+						}
+
+						return trailingslashit( $home_url ) . $lang;
+					} );
+				break;
+
+			case 'polylang':
+				if ( ! empty( $langs ) ) {
+					$GLOBALS['polylang'] = new PLL_Frontend( $data['options'] );
+					Functions\expect( 'PLL' )->andReturn( $GLOBALS['polylang'] );
+					Functions\expect( 'pll_home_url' )
+						->with( $lang )
+						->andReturnUsing( function ( $lang ) use ( $langs, $home_url ) {
+							if ( empty( $lang ) ) {
+								return $home_url;
+							}
+
+							if ( in_array( $lang, $langs, true ) ) {
+								return trailingslashit( $home_url ) . $lang;
+							}
+
+							return $home_url;
+						} );
+				} else {
+					$GLOBALS['polylang'] = 'not-empty';
+					Functions\expect( 'PLL' )->never();
+				}
+
+				Functions\expect( 'pll_languages_list' )->andReturn( $langs );
+				break;
+
+			default:
+				Functions\expect( 'get_rocket_i18n_code' )->never();
+		}
+	}
+}
