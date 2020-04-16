31c1c8eec7e37d11cb3cd4294df8d4f15a7caeb4
Rémy Perona <remperona@gmail.com>
2020-04-16 15:12:49 -0400

    update tests

--- tests/Fixtures/inc/Engine/Admin/HealthCheck/missed-cron.php
+++ tests/Fixtures/inc/Engine/Admin/HealthCheck/missed-cron.php
@@ -7,11 +7,19 @@ return [
 			'screen'    => 'options_general',
 			'dismissed' => [],
 			'options'   => [
-				0,
+				'purge_cron'                 => 0,
+				'async_css'                  => 0,
+				'manual_preload'             => 0,
+				'schedule_automatic_cleanup' => 0,
 			],
 			'events' => [
-				false,
+				'rocket_purge_time_event'                      => false,
+				'rocket_database_optimization_time_event'      => false,
+				'rocket_database_optimization_cron_interval'   => false,
+				'rocket_preload_cron_interval'                 => false,
+				'rocket_critical_css_generation_cron_interval' => false,
 			],
+			'disable_cron' => false,
 			'expected' => '',
 		],
 	],
@@ -21,11 +29,19 @@ return [
 			'screen'    => 'options_general',
 			'dismissed' => [],
 			'options'   => [
-				0,
+				'purge_cron'                 => 0,
+				'async_css'                  => 0,
+				'manual_preload'             => 0,
+				'schedule_automatic_cleanup' => 0,
 			],
 			'events' => [
-				false,
+				'rocket_purge_time_event'                      => false,
+				'rocket_database_optimization_time_event'      => false,
+				'rocket_database_optimization_cron_interval'   => false,
+				'rocket_preload_cron_interval'                 => false,
+				'rocket_critical_css_generation_cron_interval' => false,
 			],
+			'disable_cron' => false,
 			'expected' => '',
 		],
 	],
@@ -37,11 +53,19 @@ return [
 				'rocket_warning_cron'
 			],
 			'options'   => [
-				0,
+				'purge_cron'                 => 0,
+				'async_css'                  => 0,
+				'manual_preload'             => 0,
+				'schedule_automatic_cleanup' => 0,
 			],
 			'events' => [
-				false,
+				'rocket_purge_time_event'                      => false,
+				'rocket_database_optimization_time_event'      => false,
+				'rocket_database_optimization_cron_interval'   => false,
+				'rocket_preload_cron_interval'                 => false,
+				'rocket_critical_css_generation_cron_interval' => false,
 			],
+			'disable_cron' => false,
 			'expected' => '',
 		],
 	],
@@ -51,11 +75,19 @@ return [
 			'screen'    => 'settings_page_wprocket',
 			'dismissed' => [],
 			'options'   => [
-				0,
+				'purge_cron'                 => 0,
+				'async_css'                  => 0,
+				'manual_preload'             => 0,
+				'schedule_automatic_cleanup' => 0,
 			],
 			'events' => [
-				false,
+				'rocket_purge_time_event'                      => false,
+				'rocket_database_optimization_time_event'      => false,
+				'rocket_database_optimization_cron_interval'   => false,
+				'rocket_preload_cron_interval'                 => false,
+				'rocket_critical_css_generation_cron_interval' => false,
 			],
+			'disable_cron' => false,
 			'expected' => '',
 		],
 	],
@@ -65,11 +97,19 @@ return [
 			'screen'    => 'settings_page_wprocket',
 			'dismissed' => [],
 			'options'   => [
-				1,
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
 			],
 			'events' => [
-				false,
+				'rocket_purge_time_event'                      => false,
+				'rocket_database_optimization_time_event'      => false,
+				'rocket_database_optimization_cron_interval'   => false,
+				'rocket_preload_cron_interval'                 => false,
+				'rocket_critical_css_generation_cron_interval' => false,
 			],
+			'disable_cron' => false,
 			'expected' => '',
 		],
 	],
@@ -79,10 +119,18 @@ return [
 			'screen'    => 'settings_page_wprocket',
 			'dismissed' => [],
 			'options'   => [
-				1,
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
 			],
+			'disable_cron' => false,
 			'events' => [
-				time(),
+				'rocket_purge_time_event'                      => time(),
+				'rocket_database_optimization_time_event'      => time(),
+				'rocket_database_optimization_cron_interval'   => time(),
+				'rocket_preload_cron_interval'                 => time(),
+				'rocket_critical_css_generation_cron_interval' => time(),
 			],
 			'expected' => '',
 		],
@@ -93,19 +141,112 @@ return [
 			'screen'    => 'settings_page_wprocket',
 			'dismissed' => [],
 			'options'   => [
-				1,
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
 			],
 			'events' => [
-				time() - 600,
-				time()
+				'rocket_purge_time_event'                      => time() - 3600,
+				'rocket_database_optimization_time_event'      => time(),
+				'rocket_database_optimization_cron_interval'   => time(),
+				'rocket_preload_cron_interval'                 => time(),
+				'rocket_critical_css_generation_cron_interval' => time(),
 			],
+			'disable_cron' => false,
 			'expected' => '<div class="notice notice-warning ">
 			<p>The following scheduled event failed to run. This may indicate the CRON system is not running properly, which can prevent some WP Rocket features from working as intended:</p>
 			<ul>
 			<li>Scheduled Cache Purge</li>
 			</ul>
 			<p>Please contact your host to check if CRON is working.</p>
-			<p><a class="rocket-dismiss" href="http://example.org/admin-post.php?action=rocket_ignore&box=rocket_warning_cron">Dismiss this notice</a></p></div>',
+			<p><a class="rocket-dismiss" href="http://example.org/wp-admin/admin-post.php?action=rocket_ignore&amp;box=rocket_warning_cron&amp;_wpnonce=123456">Dismiss this notice.</a></p></div>',
+		],
+	],
+	'MultipleMissedEvents' => [
+		[
+			'cap'       => true,
+			'screen'    => 'settings_page_wprocket',
+			'dismissed' => [],
+			'options'   => [
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
+			],
+			'events' => [
+				'rocket_purge_time_event'                      => time() - 3600,
+				'rocket_database_optimization_time_event'      => time() - 3600,
+				'rocket_database_optimization_cron_interval'   => time(),
+				'rocket_preload_cron_interval'                 => time(),
+				'rocket_critical_css_generation_cron_interval' => time(),
+			],
+			'disable_cron' => false,
+			'expected' => '<div class="notice notice-warning ">
+			<p>The following scheduled events failed to run. This may indicate the CRON system is not running properly, which can prevent some WP Rocket features from working as intended:</p>
+			<ul>
+			<li>Scheduled Cache Purge</li>
+			<li>Scheduled Database Optimization</li>
+			</ul>
+			<p>Please contact your host to check if CRON is working.</p>
+			<p><a class="rocket-dismiss" href="http://example.org/wp-admin/admin-post.php?action=rocket_ignore&amp;box=rocket_warning_cron&amp;_wpnonce=123456">Dismiss this notice.</a></p></div>',
+		],
+	],
+	'PurgeTimeMissedEventDisabledCron' => [
+		[
+			'cap'       => true,
+			'screen'    => 'settings_page_wprocket',
+			'dismissed' => [],
+			'options'   => [
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
+			],
+			'events' => [
+				'rocket_purge_time_event'                      => time() - 7200,
+				'rocket_database_optimization_time_event'      => time(),
+				'rocket_database_optimization_cron_interval'   => time(),
+				'rocket_preload_cron_interval'                 => time(),
+				'rocket_critical_css_generation_cron_interval' => time(),
+			],
+			'disable_cron' => true,
+			'expected' => '<div class="notice notice-warning ">
+			<p>The following scheduled event failed to run. This may indicate the CRON system is not running properly, which can prevent some WP Rocket features from working as intended:</p>
+			<ul>
+			<li>Scheduled Cache Purge</li>
+			</ul>
+			<p>Please contact your host to check if CRON is working.</p>
+			<p><a class="rocket-dismiss" href="http://example.org/wp-admin/admin-post.php?action=rocket_ignore&amp;box=rocket_warning_cron&amp;_wpnonce=123456">Dismiss this notice.</a></p></div>',
+		],
+	],
+	'MultipleMissedEventsDisabledCron' => [
+		[
+			'cap'       => true,
+			'screen'    => 'settings_page_wprocket',
+			'dismissed' => [],
+			'options'   => [
+				'purge_cron'                 => 1,
+				'async_css'                  => 1,
+				'manual_preload'             => 1,
+				'schedule_automatic_cleanup' => 1,
+			],
+			'events' => [
+				'rocket_purge_time_event'                      => time() - 7200,
+				'rocket_database_optimization_time_event'      => time() - 7200,
+				'rocket_database_optimization_cron_interval'   => time(),
+				'rocket_preload_cron_interval'                 => time(),
+				'rocket_critical_css_generation_cron_interval' => time(),
+			],
+			'disable_cron' => true,
+			'expected' => '<div class="notice notice-warning ">
+			<p>The following scheduled events failed to run. This may indicate the CRON system is not running properly, which can prevent some WP Rocket features from working as intended:</p>
+			<ul>
+			<li>Scheduled Cache Purge</li>
+			<li>Scheduled Database Optimization</li>
+			</ul>
+			<p>Please contact your host to check if CRON is working.</p>
+			<p><a class="rocket-dismiss" href="http://example.org/wp-admin/admin-post.php?action=rocket_ignore&amp;box=rocket_warning_cron&amp;_wpnonce=123456">Dismiss this notice.</a></p></div>',
 		],
 	],
 ];
--- tests/Unit/inc/Engine/Admin/HealthCheck/missedCron.php
+++ tests/Unit/inc/Engine/Admin/HealthCheck/missedCron.php
@@ -49,13 +49,26 @@ class Test_MissedCron extends TestCase {
 			->times( 4 )
 			->andReturnValues( $config['options'] );
 
+		Functions\expect( 'rocket_get_constant' )
+			->atMost()
+			->times( 1 )
+			->andReturn( $config['disable_cron'] );
+
 		Functions\expect( 'wp_next_scheduled' )
 			->atMost()
 			->times( 5 )
 			->andReturnValues( $config['events'] );
+	
+		Functions\when( '_n' )->alias( function( $singular, $plural, $count ) {
+			if ( $count > 1 ) {
+				return $plural;
+			}
+
+			return $singular;
+		} );
 
 		Functions\when( 'rocket_notice_html' )->alias( function( $args ) {
-			echo '<div class="notice notice-warning ">' . $args['message'] . '<p><a class="rocket-dismiss" href="http://example.org/admin-post.php?action=rocket_ignore&box=rocket_warning_cron">Dismiss this notice</a></p></div>';
+			echo '<div class="notice notice-warning ">' . $args['message'] . '<p><a class="rocket-dismiss" href="http://example.org/wp-admin/admin-post.php?action=rocket_ignore&amp;box=rocket_warning_cron&amp;_wpnonce=123456">Dismiss this notice.</a></p></div>';
 		} );
 
 		if ( empty( $config['expected'] ) ) {
