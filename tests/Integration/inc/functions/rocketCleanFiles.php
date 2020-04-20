0f47b2abfb64f820bc9537f218215f904b48373b
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-20 17:07:13 -0500

    Uses test case methods.

--- tests/Integration/inc/functions/rocketCleanFiles.php
+++ tests/Integration/inc/functions/rocketCleanFiles.php
@@ -32,44 +32,13 @@ class Test_RocketCleanFiles extends FilesystemTestCase {
 	 * @dataProvider providerTestData
 	 */
 	public function testShouldCleanSingleDomain( $urls, $expected ) {
-		$this->dirsToClean = $expected['cleaned'];
-
-		$shouldNotClean = $this->getNonCleaned( $expected['non_cleaned'] );
+		$this->dirsToClean    = $expected['cleaned'];
+		$this->getShouldNotCleanEntries( $expected['non_cleaned'] );
 
 		// Run it.
 		rocket_clean_files( $urls );
 
-		// Check the "cleaned" directories.
-		foreach ( $expected['cleaned'] as $dir => $contents ) {
-			// Deleted.
-			if ( is_null( $contents ) ) {
-				$this->assertFalse( $this->filesystem->exists( $dir ) );
-			} else {
-				$shouldNotClean[] = trailingslashit( $dir );
-				// Emptied, but not deleted.
-				$this->assertSame( $contents, $this->filesystem->getFilesListing( $dir ) );
-			}
-		}
-
-		// Check the non-cleaned files/directories still exist.
-		$entriesAfterCleaning = $this->filesystem->getListing( $this->filesystem->getUrl( $this->config['vfs_dir'] ) );
-		$actual               = array_diff( $entriesAfterCleaning, $shouldNotClean );
-		if ( ! empty( $expected['test_it'] ) ) {
-			var_dump( $actual );
-		} else {
-			$this->assertEmpty( $actual );
-		}
-	}
-
-	private function getNonCleaned( $config ) {
-		$entries = [];
-		foreach ( $config as $entry => $scanDir ) {
-			$entries[] = $entry;
-			if ( $scanDir && $this->filesystem->is_dir( $entry ) ) {
-				$entries = array_merge( $entries, $this->filesystem->getListing( $entry ) );
-			}
-		}
-
-		return $entries;
+		$this->checkCleanedIsDeleted( $expected['cleaned'] );
+		$this->checkNonCleanedExist();
 	}
 }
