8f4ff4906082855f9c5e17e051ab95a627e5ed52
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-21 12:08:55 -0500

    Adds events property: (1) micro-optimization and (2) avoids changing events array during iteration.
    - The array is not dynamic or dependent upon the callback. Therefore, it's a good candidate for a property during instantiation.
    - Changing an array during iteration can cause weird behavior and is not performant.

--- inc/Engine/Admin/HealthCheck.php
+++ inc/Engine/Admin/HealthCheck.php
@@ -14,12 +14,20 @@ class HealthCheck implements Subscriber_Interface {
 	private $options;
 
 	/**
+	 * Array of events with their descriptions.
+	 *
+	 * @var array
+	 */
+	private $events;
+
+	/**
 	 * Creates an instance of the health checker.
 	 *
 	 * @param Options_Data $options Options_Data instance.
 	 */
 	public function __construct( Options_Data $options ) {
 		$this->options = $options;
+		$this->events  = $this->get_events();
 	}
 
 	/**
@@ -45,9 +53,9 @@ class HealthCheck implements Subscriber_Interface {
 
 		$delay  = rocket_get_constant( 'DISABLE_WP_CRON' ) ? HOUR_IN_SECONDS : 5 * MINUTE_IN_SECONDS;
 		$list   = '';
-		$events = $this->get_events();
+		$events = $this->events;
 
-		foreach ( $events as $event => $description ) {
+		foreach ( $this->events as $event => $description ) {
 			$timestamp = wp_next_scheduled( $event );
 
 			if (
