df209e158e9bebb315688c5fbdf802f588ab1b54
Git I Hate You <git@screenfeed.fr>
2020-04-23 00:48:44 +0200

    Create an alias to deprecate the old class naming

---
+++ inc/deprecated/3.6.php
@@ -0,0 +1,9 @@
+<?php
+// phpcs:ignoreFile
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class aliases.
+ */
+class_alias( '\WP_Rocket\Engine\Admin\Settings\Settings', '\WP_Rocket\Admin\Settings\Settings' );
