ac2fc02e7d6d10dfa4702e90ca94d460a3155e05
Rémy Perona <remperona@gmail.com>
2020-04-23 10:32:39 -0400

    update fixture to add new method

--- tests/Fixtures/WP_Theme.php
+++ tests/Fixtures/WP_Theme.php
@@ -5,11 +5,13 @@ if ( ! class_exists( 'WP_Theme') ) {
 		private $theme_root;
 		private $stylesheet;
 		private $template;
+		private $name;
 
 		public function __construct( $theme_dir, $theme_root, $_child = null ) {
 			$this->theme_root = $theme_root;
 			$this->stylesheet = $theme_dir;
 			$this->template = $theme_dir;
+			$this->name = 'WordPress Default';
 		}
 
 		public function get_template() {
@@ -18,6 +20,12 @@ if ( ! class_exists( 'WP_Theme') ) {
 
 		public function get_stylesheet() {
 			return $this->stylesheet;
+		}
+
+		public function get( $value ) {
+			if ( 'Name' === $value ) {
+				return $this->name;
+			}
 		}
 	}
 }
