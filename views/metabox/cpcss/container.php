b2e5462dca3801fc35556cff3a78aacb5cfb2f56
Rémy Perona <remperona@gmail.com>
2020-04-24 14:53:42 -0400

    update wording in templates

--- views/metabox/cpcss/container.php
+++ views/metabox/cpcss/container.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<p><?php esc_html_e( 'Custom CPCSS', 'rocket' ); ?></p>
+<p><?php esc_html_e( 'Critical Path CSS', 'rocket' ); ?></p>
 <div id="rocket-metabox-cpcss-notice"></div>
 <div id="rocket-metabox-cpcss-content">
 <?php do_action( 'rocket_metabox_cpcss_content' ); ?>
