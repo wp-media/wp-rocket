658eccd281035f380a1200174cb03ed174d9d8c7
hellofromtonya <hellofromtonya@knowthecode.io>
2020-04-29 10:31:41 -0500

    Adds class_alias() for Page class.

--- inc/deprecated/3.6.php
+++ inc/deprecated/3.6.php
@@ -7,3 +7,4 @@ defined( 'ABSPATH' ) || exit;
  * Class aliases.
  */
 class_alias( '\WP_Rocket\ThirdParty\Plugins\Smush', '\WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber' );
+class_alias( '\WP_Rocket\Admin\Settings\Page', '\WP_Rocket\Engine\Admin\Settings\Page' );
