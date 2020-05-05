409efb718bcc84baae887b42f20ea48cd1207150
Grégory Viguier <Screenfeed@users.noreply.github.com>
2020-05-05 15:43:16 +0200

    Closes #2431: Remove Query Strings Option Removal, Option management part (8/8) (PR #2507)
    * Upgrader: remove remove_query_strings entry and call to rocket_clean_cache_busting()
    * In rocket_after_save_options(), removed call to rocket_clean_cache_busting()
    * In sanitize_callback(), removed remove_query_strings entry
    * Unit test for rocket_first_install()
    * Integration tests for rocket_first_install()
    * Unit tests for rocket_after_save_options()
    * Integration tests for rocket_after_save_options()
    
    Co-authored-by: Rémy Perona <remperona@gmail.com>
    Co-authored-by: hellofromtonya <hellofromtonya@knowthecode.io>

---
+++ tests/Fixtures/inc/admin/rocketAfterSaveOptions.php
@@ -0,0 +1,238 @@
+<?php
+
+$rocket_clean_domain = [
+	'vfs://public/wp-content/cache/wp-rocket/example.org/'                => null,
+	'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/' => null,
+	'vfs://public/wp-content/cache/wp-rocket/example.org-tester-987654/'  => null,
+];
+$rocket_clean_minify = [
+	'vfs://public/wp-content/cache/min/1/5c795b0e3a1884eec34a989485f863ff.js'    => null,
+	'vfs://public/wp-content/cache/min/1/5c795b0e3a1884eec34a989485f863ff.js.gz' => null,
+	'vfs://public/wp-content/cache/min/1/wp-content/plugins/imagify/assets/js/'  => null,
+	'vfs://public/wp-content/cache/min/1/wp-includes/js/'                        => [],
+
+	'vfs://public/wp-content/cache/min/3rd-party/bt937b0e3a1884eec34a989485f863ff.js'    => null,
+	'vfs://public/wp-content/cache/min/3rd-party/bt937b0e3a1884eec34a989485f863ff.js.gz' => null,
+];
+
+$advanced_cache = require WP_ROCKET_TESTS_FIXTURES_DIR . '/content/advancedCacheContent.php';
+$htaccess       = require WP_ROCKET_TESTS_FIXTURES_DIR . '/content/htaccessContent.php';
+
+return [
+	'vfs_dir' => 'wp-content/',
+
+	'settings'  => [
+		'cache_mobile'        => true,
+		'purge_cron_interval' => true,
+		'purge_cron_unit'     => true,
+		'minify_css'          => false,
+		'exclude_css'         => '',
+		'minify_js'           => false,
+		'exclude_js'          => '',
+		'analytics_enabled'   => '',
+		'cdn'                 => false,
+		'cdn_cnames'          => false,
+	],
+
+	// Test data.
+	'test_data' => [
+
+		'testShouldBailOutWhenSettingsNotAndArray' => [
+			'settings' => 'not an array',
+			'expected' => [],
+		],
+
+		'testShouldTriggerCleaningsWhenOptionsChange' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => false,
+				'minify_css'          => false,
+				'exclude_css'         => '',
+				'minify_js'           => false,
+				'exclude_js'          => '',
+				'foobar'              => 'barbaz', // This one will trigger cleaning and preload.
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldNotCleanMinifyCSSWhenMinifyOptionChanges' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => true,
+				'exclude_css'         => '',
+				'minify_js'           => false,
+				'exclude_js'          => '',
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldNotCleanMinifyCSSWhenExcludeOptionChanges' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => false,
+				'exclude_css'         => 'foobar',
+				'minify_js'           => false,
+				'exclude_js'          => '',
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldCleanMinifyJSWhenMinifyOptionChanges' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => false,
+				'exclude_css'         => '',
+				'minify_js'           => true,
+				'exclude_js'          => '',
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'rocket_clean_minify'         => $rocket_clean_minify,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldCleanMinifyJSWhenExcludeOptionChanges' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => false,
+				'exclude_css'         => '',
+				'minify_js'           => false,
+				'exclude_js'          => 'foobar',
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'rocket_clean_minify'         => $rocket_clean_minify,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldCleanMinifyJSWhenCdnOptionIsEnabled' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => false,
+				'exclude_css'         => '',
+				'minify_js'           => false,
+				'exclude_js'          => '',
+				'cdn'                 => true,
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'rocket_clean_minify'         => $rocket_clean_minify,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['CORS'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+			],
+		],
+
+		'testShouldGenerateAdvancedCacheFileWhenOptionIsEnabled' => [
+			'settings' => [
+				'cache_mobile'            => true,
+				'purge_cron_interval'     => true,
+				'purge_cron_unit'         => true,
+				'minify_css'              => false,
+				'exclude_css'             => '',
+				'minify_js'               => false,
+				'exclude_js'              => '',
+				'do_caching_mobile_files' => true,
+			],
+			'expected' => [
+				'rocket_clean_domain'                 => $rocket_clean_domain,
+				'flush_rocket_htaccess'               => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file'         => '<?php $var = "Some contents.";',
+				'rocket_generate_advanced_cache_file' => $advanced_cache['mobile'],
+			],
+		],
+
+		'testShouldEnableAnalyticsWhenOptionIsEnabled' => [
+			'settings' => [
+				'cache_mobile'        => true,
+				'purge_cron_interval' => true,
+				'purge_cron_unit'     => true,
+				'minify_css'          => false,
+				'exclude_css'         => '',
+				'minify_js'           => false,
+				'exclude_js'          => '',
+				'analytics_enabled'   => true,
+			],
+			'expected' => [
+				'rocket_clean_domain'         => $rocket_clean_domain,
+				'flush_rocket_htaccess'       => [
+					$htaccess['start'],
+					$htaccess['FileETag'],
+					$htaccess['mod_alias'],
+					$htaccess['wp_rules_start'],
+					$htaccess['end'],
+				],
+				'rocket_generate_config_file' => '<?php $var = "Some contents.";',
+				'set_transient'               => '1',
+			],
+		],
+	],
+];
--- tests/Unit/inc/ThirdParty/Plugins/SimpleCustomCss/updateCacheFile.php
+++ tests/Unit/inc/ThirdParty/Plugins/SimpleCustomCss/updateCacheFile.php
@@ -8,31 +8,20 @@ use WP_Rocket\ThirdParty\Plugins\SimpleCustomCss;
 
 /**
  * @covers \WP_Rocket\ThirdParty\Plugins\SimpleCustomCss::update_cache_file
+ *
  * @group  ThirdParty
  * @group  WithSCCSS
  */
 class Test_UpdateCacheFile extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/ThirdParty/Plugins/SimpleCustomCss/updateCacheFile.php';
-	private $busting_path;
-	private $busting_url = 'http://example.org/wp-content/cache/busting/';
+	private   $busting_path      = 'vfs://public/wp-content/cache/busting/';
+	private   $busting_url       = 'http://example.org/wp-content/cache/busting/';
 
 	public function setUp() {
 		parent::setUp();
 
-		$this->busting_path = $this->filesystem->getUrl( 'wp-content/cache/busting/' );
-
-		Functions\expect( 'rocket_clean_domain' )
-			->once();
-		Functions\expect( 'wp_kses' )->andReturnFirstArg();
-
-		Functions\expect( 'get_option' )
-			->with( 'sccss_settings' )
-			->andReturn( [
-				'sccss-content' => '.simple-custom-css { color: red; }',
-			] );
-
-		$this->filesystem->chmod(  'wp-content/cache/busting/index.php', 0644 );
-		$this->filesystem->chmod(  'wp-content/cache/busting/', 0755 );
+		// Mocks the various filesystem constants.
+		$this->whenRocketGetConstant();
 	}
 
 	/**
@@ -40,11 +29,20 @@ class Test_UpdateCacheFile extends FilesystemTestCase {
 	 */
 	public function testShouldDeleteTheFileAndRecreateIt( $blog_id, $filepath ) {
 		$this->filesystem->setFilemtime( $filepath, strtotime( '11 hours ago' ) );
+
 		Functions\when( 'get_current_blog_id' )->justReturn( $blog_id );
+		Functions\expect( 'rocket_clean_domain' )->once()->andReturnNull();
+		Functions\expect( 'wp_kses' )->andReturnFirstArg();
+		Functions\expect( 'get_option' )
+			->with( 'sccss_settings' )
+			->andReturn(
+				[
+					'sccss-content' => '.simple-custom-css { color: red; }',
+				]
+			);
 
 		$sccss = new SimpleCustomCss( $this->busting_path, $this->busting_url );
 		$sccss->update_cache_file();
 		$this->assertTrue( $this->filesystem->exists( $filepath ) );
 	}
-
 }
