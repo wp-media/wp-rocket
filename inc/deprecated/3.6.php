5cf45bebc7c075322695eb64cf1f44b503772390
Rémy Perona <remperona@gmail.com>
2020-05-06 13:35:54 -0400

    remove class_alias already present in deprecated/3.5.php

--- inc/deprecated/3.6.php
+++ inc/deprecated/3.6.php
@@ -5,5 +5,4 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Class aliases.
  */
-class_alias( '\WP_Rocket\Engine\Admin\Settings\Settings', '\WP_Rocket\Admin\Settings\Settings' );
 class_alias( '\WP_Rocket\ThirdParty\Plugins\Smush', '\WP_Rocket\Subscriber\Third_Party\Plugins\Smush_Subscriber' );
