bb463a45b7be940aa55fad45940c4ffc6d55fe49
hellofromtonya <hellofromtonya@knowthecode.io>
2020-05-06 18:17:31 -0500

    Updates js to ES6.

--- assets/js/wpr-cpcss.js
+++ assets/js/wpr-cpcss.js
@@ -1,63 +1,62 @@
-var checkCPCSSGenerationCall;
-var cpcsssGenerationPending = 0;
-var rocketDeleteCPCSSbtn    = document.getElementById( 'rocket-delete-post-cpss' );
-var rocketGenerateCPCSSbtn  = document.getElementById( 'rocket-generate-post-cpss' );
-var rocketCPCSSGenerate     = document.querySelectorAll( '.cpcss_generate' );
-var rocketCPCSSReGenerate   = document.querySelectorAll( '.cpcss_regenerate' );
+let checkCPCSSGenerationCall;
+let cpcsssGenerationPending  = 0;
+const rocketDeleteCPCSSbtn   = document.getElementById( 'rocket-delete-post-cpss' );
+const rocketGenerateCPCSSbtn = document.getElementById( 'rocket-generate-post-cpss' );
+const rocketCPCSSGenerate    = document.querySelectorAll( '.cpcss_generate' );
+const rocketCPCSSReGenerate  = document.querySelectorAll( '.cpcss_regenerate' );
 
-rocketDeleteCPCSSbtn.addEventListener( 'click', function( e ) {
+rocketDeleteCPCSSbtn.addEventListener( 'click', e => {
 	e.preventDefault();
 	deleteCPCSS();
-});
+} );
 
-rocketGenerateCPCSSbtn.addEventListener( 'click', function( e ) {
+rocketGenerateCPCSSbtn.addEventListener( 'click', e => {
 	e.preventDefault();
 	checkCPCSSGeneration();
-});
+} );
 
-function checkCPCSSGeneration( timeout = null ) {
-	var spinner              = rocketGenerateCPCSSbtn.querySelector( '.spinner' );
+const checkCPCSSGeneration = ( timeout = null ) => {
+	const spinner            = rocketGenerateCPCSSbtn.querySelector( '.spinner' );
 	spinner.style.display    = 'block';
 	spinner.style.visibility = 'visible';
 
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if ( this.readyState == 4 && this.status == 200 ) {
-			var cpcss_response = JSON.parse( this.responseText );
-			if ( cpcss_response.data.status !== 200 ) {
-				stopCPCSSGeneration( spinner );
-				cpcssNotice( cpcss_response.message, 'error' );
-				return;
-			}
-
-			if ( cpcss_response.data.status === 200 && cpcss_response.code !== 'cpcss_generation_pending') {
-				stopCPCSSGeneration( spinner );
-				cpcssNotice( cpcss_response.message, 'success' );
-				// Revert view to Regenerate.
-				rocketGenerateCPCSSbtn.querySelector( '.rocket-generate-post-cpss-btn-txt' ).innerHTML = cpcss_regenerate_btn;
-				rocketDeleteCPCSSbtn.style.display                                                     = 'block';
-				rocketCPCSSGenerate.forEach( function( item ) {
-					item.style.display = 'none';
-				});
-				rocketCPCSSReGenerate.forEach( function( item ) {
-					item.style.display = 'block';
-				});
-				return;
-			}
-
-			cpcsssGenerationPending++;
-
-			if ( cpcsssGenerationPending > 10 ) {
-				stopCPCSSGeneration( spinner );
-				cpcsssGenerationPending = 0;
-				checkCPCSSGeneration( true );
-				return;
-			}
-
-			checkCPCSSGenerationCall = setTimeout(function () {
-				checkCPCSSGeneration();
-			}, 3000);
+	const xhttp              = new XMLHttpRequest();
+	xhttp.onreadystatechange = () => {
+		if ( this.readyState !== 4 || this.status !== 200 ) {
+			return;
 		}
+
+		const cpcss_response = JSON.parse( this.responseText );
+		if ( cpcss_response.data.status !== 200 ) {
+			stopCPCSSGeneration( spinner );
+			cpcssNotice( cpcss_response.message, 'error' );
+			return;
+		}
+
+		if ( cpcss_response.data.status === 200 && cpcss_response.code !== 'cpcss_generation_pending' ) {
+			stopCPCSSGeneration( spinner );
+			cpcssNotice( cpcss_response.message, 'success' );
+
+			// Revert view to Regenerate.
+			rocketGenerateCPCSSbtn.querySelector( '.rocket-generate-post-cpss-btn-txt' ).innerHTML = cpcss_regenerate_btn;
+			rocketDeleteCPCSSbtn.style.display                                                     = 'block';
+			rocketCPCSSGenerate.forEach( item => item.style.display = 'none' );
+			rocketCPCSSReGenerate.forEach( item => item.style.display = 'block' );
+			return;
+		}
+
+		cpcsssGenerationPending++;
+
+		if ( cpcsssGenerationPending > 10 ) {
+			stopCPCSSGeneration( spinner );
+			cpcsssGenerationPending = 0;
+			checkCPCSSGeneration( true );
+			return;
+		}
+
+		checkCPCSSGenerationCall = setTimeout( () => {
+			checkCPCSSGeneration();
+		}, 3000 );
 	};
 
 	xhttp.open( 'POST', cpcss_rest_url, true );
@@ -66,32 +65,29 @@ function checkCPCSSGeneration( timeout = null ) {
 	xhttp.send( JSON.stringify( { timeout: timeout } ) );
 }
 
-function stopCPCSSGeneration( spinner ) {
+const stopCPCSSGeneration = ( spinner ) => {
 	spinner.style.display = 'none';
 	clearTimeout( checkCPCSSGenerationCall );
 }
 
-function deleteCPCSS() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if ( this.readyState == 4 && this.status == 200 ) {
-			var cpcss_response = JSON.parse( this.responseText );
-			if ( cpcss_response.data.status !== 200 ) {
-				cpcssNotice( cpcss_response.message, 'error' );
-				return;
-			}
-			cpcssNotice( cpcss_response.message, 'success' );
-
-			// Revert view to Generate.
-			rocketGenerateCPCSSbtn.querySelector( '.rocket-generate-post-cpss-btn-txt' ).innerHTML = cpcss_generate_btn;
-			rocketDeleteCPCSSbtn.style.display                                                     = 'none';
-			rocketCPCSSReGenerate.forEach( function( item ) {
-				item.style.display = 'none';
-			});
-			rocketCPCSSGenerate.forEach( function( item ) {
-				item.style.display = 'block';
-			});
+const deleteCPCSS = () => {
+	const xhttp              = new XMLHttpRequest();
+	xhttp.onreadystatechange = () => {
+		if ( this.readyState !== 4 | this.status !== 200 ) {
+			return;
 		}
+		const cpcss_response = JSON.parse( this.responseText );
+		if ( cpcss_response.data.status !== 200 ) {
+			cpcssNotice( cpcss_response.message, 'error' );
+			return;
+		}
+		cpcssNotice( cpcss_response.message, 'success' );
+
+		// Revert view to Generate.
+		rocketGenerateCPCSSbtn.querySelector( '.rocket-generate-post-cpss-btn-txt' ).innerHTML = cpcss_generate_btn;
+		rocketDeleteCPCSSbtn.style.display                                                     = 'none';
+		rocketCPCSSReGenerate.forEach( item => item.style.display = 'none' );
+		rocketCPCSSGenerate.forEach( item => item.style.display = 'block' );
 	};
 
 	xhttp.open( 'DELETE', cpcss_rest_url, true );
@@ -100,15 +96,15 @@ function deleteCPCSS() {
 	xhttp.send();
 }
 
-function cpcssNotice( msg, type ) {
+const cpcssNotice = ( msg, type ) => {
 	/* Add notice class */
-	var cpcssNotice = document.getElementById( 'cpcss_response_notice' );
+	const cpcssNotice     = document.getElementById( 'cpcss_response_notice' );
 	cpcssNotice.innerHTML = '';
-	cpcssNotice.classList.remove( 'notice', 'notice-error', 'notice-success');
+	cpcssNotice.classList.remove( 'notice', 'notice-error', 'notice-success' );
 	cpcssNotice.classList.add( 'notice', 'notice-' + type );
 
 	/* create paragraph element to hold message */
-	var p = document.createElement( 'p' );
+	const p = document.createElement( 'p' );
 	p.appendChild( document.createTextNode( msg ) );
 
 	/* Add the whole message to notice div */
