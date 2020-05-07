c487547c5ea7163f7d2948b6e914bc5eff4baadc
Soponar Cristina <crystinutzaa@gmail.com>
2020-05-07 22:44:00 +0300

    Added Integration tests

--- tests/Integration/inc/Engine/CDN/RocketCDN/CDNOptionsManager/enable.php
+++ tests/Integration/inc/Engine/CDN/RocketCDN/CDNOptionsManager/enable.php
@@ -2,23 +2,30 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\CDN\RocketCDN\CDNOptionsManager;
 
-use WPMedia\PHPUnit\Integration\TestCase;
-use WP_Rocket\Engine\CDN\RocketCDN\CDNOptionsManager;
 use WP_Rocket\Admin\Options;
 use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Tests\Integration\FilesystemTestCase;
+use WP_Rocket\Engine\CDN\RocketCDN\CDNOptionsManager;
 
 /**
  * @covers \WP_Rocket\Engine\CDN\RocketCDN\CDNOptionsManager::enable
+ * @uses :rocket_clean_domain
  * @uses \WP_Rocket\Admin\Options_Data::set
  * @uses \WP_Rocket\Admin\Options::set
  * @uses \WP_Rocket\Admin\Options::get_option_name
  *
  * @group RocketCDN
  */
-class Test_Enable extends TestCase {
+class Test_Enable extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/Engine/CDN/RocketCDN/CDNOptionsManager/enable_disable.php';
 
-	public function testShouldEnableCDNOptions() {
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldEnableCDNOptions( $cleanedUrls ) {
 		set_transient( 'rocketcdn_status', [ 'transient' ], MINUTE_IN_SECONDS );
+
+		$this->generateEntriesShouldExistAfter( $cleanedUrls );
 
 		$options      = new Options( 'wp_rocket_' );
 		$option_array = new Options_Data( $options->get( 'settings' ) );
@@ -40,6 +47,8 @@ class Test_Enable extends TestCase {
 			$this->assertArrayHasKey( $key, $options );
 			$this->assertSame( $value, $options[$key] );
 		}
+
+		$this->checkEntriesDeleted( $cleanedUrls );
 
 		$this->assertFalse( get_transient( 'rocketcdn_status' ) );
 	}
