4f5236e1ea7ac2e0227d3ed916f65d1dbcbe89c6
Rémy Perona <remperona@gmail.com>
2020-05-08 08:01:04 -0400

    move and rename test file

--- tests/Integration/inc/Engine/Cache/AdvancedCache/getAdvancedCacheContent.php
+++ tests/Integration/inc/Engine/Cache/AdvancedCache/getAdvancedCacheContent.php
@@ -6,18 +6,22 @@ use Brain\Monkey\Functions;
 use WP_Rocket\Tests\Integration\FilesystemTestCase;
 
 /**
- * @covers ::get_rocket_advanced_cache_file
+ * @covers \WP_Rocket\Engine\Cache\AdvancedCache::get_advanced_cache_content
  * @uses   ::is_rocket_generate_caching_mobile_files
  * @uses   ::rocket_get_constant
  * @uses   ::rocket_direct_filesystem
  *
  * @group  AdvancedCache
- * @group  Functions
- * @group  Files
  */
-class Test_GetRocketAdvancedCacheFile extends FilesystemTestCase {
-	protected $path_to_test_data = '/inc/functions/getRocketAdvancedCacheFile.php';
+class Test_GetAdvancedCacheContent extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/Engine/Cache/AdvancedCache/getAdvancedCacheContent.php';
 	private   $original_settings;
+	private static $advanced_cache;
+
+	public static function setUpBeforeClass() {
+		$container            = apply_filters( 'rocket_container', null );
+		self::$advanced_cache = $container->get( 'advanced_cache' );
+	}
 
 	public function setUp() {
 		parent::setUp();
@@ -47,6 +51,6 @@ class Test_GetRocketAdvancedCacheFile extends FilesystemTestCase {
 			array_merge( $this->original_settings, $this->config['settings'], $settings )
 		);
 
-		$this->assertSame( $expected, get_rocket_advanced_cache_file() );
+		$this->assertSame( $expected, self::$advanced_cache->get_advanced_cache_content() );
 	}
 }
