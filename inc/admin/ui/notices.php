61d90b763fbb2b42e5e0f75cd86a88ee4f54e58b
Rémy Perona <remperona@gmail.com>
2020-05-11 10:02:32 -0400

    undeprecate busting cache dir notice

--- inc/admin/ui/notices.php
+++ inc/admin/ui/notices.php
@@ -578,6 +578,35 @@ function rocket_warning_minify_cache_dir_permissions() {
 add_action( 'admin_notices', 'rocket_warning_minify_cache_dir_permissions' );
 
 /**
+ * This warning is displayed when the busting cache dir isn't writeable
+ *
+ * @since 2.9
+ */
+function rocket_warning_busting_cache_dir_permissions() {
+	if ( current_user_can( 'rocket_manage_options' )
+		&& ( ! rocket_direct_filesystem()->is_writable( WP_ROCKET_CACHE_BUSTING_PATH ) )
+		&& rocket_valid_key() ) {
+
+		$boxes = get_user_meta( get_current_user_id(), 'rocket_boxes', true );
+
+		if ( in_array( __FUNCTION__, (array) $boxes, true ) ) {
+			return;
+		}
+
+		$message = rocket_notice_writing_permissions( trim( str_replace( ABSPATH, '', WP_ROCKET_CACHE_BUSTING_PATH ), '/' ) );
+
+		rocket_notice_html(
+			[
+				'status'      => 'error',
+				'dismissible' => '',
+				'message'     => $message,
+			]
+		);
+	}
+}
+add_action( 'admin_notices', 'rocket_warning_busting_cache_dir_permissions' );
+
+/**
  * Confirming notice when the site has been added
  *
  * @since 2.2
--- inc/deprecated/3.6.php
+++ inc/deprecated/3.6.php
@@ -198,34 +198,3 @@ function rocket_sccss_create_cache_file( $cache_busting_path, $cache_sccss_filep
 	rocket_put_content( $cache_sccss_filepath, $content );
 }
 
-/**
- * This warning is displayed when the busting cache dir isn't writeable
- *
- * @since 2.9
- * @deprecated 3.6
- * @author Remy Perona
- */
-function rocket_warning_busting_cache_dir_permissions() {
-	_deprecated_function( __FUNCTION__ . '()', '3.6' );
-	if ( current_user_can( 'rocket_manage_options' )
-		&& ( ! rocket_direct_filesystem()->is_writable( WP_ROCKET_CACHE_BUSTING_PATH ) )
-		&& ( get_rocket_option( 'remove_query_strings', false ) )
-		&& rocket_valid_key() ) {
-
-		$boxes = get_user_meta( get_current_user_id(), 'rocket_boxes', true );
-
-		if ( in_array( __FUNCTION__, (array) $boxes, true ) ) {
-			return;
-		}
-
-		$message = rocket_notice_writing_permissions( trim( str_replace( ABSPATH, '', WP_ROCKET_CACHE_BUSTING_PATH ), '/' ) );
-
-		rocket_notice_html(
-			[
-				'status'      => 'error',
-				'dismissible' => '',
-				'message'     => $message,
-			]
-		);
-	}
-}
