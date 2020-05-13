f7c3d5722c0c6a854913f94de16d35d1239cf532
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-05-13 10:54:15 +0200

    add fixture and unit testing for sendGenerationRequest at APIClient

---
+++ tests/Unit/inc/Engine/CriticalPath/APIClient/sendGenerationRequest.php
@@ -0,0 +1,81 @@
+<?php
+namespace WP_Rocket\Tests\Unit\inc\Engine\CriticalPath\APIClient;
+
+use WP_Rocket\Engine\CriticalPath\APIClient;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
+use Brain\Monkey\Functions;
+use WP_Error;
+
+/**
+ * @covers \WP_Rocket\Engine\CriticalPath\APIClient::send_generation_request
+ * @group CriticalPath
+ */
+class Test_SendGenerationRequest extends FilesystemTestCase {
+
+	protected $path_to_test_data = '/inc/Engine/CriticalPath/APIClient/sendGenerationRequest.php';
+	protected static $mockCommonWpFunctionsInSetUp = true;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/WP_Error.php';
+	}
+
+	/**
+	 * @dataProvider nonMultisiteTestData
+	 */
+	public function testShouldDoExpected( $config, $expected ) {
+		$item_url      = isset( $config['item_url'] ) ? $config['item_url'] : '';
+		$response_code = ! isset( $config['response_data']['code'] ) ? 200 : $config['response_data']['code'];
+		$response_body = ! isset( $config['response_data']['body'] ) ? '' : $config['response_data']['body'];
+
+		Functions\expect( 'wp_remote_post' )
+			->atMost()
+			->times( 1 )
+			->with(
+				'https://cpcss.wp-rocket.me/api/job/',
+				[
+					'body' => [
+						'url' => $item_url,
+					],
+				]
+			)
+			->andReturn( 'postRequest' );
+
+		Functions\expect( 'wp_remote_retrieve_response_code' )
+			->atMost()
+			->times( 1 )
+			->with( 'postRequest' )
+			->andReturn( $response_code );
+
+		Functions\expect( 'wp_remote_retrieve_body' )
+			->atMost()
+			->times( 1 )
+			->with( 'postRequest' )
+			->andReturn( $response_body );
+
+		$api_client = new APIClient();
+		$actual = $api_client->send_generation_request( $item_url );
+
+		if( isset( $expected['success'] ) && true === $expected['success'] ){
+			//Assert success.
+			$this->assertSame( $expected['success'], $actual->success );
+			$this->assertSame( $expected['data'],    (array) $actual->data );
+		}else{
+			//Assert WP_Error.
+			$this->assertInstanceOf(WP_Error::class, $actual);
+			$this->assertSame( $expected['code'], $actual->get_error_code() );
+			$this->assertSame( $expected['message'], $actual->get_error_message() );
+			$this->assertSame( $expected['data'], $actual->get_error_data() );
+		}
+	}
+
+	public function nonMultisiteTestData() {
+		if ( empty( $this->config ) ) {
+			$this->loadConfig();
+		}
+
+		return $this->config['test_data']['non_multisite'];
+	}
+
+}
