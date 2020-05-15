68fc52bec58612c7777f1ae05b6f539d0ec3abf0
Rémy Perona <remperona@gmail.com>
2020-05-15 08:56:20 -0400

    update tests to use filesystem

--- tests/Integration/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php
+++ tests/Integration/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php
@@ -2,7 +2,7 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\CDN\RocketCDN\AdminPageSubscriber;
 
-use WPMedia\PHPUnit\Integration\TestCase;
+use WP_Rocket\Tests\Integration\FilesystemTestCase;
 
 /**
  * @covers \WP_Rocket\Engine\CDN\RocketCDN\AdminPageSubscriber::display_rocketcdn_status
@@ -16,7 +16,8 @@ use WPMedia\PHPUnit\Integration\TestCase;
  * @group  RocketCDN
  * @group  AdminOnly
  */
-class Test_DisplayRocketcdnStatus extends TestCase {
+class Test_DisplayRocketcdnStatus extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php';
 
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
