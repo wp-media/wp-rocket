f5cd9b878583aaa7d7a93b4258356178f0670cf0
hellofromtonya <hellofromtonya@knowthecode.io>
2020-05-17 11:59:57 -0500

    Fixes merge commit issue with displayRocketcdnStatus.

--- tests/Integration/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php
+++ tests/Integration/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php
@@ -2,11 +2,8 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\CDN\RocketCDN\AdminPageSubscriber;
 
-<<<<<<< HEAD
 use WP_Rocket\Tests\Integration\FilesystemTestCase;
 
-=======
->>>>>>> develop
 /**
  * @covers \WP_Rocket\Engine\CDN\RocketCDN\AdminPageSubscriber::display_rocketcdn_status
  * @uses \WP_Rocket\Engine\CDN\RocketCDN\APIClient::get_subscription_data
@@ -22,7 +19,15 @@ use WP_Rocket\Tests\Integration\FilesystemTestCase;
 class Test_DisplayRocketcdnStatus extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/displayRocketcdnStatus.php';
 
+	protected $cdn_names;
+	protected $home_url = 'http://example.org';
+
+	protected static $transients = [
+		'rocketcdn_status' => null,
+	];
+
 	public static function setUpBeforeClass() {
+		static::$use_settings_trait = true;
 		parent::setUpBeforeClass();
 
 		update_option( 'date_format', 'Y-m-d' );
@@ -31,17 +36,20 @@ class Test_DisplayRocketcdnStatus extends FilesystemTestCase {
 	public function setUp() {
 		parent::setUp();
 
+		set_current_screen( 'settings_page_wprocket' );
 		add_filter( 'home_url', [ $this, 'home_url_cb' ] );
 	}
 
 	public function tearDown() {
 		parent::tearDown();
 
+		remove_filter( 'home_url', [ $this, 'home_url_cb' ] );
+		set_current_screen( 'front' );
 		delete_transient( 'rocketcdn_status' );
 	}
 
 	/**
-	 * @dataProvider configTestData
+	 * @dataProvider providerTestData
 	 */
 	public function testShouldDisplayExpected( $rocketcdn_status, $expected, $config ) {
 		$this->home_url = $config['home_url'];
@@ -55,5 +63,17 @@ class Test_DisplayRocketcdnStatus extends FilesystemTestCase {
 			$this->format_the_html( $expected['integration'] ),
 			$this->format_the_html( $actual )
 		);
+	}
+
+	public function home_url_cb() {
+		return $this->home_url;
+	}
+
+	public function cdn_names_cb() {
+		return $this->cdn_names;
+	}
+
+	public function return_empty_string() {
+		return '';
 	}
 }
