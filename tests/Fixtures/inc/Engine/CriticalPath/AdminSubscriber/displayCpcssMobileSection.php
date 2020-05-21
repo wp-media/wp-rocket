28d66c70a6bbc1254e68122e57f41b07e7c508c1
Soponar Cristina <crystinutzaa@gmail.com>
2020-05-21 23:38:22 +0300

    Fixed fixture for integration test

--- tests/Fixtures/inc/Engine/CriticalPath/AdminSubscriber/displayCpcssMobileSection.php
+++ tests/Fixtures/inc/Engine/CriticalPath/AdminSubscriber/displayCpcssMobileSection.php
@@ -77,7 +77,7 @@ return [
 			],
 			'beacon'           => 'https://docs.wp-rocket.me/article/1266-optimize-css-delivery/?utm_source=wp_plugin&utm_medium=wp_rocket',
 		],
-		'expected' => <<<HTML
+		'expected' => '
 <div id="wpr-mobile_cpcss_view" class="wpr-tools">
 <div class="wpr-tools-col">
 <div class="wpr-title3 wpr-tools-label wpr-icon-stack">
@@ -87,17 +87,16 @@ Your website currently uses the same Critical Path CSS for both desktop and mobi
 <div class="wpr-field-description wpr-hide-on-click">
 Click the button to enable mobile-specific CPCSS for your site.</div>
 <div class="wpr-field-description wpr-hide-on-click">
-This is a one-time action and this button will be removed afterwards. <a href="https://docs.wp-rocket.me/article/1266-optimize-css-delivery/?utm_source=wp_plugin&utm_medium=wp_rocket" target="_blank" rel="noopener noreferrer">More info</a></div>
+This is a one-time action and this button will be removed afterwards. <a href="https://docs.wp-rocket.me/article/1266-optimize-css-delivery/?utm_source=wp_plugin&#038;utm_medium=wp_rocket" target="_blank" rel="noopener noreferrer">More info</a>
 </div>
 <div class="wpr-field-description wpr-field wpr-isHidden wpr-show-on-click">
-Your site is now using mobile-specific critical path CSS. <a href="https://docs.wp-rocket.me/article/1266-optimize-css-delivery/?utm_source=wp_plugin&utm_medium=wp_rocket" target="_blank" rel="noopener noreferrer">More info</a></div>
+Your site is now using mobile-specific critical path CSS. <a href="https://docs.wp-rocket.me/article/1266-optimize-css-delivery/?utm_source=wp_plugin&#038;utm_medium=wp_rocket" target="_blank" rel="noopener noreferrer">More info</a></div>
 </div>
 <div class="wpr-tools-col">
 <button id="wpr-action-rocket_enable_mobile_cpcss" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-refresh">
 Generate Mobile Specific CPCSS</button>
 </div>
-</div>
-HTML,
+</div>',
 	],
 
 	'testBailoutAsyncMobileAlreadyActive' => [
