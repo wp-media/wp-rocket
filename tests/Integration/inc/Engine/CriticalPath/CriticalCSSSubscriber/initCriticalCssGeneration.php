400902a933ab0e43b3f9aa4ea10bb58dcd38cdbc
Rémy Perona <remperona@gmail.com>
2020-05-22 11:24:41 -0400

    add tests for init_critical_css_generation

---
+++ tests/Integration/inc/Engine/CriticalPath/CriticalCSSSubscriber/initCriticalCssGeneration.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Engine\CriticalPath\CriticalCSSSubscriber;
+
+use WPDieException;
+use WP_Rocket\Tests\Integration\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\CriticalPath\CriticalCSSSubscriber::init_critical_css_generation
+ *
+ * @group  AdminOnly
+ * @group  Subscribers
+ * @group  CriticalPath
+ * @group InitGeneration
+ */
+class Test_InitCriticalCssGeneration extends TestCase {
+	protected static $transients = [
+		'rocket_critical_css_generation_triggered' => null,
+	];
+	protected $user_id = 0;
+
+	public function setUp() {
+		parent::setUp();
+
+		$role = get_role( 'administrator' );
+		$role->add_cap( 'rocket_regenerate_critical_css' );
+
+		unset( $_GET['_wpnonce'] );
+	}
+
+	public function tearDown() {
+		if ( $this->user_id > 0 ) {
+			wp_delete_user( $this->user_id );
+		}
+
+		$role = get_role( 'administrator' );
+		$role->remove_cap( 'rocket_regenerate_critical_css' );
+
+		unset( $_GET['_wpnonce'] );
+
+		remove_filter( 'pre_get_rocket_option_do_caching_mobile_files', [ $this, 'return_true' ] );
+		remove_filter( 'pre_get_rocket_option_async_css_mobile', [ $this, 'return_true' ] );
+		remove_filter( 'wp_safe_redirect', [ $this, 'return_empty_string' ] );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldDoExpected( $config, $expected ) {
+		if ( isset( $config['nonce'] ) ) {
+			$_GET['_wpnonce'] = wp_create_nonce( $config['nonce'] );
+		}
+
+		if ( ! $config['cap'] ) {
+			$this->user_id = $this->factory->user->create( [ 'role' => 'editor' ] );
+		} else {
+			$this->user_id = $this->factory->user->create( [ 'role' => 'administrator' ] );
+		}
+
+		wp_set_current_user( $this->user_id );
+
+		add_filter( 'wp_safe_redirect', [ $this, 'return_empty_string' ] );
+
+		if ( $config['mobile'] ) {
+			add_filter( 'pre_get_rocket_option_do_caching_mobile_files', [ $this, 'return_true' ] );
+			add_filter( 'pre_get_rocket_option_async_css_mobile', [ $this, 'return_true' ] );
+		}
+
+		if ( $config['referer'] ) {
+			$_REQUEST['_wp_http_referer'] = addslashes( $config['referer'] );
+			$_SERVER['REQUEST_URI']       = $_REQUEST['_wp_http_referer'];
+		}
+
+		$this->expectException( WPDieException::class );
+		
+		do_action( 'admin_post_rocket_purge_rocketcdn' );
+
+		if ( ! empty( $config['referer'] ) && false === strpos( $config['referer'], 'wprocket' ) ) {
+			$this->assertSame(
+				1,
+				get_transient( 'rocket_critical_css_generation_triggered' )
+			);
+		} else {
+			$this->assertFalse( get_transient( 'rocket_critical_css_generation_triggered' ) );
+		}
+	}
+
+	public function return_empty_string() {
+		return '';
+	}
+}
---
+++ tests/Unit/inc/Engine/CriticalPath/CriticalCSSSubscriber/initCriticalCssGeneration.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\Engine\CriticalPath\CriticalCSSSubscriber;
+
+use Brain\Monkey\Functions;
+use Mockery;
+use WPDieException;
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Engine\CriticalPath\CriticalCSS;
+use WP_Rocket\Engine\CriticalPath\CriticalCSSGeneration;
+use WP_Rocket\Engine\CriticalPath\CriticalCSSSubscriber;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\CriticalPath\CriticalCSSSubscriber::init_critical_css_generation
+ *
+ * @group  Subscribers
+ * @group  CriticalPath
+ */
+class Test_InitCriticalCssGeneration extends TestCase {
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/WPDieException.php';
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		unset( $_GET['_wpnonce'] );
+	}
+
+	public function tearDown() {
+		unset( $_GET['_wpnonce'] );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldDoExpected( $config, $expected ) {
+		$_GET['_wpnonce'] = $config['nonce'];
+
+		if ( ! isset( $config['nonce'] ) ) {
+			Functions\expect( 'wp_verify_nonce' )->never();
+		} elseif ( isset( $config['nonce'] ) && 'valid' !== $config['nonce'] ) {
+			Functions\expect( 'wp_verify_nonce' )->once()->with( $config['nonce'], 'rocket_generate_critical_css' )->andReturn( false );
+		} else {
+			Functions\expect( 'wp_verify_nonce' )->once()->with( $config['nonce'], 'rocket_generate_critical_css' )->andReturn( true );
+		}
+
+		if ( 'valid' === $config['nonce'] ) {
+			Functions\expect( 'wp_nonce_ays' )->never();
+		} else {
+			Functions\expect( 'wp_nonce_ays' )->once()->andReturnUsing( function() {
+				throw new WPDieException;
+			} );
+		}
+
+		if ( ! isset( $config['cap'] ) ) {
+			Functions\expect( 'current_user_can' )->never();
+		} elseif ( ! $config['cap'] ) {
+			Functions\expect( 'current_user_can' )->once()->with( 'rocket_regenerate_critical_css' )->andReturn( false );
+			Functions\expect( 'wp_die' )->once()->andReturnUsing( function() {
+				throw new WPDieException;
+			} );
+		} else {
+			Functions\expect( 'current_user_can' )->once()->with( 'rocket_regenerate_critical_css' )->andReturn( true );
+		}
+
+		Functions\when( 'sanitize_key' )->returnArg();
+		Functions\when( 'wp_get_referer' )->justReturn( $config['referer'] );
+
+		$critical_css = Mockery::mock( CriticalCSS::class );
+		$subscriber   = new CriticalCSSSubscriber(
+			$critical_css,
+			Mockery::mock( Options_Data::class )
+		);
+
+		if ( ! $expected ) {
+			$this->expectException( WPDieException::class );
+		} else {
+			$critical_css->shouldReceive( 'is_async_css_mobile' )
+				->once()
+				->andReturn( $config['mobile'] );
+
+			if ( ! $config['mobile'] ) {
+				$critical_css->shouldReceive( 'process_handler' )
+				->once()
+				->with( 'default' );
+			} else {
+				$critical_css->shouldReceive( 'process_handler' )
+				->once()
+				->with( 'all' );
+			}
+
+			if ( 'settings_page_wprocket' !== $config['referer'] ) {
+				Functions\expect( 'set_transient' )
+					->once()
+					->with( 'rocket_critical_css_generation_triggered', 1 );
+			} else {
+				Functions\expect( 'set_transient' )->never();
+			}
+
+			Functions\expect( 'wp_safe_redirect' )->once();
+			Functions\when( 'esc_url_raw' )->returnArg();
+			Functions\expect( 'wp_die' )->once();
+		}
+		
+		$subscriber->init_critical_css_generation();
+	}
+}
