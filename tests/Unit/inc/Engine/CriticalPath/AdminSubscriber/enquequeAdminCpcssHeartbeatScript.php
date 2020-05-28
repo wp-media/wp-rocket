88a797150fde10bbef491df22ae66d95f3811ae8
Soponar Cristina <crystinutzaa@gmail.com>
2020-05-28 13:36:46 +0300

    Add unit & integration tests enqueueAdminCPCSSHeartbeatScript

---
+++ tests/Unit/inc/Engine/CriticalPath/AdminSubscriber/enquequeAdminCpcssHeartbeatScript.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\Engine\CriticalPath\AdminSubscriber;
+
+use Brain\Monkey\Functions;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\CriticalPath\AdminSubscriber::enqueue_admin_cpcss_heartbeat_script
+ * @uses   ::rocket_get_constant
+ *
+ * @group  CriticalPath
+ */
+class Test_EnqueueAdminCpcssHeartbeatScript extends TestCase {
+	use GenerateTrait;
+
+	protected static $mockCommonWpFunctionsInSetUp = true;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->setUpMocks();
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldEnqueueAdminScript( $config, $expected ) {
+		$this->options
+				->shouldReceive( 'get' )
+				->with( 'async_css', 0 )
+				->andReturn( $config['options']['async_css'] );
+
+		if ( $expected ) {
+			$this->assertExpected( $config );
+		} else {
+			$this->assertNotExpected();
+		}
+
+		$this->subscriber->enqueue_admin_cpcss_heartbeat_script();
+	}
+
+	private function assertExpected() {
+		Functions\expect( 'wp_create_nonce' )
+			->once()
+			->with( 'cpcss_heartbeat_nonce' )
+			->andReturn( 'wp_cpcss_heartbeat_nonce' );
+
+		Functions\expect( 'wp_enqueue_script' )
+			->once()
+			->with(
+				'wpr-heartbeat-cpcss-script',
+				rocket_get_constant( 'WP_ROCKET_ASSETS_JS_URL' ) . 'wpr-cpcss-heartbeat.js',
+				[],
+				rocket_get_constant( 'WP_ROCKET_VERSION' ),
+				true
+			)
+			->andReturnNull();
+
+		Functions\expect( 'wp_localize_script' )
+			->once()
+			->with(
+				'wpr-heartbeat-cpcss-script',
+				'rocket_cpcss_heartbeat',
+				[
+					'nonce' => 'wp_cpcss_heartbeat_nonce',
+				]
+			)
+			->andReturnNull();
+	}
+
+	private function assertNotExpected() {
+		Functions\expect( 'wp_enqueue_script' )->never();
+		Functions\expect( 'wp_create_nonce' )->never();
+		Functions\expect( 'wp_localize_script' )->never();
+	}
+}
