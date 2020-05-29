59960e827a7a1f970501ce9a74a7902c5b2b0c2d
Tonya Mork <hellofromtonya@knowthecode.io>
2020-05-29 10:51:08 -0500

    Updates integration test to init AdminBar.

--- tests/Integration/inc/Engine/CriticalPath/Admin/Subscriber/addRegenerateMenuItem.php
+++ tests/Integration/inc/Engine/CriticalPath/Admin/Subscriber/addRegenerateMenuItem.php
@@ -2,7 +2,6 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\CriticalPath\Admin\Subscriber;
 
-use Brain\Monkey\Functions;
 use WP_Rocket\Tests\Integration\AdminTestCase;
 
 /**
@@ -16,17 +15,17 @@ class Test_AddRegenerateMenuItem extends AdminTestCase {
 	use ProviderTrait;
 
 	protected static $class_name = 'Admin';
-	protected $user_id = 0;
-	private $filter;
-	private $option;
+	protected        $user_id    = 0;
+	private          $filter;
+	private          $option;
 
 	public function setUp() {
 		parent::setUp();
 
 		$this->setRoleCap( 'administrator', 'rocket_regenerate_critical_css' );
 
-		add_filter( 'show_admin_bar', [ $this, 'return_true'] );
-		add_filter( 'pre_get_rocket_option_async_css', [ $this, 'async_css'] );
+		add_filter( 'show_admin_bar', [ $this, 'return_true' ] );
+		add_filter( 'pre_get_rocket_option_async_css', [ $this, 'async_css' ] );
 		add_filter( 'do_rocket_critical_css_generation', [ $this, 'filter_generation' ] );
 	}
 
@@ -35,42 +34,37 @@ class Test_AddRegenerateMenuItem extends AdminTestCase {
 
 		$this->removeRoleCap( 'administrator', 'rocket_regenerate_critical_css' );
 
-		remove_filter( 'show_admin_bar', [ $this, 'return_true'] );
-		remove_filter( 'pre_get_rocket_option_async_css', [ $this, 'async_css'] );
+		remove_filter( 'show_admin_bar', [ $this, 'return_true' ] );
+		remove_filter( 'pre_get_rocket_option_async_css', [ $this, 'async_css' ] );
 		remove_filter( 'do_rocket_critical_css_generation', [ $this, 'filter_generation' ] );
 
-		unset( $_SERVER['REQUEST_URI' ] );
+		unset( $_SERVER['REQUEST_URI'] );
 	}
 
 	/**
 	 * @dataProvider providerTestData
 	 */
 	public function testShouldDoExpected( $cap, $admin, $option, $filter, $request, $expected ) {
-		global $wp_admin_bar;
-
-		if( $cap ) {
+		if ( $cap ) {
 			$this->setCurrentUser( 'administrator' );
 		} else {
 			$this->setCurrentUser( 'editor' );
 		}
 
-		Functions\when( 'is_admin' )->justReturn( $admin );
+		$this->option           = $option;
+		$this->filter           = $filter;
+		$_SERVER['REQUEST_URI'] = $request;
+		$wp_admin_bar           = $this->initAdminBar();
 
-		$this->option = $option;
-		$this->filter = $filter;
+		// Fire the hook.
+		do_action_ref_array( 'admin_bar_menu', [ $wp_admin_bar ] );
 
-		$_SERVER['REQUEST_URI' ] = $request;
-
-		$this->fireAdminInit();
-		do_action_ref_array( 'admin_bar_menu', array( &$wp_admin_bar )  );
-
+		// Check the results.
+		$actual = $wp_admin_bar->get_node( 'regenerate-critical-path' );
 		if ( false === $expected ) {
-			$this->assertNull( $wp_admin_bar->get_node( 'regenerate-critical-path') );
+			$this->assertNull( $actual );
 		} else {
-			$this->assertEquals(
-				$wp_admin_bar->get_node( 'regenerate-critical-path' ),
-				$expected
-			);
+			$this->assertEquals( $expected, $actual );
 		}
 	}
 
@@ -80,5 +74,17 @@ class Test_AddRegenerateMenuItem extends AdminTestCase {
 
 	public function filter_generation() {
 		return $this->filter;
+	}
+
+	protected function initAdminBar() {
+		global $wp_admin_bar;
+
+		set_current_screen( 'edit.php' );
+		$this->assertTrue( _wp_admin_bar_init() );
+
+		$this->assertTrue( is_admin_bar_showing() );
+		$this->assertInstanceOf( 'WP_Admin_Bar', $wp_admin_bar );
+
+		return $wp_admin_bar;
 	}
 }
