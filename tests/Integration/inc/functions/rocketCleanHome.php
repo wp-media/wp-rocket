9c4cbf1dcb23668007f8e0a6efd46bb02eb0c573
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-05-29 18:05:58 +0200

    add integration test

--- tests/Integration/inc/functions/rocketCleanHome.php
+++ tests/Integration/inc/functions/rocketCleanHome.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace WP_Rocket\Tests\Unit\inc\functions;
+namespace WP_Rocket\Tests\Integration\inc\functions;
 
+use Brain\Monkey\Functions;
 use WP_Rocket\Tests\Integration\FilesystemTestCase;
 
 /**
  * @covers ::rocket_clean_home()
  * @group Functions
+ * @group vfs
  */
 class Test_RocketCleanHome extends FilesystemTestCase {
 	protected $path_to_test_data   = '/inc/functions/rocketCleanHome.php';
@@ -15,8 +17,16 @@ class Test_RocketCleanHome extends FilesystemTestCase {
 	 * @dataProvider providerTestData
 	 */
 	public function testShouldCleanHome( $config, $expected ) {
+		Functions\when( 'home_url' )->justReturn( $config['home_url'] );
+		rocket_clean_home('', $this->filesystem);
 
-		$actual = rocket_clean_home();
+		foreach ($expected['removed_files'] as $removed_file) {
+			$this->assertFalse( $this->filesystem->exists( $this->config['vfs_dir'].$removed_file ) );
+		}
+
+		foreach ($expected['not_removed_files'] as $not_removed_file) {
+			$this->assertTrue( $this->filesystem->exists( $this->config['vfs_dir'].$not_removed_file ) );
+		}
 	}
 
 }
