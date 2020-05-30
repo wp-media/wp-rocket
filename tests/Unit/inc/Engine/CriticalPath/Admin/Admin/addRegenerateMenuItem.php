d2e80665087fca8ba3ebf03fbd400459879fd21a
hellofromtonya <hellofromtonya@knowthecode.io>
2020-05-30 12:11:56 -0500

    Improves Admin\Admin::add_regenerate_menu_item unit tests.

--- tests/Unit/inc/Engine/CriticalPath/Admin/Admin/addRegenerateMenuItem.php
+++ tests/Unit/inc/Engine/CriticalPath/Admin/Admin/addRegenerateMenuItem.php
@@ -22,6 +22,8 @@ class Test_AddRegenerateMenuItem extends TestCase {
 
 	protected static $mockCommonWpFunctionsInSetUp = true;
 
+	private $admin;
+
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
 
@@ -41,7 +43,7 @@ class Test_AddRegenerateMenuItem extends TestCase {
 
 	protected function tearDown() {
 		parent::tearDown();
-	
+
 		unset( $_SERVER['REQUEST_URI'] );
 	}
 
@@ -49,32 +51,19 @@ class Test_AddRegenerateMenuItem extends TestCase {
 	 * @dataProvider configTestData
 	 */
 	public function testShouldDoExpected( $cap, $admin, $option, $filter, $request, $expected ) {
-		Functions\when( 'current_user_can' )->justReturn( $cap );
-		Functions\when( 'is_admin' )->justReturn( $admin );
-		Functions\when( 'admin_url' )->alias( function( $path ) {
-			return "http://example.org/wp-admin/{$path}";
-		} );
-		Functions\when( 'wp_nonce_url' )->alias( function( $url ) {
-			return str_replace( '&', '&amp;', "{$url}&_wpnonce=wp_rocket_nonce" );
-		} );
-		Functions\when( 'wp_unslash' )->alias( function( $value ) {
-			return stripslashes( $value );
-		} );
-		Functions\when( 'remove_query_arg' )->returnArg( 2 );
+		$_SERVER['REQUEST_URI'] = $request;
+		$wp_admin_bar           = new WP_Admin_Bar();
 
-		$this->options->shouldReceive( 'get' )
-			->atMost()
-			->times( 1 )
-			->with( 'async_css', 0 )
-			->andReturn( $option );
+		Functions\expect( 'current_user_can' )
+			->once()
+			->with( 'rocket_regenerate_critical_css' )
+			->andReturn( $cap );
 
-		Filters\expectApplied( 'do_rocket_critical_css_generation' )
-			->atMost()
-			->times( 1 )
-			->andReturn( $filter );
-
-		$_SERVER['REQUEST_URI' ] = $request;
-		$wp_admin_bar = new WP_Admin_Bar();
+		if ( $cap ) {
+			$this->assertMocks( $admin, $option, $filter );
+		} else {
+			$this->assertBailsOut();
+		}
 
 		if ( false === $expected ) {
 			$this->assertNull( $this->admin->add_regenerate_menu_item( $wp_admin_bar ) );
@@ -86,5 +75,95 @@ class Test_AddRegenerateMenuItem extends TestCase {
 				$expected
 			);
 		}
+	}
+
+	private function assertBailsOut() {
+		Functions\expect( 'is_admin' )->never();
+
+		$this->options->shouldReceive( 'get' )->with( 'async_css', 0 )->never();
+
+	}
+
+	private function assertMocks( $admin, $option, $filter ) {
+		Functions\expect( 'is_admin' )->andReturn( $admin );
+
+		if ( ! $admin ) {
+			return $this->assertAsycnCssDisabled();
+		}
+
+		$this->options
+			->shouldReceive( 'get' )
+			->once()
+			->with( 'async_css', 0 )
+			->andReturn( $option );
+
+		if ( $option ) {
+			Filters\expectApplied( 'do_rocket_critical_css_generation' )
+				->once()
+				->with( true )
+				->andReturn( $filter );
+		} else {
+			return $this->assertAsycnCssDisabled();
+		}
+
+		if ( ! $filter ) {
+			return $this->assertBailOutForFilter();
+		}
+
+		$request_uri = $this->assertUsesRequestUri();
+		$referer     = '' === $request_uri
+			? ''
+			: '&_wp_http_referer=' . rawurlencode( $request_uri );
+
+		$action = 'rocket_generate_critical_css';
+		$admin_path = "admin-post.php?action={$action}{$referer}";
+		$admin_url  = "http://example.org/wp-admin/{$admin_path}";
+
+		Functions\expect( 'admin_url' )
+			->once()
+			->with( $admin_path )
+			->andReturn( $admin_url );
+		Functions\expect( 'wp_nonce_url' )
+			->once()
+			->with( $admin_url, $action )
+			->andReturnUsing(
+				function ( $url ) {
+					return str_replace( '&', '&amp;', "{$url}&_wpnonce=wp_rocket_nonce" );
+				}
+			);
+	}
+
+	private function assertAsycnCssDisabled() {
+		Filters\expectApplied( 'do_rocket_critical_css_generation' )->never();
+		Functions\expect( 'admin_url' )->never();
+		Functions\expect( 'wp_nonce_url' )->never();
+	}
+
+	private function assertBailOutForFilter() {
+		Functions\expect( 'admin_url' )->never();
+		Functions\expect( 'wp_nonce_url' )->never();
+	}
+
+	private function assertUsesRequestUri() {
+		if ( empty( $_SERVER['REQUEST_URI'] ) ) {
+			Functions\expect( 'wp_unslash' )->never();
+			Functions\expect( 'remove_query_arg' )->never();
+
+			return '';
+		}
+
+		$request_uri = stripslashes( $_SERVER['REQUEST_URI'] );
+
+		Functions\expect( 'wp_unslash' )
+			->once()
+			->with( $_SERVER['REQUEST_URI'] )
+			->andReturn( $request_uri );
+
+		Functions\expect( 'remove_query_arg' )
+			->once()
+			->with( 'fl_builder', $request_uri )
+			->andReturn( $request_uri );
+
+		return $request_uri;
 	}
 }
