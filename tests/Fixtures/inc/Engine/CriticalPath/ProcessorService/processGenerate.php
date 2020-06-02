7905423bde040b09354fe591a110ab7c56609497
Soponar Cristina <crystinutzaa@gmail.com>
2020-06-02 23:50:36 +0300

    [WIP] - Revisit CriticalPath ProcessorService::process_generate() tests

--- tests/Fixtures/inc/Engine/CriticalPath/ProcessorService/processGenerate.php
+++ tests/Fixtures/inc/Engine/CriticalPath/ProcessorService/processGenerate.php
@@ -1,181 +1,248 @@
 <?php
 
 return [
-	'vfs_dir'   => 'wp-content/cache/critical-css/',
-
-	'test_data' => [
-		'testShouldBailoutOnRequestTimeOut'       => [
-			'config'   => [
-				'current_user_can'   => true,
-				'post_data'          => [
-					'ID'           => 21,
-					'post_type'    => 'post',
-					'post_status'  => 'publish',
-					'post_title'   => 'CPCSS title',
-					'post_content' => 'content',
-				],
-				'cpcss_exists_after' => false,
-				'request_timeout'    => true,
+	'testShouldBailoutOnRequestTimeOut'       => [
+		'config'   => [
+			'current_user_can'   => true,
+			'post_data'          => [
+				'ID'           => 21,
+				'post_type'    => 'post',
+				'post_status'  => 'publish',
+				'post_title'   => 'CPCSS title',
+				'post_content' => 'content',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_timeout',
-				'message' => 'Critical CSS for http://example.org/?p=21 timeout. Please retry a little later.',
-				'data'    => [
-					'status' => 400,
-				],
+			'cpcss_exists_after' => false,
+			'request_timeout'    => true,
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_timeout',
+			'message' => 'Critical CSS for http://example.org/?p=21 timeout. Please retry a little later.',
+			'data'    => [
+				'status' => 400,
 			],
 		],
-		'testShouldBailoutIfPostRequest400'       => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'           => 21,
-					'post_type'    => 'post',
-					'post_status'  => 'publish',
-					'post_title'   => 'CPCSS title',
-					'post_content' => 'content',
-				],
-				'generate_post_request_data' => [
-					'code' => 400,
-					'body' => '{}',
-				],
-				'cpcss_exists_after'         => false,
+	],
+	'testShouldBailoutIfPostRequest400'       => [
+		'config'   => [
+			'current_user_can'              => true,
+			'post_data'                     => [
+				'ID'           => 21,
+				'post_type'    => 'post',
+				'post_status'  => 'publish',
+				'post_title'   => 'CPCSS title',
+				'post_content' => 'content',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_failed',
-				'message' => 'Critical CSS for http://example.org/?p=21 not generated.',
-				'data'    => [
+			'generate_post_request_data'    => [
+				'code' => 400,
+				'body' => '{}',
+			],
+			'cpcss_exists_after'            => false,
+			'send_generation_request_error' => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated.',
+				[
 					'status' => 400,
-				],
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated.',
+			'data'    => [
+				'status' => 400,
 			],
 		],
-		'testShouldBailoutIfPostRequestCodeNotExpected' => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'          => 21,
-					'post_type'   => 'post',
-					'post_status' => 'publish',
-				],
-				'generate_post_request_data' => [
-					'code' => 403,
-					'body' => '{}',
-				],
-				'cpcss_exists_after'         => false,
+	],
+	'testShouldBailoutIfPostRequestCodeNotExpected' => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_failed',
-				'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an invalid response code.',
-				'data'    => [
+			'generate_post_request_data' => [
+				'code' => 403,
+				'body' => '{}',
+			],
+			'cpcss_exists_after'         => false,
+			'send_generation_request_error' => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an invalid response code.',
+				[
 					'status' => 403,
-				],
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an invalid response code.',
+			'data'    => [
+				'status' => 403,
 			],
 		],
-		'testShouldBailoutIfPostRequestBodyEmpty' => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'          => 21,
-					'post_type'   => 'post',
-					'post_status' => 'publish',
-				],
-				'generate_post_request_data' => [
-					'code' => 200,
-					'body' => '{}',
-				],
-				'cpcss_exists_after'         => false,
+	],
+	'testShouldBailoutIfPostRequestBodyEmpty' => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_failed',
-				'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an empty response.',
-				'data'    => [
+			'generate_post_request_data' => [
+				'code' => 200,
+				'body' => '{}',
+			],
+			'cpcss_exists_after'         => false,
+			'send_generation_request_error' => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an empty response.',
+				[
 					'status' => 400,
-				],
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: The API returned an empty response.',
+			'data'    => [
+				'status' => 400,
 			],
 		],
-		'testShouldBailoutIfGetRequestCode400'    => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'          => 21,
-					'post_type'   => 'post',
-					'post_status' => 'publish',
-				],
-				'generate_post_request_data' => [
-					'code' => 200,
-					'body' => '{"success":true,"data":{"id":1}}',
-				],
-				'generate_get_request_data'  => [
-					'code' => 400,
-					'body' => '{"status":400,"success":false,"message":"Error message"}',
-				],
-				'cpcss_exists_after'         => false,
-				'cpcss_job_id'               => false,
+	],
+	'testShouldBailoutIfGetRequestCode400'    => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_failed',
-				'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: Error message',
-				'data'    => [
+			'generate_post_request_data' => [
+				'code' => 200,
+				'body' => '{"success":true,"data":{"id":1}}',
+			],
+			'generate_get_request_data'  => [
+				'code' => 400,
+				'body' => '{"status":400,"success":false,"message":"Error message"}',
+			],
+			'cpcss_exists_after'         => false,
+			'cpcss_job_id'               => false,
+			'get_job_details_error'      => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated. Error: Error message',
+				[
 					'status' => 400,
-				],
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: Error message',
+			'data'    => [
+				'status' => 400,
 			],
 		],
-		'testShouldBailoutIfGetRequestCode404'    => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'          => 21,
-					'post_type'   => 'post',
-					'post_status' => 'publish',
-				],
-				'generate_post_request_data' => [
-					'code' => 200,
-					'body' => '{"success":true,"data":{"id":1}}',
-				],
-				'generate_get_request_data'  => [
-					'code' => 404,
-					'body' => '{"status":404,"success":false,"message":"Job not found"}',
-				],
-				'cpcss_exists_after'         => false,
-				'cpcss_job_id'               => false,
+	],
+	'testShouldBailoutIfGetRequestCode404'    => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
 			],
-			'expected' => [
-				'success' => false,
-				'code'    => 'cpcss_generation_failed',
-				'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: Job not found',
-				'data'    => [
+			'generate_post_request_data' => [
+				'code' => 200,
+				'body' => '{"success":true,"data":{"id":1}}',
+			],
+			'generate_get_request_data'  => [
+				'code' => 404,
+				'body' => '{"status":404,"success":false,"message":"Job not found"}',
+			],
+			'cpcss_exists_after'         => false,
+			'cpcss_job_id'               => false,
+			'get_job_details_error'      => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated. Error: Job not found',
+				[
 					'status' => 404,
-				],
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated. Error: Job not found',
+			'data'    => [
+				'status' => 404,
 			],
 		],
-		'testShouldSaveCPCSSForPost'              => [
-			'config'   => [
-				'current_user_can'           => true,
-				'post_data'                  => [
-					'ID'          => 21,
-					'post_type'   => 'post',
-					'post_status' => 'publish',
-				],
-				'generate_post_request_data' => [
-					'code' => 200,
-					'body' => '{"success":true,"data":{"id":1}}',
-				],
-				'generate_get_request_data'  => [
-					'code' => 200,
-					'body' => '{"status":200,"data":{"state":"complete","critical_path":"body{color:#000}"}}',
-				],
-				'cpcss_exists_after'         => true,
-				'cpcss_job_id'               => false,
+	],
+	'testShouldNotSaveCPCSSForPost'           => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
 			],
-			'expected' => [
-				'code'    => 'cpcss_generation_successful',
-				'message' => 'Critical CSS for http://example.org/?p=21 generated.'
+			'generate_post_request_data' => [
+				'code' => 200,
+				'body' => '{"success":true,"data":{"id":1}}',
 			],
-		]
+			'generate_get_request_data'  => [
+				'code' => 200,
+				'body' => '{"status":200,"data":{"state":"complete","critical_path":"body{color:#000}"}}',
+			],
+			'cpcss_exists_after'         => true,
+			'cpcss_job_id'               => false,
+			'save_cpcss'                 => new WP_Error(
+				'cpcss_generation_failed',
+				'Critical CSS for http://example.org/?p=21 not generated.',
+				[
+					'status' => 400,
+				]
+			),
+		],
+		'expected' => [
+			'success' => false,
+			'code'    => 'cpcss_generation_failed',
+			'message' => 'Critical CSS for http://example.org/?p=21 not generated.',
+			'data'    => [
+				'status' => 400,
+			],
+		],
+	],
+	'testShouldSaveCPCSSForPost'              => [
+		'config'   => [
+			'current_user_can'           => true,
+			'post_data'                  => [
+				'ID'          => 21,
+				'post_type'   => 'post',
+				'post_status' => 'publish',
+			],
+			'generate_post_request_data' => [
+				'code' => 200,
+				'body' => '{"success":true,"data":{"id":1}}',
+			],
+			'generate_get_request_data'  => [
+				'code' => 200,
+				'body' => '{"status":200,"data":{"state":"complete","critical_path":"body{color:#000}"}}',
+			],
+			'cpcss_exists_after'         => true,
+			'cpcss_job_id'               => false,
+			'save_cpcss'                 => true,
+		],
+		'expected' => [
+			'code'    => 'cpcss_generation_successful',
+			'message' => 'Critical CSS for http://example.org/?p=21 generated.'
+		],
 	],
 ];
--- tests/Unit/inc/Engine/CriticalPath/ProcessorService/processGenerate.php
+++ tests/Unit/inc/Engine/CriticalPath/ProcessorService/processGenerate.php
@@ -2,161 +2,114 @@
 
 namespace WP_Rocket\Tests\Unit\inc\Engine\CriticalPath\ProcessorService;
 
+use Mockery;
+use WP_Error;
 use Brain\Monkey\Functions;
+use WP_Rocket\Tests\Unit\TestCase;
 use WP_Rocket\Engine\CriticalPath\APIClient;
 use WP_Rocket\Engine\CriticalPath\ProcessorService;
 use WP_Rocket\Engine\CriticalPath\DataManager;
-use WP_Rocket\Tests\Unit\FilesystemTestCase;
-use WP_Error;
 
 /**
  * @covers \WP_Rocket\Engine\CriticalPath\ProcessorService::process_generate
  *
- * @group  CriticalPath
- * @group  vfs
+ * @group  CriticalPathX
  */
-class Test_ProcessGenerate extends FilesystemTestCase {
-	protected $path_to_test_data = '/inc/Engine/CriticalPath/ProcessorService/processGenerate.php';
+class Test_ProcessGenerate extends TestCase {
 	protected static $mockCommonWpFunctionsInSetUp = true;
 
 	/**
-	 * @dataProvider dataProvider
+	 * @dataProvider configTestData
 	 */
 	public function testShouldDoExpected( $config, $expected ) {
-		$post_id                      = isset( $config['post_data'] )
+		$post_id                       = isset( $config['post_data'] )
 			? $config['post_data']['ID']
 			: 0;
-		$post_type                    = ! isset( $config['post_data']['post_type'] )
+		$post_type                     = ! isset( $config['post_data']['post_type'] )
 			? 'post'
 			: $config['post_data']['post_type'];
-		$post_status                  = isset( $config['post_data']['post_status'] )
+		$post_status                   = isset( $config['post_data']['post_status'] )
 			? $config['post_data']['post_status']
 			: false;
-		$post_request_response_code   = ! isset( $config['generate_post_request_data']['code'] )
+		$post_request_response_code    = ! isset( $config['generate_post_request_data']['code'] )
 			? 200
 			: $config['generate_post_request_data']['code'];
-		$post_request_response_body   = ! isset( $config['generate_post_request_data']['body'] )
-			? ''
-			: $config['generate_post_request_data']['body'];
-		$get_request_response_body    = ! isset( $config['generate_get_request_data']['body'] )
-			? ''
-			: $config['generate_get_request_data']['body'];
-		$saved_cpcss_job_id           = isset( $config['cpcss_job_id'] )
+		$saved_cpcss_job_id            = isset( $config['cpcss_job_id'] )
 			? $config['cpcss_job_id']
 			: false;
-		$cpcss_post_job_body          = ! isset( $config['generate_post_request_data']['body'] )
+		$cpcss_post_job_body           = ! isset( $config['generate_post_request_data']['body'] )
 			? ''
 			: json_decode( $config['generate_post_request_data']['body'] );
-		$cpcss_post_job_id            = ! isset( $cpcss_post_job_body->data->id )
+		$cpcss_post_job_id             = ! isset( $cpcss_post_job_body->data->id )
 			? false
 			: $cpcss_post_job_body->data->id;
-		$get_request_response_code    = ! isset( $config['generate_get_request_data']['code'] )
+		$get_request_response_code     = ! isset( $config['generate_get_request_data']['code'] )
 			? 200
 			: $config['generate_get_request_data']['code'];
-		$get_request_response_decoded = ! isset( $config['generate_get_request_data']['body'] )
+		$get_request_response_decoded  = ! isset( $config['generate_get_request_data']['body'] )
 			? ''
 			: json_decode( $config['generate_get_request_data']['body'] );
-		$get_request_response_state   = ! isset( $get_request_response_decoded->data->state )
-			? false
-			: $get_request_response_decoded->data->state;
-		$request_timeout              = isset( $config['request_timeout'] )
+		$request_timeout               = isset( $config['request_timeout'] )
 			? $config['request_timeout']
 			: false;
-		$item_path = "posts/{$post_type}-{$post_id}.css";
-		$file                         = $this->config['vfs_dir'] . "1/".$item_path;
-		$item_url = ('post_not_exists' === $expected['code'])
+		$item_path                     = "posts/{$post_type}-{$post_id}.css";
+		$item_url                      = ('post_not_exists' === $expected['code'])
 			? null
 			: "http://example.org/?p={$post_id}";
+		$save_cpcss                    =  ! isset( $config['save_cpcss'] )
+			? true
+			: $config['save_cpcss'];
+		$send_generation_request_error = ! isset( $config['send_generation_request_error'] )
+			? ''
+			: $config['send_generation_request_error'];
+		$get_job_details_error         = ! isset( $config['get_job_details_error'] )
+			? ''
+			: $config['get_job_details_error'];
 
-		//is_wp_error is called three times at normal/ideal case.
-		//validate_item_for_generate
-		//send_generation_request
-		//get_job_details
-		Functions\expect( 'is_wp_error' )
-			->andReturnUsing( function( $error_object ) {
-				return $error_object instanceof WP_Error;
-			}  );
-
-		if ( $post_id > 0 && 'publish' === $post_status ) {
-			Functions\expect( 'get_transient' )
-				->with( 'rocket_specific_cpcss_job_' . md5($item_url) )
-				->andReturn( $saved_cpcss_job_id );
-		}
-
-		if ( $post_id > 0 && 'publish' === $post_status && $cpcss_post_job_id &&
-			200 === $post_request_response_code ) {
-			Functions\expect( 'set_transient' )
-				->once()
-				->with( 'rocket_specific_cpcss_job_' . md5($item_url), $cpcss_post_job_id, HOUR_IN_SECONDS );
-		}
-
-		if ( in_array( (int) $get_request_response_code, [ 400, 404 ], true )
-			|| ( 200 === $get_request_response_code && 'complete' === $get_request_response_state )
-			|| $request_timeout ) {
-			Functions\expect( 'delete_transient' )
-				->once()
-				->with( 'rocket_specific_cpcss_job_' . md5($item_url) );
-		}
-		Functions\expect( 'get_post_type' )
-			->atMost()
-			->times( 1 )
-			->andReturn( $post_type );
-
-		Functions\expect( 'get_current_blog_id' )
-			->once()
-			->andReturn( 1 );
-		Functions\expect( 'get_permalink' )
-			->atMost()
-			->times( 1 )
-			->with( $post_id )
-			->andReturnUsing(
-				function( $post_id ) use ( $expected ) {
-					return 'post_not_exists' === $expected['code']
-						? false
-						: "http://example.org/?p={$post_id}";
-				}
-			);
-
-		Functions\expect( 'wp_remote_post' )
-			->atMost()
-			->times( 1 )
-			->with(
-				'https://cpcss.wp-rocket.me/api/job/',
-				[
-					'body' => [
-						'url' => "http://example.org/?p={$post_id}",
-					],
-				]
-			)
-			->andReturn( 'postRequest' );
-
-		Functions\expect( 'wp_remote_retrieve_response_code' )
-			->atMost()
-			->times( 1 )
-			->andReturn( $post_request_response_code );
-
-		Functions\expect( 'wp_remote_retrieve_body' )
-			->ordered()
-			->atMost()
-			->times( 1 )
-			->with( 'postRequest' )
-			->andReturn( $post_request_response_body )
-			->andAlsoExpectIt()
-			->atMost()
-			->times( 1 )
-			->with( 'getRequest' )
-			->andReturn( $get_request_response_body );
-
-		Functions\expect( 'wp_remote_get' )
-			->atMost()
-			->times( 1 )
-			->andReturn( 'getRequest' );
-
-		Functions\when( 'wp_strip_all_tags' )->returnArg();
-
-		$api_client = new APIClient();
-		$data_manager = new DataManager('wp-content/cache/critical-css/', $this->filesystem);
+		$api_client    = Mockery::mock( APIClient::class );
+		$data_manager  = Mockery::mock( DataManager::class );
 		$cpcss_service = new ProcessorService( $data_manager, $api_client );
+
+		if ( $request_timeout ) {
+			$data_manager->shouldReceive( 'delete_cache_job_id' )->once()->with( $item_url );
+		} else {
+			$data_manager->shouldReceive( 'get_cache_job_id' )->once()->with( $item_url )->andReturn( $saved_cpcss_job_id );
+
+			if ( false === $saved_cpcss_job_id) {
+				// enters send_generation_request()
+				if ( $post_id > 0 && 'publish' === $post_status && $cpcss_post_job_id && 200 === $post_request_response_code ) {
+					$api_client->shouldReceive( 'send_generation_request' )
+						->once()
+						->with( $item_url )
+						->andReturn( $cpcss_post_job_body );
+
+					$data_manager->shouldReceive( 'set_cache_job_id' )->once()->with( $item_url, $cpcss_post_job_id );
+
+					if ( ! in_array( (int) $get_request_response_code, [ 400, 404 ], true ) ) {
+						$api_client->shouldReceive( 'get_job_details' )
+							->once()
+							->with( $cpcss_post_job_id, $item_url )
+							->andReturn( $get_request_response_decoded );
+						$data_manager->shouldReceive( 'delete_cache_job_id' )->once()->with( $item_url );
+						$data_manager->shouldReceive( 'save_cpcss' )
+							->once()
+							->with( $item_path, $get_request_response_decoded->data->critical_path, $item_url )
+							->andReturn( $save_cpcss );
+					} else {
+						$api_client->shouldReceive( 'get_job_details' )
+							->once()
+							->with( $cpcss_post_job_id, $item_url )
+							->andReturn( $get_job_details_error );
+						$data_manager->shouldReceive( 'delete_cache_job_id' )->once()->with( $item_url );
+					}
+				} else {
+					$api_client->shouldReceive( 'send_generation_request' )
+						->once()
+						->with( $item_url )
+						->andReturn( $send_generation_request_error );
+				}
+			}
+		}
 
 		$generated = $cpcss_service->process_generate( $item_url, $item_path, $request_timeout );
 		if( isset( $expected['success'] ) && ! $expected['success'] ){
@@ -166,8 +119,6 @@ class Test_ProcessGenerate extends FilesystemTestCase {
 		}else{
 			$this->assertSame( $expected, $generated );
 		}
-
-		$this->assertSame( $config['cpcss_exists_after'], $this->filesystem->exists( $file ) );
 	}
 
 	public function dataProvider() {
