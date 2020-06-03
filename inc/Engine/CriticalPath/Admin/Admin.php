14e6e28fa3ac05657620ea1d44b2953e85ad470c
Soponar Cristina <crystinutzaa@gmail.com>
2020-06-03 23:00:24 +0300

    Moved update_running_transient() into TransientTrait

--- inc/Engine/CriticalPath/Admin/Admin.php
+++ inc/Engine/CriticalPath/Admin/Admin.php
@@ -4,8 +4,11 @@ namespace WP_Rocket\Engine\CriticalPath\Admin;
 
 use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Engine\CriticalPath\ProcessorService;
+use WP_Rocket\Engine\CriticalPath\TransientTrait;
 
 class Admin {
+	use TransientTrait;
+
 	/**
 	 * Instance of options handler.
 	 *
@@ -150,26 +153,6 @@ class Admin {
 		) {
 			$this->update_running_transient( $transient, $cpcss_item['path'], $mobile, $cpcss_generation['message'], ( 'cpcss_generation_successful' === $cpcss_generation['code'] ) );
 		}
-	}
-
-	/**
-	 * Updates CPCSS running transient with status notices.
-	 *
-	 * @since 3.6
-	 *
-	 * @param array       $transient Transient to be updated.
-	 * @param string      $item_path Path for processed item.
-	 * @param bool        $mobile    If this request is for mobile cpcss.
-	 * @param string      $message   CPCSS reply message.
-	 * @param bool/string $success   CPCSS success or failure.
-	 * @return void
-	 */
-	private function update_running_transient( $transient, $item_path, $mobile, $message, $success ) {
-		$path = ! (bool) $mobile ? $item_path : str_replace( '-mobile.css', '.css', $item_path );
-
-		$transient['items'][ $path ]['status'][ ! (bool) $mobile ? 'nonmobile' : 'mobile' ]['message'] = $message;
-		$transient['items'][ $path ]['status'][ ! (bool) $mobile ? 'nonmobile' : 'mobile' ]['success'] = $success;
-		set_transient( 'rocket_critical_css_generation_process_running', $transient, HOUR_IN_SECONDS );
 	}
 
 	/**
--- inc/Engine/CriticalPath/CriticalCSSGeneration.php
+++ inc/Engine/CriticalPath/CriticalCSSGeneration.php
@@ -12,6 +12,8 @@ use WP_Background_Process;
  * @see WP_Background_Process
  */
 class CriticalCSSGeneration extends WP_Background_Process {
+	use TransientTrait;
+
 	/**
 	 * Process prefix.
 	 *
@@ -84,25 +86,5 @@ class CriticalCSSGeneration extends WP_Background_Process {
 
 		$this->update_running_transient( $transient, $item['path'], $mobile, $generated['message'], ( 'cpcss_generation_successful' === $generated['code'] ) );
 		return false;
-	}
-
-	/**
-	 * Updates CPCSS running transient with status notices.
-	 *
-	 * @since 3.6
-	 *
-	 * @param array       $transient Transient to be updated.
-	 * @param string      $item_path Path for processed item.
-	 * @param bool        $mobile    If this request is for mobile cpcss.
-	 * @param string      $message   CPCSS reply message.
-	 * @param bool/string $success   CPCSS success or failure.
-	 * @return void
-	 */
-	private function update_running_transient( $transient, $item_path, $mobile, $message, $success ) {
-		$path = ! (bool) $mobile ? $item_path : str_replace( '-mobile.css', '.css', $item_path );
-
-		$transient['items'][ $path ]['status'][ ! (bool) $mobile ? 'nonmobile' : 'mobile' ]['message'] = $message;
-		$transient['items'][ $path ]['status'][ ! (bool) $mobile ? 'nonmobile' : 'mobile' ]['success'] = $success;
-		set_transient( 'rocket_critical_css_generation_process_running', $transient, HOUR_IN_SECONDS );
 	}
 }
