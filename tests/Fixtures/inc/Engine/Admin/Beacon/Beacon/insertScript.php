555023bb9e052f35983060c8bda39bf231612c68
Rémy Perona <remperona@gmail.com>
2020-06-04 16:23:43 -0400

    add fixture test for unavailable case

--- tests/Fixtures/inc/Engine/Admin/Beacon/Beacon/insertScript.php
+++ tests/Fixtures/inc/Engine/Admin/Beacon/Beacon/insertScript.php
@@ -27,6 +27,33 @@ SCRIPT
 				,
 			],
 		],
+		'testShouldEchoDefaultWithUnavailableLicense' => [
+			'config'   => [
+				'locale'        => 'en_US',
+				'customer_data' => (object) [
+					'licence_account' => 'Unvailable',
+				],
+			],
+			'expected' => [
+				'data'   => [
+					'form_id'  => '44cc73fb-7636-4206-b115-c7b33823551b',
+					'identify' => '{"email":"dummy@wp-rocket.me","Website":"http:\/\/example.org"}',
+					'session'  => '{"Website":"http:\/\/example.org","WordPress Version":"5.4","WP Rocket Version":"3.6","Theme":"WordPress Default","Plugins Enabled":"","WP Rocket Active Options":""}',
+					'prefill'  => '{"fields":[{"id":21728,"value":108003}]}',
+				],
+				'script' => <<<SCRIPT
+<script>!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+window.Beacon('init', '44cc73fb-7636-4206-b115-c7b33823551b')
+window.Beacon("identify", {"email":"dummy@wp-rocket.me","Website":"http:\/\/example.org"});
+window.Beacon("session-data", {"Website":"http:\/\/example.org","WordPress Version":"5.4","WP Rocket Version":"3.6","Theme":"WordPress Default","Plugins Enabled":"","WP Rocket Active Options":""});
+window.Beacon("prefill", {"fields":[{"id":21728,"value":108003}]});
+window.addEventListener("hashchange", function () {
+	window.Beacon("suggest");
+}, false);</script>
+SCRIPT
+				,
+			],
+		],
 		'testShouldEchoDefaultWithSingleLicense' => [
 			'config'   => [
 				'locale'        => 'en_US',
