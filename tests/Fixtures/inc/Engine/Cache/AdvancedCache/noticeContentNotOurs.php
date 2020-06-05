20c43c9dbb5a1c23596b321783400e3dda2b99bf
Rémy Perona <remy@wp-rocket.me>
2020-06-05 13:12:22 -0400

    Prevent false positive notice "content not ours" after license validation (PR #2729)

--- tests/Fixtures/inc/Engine/Cache/AdvancedCache/noticeContentNotOurs.php
+++ tests/Fixtures/inc/Engine/Cache/AdvancedCache/noticeContentNotOurs.php
@@ -4,60 +4,78 @@ return [
 
 	'testShouldReturnNullWhenPluginsActivate' => [
 		'config'   => [
-			'pagenow'   => 'plugins.php',
-			'activate'  => true,
-			'cap'       => false,
-			'valid_key' => true,
-			'constant'  => true,
-			'message'   => '',
+			'pagenow'                  => 'plugins.php',
+			'activate'                 => true,
+			'cap'                      => false,
+			'valid_key'                => true,
+			'wp_cache'                 => true,
+			'wp_rocket_advanced_cache' => true,
+			'message'                  => '',
 		],
 		'expected' => null,
 	],
 
 	'testShouldReturnNullWhenNoCap' => [
 		'config'   => [
-			'pagenow'   => 'settings-general.php',
-			'activate'  => null,
-			'cap'       => false,
-			'valid_key' => true,
-			'constant'  => true,
-			'message'   => '',
+			'pagenow'                  => 'settings-general.php',
+			'activate'                 => null,
+			'cap'                      => false,
+			'valid_key'                => true,
+			'wp_cache'                 => true,
+			'wp_rocket_advanced_cache' => true,
+			'message'                  => '',
 		],
 		'expected' => null,
 	],
 
 	'testShouldReturnNullWhenNoValidKey' => [
 		'config'   => [
-			'pagenow'   => 'settings-general.php',
-			'activate'  => null,
-			'cap'       => true,
-			'valid_key' => false,
-			'constant'  => true,
-			'message'   => '',
+			'pagenow'                  => 'settings-general.php',
+			'activate'                 => null,
+			'cap'                      => true,
+			'valid_key'                => false,
+			'wp_cache'                 => true,
+			'wp_rocket_advanced_cache' => true,
+			'message'                  => '',
+		],
+		'expected' => null,
+	],
+
+	'testShouldReturnNullWPCacheFalse' => [
+		'config'   => [
+			'pagenow'                  => 'settings-general.php',
+			'activate'                 => null,
+			'cap'                      => true,
+			'valid_key'                => true,
+			'wp_cache'                 => false,
+			'wp_rocket_advanced_cache' => true,
+			'message'                  => '',
 		],
 		'expected' => null,
 	],
 
 	'testShouldReturnNullWhenConstantSet' => [
 		'config'   => [
-			'pagenow'   => 'settings-general.php',
-			'activate'  => null,
-			'cap'       => true,
-			'valid_key' => true,
-			'constant'  => true,
-			'message'   => '',
+			'pagenow'                  => 'settings-general.php',
+			'activate'                 => null,
+			'cap'                      => true,
+			'valid_key'                => true,
+			'wp_cache'                 => true,
+			'wp_rocket_advanced_cache' => true,
+			'message'                  => '',
 		],
 		'expected' => null,
 	],
 
 	'testShouldDisplayNotice' => [
 		'config'   => [
-			'pagenow'   => 'settings-general.php',
-			'activate'  => null,
-			'cap'       => true,
-			'valid_key' => true,
-			'constant'  => false,
-			'message'   => <<<HTML
+			'pagenow'                  => 'settings-general.php',
+			'activate'                 => null,
+			'cap'                      => true,
+			'valid_key'                => true,
+			'wp_cache'                 => true,
+			'wp_rocket_advanced_cache' => false,
+			'message'                  => <<<HTML
 <strong>WP Rocket</strong> cannot configure itself due to missing writing permissions.
 <br>Affected file/folder: <code>wp-content/advanced-cache.php</code>
 <br>Troubleshoot: <a href="https://docs.wp-rocket.me/article/626-how-to-make-system-files-htaccess-wp-config-writeable/?utm_source=wp_plugin&utm_medium=wp_rocket" target="_blank">How to make system files writeable</a>
