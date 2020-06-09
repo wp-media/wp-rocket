086e309ad81f1028c25ff3ac59ef02d2534cd144
Rémy Perona <remperona@gmail.com>
2020-06-09 15:34:22 -0400

    add integration tests for purge_dates_archives

---
+++ tests/Integration/inc/Engine/Cache/PurgeActionsSubscriber/purgeDatesArchives.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Engine\Cache\PurgeActionsSubscriber;
+
+use Brain\Monkey\Functions;
+use WP_Rocket\Tests\Integration\FilesystemTestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\Cache\PurgeActionsSubscriber:purge_dates_archives
+ * @uses   ::get_rocket_parse_url
+ *
+ * @group  purge_actions
+ * @group  vfs
+ */
+class Test_PurgeDatesArchives extends FilesystemTestCase {
+	private static $user_id = 0;
+	protected $path_to_test_data = '/inc/Engine/Cache/Purge/purgeDatesArchives.php';
+
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::$user_id = $factory->user->create(
+			[
+				'role'          => 'editor',
+				'user_nicename' => 'rocket_tester',
+			]
+		);
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		wp_set_current_user( self::$user_id );
+		$this->set_permalink_structure( "/%postname%/" );
+		set_current_screen( 'edit.php' );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		set_current_screen( 'front' );
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldCleanCache( $post_data, $cleaned ) {
+		$post = $this->factory->post->create_and_get( $post_data );
+
+		$this->generateEntriesShouldExistAfter( $cleaned );
+
+		do_action( 'after_rocket_clean_post', $post, [], '' );
+
+		if ( false !== $cleaned ) {
+			$this->checkEntriesDeleted( $cleaned );
+			$this->checkShouldNotDeleteEntries();
+		}
+   }
+}
