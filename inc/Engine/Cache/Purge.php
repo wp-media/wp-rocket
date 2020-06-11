e6325eee259298f3e7329236f197c7d58cbaae04
Rémy Perona <remperona@gmail.com>
2020-06-11 14:41:28 -0400

    refactor purge_dates_archives in smaller private methods

--- inc/Engine/Cache/Purge.php
+++ inc/Engine/Cache/Purge.php
@@ -35,12 +35,7 @@ class Purge {
 		global $wp_rewrite;
 
 		foreach ( $this->get_dates_archives( $post ) as $url ) {
-			$parsed_url = get_rocket_parse_url( $url );
-
-			/** This filter is documented in inc/front/htaccess.php */
-			if ( apply_filters( 'rocket_url_no_dots', false ) ) {
-				$parsed_url['host'] = str_replace( '.', '_', $parsed_url['host'] );
-			}
+			$parsed_url = $this->parse_url( $url );			
 
 			foreach ( _rocket_get_cache_dirs( $parsed_url['host'] ) as $dir ) {
 				$path = $dir . $parsed_url['path'];
@@ -49,24 +44,13 @@ class Purge {
 					continue;
 				}
 
-				try {
-					$iterator = new DirectoryIterator( $path );
-				} catch ( Exception $e ) {
-					// No action required, as logging not enabled.
-					$iterator = [];
-				}
-
-				foreach ( $iterator as $item ) {
+				foreach ( $this->get_iterator( $path ) as $item ) {
 					if ( $item->isFile() ) {
 						$this->filesystem->delete( $item->getPathname() );
 					}
 				}
 
-				$pagination_dir = $path . DIRECTORY_SEPARATOR . $wp_rewrite->pagination_base;
-
-				if ( $this->filesystem->is_dir( $pagination_dir ) ) {
-					rocket_rrmdir( $pagination_dir, [], $this->filesystem );
-				}
+				$this->maybe_remove_dir( $path . DIRECTORY_SEPARATOR . $wp_rewrite->pagination_base );
 			}
 		}
 	}
@@ -99,5 +83,57 @@ class Purge {
 		 * @param array $urls List of dates URLs.
 		*/
 		return (array) apply_filters( 'rocket_post_dates_urls', $urls );
+	}
+
+	/**
+	 * Parses URL and return the parts array
+	 *
+	 * @since 3.6.1
+	 *
+	 * @param string $url URL to parse.
+	 * @return array
+	 */
+	private function parse_url( $url ) {
+		$parsed_url = get_rocket_parse_url( $url );
+
+		/** This filter is documented in inc/front/htaccess.php */
+		if ( apply_filters( 'rocket_url_no_dots', false ) ) {
+			$parsed_url['host'] = str_replace( '.', '_', $parsed_url['host'] );
+		}
+
+		return $parsed_url;
+	}
+
+	/**
+	 * Gets the iterator for the given path
+	 *
+	 * @since 3.6.1
+	 *
+	 * @param string $path Absolute path.
+	 * @return DirectoryIterator|array
+	 */
+	private function get_iterator( $path ) {
+		try {
+			$iterator = new DirectoryIterator( $path );
+		} catch ( Exception $e ) {
+			// No action required, as logging not enabled.
+			$iterator = [];
+		}
+
+		return $iterator;
+	}
+
+	/**
+	 * Recursively remove the provided directory and its content
+	 *
+	 * @since 3.6.1
+	 *
+	 * @param string $dir Absolute path for the directory.
+	 * @return void
+	 */
+	private function maybe_remove_dir( $dir ) {
+		if ( $this->filesystem->is_dir( $dir ) ) {
+			rocket_rrmdir( $dir, [], $this->filesystem );
+		}
 	}
 }
