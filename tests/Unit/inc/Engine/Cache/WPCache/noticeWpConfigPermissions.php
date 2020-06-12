ed5b1fc01c6d7a671ad2bf51c1d58b74f78948e3
Rémy Perona <remperona@gmail.com>
2020-06-12 12:44:51 -0400

    add fixture & unit tests for wp-config permissions notice

---
+++ tests/Unit/inc/Engine/Cache/WPCache/noticeWpConfigPermissions.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\Engine\Cache\WPCache;
+
+use Brain\Monkey\Functions;
+use Mockery;
+use WP_Rocket\Engine\Cache\WPCache;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\Cache\WPCache::notice_wp_config_permissions
+ * @uses   ::rocket_get_constant
+ * @uses   ::rocket_notice_html
+ *
+ * @group  WPCache
+ */
+class Test_NoticeWpConfigPermissions extends FileSystemTestCase {
+	protected $path_to_test_data = '/inc/Engine/Cache/WPCache/noticeWpConfigPermissions.php';
+
+	public function setUp() {
+		parent::setUp();
+
+        Functions\when( 'wp_create_nonce' )->justReturn( '123456' );
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldEchoNotice( $config, $expected ) {
+		$this->wp_cache_constant = $config['constant'];
+		$this->setUpMocks( $config );
+
+		$wp_cache = new WPCache( $this->filesystem );
+
+		if ( empty( $expected ) ) {
+			Functions\expect( 'rocket_notice_html' )->never();
+		} else {
+			Functions\expect( 'rocket_notice_writing_permissions' )
+				->once()
+				->with( 'wp-config.php' )
+				->andReturn( $config['message'] );
+
+			Functions\expect( 'rocket_notice_html' )
+				->once()
+				->with(
+					[
+						'status'           => 'error',
+						'dismissible'      => '',
+						'message'          => $config['message'],
+						'dismiss_button'   => 'rocket_warning_wp_config_permissions',
+						'readonly_content' => "/** Enable Cache by WP Rocket */\r\ndefine( 'WP_CACHE', true );\r\n",
+					]
+				)
+				->andReturnNull();
+		}
+
+		$wp_cache->notice_wp_config_permissions();
+	}
+
+	private function setUpMocks( $config ) {
+		Functions\expect( 'current_user_can' )
+			->once()
+			->with( 'rocket_manage_options' )
+			->andReturn( $config['cap'] );
+		if ( $config['cap'] ) {
+			Functions\expect( 'rocket_valid_key' )
+				->once()
+				->andReturn( $config['valid_key'] );
+		} else {
+			Functions\expect( 'rocket_valid_key' )->never();
+		}
+
+		if ( ! $config['writable'] ) {
+			$this->filesystem->chmod( 'wp-config.php', 0444 );
+		}
+
+		if ( $config['writable'] || $this->wp_cache_constant ) {
+			return;
+		}
+
+		Functions\expect( 'get_current_user_id' )->once()->andReturn( 1 );
+		Functions\expect( 'get_user_meta' )
+			->once()
+			->with( 1, 'rocket_boxes', true )
+			->andReturn( $config['boxes'] );
+	}
+}
