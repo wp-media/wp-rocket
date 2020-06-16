76b2ea504394d0dba0217292492b077314def0b5
Soponar Cristina <crystinutzaa@gmail.com>
2020-06-16 18:14:45 +0300

    Added unit tests & fixture

---
+++ tests/Unit/inc/functions/getRocketCacheRejectUri.php
@@ -0,0 +1,41 @@
+<?php
+namespace WP_Rocket\Tests\Unit\inc\functions;
+
+use stdClass;
+use Brain\Monkey\Functions;
+use WPMedia\PHPUnit\Unit\TestCase;
+
+/**
+ * @covers ::get_rocket_cache_reject_uri
+ * @group Functions
+ * @group Options
+ */
+class Test_GetRocketCacheRejectUri extends TestCase {
+	/**
+	 * @dataProvider providerTestData
+	 */
+    public function testShouldGetRocketCacheRejectUri( $config, $expected ) {
+		$GLOBALS['wp_rewrite']            = new stdClass();
+		$GLOBALS['wp_rewrite']->feed_base = 'feed/';
+
+		Functions\expect( 'get_rocket_option' )
+			->once()
+			->with( 'cache_reject_uri', [] )
+			->andReturn( $config['options']['cache_reject_uri'] );
+
+		Functions\when( 'rocket_get_home_dirname' )->justReturn( $config['home_dirname'] );
+
+		Functions\expect( 'apply_filters' )
+			->once()
+			->andReturn( (array) $config['filter_rocket_cache_reject_uri'] );
+
+        $this->assertSame(
+			$expected,
+            get_rocket_cache_reject_uri( true )
+        );
+    }
+
+	public function providerTestData() {
+		return $this->getTestData( __DIR__, 'getRocketCacheRejectUri' );
+	}
+}
