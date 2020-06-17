0416aff5240e0cf873d1f54b8b3cb7a86186b446
Rémy Perona <remy@wp-rocket.me>
2020-06-17 15:44:54 -0400

    Fixes #2730 Remove unneeded notice related to advanced-cache.php (PR #2781)

--- inc/Engine/Cache/AdminSubscriber.php
+++ inc/Engine/Cache/AdminSubscriber.php
@@ -44,7 +44,6 @@ class AdminSubscriber implements Event_Manager_Aware_Subscriber_Interface {
 			'admin_init'    => 'register_terms_row_action',
 			'admin_notices' => [
 				[ 'notice_advanced_cache_permissions' ],
-				[ 'notice_advanced_cache_content_not_ours' ],
 			],
 		];
 	}
@@ -114,16 +113,5 @@ class AdminSubscriber implements Event_Manager_Aware_Subscriber_Interface {
 	 */
 	public function notice_advanced_cache_permissions() {
 		$this->advanced_cache->notice_permissions();
-	}
-
-	/**
-	 * Displays the notice when advanced-cache.php content is not ours
-	 *
-	 * @since 3.6
-	 *
-	 * @return void
-	 */
-	public function notice_advanced_cache_content_not_ours() {
-		$this->advanced_cache->notice_content_not_ours();
 	}
 }
