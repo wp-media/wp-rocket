1a41f0f0eead8bd8b9a82adeea6ba8876da4cdd0
Soponar Cristina <crystinutzaa@gmail.com>
2020-06-19 22:02:48 +0300

    Revisit Unit test

--- tests/Unit/inc/ThirdParty/Hostings/Wpengine/cleanWpengine.php
+++ tests/Unit/inc/ThirdParty/Hostings/Wpengine/cleanWpengine.php
@@ -1,19 +1,30 @@
 <?php
 namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Hostings\Wpengine;
 
+use WpeCommon;
+
 /**
  * @covers \WP_Rocket\ThirdParty\Hostings\Wpengine::clean_wpengine
  * @group  Wpengine
  * @group  ThirdParty
  */
 class Test_CleanWpengine extends WpengineTestCase {
+	public static function tearDownAfterClass() {
+		parent::tearDownAfterClass();
+
+		WpeCommon::resetCounters();
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		WpeCommon::resetCounters();
+	}
 
 	public function testShouldCleanWPEngine() {
-		$this->wpe_common_mock->shouldReceive( 'purge_memcached' )
-							->once();
-		$this->wpe_common_mock->shouldReceive( 'purge_varnish_cache' )
-							->once();
+		$this->wpengine->clean_wpengine();
 
-		$this->wpengine->clean_wpengine( );
+		$this->assertEquals( 1, WpeCommon::getNumberTimesPurgeMemcachedCalled() );
+		$this->assertEquals( 1, WpeCommon::getNumberTimesVarnishCacheCalled() );
 	}
 }
