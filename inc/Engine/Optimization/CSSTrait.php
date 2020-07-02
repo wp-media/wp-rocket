84914a7688c6e870e1daa3b458e776650e3d2266
Caspar Green <cg@caspar.green>
2020-07-02 12:25:28 -0400

    Add CSS Trait into Engine\Optimization

---
+++ inc/Engine/Optimization/CSSTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace WP_Rocket\Engine\Optimization;
+
+
+trait CSSTrait {
+
+	/**
+	 * Applies font-display:swap to all font-family rules without a previously set font-display property.
+	 *
+	 * @since 3.7
+	 *
+	 * @param $css_file_content CSS file content to modify.
+	 *
+	 * @return string Modified CSS content.
+	 */
+	private function apply_font_display_swap( $css_file_content ) {
+		$css_file_content = (string) $css_file_content;
+
+		return preg_replace_callback(
+			'/(?:@font-face)\s*{\s*((?:.|\s)*)\s*}/',
+			function ( $matches ) {
+				if ( false !== strpos( $matches[1], 'font-display' ) ) {
+					return $matches;
+				}
+
+				$matches[1] = 'font-display:swap;' . $matches[1];
+
+				return $matches;
+			},
+			$css_file_content );
+	}
+}
