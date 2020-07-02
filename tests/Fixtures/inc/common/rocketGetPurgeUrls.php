4e1166d230d4c5db91223f2ed05f69b7f8a9c1ae
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-07-02 17:05:41 +0200

    unit test for rocket_get_purge_urls

---
+++ tests/Fixtures/inc/common/rocketGetPurgeUrls.php
@@ -0,0 +1,183 @@
+<?php
+
+return [
+	'vfs_dir' => 'wp-content/cache/wp-rocket/',
+
+	'structure' => [
+		'wp-content' => [
+			'cache' => [
+				'wp-rocket' => [
+					'test_purge.html' => ''
+				]
+			]
+		]
+	],
+
+	'urls' => [
+		'posts' => [
+			2 => 'http://www.example.org/next_post',
+			3 => 'http://www.example.org/test_parent',
+			4 => 'http://www.example.org/test_parent_2',
+			5 => 'http://www.example.org/test2',
+			6 => 'http://www.example.org/test3',
+			20 => 'http://www.example.org/blog'
+		],
+		'authors' => [
+			1 => 'http://www.example.org/author/author_name'
+		],
+		'archives' => [
+			'page' => false,
+			'post' => 'http://www.example.org/',
+			'custompost' => 'http://www.example.org/custompost/',
+		]
+	],
+
+	'test_data' => [
+		'shouldReturnPostUrls' => [
+			'config' => [
+				'options' => [
+					'page_for_posts' => 20,
+					'cache_purge_pages' => false
+				],
+
+				'post_data' => [
+					'ID' => 1,
+					'post_name' => 'test',
+					'url' => '/test',
+					'post_type' => 'post',
+					'next_post_id' => 2,
+					'post_author' => 1,
+					'ancestors' => [ 3, 4 ]
+				]
+			],
+			'expected' => [
+				'http://www.example.org/test',
+				'http://www.example.org/blog',
+				'http://www.example.org/next_post',
+				'http://www.example.org/author/author_name',
+				'http://www.example.org/test_parent',
+				'http://www.example.org/test_parent_2'
+			]
+		],
+
+		'shouldReturnPageUrls' => [
+			'config' => [
+				'options' => [
+					'page_for_posts' => 20,
+					'cache_purge_pages' => false
+				],
+				'post_data' => [
+					'ID' => 1,
+					'post_name' => 'test',
+					'url' => '/test',
+					'post_type' => 'page',
+					'next_post_id' => 2,
+					'post_author' => 1,
+					'ancestors' => [ 3, 4 ]
+				],
+
+			],
+			'expected' => [
+				'http://www.example.org/test',
+				'http://www.example.org/next_post',
+				'http://www.example.org/author/author_name',
+				'http://www.example.org/test_parent',
+				'http://www.example.org/test_parent_2'
+			]
+		],
+
+		'shouldReturnCustomPostUrls' => [
+			'config' => [
+				'options' => [
+					'page_for_posts' => 20,
+					'cache_purge_pages' => false
+				],
+				'post_data' => [
+					'ID' => 1,
+					'post_name' => 'test_custom_post',
+					'url' => '/test_custom_post',
+					'post_type' => 'custompost',
+					'next_post_id' => 2,
+					'post_author' => 1,
+					'ancestors' => [ 3, 4 ]
+				],
+
+				'is_ssl' => false,
+			],
+			'expected' => [
+				'http://www.example.org/test_custom_post',
+				'http://www.example.org/custompost/index.html',
+				'http://www.example.org/custompost/index.html_gzip',
+				'http://www.example.org/custompost/indexpage',
+				'http://www.example.org/next_post',
+				'http://www.example.org/author/author_name',
+				'http://www.example.org/test_parent',
+				'http://www.example.org/test_parent_2'
+			]
+		],
+
+		'shouldReturnPostUrlsWithPreviousPost' => [
+			'config' => [
+				'options' => [
+					'page_for_posts' => 20,
+					'cache_purge_pages' => false
+				],
+
+				'post_data' => [
+					'ID' => 1,
+					'post_name' => 'test',
+					'url' => '/test',
+					'post_type' => 'post',
+					'next_post_id' => 2,
+					'prev_post_id' => 5,
+					'prev_in_term_post_id' => 6,
+					'post_author' => 1,
+					'ancestors' => [ 3, 4 ]
+				]
+			],
+			'expected' => [
+				'http://www.example.org/test',
+				'http://www.example.org/blog',
+				'http://www.example.org/next_post',
+				'http://www.example.org/test2',
+				'http://www.example.org/test3',
+				'http://www.example.org/author/author_name',
+				'http://www.example.org/test_parent',
+				'http://www.example.org/test_parent_2'
+			]
+		],
+
+		'shouldReturnPostUrlsWithPurgeUrls' => [
+			'config' => [
+				'options' => [
+					'page_for_posts' => 20,
+					'home' => 'http://www.example.org/',
+					'cache_purge_pages' => [
+						'test_purge'
+					]
+				],
+
+				'post_data' => [
+					'ID' => 1,
+					'post_name' => 'test',
+					'url' => '/test',
+					'post_type' => 'post',
+					'next_post_id' => 2,
+					'post_author' => 1,
+					'ancestors' => [ 3, 4 ]
+				]
+			],
+			'expected' => [
+				'http://www.example.org/test',
+				'http://www.example.org/blog',
+				'http://www.example.org/next_post',
+				'http://www.example.org/test_purge',
+				'http://www.example.org/author/author_name',
+				'http://www.example.org/test_parent',
+				'http://www.example.org/test_parent_2'
+			]
+		],
+
+
+	]
+];
---
+++ tests/Unit/inc/common/rocketGetPurgeUrls.php
@@ -0,0 +1,125 @@
+<?php
+namespace WP_Rocket\Tests\Unit\inc\common;
+
+use WP_Rocket\Tests\Unit\TestCase;
+use Brain\Monkey\Functions;
+
+/**
+ * @covers ::rocket_clean_cache_theme_update
+ * @uses   ::rocket_clean_domain
+ *
+ * @group  Common
+ * @group  Purge
+ */
+class Test_RocketGetPurgeUrls extends TestCase {
+	protected $path_to_test_data = '/inc/common/rocketGetPurgeUrls.php';
+	protected static $mockCommonWpFunctionsInSetUp = true;
+
+	private $site_options = [
+		'stylesheet' => '',
+	];
+
+	protected function setUp() {
+		parent::setUp();
+
+		Functions\expect( 'get_option' )->withAnyArgs()->andReturnUsing( function( $option_name, $default=null ) {
+			return isset( $this->site_options[$option_name] ) ? $this->site_options[$option_name] : $default;
+		} );
+
+		Functions\expect( 'get_rocket_option' )->withAnyArgs()->andReturnUsing( function( $option_name, $default=null ) {
+			return isset( $this->site_options[$option_name] ) ? $this->site_options[$option_name] : $default;
+		} );
+
+		Functions\expect('get_permalink')
+			->withAnyArgs()
+			->andReturnUsing( function ( $post_id ) {
+				return $this->get_post_url( $post_id );
+			} );
+
+		require_once WP_ROCKET_PLUGIN_ROOT . 'inc/common/purge.php';
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnUrls( $config, $expected ) {
+		$post_id = isset( $config['post_data']['ID'] ) ? $config['post_data']['ID'] : 0;
+		$post    = isset( $config['post_data'] ) ? (object) $config['post_data'] : null;
+		$options = isset( $config['options'] ) ? $config['options'] : [];
+
+		$this->site_options = array_merge($this->site_options, $options);
+
+		Functions\expect('get_rocket_sample_permalink')
+			->once()
+			->with( $post_id )
+			->andReturn( ['http://www.example.org/%postname%', $post->post_name] );
+
+		Functions\expect('rocket_extract_url_component')
+			->once()
+			->with('http://www.example.org' . $post->url, PHP_URL_PATH)
+			->andReturn( $post->url );
+
+		Functions\expect('get_adjacent_post')
+			->andReturnUsing( function ( $in_same_term, $excluded_terms, $previous ) use ($post) {
+				if ( ! $previous ) {
+					//next
+					if( ! $in_same_term ) {
+						return isset( $post->next_post_id ) ? $post->next_post_id : false;
+					}
+					return isset( $post->next_in_term_post_id ) ? $post->next_in_term_post_id : false;
+				}
+
+				//prev
+				if (! $in_same_term ) {
+					return isset( $post->prev_post_id ) ? $post->prev_post_id : false;
+				}
+
+				return isset( $post->prev_in_term_post_id ) ? $post->prev_in_term_post_id : false;
+			} );
+
+		Functions\expect('get_author_posts_url')
+			->once()
+			->with($post->post_author)
+			->andReturn( $this->get_author_url( $post->post_author ) );
+
+		Functions\expect('get_post_ancestors')
+			->once()
+			->with($post_id)
+			->andReturn( isset( $post->ancestors ) ? $post->ancestors : [] );
+
+		if( 'post' !== $post->post_type ){
+			$archive_url = $this->get_archive_url( $post->post_type );
+			Functions\expect('get_post_type_archive_link')
+				->once()
+				->with( $post->post_type )
+				->andReturn( $archive_url );
+
+			if ( $archive_url ) {
+				Functions\expect('is_ssl')
+					->once()
+					->andReturn( $config['is_ssl'] );
+
+				$GLOBALS['wp_rewrite'] = (object) [
+					'pagination_base' => 'page'
+				];
+			}
+		}
+
+
+		$actual = rocket_get_purge_urls( $post_id, $post );
+		$this->assertEquals($expected, $actual);
+	}
+
+	private function get_post_url( $post_id ) {
+		return isset( $this->config['urls']['posts'][$post_id] ) ? $this->config['urls']['posts'][$post_id] : '';
+	}
+
+	private function get_author_url( $author_id ) {
+		return isset( $this->config['urls']['authors'][$author_id] ) ? $this->config['urls']['authors'][$author_id] : '';
+	}
+
+	private function get_archive_url( $post_type ) {
+		return isset( $this->config['urls']['archives'][$post_type] ) ? $this->config['urls']['archives'][$post_type] : '';
+	}
+
+}
