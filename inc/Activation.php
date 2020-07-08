2ea9fcc55d2731a550f1f462a4b66b63cda80055
Rémy Perona <remperona@gmail.com>
2020-07-08 16:51:53 -0400

    add activation class

---
+++ inc/Activation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace WP_Rocket;
+
+use WP_Rocket\Engine\Cache\AdvancedCache;
+use WP_Rocket\Engine\Cache\WPCache;
+use WP_Rocket\Engine\Capabilities\Manager;
+
+class Activation {
+	/**
+	 * Performs these actions during the plugin activation
+	 *
+	 * @return void
+	 */
+	public static function activate() {
+		$filesystem = rocket_direct_filesystem();
+
+		( new Manager() )->activate();
+		( new WPCache( $filesystem ) )->activate();
+
+		// Last constants.
+		define( 'WP_ROCKET_PLUGIN_NAME', 'WP Rocket' );
+		define( 'WP_ROCKET_PLUGIN_SLUG', sanitize_key( WP_ROCKET_PLUGIN_NAME ) );
+
+		if ( defined( 'SUNRISE' ) && SUNRISE === 'on' && function_exists( 'domain_mapping_siteurl' ) ) {
+			require WP_ROCKET_INC_PATH . 'domain-mapping.php';
+		}
+
+		require WP_ROCKET_FUNCTIONS_PATH . 'options.php';
+		require WP_ROCKET_FUNCTIONS_PATH . 'formatting.php';
+		require WP_ROCKET_FUNCTIONS_PATH . 'i18n.php';
+		require WP_ROCKET_FUNCTIONS_PATH . 'htaccess.php';
+
+		if ( class_exists( 'WPaaS\Plugin' ) ) {
+			require WP_ROCKET_3RD_PARTY_PATH . 'hosting/godaddy.php';
+		}
+		if ( defined( 'O2SWITCH_VARNISH_PURGE_KEY' ) ) {
+			require WP_ROCKET_3RD_PARTY_PATH . 'hosting/o2switch.php';
+		}
+
+		/**
+		 * WP Rocket activation.
+		 *
+		 * @since  3.1.5
+		 * @author Grégory Viguier
+		 */
+		do_action( 'rocket_activation' );
+
+		if ( rocket_valid_key() ) {
+			// Add All WP Rocket rules of the .htaccess file.
+			flush_rocket_htaccess();
+		}
+
+		// Create the cache folders (wp-rocket & min).
+		rocket_init_cache_dir();
+
+		// Create the config folder (wp-rocket-config).
+		rocket_init_config_dir();
+
+		// Create advanced-cache.php file.
+		rocket_generate_advanced_cache_file( new AdvancedCache( WP_ROCKET_PATH . 'views/cache/', $filesystem ) );
+
+		// Update customer key & licence.
+		wp_remote_get(
+			WP_ROCKET_WEB_API . 'activate-licence.php',
+			[
+				'blocking' => false,
+			]
+		);
+
+		wp_remote_get(
+			home_url(),
+			[
+				'timeout'    => 0.01,
+				'blocking'   => false,
+				'user-agent' => 'WP Rocket/Homepage Preload',
+				'sslverify'  => apply_filters( 'https_local_ssl_verify', false ), // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+			]
+		);
+	}
+}
