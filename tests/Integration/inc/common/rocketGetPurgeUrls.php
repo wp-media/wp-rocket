2101c72cfa4e8152f7b7d4e5ef090c6ed252515e
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-07-08 12:36:13 +0200

    adjust integration test

--- tests/Integration/inc/common/rocketGetPurgeUrls.php
+++ tests/Integration/inc/common/rocketGetPurgeUrls.php
@@ -21,36 +21,15 @@ class Test_RocketGetPurgeUrls extends FilesystemTestCase {
 	private $page_for_posts;
 	private $posts_map   = [];
 	private $authors_map = [];
+	private $post_data   = [];
 
 	public function setUp() {
 		$this->set_permalink_structure( "/%postname%/" );
 		parent::setUp();
 
-		$k = 1;
-		foreach ( $this->config['urls']['posts'] as $post_id => $url ) {
-			$now = time();
-			$post_name = str_replace("http://example.org/", "", $url);
-			$post_details = [
-				'post_date' => gmdate( 'Y-m-d H:i:s', $now - $k ),
-				'post_title' => !empty( $post_name ) ? $post_name : "home",
-				'post_type' => 20 !== $post_id ? 'post' : 'page'
-			];
+		$this->create_posts( $this->config['urls']['posts'] );
 
-			if ( 4 === $post_id ) {
-				$post_details['post_parent'] = $this->posts_map[3];
-			}
-
-			$new_id = $this->factory->post->create($post_details);
-			$this->posts_map[$post_id] = $new_id;
-			if ( 20 === $post_id ) {
-				$this->page_for_posts = $new_id;
-			}
-			$k++;
-		}
-
-		foreach ( $this->config['urls']['authors'] as $author_id => $url ) {
-			$this->authors_map[$author_id] = $this->factory->user->create( [ 'user_login' => str_replace("http://example.org/author/", "", $url), 'role' => 'editor' ] );
-		}
+		$this->create_authors( $this->config['urls']['authors'] );
 
 		Functions\expect( 'get_rocket_option' )->withAnyArgs()->andReturnUsing( function( $option_name, $default=null ) {
 			return isset( $this->site_options[$option_name] ) ? $this->site_options[$option_name] : $default;
@@ -62,8 +41,10 @@ class Test_RocketGetPurgeUrls extends FilesystemTestCase {
 	public function tearDown() {
 		parent::tearDown();
 		foreach ( $this->site_options as $option_name => $option_value ) {
-			remove_filter( 'pre_option_'.$option_name, [$this, 'prepare_option'], 10, 3 );
+			remove_filter( 'pre_option_'.$option_name, [$this, 'prepare_option'], 10 );
 		}
+		remove_filter( 'get_previous_post_where', [$this, 'get_previous_posts'], 10 );
+		remove_filter( 'get_next_post_where', [$this, 'get_next_posts'], 10 );
 	}
 
 	/**
@@ -72,23 +53,8 @@ class Test_RocketGetPurgeUrls extends FilesystemTestCase {
 	public function testShouldReturnUrls( $config, $expected ) {
 		global $post;
 
-		// Create the custom post firstly.
-		if ( ! post_type_exists( $config['post_data']['post_type'] ) ){
-			register_post_type( $config['post_data']['post_type'] );
-		}
+		$post_id = $this->create_current_post( $config['post_data'] );
 
-		$post_data = [
-			'post_title' => $config['post_data']['post_name'],
-			'post_name' => $config['post_data']['post_name'],
-			'post_type' => $config['post_data']['post_type'],
-			'post_author' => $this->authors_map[$config['post_data']['post_author']],
-		];
-
-		if ( isset( $config['post_data']['ancestors'] ) ){
-			$post_data['post_parent'] = $this->posts_map[4];
-		}
-
-		$post_id = $this->factory->post->create($post_data);
 		$post    = get_post($post_id);
 		$options = isset( $config['options'] ) ? $config['options'] : [];
 
@@ -102,27 +68,16 @@ class Test_RocketGetPurgeUrls extends FilesystemTestCase {
 			add_filter( 'pre_option_'.$option_name, [$this, 'prepare_option'], 10, 3 );
 		}
 
-		if( 'post' !== $post->post_type ){
-			$archive_url = $this->get_archive_url( $post->post_type );
-			Functions\expect('get_post_type_archive_link')
-				->once()
-				->with( $post->post_type )
-				->andReturn( $archive_url );
-		}
+		add_filter( 'get_previous_post_where', [$this, 'get_previous_posts'], 10, 5 );
+		add_filter( 'get_next_post_where', [$this, 'get_next_posts'], 10, 5 );
 
 
+
+		// Sort actual and expected arrays to compare between them.
 		$actual = rocket_get_purge_urls( $post_id, $post );
 		asort($expected);
 		asort($actual);
 		$this->assertEquals( array_values( $expected ) , array_values( $actual ) );
-	}
-
-	private function get_post_url( $post_id ) {
-		return isset( $this->config['urls']['posts'][$post_id] ) ? $this->config['urls']['posts'][$post_id] : '';
-	}
-
-	private function get_author_url( $author_id ) {
-		return isset( $this->config['urls']['authors'][$author_id] ) ? $this->config['urls']['authors'][$author_id] : '';
 	}
 
 	private function get_archive_url( $post_type ) {
@@ -131,6 +86,78 @@ class Test_RocketGetPurgeUrls extends FilesystemTestCase {
 
 	public function prepare_option( $pre_option, $option, $default ) {
 		return isset( $this->site_options[$option] ) ? $this->site_options[$option] : $default;
+	}
+
+	public function get_previous_posts( $where, $in_same_term, $excluded_terms, $taxonomy, $post ) {
+		if ( $in_same_term && isset( $this->post_data['prev_in_term_post_id'] ) ) {
+			$where = " WHERE p.ID = {$this->posts_map[ $this->post_data['prev_in_term_post_id'] ]} ";
+		}
+
+		if ( ! $in_same_term && isset( $this->post_data['prev_post_id'] ) ) {
+			$where = " WHERE p.ID = {$this->posts_map[ $this->post_data['prev_post_id'] ]} ";
+		}
+
+		return $where;
+	}
+
+	public function get_next_posts( $where, $in_same_term, $excluded_terms, $taxonomy, $post ) {
+		if ( $in_same_term && isset( $this->post_data['next_in_term_post_id'] ) ) {
+			$where = " WHERE p.ID = {$this->posts_map[ $this->post_data['next_in_term_post_id'] ]} ";
+		}
+
+		if ( ! $in_same_term && isset( $this->post_data['next_post_id'] ) ) {
+			$where = " WHERE p.ID = {$this->posts_map[ $this->post_data['next_post_id'] ]} ";
+		}
+
+		return $where;
+	}
+
+	private function create_posts( $posts ) {
+		foreach ( $posts as $post_id => $url ) {
+			$post_name = str_replace("http://example.org/", "", $url);
+			$post_details = [
+				'post_title' => !empty( $post_name ) ? $post_name : "home",
+				'post_type' => 20 !== $post_id ? 'post' : 'page'
+			];
+
+			if ( 4 === $post_id ) {
+				$post_details['post_parent'] = $this->posts_map[3];
+			}
+
+			$new_id = $this->factory->post->create($post_details);
+			$this->posts_map[$post_id] = $new_id;
+			if ( 20 === $post_id ) {
+				$this->page_for_posts = $new_id;
+			}
+		}
+	}
+
+	private function create_authors( $authors ) {
+		foreach ( $this->config['urls']['authors'] as $author_id => $url ) {
+			$this->authors_map[$author_id] = $this->factory->user->create( [ 'user_login' => str_replace("http://example.org/author/", "", $url), 'role' => 'editor' ] );
+		}
+	}
+
+	private function create_current_post( $current_post_data ) {
+		// Create the custom post firstly.
+		if ( ! post_type_exists( $current_post_data['post_type'] ) ){
+			register_post_type( $current_post_data['post_type'], ['has_archive' => true] );
+		}
+
+		$post_data = [
+			'post_title'  => $current_post_data['post_name'],
+			'post_name'   => $current_post_data['post_name'],
+			'post_type'   => $current_post_data['post_type'],
+			'post_author' => $this->authors_map[$current_post_data['post_author']],
+		];
+
+		if ( isset( $current_post_data['ancestors'] ) ){
+			$post_data['post_parent'] = $this->posts_map[4];
+		}
+
+		$this->post_data = $current_post_data;
+
+		return $this->factory->post->create( $post_data );
 	}
 
 }
