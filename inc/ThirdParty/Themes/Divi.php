1ba91c4df6c0f83107b467ed1b666f05ad3c18a5
Rémy Perona <remperona@gmail.com>
2020-07-10 16:10:42 -0400

    fix PHPCS

--- inc/ThirdParty/Themes/Divi.php
+++ inc/ThirdParty/Themes/Divi.php
@@ -23,22 +23,22 @@ class Divi implements Subscriber_Interface {
 	}
 
 	/**
-	* Excludes Divi's Salvatorre script from JS minification
-	*
-	* Exclude it to prevent an error after minification/concatenation
-	*
-	* @since 3.6.3
-	*
-	* @param array $excluded_js An array of JS paths to be excluded.
-	* @return array the updated array of paths
-	*/
-   function exclude_js( $excluded_js ) {
-	   if ( ! defined( 'ET_BUILDER_URI' ) ) {
-		   return $excluded_js;
-	   }
+	 * Excludes Divi's Salvatorre script from JS minification
+	 *
+	 * Exclude it to prevent an error after minification/concatenation
+	 *
+	 * @since 3.6.3
+	 *
+	 * @param array $excluded_js An array of JS paths to be excluded.
+	 * @return array the updated array of paths
+	 */
+	public function exclude_js( $excluded_js ) {
+		if ( ! defined( 'ET_BUILDER_URI' ) ) {
+			return $excluded_js;
+		}
 
-	   $excluded_js[] = str_replace( home_url(), '', ET_BUILDER_URI ) . '/scripts/salvattore.min.js';
+		$excluded_js[] = str_replace( home_url(), '', ET_BUILDER_URI ) . '/scripts/salvattore.min.js';
 
-	   return $excluded_js;
-   }
+		return $excluded_js;
+	}
 }
