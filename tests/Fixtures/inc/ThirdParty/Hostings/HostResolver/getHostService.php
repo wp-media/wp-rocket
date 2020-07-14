deea05eff7888b990976e2a45ce56693ad04bdba
Rémy Perona <remperona@gmail.com>
2020-07-14 10:59:53 -0400

    add unit tests for HostResolver::get_host_service

---
+++ tests/Fixtures/inc/ThirdParty/Hostings/HostResolver/getHostService.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'test_data' => [
+		'testShouldReturnEmptyWhenNotCompatibleHost' => [
+			'expected' => '',
+		],
+		'testShouldReturnCloudways' => [
+			'expected' => 'cloudways',
+		],
+		'testShouldReturnPressable' => [
+			'expected' => 'pressable',
+		],
+		'testShouldReturnSpinUpWP' => [
+			'expected' => 'spinupwp',
+		],
+		'testShouldReturnWPEngine' => [
+			'expected' => 'wpengine',
+		],
+	],
+];
---
+++ tests/Unit/inc/ThirdParty/Hostings/HostResolver/getHostService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Hostings\HostResolver;
+
+use Brain\Monkey\Functions;
+use WP_Rocket\ThirdParty\Hostings\HostResolver;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Hostings\HostResolver::get_host_service
+ * @uses   ::rocket_get_constant
+ *
+ * @group  Hostings
+ * @group  ThirdParty
+ */
+class Test_GetHostResolver extends TestCase {
+	public function tearDown() {
+		parent::tearDown();
+
+		unset( $_SERVER['cw_allowed_ip'] );
+		putenv( 'SPINUPWP_CACHE_PATH=' );
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpectedValue( $expected ) {
+		switch( $expected ) {
+			case 'cloudways':
+				$_SERVER['cw_allowed_ip'] = true;
+				break;
+			case 'pressable':
+				Functions\when( 'rocket_get_constant' )
+					->justReturn( true );
+				break;
+			case 'spinupwp':
+				putenv( 'SPINUPWP_CACHE_PATH=/wp-content/spinupwp-cache/' );
+				break;
+			case 'wpengine':
+				require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/inc/ThirdParty/Hostings/WPEngine/wpe_param.php';
+				require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/inc/ThirdParty/Hostings/WPEngine/WpeCommon.php';
+				break;
+			default:
+				break;
+		}
+
+		$this->assertSame(
+			$expected,
+			HostResolver::get_host_service( true )
+		);
+	}
+}
