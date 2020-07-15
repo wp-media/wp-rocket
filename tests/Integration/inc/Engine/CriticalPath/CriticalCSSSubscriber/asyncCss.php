9c2d7696df09d954bdc4e301ffc5d96b4f28b558
Soponar Cristina <crystinutzaa@gmail.com>
2020-07-16 01:37:58 +0300

    Revisited group

--- tests/Integration/inc/Engine/CriticalPath/CriticalCSSSubscriber/asyncCss.php
+++ tests/Integration/inc/Engine/CriticalPath/CriticalCSSSubscriber/asyncCss.php
@@ -11,7 +11,7 @@ use WP_Rocket\Tests\Integration\FilesystemTestCase;
  *
  * @group  Subscribers
  * @group  CriticalPath
- * @group  AsyncCSSX
+ * @group  AsyncCSS
  * @group  DOM
  */
 class Test_AsyncCss extends FilesystemTestCase {
