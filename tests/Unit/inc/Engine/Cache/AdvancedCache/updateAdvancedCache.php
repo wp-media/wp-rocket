52e671e729ebc8c1691baac6ad4e2930c2c0a40a
Caspar Green <cg@caspar.green>
2020-07-15 10:31:36 -0400

    Refactor Unit tests

--- tests/Unit/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php
+++ tests/Unit/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php
@@ -15,42 +15,27 @@ use WP_Rocket\Tests\Unit\FilesystemTestCase;
 class Test_UpdateAdvancedCache extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php';
 
-	public function testShouldBailOutWhenShortCircuitFilterReturnsFalse() {
-		$advanced_cache = new AdvancedCache(
-			$this->filesystem->getUrl( $this->config['vfs_dir'] ),
-			$this->filesystem
-		);
-
-		Filters\expectApplied( 'rocket_generate_advanced_cache_file' )->andReturn( false );
-		Functions\expect( 'rocket_get_filesystem_perms' )->never();
-
-		$advanced_cache->update_advanced_cache();
-
-		$this->assertSame(
-			[ 'vfs://public/wp-content/cache/wp-rocket/index.html' ],
-			$this->filesystem->getFilesListing( 'vfs://public/wp-content/' )
-		);
-	}
-
 	/**
 	 * @dataProvider providerTestData
 	 */
-	public function testShouldWriteAdvancedCache( $expected ) {
+	public function testUpdateAdvancedCache( $set_filter, $expected ) {
 		$advanced_cache = new AdvancedCache(
 			$this->filesystem->getUrl( $this->config['vfs_dir'] ),
 			$this->filesystem
 		);
 
-		Filters\expectApplied( 'rocket_generate_advanced_cache_file' )->andReturn( true );
-		Functions\expect( 'rocket_get_filesystem_perms' )
-			->once()
-			->andReturn( 420 );
+		if ( $set_filter ) {
+			Filters\expectApplied( 'rocket_generate_advanced_cache_file' )->andReturn( false );
+			Functions\expect( 'rocket_get_filesystem_perms' )->never();
+		} else {
+			Filters\expectApplied( 'rocket_generate_advanced_cache_file' )->andReturn( true );
+			Functions\expect( 'rocket_get_filesystem_perms' )
+				->once()
+				->andReturn( 420 );
+		}
 
 		$advanced_cache->update_advanced_cache();
 
-		$this->assertSame(
-			$expected,
-			$this->filesystem->getFilesListing( 'vfs://public/wp-content/' )
-		);
+		$this->assertSame( $expected, $this->filesystem->getFilesListing( 'vfs://public/wp-content/' ) );
 	}
 }
--- tests/Unit/inc/Engine/Cache/WPCache/maybePreventDeactivation.php
+++ tests/Unit/inc/Engine/Cache/WPCache/maybePreventDeactivation.php
@@ -4,7 +4,6 @@ namespace WP_Rocket\Tests\Unit\inc\Engine\Cache\WPCache;
 use WP_Rocket\Engine\Cache\WPCache;
 use WP_Rocket\Tests\Unit\FilesystemTestCase;
 use Brain\Monkey\Filters;
-use Brain\Monkey\Functions;
 
 /**
  * @covers \WP_Rocket\Engine\Cache\WPCache::maybe_prevent_deactivation
@@ -15,28 +14,20 @@ use Brain\Monkey\Functions;
 class Test_MaybePreventDeactivation extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/Engine/Cache/WPCache/maybePreventDeactivation.php';
 
-	public function testShouldBailOutWhenConfigFileFound() {
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldMaybePreventDeactivation( $config, $expected ) {
 		$wp_cache = new WPCache( $this->filesystem );
 
-		$this->assertSame( [], $wp_cache->maybe_prevent_deactivation( [] ) );
+		if ( ! $config['file_exist'] ) {
+			$this->filesystem->delete( 'vfs://public/wp-config.php' );
+		}
+
+		if ( isset( $config['set_filter_to_false'] ) && $config['set_filter_to_false'] ) {
+			Filters\expectApplied( 'rocket_set_wp_cache_constant' )->andReturn( false );
+		}
+
+		$this->assertSame( $expected, $wp_cache->maybe_prevent_deactivation( [] ) );
 	}
-
-	public function testShouldRBailOutWhenSetCacheConstFilterTrue() {
-		$this->filesystem->delete('vfs://public/wp-config.php' );
-		$wp_cache = new WPCache( $this->filesystem );
-
-		Filters\expectApplied( 'rocket_set_wp_cache_constant' )->andReturn( false );
-
-		$this->assertSame( [], $wp_cache->maybe_prevent_deactivation( [] ) );
-	}
-
-	public function testShouldAddCauseToCausesWhenNotPrevented() {
-		$this->filesystem->delete('vfs://public/wp-config.php' );
-		$wp_cache = new WPCache( $this->filesystem );
-
-		Filters\expectApplied( 'rocket_set_wp_cache_constant' )->andReturn( true );
-
-		$this->assertSame( [ 'wpconfig' ], $wp_cache->maybe_prevent_deactivation( [] ) );
-	}
-
 }
