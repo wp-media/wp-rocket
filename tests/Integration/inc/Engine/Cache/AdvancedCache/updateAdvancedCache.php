c6728b0fb5a2caba35e0d9d70f1389107297b13f
Caspar Green <cg@caspar.green>
2020-07-15 10:32:19 -0400

    Refactor & add integration tests

--- tests/Integration/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php
+++ tests/Integration/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php
@@ -13,36 +13,27 @@ use WP_Rocket\Tests\Integration\FilesystemTestCase;
 class Test_UpdateAdvancedCache extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/Engine/Cache/AdvancedCache/updateAdvancedCache.php';
 
-	public function testShouldBailOutWhenShortCircuitFilterReturnsFalse() {
-		$advanced_cache = new AdvancedCache(
-			$this->filesystem->getUrl( $this->config['vfs_dir'] ),
-			$this->filesystem
-		);
+	public function tearDown() {
+		parent::tearDown();
 
-		add_filter( 'rocket_generate_advanced_cache_file', '__return_false' );
-
-		$advanced_cache->update_advanced_cache();
-
-		$this->assertSame(
-			[ 'vfs://public/wp-content/cache/wp-rocket/index.html' ],
-			$this->filesystem->getFilesListing( 'vfs://public/wp-content/' )
-		);
+		remove_filter( 'rocket_generate_advanced_cache_file', [ $this, 'return_false' ] );
 	}
 
 	/**
 	 * @dataProvider providerTestData
 	 */
-	public function testShouldWriteAdvancedCache( $expected ) {
+	public function testUpdateAdvancedCache( $set_filter, $expected ) {
 		$advanced_cache = new AdvancedCache(
 			$this->filesystem->getUrl( $this->config['vfs_dir'] ),
 			$this->filesystem
 		);
 
+		if ( $set_filter ) {
+			add_filter( 'rocket_generate_advanced_cache_file', [ $this, 'return_false' ] );
+		}
+
 		$advanced_cache->update_advanced_cache();
 
-		$this->assertSame(
-			$expected,
-			$this->filesystem->getFilesListing( 'vfs://public/wp-content/' )
-		);
+		$this->assertSame( $expected, $this->filesystem->getFilesListing( 'vfs://public/wp-content/' ) );
 	}
 }
