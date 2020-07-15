e3b1ecfcce143e06bc9bcef0f085788f70c8c5b2
Rémy Perona <remperona@gmail.com>
2020-07-15 09:46:57 -0400

    add unit tests for varnish_addon_title

---
+++ tests/Fixtures/inc/ThirdParty/Hostings/Savvii/varnishAddonTitle.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+	'testShouldReplaceEmptyValueWithSavviiTitle' => [
+		'settings'      => [],
+		'expected'      => [
+			'varnish_auto_purge' => [
+				'title' => 'Your site is hosted on Savvii, we have enabled Varnish auto-purge for compatibility.'
+			],
+		],
+	],
+	'testShouldReplaceDefaultTitleWithSavviiTitle' => [
+		'settings'      => [
+			'varnish_auto_purge' => [
+				'title' => 'If Varnish runs on your server, you must activate this add-on.',
+			],
+		],
+		'expected'      => [
+			'varnish_auto_purge' => [
+				'title' => 'Your site is hosted on Cloudways, we have enabled Varnish auto-purge for compatibility.'
+			],
+		],
+	],
+];
