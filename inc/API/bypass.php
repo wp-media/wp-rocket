be06b4e3a3b0b7080c80d37d14538f01a5cc7c28
Caspar Green <cg@caspar.green>
2020-07-21 10:00:18 -0400

    Add rocket_bypass()

---
+++ inc/API/bypass.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Indicate to bypass rocket optimizations.
+ *
+ * Checks for "nowprocket" query string in the url to bypass rocket processes.
+ *
+ * @since 3.7
+ *
+ * @return bool True to indicate should bypass; false otherwise.
+ */
+function rocket_bypass() {
+	global $wp;
+
+	$url = wp_parse_url( add_query_arg( $wp->query_vars, home_url( $wp->request ) ) );
+
+	return isset( $url['query'] ) && false !== strpos( $url['query'], 'nowprocket' );
+}
