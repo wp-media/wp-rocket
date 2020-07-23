9d6fe1c76018245af7d85fa0faf2a8de88fcda6b
Caspar Green <cg@caspar.green>
2020-07-23 13:55:34 -0400

    Revise Integration test: displayGoogleFontsEnabler()

--- tests/Integration/inc/Engine/Optimization/GoogleFonts/Admin/Subscriber/displayGoogleFontsEnabler.php
+++ tests/Integration/inc/Engine/Optimization/GoogleFonts/Admin/Subscriber/displayGoogleFontsEnabler.php
@@ -2,23 +2,79 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\Optimization\GoogleFonts;
 
-use Mockery;
-use WP_Rocket\Engine\Optimization\GoogleFonts\Admin\Settings;
-use WP_Rocket\Engine\Optimization\GoogleFonts\Admin\Subscriber;
 use WP_Rocket\Tests\Integration\TestCase;
 
 /**
  * @covers \WP_Rocket\Engine\Optimization\GoogleFonts\Admin\Subscriber::display_google_fonts_enabler
  *
+ * @group  AdminOnly
  * @group  GoogleFonts
  */
 class Test_DisplayGoogleFontsEnabler extends TestCase {
 
-	public function testShouldCallSettingsFontsEnabler() {
-		$settings   = Mockery::mock( Settings::class );
-		$settings->shouldReceive('display_google_fonts_enabler')->once();
+	public function tearDown() {
+		remove_filter( 'pre_get_rocket_option_minify_google_fonts', [ $this, 'setGoogleFontsOption' ] );
+		parent::tearDown();
+	}
 
-		$subscriber = new Subscriber( $settings );
-		$subscriber->display_google_fonts_enabler();
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldCallSettingsFontsEnabler( $config, $expected ) {
+		if ( $config['is-user-auth'] ) {
+			wp_set_current_user( static::factory()->user->create( [ 'role' => 'administrator' ] ) );
+		} else {
+			wp_set_current_user( static::factory()->user->create( [ 'role' => 'editor' ] ) );
+		}
+
+		if ( $config['is-gf-minify'] ) {
+			add_filter( 'pre_get_rocket_option_minify_google_fonts', [ $this, 'setGoogleFontsOption' ] );
+		}
+
+		set_current_screen( 'settings_page_wprocket' );
+
+		$this->assertSame( $expected, $this->getActualHtml() );
+	}
+
+	public function setGoogleFontsOption() {
+		return 1;
+	}
+
+	public function providerTestData() {
+		return $this->getTestData( __DIR__, 'displayGoogleFontsEnabler' );
+	}
+
+	private function getExpectedHtml() {
+		$html = <<<HTML
+<div id="wpr-mobile_cpcss_view" class="wpr-tools">
+<div class="wpr-tools-col">
+<div class="wpr-title3 wpr-tools-label wpr-icon-stack">
+Enable Google Font Optimization</div>
+<div class="wpr-field-description wpr-hide-on-click">
+Improves font performance and combines multiple font requests to reduce the number of HTTP requests.</div>
+<div class="wpr-field-description wpr-hide-on-click">
+This is a one-time action and this button will be removed afterwards.<a href="https://docs.wp-rocket.me/article/1312-optimize-google-fonts" data-beacon-article="5e8687c22c7d3a7e9aea4c4a" target="_blank" rel="noopener noreferrer">
+More info</a>
+</div>
+<div class="wpr-field-description wpr-field wpr-isHidden wpr-show-on-click">
+Google Fonts Optimization is now enabled for your site.<a href="https://docs.wp-rocket.me/article/1312-optimize-google-fonts" data-beacon-article="5e8687c22c7d3a7e9aea4c4a" target="_blank" rel="noopener noreferrer">
+More info</a>
+</div>
+</div>
+<div class="wpr-tools-col">
+<button id="wpr-action-rocket_enable_google_fonts" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-refresh">
+Optimize Google Fonts</button>
+</div>
+</div>
+HTML;
+
+		return $this->format_the_html( $html );
+	}
+
+	private function getActualHtml() {
+		ob_start();
+		do_action( 'rocket_settings_tools_content' );
+
+		return $this->format_the_html( ob_get_clean() );
 	}
 }
