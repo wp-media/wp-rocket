32f2173b197ea4db1146f9cd6b52830a44e6306a
Caspar Green <cg@caspar.green>
2020-07-24 14:09:43 -0400

    Fix flipped conditional in HostResolver

--- inc/ThirdParty/Hostings/HostResolver.php
+++ inc/ThirdParty/Hostings/HostResolver.php
@@ -62,9 +62,9 @@ class HostResolver {
 		}
 
 		if (
-			! defined( '\Savvii\CacheFlusherPlugin::NAME_FLUSH_NOW' )
-			||
-			! defined( '\Savvii\CacheFlusherPlugin::NAME_DOMAINFLUSH_NOW' )
+			rocket_get_constant( '\Savvii\CacheFlusherPlugin::NAME_FLUSH_NOW' )
+			&&
+			rocket_get_constant( '\Savvii\CacheFlusherPlugin::NAME_DOMAINFLUSH_NOW' )
 		) {
 			return 'savvii';
 		}
