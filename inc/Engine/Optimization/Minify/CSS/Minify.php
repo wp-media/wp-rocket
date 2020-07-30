11accbeab29229e5c27fe48e5319bf2cfa75081b
Rémy Perona <remperona@gmail.com>
2020-07-30 10:50:35 -0400

    minify external CSS files

--- inc/Engine/Optimization/Minify/CSS/Minify.php
+++ inc/Engine/Optimization/Minify/CSS/Minify.php
@@ -26,12 +26,21 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 		Logger::info( 'CSS MINIFICATION PROCESS STARTED.', [ 'css minification process' ] );
 
 		$styles = $this->get_styles( $html );
-		if ( ! $styles ) {
+
+		if ( empty( $styles ) ) {
 			return $html;
 		}
 
 		foreach ( $styles as $style ) {
-			if ( $this->bailout_style( $style ) ) {
+			if ( $this->is_minify_excluded_file( $style ) ) {
+				Logger::debug(
+					'Style is excluded.',
+					[
+						'css minification process',
+						'tag' => $style[0],
+					]
+				);
+
 				continue;
 			}
 
@@ -55,66 +64,10 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 	}
 
 	/**
-	 * Replace old style tag with the minified tag.
-	 *
-	 * @param array  $style      Style matched data.
-	 * @param string $minify_url Minified URL.
-	 * @param string $html       HTML content.
-	 *
-	 * @return string
-	 */
-	protected function replace_style( $style, $minify_url, $html ) {
-		$replace_style = str_replace( $style['url'], $minify_url, $style[0] );
-		$replace_style = str_replace( '<link', '<link data-minify="1"', $replace_style );
-		$html          = str_replace( $style[0], $replace_style, $html );
-
-		Logger::info(
-			'Style minification succeeded.',
-			[
-				'css minification process',
-				'url' => $minify_url,
-			]
-		);
-
-		return $html;
-	}
-
-	/**
-	 * Check if style should be bailout (is external or is excluded).
-	 *
-	 * @param  array $style Style matched data.
-	 * @return bool
-	 */
-	protected function bailout_style( $style ) {
-		if ( $this->is_external_file( $style['url'] ) ) {
-			Logger::debug(
-				'Style is external.',
-				[
-					'css minification process',
-					'tag' => $style[0],
-				]
-			);
-			return true;
-		}
-
-		if ( $this->is_minify_excluded_file( $style ) ) {
-			Logger::debug(
-				'Style is excluded.',
-				[
-					'css minification process',
-					'tag' => $style[0],
-				]
-			);
-			return true;
-		}
-		return false;
-	}
-
-	/**
 	 * Get all style tags from HTML.
 	 *
-	 * @param  string $html       HTML content.
-	 * @return array|bool $styles Array with style tags or false.
+	 * @param  string $html HTML content.
+	 * @return array Array with style tags, empty array if no style tags found.
 	 */
 	protected function get_styles( $html ) {
 		$html_nocomments = $this->hide_comments( $html );
@@ -122,7 +75,7 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 
 		if ( ! $styles ) {
 			Logger::debug( 'No `<link>` tags found.', [ 'css minification process' ] );
-			return false;
+			return [];
 		}
 
 		Logger::debug(
@@ -168,8 +121,9 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 			return $minify_url;
 		}
 
-		$file_path = $this->get_file_path( $url );
-		if ( ! $file_path ) {
+		$file_path = $this->is_external_file( $url ) ? $this->local_cache->get_filepath( $url ) : $this->get_file_path( $url );
+
+		if ( empty( $file_path ) ) {
 			Logger::error(
 				'Couldn’t get the file path from the URL.',
 				[
@@ -180,22 +134,63 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 			return false;
 		}
 
-		$minified_content = $this->minify( $file_path, $minified_file );
-		if ( ! $minified_content ) {
+		$file_content = $this->is_external_file( $url ) ? $this->local_cache->get_content( $url ) : $this->get_file_content( $file_path );
+
+		if ( ! $file_content ) {
+			Logger::error(
+				'No file content.',
+				[
+					'css minification process',
+					'path' => $file_path,
+				]
+			);
+			return false;
+		}
+
+		$minified_content = $this->minify( $file_path, $minified_file, $file_content );
+
+		if ( empty( $minified_content ) ) {
 			return false;
 		}
 
 		$minified_content = $this->font_display_swap( $url, $minified_file, $minified_content );
-		if ( ! $minified_content ) {
+
+		if ( empty( $minified_content ) ) {
 			return false;
 		}
 
 		$save_minify_file = $this->save_minify_css_file( $minified_file, $minified_content );
+
 		if ( ! $save_minify_file ) {
 			return false;
 		}
 
 		return $minify_url;
+	}
+
+	/**
+	 * Replace old style tag with the minified tag.
+	 *
+	 * @param array  $style      Style matched data.
+	 * @param string $minify_url Minified URL.
+	 * @param string $html       HTML content.
+	 *
+	 * @return string
+	 */
+	protected function replace_style( $style, $minify_url, $html ) {
+		$replace_style = str_replace( $style['url'], $minify_url, $style[0] );
+		$replace_style = str_replace( '<link', '<link data-minify="1"', $replace_style );
+		$html          = str_replace( $style[0], $replace_style, $html );
+
+		Logger::info(
+			'Style minification succeeded.',
+			[
+				'css minification process',
+				'url' => $minify_url,
+			]
+		);
+
+		return $html;
 	}
 
 	/**
@@ -210,6 +205,7 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 	 */
 	protected function save_minify_css_file( $minified_file, $minified_content ) {
 		$save_minify_file = $this->write_file( $minified_content, $minified_file );
+
 		if ( ! $save_minify_file ) {
 			Logger::error(
 				'Minified CSS file could not be created.',
@@ -238,6 +234,7 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 	 * @param string $url           File Url.
 	 * @param string $minified_file Minified file path.
 	 * @param string $content       CSS file content.
+	 * @return string
 	 */
 	protected function font_display_swap( $url, $minified_file, $content ) {
 		if ( preg_match( '/(?:-|\.)min.css/iU', $url )
@@ -250,36 +247,24 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 					'path' => $minified_file,
 				]
 			);
-			return false;
+			return '';
 		}
 
 		return $this->apply_font_display_swap( $content );
 	}
+
 	/**
 	 * Minifies the content
 	 *
 	 * @since 2.11
-	 * @author Remy Perona
 	 *
-	 * @param string|array $file          File to minify.
-	 * @param string       $minified_file Target filepath.
-	 * @return string|bool
+	 * @param string $file_path     Source filepath.
+	 * @param string $minified_file Target filepath.
+	 * @param string $file_content  Content to minify.
+	 * @return string
 	 */
-	protected function minify( $file, $minified_file ) {
-		$file_content = $this->get_file_content( $file );
-
-		if ( ! $file_content ) {
-			Logger::error(
-				'No file content.',
-				[
-					'css minification process',
-					'path' => $minified_file,
-				]
-			);
-			return false;
-		}
-
-		$file_content     = $this->rewrite_paths( $file, $minified_file, $file_content );
+	protected function minify( $file_path, $minified_file, $file_content ) {
+		$file_content     = $this->rewrite_paths( $file_path, $minified_file, $file_content );
 		$minifier         = $this->get_minifier( $file_content );
 		$minified_content = $minifier->minify();
 
@@ -291,7 +276,7 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 					'path' => $minified_file,
 				]
 			);
-			return false;
+			return '';
 		}
 
 		return $minified_content;
@@ -301,7 +286,6 @@ class Minify extends AbstractCSSOptimization implements ProcessorInterface {
 	 * Returns a new minifier instance
 	 *
 	 * @since 3.1
-	 * @author Remy Perona
 	 *
 	 * @param string $file_content Content to minify.
 	 * @return Minifier\CSS
