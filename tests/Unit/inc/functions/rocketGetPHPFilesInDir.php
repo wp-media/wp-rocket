e38dcdcd9896fd2e557ad3d418ccea40e986a9b7
Caspar Green <cg@caspar.green>
2020-07-30 16:31:11 -0400

    Closes #146: Replace glob() usage in deactivate_plugin() method (PR #2784)
    Co-authored-by: Rémy Perona <remy@wp-rocket.me>

---
+++ tests/Unit/inc/functions/rocketGetPHPFilesInDir.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\functions;
+
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
+
+/**
+ * @covers ::_rocket_get_php_files_in_dir()
+ *
+ * @group Functions
+ */
+class Test_RocketGetDirectoryPHPFilesArray extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/functions/rocketGetPHPFilesInDir.php';
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once WP_ROCKET_PLUGIN_ROOT . '/inc/functions/files.php';
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldReturnAllPHPFilesInDirectory( $dir, array $expected ) {
+		$actual = [];
+		foreach ( _rocket_get_php_files_in_dir( $dir ) as $file ) {
+			$actual[] = $file->getFilename();
+		}
+
+		$this->assertSame( $expected, $actual );
+	}
+}
