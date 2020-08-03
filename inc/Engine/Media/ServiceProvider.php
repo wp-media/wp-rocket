3eb22e6229c0f42b24e20d54c1595ad5aeb27e4c
Rémy Perona <remperona@gmail.com>
2020-08-03 09:49:55 -0400

    use variable to pass options instance

--- inc/Engine/Media/ServiceProvider.php
+++ inc/Engine/Media/ServiceProvider.php
@@ -36,17 +36,19 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * @return void
 	 */
 	public function register() {
+		$options = $this->getContainer()->get( 'options' );
+
 		$this->getContainer()->add( 'lazyload_assets', 'RocketLazyload\Assets' );
 		$this->getContainer()->add( 'lazyload_image', 'RocketLazyload\Image' );
 		$this->getContainer()->add( 'lazyload_iframe', 'RocketLazyload\Iframe' );
 		$this->getContainer()->share( 'lazyload_subscriber', 'WP_Rocket\Engine\Media\LazyloadSubscriber' )
-			->withArgument( $this->getContainer()->get( 'options' ) )
+			->withArgument( $options )
 			->withArgument( $this->getContainer()->get( 'lazyload_assets' ) )
 			->withArgument( $this->getContainer()->get( 'lazyload_image' ) )
 			->withArgument( $this->getContainer()->get( 'lazyload_iframe' ) );
 		$this->getContainer()->share( 'embeds_subscriber', 'WP_Rocket\Engine\Media\Embeds\EmbedsSubscriber' )
-			->withArgument( $this->getContainer()->get( 'options' ) );
+			->withArgument( $options );
 		$this->getContainer()->share( 'emojis_subscriber', 'WP_Rocket\Engine\Media\Emojis\EmojisSubscriber' )
-			->withArgument( $this->getContainer()->get( 'options' ) );
+			->withArgument( $options );
 	}
 }
