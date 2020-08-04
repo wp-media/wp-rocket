7ba0661bfc627b30e9452c8a2f5d3cd9da14a80d
Rémy Perona <remperona@gmail.com>
2020-08-04 15:05:58 -0400

    add methods to set option on update and restore defaults

--- inc/Engine/Optimization/DelayJS/Admin/Settings.php
+++ inc/Engine/Optimization/DelayJS/Admin/Settings.php
@@ -2,8 +2,26 @@
 
 namespace WP_Rocket\Engine\Optimization\DelayJS\Admin;
 
+use WP_Rocket\Admin\Options_Data;
+
 class Settings {
 	private $defaults = [];
+
+	/**
+	 * Instance of options handler.
+	 *
+	 * @var Options_Data
+	 */
+	private $options;
+
+	/**
+	 * Creates an instance of the class.
+	 *
+	 * @param Options_Data $options WP Rocket Options instance.
+	 */
+	public function __construct( Options_Data $options ) {
+		$this->options = $options;
+	}
 
 	public function add_options( $options ) {
 		$options = (array) $options;
@@ -25,5 +43,27 @@ class Settings {
 				],
 			],
 		];
+	}
+
+	public function set_option_on_update( $old_version ) {
+		if ( version_compare( $old_version, '3.7', '>' ) ) {
+			return;
+		}
+
+		$this->options->set( 'delay_js', 0 );
+
+		update_option( 'wp_rocket_settings', $this->options->get_options() );
+	}
+
+	public function restore_defaults() {
+		check_ajax_referer( 'rocket-ajax', 'nonce', true );
+
+		if ( ! current_user_can( 'rocket_manage_options' ) ) {
+			return false;
+		}
+
+		$this->options->set( 'delay_js_scripts', $this->defaults );
+
+		return update_option( rocket_get_constant( 'WP_ROCKET_SLUG', 'wp_rocket_settings' ), $this->options->get_options() );
 	}
 }
--- inc/Engine/Optimization/DelayJS/Admin/Subscriber.php
+++ inc/Engine/Optimization/DelayJS/Admin/Subscriber.php
@@ -16,6 +16,9 @@ class Subscriber extends Abstract_Render implements Subscriber_Interface {
 		return [
 			'rocket_first_install_options'                 => 'add_options',
 			'rocket_after_textarea_field_delay_js_scripts' => 'display_restore_defaults_button',
+			'wp_rocket_upgrade'                            => [ 'set_option_on_update', 12, 2 ],
+			'wp_ajax_rocket_restore_delay_js_defaults'     => 'restore_defaults',
+			'rocket_safe_mode_reset_options'               => 'add_options',
 		];
 	}
 
@@ -31,5 +34,19 @@ class Subscriber extends Abstract_Render implements Subscriber_Interface {
 			$data['action'],
 			$data['attributes']
 		);
+	}
+
+	public function set_option_on_update( $new_version, $old_version ) {
+		$this->settings->set_option_on_update( $old_version );
+	}
+
+	public function restore_defaults() {
+		$result = $this->settings->restore_defaults();
+
+		if ( $result ) {
+			wp_send_json_success();
+		}
+
+		wp_send_json_error();
 	}
 }
