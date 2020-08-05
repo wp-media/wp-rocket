3c4608e2e3876e7cabf4eadb16d1b90c492a680b
Tonya Mork <hellofromtonya@knowthecode.io>
2020-08-05 16:40:24 -0500

    Adds browser check for passive option.

--- assets/js/layzload-scripts.js
+++ assets/js/layzload-scripts.js
@@ -1,26 +1,58 @@
 class RocketLazyLoadScripts {
-	options = { passive: true };
-	attrName = 'data-rocketlazyloadscript';
 
 	constructor( triggerEvents ) {
+		this.passiveSupported = false;
+		this.attrName = 'data-rocketlazyloadscript';
 		this.triggerEvents = triggerEvents;
 		this.userEventListener = this.triggerListener.bind( this );
+		this._initOptions( this );
 	}
 
+	/**
+	 * Initializes the LazyLoad Scripts handler.
+	 */
 	init() {
+		this.options = this.passiveSupported ? { passive: true } : false;
+
 		this._addEventListener( this );
 	}
 
+	/**
+	 * Resets the handler.
+	 */
 	reset() {
 		this._removeEventListener( this );
 	}
 
 	/**
+	 * Initializes browser check for addEventListener passive option.
+	 *
+	 * @link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Safely_detecting_option_support
+	 * @private
+	 *
+	 * @param self Instance of this object.
+	 * @returns {boolean}
+	 */
+	_initOptions( self ) {
+		try {
+			const options = {
+				// This function will be called when the browser attempts to access the passive property.
+				get passive() {
+					self.passiveSupported = true;
+					return false;
+				}
+			};
+
+			window.addEventListener( 'test', null, options );
+			window.removeEventListener( 'test', null, options );
+		} catch ( err ) {
+			self.passiveSupported = false;
+		}
+	}
+
+	/**
 	 * Adds a listener for each of the configured user interactivity event type. When an even is triggered, it invokes
 	 * the triggerListener() method.
-	 *
-	 * Note: self.userEventListener is holding a reference to the object's method. Why? JS returns a different function
-	 * reference back.
 	 *
 	 * @private
 	 *
@@ -34,9 +66,6 @@ class RocketLazyLoadScripts {
 
 	/**
 	 * Removes the listener for each of the configured user interactivity event type.
-	 *
-	 * Note: self.userEventListener is holding a reference to the object's method. Why? JS returns a different function
-	 * reference back.
 	 *
 	 * @private
 	 *
@@ -57,6 +86,7 @@ class RocketLazyLoadScripts {
 
 		scripts.forEach( elem => {
 			const scriptSrc = elem.getAttribute( this.attrName );
+
 
 			elem.setAttribute( 'src', scriptSrc );
 			elem.removeAttribute( this.attrName );
