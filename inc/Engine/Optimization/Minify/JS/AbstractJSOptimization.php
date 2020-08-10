845b36fa67eb383d446040c36cea82b2bcb2d22c
Rémy Perona <remperona@gmail.com>
2020-08-10 10:05:23 -0400

    move get_excluded_external_file_path() to parent class

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -171,4 +171,95 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 
 		return $exclude_jquery;
 	}
+
+	/**
+	 * Patterns in URL excluded from being combined
+	 *
+	 * @since 3.1
+	 *
+	 * @return array
+	 */
+	protected function get_excluded_external_file_path() {
+		$defaults = [
+			'html5.js',
+			'show_ads.js',
+			'histats.com/js',
+			'ws.amazon.com/widgets',
+			'/ads/',
+			'intensedebate.com',
+			'scripts.chitika.net/',
+			'jotform.com/',
+			'gist.github.com',
+			'forms.aweber.com',
+			'video.unrulymedia.com',
+			'stats.wp.com',
+			'stats.wordpress.com',
+			'widget.rafflecopter.com',
+			'widget-prime.rafflecopter.com',
+			'releases.flowplayer.org',
+			'c.ad6media.fr',
+			'cdn.stickyadstv.com',
+			'www.smava.de',
+			'contextual.media.net',
+			'app.getresponse.com',
+			'adserver.reklamstore.com',
+			's0.wp.com',
+			'wprp.zemanta.com',
+			'files.bannersnack.com',
+			'smarticon.geotrust.com',
+			'js.gleam.io',
+			'ir-na.amazon-adsystem.com',
+			'web.ventunotech.com',
+			'verify.authorize.net',
+			'ads.themoneytizer.com',
+			'embed.finanzcheck.de',
+			'imagesrv.adition.com',
+			'js.juicyads.com',
+			'form.jotformeu.com',
+			'speakerdeck.com',
+			'content.jwplatform.com',
+			'ads.investingchannel.com',
+			'app.ecwid.com',
+			'www.industriejobs.de',
+			's.gravatar.com',
+			'googlesyndication.com',
+			'a.optmstr.com',
+			'a.optmnstr.com',
+			'a.opmnstr.com',
+			'adthrive.com',
+			'mediavine.com',
+			'js.hsforms.net',
+			'googleadservices.com',
+			'f.convertkit.com',
+			'recaptcha/api.js',
+			'mailmunch.co',
+			'apps.shareaholic.com',
+			'dsms0mj1bbhn4.cloudfront.net',
+			'nutrifox.com',
+			'code.tidio.co',
+			'www.uplaunch.com',
+			'widget.reviewability.com',
+			'embed-cdn.gettyimages.com/widgets.js',
+			'app.mailerlite.com',
+			'ck.page',
+			'cdn.jsdelivr.net/gh/AmauriC/',
+			'static.klaviyo.com/onsite/js/klaviyo.js',
+			'a.omappapi.com/app/js/api.min.js',
+			'static.zdassets.com',
+			'feedbackcompany.com/widgets/feedback-company-widget.min.js',
+			'widget.gleamjs.io',
+			'phonewagon.com',
+		];
+
+		$excluded_external = array_merge( $defaults, $this->options->get( 'exclude_js', [] ) );
+
+		/**
+		 * Filters JS externals files to exclude from the combine process
+		 *
+		 * @since 2.2
+		 *
+		 * @param array $pattern Patterns to match.
+		 */
+		return apply_filters( 'rocket_minify_excluded_external_js', $excluded_external );
+	}
 }
