2dfbe11f2ef47849ae64c16f062c5495b09dc5cc
Caspar Green <cg@caspar.green>
2020-08-10 12:09:59 -0400

    Add DelayJs Fixture - first draft

---
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/HTML/DelayJs.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'shouldDoNothingWhenDoNotOptimizeEnabled' => [
+		'config' => [
+			'original-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+			'do-not-optimize' => true,
+			'do-not-delay-const' => false,
+			'do-not-delay-setting' => false,
+		],
+		'expected' => [
+			'processed-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+		],
+	],
+
+	'shouldDoNothingWhenDelayConstEnabled' => [
+		'config' => [
+			'original-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+			'do-not-optimize' => false,
+			'do-not-delay-const' => true,
+			'do-not-delay-setting' => false,
+		],
+		'expected' => [
+			'processed-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+		],
+	],
+
+	'shouldDoNothingWhenDelaySetttingEnabled' => [
+		'config' => [
+			'original-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+			'do-not-optimize' => false,
+			'do-not-delay-const' => false,
+			'do-not-delay-setting' => true,
+		],
+		'expected' => [
+			'processed-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+		],
+	],
+
+	'shouldProcessDelayWhenAllowed' => [
+		'config' => [
+			'original-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+			'do-not-optimize' => false,
+			'do-not-delay-const' => false,
+			'do-not-delay-setting' => false,
+		],
+		'expected' => [
+			'processed-html' => '<script data-rocketlazyloadscript=\'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js\' src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+		],
+	],
+
+];
