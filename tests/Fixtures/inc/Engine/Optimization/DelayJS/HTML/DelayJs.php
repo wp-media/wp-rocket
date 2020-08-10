7586144d8e4d531e1222f70032e0735ea000e5be
Caspar Green <cg@caspar.green>
2020-08-10 12:16:37 -0400

    Add allowed scripts [] to fixture configurations

--- tests/Fixtures/inc/Engine/Optimization/DelayJS/HTML/DelayJs.php
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/HTML/DelayJs.php
@@ -19,6 +19,7 @@ return [
 			'do-not-optimize' => false,
 			'do-not-delay-const' => true,
 			'do-not-delay-setting' => false,
+			'allowed-scripts' => [],
 		],
 		'expected' => [
 			'processed-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
@@ -31,6 +32,7 @@ return [
 			'do-not-optimize' => false,
 			'do-not-delay-const' => false,
 			'do-not-delay-setting' => true,
+			'allowed-scripts' => [],
 		],
 		'expected' => [
 			'processed-html' => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
@@ -43,6 +45,7 @@ return [
 			'do-not-optimize' => false,
 			'do-not-delay-const' => false,
 			'do-not-delay-setting' => false,
+			'allowed-scripts' => [],
 		],
 		'expected' => [
 			'processed-html' => '<script data-rocketlazyloadscript=\'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js\' src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
