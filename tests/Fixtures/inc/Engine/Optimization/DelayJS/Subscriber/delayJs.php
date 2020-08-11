3321b91fbd7be395e82cb86e34753957e3f8ffb3
Caspar Green <cg@caspar.green>
2020-08-11 10:47:14 -0400

    Add testcase for ignored scripts

--- tests/Fixtures/inc/Engine/Optimization/DelayJS/Subscriber/delayJs.php
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/Subscriber/delayJs.php
@@ -10,7 +10,7 @@ return [
 				'wp-rocket' => [
 					'assets' => [
 						'js' => [
-							'lazyload-scripts.js' => '',
+							'lazyload-scripts.js'     => '',
 							'lazyload-scripts.min.js' => ''
 						]
 					]
@@ -22,7 +22,7 @@ return [
 	'test_data' => [
 		'shouldDoNothingWhenDoNotOptimizeEnabled' => [
 			'config'   => [
-				'html'        => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+				'html'                 => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
 				'do-not-optimize'      => true,
 				'do-not-delay-const'   => false,
 				'do-not-delay-setting' => 1,
@@ -34,7 +34,7 @@ return [
 
 		'shouldDoNothingWhenDelayConstEnabled' => [
 			'config'   => [
-				'html'        => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+				'html'                 => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
 				'do-not-optimize'      => false,
 				'do-not-delay-const'   => true,
 				'do-not-delay-setting' => 1,
@@ -47,7 +47,7 @@ return [
 
 		'shouldDoNothingWhenDelaySettingEnabled' => [
 			'config'   => [
-				'html'        => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
+				'html'                 => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js">',
 				'do-not-optimize'      => false,
 				'do-not-delay-const'   => false,
 				'do-not-delay-setting' => 0,
@@ -60,11 +60,11 @@ return [
 
 		'shouldNotProcessDelayJsScriptWhenBypass' => [
 			'config'   => [
-				'html'        => '<script type="text/javascript" data-any="value">alert("Be alert! We need more lerts!");</script>',
+				'html'                 => '<script type="text/javascript" data-any="value">alert("Be alert! We need more lerts!");</script>',
 				'do-not-optimize'      => false,
 				'do-not-delay-const'   => false,
 				'do-not-delay-setting' => 1,
-				'allowed-scripts'      => ['alert("Be alert! We need more lerts!")'],
+				'allowed-scripts'      => [ 'alert("Be alert! We need more lerts!")' ],
 				'bypass'               => true
 			],
 			'expected' => [
@@ -74,11 +74,11 @@ return [
 
 		'shouldProcessDelayURLScript' => [
 			'config'   => [
-				'html'        => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js"></script>',
+				'html'                 => '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js"></script>',
 				'do-not-optimize'      => false,
 				'do-not-delay-const'   => false,
 				'do-not-delay-setting' => 1,
-				'allowed-scripts'      => ['https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js'],
+				'allowed-scripts'      => [ 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js' ],
 			],
 			'expected' => [
 				'html' => '<script data-rocketlazyloadscript=\'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js\' ></script>',
@@ -87,21 +87,36 @@ return [
 
 		'shouldProcessDelayInlineScript' => [
 			'config'   => [
-				'html'        => '<script type="text/javascript" data-any="value">alert("Be alert! We need more lerts!");</script>',
+				'html'                 => '<script type="text/javascript" data-any="value">alert("Be alert! We need more lerts!");</script>',
 				'do-not-optimize'      => false,
 				'do-not-delay-const'   => false,
 				'do-not-delay-setting' => 1,
-				'allowed-scripts'      => ['alert("Be alert! We need more lerts!")'],
+				'allowed-scripts'      => [ 'alert("Be alert! We need more lerts!")' ],
 			],
 			'expected' => [
 				'html' => '<script data-rocketlazyloadscript=\'data:text/javascript;base64,YWxlcnQoIkJlIGFsZXJ0ISBXZSBuZWVkIG1vcmUgbGVydHMhIik7\' type="text/javascript" data-any="value"></script>',
 			],
 		],
 
-
-	//	'shouldIgnoreScriptsNotAllowed' => [
-	//
-	//	],
+		'shouldIgnoreScriptsNotAllowed' => [
+			'config'   => [
+				'html'                 => '<script type="text/javascript" data-any="value">alert("Be alert! We need more lerts!");</script>
+<script data-ignore-me="this script should be ignored!" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>',
+				'do-not-optimize'      => false,
+				'do-not-delay-const'   => false,
+				'do-not-delay-setting' => 1,
+				'allowed-scripts'      => [
+					'alert("Be alert! We need more lerts!")',
+					'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
+				],
+			],
+			'expected' => [
+				'html' => '<script data-rocketlazyloadscript=\'data:text/javascript;base64,YWxlcnQoIkJlIGFsZXJ0ISBXZSBuZWVkIG1vcmUgbGVydHMhIik7\' type="text/javascript" data-any="value"></script>
+<script data-ignore-me="this script should be ignored!" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js"></script>
+<script data-rocketlazyloadscript=\'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js\' type="text/javascript" ></script>',
+			],
+		],
 	]
 
 ];
