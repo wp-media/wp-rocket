8ff6c2b352cb7aca6e0c8560f1fe36c04938ac0c
Piotrek <piotr@wefoster.co>
2020-08-11 17:22:06 +0100

    Excluded dynamic JS
    captcha-hash
    mapdata={
    https://www.diffchecker.com/DrDSIhOT#left-4

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -853,6 +853,8 @@ class Combine extends AbstractJSOptimization {
 			'fb_desc-',
 			'FC_regenerate_captcha',
 			'wp_post_blocks_vars.listed_posts=[',
+			'captcha-hash',
+			'mapdata={',
 		];
 
 		/**
