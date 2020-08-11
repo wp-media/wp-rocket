b34cbb3ed12688fdd59ebfef60caa87d65eb6765
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-08-11 15:29:04 +0200

    add integration test for add_delay_js_script methods (without bypass)

---
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/Subscriber/addDelayJsScript.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'shouldDoNothingWhenDoNotOptimizeEnabled' => [
+		'config'   => [
+			'do-not-optimize'      => true,
+			'do-not-delay-const'   => false,
+			'do-not-delay-setting' => 1,
+		],
+		'expected' => false,
+	],
+
+	'shouldDoNothingWhenDelayConstEnabled' => [
+		'config'   => [
+			'do-not-optimize'      => false,
+			'do-not-delay-const'   => true,
+			'do-not-delay-setting' => 1,
+		],
+		'expected' => false,
+	],
+
+	'shouldDoNothingWhenDelaySettingEnabled' => [
+		'config'   => [
+			'do-not-optimize'      => false,
+			'do-not-delay-const'   => false,
+			'do-not-delay-setting' => 0,
+		],
+		'expected' => false,
+	],
+
+	'shouldProcessDelayURLScript' => [
+		'config'   => [
+			'do-not-optimize'      => false,
+			'do-not-delay-const'   => false,
+			'do-not-delay-setting' => 1,
+		],
+		'expected' => true,
+	],
+];
---
+++ tests/Integration/inc/Engine/Optimization/DelayJS/Subscriber/addDelayJsScript.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Engine\Optimization\DelayJS\Subscriber;
+
+use WP_Rocket\Tests\Integration\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\Optimization\DelayJS\Subscriber::add_delay_js_script
+ * @group  Optimize
+ * @group  DelayJS
+ *
+ * @uses   rocket_get_constant()
+ */
+class Test_AddDelayJsScript extends TestCase {
+
+	private $delay_js = false;
+
+	public function tearDown() {
+		parent::tearDown();
+
+		remove_filter( 'pre_get_rocket_option_delay_js', [ $this, 'set_delay_js_option' ] );
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldProcessScriptHTML( $config, $expected ) {
+		$this->donotrocketoptimize       = isset( $config['do-not-optimize'] )    ? $config['do-not-optimize']    : false;
+		$this->constants['DONOTDELAYJS'] = isset( $config['do-not-delay-const'] ) ? $config['do-not-delay-const'] : false;
+
+		$this->delay_js = isset( $config['do-not-delay-setting'] ) ? $config['do-not-delay-setting'] : false;
+
+		add_filter( 'pre_get_rocket_option_delay_js', [ $this, 'set_delay_js_option' ] );
+
+		do_action( 'wp_enqueue_scripts' );
+
+		if ( false === $expected ) {
+			$this->assertFalse( wp_script_is( 'rocket-delay-js' ) );
+		} else {
+			$this->assertTrue( wp_script_is( 'rocket-delay-js' ) );
+		}
+
+	}
+
+	public function set_delay_js_option() {
+		return $this->delay_js;
+	}
+
+}
