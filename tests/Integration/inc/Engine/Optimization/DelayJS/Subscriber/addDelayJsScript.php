8dd74182c3ad8099ce4b0904344b6da0a8d78937
Rémy Perona <remperona@gmail.com>
2020-08-12 13:29:00 -0400

    update test for add_delay_js_script

--- tests/Integration/inc/Engine/Optimization/DelayJS/Subscriber/addDelayJsScript.php
+++ tests/Integration/inc/Engine/Optimization/DelayJS/Subscriber/addDelayJsScript.php
@@ -47,8 +47,10 @@ class Test_AddDelayJsScript extends TestCase {
 		do_action( 'wp_enqueue_scripts' );
 
 		if ( false === $expected ) {
+			$this->assertFalse( wp_script_is( 'rocket-browser-checker' ) );
 			$this->assertFalse( wp_script_is( 'rocket-delay-js' ) );
 		} else {
+			$this->assertTrue( wp_script_is( 'rocket-browser-checker' ) );
 			$this->assertTrue( wp_script_is( 'rocket-delay-js' ) );
 		}
 
