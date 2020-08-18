35ed31035575292c210996b571b3c9b915e6c2ed
Rémy Perona <remperona@gmail.com>
2020-08-18 17:50:17 -0400

    add missing Container namespace

--- inc/Engine/Optimization/DelayJS/ServiceProvider.php
+++ inc/Engine/Optimization/DelayJS/ServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 namespace WP_Rocket\Engine\Optimization\DelayJS;
 
-use WP_Rocket\Engine\ServiceProvider\AbstractServiceProvider;
+use WP_Rocket\Engine\Container\ServiceProvider\AbstractServiceProvider;
 
 /**
  * Service provider for the WP Rocket Delay JS
