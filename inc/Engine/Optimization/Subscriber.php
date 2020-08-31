61e84d4bfd29a905c413eeda5ec98a152da83a86
Rémy Perona <remperona@gmail.com>
2020-08-31 16:56:11 -0400

    add optimization subscriber

---
+++ inc/Engine/Optimization/Subscriber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WP_Rocket\Engine\Optimization;
+
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Event_Management\Subscriber_Interface;
+
+class Subscriber implements Subscriber_Interface {
+	private $api;
+	private $options;
+
+	public function __construct( APIclient $api, Options_Data $options ) {
+		$this->api     = $api;
+		$this->options = $options;
+	}
+
+	public static function get_subscribed_events() {
+		return [
+			'rocket_buffer' => 'optimize',
+		];
+	}
+
+	public function optimize( $html ) {
+		return $this->api->optimize(
+			$html,
+			[
+				'unused_css' => true,
+			]
+		);
+	}
+}
