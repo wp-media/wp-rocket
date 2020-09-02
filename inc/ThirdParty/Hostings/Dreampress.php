7855c49fc8abe7bcb7d8ae29b7928e986c9e236b
Rémy Perona <remperona@gmail.com>
2020-09-02 16:40:44 -0400

    add Dreampress compatibility class

---
+++ inc/ThirdParty/Hostings/Dreampress.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace WP_Rocket\ThirdParty\Hostings;
+
+use WP_Rocket\Engine\Activation\ActivationInterface;
+use WP_Rocket\Event_Management\Subscriber_Interface;
+use WP_Rocket\ThirdParty\NullSubscriber;
+use WP_Rocket\ThirdParty\ReturnTypesTrait;
+
+/**
+ * Compatibility class for DreamPress
+ *
+ * @since 3.7.1
+ */
+class Dreampress extends NullSubscriber implements Subscriber_Interface, ActivationInterface {
+	use ReturnTypesTrait;
+
+	/**
+	 * Array of events this subscriber wants to listen to.
+	 *
+	 * @since 3.7.1
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events() {
+		return [
+			'do_rocket_varnish_http_purge'            => 'return_true',
+			'rocket_varnish_field_settings'           => 'varnish_addon_title',
+			'rocket_display_input_varnish_auto_purge' => 'return_false',
+			'rocket_cache_mandatory_cookies'          => [ 'return_empty_array', PHP_INT_MAX ],
+			'rocket_htaccess_mod_expires'             => [ 'remove_htaccess_html_expire', 5 ],
+		];
+	}
+
+	/**
+	 * Changes the text on the Varnish one-click block.
+	 *
+	 * @since 3.7.1
+	 *
+	 * @param array $settings Field settings data.
+	 *
+	 * @return array modified field settings data.
+	 */
+	public function varnish_addon_title( $settings ) {
+		$settings['varnish_auto_purge']['title'] = sprintf(
+		// Translators: %s = Hosting name.
+			__( 'Your site is hosted on %s, we have enabled Varnish auto-purge for compatibility.', 'rocket' ),
+			'Dreampress'
+		);
+
+		return $settings;
+	}
+
+	/**
+	 * Remove expiration on HTML to prevent issue with Varnish cache.
+	 *
+	 * @since 3.7.1
+	 *
+	 * @param  string $rules htaccess rules.
+	 *
+	 * @return string        Updated htaccess rules.
+	 */
+	public function remove_htaccess_html_expire( $rules ) {
+		$rules = preg_replace( '@\s*#\s*Your document html@', '', $rules );
+		$rules = preg_replace( '@\s*ExpiresByType text/html\s*"access plus \d+ (seconds|minutes|hour|week|month|year)"@', '', $rules );
+
+		return $rules;
+	}
+
+	/**
+	 * Performs these actions during the plugin activation
+	 *
+	 * @return void
+	 */
+	public function activate() {
+		add_action( 'rocket_activation', [ $this, 'activate_no_htaccess_html_expire' ] );
+	}
+
+	/**
+	 * Remove expiration on HTML on activation to prevent issue with Varnish cache.
+	 *
+	 * @since 3.6.3
+	 *
+	 * @return void
+	 */
+	public function activate_no_htaccess_html_expire() {
+		add_filter( 'rocket_htaccess_mod_expires', [ $this, 'remove_htaccess_html_expire' ] );
+	}
+}
