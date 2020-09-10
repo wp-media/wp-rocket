54e38b7fbd934b3d71faa9ae1034fb1e89103473
Vasilis Manthos <vmanthos@users.noreply.github.com>
2020-09-10 12:33:07 +0300

    Add inline JS exclusions
    Related ticket: https://secure.helpscout.net/conversation/1243806177/184297?folderId=2135277

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -760,6 +760,8 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'wp_post_blocks_vars.listed_posts=[',
 			'captcha-hash',
 			'mapdata={',
+			'.ywpc-char-',
+			').countdowntimer(',
 		];
 
 		/**
