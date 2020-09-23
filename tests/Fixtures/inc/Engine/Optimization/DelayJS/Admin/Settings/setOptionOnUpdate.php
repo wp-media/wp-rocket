ceac53a5a7fd35ee3affafbaf98075544fe6ae16
Caspar Green <cg@caspar.green>
2020-09-23 13:28:41 -0400

    Revert "Update set_option_on_update"
    This reverts commit 3ca0f67b568233b67cee897bd873163f5b40f9d6.

--- tests/Fixtures/inc/Engine/Optimization/DelayJS/Admin/Settings/setOptionOnUpdate.php
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/Admin/Settings/setOptionOnUpdate.php
@@ -1,15 +1,11 @@
 <?php
 return [
-	'ShouldUpdateOptionWithVersionBelow3.7'           => [
+	'ShouldUpdateOptionWithVersionBelow3.7' => [
 		'old_version'   => '3.5',
 		'valid_version' => true,
 	],
-	'ShouldUpdateOptionWithVersionBetween3.7And3.7.2' => [
-		'old_version'   => '3.7',
-		'valid_version' => true,
-	],
-	'ShouldNotUpdateOptionWithVersionAbove3.7.2'      => [
-		'old_version'   => '3.7.3',
+	'ShouldNotUpdateOptionWithVersionAbove3.7' => [
+		'old_version'   => '3.8',
 		'valid_version' => false,
 	],
 ];
