8c5651d08c5443922e6c502135290aa2b0c734c0
Rémy Perona <remperona@gmail.com>
2020-09-29 15:38:54 -0400

    add the service provider for the license module

---
+++ inc/Engine/License/ServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace WP_Rocket\Engine\License;
+
+use WP_Rocket\Engine\Container\ServiceProvider\AbstractServiceProvider;
+use WP_Rocket\Engine\License\API\PricingClient;
+use WP_Rocket\Engine\License\API\UserClient;
+use WP_Rocket\Engine\License\Pricing;
+use WP_Rocket\Engine\License\Renewal;
+use WP_Rocket\Engine\License\Subscriber;
+use WP_Rocket\Engine\License\Upgrade;
+
+/**
+ * Service Provider for the License module
+ *
+ * @since 3.7.3
+ */
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Aliases the service provider provides
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'pricing_client',
+		'user_client',
+		'pricing',
+		'upgrade',
+		'renewal',
+		'license_subscriber',
+	];
+
+	/**
+	 * Registers items with the container
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$views = __DIR__ . '/views';
+
+		$this->getContainer()->add( 'pricing_client', PricingClient::class );
+		$this->getContainer()->add( 'user_client', UserClient::class )
+			->withArgument( $this->getContainer()->get( 'options' ) );
+		$this->getContainer()->add( 'pricing', Pricing::class )
+			->withArgument( $this->getContainer()->get( 'pricing_client' ) )
+			->withArgument( $this->getContainer()->get( 'user_client' ) );
+		$this->getContainer()->add( 'upgrade', Upgrade::class )
+			->withArgument( $this->getContainer()->get( 'pricing' ) )
+			->withArgument( $this->getContainer()->get( 'user_client' ) )
+			->withArgument( $views );
+		$this->getContainer()->add( 'renewal', Renewal::class )
+			->withArgument( $this->getContainer()->get( 'pricing' ) )
+			->withArgument( $this->getContainer()->get( 'user_client' ) )
+			->withArgument( $views );
+		$this->getContainer()->share( 'license_subscriber', Subscriber::class )
+			->withArgument( $this->getContainer()->get( 'upgrade' ) )
+			->withArgument( $this->getContainer()->get( 'renewal' ) );
+	}
+}
