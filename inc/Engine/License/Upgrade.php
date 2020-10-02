1f2fcca368adb8b2df01a43a39c33acc551037f2
Rémy Perona <remperona@gmail.com>
2020-10-02 14:14:54 -0400

    add docblocks

--- inc/Engine/License/Upgrade.php
+++ inc/Engine/License/Upgrade.php
@@ -7,9 +7,27 @@ use WP_Rocket\Engine\License\API\Pricing;
 use WP_Rocket\Engine\License\API\User;
 
 class Upgrade extends Abstract_Render {
+	/**
+	 * Pricing instance
+	 *
+	 * @var Pricing
+	 */
 	private $pricing;
+
+	/**
+	 * User instance
+	 *
+	 * @var User
+	 */
 	private $user;
 
+	/**
+	 * Instantiate the class
+	 *
+	 * @param Pricing $pricing       Pricing instance.
+	 * @param User    $user          User instance.
+	 * @param string  $template_path Path to the views.
+	 */
 	public function __construct( Pricing $pricing, User $user, $template_path ) {
 		parent::__construct( $template_path );
 
@@ -17,14 +35,24 @@ class Upgrade extends Abstract_Render {
 		$this->user    = $user;
 	}
 
+	/**
+	 * Displays the upgrade section in the license block on the dashboard
+	 *
+	 * @return void
+	 */
 	public function display_upgrade_section() {
 		if ( ! $this->can_upgrade() ) {
 			return;
 		}
 
-		echo $this->generate( 'upgrade-section' );
+		echo $this->generate( 'upgrade-section' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
+	/**
+	 * Displays the upgrade pop on the dashboard
+	 *
+	 * @return void
+	 */
 	public function display_upgrade_popin() {
 		if ( ! $this->can_upgrade() ) {
 			return;
@@ -35,9 +63,15 @@ class Upgrade extends Abstract_Render {
 			'upgrades'        => $this->get_upgrade_choices(),
 		];
 
-		echo $this->generate( 'upgrade-popin', $data );
+		echo $this->generate( 'upgrade-popin', $data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
+	/**
+	 * Adds the notification bubble to WP Rocket menu item when a promo is active
+	 *
+	 * @param string $menu_title Menu title.
+	 * @return string
+	 */
 	public function add_notification_bubble( $menu_title ) {
 		if ( ! $this->can_upgrade() ) {
 			return $menu_title;
@@ -54,6 +88,11 @@ class Upgrade extends Abstract_Render {
 		return $menu_title . '<span class="awaiting-mod">!</span>';
 	}
 
+	/**
+	 * Prevents the notification bubble from showing once the user accessed the dashboard once
+	 *
+	 * @return void
+	 */
 	public function dismiss_notification_bubble() {
 		if ( ! $this->can_upgrade() ) {
 			return;
@@ -70,6 +109,11 @@ class Upgrade extends Abstract_Render {
 		add_user_meta( get_current_user_id(), 'rocket_promo_seen', 1, true );
 	}
 
+	/**
+	 * Schedules a reset of the user meta when a promo ends to be ready for the next one
+	 *
+	 * @return void
+	 */
 	public function schedule_promo_reset() {
 		if ( ! $this->can_upgrade() ) {
 			return;
@@ -86,10 +130,20 @@ class Upgrade extends Abstract_Render {
 		wp_schedule_single_event( $this->pricing->get_promo_end(), 'rocket_schedule_promo_reset' );
 	}
 
+	/**
+	 * Deletes the user meta preventing the notification bubble from showing
+	 *
+	 * @return void
+	 */
 	public function reset_promo_user_meta() {
 		delete_metadata( 'user', 0, 'rocket_promo_seen', null, true );
 	}
 
+	/**
+	 * Checks if the current license can upgrade
+	 *
+	 * @return boolean
+	 */
 	private function can_upgrade() {
 		return (
 			-1 !== $this->user->get_license_type()
@@ -98,6 +152,11 @@ class Upgrade extends Abstract_Render {
 		);
 	}
 
+	/**
+	 * Gets the upgrade choices depending on the current license level
+	 *
+	 * @return array
+	 */
 	private function get_upgrade_choices() {
 		$choices = [];
 		$license = $this->user->get_license_type();
@@ -112,6 +171,11 @@ class Upgrade extends Abstract_Render {
 		return $choices;
 	}
 
+	/**
+	 * Gets the data to upgrade from single to plus
+	 *
+	 * @return array
+	 */
 	private function get_upgrade_from_single_to_plus_data() {
 		return [
 			'name'          => 'Plus',
@@ -122,6 +186,11 @@ class Upgrade extends Abstract_Render {
 		];
 	}
 
+	/**
+	 * Gets the data to upgrade from single to infinite
+	 *
+	 * @return array
+	 */
 	private function get_upgrade_from_single_to_infinite_data() {
 		return [
 			'name'          => 'Infinite',
@@ -132,6 +201,11 @@ class Upgrade extends Abstract_Render {
 		];
 	}
 
+	/**
+	 * Gets the data to upgrade from plus to infinite
+	 *
+	 * @return array
+	 */
 	private function get_upgrade_from_plus_to_infinite_data() {
 		return [
 			'name'          => 'Infinite',
