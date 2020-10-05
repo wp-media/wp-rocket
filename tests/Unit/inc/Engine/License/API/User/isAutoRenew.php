a2de22c74da1ea0e4e48e1607961af2896f91892
Rémy Perona <remperona@gmail.com>
2020-10-05 16:48:16 -0400

    add tests

---
+++ tests/Unit/inc/Engine/License/API/User/isAutoRenew.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\Engine\License\API\User;
+
+use WP_Rocket\Engine\License\API\User;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\License\API\User::is_auto_renew
+ *
+ * @group License
+ */
+class IsAutoRenew extends TestCase {
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpected( $data, $expected ) {
+		$user = new User( $data );
+
+		$this->assertEquals(
+			$expected,
+			$user->is_auto_renew()
+		);
+	}
+}
