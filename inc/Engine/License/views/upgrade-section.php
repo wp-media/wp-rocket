2b2ffec1986b0d49e8aa63b6e23f785bc5500ebb
Rémy Perona <remperona@gmail.com>
2020-10-06 15:34:43 -0400

    update view for the upgrade section

--- inc/Engine/License/views/upgrade-section.php
+++ inc/Engine/License/views/upgrade-section.php
@@ -5,6 +5,9 @@
  * @since 3.7.3
  */
 
-defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+defined( 'ABSPATH' ) || exit;
 ?>
-<p><?php esc_html_e( 'You can use WP Rocket on more websites by upgrading your license (you will only pay the price difference between your current and new licenses). Upgrade Now', 'rocket' ); ?></p>
+<p>
+<?php esc_html_e( 'You can use WP Rocket on more websites by upgrading your license (you will only pay the price difference between your current and new licenses).', 'rocket' ); ?>
+<button id="wpr-popin-upgrade-toggle"><?php esc_html_e( 'Upgrade Now', 'rocket' ); ?></button>
+</p>
