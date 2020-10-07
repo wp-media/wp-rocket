695da40eb365709391e1bc41c259d15f64ab0799
Rémy Perona <remperona@gmail.com>
2020-10-07 11:35:41 -0400

    add docblocks

--- inc/Engine/License/Subscriber.php
+++ inc/Engine/License/Subscriber.php
@@ -5,46 +5,49 @@ namespace WP_Rocket\Engine\License;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 class Subscriber implements Subscriber_Interface {
+	/**
+	 * Upgrade instance
+	 *
+	 * @var Upgrade
+	 */
 	private $upgrade;
 
+	/**
+	 * Instantiate the class
+	 *
+	 * @param Upgrade $upgrade Upgrade instance.
+	 */
 	public function __construct( Upgrade $upgrade ) {
 		$this->upgrade = $upgrade;
 	}
 
+	/**
+	 * Events this subscriber listens to.
+	 *
+	 * @return array
+	 */
 	public static function get_subscribed_events() {
 		return [
-			'rocket_after_license_info'   => 'display_upgrade_section',
-			'rocket_settings_page_footer' => 'display_upgrade_popin',
-			'rocket_menu_title'           => 'add_promo_bubble',
-			'admin_footer-settings_page_wprocket' => [
-				[ 'dismiss_notification_bubble' ],
-				[ 'schedule_promo_reset' ],
-			],
-			'rocket_schedule_promo_reset' => 'reset_promo_user_meta',
+			'rocket_dashboard_license_info' => 'display_upgrade_section',
+			'rocket_settings_page_footer'   => 'display_upgrade_popin',
 		];
 	}
 
+	/**
+	 * Displays the upgrade section in the license info block
+	 *
+	 * @return void
+	 */
 	public function display_upgrade_section() {
 		$this->upgrade->display_upgrade_section();
 	}
 
+	/**
+	 * Displays the upgrade popin
+	 *
+	 * @return void
+	 */
 	public function display_upgrade_popin() {
 		$this->upgrade->display_upgrade_popin();
-	}
-
-	public function add_promo_bubble( $menu_title ) {
-		return $this->upgrade->add_notification_bubble( $menu_title );
-	}
-
-	public function dismiss_notification_bubble() {
-		$this->upgrade->dismiss_notification_bubble();
-	}
-
-	public function schedule_promo_reset() {
-		$this->upgrade->schedule_promo_reset();
-	}
-
-	public function reset_promo_user_meta() {
-		$this->upgrade->reset_promo_user_meta();
 	}
 }
