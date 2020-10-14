9ff89ef43b7b6e4c3e9f93d9184f791dac0313ff
Rémy Perona <remperona@gmail.com>
2020-10-14 10:29:18 -0400

    add close button and fix display issues

--- inc/Engine/License/views/promo-banner.php
+++ inc/Engine/License/views/promo-banner.php
@@ -2,23 +2,23 @@
 /**
  * Promo banner.
  *
- * @since 3.7.3
+ * @since 3.7.4
  */
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<div>
+<div id="rocket-promo-banner">
 	<section>
 		<span>
 			<?php
 			// translators: %s = promotion discount percentage.
-			printf( esc_html__( '%s% off', 'rocket' ), esc_html( $data['promo_discount'] ) );
+			printf( esc_html__( '%s off', 'rocket' ), esc_html( $data['discount_percent'] . '%' ) );
 			?>
 		</span>
 		<h3>
 			<?php
 			// translators: %s = promotion name.
-			printf( esc_html__( '%s promotion is live!', 'rocket' ), esc_html( $data['promo_name'] ) );
+			printf( esc_html__( '%s promotion is live!', 'rocket' ), esc_html( $data['name'] ) );
 			?>
 		</h3>
 		<p><?php echo esc_html( $data['message'] ); ?></p>
@@ -28,4 +28,5 @@ defined( 'ABSPATH' ) || exit;
 		<div></div>
 		<button><?php esc_html_e( 'Upgrade now', 'rocket' ); ?></button>
 	</div>
+	<button class="wpr-notice-close wpr-icon-close" id="rocket-dismiss-promotion"><span class="screen-reader-text"><?php esc_html_e( 'Dismiss this notice.', 'rocket' ); ?></span></button>
 </div>
