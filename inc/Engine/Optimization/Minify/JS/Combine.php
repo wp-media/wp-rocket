abda304c36d44a7b6fa351cf27e84085a63a770e
Alfonso Catron <alfonso.catron@gmail.com>
2020-10-20 17:07:57 -0300

    Exclude aepc_pixel inline scripts from combine JS (PR #3226)

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -652,6 +652,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'aepro',
 			'cdn.jst.ai',
 			'w2dc_fields_in_categories',
+			'aepc_pixel',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
