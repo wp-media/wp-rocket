4ac6bfa07b720c3ecfca30c08d0366772519a9fd
Alfonso Catron <alfonso.catron@gmail.com>
2020-10-21 13:31:55 -0300

    deactivating WP Rocket lazyload only when the Avada value is selected
    Modified the code to deactivate WP Rocket Lazyload only when Avada Lazyload is selected
    
    And also disabled our option when the condition is met.

--- inc/3rd-party/themes/avada.php
+++ inc/3rd-party/themes/avada.php
@@ -18,12 +18,9 @@ if ( 'Avada' === $current_theme->get( 'Name' ) ) {
 	 * @param string $value New Avada option value.
 	 * @return void
 	 */
-	 function rocket_avada_maybe_deactivate_lazyload( $old_value, $value ) {
-		if ( (empty( $old_value['lazy_load'] ) && ! empty( $value['lazy_load'] ) ) 
-			|| ( 'none' === $old_value['lazy_load'] && 'none' !== $value['lazy_load'] ) ) {
-			
+	function rocket_avada_maybe_deactivate_lazyload( $old_value, $value ) {
+		if ( ( empty( $old_value['lazy_load'] ) && ! empty( $value['lazy_load'] ) ) || ( 'avada' === $value['lazy_load'] ) ) {
 			update_rocket_option( 'lazyload', 0 );
-			
 		}
 	}
 	add_action( 'update_option_fusion_options', 'rocket_avada_maybe_deactivate_lazyload', 10, 2 );
@@ -40,9 +37,8 @@ if ( 'Avada' === $current_theme->get( 'Name' ) ) {
 function rocket_avada_maybe_disable_lazyload() {
 	$avada_options = get_option( 'fusion_options' );
 	$current_theme = wp_get_theme();
-  
-	if ( 'Avada' === $current_theme->get( 'Name' ) 	&&  ( empty( $avada_options['lazy_load'] )  || 'none' === $avada_options['lazy_load'] ) ) {
-				
+
+	if  ( ( 'Avada' === $current_theme->get( 'Name' ) ) && ( empty( $avada_options['lazy_load'] ) || 'avada' !== $avada_options['lazy_load']  ) ) {
 		return false;
 	}
 
