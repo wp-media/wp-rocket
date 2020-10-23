d3efaf65a489ab6de988ec0abc7b91c0946ab0ff
Rémy Perona <remperona@gmail.com>
2020-10-23 10:50:45 -0400

    prevent going beyond zero in the countdown

--- src/js/global/countdown.js
+++ src/js/global/countdown.js
@@ -29,14 +29,17 @@ function initializeClock(id, endtime) {
   
     function updateClock() {
         const t = getTimeRemaining(endtime);
+
+        if (t.total < 0) {
+            clearInterval(timeinterval);
+
+            return;
+        }
+
         daysSpan.innerHTML = t.days;
         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
-        }
     }
   
     updateClock();
