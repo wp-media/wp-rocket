87504dc654956d676a098bc90cb9d951b1248179
Romain Vincent <WordPresseur@users.noreply.github.com>
2020-10-23 16:15:54 +0200

    Update inline JS exclusions from combine JS
    Exclude wpvViewHead
    Ref: https://secure.helpscout.net/conversation/1315513042/204256/

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -658,6 +658,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'fcaPcPost',
 			'csrf_token',
 			'icwp_wpsf_vars_lpantibot',
+			'wpvViewHead',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
