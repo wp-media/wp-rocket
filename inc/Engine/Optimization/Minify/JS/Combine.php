4923881245573846970195dd279d081de31668fb
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2020-10-26 20:07:45 +0100

    Add new inline JS exclusion from combine JS (#3242)
    `ed_school_plugin`

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -658,6 +658,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'fcaPcPost',
 			'csrf_token',
 			'icwp_wpsf_vars_lpantibot',
+			'ed_school_plugin',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
