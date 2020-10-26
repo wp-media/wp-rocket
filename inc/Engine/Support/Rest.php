0742fa1015aa5173bd9013d4849700ef2df43c04
Rémy Perona <remperona@gmail.com>
2020-10-26 15:40:05 -0400

    add rest class

---
+++ inc/Engine/Support/Rest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WP_Rocket\Engine\Support;
+
+class Rest {
+	private $data;
+
+	public function __construct( Data $data ) {
+		$this->data = $data;
+	}
+
+	public function register_route() {
+
+	}
+}
