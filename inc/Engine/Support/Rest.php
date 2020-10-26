1d65b33b5095b734a2dac9780745e81389e6bd00
Rémy Perona <remperona@gmail.com>
2020-10-26 17:02:53 -0400

    add class methods

--- inc/Engine/Support/Rest.php
+++ inc/Engine/Support/Rest.php
@@ -2,14 +2,72 @@
 
 namespace WP_Rocket\Engine\Support;
 
-class Rest {
-	private $data;
+use WP_Rocket\Admin\Options_Data;
 
-	public function __construct( Data $data ) {
-		$this->data = $data;
+class Rest {
+	const ROUTE_NAMESPACE = 'wp-rocket/v1';
+
+	private $data;
+	private $options;
+
+	public function __construct( Data $data, Options_Data $options ) {
+		$this->data    = $data;
+		$this->options = $options;
 	}
 
 	public function register_route() {
+		register_rest_route(
+			self::ROUTE_NAMESPACE,
+			'support',
+			[
+				'methods'             => 'POST',
+				'callback'            => [ $this, 'get_support_data' ],
+				'args'                => [
+					'email' => [
+						'required'          => true,
+						'validate_callback' => [ $this, 'validate_email' ],
+					],
+					'key'   => [
+						'required'          => true,
+						'validate_callback' => [ $this, 'validate_key' ],
+					],
+				],
+				'permission_callback' => '__return_true',
+			]
+		);
+	}
 
+	public function get_support_data() {
+		if ( false === strpos( wp_get_referer(), 'wp-rocket.me' ) ) {
+			return rest_ensure_response( [] );
+		}
+
+		return rest_ensure_response( $this->data->get_support_data() );
+	}
+
+	/**
+	 * Checks that the email sent along the request corresponds to the one saved in the DB
+	 *
+	 * @since 3.7.5
+	 *
+	 * @param string $param Parameter value to validate.
+	 *
+	 * @return bool
+	 */
+	public function validate_email( $param ) {
+		return $param === $this->options->get( 'consumer_email' );
+	}
+
+	/**
+	 * Checks that the key sent along the request corresponds to the one saved in the DB
+	 *
+	 * @since 3.7.5
+	 *
+	 * @param string $param Parameter value to validate.
+	 *
+	 * @return bool
+	 */
+	public function validate_key( $param ) {
+		return $param === $this->options->get( 'consumer_key' );
 	}
 }
