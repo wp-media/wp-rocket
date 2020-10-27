909ce8b76c13033dae75189606171ff9095ffb30
Rémy Perona <remperona@gmail.com>
2020-10-27 11:47:19 -0400

    Update beacon instantiation

--- inc/Engine/Admin/Beacon/ServiceProvider.php
+++ inc/Engine/Admin/Beacon/ServiceProvider.php
@@ -35,6 +35,7 @@ class ServiceProvider extends AbstractServiceProvider {
 	public function register() {
 		$this->getContainer()->add( 'beacon', 'WP_Rocket\Engine\Admin\Beacon\Beacon' )
 			->withArgument( $this->getContainer()->get( 'options' ) )
-			->withArgument( $this->getContainer()->get( 'template_path' ) . '/settings' );
+			->withArgument( $this->getContainer()->get( 'template_path' ) . '/settings' )
+			->withArgument( $this->getContainer()->get( 'support_data' ) );
 	}
 }
