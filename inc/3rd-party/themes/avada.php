e02e05f8495281f60e1528a1c287a8bc19714a41
Rémy Perona <remperona@gmail.com>
2020-10-28 15:43:16 -0400

    fix my typos

--- inc/3rd-party/themes/avada.php
+++ inc/3rd-party/themes/avada.php
@@ -12,14 +12,17 @@ if ( 'Avada' === $current_theme->get( 'Name' ) ) {
 	 * Deactivate WP Rocket lazyload if Avada lazyload is enabled
 	 *
 	 * @since 3.3.4
-	 * @author Remy Perona
 	 *
 	 * @param string $old_value Previous Avada option value.
 	 * @param string $value New Avada option value.
 	 * @return void
 	 */
 	function rocket_avada_maybe_deactivate_lazyload( $old_value, $value ) {
-		if ( ( empty( $old_value['lazy_load'] ) || ( ! empty( $value['lazy_load'] ) && ( 'avada' === $value['lazy_load'] ) ) {
+		if (
+			empty( $old_value['lazy_load'] )
+			||
+			( ! empty( $value['lazy_load'] ) && 'avada' === $value['lazy_load'] )
+		) {
 			update_rocket_option( 'lazyload', 0 );
 		}
 	}
@@ -30,7 +33,6 @@ if ( 'Avada' === $current_theme->get( 'Name' ) ) {
  * Disable WP Rocket lazyload field if Avada lazyload is enabled
  *
  * @since 3.3.4
- * @author Remy Perona
  *
  * @return bool
  */
@@ -38,7 +40,7 @@ function rocket_avada_maybe_disable_lazyload() {
 	$avada_options = get_option( 'fusion_options' );
 	$current_theme = wp_get_theme();
 
-	if ( ( 'Avada' !== $current_theme->get( 'Name' ) ) {
+	if ( 'Avada' !== $current_theme->get( 'Name' ) ) {
 		return false;
 	}
 
@@ -57,7 +59,8 @@ function rocket_avada_maybe_disable_lazyload() {
  * Clears WP Rocket's cache after Avada's Fusion Patcher flushes their caches
  *
  * @since 3.3.5
- * @author Vasilis Manthos
+ *
+ * @return void
  */
 function rocket_avada_clear_cache_fusion_patcher() {
 	rocket_clean_domain();
