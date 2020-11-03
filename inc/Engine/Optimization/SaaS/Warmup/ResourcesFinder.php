2145b406d9758f40fcc43bd4bb9c003e54c69bc5
Rémy Perona <remperona@gmail.com>
2020-11-03 14:19:10 -0500

    add code to find and queue resources

--- inc/Engine/Optimization/SaaS/Warmup/ResourcesFinder.php
+++ inc/Engine/Optimization/SaaS/Warmup/ResourcesFinder.php
@@ -2,23 +2,43 @@
 
 namespace WP_Rocket\Engine\Optimization\SaaS\Warmup;
 
-class ResourcesFinder {
+use \WP_Rocket_WP_Async_Request;
+
+class ResourcesFinder extends WP_Rocket_WP_Async_Request {
+	protected $prefix = 'rocket';
+	protected $action = 'launch_warmup';
+	private $styles = [];
+	private $scripts = [];
+
 	public function __construct( ResourcesFetcher $fetcher ) {
+		parent::__construct();
+
 		$this->fetcher = $fetcher;
 	}
 
-	public function warmup() {
+	protected function handle() {
 		$html = $this->get_homepage_html();
 
 		if ( empty( $html ) ) {
 			return;
 		}
 
-		$this->find_resources( $html );
+		$this->find_styles( $html );
+		$this->find_scripts( $html );
+
+		$this->queue_styles();
+		$this->queue_scripts();
+
+		$this->fetcher->save()->dispatch();
 	}
 
 	private function get_homepage_html() {
-		$response = wp_safe_remote_get( home_url() );
+		$response = wp_safe_remote_get(
+			home_url(),
+			[
+				'sslverify' => apply_filters( 'https_local_ssl_verify', false ),
+			]
+		);
 
 		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
 			return '';
@@ -27,7 +47,73 @@ class ResourcesFinder {
 		return wp_remote_retrieve_body( $response );
 	}
 
-	private function find_resources( $html ) {
-		
+	private function find_styles( $html ) {
+		$links = $this->find( '<link(?:[^>]+[\s"\'])?href\s*=\s*[\'"]\s*(?<url>[^\'"\s]+)\s*?[\'"](?:[^>]+)?\/?>', $html );
+
+		if ( empty( $links ) ) {
+			return;
+		}
+
+		foreach ( $links as $link ) {
+			if ( ! (bool) preg_match( '/rel=[\'"]stylesheet[\'"]/is', $link[0] ) ) {
+				continue;
+			}
+
+			$this->styles[] = $link['url'];
+		}
+	}
+
+	private function find_scripts( $html ) {
+		$scripts = $this->find( '<script\s+(?:[^>]+[\s\'"])?src\s*=\s*[\'"]\s*?(?<url>[^\'"\s]+)\s*?[\'"](?:[^>]+)?\/?>', $html );
+
+		if ( empty( $scripts ) ) {
+			return;
+		}
+
+		foreach ( $scripts as $script ) {
+			$this->scripts[] = $script['url'];
+		}
+	}
+
+	private function find( $pattern, $html ) {
+		if ( ! preg_match_all( '/' . $pattern . '/is', $html, $matches, PREG_SET_ORDER ) ) {
+			return [];
+		}
+
+		if ( empty( $matches ) ) {
+			return [];
+		}
+
+		return $matches;
+	}
+
+	private function queue_styles() {
+		if ( empty( $this->styles ) ) {
+			return;
+		}
+
+		foreach ( $this->styles as $style ) {
+			$this->fetcher->push_to_queue(
+				[
+					'url'  => $style,
+					'type' => 'css',
+				]
+			);
+		}
+	}
+
+	private function queue_scripts() {
+		if ( empty( $this->scripts ) ) {
+			return;
+		}
+
+		foreach ( $this->scripts as $script ) {
+			$this->fetcher->push_to_queue(
+				[
+					'url'  => $script,
+					'type' => 'js',
+				]
+			);
+		}
 	}
 }
