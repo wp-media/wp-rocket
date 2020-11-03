74ab39b8d47761e523688841a00540aa051d3cb4
Rémy Perona <remperona@gmail.com>
2020-11-03 14:18:40 -0500

    add new even to subscriber

--- inc/Engine/Optimization/SaaS/Subscriber.php
+++ inc/Engine/Optimization/SaaS/Subscriber.php
@@ -3,20 +3,24 @@
 namespace WP_Rocket\Engine\Optimization\SaaS;
 
 use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Engine\Optimization\SaaS\Warmup\ResourcesFinder;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 class Subscriber implements Subscriber_Interface {
 	private $api;
 	private $options;
+	private $finder;
 
-	public function __construct( APIclient $api, Options_Data $options ) {
+	public function __construct( APIclient $api, Options_Data $options, ResourcesFinder $finder ) {
 		$this->api     = $api;
 		$this->options = $options;
+		$this->finder  = $finder;
 	}
 
 	public static function get_subscribed_events() {
 		return [
 			'rocket_buffer' => 'optimize',
+			'update_option_wp_rocket_settings' => 'warmup',
 		];
 	}
 
@@ -27,5 +31,9 @@ class Subscriber implements Subscriber_Interface {
 				'tree_shake' => 1,
 			]
 		);
+	}
+
+	public function warmup() {
+		$this->finder->dispatch();
 	}
 }
