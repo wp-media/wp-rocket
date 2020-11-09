36bf0661d920dbcaf6a2a1d7d1fe033f76aff2f7
Rémy Perona <remy@wp-rocket.me>
2020-11-09 11:51:03 -0500

    Closes #3247 Update minimum PHP version to 7.0 (PR #3250)
    * update minimum PHP version to 7.0
    * add minimum versions in plugin header
    * update travis config
    * update composer config
    * change legacy PHP to 7.1

--- wp-rocket.php
+++ wp-rocket.php
@@ -4,6 +4,8 @@
  * Plugin URI: https://wp-rocket.me
  * Description: The best WordPress performance plugin.
  * Version: 3.7.4
+ * Requires at least: 5.2
+ * Requires PHP: 7.0
  * Code Name: Jakku
  * Author: WP Media
  * Author URI: https://wp-media.me
@@ -21,7 +23,7 @@ defined( 'ABSPATH' ) || exit;
 define( 'WP_ROCKET_VERSION',               '3.7.4' );
 define( 'WP_ROCKET_WP_VERSION',            '5.2' );
 define( 'WP_ROCKET_WP_VERSION_TESTED',     '5.5.1' );
-define( 'WP_ROCKET_PHP_VERSION',           '5.6' );
+define( 'WP_ROCKET_PHP_VERSION',           '7.0' );
 define( 'WP_ROCKET_PRIVATE_KEY',           false );
 define( 'WP_ROCKET_SLUG',                  'wp_rocket_settings' );
 define( 'WP_ROCKET_WEB_MAIN',              'https://wp-rocket.me/' );
