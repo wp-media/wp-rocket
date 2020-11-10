7ede4fca8a45ddcdca0dd9fb264a8dbd8c665f5a
Rémy Perona <remperona@gmail.com>
2020-11-10 11:20:58 -0500

    update fixture for support data

--- tests/Fixtures/inc/Engine/Support/Data/getSupportData.php
+++ tests/Fixtures/inc/Engine/Support/Data/getSupportData.php
@@ -81,7 +81,7 @@ return [
 			'WP Rocket Version'        => '3.7.5',
 			'Theme'                    => 'WordPress Default',
 			'Plugins Enabled'          => 'Hello Dolly',
-			'WP Rocket Active Options' => 'Mobile Cache - Disable Emojis - Defer JS Safe - Combine Google Fonts - Preload',
+			'WP Rocket Active Options' => 'Mobile Cache - Disable Emojis - Defer JS Safe - Combine Google Fonts - Preload - Hearbeat Control',
 		],
 	],
 ];
