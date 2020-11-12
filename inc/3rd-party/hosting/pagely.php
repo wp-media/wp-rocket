00bc3ee33d356ca8d60857816d058fb7a17972b7
klong-pagely <keelan.long@pagely.com>
2020-11-12 16:29:02 +0000

    Stop the infinite loop when purging cache on Pagely (PR #3166)

--- inc/3rd-party/hosting/pagely.php
+++ inc/3rd-party/hosting/pagely.php
@@ -9,11 +9,16 @@ defined( 'ABSPATH' ) || exit;
  *
  * @return void
  */
-function rocket_clear_cache_after_pagely() {
-	// Clear all caching files.
-	rocket_clean_domain();
-}
-add_action( 'pagely_cache_purge_after', 'rocket_clear_cache_after_pagely' );
+//  We can't have both this action and the next without infinite loop
+//    So commenting out until such time as this can be re-worked. See
+//    the following pull for comments on the way the wp-rocket team
+//    would like this refactored.
+//    https://github.com/wp-media/wp-rocket/pull/3166
+//function rocket_clear_cache_after_pagely() {
+//	// Clear all caching files.
+//	rocket_clean_domain();
+//}
+//add_action( 'pagely_cache_purge_after', 'rocket_clear_cache_after_pagely' );
 
 /**
  * Call the cache server to purge the cache with Pagely hosting.
