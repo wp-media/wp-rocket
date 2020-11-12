c57813b7f7c45e3683c6f0e16bf387349909ee11
Rémy Perona <remperona@gmail.com>
2020-11-12 16:49:52 -0500

    remove API key from fixtures

--- tests/Fixtures/inc/Engine/Optimization/DeferJS/Subscriber/deferJS.php
+++ tests/Fixtures/inc/Engine/Optimization/DeferJS/Subscriber/deferJS.php
@@ -3,7 +3,7 @@
 $html = <<<HTML
 	<script src="http://example.org/wp-includes/js/jquery/jquery.js?v=3.1.15" id="jquery-core"></script>
 	<script src="http://example.org/wp-content/plugins/hello-world/script.js" id="hello-script"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7nuSIID7tWZdjT6cI5ZTKGbjFPBOGNM0&#038;language=en&#038;ver=1"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=GOOGLE_API_KEY&#038;language=en&#038;ver=1"></script>
 	<script integrity="sha512-VtmdOFNyOniRUIHzkfL4GAe+yuAhoWzJIWYW/9elcd+7zNu12OKscWFIe9PRQ6VBy4djrwGVzK6MLD3oTpLpRQ==" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js" defer></script>
 	<script data-cfasync="false" src="/javascript.js"></script>
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51f58c4473f92506"></script>
@@ -12,7 +12,7 @@ HTML;
 $expected = <<<HTML
 	<script src="http://example.org/wp-includes/js/jquery/jquery.js?v=3.1.15" id="jquery-core" defer></script>
 	<script src="http://example.org/wp-content/plugins/hello-world/script.js" id="hello-script" defer></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7nuSIID7tWZdjT6cI5ZTKGbjFPBOGNM0&#038;language=en&#038;ver=1" defer></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=GOOGLE_API_KEY&#038;language=en&#038;ver=1" defer></script>
 	<script integrity="sha512-VtmdOFNyOniRUIHzkfL4GAe+yuAhoWzJIWYW/9elcd+7zNu12OKscWFIe9PRQ6VBy4djrwGVzK6MLD3oTpLpRQ==" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js" defer></script>
 	<script data-cfasync="false" src="/javascript.js" defer></script>
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51f58c4473f92506" defer></script>
@@ -21,7 +21,7 @@ HTML;
 $expected_safe_mode = <<<HTML
 	<script src="http://example.org/wp-includes/js/jquery/jquery.js?v=3.1.15" id="jquery-core"></script>
 	<script src="http://example.org/wp-content/plugins/hello-world/script.js" id="hello-script" defer></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7nuSIID7tWZdjT6cI5ZTKGbjFPBOGNM0&#038;language=en&#038;ver=1" defer></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=GOOGLE_API_KEY&#038;language=en&#038;ver=1" defer></script>
 	<script integrity="sha512-VtmdOFNyOniRUIHzkfL4GAe+yuAhoWzJIWYW/9elcd+7zNu12OKscWFIe9PRQ6VBy4djrwGVzK6MLD3oTpLpRQ==" crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.js" defer></script>
 	<script data-cfasync="false" src="/javascript.js" defer></script>
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51f58c4473f92506" defer></script>
