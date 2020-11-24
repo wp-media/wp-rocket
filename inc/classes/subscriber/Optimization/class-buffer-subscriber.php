91344bfc13dff710f1ce553f882cdd19112941e7
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-11-24 17:23:19 +0200

    add WP redirect_canonical before our cache starts

--- inc/classes/subscriber/Optimization/class-buffer-subscriber.php
+++ inc/classes/subscriber/Optimization/class-buffer-subscriber.php
@@ -8,7 +8,6 @@ use WP_Rocket\Event_Management\Subscriber_Interface;
  * Event subscriber to buffer and process a page content.
  *
  * @since  3.3
- * @author Grégory Viguier
  */
 class Buffer_Subscriber implements Subscriber_Interface {
 	/**
@@ -32,7 +31,10 @@ class Buffer_Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'template_redirect' => [ 'start_content_process', 2 ],
+			'template_redirect' => [
+				[ 'redirect_canonical', 1 ],
+				[ 'start_content_process', 2 ]
+			],
 		];
 	}
 
@@ -40,10 +42,26 @@ class Buffer_Subscriber implements Subscriber_Interface {
 	 * Start buffering the page content and apply optimizations if we can.
 	 *
 	 * @since  3.3
-	 * @access public
-	 * @author Grégory Viguier
+	 *
+	 * @return void
 	 */
 	public function start_content_process() {
-		return $this->optimizer->maybe_init_process();
+		$this->optimizer->maybe_init_process();
+	}
+
+	/**
+	 * Runs the main redirect_canonical function to redirect visitor to proper page url based on permalink if it has trailing slash or not.
+	 *
+	 * @since 3.8
+	 *
+	 * @return void
+	 */
+	public function redirect_canonical() {
+		if ( ! function_exists( 'redirect_canonical' ) ){
+			return;
+		}
+
+		remove_action( 'template_redirect', 'redirect_canonical' );
+		redirect_canonical();
 	}
 }
