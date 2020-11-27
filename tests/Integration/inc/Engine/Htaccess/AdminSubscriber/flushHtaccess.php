a2f8f64befcf711a881f7a9ebf0e63376b3193e3
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-11-27 15:40:18 +0200

    add mock for get_home_path at integration tests

--- tests/Integration/inc/Engine/Htaccess/AdminSubscriber/flushHtaccess.php
+++ tests/Integration/inc/Engine/Htaccess/AdminSubscriber/flushHtaccess.php
@@ -3,6 +3,7 @@
 namespace WP_Rocket\Tests\Integration\inc\Engine\Htaccess\AdminSubscriber;
 
 use WP_Rocket\Tests\Integration\FilesystemTestCase;
+use Brain\Monkey\Functions;
 
 /**
  * @covers \WP_Rocket\Engine\Htaccess\AdminSubscriber::flush_htaccess
@@ -14,11 +15,21 @@ class Test_FlushHtaccess extends FilesystemTestCase {
 
 	protected $path_to_test_data = '/inc/Engine/Htaccess/AdminSubscriber/flushHtaccess.php';
 
+	private $is_apache;
+
+	public function setUp() {
+		parent::setUp();
+		$this->is_apache = isset( $GLOBALS['is_apache'] ) ? $GLOBALS['is_apache'] : null;
+		$GLOBALS['is_apache'] = true;
+
+		Functions\expect( 'get_home_path' )->andReturn( 'vfs://public/' );
+	}
+
 	/**
 	 * @dataProvider providerTestData
 	 */
 	public function testShouldDoExpectedForFlushHtaccess( $config, $expected ) {
-		do_action( 'permalink_structure_changed', $config['old_permalink_structure'], $config['new_permalink_structure'] );
+		//do_action( 'permalink_structure_changed', $config['old_permalink_structure'], $config['new_permalink_structure'] );
 		$this->set_permalink_structure( $config['old_permalink_structure'] );
 		$this->set_permalink_structure( $config['new_permalink_structure'] );
 
@@ -30,6 +41,11 @@ class Test_FlushHtaccess extends FilesystemTestCase {
 		parent::tearDown();
 
 		$this->set_permalink_structure( '/%postname%/' );
+
+		// Restore the original state.
+		if ( ! empty( $this->is_apache ) ) {
+			$GLOBALS['is_apache'] = $this->is_apache;
+		}
 	}
 
 }
