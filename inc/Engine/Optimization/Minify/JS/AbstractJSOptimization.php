b900995e149dee216b7d04ffe2d9ae22479c2917
Vasilis Manthos <vmanthos@users.noreply.github.com>
2020-12-29 10:58:33 +0200

    Move 3d-flipbook files to the correct array.
    The files need to be added in the get_excluded_files() method, for the exclusions to work.

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -49,6 +49,10 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 	protected function get_excluded_files() {
 		$excluded_files   = $this->options->get( 'exclude_js', [] );
 		$excluded_files[] = '/wp-includes/js/dist/i18n.min.js';
+		$excluded_files[] = '/interactive-3d-flipbook-powered-physics-engine/assets/js/html2canvas.min.js';
+		$excluded_files[] = '/interactive-3d-flipbook-powered-physics-engine/assets/js/pdf.min.js';
+		$excluded_files[] = '/interactive-3d-flipbook-powered-physics-engine/assets/js/three.min.js';
+		$excluded_files[] = '/interactive-3d-flipbook-powered-physics-engine/assets/js/3d-flip-book.min.js';
 
 		/**
 		 * Filter JS files to exclude from minification/concatenation.
@@ -232,10 +236,6 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'loader.knack.com',
 			'embed.lpcontent.net/leadboxes/current/embed.js',
 			'cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js',
-			'/wp-content/plugins/interactive-3d-flipbook-powered-physics-engine/assets/js/html2canvas.min.js',
-			'/wp-content/plugins/interactive-3d-flipbook-powered-physics-engine/assets/js/pdf.min.js',
-			'/wp-content/plugins/interactive-3d-flipbook-powered-physics-engine/assets/js/three.min.js',
-			'/wp-content/plugins/interactive-3d-flipbook-powered-physics-engine/assets/js/3d-flip-book.min.js',
 		];
 
 		$excluded_external = array_merge( $defaults, $this->options->get( 'exclude_js', [] ) );
