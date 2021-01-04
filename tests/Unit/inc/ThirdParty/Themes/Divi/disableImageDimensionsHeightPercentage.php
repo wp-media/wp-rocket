f2fcc288e526c6e3710b8061308297b9de7bdaf0
Caspar Green <cg@caspar.green>
2021-01-04 12:38:31 -0500

    Fix Divi Logo image distortion from setting image dimensions (#3455)
    * Disable image dimensions on Divi height-percentage
    
    * Add case-insensitivity for attribute w/testcase
    
    * Add integration test and adjust fixture
    
    * remove array_values from code and add them to test code
    
    * Remove is_divi() check and related testcase
    
    * Revise integration test, fixture and event hook
    
    * Use .org tld for host
    
    * change fixture file extension
    
    * Adjust uses in tests per code review
    
    * Target correct filters in int test setup/teardown
    
    Co-authored-by: Ahmed Saeed <eng.ahmeds3ed@gmail.com>

---
+++ tests/Unit/inc/ThirdParty/Themes/Divi/disableImageDimensionsHeightPercentage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Themes\Divi;
+
+use Mockery;
+use WP_Rocket\Admin\Options;
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Tests\Unit\TestCase;
+use Brain\Monkey\Functions;
+use WP_Rocket\ThirdParty\Themes\Divi;
+use WP_Theme;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Themes\Divi::disable_image_dimensions_height_percentage
+ * @uses   ::is_divi
+ *
+ * @group  ThirdParty
+ */
+class Test_DisableImageDimensionsHeightPercentage extends TestCase {
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testAddDiviToDescription( $config, $expected ) {
+		$options_api = Mockery::mock( Options::class );
+		$options     = Mockery::mock( Options_Data::class );
+		$theme       = new WP_Theme( $config['theme-name'], 'wp-content/themes/' );
+		$theme->set_name( $config['theme-name'] );
+
+		if ( $config['theme-template'] ) {
+			$theme->set_template( $config['theme-template'] );
+		}
+
+		Functions\when( 'wp_get_theme' )->justReturn( $theme );
+
+		$divi = new Divi( $options_api, $options );
+
+		$this->assertSame( $expected, array_values( $divi->disable_image_dimensions_height_percentage( $config['images'] ) ) );
+	}
+
+	public function providerTestData() {
+		return $this->getTestData( __DIR__, 'disableImageDimensionsHeightPercentage' )['test_data'];
+	}
+}
