be77c772d7cccc428a9a06b1606d8fcce45991e2
Vasilis Manthos <vmanthos@users.noreply.github.com>
2021-01-29 16:50:53 +0200

    Update inc/classes/subscriber/third-party/plugins/class-ngg-subscriber.php
    Co-authored-by: Rémy Perona <remy@wp-rocket.me>

--- inc/classes/subscriber/third-party/plugins/class-ngg-subscriber.php
+++ inc/classes/subscriber/third-party/plugins/class-ngg-subscriber.php
@@ -39,10 +39,6 @@ class NGG_Subscriber implements Subscriber_Interface {
 	 * @return bool
 	 */
 	public function deactivate_resource_manager() {
-		if( is_admin() ) {
-			return true;
-		}
-		
-		return false;
+		return is_admin();
 	}
 }
