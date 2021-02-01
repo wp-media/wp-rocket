82a352fe61f0cc59cdc61995ef457c731a528735
Milind More <milindmore22@gmail.com>
2021-02-01 18:49:01 +0530

    GH-3540 Adds AMP inline script for mobile redirection to exclusion list

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -684,6 +684,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'tagGroupsTabstaggroupscloudtabs',
 			'jrRelatedWidgets',
 			'UNCODE.initRow',
+			'ampURL',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
