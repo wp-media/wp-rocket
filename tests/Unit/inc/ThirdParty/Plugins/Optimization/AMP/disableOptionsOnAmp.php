10654d3d739cf6efce2e5ff604ec89be92fd3234
Caspar Green <cg@caspar.green>
2021-02-03 10:06:31 -0500

    Update tests and fixtures.

--- tests/Unit/inc/ThirdParty/Plugins/Optimization/AMP/disableOptionsOnAmp.php
+++ tests/Unit/inc/ThirdParty/Plugins/Optimization/AMP/disableOptionsOnAmp.php
@@ -52,9 +52,9 @@ class Test_DisableOptionsOnAmp extends TestCase {
 			);
 			$this->assertFalse( has_filter( 'do_rocket_lazyload', '__return_false' ) );
 			$this->assertFalse( has_filter( 'do_rocket_lazyload_iframes', '__return_false' ) );
-			$this->assertFalse( has_filter( 'do_rocket_lazyload_iframes', '__return_false' ) );
-			$this->assertFalse( has_filter( 'rocket_do_delay_js', '__return_false' ) );
-			$this->assertFalse( has_filter( 'rocket_do_preload_links', '__return_false' ) );
+			$this->assertFalse( has_filter( 'pre_get_rocket_option_async_css', '__return_false' ) );
+			$this->assertFalse( has_filter( 'pre_get_rocket_option_delay_js', '__return_false' ) );
+			$this->assertFalse( has_filter( 'pre_get_rocket_option_preload_links', '__return_false' ) );
 
 			$this->assertSame(
 				PHP_INT_MAX,
