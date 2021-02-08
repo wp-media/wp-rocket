db33f22bffa02935af01a0ee8c4691936d5e5efe
Vasilis Manthos <vmanthos@users.noreply.github.com>
2021-02-08 22:43:00 +0200

    Fixes #3470 Exclude application/ld+json scripts from DeferJS (PR #3553)

--- inc/Engine/Optimization/DeferJS/DeferJS.php
+++ inc/Engine/Optimization/DeferJS/DeferJS.php
@@ -148,6 +148,10 @@ class DeferJS {
 				continue;
 			}
 
+			if ( preg_match( '/(application\/ld\+json)/i', $inline_js[0] ) ) {
+				continue;
+			}
+
 			if ( empty( $inline_exclusions ) || preg_match( "/({$inline_exclusions})/msi", $inline_js['content'] ) ) {
 				continue;
 			}
