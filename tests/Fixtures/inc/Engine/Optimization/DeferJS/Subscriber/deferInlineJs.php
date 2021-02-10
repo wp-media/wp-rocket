b8f237caa52d2dfe4de305ea2c0d25c17e41696d
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-02-10 22:42:57 +0200

    add testcases for string, array and object filter returns

--- tests/Fixtures/inc/Engine/Optimization/DeferJS/Subscriber/deferInlineJs.php
+++ tests/Fixtures/inc/Engine/Optimization/DeferJS/Subscriber/deferInlineJs.php
@@ -143,4 +143,76 @@ return [
 		'html'     => $html,
 		'expected' => $expected,
 	],
+
+	'testShouldExcludeUsingStringFilter' => [
+		'config' => [
+			'rocket_defer_inline_exclusions_filter' => 'first_string|third_string',
+			'donotrocketoptimize' => false,
+			'post_meta'           => false,
+			'options'             => [
+				'defer_all_js'      => 1,
+				'exclude_defer_js'  => [],
+			],
+		],
+		'html'     => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">var second_string = jQuery('#second_selector');</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML
+		,
+		'expected' => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">window.addEventListener('DOMContentLoaded', function() {var second_string = jQuery('#second_selector');});</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML,
+	],
+
+	'testShouldExcludeUsingArrayFilter' => [
+		'config' => [
+			'rocket_defer_inline_exclusions_filter' => [
+				'first_string',
+				'third_string'
+			],
+			'donotrocketoptimize' => false,
+			'post_meta'           => false,
+			'options'             => [
+				'defer_all_js'      => 1,
+				'exclude_defer_js'  => [],
+			],
+		],
+		'html'     => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">var second_string = jQuery('#second_selector');</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML
+		,
+		'expected' => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">window.addEventListener('DOMContentLoaded', function() {var second_string = jQuery('#second_selector');});</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML,
+	],
+
+	'testShouldExcludeUsingObjectFilter' => [
+		'config' => [
+			'rocket_defer_inline_exclusions_filter' => new StdClass(),
+			'donotrocketoptimize' => false,
+			'post_meta'           => false,
+			'options'             => [
+				'defer_all_js'      => 1,
+				'exclude_defer_js'  => [],
+			],
+		],
+		'html'     => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">var second_string = jQuery('#second_selector');</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML
+		,
+		'expected' => <<<HTML
+	<script type="text/javascript">var first_string = jQuery('#first_selector');</script>
+	<script type="text/javascript">var second_string = jQuery('#second_selector');</script>
+	<script type="text/javascript">var third_string = jQuery('#third_selector');</script>
+HTML,
+	],
 ];
