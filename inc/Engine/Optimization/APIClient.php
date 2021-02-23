f43b16e793e861b1c1fc6a8ab5d8dca23311997a
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-02-23 19:40:12 +0200

    adjust phpcs

--- inc/Engine/Optimization/APIClient.php
+++ inc/Engine/Optimization/APIClient.php
@@ -2,25 +2,37 @@
 
 namespace WP_Rocket\Engine\Optimization;
 
+use WP_Rocket\Logger\Logger;
+
 class APIClient {
+	/**
+	 * RUCSS API Url.
+	 */
 	const API_URL = 'https://saas.wp-rocket.me/api/';
 
+	/**
+	 * Optimize page HTML.
+	 *
+	 * @param string $html Page HTML.
+	 * @param array  $options Array of options.
+	 *
+	 * @return mixed
+	 */
 	public function optimize( $html, array $options ) {
 		global $wp;
 
 		timer_start();
 
-		$url = home_url( add_query_arg( array(), $wp->request ) );
+		$url = home_url( add_query_arg( [], $wp->request ) );
 
 		$args = [
-			'body' => [
-				'html'    => $html,
-				'url'     => $url,
+			'body'    => [
+				'html'   => $html,
+				'url'    => $url,
 				'config' => $options,
 			],
 			'timeout' => 30,
 		];
-
 
 		$request = wp_remote_post(
 			self::API_URL,
@@ -28,17 +40,17 @@ class APIClient {
 		);
 
 		if ( is_wp_error( $request ) ) {
-			error_log( $request->get_error_message() );
+			Logger::error( 'RUCSS request failure', [ 'error' => $request->get_error_message() ] );
 			return $html;
 		}
 
 		if ( 200 !== wp_remote_retrieve_response_code( $request ) ) {
-			error_log( wp_remote_retrieve_response_code( $request ) );
-			
+			Logger::error( 'RUCSS response code is not valid', [ 'code' => wp_remote_retrieve_response_code( $request ) ] );
+
 			return $html;
 		}
 
-		error_log( 'Optimization time: ' . timer_stop() );
+		Logger::debug( 'RUCSS Optimization time', [ 'elapsed' => timer_stop() ] );
 
 		return json_decode( wp_remote_retrieve_body( $request ) );
 	}
--- inc/Engine/Optimization/Subscriber.php
+++ inc/Engine/Optimization/Subscriber.php
@@ -6,20 +6,49 @@ use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 class Subscriber implements Subscriber_Interface {
+	/**
+	 * API Client.
+	 *
+	 * @var APIclient
+	 */
 	private $api;
+
+	/**
+	 * Options API.
+	 *
+	 * @var Options_Data
+	 */
 	private $options;
 
+	/**
+	 * Subscriber constructor.
+	 *
+	 * @param APIclient    $api API Client.
+	 * @param Options_Data $options Options API.
+	 */
 	public function __construct( APIclient $api, Options_Data $options ) {
 		$this->api     = $api;
 		$this->options = $options;
 	}
 
+	/**
+	 * Subscriber events
+	 *
+	 * @return string[]
+	 */
 	public static function get_subscribed_events() {
 		return [
 			'rocket_buffer' => 'optimize',
 		];
 	}
 
+	/**
+	 * Optimize HTML
+	 *
+	 * @param string $html HTML of the page.
+	 *
+	 * @return mixed
+	 */
 	public function optimize( $html ) {
 		return $this->api->optimize(
 			$html,
