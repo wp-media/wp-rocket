8799e892da39780adf40c2b12deaa502296cf943
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-03-01 21:14:56 +0200

    add hooks script to the exclude list

--- inc/Engine/Optimization/DeferJS/DeferJS.php
+++ inc/Engine/Optimization/DeferJS/DeferJS.php
@@ -215,6 +215,7 @@ class DeferJS {
 			'/wp-includes/js/dist/i18n(.min)?.js',
 			'/wp-includes/js/dist/vendor/wp-polyfill(.min)?.js',
 			'/wp-includes/js/dist/url(.min)?.js',
+			'/wp-includes/js/dist/hooks(.min)?.js',
 		];
 
 		$exclude_defer_js = array_unique( array_merge( $exclude_defer_js, $this->options->get( 'exclude_defer_js', [] ) ) );
