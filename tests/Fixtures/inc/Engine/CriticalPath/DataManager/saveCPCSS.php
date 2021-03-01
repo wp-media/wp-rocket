ed9382d74c9f898be4620d12c9e435229cacbee5
Rémy Perona <remperona@gmail.com>
2021-03-01 17:13:29 -0500

    cleanup fixtures

--- tests/Fixtures/inc/Engine/CriticalPath/DataManager/saveCPCSS.php
+++ tests/Fixtures/inc/Engine/CriticalPath/DataManager/saveCPCSS.php
@@ -20,23 +20,21 @@ return [
 	],
 
 	'test_data' => [
-		// Should update an existing file.
-		[
+		'testShouldUpdateExistingCPCSSFile' => [
 			'url'        => 'http://www.example.com/?p=1',
 			'path'       => 'posts/post-1.css',
 			'cpcss_code' => 'body{color:red;}',
 			'is_mobile'  => false,
 			'expected'   => true,
 		],
-		// Should create non-existent files.
-		[
+		'testShouldCreateCPCSSFileForPost500' => [
 			'url'        => 'http://www.example.com/?p=500',
 			'path'       => 'posts/post-500.css',
 			'cpcss_code' => 'body{ color:red }',
 			'is_mobile'  => false,
 			'expected'   => true,
 		],
-		[
+		'testShouldCreateCPCSSFileForPostLoremIpsum' => [
 			'url'        => 'http://www.example.com/?p=1',
 			'path'       => 'posts/lorem-ipsum.css',
 			'cpcss_code' => 'body{ color:red; font-size: 2em } h1 { color: black }',
@@ -44,25 +42,21 @@ return [
 			'expected'   => true,
 		],
 
-		// Mobile CPCSS
-		// Should update an existing file.
-		[
+		'testShouldUpdateExistingCPCSSFileWhenMobile' => [
 			'url'        => 'http://www.example.com/?p=1',
 			'path'       => 'posts/post-1-mobile.css',
 			'cpcss_code' => 'body{color:red;}',
 			'is_mobile'  => true,
 			'expected'   => true,
 		],
-		// Mobile CPCSS
-		// Should create non-existent files.
-		[
+		'testShouldCrateCPCSSFileForPost500WhenMobile' => [
 			'url'        => 'http://www.example.com/?p=500',
 			'path'       => 'posts/post-500-mobile.css',
 			'cpcss_code' => 'body{ color:red }',
 			'is_mobile'  => true,
 			'expected'   => true,
 		],
-		[
+		'testShouldCrateCPCSSFileForPostLoremIpsumWhenMobile' => [
 			'url'        => 'http://www.example.com/?p=1',
 			'path'       => 'posts/lorem-ipsum-mobile.css',
 			'cpcss_code' => 'body{ color:red; font-size: 2em } h1 { color: black }',
