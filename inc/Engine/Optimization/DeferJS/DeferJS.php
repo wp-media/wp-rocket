528078e5b14f9e5c14e0496694daba2ca9ff637a
Ahmed Saed <eng.ahmeds3ed@gmail.com>
2021-03-01 22:22:59 +0200

    Fix #3625 Contact form 7 stop working if dependency scripts are deferred after latest update (PR #3629)
    * exclude files from being deferred
    * add hooks script to the exclude list

--- inc/Engine/Optimization/DeferJS/DeferJS.php
+++ inc/Engine/Optimization/DeferJS/DeferJS.php
@@ -210,6 +210,12 @@ class DeferJS {
 			'cdn.voxpow.com/static/libs/v1/(.*).js',
 			'cdn.voxpow.com/media/trackers/js/(.*).js',
 			'use.typekit.net',
+			'/wp-includes/js/dist/vendor/lodash(.min)?.js',
+			'/wp-includes/js/dist/api-fetch(.min)?.js',
+			'/wp-includes/js/dist/i18n(.min)?.js',
+			'/wp-includes/js/dist/vendor/wp-polyfill(.min)?.js',
+			'/wp-includes/js/dist/url(.min)?.js',
+			'/wp-includes/js/dist/hooks(.min)?.js',
 		];
 
 		$exclude_defer_js = array_unique( array_merge( $exclude_defer_js, $this->options->get( 'exclude_defer_js', [] ) ) );
