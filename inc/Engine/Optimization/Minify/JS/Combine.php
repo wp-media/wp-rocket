1dcd34c7cd31378fa92886612bcf3e743a4c136a
Scott Hartley <45110039+ScottTravisHartley@users.noreply.github.com>
2021-03-01 16:59:19 -0600

    Stop WooCommerce Product Vendors From Exploding Cache Size
    WooCommerce Vendors has an inline script such as the following
    <script type="text/javascript" id='wcpv-frontend-scripts-js-extra'>
    /* <![CDATA[ */
    var wcpv_registration_local = {"ajaxurl":"https:\/\/example.com\/wp-admin\/admin-ajax.php","ajaxRegistrationNonce":"ac536b6c83","success":"Your request has been submitted.  You will be contacted shortly."};
    /* ]]> */
    </script>
    
    This script gets merged into the main file and causes the cache size to explode this change excludes the inline JS. 
    
    Additional context found this on a client site using both the newest version of WooCommerce Product Vendors and WordPress Rocket

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -685,6 +685,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'jrRelatedWidgets',
 			'UNCODE.initRow',
 			'amp_mobile_redirect_disabled',
+			'wcpv_registration_local',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
