d0a02f883a95e986b1c031a88d4e9d4990d89b0c
Soponar Cristina <crystinutzaa@gmail.com>
2021-03-09 00:11:31 +0200

    Finished remove_used_css_from_html()

--- inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
@@ -35,7 +35,7 @@ class UsedCSS {
 	}
 
 	/**
-	 * TO DO - alter HTML and remove all CSS which was processed from HTML page.
+	 * Alter HTML and remove all CSS which was processed from HTML page.
 	 *
 	 * @param string $html            HTML content.
 	 * @param array  $unprocessed_css List with unprocesses CSS links or inline.
@@ -43,6 +43,28 @@ class UsedCSS {
 	 * @return string HTML content.
 	 */
 	public function remove_used_css_from_html( string $html, array $unprocessed_css ) : string {
+		$html_nocomments = $this->hide_comments( $html );
+
+		$link_styles          = $this->find( '<link\s+([^>]+[\s"\'])?href\s*=\s*[\'"]\s*?(?<url>[^\'"]+\.css(?:\?[^\'"]*)?)\s*?[\'"]([^>]+)?\/?>', $html_nocomments );
+		$inline_styles        = $this->find( '<style.*>(?<content>.*)<\/style>', $html_nocomments );
+		$unprocessed_links    = $this->unprocessed_flat_array( 'link', $unprocessed_css );
+		$unprocessed_styles   = $this->unprocessed_flat_array( 'inline', $unprocessed_css );
+
+		foreach ( $link_styles as $style ) {
+			$style_url = strtok( $style['url'], '?' );
+			if ( in_array( $style_url, $unprocessed_links ) ) {
+				continue;
+			}
+			$html = str_replace( $style[0], '', $html );
+		}
+
+		foreach ( $inline_styles as $style ) {
+			if ( in_array( $this->strip_line_breaks( $style['content'] ), $unprocessed_styles ) ) {
+				continue;
+			}
+			$html = str_replace( $style[0], '', $html );
+		}
+
 		return $html;
 	}
 
@@ -55,7 +77,12 @@ class UsedCSS {
 	 * @return string HTML content.
 	 */
 	public function add_used_css_to_html( string $html, string $used_css ) : string {
-		return preg_replace( '/<\/title>/i', '$0<style rucss="usedcss" type="text/css">' . $used_css . '</style>', $html, 1 );
+		return preg_replace(
+			'#</title>#iU',
+			'</title><style id="rucss-usedcss">' . wp_strip_all_tags( $used_css ) . '</style>',
+			$html,
+			1
+		);
 	}
 
 	/**
@@ -73,4 +100,67 @@ class UsedCSS {
 			]
 		);
 	}
+
+	/**
+	 * Hides unwanted blocks from the HTML to be parsed.
+	 *
+	 * @param string $html HTML content.
+	 *
+	 * @return string
+	 */
+	protected function hide_comments( string $html ) : string {
+		$html = preg_replace( '#<!--\s*noptimize\s*-->.*?<!--\s*/\s*noptimize\s*-->#is', '', $html );
+		$html = preg_replace( '/<!--(.*)-->/Uis', '', $html );
+
+		return $html;
+	}
+
+	/**
+	 * Finds nodes matching the pattern in the HTML.
+	 *
+	 * @param string $pattern Pattern to match.
+	 * @param string $html    HTML content.
+	 *
+	 * @return bool|array
+	 */
+	protected function find( string $pattern, string $html ) {
+		preg_match_all( '/' . $pattern . '/Umsi', $html, $matches, PREG_SET_ORDER );
+
+		if ( empty( $matches ) ) {
+			return false;
+		}
+
+		return $matches;
+	}
+
+	/**
+	 * Create dedicated array of unprocessed css.
+	 *
+	 * @param string $type            CSS type (link / inline).
+	 * @param array  $unprocessed_css Array with unprocessed CSS.
+	 *
+	 * @return array Array with type of unprocessed CSS.
+	 */
+	protected function unprocessed_flat_array( string $type, array $unprocessed_css ) : array {
+		$unprocessed_array = [];
+		foreach ( $unprocessed_css as $css ) {
+			if ( $type === $css['type'] ) {
+				$unprocessed_array[] = $this->strip_line_breaks( $css['content'] );
+			}
+		}
+		return $unprocessed_array;
+	}
+
+	/**
+     * Strip line breaks.
+     *
+     * @param string $value - Value to be processed.
+     *
+     * @return string
+     */
+    protected function strip_line_breaks( string $value ): string
+    {
+        $value = str_replace( ["\r", "\n", "\r\n", "\t"], '', $value );
+        return trim( $value );
+    }
 }
--- inc/Engine/Optimization/RUCSS/Database/Row/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Database/Row/UsedCSS.php
@@ -20,7 +20,7 @@ class UsedCSS extends Row {
 		$this->id             = (int) $this->id;
 		$this->url            = (string) $this->url;
 		$this->css            = (string) $this->css;
-		$this->unprocessedcss = json_decode( $this->unprocessedcss );
+		$this->unprocessedcss = json_decode( $this->unprocessedcss, true );
 		$this->last_update    = false === $this->last_update ? 0 : strtotime( $this->last_update );
 		$this->last_accessed  = false === $this->last_accessed ? 0 : strtotime( $this->last_accessed );
 	}
