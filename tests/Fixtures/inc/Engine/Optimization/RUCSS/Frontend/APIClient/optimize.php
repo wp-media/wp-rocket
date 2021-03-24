efb3c727f553dae231e89c71ef211b62ff1baa14
Caspar Green <cg@caspar.green>
2021-03-24 16:06:50 -0400

    Modify Frontend\APIClient fixture for new test.

--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Frontend/APIClient/optimize.php
+++ tests/Fixtures/inc/Engine/Optimization/RUCSS/Frontend/APIClient/optimize.php
@@ -2,29 +2,70 @@
 
 return [
 	'test_data' => [
-		'shouldReturnExpectedResponseDataWhenSuccess' => [
-			'html'     => 'some html',
-			'url'      => 'http://example.com/path/to/style.css',
-			'success'  => true,
-			'expected' => json_encode(
-				[
-					'code' => 200,
-					'message' => 'Success',
-					'contents' => [
-						'shakedCSS' => 'h1 { color: red; }',
-						'unProcessedCss' => [],
-					],
-				]
-			)
-		],
-		'shouldReturnErrorDataWhenFail' => [
-			'html' => 'some html',
-			'url' => 'http://example.com.path/to/my-script.js',
-			'success' => false,
+		'shouldReturnExpectedDataWhenSuccess' => [
+			'config' => [
+				'html' => 'some html',
+				'url' => 'http://example.com/path/to/style.css',
+				'options' => [1],
+			],
+			'mockResponse' => [
+				'headers'  => [
+					'date'                      => 'Wed, 24 Mar, 2021 14:26:14 GMT',
+					'content-type'              => 'application/json',
+					'x-powered-by'              => 'PHP/7.4.16',
+					'cache-control'             => 'no-cache, private',
+					'x-frame-options'           => 'SAMEORIGIN',
+					'x-xss-protection'          => '1; mode=block',
+					'x-content-type-options'    => 'nosniff',
+					'strict-transport-security' => 'max-age=15724800; includeSubDomains'
+				],
+				'body'     => json_encode(
+					[
+						'code' => 200,
+						'message' => 'OK',
+						'contents' => [
+							'shakedCSS' => 'h1{color:red;}',
+							'unProcessedCss' => [],
+						],
+					]
+				),
+				'response' => [
+					'code'    => 200,
+					'message' => 'OK',
+				],
+				'cookies'  => [],
+				'filename' => null,
+			],
 			'expected' => [
-				'code' => 400,
-				'message' => 'Unused CSS service is unavailable',
-			]
+				'code' => 200,
+				'message' => 'OK',
+				'css' => 'h1{color:red;}',
+				'unprocessed_css' => [],
+			],
 		],
-	],
+
+//			'html'     => 'some html',
+//			'url'      => 'http://example.com/path/to/style.css',
+//			'success'  => true,
+//			'expected' => json_encode(
+//				[
+//					'code' => 200,
+//					'message' => 'Success',
+//					'contents' => [
+//						'shakedCSS' => 'h1 { color: red; }',
+//						'unProcessedCss' => [],
+//					],
+//				]
+//			)
+		],
+//		'shouldReturnErrorDataWhenFail' => [
+//			'html' => 'some html',
+//			'url' => 'http://example.com.path/to/my-script.js',
+//			'success' => false,
+//			'expected' => [
+//				'code' => 400,
+//				'message' => 'Unused CSS service is unavailable',
+//			]
+//		],
+//	],
 ];
