9800c312fdc4aaf34d3459d138b236d8b5740141
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-03-24 12:57:55 +0200

    fix formatting issues

--- inc/functions/options.php
+++ inc/functions/options.php
@@ -462,8 +462,13 @@ function rocket_valid_key() {
 	$valid_details = 8 === strlen( get_rocket_option( 'consumer_key' ) ) && hash_equals( $rocket_secret_key, hash( 'crc32', get_rocket_option( 'consumer_email' ) ) );
 
 	if ( ! $valid_details ) {
-		set_transient( 'rocket_check_key_errors', [ __( 'Not valid License details.' . '<br>' . sprintf( __( 'To resolve, please %1$scontact support%2$s.', 'rocket' ), '<a href="https://wp-rocket.me/support/" rel="noopener noreferrer" target=_"blank">', '</a>' ), 'rocket' ) ] );
-	}else{
+
+		set_transient(
+			'rocket_check_key_errors',
+			// Translators: %1$s = opening link tag, %2$s = closing link tag.
+			[ __( 'Not valid License details.', 'rocket' ) . ' <br>' . sprintf( __( 'To resolve, please %1$scontact support%2$s.', 'rocket' ), '<a href="https://wp-rocket.me/support/" rel="noopener noreferrer" target=_"blank">', '</a>' ) ]
+		);
+	}else {
 		delete_transient( 'rocket_check_key_errors' );
 	}
 
