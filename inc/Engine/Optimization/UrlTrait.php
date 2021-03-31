a87432c271d0c1aed736133ee4cc0176164dcb9c
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-03-31 18:06:58 +0200

    add check if query is inside parsed url

--- inc/Engine/Optimization/UrlTrait.php
+++ inc/Engine/Optimization/UrlTrait.php
@@ -100,7 +100,9 @@ trait UrlTrait {
 	private function normalize_url( $url ) {
 		$parsed_url = wp_parse_url( $url );
 
-		$url = str_replace( '?' . $parsed_url['query'], '', $url );
+		if ( ! empty( $parsed_url['query'] ) ) {
+			$url = str_replace( '?' . $parsed_url['query'], '', $url );
+		}
 
 		if ( empty( $parsed_url['host'] ) ) {
 			$relative_url        = ltrim( wp_make_link_relative( $url ), '/' );
