e2f4b10b176312469eb166c8e08d8c06a6950e47
pierre Sylvestre <pierre.s@onthegosystems.com>
2021-04-07 16:03:04 -0300

    Remove `wcml_switch_currency` action which is not needed anymore
    https://github.com/wp-media/wp-rocket/pull/3483/files#r608905155

--- inc/3rd-party/plugins/ecommerce/woocommerce-multilingual.php
+++ inc/3rd-party/plugins/ecommerce/woocommerce-multilingual.php
@@ -4,8 +4,6 @@ defined( 'ABSPATH' ) || exit;
 
 // Compatibility with the currency switcher in WooCommerce Multilingual plugin.
 if ( defined( 'WCML_VERSION' ) ) :
-	add_action( 'wcml_switch_currency', 'rocket_clean_domain' );
-
 	/**
 	 * Use Cookie instead of WCSession
 	 *
