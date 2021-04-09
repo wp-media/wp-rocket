5317e36ac0990203d00668aa76045b8323cb2b8e
Ahmed Saed <eng.ahmeds3ed@gmail.com>
2021-04-09 14:46:56 +0200

    When CPCSS + RUCSS are enabled, load the Used CSS async in a file (PR #3769)
    * with cpcss enabled add used_css to filesystem and load it as link stylesheet not inline css
    * revert the API url
    * refactor the controller and subscriber
    * adjust save_used_css_in_filesystem call

--- inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace WP_Rocket\Engine\Optimization\RUCSS\Controller;
 
+use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Engine\Cache\Purge;
 use WP_Rocket\Engine\Optimization\RegexTrait;
 use WP_Rocket\Engine\Optimization\RUCSS\Database\Row\UsedCSS as UsedCSS_Row;
 use WP_Rocket\Engine\Optimization\RUCSS\Database\Queries\UsedCSS as UsedCSS_Query;
+use WP_Rocket\Engine\Optimization\RUCSS\Frontend\APIClient;
+use WP_Rocket\Logger\Logger;
 
 class UsedCSS {
 	use RegexTrait;
@@ -26,14 +29,133 @@ class UsedCSS {
 	private $purge;
 
 	/**
-	 * Instantiate the class
+	 * Plugin options instance.
 	 *
-	 * @param UsedCSS_Query $used_css_query UsedCSS Query instance.
-	 * @param Purge         $purge          Purge instance.
+	 * @var Options_Data
 	 */
-	public function __construct( UsedCSS_Query $used_css_query, Purge $purge ) {
+	protected $options;
+
+	/**
+	 * APIClient instance
+	 *
+	 * @var APIClient
+	 */
+	private $api;
+
+
+	/**
+	 * Instantiate the class.
+	 *
+	 * @param Options_Data  $options Options instance.
+	 * @param UsedCSS_Query $used_css_query Usedcss Query instance.
+	 * @param Purge         $purge Purge instance.
+	 * @param APIClient     $api Apiclient instance.
+	 */
+	public function __construct( Options_Data $options, UsedCSS_Query $used_css_query, Purge $purge, APIClient $api ) {
+		$this->options        = $options;
 		$this->used_css_query = $used_css_query;
 		$this->purge          = $purge;
+		$this->api            = $api;
+	}
+
+	/**
+	 * Determines if we treeshake the CSS.
+	 *
+	 * @return boolean
+	 */
+	public function is_allowed() : bool {
+		if ( rocket_get_constant( 'DONOTROCKETOPTIMIZE' ) ) {
+			return false;
+		}
+
+		if ( rocket_bypass() ) {
+			return false;
+		}
+
+		if ( ! (bool) $this->options->get( 'remove_unused_css', 0 ) ) {
+			return false;
+		}
+
+		// Bailout if user is logged in and cache for logged in customers is active.
+		if ( is_user_logged_in() && (bool) $this->options->get( 'cache_logged_user', 0 ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Apply TreeShaked CSS to the current HTML page.
+	 *
+	 * @param string $html  HTML content.
+	 *
+	 * @return string  HTML content.
+	 */
+	public function treeshake( string $html ) : string {
+		if ( ! $this->is_allowed() ) {
+			return $html;
+		}
+
+		global $wp;
+		$url       = untrailingslashit( home_url( add_query_arg( [], $wp->request ) ) );
+		$is_mobile = $this->is_mobile();
+		$used_css  = $this->get_used_css( $url, $is_mobile );
+
+		if ( empty( $used_css ) || ( $used_css->retries < 3 ) ) {
+			$config = [
+				'treeshake'      => 1,
+				'wpr_email'      => $this->options->get( 'consumer_email', '' ),
+				'wpr_key'        => $this->options->get( 'consumer_key', '' ),
+				'rucss_safelist' => $this->options->get( 'remove_unused_css_safelist', [] ),
+			];
+
+			$treeshaked_result = $this->api->optimize( $html, $url, $config );
+
+			if ( 200 !== $treeshaked_result['code'] ) {
+				Logger::error(
+					'Error when contacting the RUCSS API.',
+					[
+						'rucss error',
+						'url'     => $url,
+						'code'    => $treeshaked_result['code'],
+						'message' => $treeshaked_result['message'],
+					]
+				);
+				return $html;
+			}
+
+			$retries = 0;
+			if ( isset( $used_css->retries ) ) {
+				$retries = $used_css->retries;
+			}
+
+			if ( ! empty( $treeshaked_result['unprocessed_css'] ) ) {
+				$this->schedule_rucss_retry();
+			}
+
+			$data = [
+				'url'            => $url,
+				'css'            => $treeshaked_result['css'],
+				'unprocessedcss' => wp_json_encode( $treeshaked_result['unprocessed_css'] ),
+				'retries'        => empty( $treeshaked_result['unprocessed_css'] ) ? 3 : $retries + 1,
+				'is_mobile'      => $is_mobile,
+				'modified'       => current_time( 'mysql', true ),
+			];
+
+			$used_css = $this->save_or_update_used_css( $data );
+
+			if ( ! $used_css ) {
+				return $html;
+			}
+		}
+
+		$html = $this->remove_used_css_from_html( $html, $used_css->unprocessedcss );
+
+		$html = $this->add_used_css_to_html( $html, $used_css );
+
+		$this->update_last_accessed( (int) $used_css->id );
+
+		return $html;
 	}
 
 	/**
@@ -65,6 +187,10 @@ class UsedCSS {
 	 * @return void
 	 */
 	public function retries_pages_with_unprocessed_css() {
+		if ( ! (bool) $this->options->get( 'remove_unused_css', 0 ) ) {
+			return;
+		}
+
 		$used_css_list = $this->get_used_css_with_unprocessed_css();
 
 		foreach ( $used_css_list as $used_css_item ) {
@@ -118,10 +244,20 @@ class UsedCSS {
 		$data['css'] = preg_replace( '/content\s*:\s*\'\\\\e/i', 'shaker-parser:\'dashe', $data['css'] );
 
 		if ( empty( $used_css ) ) {
-			return $this->insert_used_css( $data );
+			$inserted = $this->insert_used_css( $data );
+
+			// Save used_css into filesystem.
+			$this->save_used_css_in_filesystem( $inserted );
+
+			return $inserted;
 		}
 
-		return $this->update_used_css( (int) $used_css->id, $data );
+		$updated = $this->update_used_css( (int) $used_css->id, $data );
+
+		// Save used_css into filesystem.
+		$this->save_used_css_in_filesystem( $updated );
+
+		return $updated;
 	}
 
 	/**
@@ -225,15 +361,15 @@ class UsedCSS {
 	/**
 	 * Alter HTML string and add the used CSS style in <head> tag,
 	 *
-	 * @param string $html     HTML content.
-	 * @param string $used_css Used CSS content.
+	 * @param string      $html HTML content.
+	 * @param UsedCSS_Row $used_css Used CSS DB row.
 	 *
 	 * @return string HTML content.
 	 */
-	public function add_used_css_to_html( string $html, string $used_css ) : string {
+	public function add_used_css_to_html( string $html, UsedCSS_Row $used_css ) : string {
 		return preg_replace(
 			'#</title>#iU',
-			'</title><style id="wpr-usedcss">' . wp_strip_all_tags( $used_css ) . '</style>',
+			'</title>' . $this->get_used_css_markup( $used_css ),
 			$html,
 			1
 		);
@@ -297,5 +433,108 @@ class UsedCSS {
 	protected function strip_line_breaks( string $value ) : string {
 		$value = str_replace( [ "\r", "\n", "\r\n", "\t" ], '', $value );
 		return trim( $value );
+	}
+
+	/**
+	 * Save Used CSS into filesystem in case CPCSS is enabled only.
+	 *
+	 * @param UsedCSS_Row $used_css Used CSS contents.
+	 *
+	 * @return bool
+	 */
+	private function save_used_css_in_filesystem( UsedCSS_Row $used_css ) : bool {
+
+		if ( ! $this->cpcss_enabled() ) {
+			return false;
+		}
+
+		$used_css_path = rocket_get_constant( 'WP_ROCKET_USED_CSS_PATH' );
+
+		if ( ! rocket_direct_filesystem()->is_dir( $used_css_path ) ) {
+			if ( rocket_mkdir_p( $used_css_path ) ) {
+				rocket_direct_filesystem()->touch( $used_css_path . 'index.html' );
+			}
+		}
+
+		$used_css_filepath = rocket_get_constant( 'WP_ROCKET_USED_CSS_PATH' ) . $this->get_used_css_filename( $used_css->url, (bool) $used_css->is_mobile );
+
+		return rocket_put_content( $used_css_filepath, $used_css->css );
+	}
+
+	/**
+	 * Get Used CSS filename.
+	 *
+	 * @param string $url Page Url.
+	 * @param bool   $is_mobile For mobile version or desktop.
+	 *
+	 * @return string
+	 */
+	private function get_used_css_filename( string $url, bool $is_mobile = false ) : string {
+		return md5( $url ) . ( $is_mobile ? '-mobile' : '' ) . '.css';
+	}
+
+	/**
+	 * Return Markup for used_css into the page.
+	 *
+	 * @param UsedCSS_Row $used_css Used CSS DB Row.
+	 *
+	 * @return string
+	 */
+	private function get_used_css_markup( UsedCSS_Row $used_css ) : string {
+
+		if ( ! $this->cpcss_enabled() ) {
+			return sprintf(
+				'<style id="wpr-usedcss">%s</style>',
+				wp_strip_all_tags( $used_css->css )
+			);
+		}
+
+		$used_css_filename = $this->get_used_css_filename( $used_css->url, (bool) $used_css->is_mobile );
+		$used_css_filepath = rocket_get_constant( 'WP_ROCKET_USED_CSS_PATH' ) . $used_css_filename;
+
+		if ( ! rocket_direct_filesystem()->exists( $used_css_filepath ) ) {
+			$this->save_used_css_in_filesystem( $used_css );
+		}
+
+		return sprintf(
+			'<link rel="stylesheet" id="wpr-usedcss-css" href="%s">', // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet
+			rocket_get_constant( 'WP_ROCKET_USED_CSS_URL' ) . $used_css_filename
+		);
+	}
+
+	/**
+	 * Determines if the page is mobile and separate cache for mobile files is enabled.
+	 *
+	 * @return boolean
+	 */
+	private function is_mobile() : bool {
+		return $this->options->get( 'cache_mobile', 0 ) &&
+			$this->options->get( 'do_caching_mobile_files', 0 ) &&
+			wp_is_mobile();
+	}
+
+	/**
+	 * Schedules RUCSS to retry pages with missing CSS files.
+	 * Retries happen after 30 minutes.
+	 *
+	 * @return void
+	 */
+	public function schedule_rucss_retry() {
+		$scheduled = wp_next_scheduled( 'rocket_rucss_retries_cron' );
+
+		if ( $scheduled ) {
+			return;
+		}
+
+		wp_schedule_single_event( time() + ( 0.5 * HOUR_IN_SECONDS ), 'rocket_rucss_retries_cron' );
+	}
+
+	/**
+	 * Is CPCSS option active or not?
+	 *
+	 * @return bool
+	 */
+	private function cpcss_enabled() {
+		return (bool) $this->options->get( 'async_css', 0 );
 	}
 }
--- inc/Engine/Optimization/RUCSS/Frontend/Subscriber.php
+++ inc/Engine/Optimization/RUCSS/Frontend/Subscriber.php
@@ -3,25 +3,10 @@ declare(strict_types=1);
 
 namespace WP_Rocket\Engine\Optimization\RUCSS\Frontend;
 
-use WP_Rocket\Logger\Logger;
-use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 use WP_Rocket\Engine\Optimization\RUCSS\Controller\UsedCSS;
 
 class Subscriber implements Subscriber_Interface {
-	/**
-	 * Plugin options instance.
-	 *
-	 * @var Options_Data
-	 */
-	protected $options;
-
-	/**
-	 * APIClient instance
-	 *
-	 * @var APIClient
-	 */
-	private $api;
 
 	/**
 	 * UsedCss instance
@@ -31,23 +16,12 @@ class Subscriber implements Subscriber_Interface {
 	private $used_css;
 
 	/**
-	 * Name of the cron.
-	 *
-	 * @var string
-	 */
-	const CRON_NAME = 'rocket_rucss_retries_cron';
-
-	/**
 	 * Instantiate the class
 	 *
-	 * @param Options_Data $options  Plugin options instance.
-	 * @param UsedCSS      $used_css UsedCSS instance.
-	 * @param APIClient    $api      APIClient instance.
+	 * @param UsedCSS $used_css UsedCSS instance.
 	 */
-	public function __construct( Options_Data $options, UsedCSS $used_css, APIClient $api ) {
-		$this->options  = $options;
+	public function __construct( UsedCSS $used_css ) {
 		$this->used_css = $used_css;
-		$this->api      = $api;
 	}
 
 	/**
@@ -57,8 +31,8 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events() : array {
 		return [
-			'rocket_buffer' => [ 'treeshake', 12 ],
-			self::CRON_NAME => 'rucss_retries',
+			'rocket_buffer'             => [ 'treeshake', 12 ],
+			'rocket_rucss_retries_cron' => 'rucss_retries',
 		];
 	}
 
@@ -70,85 +44,7 @@ class Subscriber implements Subscriber_Interface {
 	 * @return string  HTML content.
 	 */
 	public function treeshake( string $html ) : string {
-		if ( ! $this->is_allowed() ) {
-			return $html;
-		}
-
-		global $wp;
-		$url       = untrailingslashit( home_url( add_query_arg( [], $wp->request ) ) );
-		$is_mobile = $this->is_mobile();
-		$used_css  = $this->used_css->get_used_css( $url, $is_mobile );
-
-		if ( empty( $used_css ) || ( $used_css->retries < 3 ) ) {
-			$config = [
-				'treeshake'      => 1,
-				'wpr_email'      => $this->options->get( 'consumer_email', '' ),
-				'wpr_key'        => $this->options->get( 'consumer_key', '' ),
-				'rucss_safelist' => $this->options->get( 'remove_unused_css_safelist', [] ),
-			];
-
-			$treeshaked_result = $this->api->optimize( $html, $url, $config );
-
-			if ( 200 !== $treeshaked_result['code'] ) {
-				Logger::error(
-					'Error when contacting the RUCSS API.',
-					[
-						'rucss error',
-						'url'     => $url,
-						'code'    => $treeshaked_result['code'],
-						'message' => $treeshaked_result['message'],
-					]
-				);
-				return $html;
-			}
-
-			$retries = 0;
-			if ( isset( $used_css->retries ) ) {
-				$retries = $used_css->retries;
-			}
-
-			if ( ! empty( $treeshaked_result['unprocessed_css'] ) ) {
-				$this->schedule_rucss_retry();
-			}
-
-			$data = [
-				'url'            => $url,
-				'css'            => $treeshaked_result['css'],
-				'unprocessedcss' => wp_json_encode( $treeshaked_result['unprocessed_css'] ),
-				'retries'        => empty( $treeshaked_result['unprocessed_css'] ) ? 3 : $retries + 1,
-				'is_mobile'      => $is_mobile,
-				'modified'       => current_time( 'mysql', true ),
-			];
-
-			$used_css = $this->used_css->save_or_update_used_css( $data );
-
-			if ( ! $used_css ) {
-				return $html;
-			}
-		}
-
-		$html = $this->used_css->remove_used_css_from_html( $html, $used_css->unprocessedcss );
-		$html = $this->used_css->add_used_css_to_html( $html, $used_css->css );
-
-		$this->used_css->update_last_accessed( (int) $used_css->id );
-
-		return $html;
-	}
-
-	/**
-	 * Schedules RUCSS to retry pages with missing CSS files.
-	 * Retries happen after 30 minutes.
-	 *
-	 * @return void
-	 */
-	public function schedule_rucss_retry() {
-		$scheduled = wp_next_scheduled( self::CRON_NAME );
-
-		if ( $scheduled ) {
-			return;
-		}
-
-		wp_schedule_single_event( time() + ( 0.5 * HOUR_IN_SECONDS ), self::CRON_NAME );
+		return $this->used_css->treeshake( $html );
 	}
 
 	/**
@@ -157,47 +53,6 @@ class Subscriber implements Subscriber_Interface {
 	 * @return void
 	 */
 	public function rucss_retries() {
-		if ( ! (bool) $this->options->get( 'remove_unused_css', 0 ) ) {
-			return;
-		}
-
 		$this->used_css->retries_pages_with_unprocessed_css();
-	}
-
-	/**
-	 * Determines if we treeshake the CSS.
-	 *
-	 * @return boolean
-	 */
-	public function is_allowed() : bool {
-		if ( rocket_get_constant( 'DONOTROCKETOPTIMIZE' ) ) {
-			return false;
-		}
-
-		if ( rocket_bypass() ) {
-			return false;
-		}
-
-		if ( ! (bool) $this->options->get( 'remove_unused_css', 0 ) ) {
-			return false;
-		}
-
-		// Bailout if user is logged in and cache for logged in customers is active.
-		if ( is_user_logged_in() && (bool) $this->options->get( 'cache_logged_user', 0 ) ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Determines if the page is mobile and separate cache for mobile files is enabled.
-	 *
-	 * @return boolean
-	 */
-	public function is_mobile() : bool {
-		return (bool) $this->options->get( 'cache_mobile', 0 ) &&
-				(bool) $this->options->get( 'do_caching_mobile_files', 0 ) &&
-				wp_is_mobile();
 	}
 }
