da390154e518210f7d022355a53bdec40b5c8401
Rémy Perona <remperona@gmail.com>
2021-04-13 16:38:33 -0400

    update pattern for unexpected spacing

--- inc/classes/Buffer/class-tests.php
+++ inc/classes/Buffer/class-tests.php
@@ -804,7 +804,7 @@ class Tests {
 	 * @return bool
 	 */
 	public function is_html( $buffer ) {
-		return (bool) preg_match( '/<\/html>/i', $buffer );
+		return (bool) preg_match( '/<\s*\/\s*html\s*>/i', $buffer );
 	}
 
 	/** ----------------------------------------------------------------------------------------- */
