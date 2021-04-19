e324e3e1f7ad840aff10ce47ef65da4070e2736b
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-04-19 19:38:17 +0200

    make saas url dynamic in tests

--- tests/Unit/inc/Engine/Optimization/RUCSS/Frontend/APIClient/optimize.php
+++ tests/Unit/inc/Engine/Optimization/RUCSS/Frontend/APIClient/optimize.php
@@ -41,7 +41,7 @@ class Test_Optimize extends TestCase {
 		Functions\expect( 'wp_remote_post' )
 			->once()
 			->with(
-				'https://central-saas.wp-rocket.me:30443/api',
+				$apiClient::API_URL . 'api',
 				$args
 			)
 			->andReturn( $mockResponse );
