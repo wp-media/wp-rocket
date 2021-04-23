c05210be024048cedc32678c961816755b61a54e
Vasilis Manthos <vmanthos@users.noreply.github.com>
2021-04-23 08:57:03 +0300

    Exclude inline JS: AJAX Search Pro
    Related ticket: https://secure.helpscout.net/conversation/1482489270/255755?folderId=2135277

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -696,6 +696,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'reload_attached_coupons',
 			'var ftpp',
 			'forminatorFront',
+			'var ASP',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
