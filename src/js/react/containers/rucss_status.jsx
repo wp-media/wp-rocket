7c3386517fcf5748f475fda77a9d0a70ad0af913
Soponar Cristina <crystinutzaa@gmail.com>
2021-05-03 01:41:53 +0300

    Add API Fetch and update dynamically the Progress Bar

--- src/js/react/containers/rucss_status.jsx
+++ src/js/react/containers/rucss_status.jsx
@@ -9,34 +9,45 @@ export default class RUCSSStatus extends Component {
 
 		// Set the default states
 		this.state = {
-			progress: 0,
+			progress:      0,
+			data:         {},
+			errorMessage: '',
 		};
-// console.log(this.props.wpObject);
-		// this.fetchWP = new fetchWP({
-		//   restURL: this.props.wpObject.api_url,
-		//   restNonce: this.props.wpObject.api_nonce,
-		// });
+	}
 
-		// wp.apiFetch( { path: this.props.wpObject.api_url } ).then( function( posts ){
-		//  console.log( 'Title of the first item is: ' + posts[0].title.rendered );
-		// } );
+	getStatus() {
+		wp.apiFetch( { url: this.props.wpObject.api_url } ).then(
+			data => this.setState({ data })
+		);
+	}
 
+	computeProgress() {
+		if (this.state.data.code == 'rest_forbidden' || this.state.data.success == false) {
+			clearInterval(this.timeout);
+			this.setState(
+				{
+					errorMessage: this.state.data.message,
+				}
+			);
+			return;
+		}
+
+		let progress = this.state.progress + 10;
+		this.setState(
+			{
+				progress: progress,
+			}
+		);
 	}
 
     componentDidMount() {
-		let progress = 0;
 		this.timeout = setInterval(() => {
-			this.setState(
-				{
-					progress: progress.toFixed(0),
-				},
-				() => {
-					progress = progress + 1;
-					if (progress > 100) {
-						clearInterval(this.timeout);
-					}
-				},
-			);
+			this.getStatus();
+			this.computeProgress();
+
+			if ( this.state.progress > 100 ) {
+				clearInterval(this.timeout);
+			}
 		}, 1000);
 	}
 
@@ -45,6 +56,12 @@ export default class RUCSSStatus extends Component {
 			<div className="rucss-status">
 				<div className="rucss-progress-bar">
 					<ProgressBar value={this.state.progress} max={100}/>
+				</div>
+				<div className="rucss-progress-error">
+					{this.state.errorMessage}
+				</div>
+				<div className="rucss-progress-step1">
+
 				</div>
 			</div>
 		);
