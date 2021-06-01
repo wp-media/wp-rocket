79f00a2e6cfc8add3dcbaaf80cb4b3ef0f9f8f16
Rémy Perona <remy@wp-rocket.me>
2021-06-01 14:59:45 -0400

    Fixes #3945 Display warning when WP Meteor is enabled with WP Rocket delay JS (PR #3974)
    * add new add_plugins_incompatibility() method
    * add options class to settings class dependencies
    * updating existing tests
    * add add_plugins_incompatibility() test file
    * add unit & integration tests

--- tests/Unit/inc/Engine/Optimization/DelayJS/Admin/Settings/sanitizeOptions.php
+++ tests/Unit/inc/Engine/Optimization/DelayJS/Admin/Settings/sanitizeOptions.php
@@ -4,6 +4,7 @@ namespace WP_Rocket\Tests\Unit\inc\Engine\Optimization\DelayJS\Admin\Settings;
 
 use Brain\Monkey\Functions;
 use Mockery;
+use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Engine\Admin\Settings\Settings as AdminSettings;
 use WP_Rocket\Engine\Optimization\DelayJS\Admin\Settings;
 use WP_Rocket\Tests\Unit\TestCase;
@@ -21,7 +22,7 @@ class Test_SanitizeOptions extends TestCase {
 		Functions\when( 'rocket_sanitize_textarea_field' )->justReturn( $config['sanitized_input']['delay_js_exclusions'] );
 
 		$admin_settings = Mockery::mock( AdminSettings::class );
-		$settings       = new Settings();
+		$settings = new Settings( Mockery::mock( Options_Data::class) );
 
 		$admin_settings->shouldReceive( 'sanitize_checkbox' )
 			->atMost()
