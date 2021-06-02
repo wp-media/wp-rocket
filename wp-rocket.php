18b08b46a81c17ceac430235692b89ed8579fbc4
Rémy Perona <remperona@gmail.com>
2021-06-02 11:22:00 -0400

    update plugin version to 3.9.0.1

--- wp-rocket.php
+++ wp-rocket.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Rocket
  * Plugin URI: https://wp-rocket.me
  * Description: The best WordPress performance plugin.
- * Version: 3.9
+ * Version: 3.9.0.1
  * Requires at least: 5.3
  * Requires PHP: 7.0
  * Code Name: Naboo
@@ -20,7 +20,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Rocket defines.
-define( 'WP_ROCKET_VERSION',               '3.9' );
+define( 'WP_ROCKET_VERSION',               '3.9.0.1' );
 define( 'WP_ROCKET_WP_VERSION',            '5.3' );
 define( 'WP_ROCKET_WP_VERSION_TESTED',     '5.5.1' );
 define( 'WP_ROCKET_PHP_VERSION',           '7.0' );
