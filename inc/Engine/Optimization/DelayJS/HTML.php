a983e2055f928711e60859e8dc678a5c9b487758
Caspar Green <cg@caspar.green>
2021-06-08 13:29:22 -0400

    Add excl patterns for divi animation and wpforms

--- inc/Engine/Optimization/DelayJS/HTML.php
+++ inc/Engine/Optimization/DelayJS/HTML.php
@@ -39,6 +39,8 @@ class HTML {
 		'/ewww-image-optimizer-cloud/includes/lazysizes.min.js',
 		'document\.body\.classList\.remove\("no-js"\)',
 		'document\.documentElement\.className\.replace\( \'no-js\', \'js\' \)',
+		'et_animation_data',
+		'wpforms_settings',
 	];
 
 	/**
