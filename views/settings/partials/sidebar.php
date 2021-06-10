dab9e791b420f3afc283198fb57147e752ed12c2
Caspar Green <cg@caspar.green>
2021-06-10 11:00:20 -0400

    Revise url handling. Modify second text and link.

--- views/settings/partials/sidebar.php
+++ views/settings/partials/sidebar.php
@@ -17,19 +17,15 @@ defined( 'ABSPATH' ) || exit;
 </div>
 <div class="wpr-Sidebar-notice">
 	<p><?php esc_html_e( 'Learn about optimal WP Rocket settings for mobile.', 'rocket' ); ?></p>
-	<a href="
-	<?php
-	if ( false !== strpos( get_locale(), 'fr' ) ) {
-		echo esc_url( __( 'https://fr.docs.wp-rocket.me/article/1432-comment-ameliorer-score-mobile-pagespeed/?utm_source=wp_plugin&utm_medium=wp_rocket', 'rocket' ) );
-	} else {
-		echo esc_url( __( 'https://docs.wp-rocket.me/article/1402-how-to-improve-your-mobile-pagespeed-score/?utm_source=wp_plugin&utm_medium=wp_rocket', 'rocket' ) );
-	}
+	<?php $rocket_mobile_doc_url = false !== strpos( get_locale(), 'fr' )
+			? 'https://fr.docs.wp-rocket.me/article/1432-comment-ameliorer-score-mobile-pagespeed/?utm_source=wp_plugin&utm_medium=wp_rocket'
+			: 'https://docs.wp-rocket.me/article/1402-how-to-improve-your-mobile-pagespeed-score/?utm_source=wp_plugin&utm_medium=wp_rocket'
 	?>
-	" target="_blank" class="wpr-Sidebar-notice-link"><?php esc_html_e( 'Read our guide', 'rocket' ); ?></a>
-	</div>
+	<a href="<?php echo esc_url( $rocket_mobile_doc_url ); ?>" target="_blank" class="wpr-Sidebar-notice-link"><?php esc_html_e( 'Read our guide', 'rocket' ); ?></a>
+</div>
 <div class="wpr-Sidebar-notice">
-	<p><?php esc_html_e( 'Why Google PageSpeed grade should not matter', 'rocket' ); ?></p>
-	<a href="<?php echo esc_url( __( 'https://wp-rocket.me/blog/the-truth-about-google-pagespeed-insights/?utm_source=wp_plugin&utm_medium=wp_rocket', 'rocket' ) ); ?>" target="_blank" class="wpr-Sidebar-notice-link"><?php esc_html_e( 'Read more', 'rocket' ); ?></a>
+	<p><?php esc_html_e( 'Test and Improve Google Core Web Vitals for WordPress.', 'rocket' ); ?></p>
+	<a href="<?php echo esc_url( __( 'https://wp-rocket.me/google-core-web-vitals-wordpress/?utm_source=wp_plugin&utm_medium=wp_rocket', 'rocket' ) ); ?>" target="_blank" class="wpr-Sidebar-notice-link"><?php esc_html_e( 'Read more', 'rocket' ); ?></a>
 </div>
 <?php if ( ! get_rocket_option( 'cache_logged_user', 0 ) ) : ?>
 <div class="wpr-Sidebar-info">
