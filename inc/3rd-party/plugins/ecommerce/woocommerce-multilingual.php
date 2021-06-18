5c7654c4ca8a466243825b0500afa38ecd50df86
Caspar Green <cg@caspar.green>
2021-06-18 11:15:47 -0400

    Disable wcml_user_store_strategy filter

--- inc/3rd-party/plugins/ecommerce/woocommerce-multilingual.php
+++ inc/3rd-party/plugins/ecommerce/woocommerce-multilingual.php
@@ -3,6 +3,8 @@
 defined( 'ABSPATH' ) || exit;
 
 // Compatibility with the currency switcher in WooCommerce Multilingual plugin.
+// @see #3998 -- We are _temporarily_ disabling this filter (@since 3.9.0.3). To be re-enabled after a future WCML release
+// that contains a better fix for the duplicate generation of cookies on this filter.
 if ( defined( 'WCML_VERSION' ) ) :
 	/**
 	 * Use Cookie instead of WCSession
@@ -12,7 +14,8 @@ if ( defined( 'WCML_VERSION' ) ) :
 	function rocket_wcml_use_cookie_storage() {
 		return 'cookie';
 	}
-	add_filter( 'wcml_user_store_strategy', 'rocket_wcml_use_cookie_storage', 10, 2 );
+	// add_filter( 'wcml_user_store_strategy', 'rocket_wcml_use_cookie_storage', 10, 2 );
+
 
 	/**
 	 * Add dynamic cookies for WCML.
