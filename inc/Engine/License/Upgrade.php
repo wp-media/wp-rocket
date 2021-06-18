7d217ad2bb3af6e09875db74107c282693d9c0c7
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2021-06-18 22:12:19 +0200

    Fixes #3807 Only show upgrade banner for license older than 14 days (PR #4024)
    * show banner if licence bought more than 14 days and update tests
    * use function fro new user condition

--- inc/Engine/License/Upgrade.php
+++ inc/Engine/License/Upgrade.php
@@ -282,6 +282,10 @@ class Upgrade extends Abstract_Render {
 			return false;
 		}
 
+		if ( $this->is_new_user() ) {
+			return false;
+		}
+
 		return $this->pricing->is_promo_active();
 	}
 
@@ -294,6 +298,15 @@ class Upgrade extends Abstract_Render {
 		$expiration_delay = $this->user->get_license_expiration() - time();
 
 		return 30 * DAY_IN_SECONDS > $expiration_delay;
+	}
+
+	/**
+	 * Checks if the User license bought less than 14 days
+	 *
+	 * @return boolean
+	 */
+	private function is_new_user() {
+		return ( 14 * DAY_IN_SECONDS ) > time() - $this->user->get_creation_date();
 	}
 
 	/**
