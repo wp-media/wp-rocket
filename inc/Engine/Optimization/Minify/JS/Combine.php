da88b71b932e8dace91615e74cf8b0d888cd603f
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2021-07-05 08:06:52 +0300

    Add new inline JS exclusion to prevent the cache_dir_size issue
    Ticket: https://secure.helpscout.net/conversation/1554177688/274649/

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -707,6 +707,8 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'translation-revision-date',
 			'google_conversion_id',
 			'hbspt',
+			'var marker_locations_',
+			'var AdmMyAjax',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
