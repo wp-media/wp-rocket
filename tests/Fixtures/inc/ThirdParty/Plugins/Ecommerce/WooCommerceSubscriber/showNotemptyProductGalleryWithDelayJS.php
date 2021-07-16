78ac4a2320cdce26860c9e659cce970523348001
Ahmed Saed <eng.ahmeds3ed@gmail.com>
2021-07-16 17:24:26 +0200

    Auto-exclusion for WooCommerce Product Gallery when Delay JS is enabled (PR #4142)

---
+++ tests/Fixtures/inc/ThirdParty/Plugins/Ecommerce/WooCommerceSubscriber/showNotemptyProductGalleryWithDelayJS.php
@@ -0,0 +1,53 @@
+<?php
+return [
+	'test_data' => [
+
+		'shouldBailoutIfNotAllowed' => [
+			'input' => [
+				'is_allowed' => false,
+			],
+			'expected' => [
+				'excluded' => [],
+			],
+		],
+
+		'shouldBailoutIfNotInProductPage' => [
+			'input' => [
+				'is_allowed' => true,
+				'in_product_page' => false,
+			],
+			'expected' => [
+				'excluded' => [],
+			],
+		],
+
+		'shouldBailoutIfGalleryHasNoImages' => [
+			'input' => [
+				'is_allowed' => true,
+				'in_product_page' => true,
+				'has_images' => false,
+			],
+			'expected' => [
+				'excluded' => []
+			],
+		],
+
+		'shouldExcludeScriptsIfGalleryHasSomeImages' => [
+			'input' => [
+				'is_allowed' => true,
+				'in_product_page' => true,
+				'has_images' => true,
+			],
+			'expected' => [
+				'excluded' => [
+					'/jquery-?[0-9.]*(.min|.slim|.slim.min)?.js',
+					'/woocommerce/assets/js/zoom/jquery.zoom(.min)?.js',
+					'/woocommerce/assets/js/photoswipe/',
+					'/woocommerce/assets/js/flexslider/jquery.flexslider(.min)?.js',
+					'/woocommerce/assets/js/frontend/single-product(.min)?.js',
+				],
+			],
+		],
+
+	],
+];
