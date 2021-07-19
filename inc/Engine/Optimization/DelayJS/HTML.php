a3b5434a1e3b503d7478b65fafa468f459aae3dd
Rémy Perona <remperona@gmail.com>
2021-07-19 16:00:56 -0400

    fix PHPCs

--- inc/Engine/Optimization/DelayJS/HTML.php
+++ inc/Engine/Optimization/DelayJS/HTML.php
@@ -52,7 +52,7 @@ class HTML {
 		'var billing_additional_field', // Woo Autocomplete Nish.
 		'var gtm4wp',
 		'var dataLayer_content',
-		'ewwwWebPInit' // EWWW WebP rewrite inline script.
+		'ewwwWebPInit', // EWWW WebP rewrite inline script.
 	];
 
 	/**
