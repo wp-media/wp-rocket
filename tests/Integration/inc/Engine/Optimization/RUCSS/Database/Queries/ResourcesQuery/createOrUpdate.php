94f2ebb7fa9c56d635abe10ae3b24b50be1ff910
Caspar Green <cg@caspar.green>
2021-07-21 12:38:59 -0400

    Add test for hash match condition

--- tests/Integration/inc/Engine/Optimization/RUCSS/Database/Queries/ResourcesQuery/createOrUpdate.php
+++ tests/Integration/inc/Engine/Optimization/RUCSS/Database/Queries/ResourcesQuery/createOrUpdate.php
@@ -26,7 +26,7 @@ class Test_CreateOrUpdate extends TestCase{
 		self::uninstallAll();
 	}
 
-	public function testShouldCreateNewItemIfNotExists(){
+	public function testShouldCreateNewItemIfUrlAndHashUnmatched() {
 		$container             = apply_filters( 'rocket_container', null );
 		$rucss_resources_query = $container->get( 'rucss_resources_query' );
 
@@ -38,13 +38,44 @@ class Test_CreateOrUpdate extends TestCase{
 		];
 
 		$this->assertFalse( $rucss_resources_query->get_item_by( 'url', $item['url'] ) );
+		$this->assertFalse( $rucss_resources_query->get_item_by( 'hash', md5( $item['content'] ) ) );
 
 		$this->assertGreaterThan( 0, $rucss_resources_query->create_or_update( $item ) );
 
 		$this->assertIsObject( $rucss_resources_query->get_item_by( 'url', $item['url'] ) );
 	}
 
-	public function testShouldUpdateItemIfExists(){
+	public function testShouldNotCreateOrUpdateItemIfHashMatchesResource() {
+		$container             = apply_filters( 'rocket_container', null );
+		$rucss_resources_query = $container->get( 'rucss_resources_query' );
+
+		$item1 = [
+			'url'           => 'https://www.example.org/style.css',
+			'type'          => 'css',
+			'content'       => '.example{color:red;}',
+			'media'         => 'all',
+			'hash'          => md5('.example{color:red;}'), // original item is stored with a hash
+			'last_accessed' => $stored_last_access = current_time( 'mysql', true )
+		];
+
+		$item2 = [
+			'url'           => 'https://www.example.org/style.css?ver=12134534537abx',
+			'type'          => 'css',
+			'content'       => '.example{color:red;}',
+			'media'         => 'all',
+		];
+
+		$original_id = $rucss_resources_query->add_item( $item1 );
+
+		$this->assertFalse( $rucss_resources_query->get_item_by( 'url', $item2['url'] ) );
+		$this->assertIsObject( $rucss_resources_query->get_item_by( 'hash', md5( $item2['content'] ) ) );
+
+		// Assert that we have not changed the original item, except to update the last_accessed time.
+		$this->assertFalse( $rucss_resources_query->create_or_update( $item2 ) );
+		$this->assertGreaterThan( $stored_last_access, $rucss_resources_query->get_item_by( 'url', $item1['url'] ) );
+	}
+
+	public function testShouldUpdateItemIfExists() {
 		$container             = apply_filters( 'rocket_container', null );
 		$rucss_resources_query = $container->get( 'rucss_resources_query' );
 
@@ -70,6 +101,4 @@ class Test_CreateOrUpdate extends TestCase{
 		$this->assertIsObject( $new_row );
 		$this->assertSame( $new_row->content, $item['content'] );
 	}
-
-
 }
