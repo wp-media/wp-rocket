00fbb6022a44cbef6d5928e9eb5251f120f2a064
Rémy Perona <remperona@gmail.com>
2021-07-27 15:48:29 -0400

    Add Adthrive class

---
+++ inc/ThirdParty/Plugins/Ads/Adthrive.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\ThirdParty\Plugins\Ads;
+
+use WP_Rocket\Event_Management\Subscriber_Interface;
+
+class Ads implements Subscriber_Interface {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function get_subscribed_events() {
+	}
+}
