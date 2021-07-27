d6c9c65a66d3cfed7415672a889917e97620d64e
Rémy Perona <remperona@gmail.com>
2021-07-27 15:56:22 -0400

    fix classname

--- inc/ThirdParty/Plugins/Ads/Adthrive.php
+++ inc/ThirdParty/Plugins/Ads/Adthrive.php
@@ -5,7 +5,7 @@ namespace WP_Rocket\ThirdParty\Plugins\Ads;
 
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
-class Ads implements Subscriber_Interface {
+class Adthrive implements Subscriber_Interface {
 	/**
 	 * {@inheritdoc}
 	 */
