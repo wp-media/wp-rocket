820031c9b0fd4fd68d08b53c86f70700954bdd28
Rémy Perona <remperona@gmail.com>
2021-07-28 17:39:15 -0400

    fix PHPCS

--- inc/ThirdParty/ServiceProvider.php
+++ inc/ThirdParty/ServiceProvider.php
@@ -128,7 +128,7 @@ class ServiceProvider extends AbstractServiceProvider {
 			->addTag( 'common_subscriber' );
 		$this->getContainer()
 			->share( 'adthrive', 'WP_Rocket\ThirdParty\Plugins\Ads\Adthrive' )
-			->addArgument( $this->getContainer()->get( 'options_api') )
+			->addArgument( $this->getContainer()->get( 'options_api' ) )
 			->addArgument( $options )
 			->addTag( 'common_subscriber' );
 	}
