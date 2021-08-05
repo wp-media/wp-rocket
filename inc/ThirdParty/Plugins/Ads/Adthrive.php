645a3eed2dd1c26520959b38f90ba685c3ae2ae5
Rémy Perona <remperona@gmail.com>
2021-08-05 12:54:15 -0400

    update adthrive to add pattern on plugin update to 3.9.2

--- inc/ThirdParty/Plugins/Ads/Adthrive.php
+++ inc/ThirdParty/Plugins/Ads/Adthrive.php
@@ -3,51 +3,19 @@ declare(strict_types=1);
 
 namespace WP_Rocket\ThirdParty\Plugins\Ads;
 
-use WP_Rocket\Admin\Options;
-use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 class Adthrive implements Subscriber_Interface {
-	/**
-	 * Options API instance
-	 *
-	 * @var Options
-	 */
-	private $options_api;
-
-	/**
-	 * Options_Data instance
-	 *
-	 * @var Options_Data
-	 */
-	private $options;
-
-	/**
-	 * Instantiate the class
-	 *
-	 * @param Options      $options_api Options API instance.
-	 * @param Options_Data $options Options_Data instance.
-	 */
-	public function __construct( Options $options_api, Options_Data $options ) {
-		$this->options_api = $options_api;
-		$this->options     = $options;
-	}
-
 	/**
 	 * Returns an array of events that this subscriber wants to listen to.
 	 *
 	 * @return array
 	 */
 	public static function get_subscribed_events(): array {
-		$events = [];
-
-		if ( is_plugin_active( 'adthrive-ads/adthrive-ads.php' ) ) {
-			$events['admin_init'] = 'add_delay_js_exclusion';
-		}
-
-		$events['activate_adthrive-ads/adthrive-ads.php'] = 'add_delay_js_exclusion';
-
-		return $events;
+		return [
+			'wp_rocket_upgrade'                      => [ 'add_delay_js_exclusion_on_plugin_update', 10, 2 ],
+			'activate_adthrive-ads/adthrive-ads.php' => 'add_delay_js_exclusion',
+		];
 	}
 
 	/**
@@ -58,11 +26,17 @@ class Adthrive implements Subscriber_Interface {
 	 * @return void
 	 */
 	public function add_delay_js_exclusion() {
-		if ( ! $this->options->get( 'delay_js', 0 ) ) {
+		$options = get_option( 'wp_rocket_settings', [] );
+
+		if (
+			! isset( $options['delay_js'] )
+			||
+			empty( $options['delay_js'] )
+		) {
 			return;
 		}
 
-		$exclusions = $this->options->get( 'delay_js_exclusions', [] );
+		$exclusions = $options['delay_js_exclusions'] ?? [];
 
 		if ( in_array( 'adthrive', $exclusions, true ) ) {
 			return;
@@ -70,7 +44,30 @@ class Adthrive implements Subscriber_Interface {
 
 		$exclusions[] = 'adthrive';
 
-		$this->options->set( 'delay_js_exclusions', $exclusions );
-		$this->options_api->set( 'settings', $this->options->get_options() );
+		$options['delay_js_exclusions'] = $exclusions;
+
+		update_option( 'wp_rocket_settings', $options );
+	}
+
+	/**
+	 * Adds adthrive to delay JS exclusion field on update to 3.9.2
+	 *
+	 * @since 3.9.2
+	 *
+	 * @param string $new_version Plugin new version.
+	 * @param string $old_version Plugin old version.
+	 *
+	 * @return void
+	 */
+	public function add_delay_js_exclusion_on_plugin_update( $new_version, $old_version ) {
+		if ( version_compare( $old_version, '3.9.2', '>' ) ) {
+			return;
+		}
+
+		if ( is_plugin_active( 'adthrive-ads/adthrive-ads.php' ) ) {
+			return;
+		}
+
+		$this->add_delay_js_exclusion();
 	}
 }
