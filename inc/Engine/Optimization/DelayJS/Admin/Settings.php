2d52a971d86f29c1ba5afb83308f5078aba171ec
Caspar Green <cg@caspar.green>
2021-08-19 11:42:38 -0400

    Re-remove deleted code

--- inc/Engine/Optimization/DelayJS/Admin/Settings.php
+++ inc/Engine/Optimization/DelayJS/Admin/Settings.php
@@ -15,34 +15,6 @@ class Settings {
 	private $options;
 
 	/**
-	 * Instantiate the class
-	 *
-	 * @param Options_Data $options Options Data instance.
-	 */
-	public function __construct( Options_Data $options ) {
-		$this->options = $options;
-	}
-
-	/**
-	 * Adds plugins incompatible with delay JS to the list
-	 *
-	 * @since 3.9.0.1
-	 *
-	 * @param string[] $plugins Array of recommended plugins to deactivate.
-	 *
-	 * @return array
-	 */
-	public function add_plugins_incompatibility( $plugins ): array {
-		if ( ! $this->options->get( 'delay_js', 0 ) ) {
-			return $plugins;
-		}
-
-		$plugins['wp-meteor'] = 'wp-meteor/wp-meteor.php';
-
-		return $plugins;
-	}
-
-	/**
 	 * Add the delay JS options to the WP Rocket options array
 	 *
 	 * @since 3.9 Removed delay_js_scripts key, added delay_js_exclusions.
