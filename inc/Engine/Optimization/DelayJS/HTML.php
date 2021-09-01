1f94150451bf49284f9a7b7f164cf184dbd5ab32
mostafa <mostafa.hisham.mahmoud@gmail.com>
2021-09-01 08:24:49 +0200

    fix delay js Cube Portfolio show images

--- inc/Engine/Optimization/DelayJS/HTML.php
+++ inc/Engine/Optimization/DelayJS/HTML.php
@@ -55,6 +55,7 @@ class HTML {
 		'/ewww-image-optimizer/includes/check-webp(\.min)?.js', // EWWW WebP check external script.
 		'ewww_webp_supported', // EWWW WebP inline scripts.
 		'/dist/js/browser-redirect/app.js', // WPML browser redirect script.
+		'initCubePortfolio', //Cube Portfolio show images.
 	];
 
 	/**
