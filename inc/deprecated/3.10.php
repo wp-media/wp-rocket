bc9ab3aa0a0e433f232bdea9e58c5bd8692111db
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2021-09-27 16:50:35 +0200

    fix class aliases in deprecated 3.10 (#4381)

--- inc/deprecated/3.10.php
+++ inc/deprecated/3.10.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) || exit;
 
 class_alias( '\WP_Rocket\deprecated\Engine\Media\Embeds\EmbedsSubscriber', '\WP_Rocket\Engine\Media\Embeds\EmbedsSubscriber' );
-class_alias( '\WP_Rocket\Admin\Database\Optimization', '\WP_Rocket\Engine\Admin\Database\Optimization' );
-class_alias( '\WP_Rocket\Admin\Database\Optimization_Process', '\WP_Rocket\Engine\Admin\Database\OptimizationProcess' );
-class_alias( '\WP_Rocket\ServiceProvider\Database', '\WP_Rocket\Engine\Admin\Database\ServiceProvider' );
-class_alias( '\WP_Rocket\Subscriber\Admin\Database\Optimization_Subscriber', '\WP_Rocket\Engine\Admin\Database\Subscriber' );
+class_alias( '\WP_Rocket\Engine\Admin\Database\Optimization','\WP_Rocket\Admin\Database\Optimization' );
+class_alias( '\WP_Rocket\Engine\Admin\Database\OptimizationProcess','\WP_Rocket\Admin\Database\Optimization_Process'  );
+class_alias( '\WP_Rocket\Engine\Admin\Database\ServiceProvider','\WP_Rocket\ServiceProvider\Database' );
+class_alias( '\WP_Rocket\Engine\Admin\Database\Subscriber','\WP_Rocket\Subscriber\Admin\Database\Optimization_Subscriber' );
