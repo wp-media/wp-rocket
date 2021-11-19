4240a8709d1c43d02364c1a1558187469d76f226
Caspar Green <cg@caspar.green>
2021-11-19 08:54:52 -0500

    Remove dead code from DelayJs Subscriber

--- inc/Engine/Optimization/DelayJS/Admin/Subscriber.php
+++ inc/Engine/Optimization/DelayJS/Admin/Subscriber.php
@@ -33,12 +33,7 @@ class Subscriber implements Subscriber_Interface {
 			'rocket_first_install_options'         => 'add_options',
 			'wp_rocket_upgrade'                    => [ 'set_option_on_update', 13, 2 ],
 			'rocket_input_sanitize'                => [ 'sanitize_options', 13, 2 ],
-			'pre_update_option_wp_rocket_settings' => [
-				[
-					[ 'maybe_disable_combine_js', 11, 2 ],
-//					[ 'add_notice_when_delayjs_and_autoptimize_aggregatejs', 10, 2 ],
-				],
-			],
+			'pre_update_option_wp_rocket_settings' => [ 'maybe_disable_combine_js', 11, 2 ],
 		];
 	}
 
@@ -93,41 +88,5 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public function maybe_disable_combine_js( $value, $old_value ): array {
 		return $this->settings->maybe_disable_combine_js( $value, $old_value );
-	}
-
-	/**
-	 * Add a compatibility notice to transient errors when Autoptimize Aggregate JS is 'on' and Delay JS is activated.
-	 *
-	 * @since 3.10.4
-	 *
-	 * @param array $value     New Settings values array.
-	 * @param array $old_value Old Settings values array.
-	 *
-	 * @return array Returns the new settings values array unchanged.
-	 */
-	public function add_notice_when_delayjs_and_autoptimize_aggregatejs( $value, $old_value ): array {
-		$autoptimize_aggregate_js_setting = get_option( 'autoptimize_js_aggregate' );
-
-		if ( 'on' !== $autoptimize_aggregate_js_setting ) {
-			return $value;
-		}
-
-		if ( ! ( 0 === (int) $old_value['delay_js'] && 1 === (int) $value['delay_js'] ) ) {
-			return $value;
-		}
-
-		add_settings_error(
-			'general',
-			'compatibility_notice',
-			'</strong>' .
-			__(
-				'We have detected that Autoptimize\'s JavaScript Aggregation feature is enabled. The Delay JavaScript Execution will not be applied to the file it creates. We suggest disabling it to take full advantage of Delay JavaScript Execution.',
-				'rocket'
-			) .
-			'<strong>',
-			'info'
-		);
-
-		return $value;
 	}
 }
