f3fb00078db1f481342a68e9288c7d7c1fe70a4f
Caspar Green <cg@caspar.green>
2021-11-30 10:21:29 -0500

    Ignore phpcs warning for supressing warning
    The supression is intentional here, as we are evaluating the preg_replace for a valid regex pattern.

--- inc/Engine/Optimization/DeferJS/DeferJS.php
+++ inc/Engine/Optimization/DeferJS/DeferJS.php
@@ -75,7 +75,7 @@ class DeferJS {
 
 		$exclude_defer_js = implode( '|', $this->get_excluded() );
 
-		if ( ! @preg_replace( '#(' . $exclude_defer_js . ')#i', '', 'dummy-string' ) ) {
+		if ( ! @preg_replace( '#(' . $exclude_defer_js . ')#i', '', 'dummy-string' ) ) { // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
 			return $html;
 		}
 
