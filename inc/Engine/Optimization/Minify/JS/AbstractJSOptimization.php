9744e9754f0f3784c293d8dda3c6ec0e946470ac
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2021-12-16 16:35:14 +0200

    Fixes #4364 exclude hcaptcha from JS minification (PR #4566)

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -249,6 +249,7 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'documentcloud.adobe.com/view-sdk/main.js',
 			'static.cleverpush.com',
 			'js.afterpay.com',
+			'hcaptcha.com/1/api.js',
 		];
 
 		$excluded_external = array_merge( $defaults, $this->options->get( 'exclude_js', [] ) );
