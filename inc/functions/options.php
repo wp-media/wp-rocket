17da2c89d1daeeed29552dcb69cd76ab24c40347
Alfonso Catron <alfonso.catron@gmail.com>
2021-12-29 00:25:26 -0300

    fix #4507 RUCSS - Used CSS Breaking Opengraph Meta for WhatsApp Link Preview
    Exclude WhatsApp user agent, by adding $ua[] = 'WhatsApp'; on line 408

--- inc/functions/options.php
+++ inc/functions/options.php
@@ -405,6 +405,7 @@ function get_rocket_cache_dynamic_cookies() { // phpcs:ignore WordPress.NamingCo
 function get_rocket_cache_reject_ua() { // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
 	$ua   = get_rocket_option( 'cache_reject_ua', [] );
 	$ua[] = 'facebookexternalhit';
+	$ua[] = 'WhatsApp';
 
 	/**
 	 * Filter the rejected User-Agent
