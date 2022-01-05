0bcab2541c5b8bfbb21b24e1de06d7b9bc6443b9
Caspar Green <cg@caspar.green>
2022-01-05 08:43:13 -0500

    Fix phpcs
    Adjust some spacing.
    Add array commas.
    Add @param to docblock.

--- inc/ThirdParty/Plugins/Ecommerce/XLWooCommerceSalesTriggersSubscriber.php
+++ inc/ThirdParty/Plugins/Ecommerce/XLWooCommerceSalesTriggersSubscriber.php
@@ -32,15 +32,19 @@ class XLWooCommerceSalesTriggersSubscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'rocket_delay_js_exclusions' => [ 'modify_delayjs_exclusions_on_product_pages', 10, 2 ]
+			'rocket_delay_js_exclusions' => [ 'modify_delayjs_exclusions_on_product_pages', 10, 2 ],
 		];
 	}
 
 	/**
 	 * Modify delay JS excluded scripts for compatibility.
+	 *
+	 * @param array $exclusions The initial Delay JS exclusion list.
+	 *
+	 * @return array The filtered Delay JS exclusion list.
 	 */
 	public function modify_delayjs_exclusions_on_product_pages( array $exclusions ) {
-		if ( empty( rocket_get_constant('WCST_VERSION' ) ) ) {
+		if ( empty( rocket_get_constant( 'WCST_VERSION' ) ) ) {
 			return $exclusions;
 		}
 
@@ -55,7 +59,7 @@ class XLWooCommerceSalesTriggersSubscriber implements Subscriber_Interface {
 					'xl-woocommerce-sales-triggers/assets/js/wcst_combined.min.js',
 					'/jquery-?[0-9.](.*)(.min|.slim|.slim.min)?.js',
 					'js-(before|after)',
-					'(?:/wp-content/|/wp-includes/)(.*)'
+					'(?:/wp-content/|/wp-includes/)(.*)',
 				]
 			)
 		);
