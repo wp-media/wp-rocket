cf346506d51fb416410bd30cbc1aeb00eccb3455
Caspar Green <cg@caspar.green>
2022-01-05 08:37:37 -0500

    Add integration test for new compat subscriber
    Add the integration test and fixture for a new subscriber to handle XLWoo Sales Triggers plugin compatibility.
    
    Given the plugin is active, we have 2 cases:
    1. When we are on a single product page, we add the plugin's script + jquery patterns to delayJS exclusions.
    2. When we are not on a single product page, we don't change any delayJS exclusions.

---
+++ tests/Fixtures/inc/ThirdParty/Plugins/Ecommerce/XLWooCommerceSalesTriggersSubscriber/modifyDelayJsExclusionsOnProductPages.php
@@ -0,0 +1,59 @@
+<?php
+
+declare( strict_types=1 );
+
+$original_html = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<script src="http://example.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+</head>
+<body>
+<script src="http://example.com/wp-content/plugins/xl-woocommerce-sales-triggers/assets/js/wcst_combined.min.js?ver=2.11.0" id="wcst_public_js-js"></script>
+</body>
+</html>
+HTML;
+
+$delay_js_html = <<<DELAYJSHTML
+<!doctype html>
+<html lang="en">
+<head>
+<script>if(navigator.userAgent.match(/MSIE|Internet Explorer/i)||navigator.userAgent.match(/Trident\/7\..*?rv:11/i)){var href=document.location.href;if(!href.match(/[?&]nowprocket/)){if(href.indexOf("?")==-1){if(href.indexOf("#")==-1){document.location.href=href+"?nowprocket=1"}else{document.location.href=href.replace("#","?nowprocket=1#")}}else{if(href.indexOf("#")==-1){document.location.href=href+"&nowprocket=1"}else{document.location.href=href.replace("#","&nowprocket=1#")}}}}</script>
+<script type="rocketlazyloadscript" src="http://example.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+</head>
+<body>
+<script type="rocketlazyloadscript" src="http://example.com/wp-content/plugins/xl-woocommerce-sales-triggers/assets/js/wcst_combined.min.js?ver=2.11.0" id="wcst_public_js-js"></script>
+</body>
+</html>
+DELAYJSHTML;
+
+$delay_with_exclusions_html = <<<DELAYEXCLUDEDHTML
+<!doctype html>
+<html lang="en">
+<head>
+<script>if(navigator.userAgent.match(/MSIE|Internet Explorer/i)||navigator.userAgent.match(/Trident\/7\..*?rv:11/i)){var href=document.location.href;if(!href.match(/[?&]nowprocket/)){if(href.indexOf("?")==-1){if(href.indexOf("#")==-1){document.location.href=href+"?nowprocket=1"}else{document.location.href=href.replace("#","?nowprocket=1#")}}else{if(href.indexOf("#")==-1){document.location.href=href+"&nowprocket=1"}else{document.location.href=href.replace("#","&nowprocket=1#")}}}}</script>
+<script src="http://example.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+</head>
+<body>
+<script src="http://example.com/wp-content/plugins/xl-woocommerce-sales-triggers/assets/js/wcst_combined.min.js?ver=2.11.0" id="wcst_public_js-js"></script>
+</body>
+</html>
+DELAYEXCLUDEDHTML;
+
+
+return [
+	'test_data' => [
+
+		'shouldNotExcludeFromDelayJsWhenNotIsProduct' => [
+			'is_product' => false,
+			'initial_html'        => $original_html,
+			'expected_html'       => $delay_js_html
+		],
+
+		'shouldExcludeFromDelayJsWhenIsProduct' => [
+			'is_product' => true,
+			'initial_html'        => $original_html,
+			'expected_html'       => $delay_with_exclusions_html
+		]
+	]
+];
---
+++ tests/Integration/inc/ThirdParty/Plugins/Ecommerce/XLWooCommerceSalesTriggersSubscriber/modifyDelayJsExclusionsOnProductPages.php
@@ -0,0 +1,58 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins\Ecommerce\XLWooCommerceSalesTriggerSubscriber;
+
+use WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins\Ecommerce\WooCommerceSubscriber\WooTrait;
+use WP_Rocket\Tests\Integration\TestCase;
+
+/**
+ * @covers  \WP_Rocket\ThirdParty\Plugins\Ecommerce\XLWooCommerceSalesTriggersSubscriber::modify_delayjs_exclusions_on_product_pages
+ * @group   ThirdParty
+ * @group   WithWoo
+ */
+class Test_ModifyDelayJsExclusionsOnProductPages extends TestCase {
+
+	use WooTrait;
+
+	private $product;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->product = $this->create_product();
+		$this->unregisterAllCallbacksExcept(
+			'rocket_delay_js_exclusions',
+			'modify_delayjs_exclusions_on_product_pages'
+		);
+		add_filter( 'pre_get_rocket_option_delay_js', [ $this, 'return_true' ] );
+		$this->constants['WCST_VERSION'] = '2.3.4';
+	}
+
+	public function tearDown(): void {
+		parent::tearDown();
+
+		$this->restoreWpFilter( 'rocket_delay_js_exclusions' );
+		remove_filter( 'pre_get_rocket_option_delay_js', [ $this, 'return_true' ] );
+		unset( $this->constants['WCST_VERSION'] );
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function test_should_modify_delay_js_exclusions_when_expected(
+		bool $is_product,
+		string $original_html,
+		string $expected_html
+	) {
+		$this->go_to( $is_product ? $this->product->get_permalink() : home_url() );
+
+		$actual_html = apply_filters( 'rocket_buffer', $original_html );
+
+		$this->assertEquals(
+			$this->format_the_html( $expected_html ),
+			$this->format_the_html( $actual_html )
+		);
+	}
+}
