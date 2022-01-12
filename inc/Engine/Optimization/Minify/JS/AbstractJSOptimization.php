f922ecabf52454a1769f1ec6f5584cf56c257505
Adame Dahmani <hello@adame.io>
2022-01-12 15:52:10 +0100

    Fixes #4616 Exclusion for yanovis.Voucher.js from JS minification (PR #4635)

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -251,6 +251,7 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'js.afterpay.com',
 			'cdn.enable.co.il/licenses/enable-',
 			'hcaptcha.com/1/api.js',
+			'voucher.getavo.it/public/js/yanovis.Voucher.js',
 			'js-eu1.hsforms.net',
 		];
 
