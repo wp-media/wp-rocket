c1c0ea1f0e97956aea2e1ee58c692f382b10f12b
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2022-01-24 16:00:54 -0500

    use the new rucss url

--- inc/Engine/Optimization/RUCSS/AbstractAPIClient.php
+++ inc/Engine/Optimization/RUCSS/AbstractAPIClient.php
@@ -11,7 +11,7 @@ abstract class AbstractAPIClient {
 	/**
 	 * API URL.
 	 */
-	const API_URL = 'http://localhost:5000/';
+	const API_URL = 'https://bmrucss.wp-rocket.me/';
 
 	/**
 	 * Part of request Url after the main API_URL.
