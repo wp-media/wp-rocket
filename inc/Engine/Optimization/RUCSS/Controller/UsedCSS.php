e5f553a80dadce1f07d2e86e4d58454443956b70
Rémy Perona <remperona@gmail.com>
2022-02-03 16:49:40 -0500

    split attributes and content exclusions in separate arrays

--- inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
@@ -53,15 +53,23 @@ class UsedCSS {
 	private $api;
 
 	/**
-	 * Inline exclusions regexes not to removed from the page after treeshaking.
+	 * Inline CSS attributes exclusions patterns to be preserved on the page after treeshaking.
 	 *
 	 * @var string[]
 	 */
-	private $inline_exclusions = [
+	private $inline_atts_exclusions = [
 		'rocket-lazyload-inline-css',
 		'divi-style-parent-inline-inline-css',
 		'gsf-custom-css',
 		'extra-style-inline-inline-css',
+	];
+
+	/**
+	 * Inline CSS content exclusions patterns to be preserved on the page after treeshaking.
+	 *
+	 * @var string[]
+	 */
+	private $inline_content_exclusions = [
 		'#text-box-',
 		'#banner-',
 		'#slider-',
@@ -421,28 +429,40 @@ class UsedCSS {
 			$html = str_replace( $style[0], '', $html );
 		}
 
-		$inline_exclusions = (array) array_map(
+		$inline_atts_exclusions = (array) array_map(
 			function ( $item ) {
 				return preg_quote( $item, '/' );
 			},
 			/**
-			 * Filters the array of inline styles to preserve
+			 * Filters the array of inline CSS attributes patterns to preserve
 			 *
 			 * @since 3.11
 			 *
-			 * @param array $inline_exclusions Array of patterns used to match against the inline style.
+			 * @param array $inline_atts_exclusions Array of patterns used to match against the inline CSS attributes.
 			 */
-			apply_filters( 'rocket_rucss_inline_exclusions', $this->inline_exclusions )
+			apply_filters( 'rocket_rucss_inline_atts_exclusions', $this->inline_atts_exclusions )
 		);
 
-		$inline_exclusions_pattern = implode( '|', $inline_exclusions );
+		$inline_content_exclusions = (array) array_map(
+			function ( $item ) {
+				return preg_quote( $item, '/' );
+			},
+			/**
+			 * Filters the array of inline CSS content patterns to preserve
+			 *
+			 * @since 3.11
+			 *
+			 * @param array $inline_atts_exclusions Array of patterns used to match against the inline CSS content.
+			 */
+			apply_filters( 'rocket_rucss_inline_content_exclusions', $this->inline_content_exclusions )
+		);
 
 		foreach ( $inline_styles as $style ) {
-			if ( ! empty( $inline_exclusions_pattern ) && $this->find( $inline_exclusions_pattern, $style['atts'] ) ) {
+			if ( ! empty( $inline_atts_exclusions ) && $this->find( implode( '|', $inline_atts_exclusions ), $style['atts'] ) ) {
 				continue;
 			}
 
-			if ( ! empty( $inline_exclusions_pattern ) && $this->find( $inline_exclusions_pattern, $style['content'] ) ) {
+			if ( ! empty( $inline_content_exclusions ) && $this->find( implode( '|', $inline_content_exclusions ), $style['content'] ) ) {
 				continue;
 			}
 
