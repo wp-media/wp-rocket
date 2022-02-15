ed576ccc0f1d212c623bc38c5ac9b0fc00ea365b
Rémy Perona <remperona@gmail.com>
2022-02-15 15:58:20 -0500

    add tests for new method

---
+++ tests/Fixtures/inc/ThirdParty/Plugins/PageBuilder/Elementor/excludeJs.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+	'shouldReturnDefaultWhenCombineJSDisabled' => [
+		'config' => [
+			'combine_js' => false,
+			'user_cache' => false,
+			'logged_in'  => false,
+		],
+		'expected' => [],
+	],
+	'shouldReturnDefaultUserCacheDisabled' => [
+		'config' => [
+			'combine_js' => true,
+			'user_cache' => false,
+			'logged_in'  => false,
+		],
+		'expected' => [],
+	],
+	'shouldReturnDefaultUserNotLoggedIn' => [
+		'config' => [
+			'combine_js' => true,
+			'user_cache' => true,
+			'logged_in'  => false,
+		],
+		'expected' => [],
+	],
+	'shouldReturnUpdated' => [
+		'config' => [
+			'combine_js' => true,
+			'user_cache' => true,
+			'logged_in'  => true,
+		],
+		'expected' => [
+			'/wp-includes/js/dist/hooks.min.js',
+		],
+	],
+];
