ee348c70d3f774a53e7cdfd8d7282b242d8251b4
Vasilis Manthos <vmanthos@users.noreply.github.com>
2022-02-21 20:02:36 +0200

    Fixes #4743 Prevent saving locally Gravatar Hovercards script (#4744)

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -256,6 +256,7 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'js-eu1.hsforms.net',
 			'statcounter.com/counter/counter.js',
 			'snapppt.com',
+			'secure.gravatar.com/js/gprofiles.js',
 			'cdn.jsdelivr.net/npm/hockeystack',
 		];
 
