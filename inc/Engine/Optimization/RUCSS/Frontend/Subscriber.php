f7c0ef1f55ae7efa17b99e69b0f0aaf91c6633ad
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2022-02-24 08:00:52 -0500

    remove tests that need full refactoring

--- inc/Engine/Optimization/RUCSS/Frontend/Subscriber.php
+++ inc/Engine/Optimization/RUCSS/Frontend/Subscriber.php
@@ -32,7 +32,7 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events(): array {
 		return [
-			'rocket_buffer'                  => [ 'maybe_apply_rucss', 1000 ],
+			'rocket_buffer'                  => [ 'treeshake', 1000 ],
 			'rocket_disable_preload_fonts'   => 'maybe_disable_preload_fonts',
 			'rocket_rucss_pending_jobs_cron' => 'process_pending_jobs',
 			'rocket_rucss_job_check_status'  => 'check_job_status',
@@ -47,7 +47,7 @@ class Subscriber implements Subscriber_Interface {
 	 *
 	 * @return string  HTML content.
 	 */
-	public function maybe_apply_rucss( string $html ): string {
+	public function treeshake( string $html ): string {
 		return $this->used_css->treeshake( $html );
 	}
 
