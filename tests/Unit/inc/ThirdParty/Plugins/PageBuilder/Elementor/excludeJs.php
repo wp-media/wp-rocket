949728fd4fed97d2e36292e8ab45dba56a231445
Rémy Perona <remperona@gmail.com>
2022-02-28 14:03:38 -0500

    fix setup

--- tests/Unit/inc/ThirdParty/Plugins/PageBuilder/Elementor/excludeJs.php
+++ tests/Unit/inc/ThirdParty/Plugins/PageBuilder/Elementor/excludeJs.php
@@ -4,7 +4,6 @@ namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Plugins\PageBuilder\Elementor;
 use Mockery;
 use Brain\Monkey\Functions;
 use WP_Rocket\Tests\Unit\TestCase;
-use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Engine\Optimization\DelayJS\HTML;
 use WP_Rocket\ThirdParty\Plugins\PageBuilder\Elementor;
 
@@ -15,14 +14,12 @@ use WP_Rocket\ThirdParty\Plugins\PageBuilder\Elementor;
  * @group ThirdParty
  */
 class Test_ExcludeJs extends TestCase {
-	private $options;
 	private $elementor;
 
 	public function setUp(): void {
 		parent::setUp();
 
-		$this->options   = Mockery::mock( Options_Data::class );
-		$this->elementor = new Elementor( $this->options, null, Mockery::mock( HTML::class ));
+		$this->elementor = new Elementor( null, Mockery::mock( HTML::class ));
 	}
 
 	/**
