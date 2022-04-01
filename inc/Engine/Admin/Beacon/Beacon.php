8c3954f29a03d3006acf32fa0d47ea9e8d85e281
Michael Lee <michaelleemichaellee408@gmail.com>
2022-04-01 15:25:24 +0100

    phpcs fix

--- inc/Engine/Admin/Beacon/Beacon.php
+++ inc/Engine/Admin/Beacon/Beacon.php
@@ -687,7 +687,7 @@ class Beacon extends Abstract_Render implements Subscriber_Interface {
 					'url' => 'https://fr.docs.wp-rocket.me/article/1521-utiliser-wp-rocket-sur-un-intranet-prive-ou-hors-ligne?utm_source=wp_plugin&utm_medium=wp_rocket',
 				],
 			],
-			'fallback_css'             => [
+			'fallback_css'               => [
 				'en' => [
 					'id'  => '5ec5c4072c7d3a5ea54b7de7',
 					'url' => 'https://docs.wp-rocket.me/article/1321-critical-css-issues-fouc#use-fallback-critical-css',
