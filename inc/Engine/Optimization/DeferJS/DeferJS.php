bca7e2b3a47402468fefb18db091ba2895c30c8c
Vasilis Manthos <vmanthos@users.noreply.github.com>
2022-04-05 15:46:51 +0300

    Update DeferJS.php

--- inc/Engine/Optimization/DeferJS/DeferJS.php
+++ inc/Engine/Optimization/DeferJS/DeferJS.php
@@ -234,6 +234,7 @@ class DeferJS {
 			'yanovis.Voucher.js',
 			'/carousel-upsells-and-related-product-for-woocommerce/assets/js/glide.min.js',
 			'use.typekit.com',
+			'/api/scripts/lb_cs.js',
 		];
 
 		$exclude_defer_js = array_unique( array_merge( $exclude_defer_js, $this->options->get( 'exclude_defer_js', [] ) ) );
