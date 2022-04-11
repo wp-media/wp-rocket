2c245276a516e8a1cb870910f3da582389aef72c
Michael Lee <michaelleemichaellee408@gmail.com>
2022-04-11 20:04:19 +0100

    Check if one of minify options is enabled or bail out.

--- inc/Engine/Optimization/Minify/AdminSubscriber.php
+++ inc/Engine/Optimization/Minify/AdminSubscriber.php
@@ -3,6 +3,7 @@
 namespace WP_Rocket\Engine\Optimization\Minify;
 
 use WP_Rocket\Event_Management\Subscriber_Interface;
+use WP_Rocket\Admin\Options_Data;
 
 /**
  * Minify Admin subscriber
@@ -10,6 +11,22 @@ use WP_Rocket\Event_Management\Subscriber_Interface;
  * @since 3.5.4
  */
 class AdminSubscriber implements Subscriber_Interface {
+	/**
+	 * WP Rocket Options
+	 *
+	 * @var Options_Data
+	 */
+	private $options;
+
+	/**
+	 * Run on new instance
+	 *
+	 * @param Options_Data $options WP Rocket Options Instance.
+	 */
+	public function __construct( Options_Data $options ) {
+		$this->options = $options;
+	}
+
 	/**
 	 * Return an array of events that this subscriber wants to listen to.
 	 *
@@ -23,8 +40,14 @@ class AdminSubscriber implements Subscriber_Interface {
 
 	/**
 	 * Delete all minified cache file
+	 *
+	 * @return void
 	 */
 	public function clean_minify_all() {
+		// Bail out if minify_js or minify_css is not enabled.
+		if ( ! (bool) $this->options->get( 'minify_js' ) && ! (bool) $this->options->get( 'minify_css' ) ) {
+			return;
+		}
 		// Delete all minify cache files.
 		rocket_clean_minify();
 	}
