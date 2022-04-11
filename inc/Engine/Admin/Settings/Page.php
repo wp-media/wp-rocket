413d30f1f0b1e43390f69252891886e9a4590304
Rémy Perona <remperona@gmail.com>
2022-04-11 16:36:13 -0400

    remove sitemaps fields

--- inc/Engine/Admin/Settings/Page.php
+++ inc/Engine/Admin/Settings/Page.php
@@ -1166,47 +1166,6 @@ class Page {
 					],
 					'sanitize_callback' => 'sanitize_checkbox',
 				],
-			]
-		);
-
-		// Add this separately to be able to filter it easily.
-		$this->settings->add_settings_fields(
-			apply_filters(
-				'rocket_sitemap_preload_options',
-				[
-					'sitemap_preload' => [
-						'type'              => 'checkbox',
-						'label'             => __( 'Activate sitemap-based cache preloading', 'rocket' ),
-						'container_class'   => [
-							'wpr-isParent',
-							'wpr-field--children',
-						],
-						'parent'            => 'manual_preload',
-						'section'           => 'preload_section',
-						'page'              => 'preload',
-						'default'           => 0,
-						'sanitize_callback' => 'sanitize_checkbox',
-					],
-				]
-			)
-		);
-
-		$this->settings->add_settings_fields(
-			[
-				'sitemaps'      => [
-					'type'              => 'textarea',
-					'label'             => __( 'Sitemaps for preloading', 'rocket' ),
-					'container_class'   => [
-						'wpr-field--children',
-					],
-					'description'       => __( 'Specify XML sitemap(s) to be used for preloading', 'rocket' ),
-					'placeholder'       => 'http://example.com/sitemap.xml',
-					'parent'            => 'sitemap_preload',
-					'section'           => 'preload_section',
-					'page'              => 'preload',
-					'default'           => [],
-					'sanitize_callback' => 'sanitize_textarea',
-				],
 				'dns_prefetch'  => [
 					'type'              => 'textarea',
 					'label'             => __( 'URLs to prefetch', 'rocket' ),
@@ -2109,7 +2068,6 @@ class Page {
 					'minify_js_key',
 					'version',
 					'cloudflare_old_settings',
-					'sitemap_preload_url_crawl',
 					'cache_ssl',
 					'minify_google_fonts',
 					'emoji',
