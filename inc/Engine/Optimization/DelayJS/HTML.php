98ade0574e0ed023ca9b50c15cd2e1c37868efec
Vasilis Manthos <vmanthos@users.noreply.github.com>
2022-04-18 15:57:52 +0300

    Update the excluded file.
    https://github.com/wp-media/wp-rocket/issues/4769#issuecomment-1101387042

--- inc/Engine/Optimization/DelayJS/HTML.php
+++ inc/Engine/Optimization/DelayJS/HTML.php
@@ -70,7 +70,7 @@ class HTML {
 		'statcounter.com/counter/counter.js', // StatsCounter.
 		'var sc_project', // Statscounter.
 		'/jetpack/jetpack_vendor/automattic/jetpack-lazy-images/(.*)', // Jetpack plugin lazyload.
-		'/photonic/include/js/front-end/', // Photonic plugin.
+		'/photonic/include/js/front-end/nomodule/photonic-baguettebox.min.js', // Photonic plugin.
 	];
 
 	/**
