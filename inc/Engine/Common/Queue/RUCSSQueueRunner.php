1d35d6aca4e0a042bfc6b178f2f40de978a138c7
Ahmed Saed <eng.ahmeds3ed@gmail.com>
2022-04-21 13:24:48 +0200

    Clean our RUCSS action scheduler jobs periodically without affecting others (PR #4945)
    * add custom cleaner for our RUCSS jobs only with a dedicated filter with default lifespan of 1 hour
    * add group to the new filter
    * cast the filter return
    * make clean batch size to be 100 by default

--- inc/Engine/Common/Queue/RUCSSQueueRunner.php
+++ inc/Engine/Common/Queue/RUCSSQueueRunner.php
@@ -56,10 +56,24 @@ class RUCSSQueueRunner extends \ActionScheduler_Abstract_QueueRunner {
 	 *
 	 * @param \ActionScheduler_Store|null                    $store Store Instance.
 	 * @param \ActionScheduler_FatalErrorMonitor|null        $monitor Fatal Error monitor instance.
-	 * @param \ActionScheduler_QueueCleaner|null             $cleaner Cleaner instance.
+	 * @param Cleaner|null                                   $cleaner Cleaner instance.
 	 * @param \ActionScheduler_AsyncRequest_QueueRunner|null $async_request Async Request Queue Runner instance.
 	 */
-	public function __construct( \ActionScheduler_Store $store = null, \ActionScheduler_FatalErrorMonitor $monitor = null, \ActionScheduler_QueueCleaner $cleaner = null, \ActionScheduler_AsyncRequest_QueueRunner $async_request = null ) {
+	public function __construct( \ActionScheduler_Store $store = null, \ActionScheduler_FatalErrorMonitor $monitor = null, Cleaner $cleaner = null, \ActionScheduler_AsyncRequest_QueueRunner $async_request = null ) {
+		if ( is_null( $cleaner ) ) {
+			/**
+			 * Filters the clean batch size.
+			 *
+			 * @since 3.11.0.5
+			 *
+			 * @param int $batch_size Batch size.
+			 *
+			 * @return int
+			 */
+			$batch_size = (int) apply_filters( 'rocket_action_scheduler_clean_batch_size', 100, $this->group );
+			$cleaner    = new Cleaner( $store, $batch_size, $this->group );
+		}
+
 		parent::__construct( $store, $monitor, $cleaner );
 
 		if ( is_null( $async_request ) ) {
