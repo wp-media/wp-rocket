72f316f46b1d5b9ac2ec822bcb28e0e567f9d988
COQUARD Cyrille <cyrille@wp-media.me>
2022-04-26 11:48:01 +0200

    Fixed search

--- inc/Engine/Common/Queue/AbstractASQueue.php
+++ inc/Engine/Common/Queue/AbstractASQueue.php
@@ -48,9 +48,8 @@ abstract class AbstractASQueue implements QueueInterface {
 	 */
 	public function schedule_recurring( $timestamp, $interval_in_seconds, $hook, $args = [] ) {
 		if ( $this->is_scheduled( $hook, $args ) ) {
-			$running = $this->search( [ 'status' => ActionScheduler_Store::STATUS_RUNNING ] );
-			$pending = $this->search( [ 'status' => ActionScheduler_Store::STATUS_PENDING ] );
-			if ( count( $running ) > 1 || count($pending) > 1 ) {
+			$all = $this->search( [ 'status' => [ActionScheduler_Store::STATUS_RUNNING, ActionScheduler_Store::STATUS_PENDING ] ] );
+			if ( count( $all ) > 1 ) {
 				$this->cancel_all( $hook, $args );
 			}
 			return '';
