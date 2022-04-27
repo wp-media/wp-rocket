8234f40c05bc9ece509264c463a82ddaa435347e
COQUARD Cyrille <cyrille@wp-media.me>
2022-04-27 09:58:57 +0200

    Fixed code standards

--- inc/Engine/Common/Queue/AbstractASQueue.php
+++ inc/Engine/Common/Queue/AbstractASQueue.php
@@ -48,13 +48,16 @@ abstract class AbstractASQueue implements QueueInterface {
 	 */
 	public function schedule_recurring( $timestamp, $interval_in_seconds, $hook, $args = [] ) {
 		if ( $this->is_scheduled( $hook, $args ) ) {
-			$scheduled_actions = $this->search( [
-				'hook' => $hook,
-				'status' => [
-					ActionScheduler_Store::STATUS_RUNNING,
-					ActionScheduler_Store::STATUS_PENDING
-				]
-			], 'ids' );
+			$scheduled_actions = $this->search(
+				 [
+					 'hook'   => $hook,
+					 'status' => [
+						 ActionScheduler_Store::STATUS_RUNNING,
+						 ActionScheduler_Store::STATUS_PENDING,
+					 ],
+				 ],
+				 'ids'
+				);
 
 			if ( 1 === count( $scheduled_actions ) ) {
 				return '';
