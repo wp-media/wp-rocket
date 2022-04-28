5e688e68116768521683b68dbdfcc95d3f799366
COQUARD Cyrille <cyrille@wp-media.me>
2022-04-28 14:59:03 +0200

    Added tests for `truncate_used_css`

--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/truncateUsedCss.php
+++ tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/truncateUsedCss.php
@@ -4,14 +4,14 @@ $items = [
 	[
 		'url'            => 'http://example.org/home',
 		'css'            => 'h1{color:red;}',
-		'unprocessedcss' => wp_json_encode( [] ),
+		'unprocessedcss' => json_encode( [] ),
 		'retries'        => 3,
 		'is_mobile'      => false,
 	],
 	[
 		'url'            => 'http://example.org/home',
 		'css'            => 'h1{color:red;}',
-		'unprocessedcss' => wp_json_encode( [] ),
+		'unprocessedcss' => json_encode( [] ),
 		'retries'        => 3,
 		'is_mobile'      => true,
 	],
@@ -21,23 +21,14 @@ $items = [
 return [
 	'shouldNotTruncateUnusedCSSDueToMissingSettings' => [
 		'input' => [
-			'remove_unused_css' => false,
 			'items'             => $items,
-		],
-		'config' => [
-			'remove_unused_css' => true,
-			'is_disabled' => true,
-			'delete_used_css_row' => true,
-			'used_css_count' => 10,
+			'remove_unused_css' => false,
 		],
 	],
 	'shouldTruncateUnusedCSS' => [
 		'input' => [
 			'remove_unused_css' => true,
 			'items'             => $items,
-		],
-		'config' => [
-			'remove_unused_css' => true,
 			'is_disabled' => true,
 			'delete_used_css_row' => true,
 			'used_css_count' => 10,
@@ -47,11 +38,8 @@ return [
 		'input' => [
 			'remove_unused_css' => true,
 			'items'             => $items,
-		],
-		'config' => [
-			'remove_unused_css' => true,
 			'is_disabled' => false,
 			'used_css_count' => 10,
-		],
+		]
 	]
 ];
