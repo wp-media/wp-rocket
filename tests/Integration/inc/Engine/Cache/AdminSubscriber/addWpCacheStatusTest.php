72af7660eea5a5d2e6851bf38a061ac7306f3430
COQUARD Cyrille <18537428+CrochetFeve0251@users.noreply.github.com>
2022-05-06 15:22:44 +0200

    Change tear down function to fix type error with parent class
    Co-authored-by: Rémy Perona <remy@wp-rocket.me>

--- tests/Integration/inc/Engine/Cache/AdminSubscriber/addWpCacheStatusTest.php
+++ tests/Integration/inc/Engine/Cache/AdminSubscriber/addWpCacheStatusTest.php
@@ -13,10 +13,10 @@ use WP_Rocket\Tests\Integration\AdminTestCase;
 class Test_AddWpCacheStatusTest extends AdminTestCase {
 	protected $path_to_test_data = '/inc/Engine/Cache/WPCache/addWpCacheStatusTest.php';
 
-	public function tearDown(): void {
+	public function tear_down() {
 		remove_filter( 'rocket_set_wp_cache_constant', [ $this, 'return_false' ] );
 
-		parent::tearDown();
+		parent::tear_down();
 	}
 
 	/**
