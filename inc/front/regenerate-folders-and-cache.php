9648f54e1cc205343c22160117c6bfce2599b3b6
Dalias96 <84038002+Dalias96@users.noreply.github.com>
2022-05-06 11:27:07 +0200

    Codacy Static Code Analysis fixes

--- inc/front/regenerate-folders-and-cache.php
+++ inc/front/regenerate-folders-and-cache.php
@@ -9,27 +9,25 @@ defined( 'ABSPATH' ) || exit;
  * @since  3.11.2
  * @author Hubert Badura
  * @author Jarosław Krawczyk
- *
  */
-
-function wp_rocket_regenerate_cache_filesystem_and_config(){
-    // If cache folders are missing, then regenerate.
-    if (!file_exists(ABSPATH . 'wp-content/cache/wp-rocket/') || 
-        !file_exists(ABSPATH . 'wp-content/cache/min/') ||
-        !file_exists(ABSPATH . 'wp-content/cache/busting/') ||
-        !file_exists(ABSPATH . 'wp-content/cache/critical-css/')){
-        rocket_init_cache_dir();
-    }
-    
-    // If config folder is missing, then regenerate.
-    if (!file_exists(ABSPATH . 'wp-content/cache/wp-rocket-config/')){
-        rocket_generate_config_file();
-    }
-
-    // If advanced-cache.php is missing, then regenerate.
-    if (!file_exists(ABSPATH . 'wp-content/cache/advanced-cache.php')){
-        rocket_generate_advanced_cache_file();
-    }    
+function wp_rocket_regenerate_cache_filesystem_and_config() {
+	// If cache folders are missing, then regenerate.
+	if ( ! file_exists(ABSPATH . 'wp-content/cache/wp-rocket/' ) ||
+		!file_exists(ABSPATH . 'wp-content/cache/min/' ) ||
+		!file_exists(ABSPATH . 'wp-content/cache/busting/' ) ||
+		!file_exists(ABSPATH . 'wp-content/cache/critical-css/' ) ) {
+		rocket_init_cache_dir();
+	}
+	
+	// If config folder is missing, then regenerate.
+	if ( ! file_exists(ABSPATH . 'wp-content/cache/wp-rocket-config/' ) ) {
+		rocket_generate_config_file();
+	}
+	
+	// If advanced-cache.php is missing, then regenerate.
+	if ( ! file_exists(ABSPATH . 'wp-content/cache/advanced-cache.php' ) ) {
+		rocket_generate_advanced_cache_file();
+	}    
 }
 
 add_action( 'wp_loaded', 'wp_rocket_regenerate_cache_filesystem_and_config' );
