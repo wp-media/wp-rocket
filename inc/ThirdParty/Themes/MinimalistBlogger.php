0b1b9b60932ab5fbc2e09572cffb0e55df0bc684
COQUARD Cyrille <cyrille@wp-media.me>
2022-05-12 15:55:23 +0200

    Fixed code standards and CR changes

--- inc/ThirdParty/Themes/MinimalistBlogger.php
+++ inc/ThirdParty/Themes/MinimalistBlogger.php
@@ -17,17 +17,19 @@ class MinimalistBlogger extends ThirdpartyTheme {
 	 * @return array
 	 */
 	public static function get_subscribed_events() {
-		$events = [];
-		if ( self::is_theme() ) {
-			$events['rocket_delay_js_exclusions'] = 'exclude_jquery_from_delay_js';
+		if ( ! self::is_theme() ) {
+			return [];
 		}
-		return $events;
+
+		return [
+			'rocket_delay_js_exclusions' => 'exclude_jquery_from_delay_js',
+		];
 	}
 
 	/**
 	 * Excludes some MinimalistBlogger JS from delay JS execution
 	 *
-	 * @since 3.11.2
+	 * @since 3.11.3
 	 *
 	 * @param array $exclusions Array of exclusion patterns.
 	 *
--- inc/ThirdParty/Themes/ThirdpartyTheme.php
+++ inc/ThirdParty/Themes/ThirdpartyTheme.php
@@ -5,13 +5,6 @@ namespace WP_Rocket\ThirdParty\Themes;
 use WP_Theme;
 
 abstract class ThirdpartyTheme implements \WP_Rocket\Event_Management\Subscriber_Interface {
-
-	/**
-	 * Current theme.
-	 *
-	 * @var WP_Theme
-	 */
-	protected static $theme;
 	/**
 	 * Name from the theme.
 	 *
@@ -21,10 +14,11 @@ abstract class ThirdpartyTheme implements \WP_Rocket\Event_Management\Subscriber
 	/**
 	 * Check if the theme or one of its child theme is activated.
 	 *
+	 * @param WP_Theme $theme current theme.
 	 * @return bool
 	 */
-	protected static function is_theme() {
-		$theme = self::$theme instanceof WP_Theme ? self::$theme : wp_get_theme();
+	protected static function is_theme( $theme = null ) {
+		$theme = $theme instanceof WP_Theme ? $theme : wp_get_theme();
 
 		return ( str_contains( static::$theme_name, $theme->get_template() ) );
 	}
