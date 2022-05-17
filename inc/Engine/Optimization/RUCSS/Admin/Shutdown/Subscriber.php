811ad1c9ab17a724b5bafdecbaebcbac2635f396
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2022-05-17 04:03:27 -0400

    make it works

---
+++ inc/Engine/Optimization/RUCSS/Admin/Shutdown/Subscriber.php
@@ -0,0 +1,90 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace WP_Rocket\Engine\Optimization\RUCSS\Admin\Shutdown;
+
+use WP_Rocket\Event_Management\Subscriber_Interface;
+use Exception;
+
+class Subscriber implements Subscriber_Interface {
+	/**
+	 * RUCSS Shutdown banner render object.
+	 *
+	 * @var Shutdown
+	 */
+	private $shutdown;
+
+	/**
+	 * Instantiate the class
+	 *
+	 * @param Shutdown $shutdown RUCSS Shutdown instance.
+	 */
+	public function __construct( Shutdown $shutdown ) {
+		$this->shutdown = $shutdown;
+	}
+
+	/**
+	 * Return an array of events that this subscriber listens to.
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events(): array {
+		return [
+			'rocket_before_dashboard_content'     => 'display_before_shutdown_rucss_banner',
+			'get_rocket_option_remove_unused_css' => 'disable_rucss_with_shutdown_date',
+			'rocket_rucss_shutdown_details'       => 'get_shutdown_details',
+		];
+	}
+
+	/**
+	 * Display RUCSS shutdown warning banner.
+	 *
+	 * @return void
+	 */
+	public function display_before_shutdown_rucss_banner() {
+		try {
+			$this->shutdown->display_shutdown_banner();
+		} catch ( Exception $e ) {
+			// Do nothing, Don't show the banner.
+			return;
+		}
+	}
+
+	/**
+	 * Disable RUCSS option with the shutdown date.
+	 *
+	 * @param bool $enabled RUCSS option status.
+	 *
+	 * @return bool
+	 */
+	public function disable_rucss_with_shutdown_date( $enabled ) {
+		try {
+			return $enabled && ! $this->shutdown->is_expired();
+		} catch ( Exception $e ) {
+			// Do nothing, Don't show the banner.
+			return $enabled;
+		}
+	}
+
+	/**
+	 * Get shutdown event details.
+	 *
+	 * @param array $details array of shutdown details.
+	 *
+	 * @return array
+	 */
+	public function get_shutdown_details( $details ) {
+		try {
+			$default = [
+				'status'      => 0,
+				'renewal_url' => '',
+			];
+
+			return wp_parse_args( $this->shutdown->get_shutdown_details( $details ), $default );
+		} catch ( Exception $e ) {
+			// Do nothing, Don't change anything.
+			return $details;
+		}
+	}
+}
---
+++ views/rucss-shutdown/before.php
@@ -0,0 +1,46 @@
+<div class="rocket-promo-banner" id="rocket-shutdown-before-banner">
+	<div class="rocket-expired-message">
+		<h3 class="rocket-expired-title"><?php esc_html_e( 'Important: Feature Shutting Down Soon', 'rocket' ); ?></h3>
+		<p>
+			<?php
+			printf(
+			// translators: %1$s = <strong>, %2$s = </strong>, %3$s = date.
+					esc_html__( 'You’re running on an old version of WP Rocket whose %1$sRemove Unused CSS feature%2$s will be shut down %1$sfrom %3$s.%2$s', 'rocket' ),
+					'<strong>',
+					'</strong>',
+					esc_html( $data['formatted_date'] )
+			);
+			?>
+		</p>
+		<p>
+			<?php
+			esc_html_e(
+				'This option was completely revamped in 3.11 to more easily address the PageSpeed Insights recommendation
+			and improve your page performance.',
+				'rocket'
+			);
+			?>
+		</p>
+		<p>
+			<?php
+			printf(
+			// translators: %1$s = <strong>, %2$s = discount percentage, %3$s = </strong>.
+				esc_html__( '%1$sRenew your license now and get %2$s OFF%3$s to update your WP Rocket version!', 'rocket' ),
+				'<strong>',
+					(int) sanitize_text_field( $data['discount_percentage'] ) . '%',
+				'</strong>'
+			);
+			?>
+		</p>
+	</div>
+	<div class="rocket-expired-cta-container">
+		<a href="<?php echo esc_url( $data['renewal_url'] ); ?>" class="rocket-renew-cta" target="_blank" rel="noopener noreferrer">
+			<?php esc_html_e( 'Renew now', 'rocket' ); ?>
+		</a>
+	</div>
+	<button class="wpr-notice-close wpr-icon-close" id="rocket-dismiss-renewal">
+		<span class="screen-reader-text">
+			<?php esc_html_e( 'Dismiss this notice.', 'rocket' ); ?>
+		</span>
+	</button>
+</div>
