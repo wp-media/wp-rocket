3564a6d0cc8094b6ca07b996058b7ac6aa76c0a3
Rémy Perona <remperona@gmail.com>
2022-05-20 15:52:59 -0400

    add callback to update the DB cache row

---
+++ inc/Engine/Preload/Subscriber.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\Engine\Preload;
+
+use WP_Rocket\Event_Management\Subscriber_Interface;
+
+class Subscriber implements Subscriber_Interface {
+	/**
+	 * Cache query instance
+	 *
+	 * @var CacheQuery
+	 */
+	private $query;
+
+	/**
+	 * Instantiate the class
+	 *
+	 * @param [type] $query Cache query instance.
+	 */
+	public function __construct( $query ) {
+		$this->query = $query;
+	}
+
+	/**
+	 * Array of events this subscriber listens to
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events() {
+		return [
+			'after_rocket_buffer' => 'update_cache_row',
+		];
+	}
+
+	/**
+	 * Update the cache row when processing the buffer
+	 *
+	 * @return void
+	 */
+	public function update_cache_row() {
+		global $wp;
+
+		$url = untrailingslashit( home_url( add_query_arg( [], $wp->request ) ) );
+
+		$this->query->create_or_update(
+			[
+				'url' => $url,
+				'status' => 'completed',
+			]
+		);
+	}
+}
