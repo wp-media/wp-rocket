f76a34fdd5dc0567f8db638e7fea620aaa659c62
Michael Lee <michaelleemichaellee408@gmail.com>
2022-05-25 11:35:35 +0100

    Updated integration test

--- tests/Integration/inc/ThirdParty/Plugins/InlineRelatedPosts/excludeInlineFromRucss.php
+++ tests/Integration/inc/ThirdParty/Plugins/InlineRelatedPosts/excludeInlineFromRucss.php
@@ -1,6 +1,8 @@
 <?php
+namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Plugins;
 
 use WP_Rocket\Tests\Integration\TestCase;
+use WP_Rocket\ThirdParty\Plugins\InlineRelatedPosts;
 
 /**
  * @covers  \WP_Rocket\ThirdParty\Plugins\InlineRelatedPosts::exclude_inline_from_rucss
@@ -8,11 +10,18 @@ use WP_Rocket\Tests\Integration\TestCase;
  */
 class Test_ExcludeInlineFromRucss extends TestCase {
 
+	public function tear_down() {
+		remove_filter( 'rocket_rucss_inline_content_exclusions', [ $this, 'return_false' ] );
+
+		parent::tear_down();
+	}
+
 	/**
 	 * @dataProvider configTestData
 	 */
 	public function testShouldReturnExpected( $config, $expected ) {
-		$this->assertSame( $expected, apply_filters( 'rocket_rucss_inline_content_exclusions', $config['excluded'] ));
+        $inline_related_posts = new InlineRelatedPosts();
+		$this->assertSame( $expected, apply_filters( 'rocket_rucss_inline_content_exclusions', $inline_related_posts->exclude_inline_from_rucss($config['excluded'] ) ) );
 	}
 
 }
