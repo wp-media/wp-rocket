35f11a89857af14651b88d413d4e159559d6b13b
Michael Lee <michaelleemichaellee408@gmail.com>
2022-05-26 15:46:49 +0100

    Removed tear_down method and filters

--- tests/Integration/inc/ThirdParty/Plugins/InlineRelatedPosts/excludeInlineFromRucss.php
+++ tests/Integration/inc/ThirdParty/Plugins/InlineRelatedPosts/excludeInlineFromRucss.php
@@ -10,18 +10,12 @@ use WP_Rocket\ThirdParty\Plugins\InlineRelatedPosts;
  */
 class Test_ExcludeInlineFromRucss extends TestCase {
 
-	public function tear_down() {
-		remove_filter( 'rocket_rucss_inline_content_exclusions', [ $this, 'return_false' ] );
-
-		parent::tear_down();
-	}
-
 	/**
 	 * @dataProvider configTestData
 	 */
 	public function testShouldReturnExpected( $config, $expected ) {
         $inline_related_posts = new InlineRelatedPosts();
-		$this->assertSame( $expected, apply_filters( 'rocket_rucss_inline_content_exclusions', $inline_related_posts->exclude_inline_from_rucss($config['excluded'] ) ) );
+		$this->assertSame( $expected, $inline_related_posts->exclude_inline_from_rucss($config['excluded'] ) );
 	}
 
 }
