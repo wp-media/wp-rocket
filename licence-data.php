ca9bbb6bddd3cbeb2c09dd11a9ed1ccc3f345c9e
COQUARD Cyrille <cyrille@wp-media.me>
2022-05-30 16:57:22 +0200

    Added back license data

--- licence-data.php
+++ licence-data.php
@@ -8,3 +8,11 @@ defined( 'ABSPATH' ) || exit;
  */
 
 // Your license KEY.
+if ( ! defined( 'WP_ROCKET_KEY' ) ) {
+	define( 'WP_ROCKET_KEY', '' );
+}
+
+// Your email, the one you used for the purchase.
+if ( ! defined( 'WP_ROCKET_EMAIL' ) ) {
+	define( 'WP_ROCKET_EMAIL', '' );
+}
