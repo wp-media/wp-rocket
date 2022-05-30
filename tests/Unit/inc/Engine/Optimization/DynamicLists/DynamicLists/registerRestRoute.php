9778818a3c137988e33faf426e9e30c30a47e083
Rémy Perona <remperona@gmail.com>
2022-05-30 15:48:07 -0400

    move and update test

--- tests/Unit/inc/Engine/Optimization/DynamicLists/DynamicLists/registerRestRoute.php
+++ tests/Unit/inc/Engine/Optimization/DynamicLists/DynamicLists/registerRestRoute.php
@@ -8,7 +8,6 @@ use WP_Rocket\Engine\Optimization\DynamicLists\APIClient;
 use WP_Rocket\Engine\Optimization\DynamicLists\DataManager;
 use WP_Rocket\Engine\Optimization\DynamicLists\DynamicLists;
 use WP_Rocket\Tests\Unit\TestCase;
-use WP_Rocket\Engine\Optimization\DynamicLists\Subscriber;
 
 /**
  * @covers \WP_Rocket\Engine\Optimization\DynamicLists::register_rest_route
@@ -20,8 +19,8 @@ class Test_RegisterRestRoute extends TestCase {
 	public function testShouldRegisterRestRoute() {
 		$dynamic_lists_api = Mockery::mock( APIClient::class );
 		$data_manager = Mockery::mock( DataManager::class );
-		$dynamic_lists= new DynamicLists($dynamic_lists_api,$data_manager);
-		$subscriber    = new Subscriber( $dynamic_lists );
+		$dynamic_lists = new DynamicLists($dynamic_lists_api,$data_manager, '');
+
 		Functions\expect( 'register_rest_route' )
 			->once()
 			->with( $dynamic_lists::ROUTE_NAMESPACE,
@@ -30,9 +29,8 @@ class Test_RegisterRestRoute extends TestCase {
 					'methods'             => 'PUT',
 					'callback'            => [ $dynamic_lists, 'rest_update_response' ],
 					'permission_callback' => [ $dynamic_lists, 'check_permissions' ],
-				] )
-			->andReturnNull();
+				] );
 
-		$subscriber->register_rest_route();
+		$dynamic_lists->register_rest_route();
 	}
 }
