95d626766fe72bdb1a49f39070bd9846598181d9
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2022-06-01 16:22:33 +0300

    Fixes #5026 Exclude cdn.amcharts.com from Minify JS (#5027)

--- inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -263,6 +263,7 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'ga.getresponse.com/script/ga.js',
 			'cognitoforms.com',
 			'usercentrics.eu',
+			'cdn.amcharts.com',
 			'umami',
 			'cdn.popt.in/pixel.js',
 		];
