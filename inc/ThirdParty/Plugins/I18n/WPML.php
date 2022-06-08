34d71961da53ebbfd5f2d2c6b803424cffd735eb
Michael Lee <michaelleemichaellee408@gmail.com>
2022-06-08 15:25:35 +0100

    Changed is_home to is_secondary_home and refactored method body

--- inc/ThirdParty/Plugins/I18n/WPML.php
+++ inc/ThirdParty/Plugins/I18n/WPML.php
@@ -23,49 +23,36 @@ class WPML implements Subscriber_Interface {
 
 		$events = [
 			'wcml_is_cache_enabled_for_switching_currency' => 'return_true',
-			'rocket_rucss_is_home_url'                     => [ 'is_home', 10, 2 ],
+			'rocket_rucss_is_home_url'                     => [ 'is_secondary_home', 10, 2 ],
 		];
 
 		return $events;
 	}
 
 	/**
-	 * Check if current url is home.
+	 * Checks if page to be processed is secondary homepage.
 	 *
 	 * @param string $home_url home url.
 	 * @param string $url url of current page.
 	 * @return string
 	 */
-	public static function is_home( string $home_url, string $url ): string {
+	public function is_secondary_home( string $home_url, string $url ): string {
+		global $sitepress;
 
-		// Prepare current url for segmentation.
-		$modified_url = str_replace( '://', '', $url );
+		// Get active languages on site.
+		$languages = $sitepress->get_active_languages();
 
-		// Check if modified url has segments.
-		if ( false !== strpos( $modified_url, '/' ) ) {
-			// Convert segments to array.
-			$lang_code = explode( '/', $modified_url );
+		foreach ( $languages as $lang ) {
+			$lang_url = $sitepress->language_url( $lang['code'] );
 
-			// Get the second segment which may be a lang code.
-			$lang_code = $lang_code[1];
-
-			// Check if supposed lang code is actually a valid lang code: else return home url.
-			if ( strlen( $lang_code ) < 2 || strlen( $lang_code ) > 7 ) {
-				return $home_url;
+			// Check if current url is a secondary homepage.
+			if ( untrailingslashit( $lang_url ) !== $url ) {
+				continue;
 			}
 
-			/**
-			 * Filters the active language with lang code.
-			 *
-			 * @since 3.11.4
-			 *
-			 * @param string  $empty_value NULL value.
-			 * @param string  $lang_code language code.
-			 */
-			return apply_filters( 'wpml_language_is_active', null, $lang_code ) ? $url : $home_url; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
+			return $url;
 		}
 
-		// Return home url if current url has no segments (is default home).
 		return $home_url;
 	}
 }
