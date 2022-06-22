9f678333b67e3aec6e44e026c596a7718380aa42
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2022-06-22 15:30:12 +0200

    Add new Geo Maps plugin exclusion from combine JS (#5173)

--- inc/Engine/Optimization/Minify/JS/Combine.php
+++ inc/Engine/Optimization/Minify/JS/Combine.php
@@ -718,6 +718,7 @@ class Combine extends AbstractJSOptimization implements ProcessorInterface {
 			'urls_polylangREPLACETOID',
 			'e.setAttribute(\'unselectable\',on);',
 			'try{Typekit.load',
+			'iMapsData',
 		];
 
 		$excluded_inline = array_merge( $defaults, $this->options->get( 'exclude_inline_js', [] ) );
