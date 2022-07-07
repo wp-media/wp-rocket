900cbc4764759e0b9057c1851b5a53b619fce319
Michael Lee <michaelleemichaellee408@gmail.com>
2022-07-07 19:12:36 +0100

    Updated subscriber event

--- inc/ThirdParty/Plugins/PageBuilder/Elementor.php
+++ inc/ThirdParty/Plugins/PageBuilder/Elementor.php
@@ -63,9 +63,10 @@ class Elementor implements Subscriber_Interface {
 			'delete_option__elementor_global_css'      => 'clear_cache',
 			'rocket_buffer'                            => [ 'add_fix_animation_script', 28 ],
 			'rocket_exclude_js'                        => 'exclude_js',
-			'rocket_skip_post_row_actions'             => [ 'remove_rocket_row_action', 1, 2 ],
-			'rocket_metabox_options_post_types'        => 'remove_rocket_metabox_option',
+			'rocket_skip_post_row_actions'             => 'remove_rocket_option',
+			'rocket_metabox_options_post_types'        => 'remove_rocket_option',
 			'rocket_skip_admin_bar_cache_purge_option' => [ 'skip_admin_bar_cache_purge_option', 1, 2 ],
+			'rocket_submitbox_options_post_types'      => 'remove_rocket_option',
 		];
 	}
 
@@ -176,27 +177,12 @@ class Elementor implements Subscriber_Interface {
 	 * @param array $cpts Custom post type.
 	 * @return array
 	 */
-	public function remove_rocket_metabox_option( array $cpts ): array {
+	public function remove_rocket_option( array $cpts ): array {
 		if ( isset( $cpts['elementor_library'] ) ) {
 			unset( $cpts['elementor_library'] );
 		}
 
 		return $cpts;
-	}
-
-	/**
-	 * Remove rocket option from row actions.
-	 *
-	 * @param boolean $default Filter default value.
-	 * @param mixed   $post Post object.
-	 * @return boolean
-	 */
-	public function remove_rocket_row_action( bool $default, $post ): bool {
-		if ( 'elementor_library' === $post->post_type ) {
-			return true;
-		}
-
-		return $default;
 	}
 
 	/**
