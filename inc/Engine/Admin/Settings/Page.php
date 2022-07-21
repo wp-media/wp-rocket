eae541dd4fe7c9044a256c8aed1e08b5337f56b5
COQUARD Cyrille <cyrille@wp-media.me>
2022-07-21 13:41:31 +0200

    Removed beta mention

--- inc/Engine/Admin/Settings/Page.php
+++ inc/Engine/Admin/Settings/Page.php
@@ -720,15 +720,10 @@ class Page {
 					'sanitize_callback'       => 'sanitize_checkbox',
 					'options'                 => [
 						'remove_unused_css' => [
-							'label'       => __( 'Remove Unused CSS (Beta)', 'rocket' ),
+							'label'       => __( 'Remove Unused CSS', 'rocket' ),
 							'disabled'    => $invalid_license ? 'disabled' : false,
 							// translators: %1$s = opening <a> tag, %2$s = closing </a> tag.
 							'description' => sprintf( __( 'Removes unused CSS per page and helps to reduce page size and HTTP requests. Recommended for best performance. Test thoroughly! %1$sMore info%2$s', 'rocket' ), '<a href="' . esc_url( $rucss_beacon['url'] ) . '" data-beacon-article="' . esc_attr( $rucss_beacon['id'] ) . '" target="_blank">', '</a>' ),
-							'warning'     => $invalid_license ? [] : [
-								'title'        => __( 'We’re still working on it!', 'rocket' ),
-								'description'  => __( 'This is a beta feature. We’re providing you early access but some changes might be added later on. If you notice any errors on your website, simply deactivate the feature.', 'rocket' ),
-								'button_label' => __( 'Activate Remove Unused CSS', 'rocket' ),
-							],
 							'sub_fields'  => $invalid_license ? [] : [
 								'remove_unused_css_safelist' =>
 								[
