54b5122b95b468323ad46f024509328a1920ec97
Alfonso Catron <alfonso.catron@gmail.com>
2022-08-03 14:28:58 -0300

    adding Convert Plus plugin to $inline_content_exclusions

--- inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Controller/UsedCSS.php
@@ -98,6 +98,7 @@ class UsedCSS {
 		'.wprm-advanced-list-',
 		'.adsslot_', // For Advanced Ads plugin ads.
 		'.jnews_', // For JNews theme.
+		'.cp-info-bar.content-', // For Convert Plus plugin.
 	];
 
 	/**
