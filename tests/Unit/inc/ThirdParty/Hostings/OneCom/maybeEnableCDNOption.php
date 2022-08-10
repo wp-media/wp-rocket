111ceffbd646e0366ad1204916ae7a12ceeea8f2
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-10 13:51:35 +0100

    Added unit tests

---
+++ tests/Unit/inc/ThirdParty/Hostings/OneCom/maybeEnableCDNOption.php
@@ -0,0 +1,113 @@
+<?php
+namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Hostings\OneCom;
+
+use WP_Rocket\Admin\Options;
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\ThirdParty\Hostings\OneCom;
+use WPMedia\PHPUnit\Unit\TestCase;
+use Brain\Monkey\Functions;
+use Mockery;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Hostings\OneCom::maybe_enable_cdn_option
+ * @group OneCom
+ * @group ThirdParty
+ */
+class Test_MaybeEnableCDNOption extends TestCase {
+    private $onecom, $options_api, $option;
+
+	public function setUp() : void {
+		parent::setUp();
+        
+        $this->options_api = Mockery::mock( Options::class );
+        $this->options = Mockery::mock( Options_Data::class );
+
+        $this->onecom = new OneCom( $this->options_api, $this->options );
+	}
+
+    public function tear_down() {
+		parent::tear_down();
+
+		// Reset after each test.
+		unset( $_SERVER['ONECOM_DOMAIN_NAME'] );
+		unset( $_SERVER['HTTP_HOST'] );
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldReturnExpected( $config, $expected ) {
+
+        Functions\expect( 'rest_sanitize_boolean' )
+				->once()
+				->andReturn( $config['oc_cdn_enabled'] );
+
+        Functions\when( 'get_option' )
+			->alias( function( $value ) use( $config ) {
+				if ( 'oc_cdn_enabled' === $value ) {
+                    return $config['oc_cdn_enabled'];
+                }
+			}
+		);
+
+        if ( $config['oc_cdn_enabled'] === true ) {
+            $this->options
+                ->shouldReceive( 'get' )
+                ->with( 'cdn_cnames', [] )
+                ->andReturn( $config['options']['cdn_cnames'] );
+
+            $this->options
+                ->shouldReceive( 'get' )
+                ->with( 'cdn_zone', [] )
+                ->andReturn( $config['options']['cdn_zones'] );
+
+            $this->options
+                ->shouldReceive( 'get' )
+                ->with( 'cdn', 0 )
+                ->andReturn( $config['cdn'] );
+        }
+
+        if ( $config['oc_cdn_enabled'] === true && $config['cdn'] === 0 ) {
+
+            $_SERVER['ONECOM_DOMAIN_NAME'] = 'example.com';
+            $_SERVER['HTTP_HOST'] = 'example.com';
+
+            $cdn_url = $this->onecom->build_cname();
+
+            $this->assertSame( $expected['cdn_cname'], $cdn_url );
+
+            $this->options
+                ->shouldReceive( 'set' )
+                ->with( 'cdn', $config['options']['cdn'] )
+                ->andReturn( true );
+
+            $this->options
+                ->shouldReceive( 'set' )
+                ->with( 'cdn_cnames', $config['options']['cdn_cnames'] )
+                ->once()
+                ->andReturn( true );
+
+            $this->options
+                ->shouldReceive( 'set' )
+                ->with( 'cdn_zone', $config['options']['cdn_zones'] )
+                ->andReturn( true );
+
+            $this->options
+                ->shouldReceive( 'get_options' )
+                ->andReturn( [] );
+
+            $this->options_api
+                ->shouldReceive( 'set' )
+                ->with( 'settings', [] )
+                ->andReturn( true );
+
+            Functions\expect( 'rocket_clean_domain' )->once();
+        }
+
+		$this->onecom->maybe_enable_cdn_option();
+	}
+
+	public function providerTestData() {
+		return $this->getTestData( __DIR__, 'maybeEnableCDNOption' );
+	}
+}
