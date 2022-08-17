1996133dcb5e29c00f5a62a13b90dc204b05165c
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2022-08-17 11:55:12 +0200

    Fixed the warning that displays on php 8.1 while using ctype_digit (#5313)
    * Fixed the warning that displays on php 8.1
    
    * Fixed broken tests

--- tests/Fixtures/inc/Engine/Optimization/DelayJS/Admin/Subscriber/setOptionOnUpdate.php
+++ tests/Fixtures/inc/Engine/Optimization/DelayJS/Admin/Subscriber/setOptionOnUpdate.php
@@ -3,34 +3,34 @@
 return [
 	'testShouldNotUpdateOptionWhenVersionAbove3.9' => [
 		'options'       => [
-			'delay_js'            => 1.0,
+			'delay_js'            => 1,
 			'delay_js_exclusions' => [],
 		],
 		'old_version'   => '3.10',
 		'expected'      => [
-			'delay_js'            => 1.0,
+			'delay_js'            => 1,
 			'delay_js_exclusions' => [],
 		],
 	],
 	'testShouldNotUpdateOptionWhenDelayJSEquals0'  => [
 		'options'       => [
-			'delay_js' => 0.0,
+			'delay_js' => 0,
 		],
 		'old_version'   => '3.8',
 		'expected'      => [
-			'delay_js'            => 0.0,
+			'delay_js'            => 0,
 			'delay_js_exclusions' => [],
 		],
 	],
 	'testShouldUpdateOptionWhenVersionBelow3.9AndDelayJSEquals1' => [
 		'options'       => [
-			'delay_js'              => 1.0,
-			'minify_concatenate_js' => 1.0,
+			'delay_js'              => 1,
+			'minify_concatenate_js' => 1,
 		],
 		'old_version'   => '3.8',
 		'expected'      => [
-			'delay_js'              => 1.0,
-			'minify_concatenate_js' => 0.0,
+			'delay_js'              => 1,
+			'minify_concatenate_js' => 0,
 			'delay_js_exclusions'   => [],
 		],
 	],
