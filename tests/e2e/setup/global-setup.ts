79536162ed1d9dc644dcf2eca63236e4d76e3b47
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-18 15:38:15 +0100

    Created global setup

---
+++ tests/e2e/setup/global-setup.ts
@@ -0,0 +1,20 @@
+// global-setup.ts
+import { chromium, FullConfig } from '@playwright/test';
+import { WP_BASE_URL, WP_USERNAME, WP_PASSWORD } from './../wp.config';
+
+async function globalSetup(config: FullConfig) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(WP_BASE_URL + '/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8889%2Fwp-admin%2F&reauth=1');
+  // Fill username & password.
+  await page.locator('input[name="log"]').fill(WP_USERNAME);
+  await page.locator('input[name="pwd"]').fill(WP_PASSWORD);
+  // Click login.
+  await page.locator('text=Log In').click();
+
+  // Save signed-in state to 'storageState.json'.
+  await page.context().storageState({ path: 'tests/e2e/storageState.json' });
+  await browser.close();
+}
+
+export default globalSetup;
