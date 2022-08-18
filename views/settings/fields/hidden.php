8052fef1f8307fc5cae33d932b49832972eca50c
Rémy Perona <remperona@gmail.com>
2022-08-18 15:58:23 -0400

    create multiple fields if value is array

--- views/settings/fields/hidden.php
+++ views/settings/fields/hidden.php
@@ -14,5 +14,14 @@
 
 defined( 'ABSPATH' ) || exit;
 
+if ( is_array( $data['value'] ) ) {
+	foreach( $data['value'] as $value ) {
+		?>
+		<input type="hidden" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>[]]" value="<?php echo esc_attr( $value ); ?>">
+		<?php
+	}
+} else {
 ?>
 <input type="hidden" id="<?php echo esc_attr( $data['id'] ); ?>" name="wp_rocket_settings[<?php echo esc_attr( $data['id'] ); ?>]" value="<?php echo esc_attr( $data['value'] ); ?>">
+<?php
+}
