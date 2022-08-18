c25b2e9335cf6afa86c80421b4da53bfc472461c
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-18 18:48:24 +0100

    Remove action to test

--- tests/e2e/setup/global-setup.ts
+++ tests/e2e/setup/global-setup.ts
@@ -6,9 +6,7 @@ async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(WP_BASE_URL + '/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8889%2Fwp-admin%2F&reauth=1');
-  // Fill username & password.
-  await page.locator('input[name="log"]').fill(WP_USERNAME, { timeout: 120000 });
-  await page.locator('input[name="pwd"]').fill(WP_PASSWORD, { timeout: 120000 });
+
   // Click login.
   await page.locator('text=Log In').click();
 
