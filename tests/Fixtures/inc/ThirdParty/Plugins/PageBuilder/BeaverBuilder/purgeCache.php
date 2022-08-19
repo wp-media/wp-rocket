dad997f96c8da365461de401b1694f49c5c0d290
COQUARD Cyrille <cyrille@wp-media.me>
2022-08-19 10:38:15 +0200

    Fixed tests

--- tests/Fixtures/inc/ThirdParty/Plugins/PageBuilder/BeaverBuilder/purgeCache.php
+++ tests/Fixtures/inc/ThirdParty/Plugins/PageBuilder/BeaverBuilder/purgeCache.php
@@ -26,17 +26,6 @@ return [
 		],
 	],
 	'test_data' => [
-		'FlBuilderBeforeSaveLayout' => [
-			'fl_builder_before_save_layout',
-			[
-				'wp-content/cache/wp-rocket/example.org/index.html',
-				'wp-content/cache/wp-rocket/example.org/index.html_gzip',
-				'wp-content/cache/wp-rocket/example.org-wpmedia-594d03f6ae698691165999/about/index.html',
-				'wp-content/cache/wp-rocket/example.org-wpmedia-594d03f6ae698691165999/about/index.html_gzip',
-				'wp-content/cache/min/1/5c795b0e3a1884eec34a989485f863ff.js',
-				'wp-content/cache/min/1/fa2965d41f1515951de523cecb81f85e.css',
-			],
-		],
 		'FlBuilderCacheCleared' => [
 			'fl_builder_cache_cleared',
 			[
