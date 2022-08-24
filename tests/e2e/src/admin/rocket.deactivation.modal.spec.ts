29100dc42b805359b8ea8e263b4ca569aca8a1bc
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-24 19:10:31 +0100

    Updated deactivation modal test

--- tests/e2e/src/admin/rocket.deactivation.modal.spec.ts
+++ tests/e2e/src/admin/rocket.deactivation.modal.spec.ts
@@ -1,36 +1,12 @@
-import { test, expect } from '@playwright/test';
-import { WP_BASE_URL } from '../../wp.config';
+import { test } from '@playwright/test';
 
-import { pageUtils } from '../../utils/page.utils';
+/**
+ * Local deps.
+ */
+import { deactivationModal } from '../common/deactivation.modal';
 
 test.describe('WPR Deactivation Modal', () => {
     test('should pop up deactivation modal', async ( { page } ) => {
-
-        const page_utils = new pageUtils( page );
-
-        await page_utils.visitPage( 'wp-admin' );
-
-        const plugin_menu = '#menu-plugins';
-
-        const locator = {
-            'plugin': page.locator( plugin_menu ),
-            'deactivate': page.locator( '[aria-label="Deactivate WP Rocket"]' ),
-            'safe_mode': page.locator( '#safe_mode' ),
-        };
-
-        await page.waitForSelector( plugin_menu )
-        // Expect plugins link to be in view.
-        await expect( locator.plugin ).toBeVisible();
-
-        // Navigate to plugins page.
-        await locator.plugin.click();
-        await expect( page ).toHaveURL( WP_BASE_URL + '/wp-admin/plugins.php' );
-
-        // Expect WPR to be active: Deactivate link to be visible.
-        await expect( locator.deactivate ).toBeVisible();
-        await locator.deactivate.click();
-        
-        // Expect Deactivation Modal to be visible
-        await expect( locator.safe_mode ).toBeVisible();
+        await deactivationModal( page );
     });
 });
