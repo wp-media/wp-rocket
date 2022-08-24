2f04fdf375faf24fbc98cddd6f1768502a8ab52b
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-24 11:44:23 +0100

    Created new class for page related tests

---
+++ tests/e2e/utils/page.utils.ts
@@ -0,0 +1,23 @@
+import type { Page } from '@playwright/test';
+import { WP_BASE_URL, WP_USERNAME, WP_PASSWORD } from '../wp.config';
+
+export class pageUtils {
+	readonly page: Page;
+
+    constructor( page: Page ){
+        this.page = page;
+    }
+
+    wpAdminLogin = async () => {
+        // Fill username & password.
+        await this.page.locator('input[name="log"]').fill(WP_USERNAME);
+        await this.page.locator('input[name="pwd"]').fill(WP_PASSWORD);
+
+        // Click login.
+        await this.page.locator('text=Log In').click();
+    }
+
+    visitPage = async ( page_url ) => {
+        await this.page.goto(WP_BASE_URL + '/' + page_url);
+    }
+}
