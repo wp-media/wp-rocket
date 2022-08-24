38020790f49375b6bddd319aae7c2495b09881b5
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2022-08-24 04:52:14 -0400

    adjust unit tests

--- tests/Fixtures/inc/Engine/Preload/Activation/refreshOnUpdate.php
+++ tests/Fixtures/inc/Engine/Preload/Activation/refreshOnUpdate.php
@@ -2,14 +2,23 @@
 return [
 	'versionInferiorShouldDoNothing' => [
 		'config' => [
-			'new_version' => '3.12.0',
+			'new_version' => '3.14.0',
 			'old_version' => '3.13.0',
 		],
+		'result' => false,
 	],
 	'versionSuperiorShouldReload' => [
 		'config' => [
-			'new_version' => '3.11.0',
-			'old_version' => '3.12.0',
+			'new_version' => '3.12.2',
+			'old_version' => '3.11.0',
 		],
-	]
+		'result' => true,
+	],
+	'versionExactlyShouldReload' => [
+		'config' => [
+			'new_version' => '3.12.0.3',
+			'old_version' => '3.12.0.2',
+		],
+		'result' => true,
+	],
 ];
