80d5fe0631cb64144bdb619f72d2b189b197b407
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-24 11:51:25 +0100

    Update global setup to use utils class

--- tests/e2e/setup/global-setup.ts
+++ tests/e2e/setup/global-setup.ts
@@ -1,16 +1,17 @@
 // global-setup.ts
 import { chromium, FullConfig } from '@playwright/test';
-import { WP_BASE_URL, WP_USERNAME, WP_PASSWORD } from './../wp.config';
 
-async function globalSetup(config: FullConfig) {
+import { pageUtils } from '../utils/page.utils';
+
+export async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(WP_BASE_URL + '/wp-login.php');
-  // Fill username & password.
-  await page.locator('input[name="log"]').fill(WP_USERNAME);
-  await page.locator('input[name="pwd"]').fill(WP_PASSWORD);
-  // Click login.
-  await page.locator('text=Log In').click();
+
+  const page_utils = new pageUtils( page );
+
+  await page_utils.visitPage('wp-login.php');
+
+  await page_utils.wpAdminLogin();
 
   // Save signed-in state to 'storageState.json'.
   await page.context().storageState({ path: 'tests/e2e/storageState.json' });
