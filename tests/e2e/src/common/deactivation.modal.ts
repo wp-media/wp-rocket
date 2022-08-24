7b7b19bd638527081c3f3a4f9ec3fa23dc426742
Michael Lee <michaelleemichaellee408@gmail.com>
2022-08-24 18:12:51 +0100

    Added common function for deactivation modal engagement

---
+++ tests/e2e/src/common/deactivation.modal.ts
@@ -0,0 +1,45 @@
+import { expect, Page } from '@playwright/test';
+
+/**
+ * Local deps.
+ */
+import { WP_BASE_URL } from '../../wp.config';
+import { pageUtils } from '../../utils/page.utils';
+
+export const deactivationModal = async ( page: Page ) => {
+  const page_utils = new pageUtils( page );
+
+  const plugin_menu = '#menu-plugins';
+
+  const locator = {
+      'plugin': page.locator( plugin_menu ),
+      'deactivate': page.locator( '[aria-label="Deactivate WP Rocket"]' ),
+      'safe_mode': page.locator( '#safe_mode' ),
+  };
+
+  await page_utils.visitPage( 'wp-admin' );
+
+  await page.waitForSelector( plugin_menu )
+  // Expect plugins link to be in view.
+  await expect( locator.plugin ).toBeVisible();
+
+  // Navigate to plugins page.
+  await locator.plugin.click();
+  await expect( page ).toHaveURL( WP_BASE_URL + '/wp-admin/plugins.php' );
+
+  // Expect WPR to be active: Deactivate link to be visible.
+  await expect( locator.deactivate ).toBeVisible();
+  await locator.deactivate.click();
+
+  // Expect Deactivation Modal to be visible
+  await expect( locator.safe_mode ).toBeVisible();
+}
+
+
+
+
+
+
+
+
+
