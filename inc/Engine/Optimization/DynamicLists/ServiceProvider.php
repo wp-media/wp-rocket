509aafaf447fcc3c6c716f0711f4c93a7a70801c
Rémy Perona <remperona@gmail.com>
2022-08-29 11:34:20 -0400

    fix PHPCS

--- inc/Engine/Optimization/DynamicLists/ServiceProvider.php
+++ inc/Engine/Optimization/DynamicLists/ServiceProvider.php
@@ -37,7 +37,7 @@ class ServiceProvider extends AbstractServiceProvider {
 		$this->getContainer()->add( 'dynamic_lists', DynamicLists::class )
 			->addArgument( $this->getContainer()->get( 'dynamic_lists_api_client' ) )
 			->addArgument( $this->getContainer()->get( 'dynamic_lists_data_manager' ) )
-			->addArgument( $this->getContainer()->get( 'user') )
+			->addArgument( $this->getContainer()->get( 'user' ) )
 			->addArgument( $this->getContainer()->get( 'template_path' ) );
 		$this->getContainer()->share( 'dynamic_lists_subscriber', Subscriber::class )
 			->addArgument( $this->getContainer()->get( 'dynamic_lists' ) );
