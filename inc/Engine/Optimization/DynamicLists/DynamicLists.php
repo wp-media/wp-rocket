a419299bde099dfe9ef782ac881be31fdc466b4e
mostafa <mostafa.hisham.mahmoud@gmail.com>
2022-08-31 10:33:28 +0200

    print user ignore phpcs

--- inc/Engine/Optimization/DynamicLists/DynamicLists.php
+++ inc/Engine/Optimization/DynamicLists/DynamicLists.php
@@ -86,7 +86,7 @@ class DynamicLists extends Abstract_Render {
 	 * @return array
 	 */
 	public function update_lists_from_remote() {
-		var_dump($this->user);
+		var_dump( $this->user );// phpcs:ignore
 		if ( $this->user->is_license_expired() ) {
 			return [
 				'success' => false,
