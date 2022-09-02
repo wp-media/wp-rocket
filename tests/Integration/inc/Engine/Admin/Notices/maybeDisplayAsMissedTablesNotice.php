0fbc3d74ff46d548bb727e7cb67a3b84139213ae
Michael Lee <michaelleemichaellee408@gmail.com>
2022-09-02 20:18:55 +0100

    Updated integration test

--- tests/Integration/inc/Engine/Admin/Notices/maybeDisplayAsMissedTablesNotice.php
+++ tests/Integration/inc/Engine/Admin/Notices/maybeDisplayAsMissedTablesNotice.php
@@ -5,7 +5,8 @@ namespace WP_Rocket\Tests\Integration\inc\Engine\Admin;
 use Mockery;
 use Brain\Monkey\Functions;
 use WP_Rocket\Tests\Integration\TestCase;
-use wpdb;
+use ActionScheduler_StoreSchema;
+use ActionScheduler_LoggerSchema;
 
 /**
  * @covers \WP_Rocket\Engine\Admin\Notices::maybe_display_as_missed_tables_notice
@@ -26,20 +27,24 @@ class Test_MaybeDisplayAsMissedTables extends TestCase{
 		self::$admin_user_id = static::factory()->user->create( [ 'role' => 'administrator' ] );
 		self::$contributer_user_id = static::factory()->user->create( [ 'role' => 'contributor' ] );
 	}
+
+    public function tear_down() {
+        parent::tear_down();
+
+        remove_filter( 'query', [ $this, 'drop_as_table' ] );
+        $this->recreate_as_tables();
+    }
 	
     /**
 	 * @dataProvider configTestData
 	 */
 	public function testShouldDoAsExpected( $config, $expected ) {
-        global $wpdb;
 
         set_current_screen( $config['current_screen']->id );
 
         $this->configUser( $config['is_admin'] );
 
-        $table_name = $wpdb->prefix . 'actionscheduler_claims';
-        $sql = "DROP TABLE IF EXISTS $table_name";
-        $wpdb->query( $sql );
+        add_filter( 'query', [ $this, 'drop_as_table' ] );
 
         ob_start();
 		do_action( 'admin_notices' );
@@ -60,5 +65,18 @@ class Test_MaybeDisplayAsMissedTables extends TestCase{
         $user_id = $is_admin ? self::$admin_user_id : self::$contributer_user_id;
 
         wp_set_current_user( $user_id );
+    }
+
+    public function drop_as_table() {
+        global $wpdb;
+
+        return "DROP TABLE IF EXISTS {$wpdb->prefix}actionscheduler_claims";
+    }
+
+    private function recreate_as_tables() {
+        $store_schema  = new ActionScheduler_StoreSchema();
+		$logger_schema = new ActionScheduler_LoggerSchema();
+		$store_schema->register_tables( true );
+		$logger_schema->register_tables( true );
     }
 }
