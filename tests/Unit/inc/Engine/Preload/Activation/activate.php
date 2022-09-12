8568459cc26fb22d251b10426fa42d6502af0a57
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2022-09-12 10:44:05 +0100

    Closes #5396 on fatal error when no AS table on fresh install (PR #5398)
    * Added check for missing AS tables
    * Updated tests
    * Updated method name

--- tests/Unit/inc/Engine/Preload/Activation/activate.php
+++ tests/Unit/inc/Engine/Preload/Activation/activate.php
@@ -9,6 +9,7 @@ use WP_Rocket\Engine\Preload\Controller\LoadInitialSitemap;
 use WP_Rocket\Engine\Preload\Controller\Queue;
 use WP_Rocket\Engine\Preload\Database\Queries\Cache;
 use WP_Rocket\Tests\Unit\TestCase;
+use wpdb;
 
 /**
  * @covers \WP_Rocket\Engine\Preload\Activation::activate
@@ -17,24 +18,42 @@ use WP_Rocket\Tests\Unit\TestCase;
 class Test_Activate extends TestCase
 {
 
-	protected $activation;
-	protected $queue;
-	protected $query;
+    protected $activation;
+    protected $queue;
+    protected $query;
+    private $wpdb;
 
-	protected function setUp(): void
-	{
-		parent::setUp();
-		$this->queue = Mockery::mock(Queue::class);
-		$this->query = $this->createMock(Cache::class);
-		$this->activation = new Activation($this->queue, $this->query);
-	}
+    public static function setUpBeforeClass(): void {
+        parent::setUpBeforeClass();
 
-	/**
-	 * @dataProvider configTestData
-	 */
-	public function testShouldDoAsExpected($config) {
-		$this->queue->expects()->add_job_preload_job_load_initial_sitemap_async();
+        require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/wpdb.php';
+        require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/inc/Engine/Preload/Activation/ActionScheduler_StoreSchema.php';
+        require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/inc/Engine/Preload/Activation/ActionScheduler_LoggerSchema.php';
+    }
 
-		$this->activation->activate();
-	}
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->queue = Mockery::mock(Queue::class);
+        $this->query = $this->createMock(Cache::class);
+        $this->activation = new Activation($this->queue, $this->query);
+        
+        $GLOBALS['wpdb'] = $this->wpdb = new wpdb();
+    }
+
+    public function tear_down() {
+        parent::tear_down();
+
+        unset($GLOBALS['wpdb']);
+    }
+
+    /**
+     * @dataProvider configTestData
+     */
+    public function testShouldDoAsExpected( $config ) {
+        $this->queue->expects()->add_job_preload_job_load_initial_sitemap_async();
+
+        $this->activation->activate();
+    }
 }
+
