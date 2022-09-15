d80ce44271bdfbdfdf6a2a7fc3f97416e3c571f5
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2022-09-15 16:27:21 +0200

    Update inc/Engine/Preload/Controller/CheckExcludedTrait.php
    Co-authored-by: Rémy Perona <remy@wp-rocket.me>

--- inc/Engine/Preload/Controller/CheckExcludedTrait.php
+++ inc/Engine/Preload/Controller/CheckExcludedTrait.php
@@ -21,7 +21,7 @@ trait CheckExcludedTrait {
 	 * @param string $url url to check.
 	 * @return bool
 	 */
-	protected function is_excluded_by_filter( string $url ) {
+	protected function is_excluded_by_filter( string $url ): bool {
 		$regexes = apply_filters( 'rocket_preload_exclude_urls', [] );
 
 		if ( ! $regexes ) {
