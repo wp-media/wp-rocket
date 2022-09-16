b1a7db8817b04733bcc8ce77e74609bc97c3c39f
Rémy Perona <remperona@gmail.com>
2022-09-16 11:46:03 -0400

    fix treeshake tests

--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Controller/UsedCSS/HTML/filtred_exclude_attr.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<title>Test</title>
-	<style id="wpr-usedcss">h1{color:red;}</style>
-	<style id="rocket-lazyload-inline-css">body{background:blue;}</style>
-</head>
-<body>
-</body>
-</html>
--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Controller/UsedCSS/HTML/filtred_exclude_content.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<title>Test</title>
-	<style id="wpr-usedcss">h1{color:red;}</style>
-	<style>#wpv-expandable-test{background:blue;}</style>
-</head>
-<body>
-</body>
-</html>
