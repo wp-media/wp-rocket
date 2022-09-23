8d97fe41c011612f24c9193765d53f899c48179a
Michael Lee <michaelleemichaellee408@gmail.com>
2022-09-23 21:11:03 +0100

    Added new helper function

--- tests/e2e/utils/helpers.ts
+++ tests/e2e/utils/helpers.ts
@@ -1,8 +1,12 @@
-
 /**
  * 
  * @param err Error message.
  */
 export async function log_error(err, prefix = ''){
     console.log(prefix != '' ? prefix + ' - ' : '', err);
+}
+
+export const save_settings = async ( page ) => {
+    // save settings
+    await page.locator('#wpr-options-submit').click();
 }
