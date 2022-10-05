453f9fe47500bc1da4bfc7da1806168f159056c5
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-05 09:49:28 +0100

    use new plugin nav method

--- tests/e2e/src/common/deactivation.modal.ts
+++ tests/e2e/src/common/deactivation.modal.ts
@@ -9,22 +9,19 @@ import { pageUtils } from '../../utils/page.utils';
 export const deactivationModal = async ( page: Page ) => {
   const page_utils = new pageUtils( page );
 
-  const plugin_menu = '#menu-plugins';
-
   const locator = {
-      'plugin': page.locator( plugin_menu ),
       'deactivate': page.locator( '[aria-label="Deactivate WP Rocket"]' ),
       'safe_mode': page.locator( '#safe_mode' ),
   };
 
   await page_utils.visit_page( 'wp-admin' );
 
-  await page.waitForSelector( plugin_menu )
+  await page.waitForSelector( page_utils.selectors.plugins )
   // Expect plugins link to be in view.
-  await expect( locator.plugin ).toBeVisible();
+  await expect( page_utils.locators.plugin ).toBeVisible();
 
   // Navigate to plugins page.
-  await locator.plugin.click();
+  await page_utils.goto_plugin();
   await expect( page ).toHaveURL( WP_BASE_URL + '/wp-admin/plugins.php' );
 
   // Expect WPR to be active: Deactivate link to be visible.
