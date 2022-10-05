2129ac9f7748f1706b573f65459174a2b4fbc35d
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2022-10-05 10:27:29 +0100

    Closes #5149 - remove wpr options from not public post types (#5224)
    * Added new filter to filter post type on the submitbox
    * Updated filter argument
    * Updated subscriber event
    * Added docblock for new filters
    * Remove rocket options from posts with draft status
    * Updated doc block
    * Remove WPR options for trash post status
    * New method to check if wpr options can be displayed
    * Removed RUCSS option from non-public post types
    * New filter for clear used css
    * Updated subscriber

--- inc/admin/ui/meta-boxes.php
+++ inc/admin/ui/meta-boxes.php
@@ -8,10 +8,34 @@ defined( 'ABSPATH' ) || exit;
  * @since 1.0
  */
 function rocket_post_submitbox_start() {
+
+	if ( ! rocket_can_display_options() ) {
+		return;
+	}
+
 	if ( current_user_can( 'rocket_purge_posts' ) ) {
 		global $post;
-		$url = wp_nonce_url( admin_url( 'admin-post.php?action=purge_cache&type=post-' . $post->ID ), 'purge_cache_post-' . $post->ID );
-		printf( '<div id="purge-action"><a class="button-secondary" href="%s">%s</a></div>', esc_url( $url ), esc_html__( 'Clear cache', 'rocket' ) );
+
+		$cpts = get_post_types(
+			[
+				'public' => true,
+			],
+			'objects'
+		);
+
+		/**
+		 * Filters the post type on submitbox.
+		 *
+		 * @since 3.12.1
+		 *
+		 * @param array $cpts Post Types.
+		 */
+		$cpts = apply_filters( 'rocket_submitbox_options_post_types', $cpts );
+
+		if ( isset( $cpts[ $post->post_type ] ) ) {
+			$url = wp_nonce_url( admin_url( 'admin-post.php?action=purge_cache&type=post-' . $post->ID ), 'purge_cache_post-' . $post->ID );
+			printf( '<div id="purge-action"><a class="button-secondary" href="%s">%s</a></div>', esc_url( $url ), esc_html__( 'Clear cache', 'rocket' ) );
+		}
 	}
 }
 add_action( 'post_submitbox_start', 'rocket_post_submitbox_start' );
@@ -22,6 +46,11 @@ add_action( 'post_submitbox_start', 'rocket_post_submitbox_start' );
  * @since 2.5
  */
 function rocket_cache_options_meta_boxes() {
+
+	if ( ! rocket_can_display_options() ) {
+		return;
+	}
+
 	if ( current_user_can( 'rocket_manage_options' ) ) {
 		$cpts = get_post_types(
 			[
