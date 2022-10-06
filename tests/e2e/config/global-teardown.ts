aa786cc300e7d5366cb8c16a7ed3ed4138db5369
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-06 11:41:10 +0100

    Added global teardown

---
+++ tests/e2e/config/global-teardown.ts
@@ -0,0 +1,17 @@
+async function globalTeardown() {
+
+  // Stop dev server
+  const {execSync} = require('child_process');
+  execSync('npm run wp-env stop');
+  execSync('npm run wp-env clean all');
+
+  // Delete 'storageState.json'.
+  const fs = require('fs');
+  fs.unlink('tests/e2e/config/storageState.json', err => {
+    if (err) {
+        console.log('Unable to clear storage.')
+    }
+  });
+}
+
+export default globalTeardown;
