fa4005c2d9560bb4e37557a5456b666bd7b85459
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2022-10-07 13:47:31 +0200

    Update inc/Engine/Preload/Subscriber.php
    Co-authored-by: Ahmed Saed <eng.ahmeds3ed@gmail.com>

--- inc/Engine/Preload/Subscriber.php
+++ inc/Engine/Preload/Subscriber.php
@@ -192,7 +192,7 @@ class Subscriber implements Subscriber_Interface {
 			do_action( 'rocket_preload_completed', $url, $detected );
 		}
 
-		if ( ( isset( $_GET ) && is_array( $_GET ) ) && 0 < count( $_GET ) || ( $this->query->is_pending( $url ) && $this->options->get( 'do_caching_mobile_files', false ) ) ) { //phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		if ( ! empty( (array) $_GET ) || ( $this->query->is_pending( $url ) && $this->options->get( 'do_caching_mobile_files', false ) ) ) { //phpcs:ignore WordPress.Security.NonceVerification.Recommended
 			return;
 		}
 
