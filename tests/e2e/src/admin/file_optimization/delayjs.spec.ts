725148ef6265de5300f4c557984fee4fafa2b5a3
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-07 12:55:17 +0100

    New tests for excluding patterns and field sanitization

--- tests/e2e/src/admin/file_optimization/delayjs.spec.ts
+++ tests/e2e/src/admin/file_optimization/delayjs.spec.ts
@@ -8,14 +8,41 @@ import { save_settings } from '../../../utils/helpers';
 import { fileOptimization } from '../../common/sections/file.optimization';
 
 test.describe('Delay JS', () => {
+    test('Should not activate DelayJS automatically and not exclude any patterns for the new installs', async ( { page } ) => {
+
+        const page_utils = new pageUtils( page );
+        const fileOpt = new fileOptimization( page );
+
+        // Visit WPR settings
+        await page_utils.goto_wpr();
+
+        // Goto file optimization section
+        await fileOpt.visit();
+
+        // Assert that delayjs is not enabled.
+        expect(await page.isChecked(fileOpt.selectors.delay_js.checkbox)).toBeFalsy();
+
+        // Assert that there is no exclusion.
+        await checkForNoExclusion(page, fileOpt);
+
+        // Reload page
+        await page.reload();
+
+        // Assert that delayjs is not enabled after refresh.
+        expect(await page.isChecked(fileOpt.selectors.delay_js.checkbox)).toBeFalsy();
+
+        // Assert that there is still no exclusion after refresh.
+        await checkForNoExclusion(page, fileOpt);
+    });
+    
     test('Should not display explanatory text about default exclusion if it was already there', async ( { page } ) => {
 
         const config = {
             'default_exclusion': '/jquery-?[0-9.](.*)(.min|.slim|.slim.min)?.js\njs-(before|after)\n(?:/wp-content/|/wp-includes/)(.*)',
             'texts': {
                 'default': 'If you have problems after activating this option, copy and paste the default exclusions to quickly resolve issues:',
-                'after_exclusion': 'Internal scripts are excluded by default to prevent issues.'
-            }
+                'after_exclusion': 'Internal scripts are excluded by default to prevent issues.',
+            },
         };
     
         const page_utils = new pageUtils( page );
@@ -41,6 +68,30 @@ test.describe('Delay JS', () => {
 
         // Test with exclusions removed.
         await removeExclusion(page, config);
+    });
+
+    test('Should handle the field sanitization correctly', async ( { page } ) => {
+        const config = {
+            'exclusions': 'https://www.wp-media.me \nhttps://newer.rocketlabsqa.ovh/ \nhttps://newer.rocketlabsqa.ovh/wp-content/js.css \nc \n\n/wp-content/js.css',
+            'expected': 'https://www.wp-media.me\nhttps://newer.rocketlabsqa.ovh/\nhttps://newer.rocketlabsqa.ovh/wp-content/js.css\nc\n/wp-content/js.css',
+        };
+
+        const page_utils = new pageUtils( page );
+        const fileOpt = new fileOptimization( page );
+ 
+        // Visit WPR settings
+        await page_utils.goto_wpr();
+
+        // Goto file optimization section
+        await fileOpt.visit();
+
+        // Add new exclusions
+        await page.locator('#delay_js_exclusions').fill(config.exclusions);
+        await save_settings(page);
+
+        // Assert that empty line is removed
+        const exclusions = await page.inputValue('#delay_js_exclusions');
+        expect(exclusions).toBe(config.expected);
     });
 });
 
@@ -103,4 +154,17 @@ const removeExclusion = async (page, config) => {
 
     // Perform Assertion
     await expect(page.locator('text=' + config.texts.default)).toBeVisible();
+}
+
+/**
+ * Assert that there is no exclusion
+ * @param page Object
+ * @param fileOpt Object
+ */
+const checkForNoExclusion = async (page, fileOpt) => {
+    // Check delayjs
+    await fileOpt.toggleDelayJs();
+
+    const exclusions = await page.inputValue('#delay_js_exclusions');
+    expect(exclusions).toBe('');
 }
