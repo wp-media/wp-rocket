558e562a49ea28e9366d993ddb711aba15ec5465
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-07 13:39:22 +0100

    New test for preload

--- tests/e2e/src/admin/preload/index.spec.ts
+++ tests/e2e/src/admin/preload/index.spec.ts
@@ -6,9 +6,10 @@ import { test, expect } from '@playwright/test';
 import { preload } from '../../common/sections/preload';
 import { dashboard } from '../../common/sections/dashboard';
 import { save_settings } from '../../../utils/helpers';
+import { pageUtils } from '../../../utils/page.utils';
 
 test.describe( 'Preload', () => {
-    test('should display preload notice', async ( { page } ) => {
+    test('Should display preload trigger msg', async ( { page } ) => {
 
         await page.goto('/wp-admin/options-general.php?page=wprocket#dashboard');
 
@@ -23,6 +24,38 @@ test.describe( 'Preload', () => {
         await when_preload_enabled(page, data);
         await when_clear_cache_and_preload_admin_bar(page, data);
         await when_clear_cache_and_preload_settings(page, data);
+    });
+
+    test('Should display single option to preload and clear cache from admin bar / dashboard', async ( { page } ) => {
+        const data = {
+            'preload': new preload(page),
+            'dashboard': new dashboard(page),
+            'page_utils': new pageUtils( page ),
+        };
+
+        await data.page_utils.visit_page('wp-admin');
+        await page.locator('#wp-admin-bar-wp-rocket').hover();
+
+        // Assert that clear and preload cache option is in the admin bar dropdown.
+        await expect(page.locator('#wp-admin-bar-purge-all a:has-text("Clear and preload cache")')).toBeVisible();
+
+        await data.page_utils.goto_wpr();
+        // Assert that clear and preload cache option is on WPR Dashboard.
+        await expect(page.locator('.wpr-field a:has-text("Clear and preload cache")')).toBeVisible();
+
+        // Disable preload
+        await data.preload.visit();
+        await data.preload.togglePreload();
+        await save_settings(page);
+
+        await page.locator('#wp-admin-bar-wp-rocket').hover();
+
+        // Assert that clear cache option is in the admin bar dropdown.
+        await expect(page.locator('#wp-admin-bar-purge-all a:has-text("Clear cache")')).toBeVisible();
+
+        await data.page_utils.goto_wpr();
+        // Assert that clear cache option is on WPR Dashboard.
+        await expect(page.locator('.wpr-field a:has-text("Clear cache")')).toBeVisible();
     });
 });
 
