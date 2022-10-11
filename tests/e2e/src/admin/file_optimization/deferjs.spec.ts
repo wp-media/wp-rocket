fe18dcd0ebeb412e9913b2ba86daa25015c61aa2
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-11 10:58:48 +0100

    Added new test for deferjs

---
+++ tests/e2e/src/admin/file_optimization/deferjs.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * Local deps.
+ */
+import { pageUtils } from '../../../utils/page.utils';
+import { fileOptimization } from '../../common/sections/file.optimization';
+
+test.describe('Defer JS', () => {
+    test('Should display the Defer JS UI', async ( { page } ) => {
+
+        const page_utils = new pageUtils( page );
+        const fileOpt = new fileOptimization( page );
+
+        let txtarea = '#exclude_defer_js';
+
+        // Visit WPR settings
+        await page_utils.goto_wpr();
+
+        // Goto file optimization section
+        await fileOpt.visit();
+
+        // Assert that deferjs exclusion textarea is not visible.
+        await expect(page.locator(txtarea)).not.toBeVisible();
+
+        // Check defer js option
+        await fileOpt.toggleDeferJs();
+
+        // Assert that deferjs exclusion textarea is now visible.
+        await expect(page.locator(txtarea)).toBeVisible();
+
+        // Uncheck defer js option
+        await fileOpt.toggleDeferJs();
+
+        // Assert that deferjs exclusion textarea is no longer visible.
+        await expect(page.locator(txtarea)).not.toBeVisible();
+    });
+    
+});
