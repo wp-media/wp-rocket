8bddb4bd6ba5503cd5d06e1b627e5b060591fb9d
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-13 13:47:30 +0100

    Test for preload notice

--- tests/e2e/src/admin/a_a.rocket.license.auth.spec.ts
+++ tests/e2e/src/admin/a_a.rocket.license.auth.spec.ts
@@ -6,11 +6,22 @@ import { test, expect } from '@playwright/test';
  import { pageUtils } from '../../utils/page.utils';
 
 test.describe('Rocket License', () => {
-    test( 'should validate license if customer is key is correct', async ( { page } ) => {
+    let page;
+    
+    test.beforeAll(async ({ browser }) => {
+        const context = await browser.newContext();
+        page = await context.newPage();
 
         // Goto WPR settings.
         const page_utils = new pageUtils(page);
         await page_utils.goto_wpr();
+    });
+
+    test.afterAll(async ({ browser }) => {
+        browser.close;
+    });
+
+    test( 'should validate license if customer key is correct', async () => {
 
         const validate_btn = 'text=Validate License';
 
@@ -19,7 +30,7 @@ test.describe('Rocket License', () => {
             'has_license': page.locator( 'span:has-text("License")' )
         };
 
-        try {
+        if (await locator.validate.isVisible()) {
             await page.waitForSelector( validate_btn, { timeout: 5000 } )
             await expect( locator.validate ).toBeVisible();
             // Validate license
@@ -27,12 +38,14 @@ test.describe('Rocket License', () => {
 
             // Expect validation to be successful
             await expect(locator.has_license).toBeVisible();
-            
-        } catch (err) {
-            await expect(locator.has_license).toBeVisible();
-
-            // Should display preload trigger message on first activation.
-            await expect(page.locator('#rocket-notice-preload-processing')).toContainText('The preload service is now active');
+            return;
         }
+
+        // Expect validation to be successful
+        await expect(locator.has_license).toBeVisible();
+    });
+
+    test( 'Should display preload trigger message on first activation', async () => {
+        await expect(page.locator('#rocket-notice-preload-processing')).toContainText('The preload service is now active');
     });
 });
