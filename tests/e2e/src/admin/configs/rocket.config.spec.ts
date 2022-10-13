8b2bd972f960b64003ed90ed42dfd7bc71770688
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-13 14:26:53 +0100

    Removed advanced config test

--- tests/e2e/src/admin/configs/rocket.config.spec.ts
+++ tests/e2e/src/admin/configs/rocket.config.spec.ts
@@ -12,14 +12,3 @@ test.describe('Rocket Config File', () => {
         await page.goto('/wp-content/wp-rocket-config/localhost.php');
     });
 });
-
-test.describe('Advanced Cache File', () => {
-    test('should have advanced cache file', async ({ page }) => {
-
-        page.on('response', async (response) => {
-            expect(allowed_status).toContain(response.status());
-        });
-
-        await page.goto('/wp-content/advanced-cache.php');
-    });
-});
