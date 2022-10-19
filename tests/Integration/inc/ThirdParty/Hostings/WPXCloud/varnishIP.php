f8d61576da891c79a8163e79fbb006fbb18f3359
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-19 13:17:00 +0100

    Added tests

---
+++ tests/Integration/inc/ThirdParty/Hostings/WPXCloud/varnishIP.php
@@ -0,0 +1,26 @@
+<?php
+namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Hostings\WPXCloud;
+
+use WP_Rocket\ThirdParty\Hostings\WPXCloud;
+use WPMedia\PHPUnit\Integration\TestCase;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Hostings\WPXCloud::varnish_ip
+ * @group WPXCloud
+ */
+class Test_VarnishIP extends TestCase {
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldReturnWPXCloudVarnishIP( $varnish_ip, $expected ) {
+		$this->assertSame(
+			$expected,
+			apply_filters( 'rocket_varnish_ip', $varnish_ip )
+		);
+	}
+
+	public function providerTestData() {
+		return $this->getTestData( __DIR__, 'varnishIP' );
+	}
+}
---
+++ tests/Unit/inc/ThirdParty/Hostings/WPXCloud/varnishIP.php
@@ -0,0 +1,24 @@
+<?php
+namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Hostings\WPXCloud;
+
+use WP_Rocket\ThirdParty\Hostings\WPXCloud;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Hostings\WPXCloud::varnish_ip
+ * @group WPXCloud
+ */
+class Test_VarnishIP extends TestCase {
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnWPXCloudVarnishIP( $varnish_ip, $expected ) {
+
+		$wpx_cloud = new WPXCloud();
+
+		$this->assertSame(
+			$expected,
+			$wpx_cloud->varnish_ip( $varnish_ip )
+		);
+	}
+}
