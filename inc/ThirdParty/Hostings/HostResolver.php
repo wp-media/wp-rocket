dead0105d30073e99b874b080d3863fc67e82021
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-19 11:35:08 +0100

    Check for wpx

--- inc/ThirdParty/Hostings/HostResolver.php
+++ inc/ThirdParty/Hostings/HostResolver.php
@@ -85,6 +85,11 @@ class HostResolver {
 			return 'dreampress';
 		}
 
+		if ( isset( $_SERVER['HTTP_WPXCLOUD'] ) ) {
+			self::$hostname = 'wpxcloud';
+			return 'wpxcloud';
+		}
+
 		if ( isset( $_SERVER['X-LSCACHE'] ) ) {
 			self::$hostname = 'litespeed';
 			return 'litespeed';
