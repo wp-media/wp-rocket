40822fb2e786a97e554c410fb03d353d4103e6f8
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-21 18:49:34 +0100

    New functions for file operations

--- tests/e2e/utils/helpers.ts
+++ tests/e2e/utils/helpers.ts
@@ -1,3 +1,8 @@
+import os from 'os';
+import fs from 'fs/promises';
+
+let home_dir = os.homedir();
+
 /**
  * 
  * @param err Error message.
@@ -6,7 +11,39 @@ export async function log_error(err, prefix = ''){
     console.log(prefix != '' ? prefix + ' - ' : '', err);
 }
 
+/**
+ * 
+ * @param page Page Object.
+ */
 export const save_settings = async ( page ) => {
     // save settings
     await page.locator('#wpr-options-submit').click();
+}
+
+/**
+ * 
+ * @param file String File name.
+ * @returns String Absolute path to give file from OS.
+ */
+const get_dir = async (file: String) => {
+    let dir = (await fs.readdir(home_dir + '/.wp-env', { withFileTypes: true })).filter(dirent => dirent.isDirectory())[0].name;
+    return home_dir + '/.wp-env/' + dir + '/WordPress/' + file;
+}
+
+/**
+ * 
+ * @param file Path to file to be read.
+ * @returns String File content.
+ */
+export const read_file = async (file) => {
+    return await fs.readFile(await get_dir(file), 'utf8');
+}
+
+/**
+ * 
+ * @param file Path to file to be written.
+ * @param data Data to be written to file.
+ */
+export const write_to_file = async (file: String, data: String) => {
+    await fs.writeFile(await get_dir(file), data);
 }
