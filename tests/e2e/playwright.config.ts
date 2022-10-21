ac693bc13da86814fcea51276dad6d702ed31d14
Michael Lee <michaelleemichaellee408@gmail.com>
2022-10-21 10:06:25 +0100

    use different directory for html report

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -20,7 +20,7 @@ const config: PlaywrightTestConfig = {
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['list'],
-		['html', { outputFolder: path.join( process.cwd(), 'artifacts' ) }]
+		['html', { outputFolder: path.join( process.cwd(), 'reports' ) }]
 	],
 	outputDir: path.join( process.cwd(), 'artifacts' ),
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
