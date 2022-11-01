9c27d80eea67868bfbafb9cf65de0f3eb9591fb9
Michael Lee <michaelleemichaellee408@gmail.com>
2022-11-01 10:47:29 +0100

    Display title for varnish add-on and remove varnish auto purge button

--- inc/ThirdParty/Hostings/WPXCloud.php
+++ inc/ThirdParty/Hostings/WPXCloud.php
@@ -15,9 +15,11 @@ class WPXCloud extends AbstractNoCacheHost {
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'rocket_varnish_ip'            => 'varnish_ip',
-			'do_rocket_varnish_http_purge' => 'return_true',
-			'rocket_htaccess_mod_expires'  => [ 'remove_htaccess_html_expire', 5 ],
+			'rocket_varnish_ip'                       => 'varnish_ip',
+			'rocket_display_input_varnish_auto_purge' => 'return_false',
+			'do_rocket_varnish_http_purge'            => 'return_true',
+			'rocket_varnish_field_settings'           => 'varnish_addon_title',
+			'rocket_htaccess_mod_expires'             => [ 'remove_htaccess_html_expire', 5 ],
 		];
 	}
 
@@ -48,5 +50,21 @@ class WPXCloud extends AbstractNoCacheHost {
 		$rules = preg_replace( '@\s*ExpiresByType text/html\s*"access plus \d+ (seconds|minutes|hour|week|month|year)"@', '', $rules );
 
 		return $rules;
+	}
+
+	/**
+	 * Displays custom title for the Varnish add-on
+	 *
+	 * @param array $settings Array of settings for Varnish.
+	 * @return array
+	 */
+	public function varnish_addon_title( array $settings ) {
+		$settings['varnish_auto_purge']['title'] = sprintf(
+			// Translators: %s = Hosting name.
+			__( 'Your site is hosted on %s, we have enabled Varnish auto-purge for compatibility.', 'rocket' ),
+			'WPX'
+		);
+
+		return $settings;
 	}
 }
