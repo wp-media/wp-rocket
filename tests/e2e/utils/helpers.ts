8fe5b700a7000e3febffa89ee2543ba5282426af
Michael Lee <michaelleemichaellee408@gmail.com>
2022-11-02 11:14:31 +0100

    Change command

--- tests/e2e/utils/helpers.ts
+++ tests/e2e/utils/helpers.ts
@@ -41,8 +41,8 @@ const get_dir = async (file: String) => {
     let dir;
     dir = (await fs.readdir(home_dir + '/' + install_path, { withFileTypes: true })).filter(dirent => dirent.isDirectory())[0].name;
     dir = home_dir + '/' + install_path + '/' + dir + '/WordPress/' + file;
-    await fs.chmod(dir, 0o600);
-    
+    await fs.chmod(dir, 0o755);
+
     return dir;
 }
 
