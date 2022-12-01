ac37a2d8707caf54ad9c4ac49613cfe94b1cb6a5
Michael Lee <michaelleemichaellee408@gmail.com>
2022-12-01 11:14:21 +0100

    Update to match with permalink structure

--- inc/Engine/Cache/Config/ConfigSubscriber.php
+++ inc/Engine/Cache/Config/ConfigSubscriber.php
@@ -15,8 +15,28 @@ class ConfigSubscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events() {
 		return [
-			'permalink_structure_changed' => 'regenerate_config_file',
+			'permalink_structure_changed'        => 'regenerate_config_file',
+			'get_rocket_option_cache_reject_uri' => 'match_pattern_with_permalink_structure',
 		];
+	}
+
+	/**
+	 * Returns a matching user added patterns with permalink structure
+	 *
+	 * @param array $patterns user pattern.
+	 * @return array
+	 */
+	public function match_pattern_with_permalink_structure( array $patterns ): array {
+		if ( empty( $patterns ) ) {
+			return $patterns;
+		}
+
+		return array_map(
+		function ( $uri ) {
+			return user_trailingslashit( $uri );
+		},
+		$patterns
+		);
 	}
 
 	/**
