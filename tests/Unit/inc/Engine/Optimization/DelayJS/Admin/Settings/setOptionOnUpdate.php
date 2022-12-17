f0ce07711da3f6f1433876007a5355b72c955959
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2022-12-17 17:48:41 -0500

    fix unit tests again :D

--- tests/Unit/inc/Engine/Optimization/DelayJS/Admin/Settings/setOptionOnUpdate.php
+++ tests/Unit/inc/Engine/Optimization/DelayJS/Admin/Settings/setOptionOnUpdate.php
@@ -4,7 +4,7 @@ namespace WP_Rocket\Tests\Unit\inc\Engine\Optimization\DelayJS\Admin\Settings;
 
 use Brain\Monkey\Functions;
 use Mockery;
-use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Admin\Options;
 use WP_Rocket\Engine\Optimization\DelayJS\Admin\Settings;
 use WP_Rocket\Tests\Unit\TestCase;
 
@@ -21,7 +21,7 @@ class Test_SetOptionOnUpdate extends TestCase {
 	 * @dataProvider configTestData
 	 */
 	public function testShouldDoExpected( $options, $old_version, $valid_version, $expected ) {
-		$this->option = Mockery::mock(Options_Data::class);
+		$this->option = Mockery::mock(Options::class);
 		$settings = new Settings( $this->option );
 
 		if ( $valid_version ) {
@@ -29,11 +29,9 @@ class Test_SetOptionOnUpdate extends TestCase {
 			$this->option->shouldReceive('get')->zeroOrMoreTimes()->andReturn($options);
 			Functions\when( 'content_url' )->justReturn( 'http://example.org/wp-content' );
 			Functions\when( 'includes_url' )->justReturn( 'http://example.org/wp-includes' );
-			Functions\expect( 'update_option' )
-				->with( 'wp_rocket_settings', $expected )
-				->once();
+			$this->option->shouldReceive('set')->with( 'settings', $expected )->once();
 		} else {
-			Functions\expect( 'update_option' )->never();
+			$this->option->shouldReceive('set')->never();
 		}
 
 		$settings->set_option_on_update( $old_version );
