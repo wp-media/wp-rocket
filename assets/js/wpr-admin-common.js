35e8e1907c8eba975884c86febd6d42a50197409
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2022-12-19 08:45:33 +0100

    Feature 5094: export settings when activating safe mode (#5607)
    * Added export logic
    * Added logic to create export options
    * Fixed code standards
    * Fixed the logic to send the request to export options
    * Fixed code standards
    * Fixed code standards
    * Fixed js error
    * Added JS variable
    * Fixed QA review
    * Fixed strings
    * Fixed strings

--- assets/js/wpr-admin-common.js
+++ assets/js/wpr-admin-common.js
@@ -4,4 +4,62 @@ jQuery( document ).ready( function( $ ){
 		var url = $( this ).attr( 'href' ).replace( 'admin-post', 'admin-ajax' );
 		$.get( url ).done( $( this ).closest( '.notice' ).hide( 'slow' ) );
 	});
+
+	$( '#deactivate' ).click( function() {
+		$( '#export_settings' ).prop( 'checked', false );
+		$( '#export_settings' ).hide();
+		$( 'label[for=export_settings]' ).hide();
+	});
+
+	$( '#safe_mode' ).click( function() {
+		$( '#export_settings' ).show();
+		$( 'label[for=export_settings]' ).show();
+		$( '#export_settings' ).prop( 'checked', true );
+	});
+
+	$( '#wpr-deactivation-intent-form' ).submit(function (e) {
+		const checked = $( '#export_settings' ).prop('checked');
+		if(! checked) {
+			return true;
+		}
+
+		e.preventDefault();
+
+		$.ajax( {
+			url: rocket_option_export.rest_url_option_export,
+			method: 'GET',
+			beforeSend: function ( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce', jQuery('#wpr-deactivation-intent-form input[name="rest_auth_nonce"]').val() );
+			},
+			success: function( data, textStatus, xhr ) {
+				const disposition = xhr.getResponseHeader('content-disposition');
+
+				const filenames = disposition.match('filename="([^"]+)"');
+
+				if(! filenames.length) {
+					return;
+				}
+
+				const filename = filenames.pop();
+
+				const url = URL.createObjectURL( new Blob( [ JSON.stringify(data, null, 2) ], {
+					type: "octet/stream"
+				}));
+
+				var a = document.createElement("a");
+				document.body.appendChild(a);
+				a.style = "display: none";
+				a.href = url;
+				a.download = filename;
+				a.click();
+				window.URL.revokeObjectURL(url);
+			},
+			complete: function () {
+				$( '#export_settings' ).prop('checked', false);
+				$( '#wpr-deactivation-intent-form' ).submit();
+			}
+		} );
+
+		return true;
+	});
 } );
