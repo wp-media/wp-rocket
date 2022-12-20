42fd670cd263575006fa33c4134c4f9621f1d3e1
COQUARD Cyrille <cyrille@wp-media.me>
2022-12-20 14:50:04 +0100

    Updated prices

--- inc/Engine/License/Renewal.php
+++ inc/Engine/License/Renewal.php
@@ -68,6 +68,29 @@ class Renewal extends Abstract_Render {
 
 		$data              = $this->get_banner_data();
 		$data['countdown'] = $this->get_countdown_data();
+		$discount  = esc_html( '$' . number_format_i18n( $this->get_discount_percent(), 2 ) );
+		$price     = esc_html( '$' . number_format_i18n( $this->get_price(), 2 ) );
+
+
+
+		if( $this->get_discount_percent() ) {
+			$data['message'] = sprintf(
+				esc_html__('Renew with a %1$s%2$s discount%3$s before it is too late, you will only pay %4$s%5$s%6$s!', "rocket"),
+				'<strong>',
+				$discount,
+				'</strong>',
+				'<strong>',
+				$price,
+				'</strong>',
+			);
+		} else {
+			$data['message'] = sprintf(
+				esc_html__('Renew before it is too late, you will only pay %1$s%2$s%3$s!', "rocket"),
+				'<strong>',
+				$price,
+				'</strong>'
+			);
+		}
 
 		echo $this->generate( 'renewal-soon-banner', $data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
@@ -129,7 +152,7 @@ class Renewal extends Abstract_Render {
 				esc_html__( 'Renew your license for 1 year now and get %1$s%3$s OFF%2$s immediately: you will only pay %1$s%4$s%2$s!', 'rocket' ),
 				'<strong>',
 				'</strong>',
-				esc_html( $this->get_discount_percent() . '%' ),
+				esc_html( '$' . number_format_i18n( $this->get_discount_percent(), 2 ) ),
 				$price
 			);
 		}
@@ -203,7 +226,7 @@ class Renewal extends Abstract_Render {
 				// translators: %1$s = <strong>, %2$s = discount percentage, %3$s = </strong>, %4$s = discount price.
 				esc_html__( 'Renew with a %1$s%2$s discount%3$s before it is too late, you will only pay %1$s%4$s%3$s!', 'rocket' ),
 				'<strong>',
-				esc_html( $this->get_discount_percent() . '%' ),
+				esc_html( '$' . number_format_i18n( $this->get_discount_percent(), 2 ) ),
 				'</strong>',
 				$price
 			);
@@ -284,24 +307,32 @@ class Renewal extends Abstract_Render {
 	}
 
 	/**
-	 * Gets the discount percentage corresponding to the current user status
+	 * Gets the discount corresponding to the current user status
 	 *
 	 * @since 3.7.5
 	 *
 	 * @return int
 	 */
 	private function get_discount_percent() {
+		$prices = $this->get_license_pricing_data();
+
 		$renewals = $this->get_user_renewal_status();
 
-		if ( false === $renewals ) {
+		if ( false === $renewals || ! isset($prices->prices, $prices->prices->renewal ) ) {
 			return 0;
 		}
 
+		$prices = $prices->prices;
+
 		if ( $renewals['is_grandfather'] ) {
-			return isset( $renewals['discount_percent']->is_grandfather ) ? $renewals['discount_percent']->is_grandfather : 0;
+			return isset( $prices->renewal->is_grandfather, $prices->renewal->not_grandfather ) ? $prices->renewal->not_grandfather - $prices->renewal->is_grandfather : 0;
 		}
 
-		return isset( $renewals['discount_percent']->not_grandfather ) ? $renewals['discount_percent']->not_grandfather : 0;
+		if ( $renewals['is_grandmother'] ) {
+			return isset( $prices->renewal->is_grandmother, $prices->renewal->not_grandfather ) ? $prices->renewal->not_grandfather - $prices->renewal->is_grandmother : 0;
+		}
+
+		return 0;
 	}
 
 	/**
@@ -313,6 +344,16 @@ class Renewal extends Abstract_Render {
 		$renewals = $this->get_user_renewal_status();
 
 		return $renewals['is_grandfather'];
+	}
+	/**
+	 * Is user grandmothered
+	 *
+	 * @return bool
+	 */
+	private function has_grandmother(): bool {
+		$renewals = $this->get_user_renewal_status();
+
+		return key_exists('is_grandmother', $renewals ) && $renewals['is_grandmother'];
 	}
 
 	/**
@@ -339,6 +380,10 @@ class Renewal extends Abstract_Render {
 			return isset( $license->prices->renewal->is_grandfather ) ? $license->prices->renewal->is_grandfather : 0;
 		}
 
+		if($renewals['is_grandmother']) {
+			return isset( $license->prices->renewal->is_grandmother ) ? $license->prices->renewal->is_grandmother : 0;
+		}
+
 		return isset( $license->prices->renewal->not_grandfather ) ? $license->prices->renewal->not_grandfather : 0;
 	}
 
@@ -352,7 +397,7 @@ class Renewal extends Abstract_Render {
 	private function get_user_renewal_status() {
 		$renewals = $this->pricing->get_renewals_data();
 
-		if ( ! isset( $renewals->extra_days, $renewals->grandfather_date, $renewals->discount_percent ) ) {
+		if ( ! isset( $renewals->extra_days, $renewals->grandfather_date, $renewals->discount_percent, $renewals->grandmother_date ) ) {
 			return false;
 		}
 
@@ -360,6 +405,7 @@ class Renewal extends Abstract_Render {
 			'discount_percent' => $renewals->discount_percent,
 			'is_expired'       => time() > ( $this->user->get_license_expiration() + ( $renewals->extra_days * DAY_IN_SECONDS ) ),
 			'is_grandfather'   => $renewals->grandfather_date > $this->user->get_creation_date(),
+			'is_grandmother'   => $renewals->grandmother_date > $this->user->get_creation_date(),
 		];
 	}
 
--- tests/Fixtures/inc/Engine/License/Renewal/displayRenewalExpiredBanner.php
+++ tests/Fixtures/inc/Engine/License/Renewal/displayRenewalExpiredBanner.php
@@ -5,8 +5,9 @@ $pricing = [
 		'prices'=> [
 			'renewal'=> [
 				'is_grandfather'=> 39.2,
-				'not_grandfather'=> 49,
-				'is_expired'=> 49
+				'is_grandmother'=> 49,
+				'not_grandfather'=> 59,
+				'is_expired'=> 59
 			]
 		],
 		'websites'=> 1
@@ -15,8 +16,9 @@ $pricing = [
 		'prices'=> [
 			'renewal'=> [
 				'is_grandfather'=> 79.2,
-				'not_grandfather'=> 99,
-				'is_expired'=> 99
+				'is_grandmother'=> 99,
+				'not_grandfather'=> 119,
+				'is_expired'=> 119
 			]
 		],
 		'websites'=> 3
@@ -25,14 +27,16 @@ $pricing = [
 		'prices'=> [
 			'renewal'=> [
 				'is_grandfather'=> 199.2,
-				'not_grandfather'=> 249,
-				'is_expired'=> 249
+				'is_grandmother'=> 249,
+				'not_grandfather'=> 299,
+				'is_expired'=> 299
 			]
 		],
 	] ) ),
 	'renewals' => json_decode( json_encode( [
 		'extra_days'=> 15,
 		'grandfather_date'=> 1640995200,
+		'grandmother_date'=> 1672531200,
 		'discount_percent'=> [
 			'is_grandfather' => 20,
 			'not_grandfather'=> 0,
@@ -91,7 +95,7 @@ return [
 			'template' => 'renewal-expired-banner-ocd',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>$19.8 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 				'disabled_date' => strtotime( 'now + 5 days' ),
 			],
 		],
@@ -202,7 +206,7 @@ return [
 			'template' => 'renewal-expired-banner-ocd',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>$19.8 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 				'disabled_date' => strtotime( 'now + 5 days' ),
 			],
 		],
@@ -296,7 +300,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>$19.8 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 			],
 		],
 	],
@@ -382,7 +386,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>$19.8 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 			],
 		],
 	],
