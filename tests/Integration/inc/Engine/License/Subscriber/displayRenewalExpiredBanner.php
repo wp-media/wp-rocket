7a0300e28545f0bc951ca9322ed4b0f26fbbe643
COQUARD Cyrille <cyrille@wp-media.me>
2022-12-21 13:51:30 +0100

    Updated integration tests

--- tests/Integration/inc/Engine/License/Subscriber/displayRenewalExpiredBanner.php
+++ tests/Integration/inc/Engine/License/Subscriber/displayRenewalExpiredBanner.php
@@ -12,7 +12,9 @@ use WP_Rocket\Tests\Integration\TestCase;
  */
 class DisplayRenewalExpiredBanner extends TestCase {
 	private static $user;
+	private static $pricing;
 	private $original_user;
+	private $original_pricing;
 	private static $user_id = 0;
 
 	public static function wpSetUpBeforeClass( $factory ) {
@@ -24,6 +26,7 @@ class DisplayRenewalExpiredBanner extends TestCase {
 
 		$container  = apply_filters( 'rocket_container', null );
 		self::$user = $container->get( 'user' );
+		self::$pricing = $container->get( 'pricing' );
 	}
 
 	public function set_up() {
@@ -34,12 +37,14 @@ class DisplayRenewalExpiredBanner extends TestCase {
 		wp_set_current_user( self::$user_id );
 
 		$this->original_user = $this->getNonPublicPropertyValue( 'user', self::$user, self::$user );
+		$this->original_pricing = $this->getNonPublicPropertyValue( 'pricing', self::$pricing, self::$pricing );
 	}
 
 	public function tear_down() {
 		$this->restoreWpFilter( 'rocket_before_dashboard_content' );
 
 		$this->set_reflective_property( $this->original_user, 'user', self::$user );
+		$this->set_reflective_property( $this->original_pricing, 'pricing', self::$pricing );
 
 		delete_transient( 'rocket_renewal_banner_' . self::$user_id );
 
@@ -51,6 +56,7 @@ class DisplayRenewalExpiredBanner extends TestCase {
 	 */
 	public function testShouldReturnExpected( $config, $expected ) {
 		$this->set_reflective_property( $config['user'], 'user', self::$user );
+		$this->set_reflective_property( $config['pricing'], 'pricing', self::$pricing );
 
 		if ( false !== $config['transient'] ) {
 			set_transient( 'rocket_renewal_banner_' . self::$user_id, 1, MINUTE_IN_SECONDS );
