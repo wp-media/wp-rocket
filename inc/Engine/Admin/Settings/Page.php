fe514ffca09c8cdbca265fa9fc2a9e75a0e0fff1
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2023-01-03 07:19:05 -0700

    remove not used code

--- inc/Engine/Admin/Settings/Page.php
+++ inc/Engine/Admin/Settings/Page.php
@@ -616,20 +616,6 @@ class Page {
 			]
 		);
 
-		$delay_js_list_helper = sprintf(
-			// translators: %1$s = exclusion list, %2$s = opening </a> tag, %3$s = closing </a> tag.
-			__( 'If you have problems after activating this option, copy and paste the default exclusions to quickly resolve issues:<br><pre><code>%1$s</code></pre><br>Also, please check our %2$sdocumentation%3$s for a list of compatibility exclusions.', 'rocket' ),
-			implode( '<br>', DelayJSSettings::get_delay_js_default_exclusions() ),
-			'<a href="' . esc_url( $delay_js_exclusions_beacon['url'] ) . '"  target="_blank" rel="noopener">',
-			'</a>'
-		);
-		$delay_js_found_list_helper = sprintf(
-			// translators: %1$s = opening </a> tag, %2$s = closing </a> tag.
-			__( 'Internal scripts are excluded by default to prevent issues. Remove them to take full advantage of this option.<br>If this causes trouble, restore the default exclusions, found %1$shere%2$s', 'rocket' ),
-			'<a href="' . esc_url( $delay_js_beacon['url'] ) . '"  target="_blank" rel="noopener">',
-			'</a>'
-		);
-
 		$this->settings->add_settings_fields(
 			[
 				'minify_css'                   => [
@@ -943,7 +929,6 @@ class Page {
 					'input_attr'        => [
 						'disabled' => get_rocket_option( 'delay_js' ) ? 0 : 1,
 					],
-					// 'helper'            => DelayJSSettings::exclusion_list_has_default() ? $delay_js_found_list_helper : $delay_js_list_helper,
 					'placeholder'       => '/wp-includes/js/jquery/jquery.min.js',
 				],
 			]
