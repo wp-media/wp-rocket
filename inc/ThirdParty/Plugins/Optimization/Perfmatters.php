f80f2efcf12c8aa8e2efcd0be2828732c918a273
Michael Lee <michaelleemichaellee408@gmail.com>
2023-01-10 15:07:04 +0100

    Disable RUCSS when perfmatters is enabled

--- inc/ThirdParty/Plugins/Optimization/Perfmatters.php
+++ inc/ThirdParty/Plugins/Optimization/Perfmatters.php
@@ -17,25 +17,50 @@ class Perfmatters implements Subscriber_Interface {
 		}
 
 		return [
-			'rocket_maybe_disable_rucss' => 'disable_rucss_option',
+			'rocket_maybe_disable_rucss'              => 'disable_rucss_option',
+			'pre_get_rocket_option_remove_unused_css' => 'maybe_disable_rucss',
 		];
 	}
 
 	/**
 	 * Disable RUCSS option.
 	 *
+	 * @param array $status RUCSS option status.
 	 * @return array
 	 */
-	public function disable_rucss_option(): array {
-		$perfmatters_options = get_option( 'perfmatters_options' );
-
-		if ( empty( $perfmatters_options['assets']['remove_unused_css'] ) ) {
-			return [];
+	public function disable_rucss_option( array $status ): array {
+		if ( ! $this->is_perfmatters_rucss_active() ) {
+			return $status;
 		}
 
 		return [
 			'disable' => true,
 			'text'    => __( 'Removing Unused CSS is currently activated in Perfmatters. If you want to use WP Rocket\'s Remove Unused CSS feature, disable this option in Perfmatters.', 'rocket' ),
 		];
+	}
+
+	/**
+	 * Disable RUCSS option.
+	 *
+	 * @param integer|null $rucss RUCSS option.
+	 * @return integer|null
+	 */
+	public function maybe_disable_rucss( ?int $rucss ) {
+		return $this->is_perfmatters_rucss_active() ? 0 : $rucss;
+	}
+
+	/**
+	 * Check if perfmatters rucss is active.
+	 *
+	 * @return boolean
+	 */
+	private function is_perfmatters_rucss_active(): bool {
+		$perfmatters_options = get_option( 'perfmatters_options' );
+
+		if ( ! empty( $perfmatters_options['assets']['remove_unused_css'] ) ) {
+			return true;
+		}
+
+		return false;
 	}
 }
