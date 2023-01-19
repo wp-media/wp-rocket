e343c8de10d7822cede2c328441e90b49a066678
Michael Lee <michaelleemichaellee408@gmail.com>
2023-01-19 11:56:05 +0100

    Updated tests

--- tests/Fixtures/inc/ThirdParty/Hostings/WPXCloud/appendCacheControlHeader.php
+++ tests/Fixtures/inc/ThirdParty/Hostings/WPXCloud/appendCacheControlHeader.php
@@ -1,79 +1,11 @@
 <?php
+$rule = '<IfModule mod_headers.c>
+Header append Cache-Control " s-maxage=3600, stale-while-revalidate=21600" "expr=%{CONTENT_TYPE} =~ m#text/html#"
+</IfModule>
+';
+
 return [
 	'testShouldChangeUpdateHtaccessRules' => [
-		'htaccess_rules' => '<ifmodule mod_expires.c="">
-	ExpiresActive on
-	ExpiresDefault                              "access plus 1 month"
-	# cache.appcache needs re-requests in FF 3.6 (thanks Remy ~Introducing HTML5)
-	ExpiresByType text/cache-manifest           "access plus 0 seconds"
-	# Your document html
-	ExpiresByType text/html                     "access plus 0 seconds"
-	# Data
-	ExpiresByType text/xml                      "access plus 0 seconds"
-	ExpiresByType application/xml               "access plus 0 seconds"
-	ExpiresByType application/json              "access plus 0 seconds"
-	# Feed
-	ExpiresByType application/rss+xml           "access plus 1 hour"
-	ExpiresByType application/atom+xml          "access plus 1 hour"
-	# Favicon (cannot be renamed)
-	ExpiresByType image/x-icon                  "access plus 1 week"
-	# Media: images, video, audio
-	ExpiresByType image/gif                     "access plus 4 months"
-	ExpiresByType image/png                     "access plus 4 months"
-	ExpiresByType image/jpeg                    "access plus 4 months"
-	ExpiresByType image/webp                    "access plus 4 months"
-	ExpiresByType video/ogg                     "access plus 4 months"
-	ExpiresByType audio/ogg                     "access plus 4 months"
-	ExpiresByType video/mp4                     "access plus 4 months"
-	ExpiresByType video/webm                    "access plus 4 months"
-	# HTC files  (css3pie)
-	ExpiresByType text/x-component              "access plus 1 month"
-	# Webfonts
-	ExpiresByType font/ttf                      "access plus 4 months"
-	ExpiresByType font/otf                      "access plus 4 months"
-	ExpiresByType font/woff                     "access plus 4 months"
-	ExpiresByType font/woff2                    "access plus 4 months"
-	ExpiresByType image/svg+xml                 "access plus 1 month"
-	ExpiresByType application/vnd.ms-fontobject "access plus 1 month"
-	# CSS and JavaScript
-	ExpiresByType text/css                      "access plus 1 year"
-	ExpiresByType application/javascript        "access plus 1 year"
-</ifmodule>',
-		'expected' => '<ifmodule mod_expires.c="">
-	ExpiresActive on
-	ExpiresDefault                              "access plus 1 month"
-	# cache.appcache needs re-requests in FF 3.6 (thanks Remy ~Introducing HTML5)
-	ExpiresByType text/cache-manifest           "access plus 0 seconds"
-	# Data
-	ExpiresByType text/xml                      "access plus 0 seconds"
-	ExpiresByType application/xml               "access plus 0 seconds"
-	ExpiresByType application/json              "access plus 0 seconds"
-	# Feed
-	ExpiresByType application/rss+xml           "access plus 1 hour"
-	ExpiresByType application/atom+xml          "access plus 1 hour"
-	# Favicon (cannot be renamed)
-	ExpiresByType image/x-icon                  "access plus 1 week"
-	# Media: images, video, audio
-	ExpiresByType image/gif                     "access plus 4 months"
-	ExpiresByType image/png                     "access plus 4 months"
-	ExpiresByType image/jpeg                    "access plus 4 months"
-	ExpiresByType image/webp                    "access plus 4 months"
-	ExpiresByType video/ogg                     "access plus 4 months"
-	ExpiresByType audio/ogg                     "access plus 4 months"
-	ExpiresByType video/mp4                     "access plus 4 months"
-	ExpiresByType video/webm                    "access plus 4 months"
-	# HTC files  (css3pie)
-	ExpiresByType text/x-component              "access plus 1 month"
-	# Webfonts
-	ExpiresByType font/ttf                      "access plus 4 months"
-	ExpiresByType font/otf                      "access plus 4 months"
-	ExpiresByType font/woff                     "access plus 4 months"
-	ExpiresByType font/woff2                    "access plus 4 months"
-	ExpiresByType image/svg+xml                 "access plus 1 month"
-	ExpiresByType application/vnd.ms-fontobject "access plus 1 month"
-	# CSS and JavaScript
-	ExpiresByType text/css                      "access plus 1 year"
-	ExpiresByType application/javascript        "access plus 1 year"
-</ifmodule>',
+		'expected' => $rule,
 	],
 ];
--- tests/Integration/inc/ThirdParty/Hostings/WPXCloud/appendCacheControlHeader.php
+++ tests/Integration/inc/ThirdParty/Hostings/WPXCloud/appendCacheControlHeader.php
@@ -4,7 +4,7 @@ namespace WP_Rocket\Tests\Integration\inc\ThirdParty\Hostings\WPXCloud;
 use WP_Rocket\Tests\Integration\TestCase;
 
 /**
- * @covers \WP_Rocket\ThirdParty\Hostings\WPXCloud::remove_htaccess_html_expire
+ * @covers \WP_Rocket\ThirdParty\Hostings\WPXCloud::append_cache_control_header
  *
  * @group  WPXCloud
  */
@@ -12,10 +12,10 @@ class Test_RemoveHtaccessHtmlExpire extends TestCase {
 	/**
 	 * @dataProvider configTestData
 	 */
-	public function testShouldDoExpected( $htaccess_rules, $expected ) {
+	public function testShouldDoExpected( $expected ) {
 		$this->assertSame(
 			$expected,
-			apply_filters( 'rocket_htaccess_mod_expires', $htaccess_rules )
+			apply_filters( 'after_rocket_htaccess_rules', '' )
 		);
 	}
 }
