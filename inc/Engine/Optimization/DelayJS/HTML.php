3426f65dfb2612db6ce20cc843506e00a615cda2
mostafa <mostafa.hisham.mahmoud@gmail.com>
2023-01-23 12:41:35 +0200

    keep src don't change it to data-rocket-src (test)

--- inc/Engine/Optimization/DelayJS/HTML.php
+++ inc/Engine/Optimization/DelayJS/HTML.php
@@ -218,9 +218,9 @@ class HTML {
 		// To remove type attribute without any value.
 		$matches['attr'] = preg_replace( '/(\s+type\s+)|(^type\s+)|(\s+type$)/i', '', $matches['attr'] );
 
-		// Checks if script has src attribute so then treat as external script and replace src with data-rocket-src.
+		/*// Checks if script has src attribute so then treat as external script and replace src with data-rocket-src.
 		$src_regex       = '/src\s*=\s*(["\'])(.*)\1/i';
-		$matches['attr'] = preg_replace( $src_regex, 'data-rocket-src=$1$2$1', $matches['attr'] );
+		$matches['attr'] = preg_replace( $src_regex, 'data-rocket-src=$1$2$1', $matches['attr'] );*/
 
 		return '<script type="rocketlazyloadscript" ' . trim( $matches['attr'] ) . '>' . $matches['content'] . '</script>';
 	}
