6d4fc020c889b79f11082c079db39ad3a48b134c
Michael Lee <michaelleemichaellee408@gmail.com>
2023-01-24 10:14:21 +0100

    Update status text

--- inc/ThirdParty/Plugins/Optimization/RapidLoad.php
+++ inc/ThirdParty/Plugins/Optimization/RapidLoad.php
@@ -36,7 +36,7 @@ class RapidLoad implements Subscriber_Interface {
 
 		return [
 			'disable' => true,
-			'text'    => __( 'Automated unused CSS removal is currently activated in RapidLoad Power-Up for Autoptimize. If you want to use WP Rocket\'s Remove Unused CSS feature, disable this option in RapidLoad Power-Up for Autoptimize.', 'rocket' ),
+			'text'    => __( 'Automated unused CSS removal is currently activated in RapidLoad Power-Up for Autoptimize. If you want to use WP Rocket\'s Remove Unused CSS feature, disable the  RapidLoad Power-Up for Autoptimize plugin.', 'rocket' ),
 		];
 	}
 
