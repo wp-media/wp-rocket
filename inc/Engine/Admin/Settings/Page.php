69888463a1b5ea4c63d587b7d9afe4b8ee75c1b8
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2023-02-01 06:36:06 -0700

    change my scripts text

--- inc/Engine/Admin/Settings/Page.php
+++ inc/Engine/Admin/Settings/Page.php
@@ -931,7 +931,7 @@ class Page {
 				],
 				'delay_js_exclusions'          => [
 					'type'              => 'textarea',
-					'label'             => __( 'My Scripts', 'rocket' ),
+					'label'             => __( 'Excluded JavaScript Files', 'rocket' ),
 					'description'       => __( 'Specify URLs or keywords that can identify inline or JavaScript files to be excluded from delaying execution (one per line).', 'rocket' ),
 					'container_class'   => [
 						'wpr-field--children',
