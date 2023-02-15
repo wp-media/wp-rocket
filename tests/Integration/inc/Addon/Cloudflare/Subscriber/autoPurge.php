46842b104a0441e73734b1db36fabe06791acbda
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-02-15 18:29:28 +0100

    Added a potential fix

--- tests/Integration/inc/Addon/Cloudflare/Subscriber/autoPurge.php
+++ tests/Integration/inc/Addon/Cloudflare/Subscriber/autoPurge.php
@@ -4,6 +4,7 @@ namespace WP_Rocket\Tests\Integration\inc\Addons\Cloudflare\Subscriber;
 
 use Brain\Monkey\Functions;
 use Exception;
+use WP_Rocket\Tests\Integration\ActionTrait;
 
 /**
  * @covers \WPMedia\Cloudflare\Subscriber::auto_purge
@@ -11,46 +12,56 @@ use Exception;
  * @group  Addons
  */
 class Test_AutoPurge extends TestCase {
-
+	use ActionTrait;
 	public static function set_up_before_class() {
 		parent::set_up_before_class();
 		self::setApiCredentialsInOptionsWithFilter();
 	}
 
+	public function set_up()
+	{
+		parent::set_up();
+		$this->unregisterAllCallbacksActionsExcept('after_rocket_clean_domain', 'auto_purge');
+	}
+
+	public function tear_down()
+	{
+		$this->restoreWpAction('after_rocket_clean_domain');
+		parent::tear_down();
+	}
+
 	public function testShouldBailoutWhenUserCantPurgeCF() {
-		var_dump('start_cant');
 
 		$user = $this->factory->user->create( [ 'role' => 'contributor' ] );
 		wp_set_current_user( $user );
-		Functions\when('is_wp_error')->alias(function ($t) {
+		/*Functions\when('is_wp_error')->alias(function ($t) {
+			var_dump($t);
+			var_dump($t);
 			var_dump($t);
 			return false;
-		});
-		///Functions\expect( 'is_wp_error' )->never();
+		});*/
+		Functions\expect( 'is_wp_error' )->never();
 
 		do_action( 'after_rocket_clean_domain' );
-		var_dump('end_cant');
 	}
 
 	public function testShouldBailoutWhenNoPageRule() {
-	var_dump('start');
 		$admin = get_role( 'administrator' );
 		$admin->add_cap( 'rocket_purge_cloudflare_cache' );
 		$user = $this->factory->user->create( [ 'role' => 'administrator' ] );
 		wp_set_current_user( $user );
-		Functions\when('is_wp_error')->alias(function ($t) {
+		/*Functions\when('is_wp_error')->alias(function ($t) {
 			var_dump($t);
 			return false;
-		});
-		/*Functions\expect( 'is_wp_error' )
+		});*/
+		Functions\expect( 'is_wp_error' )
 			->ordered()
 			->once()
 			->with( null )
 			->andAlsoExpectIt()
 			->once()
-			->with( 0 );*/
+			->with( 0 );
 
 		do_action( 'after_rocket_clean_domain' );
-		var_dump('end');
 	}
 }
