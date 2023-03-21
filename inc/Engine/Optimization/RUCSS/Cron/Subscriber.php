7372323986e00a3e2fa90d2066170ef624bb537d
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-21 15:37:32 +0100

    Move filter docblock

--- inc/Engine/Optimization/RUCSS/Cron/Subscriber.php
+++ inc/Engine/Optimization/RUCSS/Cron/Subscriber.php
@@ -160,14 +160,14 @@ class Subscriber implements Subscriber_Interface {
 			'display'  => esc_html__( 'WP Rocket Remove Unused CSS pending jobs', 'rocket' ),
 		];
 
+		$default_interval = 3 * rocket_get_constant( 'DAY_IN_SECONDS', 86400 );
 		/**
 		 * Filters the cron interval for clearing failed jobs.
 		 *
 		 * @param int $interval Interval in seconds.
 		 */
-		$default_interval = 3 * rocket_get_constant( 'DAY_IN_SECONDS', 86400 );
-		$interval         = apply_filters( 'rocket_remove_rucss_failed_jobs_cron_interval', $default_interval );
-		$interval         = (bool) $interval ? $interval : $default_interval;
+		$interval = apply_filters( 'rocket_remove_rucss_failed_jobs_cron_interval', $default_interval );
+		$interval = (bool) $interval ? $interval : $default_interval;
 
 		$schedules['rocket_remove_rucss_failed_jobs'] = [
 			'interval' => $interval,
