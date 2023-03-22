f8b378cea346a148a7cec5299e2fc9e53cb70496
Rémy Perona <remperona@gmail.com>
2023-03-22 18:20:15 -0400

    update method name

--- inc/Addon/Cloudflare/Admin/Subscriber.php
+++ inc/Addon/Cloudflare/Admin/Subscriber.php
@@ -13,7 +13,7 @@ class Subscriber implements Subscriber_Interface {
 		return [
 			'admin_notices' => [
 				[ 'maybe_display_purge_notice' ],
-				[ 'maybe_print_update_settings_notice' ],
+				[ 'maybe_display_update_settings_notice' ],
 			],
 		];
 	}
@@ -46,7 +46,7 @@ class Subscriber implements Subscriber_Interface {
 	/**
 	 * This notice is displayed after modifying the CloudFlare settings.
 	 */
-	public function maybe_print_update_settings_notice() {
+	public function maybe_display_update_settings_notice() {
 		$screen = get_current_screen();
 
 		if ( ! current_user_can( 'rocket_manage_options' ) || 'settings_page_wprocket' !== $screen->id ) {
@@ -55,6 +55,7 @@ class Subscriber implements Subscriber_Interface {
 
 		$user_id = get_current_user_id();
 		$notices = get_transient( $user_id . '_cloudflare_update_settings' );
+
 		if ( ! $notices ) {
 			return;
 		}
