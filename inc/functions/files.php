f51b9d29239638d3cc6f833c6e195dc2ee304173
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-31 17:35:11 +0100

    Sanitize only user and key

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -947,8 +947,8 @@ function rocket_clean_user( $user_id, $lang = '' ) {
 			$parse_url['host'] = str_replace( '.', '_', $parse_url['host'] );
 		}
 
-		$cache_dir = $parse_url['host'] . '-' . $user_key;
-		$cache_dir = preg_replace( '/[^a-z0-9_\-]/', '', strtolower( $cache_dir ) ) . $parse_url['path'];
+		$cache_dir = $parse_url['host'] . '-' . preg_replace( '/[^a-z0-9_\-]/', '', strtolower( $user_key ) );
+		$cache_dir = $cache_dir . $parse_url['path'];
 		$root      = rocket_get_constant( 'WP_ROCKET_CACHE_PATH' ) . $cache_dir;
 
 		/**
