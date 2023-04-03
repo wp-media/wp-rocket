b49d08f46498615665b165c93fd527d2725f2419
Rémy Perona <remperona@gmail.com>
2023-04-03 16:16:34 -0400

    add tests for protocol rewrite methods

---
+++ tests/Unit/inc/Addon/Cloudflare/Subscriber/protocolRewriteSrcset.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\Inc\Addon\Cloudflare\Subscriber;
+
+use Brain\Monkey\Filters;
+use Mockery;
+use WP_Rocket\Addon\Cloudflare\Subscriber;
+use WP_Rocket\Addon\Cloudflare\Cloudflare;
+use WP_Rocket\Admin\{Options, Options_Data};
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers WP_Rocket\Addon\Cloudflare\Subscriber::protocol_rewrite_srcset
+ *
+ * @group Cloudflare
+ */
+class TestProtocolRewriteSrcset extends TestCase {
+	private $options_api;
+	private $options;
+	private $cloudflare;
+	private $subscriber;
+
+	protected function setUp(): void {
+		$this->options_api = Mockery::mock( Options::class );
+		$this->options     = Mockery::mock( Options_Data::class );
+		$this->cloudflare  = Mockery::mock( Cloudflare::class );
+		$this->subscriber  = new Subscriber( $this->cloudflare, $this->options, $this->options_api );
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpected( $config, $value, $expected ) {
+		$this->options->expects()
+			->get( 'do_cloudflare', 0 )
+			->once()
+			->andReturn( $config['cloudflare'] );
+
+		$this->options->expects()
+			->get( 'cloudflare_protocol_rewrite', 0 )
+			->atMost()
+			->once()
+			->andReturn( $config['rewrite'] );
+
+		Filters\expectApplied( 'do_rocket_protocol_rewrite' )
+			->andReturn( $config['filter'] );
+
+		$this->assertSame(
+			$expected,
+			$this->subscriber->protocol_rewrite_srcset( $value )
+		);
+	}
+}
