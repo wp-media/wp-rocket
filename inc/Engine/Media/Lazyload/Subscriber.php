6a994f879ed2b009574702e43a7117eee646a23f
COQUARD Cyrille <coquardcyr@gmail.com>
2023-04-20 15:16:08 +0200

    Fixed QA

--- inc/Engine/Media/Lazyload/Subscriber.php
+++ inc/Engine/Media/Lazyload/Subscriber.php
@@ -245,7 +245,7 @@ class Subscriber implements Subscriber_Interface {
 		 */
 		$extension = apply_filters( 'rocket_lazyload_youtube_thumbnail_extension', 'jpg' );
 
-		if ( is_string( $extension ) ) {
+		if ( ! is_string( $extension ) ) {
 			$extension = 'jpg';
 		}
 
