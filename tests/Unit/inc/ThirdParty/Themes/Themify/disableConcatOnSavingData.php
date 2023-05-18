10e82c0af9670f20b48150a89d858108ee2834a1
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-05-18 14:33:24 +0200

    Enhancement/5736 disable concat on rucss (#5890)
    * Added themify 3rd party class to disable concat css when rucss enabled
    
    * Fixed code standards
    
    * Added fix for rucss
    
    * Added a fix for themify
    
    * Fixed issues with the PR
    
    * Added fix for qa
    
    * Fixed tests and QA review
    
    * Fixed tests and QA review
    
    * Fixed events
    
    * Added fully working version
    
    * Fixed code standards
    
    * Fixed unit tests
    
    * Fixed code standards
    
    * Fxied unit tests
    
    * Removed unused method
    
    ---------
    
    Co-authored-by: Vasilis Manthos <vmanthos@gmail.com>

---
+++ tests/Unit/inc/ThirdParty/Themes/Themify/disableConcatOnSavingData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Themes\Themify;
+
+use Mockery;
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\ThirdParty\Themes\Themify;
+
+
+use WP_Rocket\Tests\Unit\TestCase;
+use Brain\Monkey\Filters;
+
+/**
+ * @covers \WP_Rocket\ThirdParty\Themes\Themify::disable_concat_on_saving_data
+ */
+class Test_disableConcatOnSavingData extends TestCase {
+
+    /**
+     * @var Themify
+     */
+    protected $themify;
+
+	protected $options;
+
+    public function set_up() {
+        parent::set_up();
+
+		$this->options = Mockery::mock(Options_Data::class);
+		$this->themify = new Themify($this->options);
+    }
+
+    /**
+     * @dataProvider configTestData
+     */
+    public function testShouldReturnAsExpected( $config, $expected )
+    {
+		$this->options->expects()->get( 'remove_unused_css', false )->andReturn( $config['rucss_enabled'] );
+		$this->assertSame($expected, $this->themify->disable_concat_on_saving_data($config['value']));
+    }
+}
