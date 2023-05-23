4b4ce3444eaefbc543fca0e88764f339b8a063b8
COQUARD Cyrille <coquardcyr@gmail.com>
2023-05-23 15:12:53 +0200

    Fixed a issue due to bad type

--- inc/Engine/Optimization/RUCSS/Admin/Subscriber.php
+++ inc/Engine/Optimization/RUCSS/Admin/Subscriber.php
@@ -712,7 +712,7 @@ class Subscriber implements Subscriber_Interface {
 	 * @return bool
 	 */
 	public function disable_russ_on_wrong_license() {
-		if ( false !== get_option( 'wp_rocket_no_licence' ) ) {
+		if ( false !== (bool) get_option( 'wp_rocket_no_licence' ) ) {
 			return false;
 		}
 		return null;
