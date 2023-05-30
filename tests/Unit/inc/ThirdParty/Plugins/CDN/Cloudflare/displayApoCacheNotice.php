fc9b4a10f4cb55638e4873bca4e201ee5cd88aa4
COQUARD Cyrille <coquardcyr@gmail.com>
2023-05-30 17:53:23 +0200

    Swapped value from both notice tests

--- tests/Unit/inc/ThirdParty/Plugins/CDN/Cloudflare/displayApoCacheNotice.php
+++ tests/Unit/inc/ThirdParty/Plugins/CDN/Cloudflare/displayApoCacheNotice.php
@@ -9,8 +9,6 @@ use WP_Rocket\Admin\Options;
 
 
 use WP_Rocket\Tests\Unit\TestCase;
-use Brain\Monkey\Functions;
-use Brain\Monkey\Filters;
 
 /**
  * @covers \WP_Rocket\ThirdParty\Plugins\CDN\Cloudflare::display_apo_cache_notice
@@ -45,40 +43,7 @@ class Test_displayApoCacheNotice extends TestCase {
      */
     public function testShouldDoAsExpected( $config, $expected )
     {
-		Functions\when('home_url')->justReturn($config['home_url']);
-	    Functions\expect('wp_remote_retrieve_headers')->with($expected['response'])->andReturn($config['headers']);
-	    Functions\expect('wp_remote_request')->with($expected['home_url'], $expected['configs'])->andReturn($config['response']);
-		$this->configure_apply_mandatory_cookies($config, $expected);
-		$this->configure_apply_dynamic_cookies($config, $expected);
-		$this->configure_notice($config, $expected);
         $this->cloudflare->display_apo_cache_notice();
     }
 
-	protected function configure_apply_mandatory_cookies($config, $expected) {
-		if(! $config['has_apo']) {
-			return;
-		}
-
-		Filters\expectApplied('rocket_cache_mandatory_cookies')->with([])->andReturn($config['mandatory_cookies']);
-	}
-
-	protected function configure_apply_dynamic_cookies($config, $expected) {
-		if(! $config['has_apo']) {
-			return;
-		}
-
-		Filters\expectApplied('rocket_cache_dynamic_cookies')->with([])->andReturn($config['dynamic_cookies']);
-	}
-
-	protected function configure_screen($config, $expected) {
-
-	}
-
-	protected function configure_notice($config, $expected) {
-		if(! $config['should_display']) {
-			Functions\expect('rocket_notice_html')->never();
-			return;
-		}
-		Functions\expect('rocket_notice_html')->with($expected['notice']);
-	}
 }
