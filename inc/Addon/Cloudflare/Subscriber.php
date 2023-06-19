76031e933feeaf45162c904bab5f25043c985692
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-06-19 15:45:25 +0200

    Fixed disappearing notice Cloudflare (#5979)

--- inc/Addon/Cloudflare/Subscriber.php
+++ inc/Addon/Cloudflare/Subscriber.php
@@ -70,12 +70,12 @@ class Subscriber implements Subscriber_Interface {
 			'update_option_' . $slug                    => [
 				[ 'save_cloudflare_options', 10, 2 ],
 				[ 'update_dev_mode', 11, 2 ],
-				[ 'display_settings_notice', 11, 2 ],
 			],
 			'pre_update_option_' . $slug                => [
 				[ 'change_auth', 8, 2 ],
 				[ 'delete_connection_transient', 10, 2 ],
 				[ 'save_cloudflare_old_settings', 10, 2 ],
+				[ 'display_settings_notice', 11, 2 ],
 			],
 			'rocket_buffer'                             => [ 'protocol_rewrite', PHP_INT_MAX ],
 			'wp_calculate_image_srcset'                 => [ 'protocol_rewrite_srcset', PHP_INT_MAX ],
@@ -602,13 +602,13 @@ class Subscriber implements Subscriber_Interface {
 			'cloudflare_protocol_rewrite',
 		];
 
-		$out = false;
+		$change = false;
 
 		foreach ( $fields as $field ) {
-			$out &= ! isset( $old_value[ $field ], $value[ $field ] ) || $old_value[ $field ] !== $value[ $field ];
+			$change |= ! isset( $old_value[ $field ], $value[ $field ] ) || $old_value[ $field ] !== $value[ $field ];
 		}
 
-		if ( $out ) {
+		if ( ! $change ) {
 			return $value;
 		}
 
@@ -621,19 +621,24 @@ class Subscriber implements Subscriber_Interface {
 	/**
 	 * Display the error notice.
 	 *
-	 * @param array $old_value An array of submitted values for the settings.
 	 * @param array $value     An array of previous values for the settings.
+	 * @param array $old_value An array of submitted values for the settings.
 	 *
 	 * @return mixed
 	 */
-	public function display_settings_notice( $old_value, $value ) {
+	public function display_settings_notice( $value, $old_value ) {
+
+		if ( ! key_exists( 'cloudflare_zone_id', $value ) ) {
+			return $value;
+		}
+
 		$connection = $this->cloudflare->check_connection( $value['cloudflare_zone_id'] );
 
 		if ( is_wp_error( $connection ) ) {
 			add_settings_error( 'general', 'cloudflare_api_key_invalid', __( 'WP Rocket: ', 'rocket' ) . '</strong>' . $connection->get_error_message() . '<strong>', 'error' );
 		}
 
-		return $old_value;
+		return $value;
 	}
 
 	/**
