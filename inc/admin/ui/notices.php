22aacc18773370dc466eea0c7d02d3b875cc4f75
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2023-07-05 16:13:00 +0300

    add wp-asset-clean-up in plugins to deactivate (#6022)

--- inc/admin/ui/notices.php
+++ inc/admin/ui/notices.php
@@ -148,6 +148,8 @@ function rocket_plugins_to_deactivate() {
 		'add-expires-headers'                        => 'add-expires-headers/add-expires-headers.php',
 		'page-optimize'                              => 'page-optimize/page-optimize.php',
 		'psn-pagespeed-ninja'                        => 'psn-pagespeed-ninja/pagespeedninja.php',
+		'wp-asset-clean-up'                          => 'wp-asset-clean-up/wpacu.php',
+		'wp-asset-clean-up-pro'                      => 'wp-asset-clean-up-pro/wpacu.php',
 	];
 
 	if ( get_rocket_option( 'lazyload' ) ) {
