1df71347d01682e9b57e0d37f57b7e11e74e5557
Michael Lee <michaelleemichaellee408@gmail.com>
2023-07-07 08:50:12 +0100

    Updated tests

--- tests/Fixtures/content/advancedCacheContent.php
+++ tests/Fixtures/content/advancedCacheContent.php
@@ -39,36 +39,41 @@ MOBILE_CONTENTS;
 $end = <<<ENDING_CONTENTS
 
 
-if ( defined( 'WP_ROCKET_DEBUG' ) && WP_ROCKET_DEBUG ) {
-	spl_autoload_register(
-		function( \$class ) use ( \$rocket_path ) {
-			\$rocket_classes = [
-				'WP_Rocket\\\Buffer\\\Abstract_Buffer' => \$rocket_path . 'inc/classes/Buffer/class-abstract-buffer.php',
-				'WP_Rocket\\\Buffer\\\Cache'           => \$rocket_path . 'inc/classes/Buffer/class-cache.php',
-				'WP_Rocket\\\Buffer\\\Tests'           => \$rocket_path . 'inc/classes/Buffer/class-tests.php',
-				'WP_Rocket\\\Buffer\\\Config'          => \$rocket_path . 'inc/classes/Buffer/class-config.php',
-				'WP_Rocket\\\Logger\\\HTML_Formatter'  => \$rocket_path . 'inc/classes/logger/class-html-formatter.php',
-				'WP_Rocket\\\Logger\\\Logger'          => \$rocket_path . 'inc/classes/logger/class-logger.php',
-				'WP_Rocket\\\Logger\\\Stream_Handler'  => \$rocket_path . 'inc/classes/logger/class-stream-handler.php',
-				'WP_Rocket\\\Traits\\\Memoize'         => \$rocket_path . 'inc/classes/traits/trait-memoize.php',
-			];
 
-			if ( isset( \$rocket_classes[ \$class ] ) ) {
-				\$file = \$rocket_classes[ \$class ];
-			} elseif ( strpos( \$class, 'Monolog\\\' ) === 0 ) {
-				\$file = \$rocket_path . 'vendor/monolog/monolog/src/' . str_replace( '\\\', '/', \$class ) . '.php';
-			} elseif ( strpos( \$class, 'Psr\\\Log\\\' ) === 0 ) {
-				\$file = \$rocket_path . 'vendor/psr/log/' . str_replace( '\\\', '/', \$class ) . '.php';
-			} else {
-				return;
-			}
+spl_autoload_register(
+	function( \$class ) use ( \$rocket_path ) {
+		\$rocket_classes = [
+			'WP_Rocket\\\Buffer\\\Abstract_Buffer' => \$rocket_path . 'inc/classes/Buffer/class-abstract-buffer.php',
+			'WP_Rocket\\\Buffer\\\Cache'           => \$rocket_path . 'inc/classes/Buffer/class-cache.php',
+			'WP_Rocket\\\Buffer\\\Tests'           => \$rocket_path . 'inc/classes/Buffer/class-tests.php',
+			'WP_Rocket\\\Buffer\\\Config'          => \$rocket_path . 'inc/classes/Buffer/class-config.php',
+			'WP_Rocket\\\Logger\\\Logger'          => \$rocket_path . 'inc/classes/logger/class-logger.php',
+			'WP_Rocket\\\Traits\\\Memoize'         => \$rocket_path . 'inc/classes/traits/trait-memoize.php',
+		];
 
-			if ( file_exists( \$file ) ) {
-				require \$file;
-			}
+		\$rocket_logger_classes = [
+			'WP_Rocket\\\Logger\\\HTML_Formatter' => \$rocket_path . 'inc/classes/logger/class-html-formatter.php',
+			'WP_Rocket\\\Logger\\\Stream_Handler' => \$rocket_path . 'inc/classes/logger/class-stream-handler.php',
+		];
+
+		// Load logger dependent classes only when debug is enabled.
+		if ( isset( \$rocket_logger_classes[ \$class ] ) && defined( 'WP_ROCKET_DEBUG' ) && WP_ROCKET_DEBUG ) {
+			\$file = \$rocket_logger_classes[ \$class ];
+		} elseif ( isset( \$rocket_classes[ \$class ] ) ) {
+			\$file = \$rocket_classes[ \$class ];
+		} elseif ( strpos( \$class, 'Monolog\\\' ) === 0 && defined( 'WP_ROCKET_DEBUG' ) && WP_ROCKET_DEBUG ) {
+			\$file = \$rocket_path . 'vendor/monolog/monolog/src/' . str_replace( '\\\', '/', \$class ) . '.php';
+		} elseif ( strpos( \$class, 'Psr\\\Log\\\' ) === 0 ) {
+			\$file = \$rocket_path . 'vendor/psr/log/' . str_replace( '\\\', '/', \$class ) . '.php';
+		} else {
+			return;
 		}
-	);
-}
+
+		if ( file_exists( \$file ) ) {
+			require \$file;
+		}
+	}
+);
 
 if ( ! class_exists( '\WP_Rocket\Buffer\Cache' ) ) {
 	if ( ! defined( 'DONOTROCKETOPTIMIZE' ) ) {
