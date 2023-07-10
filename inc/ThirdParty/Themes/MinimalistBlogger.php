c2cde694c47d8f4d8900f67c576cf033810f3ae5
Michael Lee <michaelleemichaellee408@gmail.com>
2023-07-10 10:55:21 +0100

    Revert regex pattern

--- inc/ThirdParty/Themes/MinimalistBlogger.php
+++ inc/ThirdParty/Themes/MinimalistBlogger.php
@@ -35,7 +35,7 @@ class MinimalistBlogger extends ThirdpartyTheme {
 	 * @return array
 	 */
 	public function exclude_jquery_from_delay_js( array $exclusions = [] ) {
-		$exclusions[] = '\/jquery(-migrate)?-?([0-9.]+)?(.min|.slim|.slim.min)?.js(\?(.*))?(\'|"| |<|>)';
+		$exclusions[] = '\/jquery(-migrate)?-?([0-9.]+)?(.min|.slim|.slim.min)?.js(\?(.*))?( |\'|"|>)';
 		return $exclusions;
 	}
 }
