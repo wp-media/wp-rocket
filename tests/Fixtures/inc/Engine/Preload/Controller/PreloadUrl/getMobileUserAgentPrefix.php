b40450340616a676753f171224e63ae0cfeaff47
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2023-07-11 14:52:29 +0300

    fix tests

--- tests/Fixtures/inc/Engine/Preload/Controller/PreloadUrl/getMobileUserAgentPrefix.php
+++ tests/Fixtures/inc/Engine/Preload/Controller/PreloadUrl/getMobileUserAgentPrefix.php
@@ -2,15 +2,15 @@
 return [
 	'shouldUsePrefixOnInvalidFilterPrefix' => [
 		'config' => [
-			'prefix' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+			'prefix' => 'WP Rocket Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
 			'filter' => '',
 
 		],
-		'expected' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+		'expected' => 'WP Rocket Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
 	],
 	'shouldUseFilterPrefixOnFilterPrefix' => [
 		'config' => [
-			'prefix' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+			'prefix' => 'WP Rocket Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
 			'filter' => 'new_prefix',
 
 		],
