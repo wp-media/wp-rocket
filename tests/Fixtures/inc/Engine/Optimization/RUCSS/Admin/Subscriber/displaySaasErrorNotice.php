de5c5b3f08f9a543b38f09ea7fa4e4619901a184
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2023-07-17 15:05:53 +0300

    update msg in integration tests

--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/displaySaasErrorNotice.php
+++ tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/displaySaasErrorNotice.php
@@ -5,7 +5,7 @@ $content = <<<Notice
 <p>
 <strong>
 WP Rocket</strong>
-: It seems a security plugin or the server's firewall prevents WP Rocket from accessing the Remove Unused CSS generator. The following IP address 135.125.83.227 should be allowlisted:
+: It seems a security plugin or the server\'s firewall prevents WP Rocket from accessing the Remove Unused CSS generator. IPs listed <a href="https://docs.wp-rocket.me/article/1529-remove-unused-css?utm_source=wp_plugin&utm_medium=wp_rocket#basic-requirements" data-beacon-article="6076083ff8c0ef2d98df1f97" rel="noopener noreferrer" target="_blank">here in our documentation</a> should be added to your allowlists:
 <ul>
 <li>
 - In the security plugin, if you are using one</li>
