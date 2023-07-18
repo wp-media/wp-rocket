d34fa66d7141d20abbb76199414f35eb29b13ee1
COQUARD Cyrille <coquardcyr@gmail.com>
2023-07-18 09:07:23 +0200

    Fixed code standards

--- inc/Engine/Media/Lazyload/CSS/Subscriber.php
+++ inc/Engine/Media/Lazyload/CSS/Subscriber.php
@@ -265,19 +265,22 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 
 		$css_files = $data['css_files'];
 
-		usort($css_files, function ($url1, $url2) {
-			return strlen($url1) < strlen($url2);
-		});
+		usort(
+			$css_files,
+			function ( $url1, $url2 ) {
+				return strlen( $url1 ) < strlen( $url2 );
+			}
+			);
 
 		$css_files_mapping = [];
 
-		foreach ($css_files as $url ) {
-			$placeholder = uniqid("url_bg_css_");
-			$url_key = $this->format_url( $url );
+		foreach ( $css_files as $url ) {
+			$placeholder = uniqid( 'url_bg_css_' );
+			$url_key     = $this->format_url( $url );
 
-			$html = str_replace($url_key, $placeholder, $html);
+			$html = str_replace( $url_key, $placeholder, $html );
 
-			$css_files_mapping[$url_key] = $placeholder;
+			$css_files_mapping[ $url_key ] = $placeholder;
 		}
 
 		foreach ( $css_files as $url ) {
@@ -330,7 +333,7 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 					'data' => $cached_url,
 				]
 				);
-			$html = str_replace( $css_files_mapping[$url_key], $cached_url, $html );
+			$html = str_replace( $css_files_mapping[ $url_key ], $cached_url, $html );
 		}
 
 		$data['html'] = $html;
@@ -340,7 +343,7 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 		}
 
 		$data['lazyloaded_images'] = array_merge( $data['lazyloaded_images'], $mapping );
-		$data['lazyloaded_images'] = array_unique($data['lazyloaded_images'], SORT_REGULAR);
+		$data['lazyloaded_images'] = array_unique( $data['lazyloaded_images'], SORT_REGULAR );
 
 		return $data;
 	}
