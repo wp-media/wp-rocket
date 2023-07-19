7d72f1d1b1bd4cc1d1a91c6afabe8b5ea5c319cb
COQUARD Cyrille <coquardcyr@gmail.com>
2023-07-19 15:17:43 +0200

    Isolated filter add_lazyload_script_exclude_js

--- tests/Integration/inc/Engine/Media/Lazyload/CSS/Subscriber/addLazyloadScriptExcludeJs.php
+++ tests/Integration/inc/Engine/Media/Lazyload/CSS/Subscriber/addLazyloadScriptExcludeJs.php
@@ -2,6 +2,7 @@
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\Media\Lazyload\CSS\Subscriber;
 
+use WP_Rocket\Tests\Integration\FilterTrait;
 use WP_Rocket\Tests\Integration\TestCase;
 
 /**
@@ -9,17 +10,21 @@ use WP_Rocket\Tests\Integration\TestCase;
  */
 class Test_addLazyloadScriptExcludeJs extends TestCase {
 
+	use FilterTrait;
+
 	protected $config;
 
 	public function set_up()
 	{
 		parent::set_up();
+		$this->unregisterAllCallbacksExcept('rocket_exclude_js', 'add_lazyload_script_exclude_js');
 		add_filter('pre_get_rocket_option_lazyload_css_bg_img', [$this, 'lazyload_css_bg_img']);
 	}
 
 	public function tear_down()
 	{
 		remove_filter('pre_get_rocket_option_lazyload_css_bg_img', [$this, 'lazyload_css_bg_img']);
+		$this->restoreWpFilter('');
 		parent::tear_down();
 	}
 
