0b4f770ef614888a6d4b121cb1538948d3261d79
Rémy Perona <remperona@gmail.com>
2023-07-26 07:27:27 -0400

    update service providers

--- inc/Engine/Preload/ServiceProvider.php
+++ inc/Engine/Preload/ServiceProvider.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace WP_Rocket\Engine\Preload;
 
 use WP_Filesystem_Direct;
@@ -6,12 +8,7 @@ use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvi
 use WP_Rocket\Engine\Preload\Activation\Activation;
 use WP_Rocket\Engine\Preload\Admin\Settings;
 use WP_Rocket\Engine\Preload\Admin\Subscriber as AdminSubscriber;
-use WP_Rocket\Engine\Preload\Controller\CheckFinished;
-use WP_Rocket\Engine\Preload\Controller\ClearCache;
-use WP_Rocket\Engine\Preload\Controller\CrawlHomepage;
-use WP_Rocket\Engine\Preload\Controller\LoadInitialSitemap;
-use WP_Rocket\Engine\Preload\Controller\PreloadUrl;
-use WP_Rocket\Engine\Preload\Controller\Queue;
+use WP_Rocket\Engine\Preload\Controller\{CheckFinished,ClearCache,CrawlHomepage,LoadInitialSitemap,PreloadUrl,Queue};
 use WP_Rocket\Engine\Preload\Cron\Subscriber as CronSubscriber;
 use WP_Rocket\Engine\Preload\Database\Queries\Cache as CacheQuery;
 use WP_Rocket\Engine\Preload\Database\Tables\Cache as CacheTable;
@@ -63,102 +60,103 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * @return void
 	 */
 	public function register() {
-		$options = $this->getContainer()->get( 'options' );
+		$options = $this->getLeagueContainer()->get( 'options' );
 
-		$this->getContainer()->add( 'preload_mobile_detect', WP_Rocket_Mobile_Detect::class );
+		$this->getLeagueContainer()->add( 'preload_mobile_detect', WP_Rocket_Mobile_Detect::class );
 
-		$this->getContainer()->add( 'wp_direct_filesystem', WP_Filesystem_Direct::class )
+		$this->getLeagueContainer()->add( 'wp_direct_filesystem', WP_Filesystem_Direct::class )
 			->addArgument( [] );
-		$wp_file_system = $this->getContainer()->get( 'wp_direct_filesystem' );
+		$wp_file_system = $this->getLeagueContainer()->get( 'wp_direct_filesystem' );
 
-		$this->getContainer()->add( 'preload_caches_table', CacheTable::class );
-		$this->getContainer()->add( 'preload_caches_query', CacheQuery::class )
+		$this->getLeagueContainer()->add( 'preload_caches_table', CacheTable::class );
+		$this->getLeagueContainer()->add( 'preload_caches_query', CacheQuery::class )
 			->addArgument( new Logger() );
-		$this->getContainer()->get( 'preload_caches_table' );
+		$this->getLeagueContainer()->get( 'preload_caches_table' );
 
-		$cache_query = $this->getContainer()->get( 'preload_caches_query' );
+		$cache_query = $this->getLeagueContainer()->get( 'preload_caches_query' );
 
-		$this->getContainer()->add( 'preload_queue', Queue::class );
-		$queue = $this->getContainer()->get( 'preload_queue' );
+		$this->getLeagueContainer()->add( 'preload_queue', Queue::class );
+		$queue = $this->getLeagueContainer()->get( 'preload_queue' );
 
-		$this->getContainer()->add( 'preload_url_controller', PreloadUrl::class )
+		$this->getLeagueContainer()->add( 'preload_url_controller', PreloadUrl::class )
 			->addArgument( $options )
 			->addArgument( $queue )
 			->addArgument( $cache_query )
 			->addArgument( $wp_file_system );
 
-		$preload_url_controller = $this->getContainer()->get( 'preload_url_controller' );
+		$preload_url_controller = $this->getLeagueContainer()->get( 'preload_url_controller' );
 
-		$this->getContainer()->add( 'homepage_crawler', CrawlHomepage::class );
-		$crawl_homepage = $this->getContainer()->get( 'homepage_crawler' );
+		$this->getLeagueContainer()->add( 'homepage_crawler', CrawlHomepage::class );
+		$crawl_homepage = $this->getLeagueContainer()->get( 'homepage_crawler' );
 
-		$this->getContainer()->add( 'sitemap_parser', SitemapParser::class );
-		$sitemap_parser = $this->getContainer()->get( 'sitemap_parser' );
+		$this->getLeagueContainer()->add( 'sitemap_parser', SitemapParser::class );
+		$sitemap_parser = $this->getLeagueContainer()->get( 'sitemap_parser' );
 
-		$this->getContainer()->add( 'fetch_sitemap_controller', FetchSitemap::class )
+		$this->getLeagueContainer()->add( 'fetch_sitemap_controller', FetchSitemap::class )
 			->addArgument( $sitemap_parser )
 			->addArgument( $queue )
 			->addArgument( $cache_query );
 
-		$fetch_sitemap_controller = $this->getContainer()->get( 'fetch_sitemap_controller' );
+		$fetch_sitemap_controller = $this->getLeagueContainer()->get( 'fetch_sitemap_controller' );
 
-		$this->getContainer()->add( 'load_initial_sitemap_controller', LoadInitialSitemap::class )
+		$this->getLeagueContainer()->add( 'load_initial_sitemap_controller', LoadInitialSitemap::class )
 			->addArgument( $queue )
 			->addArgument( $cache_query )
 			->addArgument( $crawl_homepage );
 
-		$this->getContainer()->add( 'preload_activation', Activation::class )
+		$this->getLeagueContainer()->add( 'preload_activation', Activation::class )
+			->addArgument( $preload_url_controller )
 			->addArgument( $queue )
 			->addArgument( $cache_query )
 			->addArgument( $options );
 
-		$this->getContainer()->add( 'preload_settings', Settings::class )
+		$this->getLeagueContainer()->add( 'preload_settings', Settings::class )
 			->addArgument( $options )
 			->addArgument( $preload_url_controller );
 
-		$preload_settings = $this->getContainer()->get( 'preload_settings' );
+		$preload_settings = $this->getLeagueContainer()->get( 'preload_settings' );
 
-		$this->getContainer()->add( 'check_finished_controller', CheckFinished::class )
+		$this->getLeagueContainer()->add( 'check_finished_controller', CheckFinished::class )
 			->addArgument( $preload_settings )
 			->addArgument( $cache_query )
 			->addArgument( $queue );
 
-		$check_finished_controller = $this->getContainer()->get( 'check_finished_controller' );
+		$check_finished_controller = $this->getLeagueContainer()->get( 'check_finished_controller' );
 
-		$this->getContainer()->share( 'preload_front_subscriber', FrontEndSubscriber::class )
+		$this->getLeagueContainer()->share( 'preload_front_subscriber', FrontEndSubscriber::class )
 			->addArgument( $fetch_sitemap_controller )
 			->addArgument( $preload_url_controller )
 			->addArgument( $check_finished_controller )
-			->addArgument( $this->getContainer()->get( 'load_initial_sitemap_controller' ) )
+			->addArgument( $this->getLeagueContainer()->get( 'load_initial_sitemap_controller' ) )
 			->addTag( 'common_subscriber' );
 
-		$this->getContainer()->add( 'preload_clean_controller', ClearCache::class )
+		$this->getLeagueContainer()->add( 'preload_clean_controller', ClearCache::class )
 			->addArgument( $cache_query );
 
-		$clean_controller = $this->getContainer()->get( 'preload_clean_controller' );
+		$clean_controller = $this->getLeagueContainer()->get( 'preload_clean_controller' );
 
-		$this->getContainer()->share( 'preload_subscriber', Subscriber::class )
+		$this->getLeagueContainer()->share( 'preload_subscriber', Subscriber::class )
 			->addArgument( $options )
-			->addArgument( $this->getContainer()->get( 'load_initial_sitemap_controller' ) )
+			->addArgument( $this->getLeagueContainer()->get( 'load_initial_sitemap_controller' ) )
 			->addArgument( $cache_query )
-			->addArgument( $this->getContainer()->get( 'preload_activation' ) )
-			->addArgument( $this->getContainer()->get( 'preload_mobile_detect' ) )
+			->addArgument( $this->getLeagueContainer()->get( 'preload_activation' ) )
+			->addArgument( $this->getLeagueContainer()->get( 'preload_mobile_detect' ) )
 			->addArgument( $clean_controller )
 			->addArgument( $queue )
 			->addTag( 'common_subscriber' );
 
-		$this->getContainer()->share( 'preload_cron_subscriber', CronSubscriber::class )
+		$this->getLeagueContainer()->share( 'preload_cron_subscriber', CronSubscriber::class )
 			->addArgument( $preload_settings )
 			->addArgument( $cache_query )
 			->addArgument( $preload_url_controller )
 			->addTag( 'common_subscriber' );
 
-		$this->getContainer()->share( 'fonts_preload_subscriber', Fonts::class )
+		$this->getLeagueContainer()->share( 'fonts_preload_subscriber', Fonts::class )
 			->addArgument( $options )
-			->addArgument( $this->getContainer()->get( 'cdn' ) )
+			->addArgument( $this->getLeagueContainer()->get( 'cdn' ) )
 			->addTag( 'common_subscriber' );
 
-		$this->getContainer()->add( 'preload_admin_subscriber', AdminSubscriber::class )
+		$this->getLeagueContainer()->add( 'preload_admin_subscriber', AdminSubscriber::class )
 			->addArgument( $options )
 			->addArgument( $preload_settings )
 			->addTag( 'common_subscriber' );
