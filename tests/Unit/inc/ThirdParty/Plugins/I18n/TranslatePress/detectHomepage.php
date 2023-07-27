5831bcaf24b7133c6d537f2baa7bdd825fbd3bfa
Rémy Perona <remperona@gmail.com>
2023-07-27 17:31:59 -0400

    fix test

--- tests/Unit/inc/ThirdParty/Plugins/I18n/TranslatePress/detectHomepage.php
+++ tests/Unit/inc/ThirdParty/Plugins/I18n/TranslatePress/detectHomepage.php
@@ -2,27 +2,23 @@
 
 namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Plugins\I18n\TranslatePress;
 
-use Mockery;
+use Brain\Monkey\Functions;
 use TRP_Url_Converter;
 use WP_Rocket\ThirdParty\Plugins\I18n\TranslatePress;
-
-
 use WP_Rocket\Tests\Unit\TestCase;
-use Brain\Monkey\Functions;
 
 /**
  * @covers \WP_Rocket\ThirdParty\Plugins\I18n\TranslatePress::detect_homepage
  * @group TranslatePress
  */
 class Test_detectHomepage extends TestCase {
-
     /**
      * @var TranslatePress
      */
     protected $translatepress;
 
-    public function set_up() {
-        parent::set_up();
+    protected function setUp(): void {
+        parent::setUp();
 
         $this->translatepress = new TranslatePress();
     }
@@ -30,11 +26,15 @@ class Test_detectHomepage extends TestCase {
     /**
      * @dataProvider configTestData
      */
-    public function testShouldReturnAsExpected( $config, $expected )
-    {
+    public function testShouldReturnAsExpected( $config, $expected ) {
 		TRP_Url_Converter::$url = $config['url_language'];
 		TRP_Url_Converter::$lang = $config['language'];
-		Functions\when('home_url')->justReturn('home_url');
-        $this->assertSame($expected['result'], $this->translatepress->detect_homepage($config['home_url'], $config['url']));
+
+		Functions\when('home_url')->justReturn( $config['home_url'] );
+
+        $this->assertSame(
+			$expected['result'],
+			$this->translatepress->detect_homepage( $config['home_url'], $config['url'] )
+		);
     }
 }
