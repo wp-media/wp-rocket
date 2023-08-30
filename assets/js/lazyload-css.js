e1decaf435078b9ec96104ad272d2c8dddabbc6a
COQUARD Cyrille <coquardcyr@gmail.com>
2023-08-30 15:25:10 +0200

    Added new script to handle dynamic elements

--- assets/js/lazyload-css.js
+++ assets/js/lazyload-css.js
@@ -1,4 +1,4 @@
-function rocket_css_lazyload() {
+function rocket_css_lazyload_launch() {
 
 	const pairs = rocket_pairs || [];
 
@@ -6,7 +6,7 @@ function rocket_css_lazyload() {
 
 	const threshold = rocket_lazyload_css_data.threshold || 300;
 
-	const observer = new IntersectionObserver(entries => {
+	const observer = rocket_lazyload_css_observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
 			if (entry.isIntersecting) {
 				const pairs = rocket_pairs.filter(s => entry.target.matches(s.selector));
@@ -16,7 +16,7 @@ function rocket_css_lazyload() {
 						pair.elements.forEach(el => {
 							el.setAttribute('data-rocket-lazy-bg', 'loaded');
 							// Stop observing the target element
-							observer.unobserve(el);
+							rocket_lazyload_css_observer.unobserve(el);
 						});
 					}
 				})
@@ -26,18 +26,51 @@ function rocket_css_lazyload() {
 		rootMargin: threshold + 'px'
 	});
 
-	pairs.forEach(pair => {
-		try {
-			const elements = document.querySelectorAll(pair.selector);
-			elements.forEach(el => {
-				observer.observe(el);
-				// Save el in the pair object (create a new empty array if it doesn't exist)
-				(pair.elements ||= []).push(el);
-			});
-		} catch (error) {
-			console.error(error);
+	function lazyload() {
+
+
+		pairs.forEach(pair => {
+			try {
+				const elements = document.querySelectorAll(pair.selector);
+				elements.forEach(el => {
+					observer.observe(el);
+					// Save el in the pair object (create a new empty array if it doesn't exist)
+					(pair.elements ||= []).push(el);
+				});
+			} catch (error) {
+				console.error(error);
+			}
+		});
+	}
+
+	lazyload();
+
+	const observe_DOM = (function(){
+		const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+		return function( obj, callback ){
+			if( !obj || obj.nodeType !== 1 ) return;
+
+			if( MutationObserver ){
+				// define a new observer
+				const mutationObserver = new MutationObserver(callback);
+
+				// have the observer observe for changes in children
+				mutationObserver.observe( obj, { childList:true, subtree:true })
+				return mutationObserver
+			}
+
+			// browser support fallback
+			else if( window.addEventListener ){
+				obj.addEventListener('DOMNodeInserted', callback, false)
+				obj.addEventListener('DOMNodeRemoved', callback, false)
+			}
+
+			obj.addEventListener('DOMSubtreeModified', callback, false)
 		}
-	});
+	})()
+
+	observe_DOM(document.querySelector('body'), lazyload)
 }
 
-rocket_css_lazyload();
+rocket_css_lazyload_launch();
