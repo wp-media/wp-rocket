6ff262df786265767206d076457e0c505a985528
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-09-12 11:39:12 +0200

    Fixes 6132: Added tests and new logic for supporting pseudoclasses (#6150)
    Co-authored-by: Vasilis Manthos <vmanthos@gmail.com>

--- tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Front/MappingFormatter/format.php
+++ tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Front/MappingFormatter/format.php
@@ -43,7 +43,72 @@ return [
 					'hash' => 'a122ad12df3',
 					'selector' => '::after',
 					'url' => 'http://example.org/test',
-				]
+				],
+				[
+					'hash' => 'a122ad12df3',
+					'selector' => '::after',
+					'url' => 'http://example.org/test',
+				],
+				[
+					'hash' => 'a21ss2',
+					'selector' => 'dd:nth-last-of-type(3n)',
+					'url' => 'images/underline.png'
+				],
+				[
+					'hash' => 'a21ss4',
+					'selector' => '.background-image~:after',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss8',
+					'selector' => '.background-image>:before,.background-image>:after',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss18',
+					'selector' => '.background-image>::before,.background-image>::after',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss18',
+					'selector' => '.background-image>::before',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.background-image::first-letter',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.subscribe:active',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.subscribe:focus',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.subscribe:hover',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => 'a:visited',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.subscribe:focus-within',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
+				[
+					'hash' => 'a21ss25',
+					'selector' => '.subscribe:focus-visible',
+					'url' => 'images/maxime-lebrun-6g3Akg708E0-unsplash.jpg'
+				],
 			],
 		],
 		'expected' => [
@@ -58,7 +123,59 @@ return [
 			[
 				'selector' => 'body',
 				'style' => ':root{--wpr-bg-a122ad12df3: http://example.org/test;}'
-			]
+			],
+			[
+				'selector' => 'body',
+        		'style' => ':root{--wpr-bg-a122ad12df3: http://example.org/test;}'
+			],
+			[
+				'selector' => 'dd:nth-last-of-type(3n)',
+				'style' => ':root{--wpr-bg-a21ss2: images/underline.png;}'
+			],
+			[
+				'selector' => '.background-image~*',
+				'style' => ':root{--wpr-bg-a21ss4: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.background-image>*',
+				'style' => ':root{--wpr-bg-a21ss8: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.background-image>*',
+				'style' => ':root{--wpr-bg-a21ss18: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.background-image>*',
+				'style' => ':root{--wpr-bg-a21ss18: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.background-image',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.subscribe',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.subscribe',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.subscribe',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => 'a',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.subscribe',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
+			[
+				'selector' => '.subscribe',
+				'style' => ':root{--wpr-bg-a21ss25: images/maxime-lebrun-6g3Akg708E0-unsplash.jpg;}'
+			],
 		]
 	],
 ];
