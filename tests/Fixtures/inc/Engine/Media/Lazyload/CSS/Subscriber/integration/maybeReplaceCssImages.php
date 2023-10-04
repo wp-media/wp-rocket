0374fcd9bd054ed438b98f090f5e77dd09e96b40
Gael <gael@wp-media.me>
2023-10-04 18:43:53 +0700

    Test changes

--- tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Subscriber/integration/maybeReplaceCssImages.php
+++ tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Subscriber/integration/maybeReplaceCssImages.php
@@ -31,7 +31,21 @@ return [
 					'url' => 'https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/styles/no_background.css',
 					'response' => [ 'body' => file_get_contents(__DIR__ . '/CSS/no_background.css'), 'response' => ['code' => 200 ] ],
 				],
-				'excluded' => []
+				'excluded' => [],
+				'hash_mapping' => [
+					"url('http://example.org/wp-content/rocket-test-data/images/test.png')" => '15ef8',
+					"url('http://example.org/test.png')" => '16ef9',
+					"url('https://upload.wikimedia.org/wikipedia/commons/1/11/Test-Logo.svg')" => '17ef10',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/paper.jpeg')" => '18ef11',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/styles/./wp-content/rocket-test-data/images/test.png')" => '19ef12',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/styles/../rocket-test-data/images/papertest.jpeg')" => '20ef13',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/papertest.jpeg')" => '21ef14',
+					"url('http://example.org/wp-content/rocket-test-data/images/testnotExist.png')" => '22ef15',
+					"url('http://example.org/wp-content/rocket-test-data/images/butterfly.avif')" => '23ef16',
+					"url('http://example.org/wp-content/rocket-test-data/images/file_example_TIFF_1MB.tiff')" => '24ef17',
+					"url('http://example.org/wp-content/rocket-test-data/images/paper.jpeg')" => '25ef18',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/test.png')" => '26ef19',
+				],
 			],
 			'expected' => [
 				'output' => $html_filtered,
@@ -77,7 +91,21 @@ return [
 				'excluded' => [
 					'.external-css-background-images',
 					'http://example.org/wp-content/rocket-test-data/styles/excluded.css'
-				]
+				],
+				'hash_mapping' => [
+					"url('http://example.org/wp-content/rocket-test-data/images/test.png')" => '15ef8',
+					"url('http://example.org/test.png')" => '16ef9',
+					"url('https://upload.wikimedia.org/wikipedia/commons/1/11/Test-Logo.svg')" => '17ef10',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/paper.jpeg')" => '18ef11',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/styles/./wp-content/rocket-test-data/images/test.png')" => '19ef12',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/styles/../rocket-test-data/images/papertest.jpeg')" => '20ef13',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/papertest.jpeg')" => '21ef14',
+					"url('http://example.org/wp-content/rocket-test-data/images/testnotExist.png')" => '22ef15',
+					"url('http://example.org/wp-content/rocket-test-data/images/butterfly.avif')" => '23ef16',
+					"url('http://example.org/wp-content/rocket-test-data/images/file_example_TIFF_1MB.tiff')" => '24ef17',
+					"url('http://example.org/wp-content/rocket-test-data/images/paper.jpeg')" => '25ef18',
+					"url('https://new.rocketlabsqa.ovh/wp-content/rocket-test-data/images/test.png')" => '26ef19',
+				],
 			],
 			'expected' => [
 				'output' => $html_excluded,
