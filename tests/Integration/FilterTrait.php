3a150c919c44a4d7531425b1ed601fb73b5ac782
COQUARD Cyrille <coquardcyr@gmail.com>
2023-11-09 10:33:18 +0100

    Fix filter isolation

--- tests/Integration/FilterTrait.php
+++ tests/Integration/FilterTrait.php
@@ -2,6 +2,7 @@
 
 namespace WP_Rocket\Tests\Integration;
 
+use ReflectionClass;
 use ReflectionObject;
 use WP_Rocket\Tests\SettingsTrait;
 use WP_Rocket\Tests\StubTrait;
@@ -11,9 +12,17 @@ Trait FilterTrait {
 
 	protected $original_wp_filter;
 
+	protected $original_wp_priorities;
+
 	protected function unregisterAllCallbacksExcept( $event_name, $method_name, $priority = 10 ) {
 		global $wp_filter;
 		$this->original_wp_filter = $wp_filter[ $event_name ]->callbacks;
+		$wp_hooks = $wp_filter[ $event_name ];
+		$reflection = new ReflectionClass($wp_hooks);
+		$property = $reflection->getProperty('priorities');
+		$property->setAccessible(true);
+		$this->original_wp_priorities = $property->getValue($wp_hooks);
+		$priorities = $property->getValue($wp_hooks);
 
 		foreach ( $this->original_wp_filter[ $priority ] as $key => $config ) {
 
@@ -26,11 +35,28 @@ Trait FilterTrait {
 				$priority => [ $key => $config ],
 			];
 		}
+
+		foreach ($this->original_wp_priorities as $priority) {
+			if ( key_exists($priority, $wp_filter[ $event_name ]->callbacks)) {
+				continue;
+			}
+
+			$priorities = array_values(array_filter($priorities, function ($current) use ($priority) {
+				return $current !== $priority;
+			}));
+
+		}
+
+		$property->setValue($wp_hooks, $priorities);
 	}
 
 	protected function restoreWpFilter( $event_name ) {
 		global $wp_filter;
 		$wp_filter[ $event_name ]->callbacks = $this->original_wp_filter;
-
+		$wp_hooks = $wp_filter[ $event_name ];
+		$reflection = new ReflectionClass($wp_hooks);
+		$property = $reflection->getProperty('priorities');
+		$property->setAccessible(true);
+		$property->setValue($wp_hooks, $this->original_wp_priorities);
 	}
 }
