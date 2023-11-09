212b426fb982b877a42b4db3713fe846d73172d2
Rémy Perona <remperona@gmail.com>
2023-11-09 15:01:18 -0500

    update with 3.15.4 changes

--- inc/Engine/Common/Database/Queries/AbstractQuery.php
+++ inc/Engine/Common/Database/Queries/AbstractQuery.php
@@ -131,7 +131,6 @@ class AbstractQuery extends Query {
 			'status'        => 'to-submit',
 			'retries'       => 0,
 			'last_accessed' => current_time( 'mysql', true ),
-			'submitted_at'  => current_time( 'mysql', true ),
 		];
 		return $this->add_item( $item );
 	}
@@ -179,20 +178,33 @@ class AbstractQuery extends Query {
 	/**
 	 * Increment retries number and change status back to pending.
 	 *
-	 * @param int $id DB row ID.
-	 * @param int $retries Current number of retries.
+	 * @param int    $id DB row ID.
+	 * @param int    $error_code error code.
+	 * @param string $error_message error message.
 	 *
 	 * @return bool
 	 */
-	public function increment_retries( $id, $retries = 0 ) {
+	public function increment_retries( $id, int $error_code, string $error_message ) {
 		if ( ! self::$table_exists && ! $this->table_exists() ) {
 			return false;
 		}
 
+		$old = $this->get_item( $id );
+
+		$retries          = 0;
+		$previous_message = '';
+
+		if ( $old ) {
+			$retries          = $old->retries;
+			$previous_message = $old->error_message;
+		}
+
 		$update_data = [
-			'retries' => $retries + 1,
-			'status'  => 'pending',
+			'retries'       => $retries + 1,
+			'status'        => 'pending',
+			'error_message' => $previous_message . ' - ' . current_time( 'mysql', true ) . " {$error_code}: {$error_message}",
 		];
+
 		return $this->update_item( $id, $update_data );
 	}
 
@@ -274,12 +286,16 @@ class AbstractQuery extends Query {
 			return false;
 		}
 
+		$old = $this->get_item( $id );
+
+		$previous_message = $old ? $old->error_message : '';
+
 		return $this->update_item(
 			$id,
 			[
 				'status'        => 'failed',
 				'error_code'    => $error_code,
-				'error_message' => current_time( 'mysql', true ) . " {$error_code}: {$error_message}",
+				'error_message' => $previous_message . ' - ' . current_time( 'mysql', true ) . " {$error_code}: {$error_message}",
 			]
 		);
 	}
@@ -456,26 +472,6 @@ class AbstractQuery extends Query {
 		return $exists;
 	}
 
-
-	/**
-	 * Update the error message.
-	 *
-	 * @param int    $job_id Job ID.
-	 * @param int    $code Response code.
-	 * @param string $message Response message.
-	 * @param string $previous_message Previous saved message.
-	 *
-	 * @return bool
-	 */
-	public function update_message( int $job_id, int $code, string $message, string $previous_message = '' ): bool {
-		return $this->update_item(
-			$job_id,
-			[
-				'error_message' => $previous_message . ' - ' . current_time( 'mysql', true ) . " {$code}: {$message}",
-			]
-		);
-	}
-
 	/**
 	 * Change the status to be pending.
 	 *
@@ -493,10 +489,11 @@ class AbstractQuery extends Query {
 		return $this->update_item(
 			$id,
 			[
-				'job_id'     => $job_id,
-				'queue_name' => $queue_name,
-				'status'     => 'pending',
-				'is_mobile'  => $is_mobile,
+				'job_id'       => $job_id,
+				'queue_name'   => $queue_name,
+				'status'       => 'pending',
+				'is_mobile'    => $is_mobile,
+				'submitted_at' => current_time( 'mysql', true ),
 			]
 		);
 	}
