ef4485836c350573bb6d1593a76a77153177792f
Rémy Perona <remperona@gmail.com>
2023-11-20 16:24:52 -0500

    add first callbacks

--- inc/Engine/Media/AboveTheFold/Admin/Subscriber.php
+++ inc/Engine/Media/AboveTheFold/Admin/Subscriber.php
@@ -28,6 +28,34 @@ class Subscriber implements Subscriber_Interface {
 	 * @return array
 	 */
 	public static function get_subscribed_events(): array {
-		return [];
+		return [
+			'switch_theme'                  => 'truncate_atf',
+			'permalink_structure_changed'   => 'truncate_atf',
+			'rocket_domain_options_changed' => 'truncate_atf',
+			'wp_trash_post'                 => 'delete_atf',
+			'delete_post'                   => 'delete_atf',
+			'clean_post_cache'              => 'delete_atf',
+			'wp_update_comment_count'       => 'delete_atf',
+		];
+	}
+
+	/**
+	 * Truncate delete ATF DB table.
+	 *
+	 * @return void
+	 */
+	public function truncate_atf() {
+		$this->controller->truncate_atf();
+	}
+
+	/**
+	 * Delete ATF row on update Post or delete post.
+	 *
+	 * @param int $post_id The post ID.
+	 *
+	 * @return void
+	 */
+	public function delete_post_atf( $post_id ) {
+		$this->controller->delete_post_atf( $post_id );
 	}
 }
