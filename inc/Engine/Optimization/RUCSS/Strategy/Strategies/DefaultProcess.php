c1bce62ea77f6637f121b9b1143aa69896ac4564
Gaël Robin <robin.gael@gmail.com>
2023-11-28 12:49:11 +0100

    Closes #6290: Fix the last retry duration RUCSS (#6294)

--- inc/Engine/Optimization/RUCSS/Strategy/Strategies/DefaultProcess.php
+++ inc/Engine/Optimization/RUCSS/Strategy/Strategies/DefaultProcess.php
@@ -34,7 +34,7 @@ class DefaultProcess implements StrategyInterface {
 		2 => 300,  // 5 minutes
 		3 => 600,  // 10 minutes.
 		4 => 1200, // 20 minutes.
-		5 => 1800, // 30 minutes.
+		5 => 1500, // 25 minutes.
 	];
 
 	/**
@@ -42,7 +42,7 @@ class DefaultProcess implements StrategyInterface {
 	 *
 	 * @var int
 	 */
-	private $default_waiting_retry = 1800;
+	private $default_waiting_retry = 1500;
 
 	/**
 	 * Strategy Constructor.
