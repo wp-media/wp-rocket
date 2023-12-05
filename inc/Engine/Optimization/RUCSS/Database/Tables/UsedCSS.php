6496738316b5fae0f6c11d0550f3343caec8f2e7
Michael Lee <michaelleemichaellee408@gmail.com>
2023-12-05 09:22:42 +0100

    Remove abstracted methods

--- inc/Engine/Optimization/RUCSS/Database/Tables/UsedCSS.php
+++ inc/Engine/Optimization/RUCSS/Database/Tables/UsedCSS.php
@@ -2,12 +2,12 @@
 
 namespace WP_Rocket\Engine\Optimization\RUCSS\Database\Tables;
 
-use WP_Rocket\Dependencies\Database\Table;
+use WP_Rocket\Engine\Common\Database\Tables\AbstractTable;
 
 /**
  * RUCSS UsedCSS Table.
  */
-class UsedCSS extends Table {
+class UsedCSS extends AbstractTable {
 
 	/**
 	 * Table name
@@ -45,99 +45,35 @@ class UsedCSS extends Table {
 		20231031 => 'add_next_retry_time_column',
 	];
 
-	/**
-	 * Instantiate class.
-	 */
-	public function __construct() {
-		parent::__construct();
-		add_action( 'admin_init', [ $this, 'maybe_trigger_recreate_table' ], 9 );
-		add_action( 'init',  [ $this, 'maybe_upgrade' ] );
-	}
-
-	/**
-	 * Setup the database schema
+	 /**
+	 * Table schema data.
 	 *
-	 * @return void
+	 * @var   string
 	 */
-	protected function set_schema() {
-		$this->schema = "
-			id               		bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-			url              		varchar(2000)       NOT NULL default '',
-			css              		longtext                     default NULL,
-			hash             		varchar(32)                  default '',
-			error_code       		varchar(32)             NULL default NULL,
-			error_message    		longtext                NULL default NULL,
-			unprocessedcss   		longtext                NULL,
-			retries          		tinyint(1)          NOT NULL default 1,
-			is_mobile        		tinyint(1)          NOT NULL default 0,
-			job_id           		varchar(255)        NOT NULL default '',
-			queue_name       		varchar(255)        NOT NULL default '',
-			status           		varchar(255)        NOT NULL default '',
-			modified         		timestamp           NOT NULL default '0000-00-00 00:00:00',
-			last_accessed    		timestamp           NOT NULL default '0000-00-00 00:00:00',
-			submitted_at     		timestamp           NULL,
-			next_retry_time     	timestamp           NOT NULL default '0000-00-00 00:00:00',
-			PRIMARY KEY (id),
-			KEY url (url(150), is_mobile),
-			KEY modified (modified),
-			KEY last_accessed (last_accessed),
-			INDEX `status_index` (`status`(191)),
-			INDEX `error_code_index` (`error_code`(32)),
-			KEY hash (hash)";
-	}
-
-	/**
-	 * Delete all used_css which were not accessed in the last month.
-	 *
-	 * @return bool|int
-	 */
-	public function delete_old_used_css() {
-		// Get the database interface.
-		$db = $this->get_db();
-
-		// Bail if no database interface is available.
-		if ( empty( $db ) ) {
-			return false;
-		}
-
-		/**
-		 * Filters the old RUCSS deletion interval
-		 *
-		 * @param int $delete_interval Old RUCSS deletion interval in months
-		 */
-		$delete_interval = (int) apply_filters( 'rocket_rucss_delete_interval', 1 );
-
-		if ( $delete_interval <= 0 ) {
-			return false;
-		}
-
-		$prefixed_table_name = $this->apply_prefix( $this->table_name );
-		$query               = "DELETE FROM `$prefixed_table_name` WHERE `last_accessed` <= date_sub(now(), interval $delete_interval month)";
-		$rows_affected       = $db->query( $query );
-
-		return $rows_affected;
-	}
-
-	/**
-	 * Get all used_css which were not accessed in the last month.
-	 *
-	 * @return array
-	 */
-	public function get_old_used_css(): array {
-		// Get the database interface.
-		$db = $this->get_db();
-
-		// Bail if no database interface is available.
-		if ( empty( $db ) ) {
-			return false;
-		}
-
-		$prefixed_table_name = $this->apply_prefix( $this->table_name );
-		$query               = "SELECT * FROM `$prefixed_table_name` WHERE `last_accessed` <= date_sub(now(), interval 1 month)";
-		$rows_affected       = $db->get_results( $query );
-
-		return $rows_affected;
-	}
+	protected $schema_data = "
+				id               		bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+				url              		varchar(2000)       NOT NULL default '',
+				css              		longtext                     default NULL,
+				hash             		varchar(32)                  default '',
+				error_code       		varchar(32)             NULL default NULL,
+				error_message    		longtext                NULL default NULL,
+				unprocessedcss   		longtext                NULL,
+				retries          		tinyint(1)          NOT NULL default 1,
+				is_mobile        		tinyint(1)          NOT NULL default 0,
+				job_id           		varchar(255)        NOT NULL default '',
+				queue_name       		varchar(255)        NOT NULL default '',
+				status           		varchar(255)        NOT NULL default '',
+				modified         		timestamp           NOT NULL default '0000-00-00 00:00:00',
+				last_accessed    		timestamp           NOT NULL default '0000-00-00 00:00:00',
+				submitted_at     		timestamp           NULL,
+				next_retry_time     	timestamp           NOT NULL default '0000-00-00 00:00:00',
+				PRIMARY KEY (id),
+				KEY url (url(150), is_mobile),
+				KEY modified (modified),
+				KEY last_accessed (last_accessed),
+				INDEX `status_index` (`status`(191)),
+				INDEX `error_code_index` (`error_code`(32)),
+				KEY hash (hash)";
 
 	/**
 	 * Add queue columns.
@@ -224,46 +160,6 @@ class UsedCSS extends Table {
 		$index_added = $this->get_db()->query( "ALTER TABLE {$this->table_name} ADD INDEX `status_index` (`status`(191)) " );
 
 		return $this->is_success( $index_added );
-	}
-
-	/**
-	 * Remove all completed rows.
-	 *
-	 * @return bool|int
-	 */
-	public function remove_all_completed_rows() {
-		// Get the database interface.
-		$db = $this->get_db();
-
-		// Bail if no database interface is available.
-		if ( empty( $db ) ) {
-			return false;
-		}
-
-		$prefixed_table_name = $this->apply_prefix( $this->table_name );
-		return $db->query( "DELETE FROM `$prefixed_table_name` WHERE status IN ( 'failed', 'completed' )" );
-	}
-
-	/**
-	 * Returns name from table.
-	 *
-	 * @return string
-	 */
-	public function get_name() {
-		return $this->apply_prefix( $this->table_name );
-	}
-
-	/**
-	 * Trigger recreation of cache table if not exist.
-	 *
-	 * @return void
-	 */
-	public function maybe_trigger_recreate_table() {
-		if ( $this->exists() ) {
-			return;
-		}
-
-		delete_option( $this->db_version_key );
 	}
 
 	/**
