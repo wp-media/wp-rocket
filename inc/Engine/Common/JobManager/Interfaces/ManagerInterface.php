592c296ff6511ce89264f5fcc882b1b54b915eab
Michael Lee <michaelleemichaellee408@gmail.com>
2023-12-05 12:36:39 +0100

    Remove unimplemented method from interface

--- inc/Engine/Common/JobManager/Interfaces/ManagerInterface.php
+++ inc/Engine/Common/JobManager/Interfaces/ManagerInterface.php
@@ -26,14 +26,4 @@ interface ManagerInterface {
      * @return void
      */
     public function log_start_process(): void;
-
-    /**
-	 * Send the request to add url into the queue.
-	 *
-	 * @param string $url page URL.
-	 * @param boolean $is_mobile page is for mobile.
-	 *
-	 * @return void
-	 */
-    public function add_url_to_the_queue( string $url, bool $is_mobile );
 }
