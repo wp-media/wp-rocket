77a94b8777512d4cf83b620afe5a57276ae4032c
Gael Robin <robin.gael@gmail.com>
2023-12-05 07:52:48 +0100

    Closes #6301: Purging cache from post/page editor redirects to posts/pages list when using Classic

--- inc/common/admin-bar.php
+++ inc/common/admin-bar.php
@@ -22,7 +22,7 @@ function rocket_admin_bar( $wp_admin_bar ) {
 		 *
 		 * @param string $uri Current uri
 		 */
-		$referer = (string) apply_filters( 'rocket_admin_bar_referer', esc_url( $uri ) );
+		$referer = (string) apply_filters( 'rocket_admin_bar_referer', sanitize_url( $uri ) );
 		$referer = '&_wp_http_referer=' . rawurlencode( remove_query_arg( 'fl_builder', $referer ) );
 	} else {
 		$referer = '';
