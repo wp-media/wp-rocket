09663452bb45e7bc371c7ff9760b94a199aba802
Michael Lee <michaelleemichaellee408@gmail.com>
2023-12-06 12:10:20 +0100

    Added is_home method

--- inc/Engine/Common/JobManager/JobProcessor.php
+++ inc/Engine/Common/JobManager/JobProcessor.php
@@ -380,6 +380,26 @@ class JobProcessor implements LoggerAwareInterface {
 		return $config;
 	}
 
+	/**
+	 * Check if current page is the home page.
+	 *
+	 * @param string $url Current page url.
+	 *
+	 * @return bool
+	 */
+	private function is_home( string $url ): bool {
+		/**
+		 * Filters the home url.
+		 *
+		 * @since 3.11.4
+		 *
+		 * @param string  $home_url home url.
+		 * @param string  $url url of current page.
+		 */
+		$home_url = apply_filters( 'rocket_rucss_is_home_url', home_url(), $url );
+		return untrailingslashit( $url ) === untrailingslashit( $home_url );
+	}
+
     /**
      * Get pending jobs based on enabled option.
      *
