e7bcf34f6443b902a6397c380fe0e3b9d87985c7
Michael Lee <michaelleemichaellee408@gmail.com>
2023-12-10 16:31:20 +0100

    Updated Interface

--- inc/Engine/Common/JobManager/Managers/ManagerInterface.php
+++ inc/Engine/Common/JobManager/Managers/ManagerInterface.php
@@ -17,7 +17,8 @@ interface ManagerInterface {
 	  *
 	  * @param array $job_details Details related to the job..
 	  * @param object $row_details Details related to the row.
+      * @param string $optimization_type The type of optimization applied for the current job.
 	  * @return void
 	  */
-    public function process( array $job_details, $row_details ): void;
+    public function process( array $job_details, $row_details, string $optimization_type ): void;
 }
