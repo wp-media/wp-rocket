03803c9171e365f0f06b1b66fb852ea0070c3603
Michael Lee <michaelleemichaellee408@gmail.com>
2024-01-19 12:55:31 +0100

    Update tests

--- tests/Fixtures/inc/Engine/License/Renewal/displayRenewalExpiredBanner.php
+++ tests/Fixtures/inc/Engine/License/Renewal/displayRenewalExpiredBanner.php
@@ -36,7 +36,7 @@ $pricing = [
 	'renewals' => json_decode( json_encode( [
 		'extra_days'=> 15,
 		'grandfather_date'=> 1640995200,
-		'grandmother_date'=> 1672531200,
+		'grandmother_date'=> 315532800,
 		'discount_percent'=> [
 			'is_grandfather' => 20,
 			'not_grandfather'=> 0,
@@ -95,7 +95,7 @@ return [
 			'template' => 'renewal-expired-banner-ocd',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$20 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 				'disabled_date' => strtotime( 'now + 5 days' ),
 			],
 		],
@@ -119,7 +119,7 @@ return [
 			'template' => 'renewal-expired-banner-ocd',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$10 OFF</strong> immediately: you will only pay <strong>$49</strong>!',
+				'message' => 'Renew your license for 1 year now at <strong>$59</strong>.',
 				'disabled_date' => strtotime( 'now + 5 days' ),
 			],
 		],
@@ -184,7 +184,7 @@ return [
 			'template' => 'renewal-expired-banner-ocd',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$20 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 				'disabled_date' => strtotime( 'now + 5 days' ),
 			],
 		],
@@ -278,7 +278,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$20 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 			],
 		],
 	],
@@ -301,7 +301,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$10 OFF</strong> immediately: you will only pay <strong>$49</strong>!',
+				'message' => 'Renew your license for 1 year now at <strong>$59</strong>.',
 			],
 		],
 	],
@@ -387,7 +387,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$20 OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
+				'message' => 'Renew your license for 1 year now and get <strong>20% OFF</strong> immediately: you will only pay <strong>$39.2</strong>!',
 			],
 		],
 	],
@@ -410,7 +410,7 @@ return [
 			'template' => 'renewal-expired-banner',
 			'data' => [
 				'renewal_url'   => 'https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/',
-				'message' => 'Renew your license for 1 year now and get <strong>$10 OFF</strong> immediately: you will only pay <strong>$49</strong>!',
+				'message' => 'Renew your license for 1 year now at <strong>$59</strong>.',
 			],
 		],
 	],
