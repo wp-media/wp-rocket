5771940c7c4f477300e9eb61ff97fe4f4059a5fe
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2024-01-29 12:54:42 +0100

    Closes #6349: Update renewal banner with discount percentage instead of difference (#6397)

--- tests/Fixtures/inc/Engine/License/Subscriber/displayRenewalSoonBanner.php
+++ tests/Fixtures/inc/Engine/License/Subscriber/displayRenewalSoonBanner.php
@@ -38,7 +38,7 @@ $pricing = json_decode( json_encode( [
 	'renewals' => [
 		'extra_days'=> 90,
 		'grandfather_date'=> 1567296000,
-		'grandmother_date'=> 1672389000,
+		'grandmother_date'=> 315532800,
 		'discount_percent'=> [
 			'is_grandfather' => 20,
 			'not_grandfather'=> 0,
@@ -133,7 +133,7 @@ return [
 				Your <strong>WP Rocket license is about to expire</strong>: you will soon lose access to product updates and support.
 			</p>
 			<p>
-			Renew with a <strong>$9.80 discount</strong> before it is too late, you will only pay <strong>$24.50</strong>!
+			Renew with a <strong>20% discount</strong> before it is too late, you will only pay <strong>$24.50</strong>!
 			</p>
 		</div>
 		<div class="rocket-renew-cta-container">
@@ -193,7 +193,7 @@ return [
 				Your <strong>WP Rocket license is about to expire</strong>: you will soon lose access to product updates and support.
 			</p>
 			<p>
-			Renew with a <strong>$19.80 discount</strong> before it is too late, you will only pay <strong>$49.50</strong>!
+			Renew with a <strong>20% discount</strong> before it is too late, you will only pay <strong>$49.50</strong>!
 			</p>
 		</div>
 		<div class="rocket-renew-cta-container">
@@ -254,7 +254,7 @@ return [
 				Your <strong>WP Rocket license is about to expire</strong>: you will soon lose access to product updates and support.
 			</p>
 			<p>
-			Renew with a <strong>$49.80 discount</strong> before it is too late, you will only pay <strong>$124.50</strong>!
+			Renew with a <strong>20% discount</strong> before it is too late, you will only pay <strong>$124.50</strong>!
 			</p>
 		</div>
 		<div class="rocket-renew-cta-container">
