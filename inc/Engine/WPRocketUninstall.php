f14879414cf8c7d2b5e9b305961b428b6d007dd1
Gaël Robin <robin.gael@gmail.com>
2024-01-31 15:53:30 +0100

    Closes #4943: Delete transients on plugin uninstallation (#6386)

--- inc/Engine/WPRocketUninstall.php
+++ inc/Engine/WPRocketUninstall.php
@@ -73,6 +73,12 @@ class WPRocketUninstall {
 		'wp_rocket_rucss_errors_count',
 		'wpr_dynamic_lists_incompatible_plugins',
 		'rocket_divi_notice',
+		'rocket_rucss_processing',
+		'rocket_mod_pagespeed_enabled',
+		'wp_rocket_pricing',
+		'wp_rocket_pricing_timeout',
+		'wp_rocket_pricing_timeout_active',
+		'rocket_get_refreshed_fragments_cache',
 	];
 
 	/**
