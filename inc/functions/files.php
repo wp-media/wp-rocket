5adf744351a77f4d7f69e75a8718c6779f5c0571
Rémy Perona <remy@wp-media.me>
2024-02-12 09:09:17 -0500

    Fixes #6276 Prevent running the function `rocket_clean_domain()` multiple times (#6402)

--- inc/functions/files.php
+++ inc/functions/files.php
@@ -803,6 +803,10 @@ function rocket_clean_home_feeds() {
  * @param WP_Filesystem_Direct|null $filesystem Optional. Instance of filesystem handler.
  */
 function rocket_clean_domain( $lang = '', $filesystem = null ) {
+	if ( did_action( 'rocket_after_clean_domain' ) ) {
+		return;
+	}
+
 	if ( rocket_is_importing() ) {
 		return;
 	}
