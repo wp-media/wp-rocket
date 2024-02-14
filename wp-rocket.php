e0f3df6f7145cc73fe7dec96ca32549c03e5dca3
Michael Lee <michaelleemichaellee408@gmail.com>
2024-02-14 20:49:42 +0100

    Remove constant def

--- wp-rocket.php
+++ wp-rocket.php
@@ -26,7 +26,6 @@ define( 'WP_ROCKET_WP_VERSION_TESTED',     '6.3.1' );
 define( 'WP_ROCKET_PHP_VERSION',           '7.3' );
 define( 'WP_ROCKET_PRIVATE_KEY',           false );
 define( 'WP_ROCKET_SLUG',                  'wp_rocket_settings' );
-define( 'WP_ROCKET_DEBUG_SLUG',            'wp_rocket_debug' );
 define( 'WP_ROCKET_WEB_MAIN',              'https://wp-rocket.me/' );
 define( 'WP_ROCKET_WEB_API',               WP_ROCKET_WEB_MAIN . 'api/wp-rocket/' );
 define( 'WP_ROCKET_WEB_CHECK',             WP_ROCKET_WEB_MAIN . 'check_update.php' );
