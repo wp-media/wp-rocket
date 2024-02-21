6e09e310ecb907dfd91a134e03467e2c3b9d28b1
Gaël Robin <robin.gael@gmail.com>
2024-02-21 14:36:43 +0100

    Closes #6373 #6374: Add logs to rucss (#6412)

--- inc/Engine/Debug/RUCSS/Subscriber.php
+++ inc/Engine/Debug/RUCSS/Subscriber.php
@@ -4,6 +4,7 @@ namespace WP_Rocket\Engine\Debug\RUCSS;
 
 use WP_Rocket\Admin\{Options, Options_Data};
 use WP_Rocket\Event_Management\Subscriber_Interface;
+use WP_Rocket\Logger\Logger;
 
 class Subscriber implements Subscriber_Interface {
 
@@ -39,7 +40,12 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events(): array {
 		return [
-			'rocket_last_rucss_job_added_time' => [ 'log_last_added_job_time', 10, 2 ],
+			'rocket_last_rucss_job_added_time'          => [ 'log_last_added_job_time', 10, 2 ],
+			'rocket_rucss_process_pending_jobs_start'   => [ 'log_process_pending_job_start_time', 10, 1 ],
+			'rocket_rucss_process_pending_jobs_end'     => [ 'log_process_pending_job_end_time', 10, 1 ],
+			'rocket_rucss_check_job_status_end'         => [ 'log_check_job_status_end', 10, 1 ],
+			'rocket_rucss_process_on_submit_jobs_start' => [ 'log_process_on_submit_start', 10, 1 ],
+			'rocket_rucss_process_on_submit_jobs_end'   => [ 'log_process_on_submit_end', 10, 1 ],
 		];
 	}
 
@@ -51,11 +57,78 @@ class Subscriber implements Subscriber_Interface {
 	 * @return void
 	 */
 	public function log_last_added_job_time( $is_success, $timestamp ) {
-		if ( ! $is_success ) {
-			return;
-		}
+		if ( Logger::debug_enabled() ) {
+			if ( ! $is_success ) {
+				return;
+			}
 
-		$this->options->set( 'last_rucss_job_added', $timestamp );
-		$this->options_api->set( 'debug', $this->options->get_options() );
+			$this->options->set( 'last_rucss_job_added', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
+	}
+
+	/**
+	 * Saves the time when the process pending jobs started.
+	 *
+	 * @param string $timestamp Current timestamp.
+	 * @return void
+	 */
+	public function log_process_pending_job_start_time( $timestamp ) {
+		if ( Logger::debug_enabled() ) {
+			$this->options->set( 'rucss_process_pending_jobs_start', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
+	}
+
+	/**
+	 * Saves the time when the process pending jobs ended.
+	 *
+	 * @param string $timestamp Current timestamp.
+	 * @return void
+	 */
+	public function log_process_pending_job_end_time( $timestamp ) {
+		if ( Logger::debug_enabled() ) {
+			$this->options->set( 'rucss_process_pending_jobs_end', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
+	}
+
+	/**
+	 * Saves the time when the check job status ended.
+	 *
+	 * @param string $timestamp Current timestamp.
+	 * @return void
+	 */
+	public function log_check_job_status_end( $timestamp ) {
+		if ( Logger::debug_enabled() ) {
+			$this->options->set( 'rucss_check_job_status_end', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
+	}
+
+	/**
+	 * Saves the time when the process on submit jobs started.
+	 *
+	 * @param string $timestamp Current timestamp.
+	 * @return void
+	 */
+	public function log_process_on_submit_start( $timestamp ) {
+		if ( Logger::debug_enabled() ) {
+			$this->options->set( 'rucss_process_on_submit_jobs_start', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
+	}
+
+	/**
+	 * Saves the time when the process on submit jobs ended.
+	 *
+	 * @param string $timestamp Current timestamp.
+	 * @return void
+	 */
+	public function log_process_on_submit_end( $timestamp ) {
+		if ( Logger::debug_enabled() ) {
+			$this->options->set( 'rucss_process_on_submit_jobs_end', $timestamp );
+			$this->options_api->set( 'debug', $this->options->get_options() );
+		}
 	}
 }
