4ff9c0a440f3d79161ad4d878e6c96ee9942e8eb
Michael Lee <michaelleemichaellee408@gmail.com>
2024-03-07 15:46:55 +0100

    Added tests

---
+++ tests/Unit/inc/Engine/Media/AboveTheFold/WarmUp/Controller/fetchLinks.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace WP_Rocket\Tests\Unit\Inc\Engine\Media\AboveTheFold\WarmUp\Controller;
+
+use Brain\Monkey\{Filters, Functions};
+use Mockery;
+use WP_Rocket\Engine\Media\AboveTheFold\Jobs\Manager;
+use WP_Rocket\Engine\Media\AboveTheFold\WarmUp\Controller;
+use WP_Rocket\Tests\Unit\TestCase;
+
+/**
+ * @covers \WP_Rocket\Engine\Media\AboveTheFold\WarmUp\Controller::fetch_links
+ *
+ * @group Media
+ * @group ATF
+ */
+class Test_addExclusions extends TestCase {
+	private $manager;
+	private $controller;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->manager = Mockery::mock( Manager::class );
+		$this->controller = new Controller( $this->manager );
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpected( $config, $expected ) {
+
+		Functions\when( 'home_url' )->justReturn( 'https://example.org' );
+
+        Functions\expect( 'wp_remote_get' )
+            ->once()
+            ->with( 'https://example.org', $config['headers'] )
+            ->andReturn( $config['response'] );
+
+        Functions\expect( 'wp_remote_retrieve_response_code' )
+            ->once()
+            ->with( $config['response'] )
+            ->andReturn( $config['response']['response']['code'] );
+
+        if ( 200 === $config['response']['response']['code'] ) {
+            Functions\expect( 'wp_remote_retrieve_body' )
+                ->once()
+                ->with( $config['response'] )
+                ->andReturn( $config['response']['body'] );
+        }
+
+        if ( isset( $config['found_link'] ) && $config['found_link'] ) {
+
+            Functions\when( 'wp_parse_url' )->alias( function( $link ) {
+                return parse_url( $link );
+            } );
+    
+            Functions\when( 'wp_http_validate_url' )->alias( function( $link ) {
+                return false !== strpos( $link, 'https' ) ? $link : false;
+            } );
+    
+            Filters\expectApplied( 'rocket_atf_warmup_links_number' )
+                ->once()
+                ->with( 10 );
+        }
+
+		$this->assertSame(
+			$expected,
+			$this->controller->fetch_links()
+		);
+	}
+}
