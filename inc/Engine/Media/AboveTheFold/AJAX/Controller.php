25ae13bc73aaa74680b56bb397a9dd79c864301c
Rémy Perona <remperona@gmail.com>
2024-03-11 15:55:28 -0400

    add base controller and subscriber

---
+++ inc/Engine/Media/AboveTheFold/AJAX/Controller.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\Engine\Media\AboveTheFold\AJAX;
+
+class Controller {
+	/**
+	 * Add LCP data to the database
+	 *
+	 * @return void
+	 */
+	public function add_lcp_data() {
+
+	}
+}
---
+++ inc/Engine/Media/AboveTheFold/AJAX/Subscriber.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\Engine\Media\AboveTheFold\AJAX;
+
+use WP_Rocket\Event_Management\Subscriber_Interface;
+
+class Subscriber implements Subscriber_Interface {
+	/**
+	 * Controller instance
+	 *
+	 * @var Controller
+	 */
+	private $controller;
+
+	/**
+	 * Constructor
+	 *
+	 * @param Controller $controller Controller instance.
+	 */
+	public function __construct( $controller ) {
+		$this->controller = $controller;
+	}
+
+	/**
+	 * Array of events this subscriber listens to
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events() {
+		return [
+			'wp_ajax_rocket_lcp' => 'add_lcp_data',
+		];
+	}
+
+	/**
+	 * Callback for data received from lcp script
+	 *
+	 * @return void
+	 */
+	public function add_lcp_data() {
+		$this->controller->add_lcp_data();
+	}
+}
