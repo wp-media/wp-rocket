619dab9d46746b74fb8fd744568b9abe0a118c9f
Rémy Perona <remperona@gmail.com>
2024-03-13 15:08:26 -0400

    update add_lcp_data() method

--- inc/Engine/Media/AboveTheFold/AJAX/Controller.php
+++ inc/Engine/Media/AboveTheFold/AJAX/Controller.php
@@ -41,8 +41,15 @@ class Controller {
 		check_ajax_referer( 'rocket_lcp' );
 
 		if ( ! $this->context->is_allowed() ) {
+			wp_send_json_error( 'not allowed' );
 			return;
 		}
+
+		$url       = isset( $_POST['url'] ) ? esc_url_raw( wp_unslash( $_POST['url'] ) ) : '';
+		$is_mobile = isset( $_POST['is_mobile'] ) ? wp_unslash( (bool) $_POST['is_mobile'] ) : false;
+		$status    = isset( $_POST['status'] ) ? sanitize_text_field( wp_unslash( $_POST['status'] ) ) : '';
+		$lcp       = isset( $_POST['lcp'] ) ? wp_json_encode( wp_unslash( $_POST['lcp'] ), JSON_UNESCAPED_SLASHES ) : 'not found';
+		$viewport  = isset( $_POST['viewport'] ) ? wp_json_encode( wp_unslash( $viewport ), JSON_UNESCAPED_SLASHES ) : 'not found';
 
 		$item = [
 			'url'           => untrailingslashit( $url ),
@@ -53,6 +60,13 @@ class Controller {
 			'last_accessed' => current_time( 'mysql', true ),
 		];
 
-		return $this->query->add_item( $item );
+		$insert = $this->query->add_item( $item );
+
+		if ( ! $insert ) {
+			wp_send_json_error( 'error when inserting the item in the database' );
+			return;
+		}
+
+		wp_send_json_success( 'item added to the database' );
 	}
 }
