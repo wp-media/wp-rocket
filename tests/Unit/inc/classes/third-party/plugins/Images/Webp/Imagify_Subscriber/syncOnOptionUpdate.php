efa4749ff98b2b61073a5580991e0fbf3c70574b
Gael Robin <robin.gael@gmail.com>
2024-03-13 10:55:16 +0100

    Fix tests

--- tests/Unit/inc/classes/third-party/plugins/Images/Webp/Imagify_Subscriber/syncOnOptionUpdate.php
+++ tests/Unit/inc/classes/third-party/plugins/Images/Webp/Imagify_Subscriber/syncOnOptionUpdate.php
@@ -21,23 +21,23 @@ class Test_SyncOnOptionUpdate extends TestCase {
 
 		Actions\expectDone( 'rocket_third_party_webp_change' )->times( 4 );
 
-		$old_value = [ 'display_webp' => 1, 'display_webp_method' => 'a' ];
-		$new_value = [ 'display_webp' => 0, 'display_webp_method' => 'a' ];
+		$old_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'a' ];
+		$new_value = [ 'display_nextgen' => 0, 'display_nextgen_method' => 'a' ];
 
 		$subscriber->sync_on_option_update( $old_value, $new_value );
 
-		$old_value = [ 'display_webp' => 0, 'display_webp_method' => 'a' ];
-		$new_value = [ 'display_webp' => 1, 'display_webp_method' => 'a' ];
+		$old_value = [ 'display_nextgen' => 0, 'display_nextgen_method' => 'a' ];
+		$new_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'a' ];
 
 		$subscriber->sync_on_option_update( $old_value, $new_value );
 
-		$old_value = [ 'display_webp' => 0, 'display_webp_method' => 'a' ];
-		$new_value = [ 'display_webp' => 0, 'display_webp_method' => 'b' ];
+		$old_value = [ 'display_nextgen' => 0, 'display_nextgen_method' => 'a' ];
+		$new_value = [ 'display_nextgen' => 0, 'display_nextgen_method' => 'b' ];
 
 		$subscriber->sync_on_option_update( $old_value, $new_value );
 
-		$old_value = [ 'display_webp' => 1, 'display_webp_method' => 'a' ];
-		$new_value = [ 'display_webp' => 1, 'display_webp_method' => 'b' ];
+		$old_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'a' ];
+		$new_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'b' ];
 
 		$subscriber->sync_on_option_update( $old_value, $new_value );
 	}
@@ -48,8 +48,8 @@ class Test_SyncOnOptionUpdate extends TestCase {
 
 		Actions\expectDone( 'rocket_third_party_webp_change' )->never();
 
-		$old_value = [ 'display_webp' => 1, 'display_webp_method' => 'a' ];
-		$new_value = [ 'display_webp' => 1, 'display_webp_method' => 'a' ];
+		$old_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'a' ];
+		$new_value = [ 'display_nextgen' => 1, 'display_nextgen_method' => 'a' ];
 
 		$subscriber->sync_on_option_update( $old_value, $new_value );
 		$subscriber->sync_on_option_update( $old_value, $new_value );
