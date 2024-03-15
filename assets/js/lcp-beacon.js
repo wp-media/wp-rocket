24ed1bc47503fca4cd0fb48a5811d2ae46f2c0b1
Rémy Perona <remperona@gmail.com>
2024-03-15 16:06:10 -0400

    add fetch request

---
+++ assets/js/lcp-beacon.js
@@ -0,0 +1,21 @@
+(function () {
+	const data = new FormData();
+
+	data.append('action', 'rocket_lcp');
+	data.append('nonce', rocket_lcp_data.nonce);
+	data.append('url', rocket_lcp_data.url);
+	data.append('is_mobile', rocket_lcp_data.is_mobile);
+
+	fetch(rocket_lcp_data.ajax_url, {
+		method: "POST",
+		credentials: 'same-origin',
+		body: data
+	})
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+}());
