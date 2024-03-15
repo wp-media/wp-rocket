208c20ce4fb24424cc7d419eebae78de3033a508
WordPressFan <ahmed@wp-media.me>
2024-03-15 11:46:29 +0200

    make it works

--- inc/Engine/Media/ImageDimensions/Subscriber.php
+++ inc/Engine/Media/ImageDimensions/Subscriber.php
@@ -2,6 +2,7 @@
 
 namespace WP_Rocket\Engine\Media\ImageDimensions;
 
+use WP_Rocket\Buffer\Tests;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 /**
@@ -19,12 +20,20 @@ class Subscriber implements Subscriber_Interface {
 	private $dimensions;
 
 	/**
+	 * Buffer tests to run against current page, to decide if we can start the buffer or not.
+	 *
+	 * @var Tests
+	 */
+	private $buffer_tests;
+
+	/**
 	 * Subscriber constructor.
 	 *
 	 * @param ImageDimensions $dimensions Images dimensions class that handles all business logic.
 	 */
-	public function __construct( ImageDimensions $dimensions ) {
-		$this->dimensions = $dimensions;
+	public function __construct( ImageDimensions $dimensions, Tests $buffer_tests ) {
+		$this->dimensions   = $dimensions;
+		$this->buffer_tests = $buffer_tests;
 	}
 
 	/**
@@ -35,6 +44,7 @@ class Subscriber implements Subscriber_Interface {
 	public static function get_subscribed_events() {
 		return [
 			'rocket_buffer' => [ 'specify_image_dimensions', 17 ],
+			'template_redirect' => [ 'start_image_dimensions_buffer', 3 ],
 		];
 	}
 
@@ -51,5 +61,24 @@ class Subscriber implements Subscriber_Interface {
 		}
 
 		return $this->dimensions->specify_image_dimensions( $buffer );
+	}
+
+	/**
+	 * Start image dimensions buffer to add
+	 *
+	 * @return void
+	 */
+	public function start_image_dimensions_buffer() {
+		if ( empty( $_GET['wpr_imagedimensions'] ) ) {
+			return;
+		}
+
+		if ( ! $this->buffer_tests->can_process_any_buffer() ) {
+			return;
+		}
+
+		add_filter( 'rocket_specify_image_dimensions', '__return_true' );
+
+		ob_start( [ $this, 'specify_image_dimensions' ] );
 	}
 }
