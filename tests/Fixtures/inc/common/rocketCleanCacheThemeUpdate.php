10d2c70ea994e70fdbf7ae4ff3f495a4ae8b2daf
Michael Lee <michaelleemichaellee408@gmail.com>
2024-03-20 14:46:41 +0100

    Updated test

--- tests/Fixtures/inc/common/rocketCleanCacheThemeUpdate.php
+++ tests/Fixtures/inc/common/rocketCleanCacheThemeUpdate.php
@@ -55,12 +55,7 @@ return [
 				'themes' => [ 'default' ],
 			],
 			'expected'   => [
-				'rocket_clean_domain_urls' => [ 'http://example.org' ],
-				'cleaned'      => [
-					'vfs://public/wp-content/cache/wp-rocket/example.org/'                => null,
-					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/' => null,
-					'vfs://public/wp-content/cache/wp-rocket/example.org-tester-987654/'  => null,
-				],
+				'cleaned'      => [],
 				'wp_get_theme' => true,
 			],
 		],
