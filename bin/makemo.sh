2f0d2851a23ae8c540d95f929afc54e1c6545ad7
WordPressFan <ahmed@wp-media.me>
2024-04-02 15:01:08 +0200

    first version

--- bin/makemo.sh
+++ bin/makemo.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 # Create .mo files from .po files.
 # Twisted by WP-Translations.org, created by grappler.
-for file in `find . -name "*.po"` ; do /usr/local/opt/gettext/bin/msgfmt -o ${file/.po/.mo} $file ; done
+for file in `find . -name "*.po"` ; do msgfmt -o ${file/.po/.mo} $file ; done
