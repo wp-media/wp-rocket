a9843ed8c6167656cd42ea87cda1ca967cc80053
Rémy Perona <remperona@gmail.com>
2024-04-04 16:14:42 -0400

    update provider

--- inc/Engine/Admin/Database/ServiceProvider.php
+++ inc/Engine/Admin/Database/ServiceProvider.php
@@ -5,17 +5,10 @@ use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvi
 
 /**
  * Service Provider for database optimization
- *
- * @since 3.3
  */
 class ServiceProvider extends AbstractServiceProvider {
-
 	/**
-	 * The provides array is a way to let the container
-	 * know that a service is provided by this service
-	 * provider. Every service that is registered via
-	 * this service provider must have an alias added
-	 * to this array or it will be ignored.
+	 * Array of services provided by this service provider
 	 *
 	 * @var array
 	 */
@@ -26,9 +19,18 @@ class ServiceProvider extends AbstractServiceProvider {
 	];
 
 	/**
-	 * Registers the option array in the container
+	 * Check if the service provider provides a specific service.
 	 *
-	 * @since 3.3
+	 * @param string $id The id of the service.
+	 *
+	 * @return bool
+	 */
+	public function provides( string $id ): bool {
+		return in_array( $id, $this->provides, true );
+	}
+
+	/**
+	 * Registers the option array in the container
 	 *
 	 * @return void
 	 */
