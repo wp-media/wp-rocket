dc9e0b2911325970fb2b948221381c2215227e61
Michael Lee <michaelleemichaellee408@gmail.com>
2024-04-11 10:52:52 +0100

    Removed forward slash from exclusions

--- inc/Engine/Media/AboveTheFold/Frontend/Controller.php
+++ inc/Engine/Media/AboveTheFold/Frontend/Controller.php
@@ -213,7 +213,7 @@ class Controller {
 		$exclusions = array_map(
 				function ( $exclusion ) {
 					$exclusion = wp_parse_url( $exclusion );
-					return $exclusion['path'];
+					return ltrim( $exclusion['path'], '/' );
 				},
 			$exclusions
 			);
