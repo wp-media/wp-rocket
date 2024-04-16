abee9d80060974b894ffd846e4d6d857ed9d0c02
Rémy Perona <remy@wp-media.me>
2024-04-16 10:37:49 -0400

    Closes #6539 Update container to v4 (#6542)

--- inc/Addon/Varnish/ServiceProvider.php
+++ inc/Addon/Varnish/ServiceProvider.php
@@ -7,13 +7,8 @@ use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvi
  * Service provider for Varnish Addon.
  */
 class ServiceProvider extends AbstractServiceProvider {
-
 	/**
-	 * The provides array is a way to let the container
-	 * know that a service is provided by this service
-	 * provider. Every service that is registered via
-	 * this service provider must have an alias added
-	 * to this array or it will be ignored.
+	 * Array of services provided by this service provider
 	 *
 	 * @var array
 	 */
@@ -23,13 +18,24 @@ class ServiceProvider extends AbstractServiceProvider {
 	];
 
 	/**
+	 * Check if the service provider provides a specific service.
+	 *
+	 * @param string $id The id of the service.
+	 *
+	 * @return bool
+	 */
+	public function provides( string $id ): bool {
+		return in_array( $id, $this->provides, true );
+	}
+
+	/**
 	 * Registers items with the container
 	 *
 	 * @return void
 	 */
-	public function register() {
+	public function register(): void {
 		$this->getContainer()->add( 'varnish', Varnish::class );
-		$this->getContainer()->share( 'varnish_subscriber', Subscriber::class )
+		$this->getContainer()->addShared( 'varnish_subscriber', Subscriber::class )
 			->addArgument( $this->getContainer()->get( 'varnish' ) )
 			->addArgument( $this->getContainer()->get( 'options' ) )
 			->addTag( 'common_subscriber' );
--- inc/Dependencies/League/Container/Argument/ArgumentInterface.php
+++ inc/Dependencies/League/Container/Argument/ArgumentInterface.php
@@ -1,12 +1,12 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WP_Rocket\Dependencies\League\Container\Argument;
 
-interface RawArgumentInterface
+interface ArgumentInterface
 {
     /**
-     * Return the value of the raw argument.
-     *
      * @return mixed
      */
     public function getValue();
