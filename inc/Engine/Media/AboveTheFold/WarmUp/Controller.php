0328cda7510ad024d494f495658561b3fa99164d
Michael Lee <michaelleemichaellee408@gmail.com>
2024-04-17 10:55:55 +0100

    phpcs fix

--- inc/Engine/Media/AboveTheFold/WarmUp/Controller.php
+++ inc/Engine/Media/AboveTheFold/WarmUp/Controller.php
@@ -133,7 +133,7 @@ class Controller {
 				 */
 				$is_valid_url = wp_http_validate_url( $link );
 				$is_same_host = isset( $link_host['host'] ) ? $link_host['host'] === $site_host['host'] : false;
-				$is_not_home = ! Utils::is_home( $link );
+				$is_not_home  = ! Utils::is_home( $link );
 
 				return $is_valid_url && $is_same_host && $is_not_home;
 			}
