0a85128435a8ae95527fda648111120e4a9ae6cc
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2024-04-18 18:24:30 +0100

    Closes #6553: Frontend optimization applied for the wrong element in some cases (#6554)

--- tests/Fixtures/inc/Engine/Media/AboveTheFold/Frontend/Subscriber/lcp.php
+++ tests/Fixtures/inc/Engine/Media/AboveTheFold/Frontend/Subscriber/lcp.php
@@ -4,6 +4,12 @@ $html_input = file_get_contents(__DIR__ . '/HTML/input.html');
 $html_output = file_get_contents(__DIR__ . '/HTML/output.html');
 $html_output_with_preload = file_get_contents(__DIR__ . '/HTML/output_w_preload.html');
 $html_output_with_beacon = file_get_contents(__DIR__ . '/HTML/output_w_beacon.html');
+$html_input_with_bg_image_lcp = file_get_contents(__DIR__ . '/HTML/input_w_bg_image_lcp.html');
+$html_output_with_bg_image_lcp = file_get_contents(__DIR__ . '/HTML/output_w_bg_image_lcp.html');
+$html_input_with_picture_img_lcp = file_get_contents(__DIR__ . '/HTML/input_w_picture_img_lcp.html');
+$html_output_with_picture_img_lcp = file_get_contents(__DIR__ . '/HTML/output_w_picture_img_lcp.html');
+$html_input_with_img_lcp = file_get_contents(__DIR__ . '/HTML/input_w_img_lcp.html');
+$html_output_with_img_lcp = file_get_contents(__DIR__ . '/HTML/output_w_img_lcp.html');
 
 return [
 	'test_data' => [
@@ -27,7 +33,7 @@ return [
 					'viewport' => json_encode( [
 						0 => (object) [
 							'type' => 'img',
-							'src'  => 'http://example.org/wp-content/uploads/image.jpg',
+							'src'  => 'http://example.org/wp-content/uploads/image2.jpg',
 						],
 					] ),
 				],
@@ -48,5 +54,77 @@ return [
 			],
 			'expected' => $html_output,
 		],
+	],
+	'shouldNotApplyFetchPriorityToTheWrongElement' => [
+		'config' => [
+			'html' => $html_input_with_bg_image_lcp,
+			'row' => [
+				'status' => 'completed',
+				'url' => 'http://example.org',
+				'lcp'      => json_encode( (object) [
+					'type' => 'img',
+					'src'  => 'http://example.org/wp-content/uploads/image.jpg',
+				] ),
+				'viewport' => json_encode( [
+					0 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image2.jpg',
+					],
+					1 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image3.jpg',
+					],
+				] ),
+			],
+		],
+		'expected' => $html_output_with_bg_image_lcp,
+	],
+	'shouldApplyFetchPriorityToTheImgTagWithPictureElement' => [
+		'config' => [
+			'html' => $html_input_with_picture_img_lcp,
+			'row' => [
+				'status' => 'completed',
+				'url' => 'http://example.org',
+				'lcp'      => json_encode( (object) [
+					'type' => 'img',
+					'src'  => 'http://example.org/wp-content/uploads/image.jpg',
+				] ),
+				'viewport' => json_encode( [
+					0 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image2.jpg',
+					],
+					1 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image3.jpg',
+					],
+				] ),
+			],
+		],
+		'expected' => $html_output_with_picture_img_lcp,
+	],
+	'shouldApplyFetchPriorityToTheImgElement' => [
+		'config' => [
+			'html' => $html_input_with_img_lcp,
+			'row' => [
+				'status' => 'completed',
+				'url' => 'http://example.org',
+				'lcp'      => json_encode( (object) [
+					'type' => 'img',
+					'src'  => 'http://example.org/wp-content/uploads/image.jpg',
+				] ),
+				'viewport' => json_encode( [
+					0 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image2.jpg',
+					],
+					1 => (object) [
+						'type' => 'img',
+						'src'  => 'http://example.org/wp-content/uploads/image3.jpg',
+					],
+				] ),
+			],
+		],
+		'expected' => $html_output_with_img_lcp,
 	],
 ];
