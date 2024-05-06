692321059af622320d983fbd0b79bfdf447fe69a
Rémy Perona <remperona@gmail.com>
2024-05-06 10:35:09 -0400

    update default value

--- inc/Engine/Media/AboveTheFold/WarmUp/APIClient.php
+++ inc/Engine/Media/AboveTheFold/WarmUp/APIClient.php
@@ -30,7 +30,7 @@ class APIClient extends BaseAPIClient {
 			'optimization_list' => '',
 			'is_home'           => $is_home,
 			'is_mobile'         => 'mobile' === $device,
-			'blocking'          => rocket_get_constant( 'WP_ROCKET_DEBUG' ),
+			'blocking'          => rocket_get_constant( 'WP_ROCKET_DEBUG', false ),
 		];
 
 		return $this->add_to_queue( $url, $config );
