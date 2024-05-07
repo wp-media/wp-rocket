7f1ae66863cde42f1cbb2f1dba0639fbdd27e368
Rémy Perona <remperona@gmail.com>
2024-05-07 10:41:14 -0400

    ignore 3rd party functions

--- inc/Engine/Capabilities/Manager.php
+++ inc/Engine/Capabilities/Manager.php
@@ -144,7 +144,8 @@ class Manager implements ActivationInterface, DeactivationInterface {
 	 * Add WP Rocket as a cap group in Members
 	 */
 	public function add_cap_group_to_members() {
-		members_register_cap_group(
+		// @phpstan-ignore-next-line
+		\members_register_cap_group(
 			'wp_rocket',
 			[
 				'label'    => esc_html( 'WP Rocket' ),
@@ -159,7 +160,8 @@ class Manager implements ActivationInterface, DeactivationInterface {
 	 */
 	public function add_caps_to_members() {
 		foreach ( $this->get_capabilities() as $cap ) {
-			members_register_cap( $cap, [ 'label' => $cap ] );
+			// @phpstan-ignore-next-line
+			\members_register_cap( $cap, [ 'label' => $cap ] );
 		}
 	}
 
