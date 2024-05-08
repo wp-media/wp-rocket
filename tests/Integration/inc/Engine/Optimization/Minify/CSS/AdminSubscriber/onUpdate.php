11ff71abb2a2301aa8452a45ae88a76630e63d1d
Rémy Perona <remperona@gmail.com>
2024-05-08 16:58:26 -0400

    update on_update test

---
+++ tests/Integration/inc/Engine/Optimization/Minify/CSS/AdminSubscriber/onUpdate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Engine\Optimization\Minify\CSS\AdminSubscriber;
+
+use WP_Rocket\Tests\Integration\TestCase;
+use Brain\Monkey\Functions;
+
+/**
+ * Test class covering \WP_Rocket\Engine\Optimization\Minify\CSS\AdminSubscriber::on_update
+ */
+class Test_OnUpdate extends TestCase {
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldDoAsExpected( $config, $expected ) {
+		if ( $expected ) {
+			Functions\expect( 'rocket_clean_domain' );
+		} else {
+			Functions\expect( 'rocket_clean_domain' )->never();
+		}
+
+		do_action( 'wp_rocket_upgrade', '3.15', $config['old_version'] );
+	}
+}
