91d30c9cac6c431b6717009f4d3a3868acdff8b3
Rémy Perona <remperona@gmail.com>
2024-05-16 13:53:38 -0400

    update test setup

--- tests/Integration/inc/Engine/Common/JobManager/Cron/Subscriber/checkJobStatus.php
+++ tests/Integration/inc/Engine/Common/JobManager/Cron/Subscriber/checkJobStatus.php
@@ -9,53 +9,57 @@ use WP_Rocket\Tests\Integration\FilesystemTestCase;
  *
  * @group JobManager
  */
-class Test_checkJobStatus extends FilesystemTestCase {
-
+class Test_CheckJobStatus extends FilesystemTestCase {
 	protected $path_to_test_data = '/inc/Engine/Common/JobManager/Cron/Subscriber/checkJobStatus.php';
 
 	protected $config;
 
-	public function set_up()
-	{
+	public function set_up() {
 		parent::set_up();
-		add_filter('pre_http_request', [$this, 'mock_http'], 10, 3);
-		add_filter('rocket_rucss_hash', [$this, 'rucss_hash']);
+
+		self::installUsedCssTable();
+		self::installPreloadCacheTable();
+
+		add_filter( 'pre_http_request', [ $this, 'mock_http' ], 10, 3 );
+		add_filter( 'rocket_rucss_hash', [ $this, 'rucss_hash' ] );
 	}
 
-	public function tear_down()
-	{
-		remove_filter('rocket_rucss_hash', [$this, 'rucss_hash']);
-		remove_filter('pre_http_request', [$this, 'mock_http']);
+	public function tear_down() {
+		self::uninstallUsedCssTable();
+		self::uninstallPreloadCacheTable();
+
+		remove_filter( 'rocket_rucss_hash', [ $this, 'rucss_hash' ] );
+		remove_filter( 'pre_http_request', [ $this, 'mock_http' ] );
 		remove_filter( 'pre_get_rocket_option_remove_unused_css', [ $this, 'set_rucss_option' ] );
 		parent::tear_down();
 	}
 
 	/**
-     * @dataProvider providerTestData
-     */
-    public function testShouldDoAsExpected( $config, $expected )
-    {
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldDoAsExpected( $config, $expected ) {
 		add_filter( 'pre_get_rocket_option_remove_unused_css', [ $this, 'set_rucss_option' ] );
 
 		$this->config = $config;
-		$id = self::addResource($config['row']);
-        do_action('rocket_saas_job_check_status', $config['row']['url'], $config['row']['is_mobile'], $config['optimization_type']);
+		self::addResource( $config['row'] );
 
-		foreach ($expected['rows'] as $row) {
-			self::assertTrue(self::resourceFound($row));
+		do_action( 'rocket_saas_job_check_status', $config['row']['url'], $config['row']['is_mobile'], $config['optimization_type'] );
+
+		foreach ( $expected['rows'] as $row ) {
+			self::assertTrue( self::resourceFound( $row ) );
 		}
-		foreach ($expected['files'] as $path => $file) {
-			self::assertSame($file['exists'], $this->filesystem->exists($path));
+		foreach ( $expected['files'] as $path => $file ) {
+			self::assertSame( $file['exists'], $this->filesystem->exists( $path ) );
 		}
-    }
+	}
 
 
-	public function mock_http($response, $args, $url) {
-		if($url === $this->config['request']['url'] && $args['method'] === $this->config['request']['method'] ) {
+	public function mock_http( $response, $args, $url ) {
+		if ( $url === $this->config['request']['url'] && $args['method'] === $this->config['request']['method'] ) {
 			return $this->config['request']['response'];
 		}
 
-		if($url === $this->config['create']['url'] && $args['method'] === $this->config['create']['method'] ) {
+		if ( $url === $this->config['create']['url'] && $args['method'] === $this->config['create']['method'] ) {
 			return $this->config['create']['response'];
 		}
 
