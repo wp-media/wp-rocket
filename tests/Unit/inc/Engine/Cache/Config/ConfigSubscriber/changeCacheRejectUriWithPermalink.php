7af022a2b88c597c43363e90c017529e4c023445
Rémy Perona <remperona@gmail.com>
2024-06-06 14:47:52 -0400

    fix array offset

--- tests/Unit/inc/Engine/Cache/Config/ConfigSubscriber/changeCacheRejectUriWithPermalink.php
+++ tests/Unit/inc/Engine/Cache/Config/ConfigSubscriber/changeCacheRejectUriWithPermalink.php
@@ -2,22 +2,24 @@
 
 namespace WP_Rocket\Tests\Unit\inc\Engine\Cache\Config\ConfigSubscriber;
 
-use Mockery;
-use WP_Rocket\Tests\Unit\TestCase;
 use Brain\Monkey\Functions;
+use Mockery;
 use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Admin\Options;
 use WP_Rocket\Engine\Cache\Config\ConfigSubscriber;
+use WP_Rocket\Tests\Unit\TestCase;
 
 /**
  * Test class covering \WP_Rocket\Engine\Cache\Config\ConfigSubscriber::change_cache_reject_uri_with_permalink
+ *
+ * @group Config
  */
 class Test_ChangeCacheRejectUriWithPermalink extends TestCase {
-    private $config_subscriber;
+	private $config_subscriber;
 
-	public function setUp() : void {
+	public function setUp(): void {
 		parent::setUp();
-		
+
 		$options_data = Mockery::mock( Options_Data::class );
 		$options      = Mockery::mock( Options::class );
 		$this->config_subscriber = new ConfigSubscriber( $options_data, $options );
@@ -31,26 +33,26 @@ class Test_ChangeCacheRejectUriWithPermalink extends TestCase {
 
 		if ( isset( $config['permalink'] ) ) {
 			$times = 2;
+
 			$maybe_trailing_slash = $config['permalink']['trailing_slash'] ? '/' : '';
 
 			$return_values[] = $config['value']['cache_reject_uri'][0] . $maybe_trailing_slash;
 
 			if ( false !== strpos( $config['value']['cache_reject_uri'][1], 'index.php' ) || '/' === $config['value']['cache_reject_uri'][1] ) {
-				unset( $return_values[1] );
+				unset( $return_values[0] );
 				$times = 1;
-			}
-			else{
+			} else {
 				$return_values[] = $config['value']['cache_reject_uri'][1] . $maybe_trailing_slash;
 			}
 
-			Functions\expect('user_trailingslashit')
-			->times($times)
+			Functions\expect( 'user_trailingslashit' )
+			->times( $times )
 			->andReturnValues( $return_values );
 		}
 
-		$this->assertSame( 
+		$this->assertSame(
 			$expected,
-			$this->config_subscriber->change_cache_reject_uri_with_permalink( $config['value'], $config['old_value'] ) 
+			$this->config_subscriber->change_cache_reject_uri_with_permalink( $config['value'], $config['old_value'] )
 		);
 	}
 }
