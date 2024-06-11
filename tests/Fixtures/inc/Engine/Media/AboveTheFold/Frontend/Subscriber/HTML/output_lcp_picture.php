82bed9f5fda5051cb38741fc900f0865d996f862
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-06-11 11:37:47 +0100

    fixed test

--- tests/Fixtures/inc/Engine/Media/AboveTheFold/Frontend/Subscriber/HTML/output_lcp_picture.php
+++ tests/Fixtures/inc/Engine/Media/AboveTheFold/Frontend/Subscriber/HTML/output_lcp_picture.php
@@ -8,7 +8,7 @@
 <html>
 <head>
 	<?php wp_head() ?>
-	<title>lcp_responsive_bg</title><link rel="preload" data-rocket-preload href="small_cat.jpg" as="image" media="(max-width: 400px)" fetchpriority="high"><link rel="preload" href="medium_cat.jpg" as="image" media="(min-width: 400.1px) and (max-width: 800px)" fetchpriority="high"><link rel="preload" href="large_cat.jpg" as="image" media="(min-width: 800.1px)" fetchpriority="high">
+	<title>lcp_responsive_bg</title><link rel="preload" data-rocket-preload as="image" href="small_cat.jpg" media="(max-width: 400px)" fetchpriority="high"><link rel="preload" data-rocket-preload as="image" href="medium_cat.jpg" media="(min-width: 400.1px) and (max-width: 800px)" fetchpriority="high"><link rel="preload" data-rocket-preload as="image" href="large_cat.jpg" media="(min-width: 800.1px)" fetchpriority="high">
 </head>
 <body>
 
