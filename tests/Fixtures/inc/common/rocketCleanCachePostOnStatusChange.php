2f5db85648b7a071f0e960fa3e54a85d546b7d6f
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-06-20 02:15:03 +0100

    Modified test class

--- tests/Fixtures/inc/common/rocketCleanCachePostOnStatusChange.php
+++ tests/Fixtures/inc/common/rocketCleanCachePostOnStatusChange.php
@@ -1,23 +1,21 @@
 <?php
 
 return [
-	'test_data' => [
-		'testShouldBailOutWhenPublicTypeIsFalse' => [
-			'config' => [
-				'post_data' => [
-					'ID' => 1,
-					'post_name' => 'test',
-					'url' => '/test-page/',
-					'post_type' => 'post',
-					'next_post_id' => 2,
-					'post_author' => 1,
-					'post_status' => 'draft'
-				],
-				'post_type_public' => [
-					'public' => false
-				],
+	'testShouldBailOutWhenPublicTypeIsFalse' => [
+		'config' => [
+			'post_data' => [
+				'ID' => 1,
+				'post_name' => 'test',
+				'url' => '/test-page/',
+				'post_type' => 'post',
+				'next_post_id' => 2,
+				'post_author' => 1,
+				'post_status' => 'draft'
 			],
-			'expected' => []
-		]
+			'post_type_public' => [
+				'public' => false
+			],
+		],
+		'expected' => []
 	]
 ];
--- tests/Unit/inc/common/rocketCleanCachePostOnStatusChange.php
+++ tests/Unit/inc/common/rocketCleanCachePostOnStatusChange.php
@@ -3,7 +3,7 @@
 namespace WP_Rocket\Tests\Unit\inc\common;
 
 use Brain\Monkey\Functions;
-
+use WP_Rocket\Tests\Unit\TestCase;
 
 /**
  * Test class covering ::rocket_clean_post_cache_on_status_change
@@ -12,13 +12,24 @@ use Brain\Monkey\Functions;
  * @group  Common
  * @group  Purge
  */
-class TestRocketCleanCachePostOnStatusChange extends TestCase {
+class Test_RocketCleanCachePostOnStatusChange extends TestCase {
 	protected $path_to_test_data = 'rocketCleanCachePostOnStatusChange.php';
+
+	public function setUp() : void {
+		parent::setUp();
+
+		Functions\expect( 'get_option' )->with( 'stylesheet' )->andReturn( 'twentytwelve' );
+
+		// Load the file once.
+		if ( ! function_exists( 'rocket_clean_post_cache_on_status_change' ) ) {
+			require_once WP_ROCKET_PLUGIN_ROOT . 'inc/common/purge.php';
+		}
+	}
 
 	/**
 	 * @dataProvider configTestData
 	 */
-	public function testShouldBailOutWhenPublicTypeIsFalse( $config, $expected ) {
+	public function testShouldReturnAsExpected( $config, $expected ) {
 		$post             = (object) $config['post_data'] ?? [];
 		$post_id          = $config['post_data']['ID'] ?? 0;
 		$post_type_public = isset( $config['post_type_public'] ) ? (object) $config['post_type_public'] : null; //post_type_public
