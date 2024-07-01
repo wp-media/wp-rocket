41d2494a925ae5d74b7ca70028b96fac61bda128
Gael Robin <robin.gael@gmail.com>
2024-07-01 04:25:33 +0200

    Change phpstan level + bootstrap file

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+	define('WP_ROCKET_PLUGIN_NAME', 'WP Rocket');
+	define('WP_ROCKET_PLUGIN_SLUG', 'wp-rocket');
+
+	define('AUTH_COOKIE', 'default_auth_cookie');
+	define('LOGGED_IN_COOKIE', 'default_logged_in_cookie');
+	define('PASS_COOKIE', 'default_pass_cookie');
+	define('SECURE_AUTH_COOKIE', 'default_secure_auth_cookie');
+	define('USER_COOKIE', 'default_user_cookie');
