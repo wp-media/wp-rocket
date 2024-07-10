59273780a374af694916aa545da5b2461cf188af
Rémy Perona <remperona@gmail.com>
2024-07-10 16:35:30 -0400

    update test

--- tests/Unit/inc/Engine/Admin/DomainChange/Subscriber/regenerateConfiguration.php
+++ tests/Unit/inc/Engine/Admin/DomainChange/Subscriber/regenerateConfiguration.php
@@ -2,65 +2,69 @@
 
 namespace WP_Rocket\Tests\Unit\inc\Engine\Admin\DomainChange\Subscriber;
 
+use Brain\Monkey\{Actions, Functions};
 use Mockery;
 use WP_Rocket\Engine\Admin\Beacon\Beacon;
 use WP_Rocket\Engine\Admin\DomainChange\Subscriber;
 use WP_Rocket\Engine\Common\Ajax\AjaxHandler;
-use Brain\Monkey\Functions;
-use Brain\Monkey\Actions;
 use WP_Rocket\Tests\Unit\TestCase;
 
 /**
  * Test class covering \WP_Rocket\Engine\Admin\DomainChange\Subscriber::regenerate_configuration
  */
-class Test_regenerateConfiguration extends TestCase {
-
-    /**
-     * @var Mockery\MockInterface|AjaxHandler
-     */
-    protected $ajax_handler;
+class Test_RegenerateConfiguration extends TestCase {
+	/**
+	 * @var Mockery\MockInterface|AjaxHandler
+	 */
+	protected $ajax_handler;
 
 	/**
 	 * @var Beacon
 	 */
 	protected $beacon;
 
-    /**
-     * @var Subscriber
-     */
-    protected $subscriber;
+	/**
+	 * @var Subscriber
+	 */
+	protected $subscriber;
 
-    public function set_up() {
-        parent::set_up();
-        $this->ajax_handler = Mockery::mock(AjaxHandler::class);
-		$this->beacon       = Mockery::mock(Beacon::class);
+	public function set_up() {
+		parent::set_up();
 
-        $this->subscriber = new Subscriber($this->ajax_handler, $this->beacon);
-    }
+		$this->ajax_handler = Mockery::mock( AjaxHandler::class );
+		$this->beacon       = Mockery::mock( Beacon::class );
+		$this->subscriber   = new Subscriber( $this->ajax_handler, $this->beacon );
+	}
 
-    /**
-     * @dataProvider configTestData
-     */
-    public function testShouldDoAsExpected( $config, $expected )
-    {
-	    Functions\expect('get_option')->with('home')->andReturn($config['home_url']);
-		Functions\when('trailingslashit')->returnArg();
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldDoAsExpected( $config, $expected ) {
+		Functions\expect( 'get_option' )
+			->with( 'home' )
+			->andReturn( $config['home_url'] );
+		Functions\when( 'trailingslashit' )->returnArg();
 
-		$this->ajax_handler->shouldReceive('validate_referer')->with('rocket_regenerate_configuration', 'rocket_manage_options')->andReturn($config['is_validated']);
+		$this->ajax_handler->shouldReceive( 'validate_referer' )
+			->with( 'rocket_regenerate_configuration', 'rocket_manage_options' )
+			->andReturn( $config['is_validated'] );
 
-		if( $config['is_validated']) {
-			Functions\expect("get_transient")->with('rocket_domain_changed')->andReturn($config['transient']);
+		if ( $config['is_validated'] ) {
+			Functions\expect( 'get_transient' )
+				->with( 'rocket_domain_changed' )
+				->andReturn( $config['transient'] );
 		}
 
-		if($config['is_validated'] && $config['transient']) {
-			Actions\expectDone('rocket_domain_changed')->with($config['home_url'], $config['last_base_url']);
-			Functions\expect('delete_transient')->with('rocket_domain_changed');
-			$this->ajax_handler->shouldReceive('redirect');
-		} else {
-			$this->expectNotToPerformAssertions();
+		if ( $config['is_validated'] && $config['transient'] ) {
+			Actions\expectDone( 'rocket_domain_changed' )
+				->with( $config['home_url'], $config['last_base_url'] );
+
+			Functions\expect( 'delete_transient' )
+				->with( 'rocket_domain_changed' );
+
+			$this->ajax_handler->shouldReceive( 'redirect' );
 		}
 
-        $this->subscriber->regenerate_configuration();
-
-    }
+		$this->subscriber->regenerate_configuration();
+	}
 }
