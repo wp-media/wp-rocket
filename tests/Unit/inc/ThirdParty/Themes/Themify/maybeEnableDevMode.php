249e46fb6e468a1e29877dd64572b6133894646f
Rémy Perona <remperona@gmail.com>
2024-07-11 10:09:04 -0400

    formatting

--- tests/Unit/inc/ThirdParty/Themes/Themify/maybeEnableDevMode.php
+++ tests/Unit/inc/ThirdParty/Themes/Themify/maybeEnableDevMode.php
@@ -3,43 +3,38 @@
 namespace WP_Rocket\Tests\Unit\inc\ThirdParty\Themes\Themify;
 
 use Mockery;
-use WP_Rocket\ThirdParty\Themes\Themify;
 use WP_Rocket\Admin\Options_Data;
-
-
 use WP_Rocket\Tests\Unit\TestCase;
+use WP_Rocket\ThirdParty\Themes\Themify;
 
 /**
  * Test class covering \WP_Rocket\ThirdParty\Themes\Themify::maybe_enable_dev_mode
+ *
+ * @group ThirdyParty
  */
-class Test_maybeEnableDevMode extends TestCase {
+class TestMaybeEnableDevMode extends TestCase {
+	protected $options;
+	protected $themify;
 
-    /**
-     * @var Mockery\MockInterface
-     */
-    protected $options;
+	public function set_up() {
+		parent::set_up();
 
-    /**
-     * @var Themify
-     */
-    protected $themify;
+		$this->options = Mockery::mock(Options_Data::class);
+		$this->themify = new Themify( $this->options );
+	}
 
-    public function set_up() {
-        parent::set_up();
-        $this->options = Mockery::mock(Options_Data::class);
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnAsExpected( $config, $expected ) {
+		$this->options
+			->shouldReceive( 'get' )
+			->with( 'remove_unused_css', false )
+			->andReturn( $config['rucss_enabled'] );
 
-        $this->themify = new Themify($this->options);
-    }
-
-    /**
-     * @dataProvider configTestData
-     */
-    public function testShouldReturnAsExpected( $config, $expected )
-    {
-	    $this->options
-		    ->shouldReceive('get')
-		    ->with('remove_unused_css', false)
-		    ->andReturn($config['rucss_enabled']);
-        $this->assertSame($expected, $this->themify->maybe_enable_dev_mode($config['is_enabled']));
-    }
+		$this->assertSame(
+			$expected,
+			$this->themify->maybe_enable_dev_mode( $config['is_enabled'] )
+		);
+	}
 }
