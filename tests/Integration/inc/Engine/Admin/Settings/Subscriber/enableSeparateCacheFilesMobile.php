1b8552bbdeb697972db663ca527d42c7a73573a1
Rémy Perona <remperona@gmail.com>
2024-07-18 15:50:17 -0400

    prevent risky tests because of ATF query

--- tests/Integration/inc/Engine/Admin/Settings/Subscriber/enableSeparateCacheFilesMobile.php
+++ tests/Integration/inc/Engine/Admin/Settings/Subscriber/enableSeparateCacheFilesMobile.php
@@ -1,27 +1,33 @@
 <?php
+declare(strict_types=1);
+
 namespace WP_Rocket\Tests\Integration\inc\Engine\Admin\Settings\Subscriber;
 
 use WP_Rocket\Tests\Integration\TestCase;
 
 /**
  * @covers \WP_Rocket\Engine\Admin\Settings\Subscriber::enable_separate_cache_files_mobile
+ *
+ * @group Settings
  */
-class Test_EnableSeparateCAcheFilesMobile extends TestCase {
-
+class Test_EnableSeparateCacheFilesMobile extends TestCase {
 	public function set_up() {
 		parent::set_up();
 
 		$this->unregisterAllCallbacksExcept( 'wp_rocket_upgrade', 'enable_separate_cache_files_mobile', 10 );
+		// Disable ATF optimization to prevent DB request (unrelated to the test).
+		add_filter( 'rocket_above_the_fold_optimization', '__return_false' );
 	}
 
 	public function tear_down() {
 		parent::tear_down();
 
+		remove_filter( 'rocket_above_the_fold_optimization', '__return_false' );
 		$this->restoreWpHook( 'wp_rocket_upgrade' );
 	}
 
 	/**
-	 * @dataProvider provideTestData
+	 * @dataProvider configTestData
 	 */
 	public function testShouldEnableSeparateCacheFilesMobile( $config ) {
 		$options = get_option( 'wp_rocket_settings', [] );
@@ -39,9 +45,5 @@ class Test_EnableSeparateCAcheFilesMobile extends TestCase {
 		} else {
 			$this->assertEquals( 0, $do_caching_mobile_files );
 		}
-	}
-
-	public function provideTestData() {
-		return $this->getTestData( __DIR__, 'enableSeparateCacheFilesMobile' );
 	}
 }
