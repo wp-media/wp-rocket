773ba6fae817c66629d2624271d33907d78a251c
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-07-25 16:07:56 +0100

    update test file

--- tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/displayNoTableNotice.php
+++ tests/Fixtures/inc/Engine/Optimization/RUCSS/Admin/Subscriber/displayNoTableNotice.php
@@ -4,7 +4,7 @@ $content = <<<Notice
 <p>
 <strong>
 WP Rocket</strong>
-: Could not create the wpr_table table in the database which is necessary for the Remove Unused CSS feature to work. Please reach out to <a href="https://docs.wp-rocket.me/article/1828-could-not-create-the-rucss-usedcss-table/?utm_source=wp_plugin&#038;utm_medium=wp_rocket" target="_blank" rel="noopener">our support</a>.</p>
+: Could not create the wpr_table table in the database which is necessary for the Remove Unused CSS feature to work. Please reach out to <a href="https://docs.wp-rocket.me/article/1828-could-not-create-the-rucss-usedcss-table/?utm_source=wp_plugin&#038;utm_medium=wp_rocket" data-beacon-article="668f1284f0fdf93e4cf10825" target="_blank" rel="noopener">our support</a>.</p>
 </div>
 Notice;
 
