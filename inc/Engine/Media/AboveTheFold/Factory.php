b23a7510b6b3ead5aaddf4cf2ddb9aa97639db4b
Michael Lee <michaelleemichaellee408@gmail.com>
2024-07-25 20:36:28 +0100

    Updated concrete factory

--- inc/Engine/Media/AboveTheFold/Factory.php
+++ inc/Engine/Media/AboveTheFold/Factory.php
@@ -7,6 +7,8 @@ namespace WP_Rocket\Engine\Media\AboveTheFold;
 use WP_Rocket\Engine\Common\PerformanceHints\FactoryInterface;
 use WP_Rocket\Engine\Common\PerformanceHints\AJAX\ControllerInterface as AjaxControllerInterface;
 use WP_Rocket\Engine\Common\PerformanceHints\Frontend\ControllerInterface as FrontendControllerInterface;
+use WP_Rocket\Engine\Common\PerformanceHints\Database\Table\TableInterface;
+use WP_Rocket\Engine\Common\PerformanceHints\Database\Queries\QueriesInterface;
 use WP_Rocket\Engine\Common\Context\ContextInterface;
 
 class Factory implements FactoryInterface {
@@ -25,6 +27,20 @@ class Factory implements FactoryInterface {
 	 */
 	protected $frontend_controller;
 
+    /**
+	 * Table instance.
+	 *
+	 * @var TableInterface
+	 */
+	protected $table;
+
+    /**
+	 * Queries instance.
+	 *
+	 * @var QueriesInterface
+	 */
+	protected $queries;
+
 	/**
 	 * Context instance.
 	 *
@@ -37,11 +53,15 @@ class Factory implements FactoryInterface {
 	 *
 	 * @param AjaxControllerInterface     $ajax_controller ATF AJAX Controller instance.
 	 * @param FrontendControllerInterface $frontend_controller ATF Frontend Controller instance.
+	 * @param TableInterface $table ATF Table instance.
+	 * @param QueriesInterface $queries ATF Queries instance.
 	 * @param ContextInterface            $context ATF Context instance.
 	 */
-	public function __construct( AjaxControllerInterface $ajax_controller, FrontendControllerInterface $frontend_controller, ContextInterface $context ) {
+	public function __construct( AjaxControllerInterface $ajax_controller, FrontendControllerInterface $frontend_controller, TableInterface $table, QueriesInterface $queries, ContextInterface $context ) {
 		$this->ajax_controller     = $ajax_controller;
 		$this->frontend_controller = $frontend_controller;
+		$this->table = $table;
+		$this->queries = $queries;
 		$this->context             = $context;
 	}
 
@@ -65,16 +85,20 @@ class Factory implements FactoryInterface {
 
 	/**
 	 * Provides a Table object.
+     * 
+     * @return TableInterface
 	 */
-	public function table() {
-		// Return Table object.
+	public function table(): TableInterface {
+		return $this->table;
 	}
 
 	/**
 	 * Provides a Queries object.
+     * 
+     * @return QueriesInterface
 	 */
-	public function queries() {
-		// Return Queries object.
+	public function queries(): QueriesInterface {
+		return $this->queries;
 	}
 
 	/**
