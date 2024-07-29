31d60ab669ddfa4b9148205dc80d7fffc2819014
Michael Lee <michaelleemichaellee408@gmail.com>
2024-07-29 13:18:53 +0100

    Removed old admin subscriber and added new

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -387,7 +387,6 @@ class Plugin {
 			'lazyload_css_subscriber',
 			'weglot',
 			'cron_subscriber',
-			'atf_admin_subscriber',
 			'contactform7',
 			'debug_subscriber',
 			'rucss_cron_subscriber',
@@ -396,6 +395,7 @@ class Plugin {
 			'performance_hints_frontend_subscriber',
 			'performance_hints_cron_subscriber',
 			'performance_hints_warmup_subscriber',
+			'performance_hints_admin_subscriber',
 		];
 
 		$host_type = HostResolver::get_host_service();
