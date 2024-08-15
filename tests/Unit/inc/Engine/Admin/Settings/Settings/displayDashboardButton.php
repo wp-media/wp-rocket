60028ea9e14185a5383c83b8577a6dbebe4a81ec
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-08-15 02:33:16 +0100

    Improve codacy test coverage

---
+++ tests/Unit/inc/Engine/Admin/Settings/Settings/displayDashboardButton.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace WP_Rocket\tests\Unit\inc\Engine\Admin\Settings\Settings;
+
+use WP_Admin_Bar;
+use Brain\Monkey\Functions;
+use WP_Rocket\Abstract_Render;
+use WP_Rocket\Tests\Unit\TestCase;
+use WP_Rocket\Engine\Admin\Settings\AdminBarMenuTrait;
+
+class Test_DisplayDashboardButton extends TestCase {
+	private $admin_bar;
+
+	private $wp_admin_bar;
+
+	public static function setUpBeforeClass(): void {
+		parent::setUpBeforeClass();
+
+		require_once WP_ROCKET_TESTS_FIXTURES_DIR . '/WP_Admin_Bar.php';
+	}
+
+	public function setUp(): void {
+		parent::setUp();
+		$this->mocked_class = $this->getMockForTrait(
+			AdminBarMenuTrait::class,
+			[],
+			'',
+			true,
+			true,
+			true,
+			['generate']
+		);
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnAsExpected($config, $expected) {
+		Functions\when( 'wp_get_environment_type' )
+			->justReturn( $config['environment'] );
+
+		$data = [
+			'title' => $config['title'],
+			'action' => $config['action'],
+			'label' => $config['label'],
+			'hover_text' => $config['title_attr_text']
+		];
+
+		if ( null !== $expected ) {
+			$this->mocked_class->expects($this->once())
+				->method('generate')
+				->with('sections/clean-section', $data)
+				->willReturn($expected);
+		}
+
+		ob_start();
+		$this->mocked_class->dashboard_button(
+			$config['context'],
+			$config['title'],
+			$config['label'],
+			$config['action'],
+			$config['title_attr_text']
+		);
+		$output = ob_get_clean();
+
+		if ( null === $expected ) {
+			$this->assertEmpty( $output );
+			return;
+		}
+		$title_output = '<h4 class="wpr-title3">'. $config['label'] . '</h4>';
+		$this->assertStringContainsString($title_output, $output);
+	}
+}
