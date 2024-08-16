f4c83a4d8388a6ec0350f01ba1504564314a53e5
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-08-16 16:46:29 +0100

    Add initial unit test

---
+++ tests/Unit/inc/Engine/Optimization/LazyRenderContent/AJAX/Controller/addData.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace WP_Rocket\tests\Fixtures\inc\Engine\AJAX\Controller;
+
+use Brain\Monkey\Functions;
+use Mockery;
+use WP_Rocket\Engine\Media\AboveTheFold\Context\Context;
+use WP_Rocket\Engine\Optimization\LazyRenderContent\AJAX\Controller;
+use WP_Rocket\Engine\Optimization\LazyRenderContent\Database\Queries\LazyRenderContent;
+use WP_Rocket\Tests\Unit\TestCase;
+use function Brain\Monkey\Functions;
+
+/**
+ * Test class covering WP_Rocket\Engine\Optimization\LazyRenderContent\AJAX\Controller::add_data
+ *
+ * @group LazyRenderContent
+ */
+class Test_AddData extends TestCase {
+	private $query;
+	private $controller;
+	private $context;
+
+	private $temp_post = [];
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->query      = $this->createPartialMock( LazyRenderContent::class, [ 'add_item' ] );
+		$this->controller = new Controller( $this->query );
+		$this->context    = Mockery::mock( Context::class );
+		$this->temp_post  = $_POST;
+
+
+		$this->stubEscapeFunctions();
+	}
+
+	protected function tearDown(): void {
+		unset( $_POST );
+		$_POST = $this->temp_post;
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpected( $config, $expected ) {
+
+
+		$this->stubEscapeFunctions();
+		$this->stubTranslationFunctions();
+
+		$_POST = [
+			'url'       => addslashes( $config['url'] ),
+			'is_mobile' => addslashes( $config['is_mobile'] ),
+			'results'   => addslashes( $config['results'] ),
+			'status'    => addslashes( $config['status'] ?? 'success' ),
+		];
+
+		Functions\expect( 'check_ajax_referer' )
+			->once()
+			->with( 'rocket_beacon', 'rocket_beacon_nonce' )
+			->andReturn( true );
+
+		Functions\when( 'sanitize_text_field' )->alias(
+			function ( $value ) {
+				return is_string( $value ) ? strip_tags( $value ) : $value;
+			}
+		);
+
+		Functions\when( 'current_time' )
+			->justReturn( $expected['item']['last_accessed'] );
+
+		Functions\when( 'wp_unslash' )->alias(
+			function ( $value ) {
+				return is_string( $value ) ? stripslashes( $value ) : $value;
+			}
+		);
+
+		$this->query->method( 'add_item' )
+			->with( $expected['item'] )
+			->willReturn( $expected['result'] );
+
+		if ( ! $expected['result'] ) {
+			Functions\expect( 'wp_send_json_error' )
+				->once()
+				->with( $expected['message'] );
+		} elseif ( $expected['result'] ) {
+			Functions\expect( 'wp_send_json_success' )
+				->once()
+				->with( $expected['message'] );
+		}
+
+		$this->stubWpParseUrl();
+
+
+		$this->controller->add_data();
+	}
+}
