850a76ddda96e479341036715189596cb81411e3
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-08-19 12:27:24 +0100

    Add unit tests for LRC

--- tests/Unit/inc/Engine/Optimization/LazyRenderContent/AJAX/Controller/addData.php
+++ tests/Unit/inc/Engine/Optimization/LazyRenderContent/AJAX/Controller/addData.php
@@ -8,7 +8,6 @@ use WP_Rocket\Engine\Media\AboveTheFold\Context\Context;
 use WP_Rocket\Engine\Optimization\LazyRenderContent\AJAX\Controller;
 use WP_Rocket\Engine\Optimization\LazyRenderContent\Database\Queries\LazyRenderContent;
 use WP_Rocket\Tests\Unit\TestCase;
-use function Brain\Monkey\Functions;
 
 /**
  * Test class covering WP_Rocket\Engine\Optimization\LazyRenderContent\AJAX\Controller::add_data
@@ -44,8 +43,6 @@ class Test_AddData extends TestCase {
 	 * @dataProvider configTestData
 	 */
 	public function testShouldReturnExpected( $config, $expected ) {
-
-
 		$this->stubEscapeFunctions();
 		$this->stubTranslationFunctions();
 
@@ -61,24 +58,42 @@ class Test_AddData extends TestCase {
 			->with( 'rocket_beacon', 'rocket_beacon_nonce' )
 			->andReturn( true );
 
-		Functions\when( 'sanitize_text_field' )->alias(
-			function ( $value ) {
-				return is_string( $value ) ? strip_tags( $value ) : $value;
-			}
-		);
+		$valid_source = $expected['valid_source'] ?? [];
+
+		if(empty($valid_source)) {
+			Functions\when( 'sanitize_text_field' )->alias(
+				function ( $value ) {
+					return is_string( $value ) ? strip_tags( $value ) : $value;
+				}
+			);
+		} else{
+			Functions\when('sanitize_text_field')->alias(
+				function ($value) use ($valid_source) {
+					$arr_value = [];
+					if (!is_string($value)) {
+						foreach ($valid_source as $key => $replacement) {
+							$arr_value[] = strip_tags($replacement);
+						}
+						return (object) $arr_value;
+					}
+
+					return strip_tags($value);
+				}
+			);
+		}
 
 		Functions\when( 'current_time' )
 			->justReturn( $expected['item']['last_accessed'] );
+
+		$this->query->method( 'add_item' )
+			->with( $expected['item'] )
+			->willReturn( $expected['result'] );
 
 		Functions\when( 'wp_unslash' )->alias(
 			function ( $value ) {
 				return is_string( $value ) ? stripslashes( $value ) : $value;
 			}
 		);
-
-		$this->query->method( 'add_item' )
-			->with( $expected['item'] )
-			->willReturn( $expected['result'] );
 
 		if ( ! $expected['result'] ) {
 			Functions\expect( 'wp_send_json_error' )
