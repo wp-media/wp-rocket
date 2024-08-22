50d2a2a77586ecec9096cde9279c9f2ca3e369fe
Rémy Perona <remperona@gmail.com>
2024-08-22 09:38:36 -0400

    update bootstrap for LRC

--- tests/Integration/bootstrap.php
+++ tests/Integration/bootstrap.php
@@ -15,12 +15,13 @@ define( 'WP_ROCKET_IS_TESTING', true );
 // Manually load the plugin being tested.
 tests_add_filter(
 	'muplugins_loaded',
-	function() {
+	function () {
 
-		// Disable ATF optimization to prevent DB request (unrelated to other tests).
+		// Disable ATF & LRC optimizations to prevent DB requests (unrelated to other tests).
 		add_filter( 'rocket_above_the_fold_optimization', '__return_false' );
+		add_filter( 'rocket_lazy_render_content_optimization', '__return_false' );
 
-        if ( BootstrapManager::isGroup( 'TranslatePress' ) ) {
+		if ( BootstrapManager::isGroup( 'TranslatePress' ) ) {
 			require WP_ROCKET_TESTS_FIXTURES_DIR . '/classes/TRP_Translate_Press.php';
 			require WP_ROCKET_TESTS_FIXTURES_DIR . '/classes/TRP_Url_Converter.php';
 			require WP_ROCKET_TESTS_FIXTURES_DIR . '/classes/TRP_Settings.php';
@@ -264,6 +265,7 @@ tests_add_filter(
 
 		if ( BootstrapManager::isGroup( 'PerformanceHints' ) ) {
 			add_filter( 'rocket_above_the_fold_optimization', '__return_true' );
+			add_filter( 'rocket_lazy_render_content_optimization', '__return_true' );
 		}
 
 		// Load the plugin.
