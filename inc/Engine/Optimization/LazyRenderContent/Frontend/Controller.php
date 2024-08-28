cde0123ed87264776a1ea1a80adbf37860bb95af
Mathieu Lamiot <mathieu.lamiot@free.fr>
2024-08-28 18:36:52 +0200

    make remove_hashes public

--- inc/Engine/Optimization/LazyRenderContent/Frontend/Controller.php
+++ inc/Engine/Optimization/LazyRenderContent/Frontend/Controller.php
@@ -75,7 +75,7 @@ class Controller implements ControllerInterface {
 	 *
 	 * @return string
 	 */
-	private function remove_hashes( $html ) {
+	public function remove_hashes( $html ) {
 		$result = preg_replace( '/data-rocket-location-hash="[^"]*"/i', '', $html );
 
 		if ( null === $result ) {
