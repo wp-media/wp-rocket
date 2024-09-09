23f638d3cca60a4821bb56530d2ead653b898e1a
Rémy Perona <remperona@gmail.com>
2024-09-09 14:53:55 -0400

    cleanup

--- tests/Fixtures/inc/Engine/Common/PerformanceHints/Frontend/Subscriber/start_performance_hints_buffer.php
+++ tests/Fixtures/inc/Engine/Common/PerformanceHints/Frontend/Subscriber/start_performance_hints_buffer.php
@@ -1,25 +1,25 @@
 <?php
 return [
-    'shouldStartBufferWithWprImagedimensions' => [
-        'config' => [
-            'wpr_imagedimensions' => '1',
-        ],
-        'expected' => 1,
-    ],
-    'shouldStartBufferWithWprLazyrendercontent' => [
-        'config' => [
-            'wpr_lazyrendercontent' => '1',
-        ],
-        'expected' => 1,
-    ],
-    'shouldNotStartBufferWithNoRelevantGETParams' => [
-        'config' => [
-            'unrelated_param' => '1',
-        ],
-        'expected' => 0,
-    ],
-    'shouldNotStartBufferWithEmptyGETParams' => [
-        'config' => [],
-        'expected' => 0,
-    ],
+	'shouldStartBufferWithWprImagedimensions' => [
+		'config' => [
+			'wpr_imagedimensions' => '1',
+		],
+		'expected' => 1,
+	],
+	'shouldStartBufferWithWprLazyrendercontent' => [
+		'config' => [
+			'wpr_lazyrendercontent' => '1',
+		],
+		'expected' => 1,
+	],
+	'shouldNotStartBufferWithNoRelevantGETParams' => [
+		'config' => [
+			'unrelated_param' => '1',
+		],
+		'expected' => 0,
+	],
+	'shouldNotStartBufferWithEmptyGETParams' => [
+		'config' => [],
+		'expected' => 0,
+	],
 ];
--- tests/Integration/inc/Engine/Common/PerformanceHints/Frontend/Subscriber/start_performance_hints_buffer.php
+++ tests/Integration/inc/Engine/Common/PerformanceHints/Frontend/Subscriber/start_performance_hints_buffer.php
@@ -3,16 +3,14 @@
 namespace WP_Rocket\Tests\Integration\inc\Engine\Common\PerformanceHints\Frontend\Subscriber;
 
 use WP_Rocket\Tests\Integration\TestCase;
-use WP_Rocket\Buffer\Tests;
 
 /**
  * Test class covering \WP_Rocket\Engine\Common\PerformanceHints\Frontend\Subscriber::start_performance_hints_buffer
  *
  * @group PerformanceHints
  */
-class Test_start_performance_hints_buffer extends TestCase {
-
-    public static function set_up_before_class() {
+class Test_StartPerformanceHintsBuffer extends TestCase {
+	public static function set_up_before_class() {
 		parent::set_up_before_class();
 
 		// Install in set_up_before_class because of exists().
@@ -27,43 +25,49 @@ class Test_start_performance_hints_buffer extends TestCase {
 		parent::tear_down_after_class();
 	}
 
-    public function set_up() {
-        parent::set_up();
+	public function set_up() {
+		parent::set_up();
 
-        $this->unregisterAllCallbacksExcept( 'template_redirect', 'start_performance_hints_buffer', 3 );
-    }
+		$this->unregisterAllCallbacksExcept( 'template_redirect', 'start_performance_hints_buffer', 3 );
+	}
 
-    public function tear_down() {
-        $this->restoreWpHook( 'template_redirect' );
+	public function tear_down() {
+		$this->restoreWpHook( 'template_redirect' );
 
-        // Reset GET parameters
-        $_GET = [];
+		// Reset GET parameters
+		$_GET = [];
 
-        parent::tear_down();
-    }
+		parent::tear_down();
+	}
 
 	/**
 	 * @dataProvider configTestData
 	 */
-    public function testShouldReturnAsExpected($config, $expected) {
-        // Set up GET parameters
-        $_GET = $config;
-        
-        ob_start();
+	public function testShouldReturnAsExpected($config, $expected) {
+		// Set up GET parameters
+		$_GET = $config;
+		ob_start();
 
-        $before_ob_level = ob_get_level();
+		$before_ob_level = ob_get_level();
 
-        do_action('template_redirect');
+		do_action( 'template_redirect' );
 
-        $after_ob_level = ob_get_level();
+		$after_ob_level = ob_get_level();
 
-        $this->loadBuffer();
+		$this->loadBuffer();
 
-        $this->assertEquals($expected, $after_ob_level - $before_ob_level);
-    }
+		if ( $expected > 0 ) {
+			ob_end_clean();
+		}
 
-    protected function loadBuffer() {
-        echo '<html><head><title></title></head><body></body></html>';
-        ob_get_clean();
-    }
+		$this->assertEquals(
+			$expected,
+			$after_ob_level - $before_ob_level
+		);
+	}
+
+	protected function loadBuffer() {
+		echo '<html><head><title></title></head><body></body></html>';
+		ob_get_clean();
+	}
 }
