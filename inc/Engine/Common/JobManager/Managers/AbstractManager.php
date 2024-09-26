9a767a073f5ab5acb3ec51e232753762a3d0d25d
Gael Robin <robin.gael@gmail.com>
2024-09-26 17:09:03 +0200

    Apply linter

--- inc/Engine/Common/JobManager/Managers/AbstractManager.php
+++ inc/Engine/Common/JobManager/Managers/AbstractManager.php
@@ -190,7 +190,7 @@ trait AbstractManager {
 	 *
 	 * @param string  $url Url from DB row.
 	 * @param boolean $is_mobile Is mobile from DB row.
-	 * @param int  $error_code error code.
+	 * @param int     $error_code error code.
 	 * @param string  $error_message error message.
 	 * @param string  $previous_message Previous saved message.
 	 *
