968eefde0c671b191824ad30ee74db680fb4fac0
Gael Robin <robin.gael@gmail.com>
2024-11-06 15:22:50 +0100

    Change use of `home_url` to WPR constant

--- inc/Engine/Media/Fonts/Factory/Fonts/GoogleFontV2.php
+++ inc/Engine/Media/Fonts/Factory/Fonts/GoogleFontV2.php
@@ -29,6 +29,6 @@ class GoogleFontV2 extends GoogleFontVersion {
 	public function get_local_url(): string {
 		// To be replaced when developing the backend.
 		$hash = md5( $this->url );
-		return home_url( "/wp-content/cache/wp-rocket/fonts/google-fonts/2/{$hash}.css" );
+		return rocket_get_constant( 'WP_ROCKET_CACHE_PATH' ) . "fonts/google-fonts/2/{$hash}.css";
 	}
 }
