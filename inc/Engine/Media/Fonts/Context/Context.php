5160b3d1c671d27c5ab18b9ecb443ab308652966
Rémy Perona <remperona@gmail.com>
2024-11-08 16:58:52 -0500

    update context checking

--- inc/Engine/Media/Fonts/Context/Context.php
+++ inc/Engine/Media/Fonts/Context/Context.php
@@ -1,22 +1,28 @@
 <?php
+declare(strict_types=1);
 
 namespace WP_Rocket\Engine\Media\Fonts\Context;
 
-use WP_Rocket\Engine\Common\Context\ContextInterface;
+use WP_Rocket\Engine\Common\Context\AbstractContext;
 
-
-class Context implements ContextInterface {
+class Context extends AbstractContext {
 	/**
 	 * Checks if the feature is allowed.
 	 *
 	 * @param array $data Optional. Data to check against.
+	 *
 	 * @return bool
 	 */
 	public function is_allowed( array $data = [] ): bool {
-		if ( get_option( 'local_google_fonts' ) ) {
-			return false;
-		}
+		$is_allowed = $this->run_common_checks(
+			[
+				'do_not_optimize'    => false,
+				'bypass'             => false,
+				'option'             => 'host_fonts_locally',
+				'logged_in'          => false,
+			]
+		);
 
-		return wpm_apply_filters_typed( 'boolean', 'rocket_self_host_fonts', true );
+		return $is_allowed;
 	}
 }
