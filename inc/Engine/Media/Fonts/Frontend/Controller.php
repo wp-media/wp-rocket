0f2c05b16691c4f4ee82c42a1aa02e19a894ca9c
Gael Robin <robin.gael@gmail.com>
2024-11-08 14:56:40 +0100

    First working prototype

--- inc/Engine/Media/Fonts/Frontend/Controller.php
+++ inc/Engine/Media/Fonts/Frontend/Controller.php
@@ -45,6 +45,7 @@ class Controller {
 		}
 		$html_nocomments = $this->hide_comments( $html );
 		$v1_url          = $v2_url = '';
+		$v1_path         = $v2_path = '';
 
 		$v1_fonts = $this->find( '<link(?:\s+(?:(?!href\s*=\s*)[^>])+)?(?:\s+href\s*=\s*([\'"])(?<url>(?:https?:)?\/\/fonts\.googleapis\.com\/css[^\d](?:(?!\1).)+)\1)(?:\s+[^>]*)?>', $html_nocomments );
 		$v2_fonts = $this->find( '<link(?:\s+(?:(?!href\s*=\s*)[^>])+)?(?:\s+href\s*=\s*([\'"])(?<url>(?:https?:)?\/\/fonts\.googleapis\.com\/css2(?:(?!\1).)+)\1)(?:\s+[^>]*)?>', $html_nocomments );
@@ -56,6 +57,8 @@ class Controller {
 
 		if ( $v1_fonts ) {
 			$this->combineV1->parse( $v1_fonts );
+			$v1_url  = $this->combineV1->get_combined_url();
+			$v1_path = $this->get_local_path( $this->get_current_url(), md5( $v1_url ) );
 		}
 
 		$families = [];
@@ -71,17 +74,64 @@ class Controller {
 			$this->combineV2->parse( $v2_fonts );
 		}
 
-		$v1_url = $this->combineV1->get_combined_url();
 		if ( ! empty( $families ) ) {
-			$v2_url = $this->combineV2->get_combined_url( $families );
+			$v2_url  = $this->combineV2->get_combined_url( $families );
+			$v2_path = $this->get_local_path( $this->get_current_url(), md5( $v2_url ) );
 		}
 
-		// debug print
-		return print_r( $v1_url, true ) . ' | ' . print_r( md5( $v1_url ), true ) . ' ||| ' . print_r( $v2_url, true ) . ' | ' . print_r( md5( $v2_url ), true );
+		if ( file_exists( $v1_path ) ) {
+			$html = preg_replace( '@<\/title>@i', '$0' . $this->get_optimized_markup( $v1_path ), $html, 1 );
+		}
 
-		// Need to add check if font file is existing depending on the URL path.
-		// If it exists => change HTML to that path.
+		if ( file_exists( $v2_path ) ) {
+			$html = preg_replace( '@<\/title>@i', '$0' . $this->get_optimized_markup( $v2_path ), $html, 1 );
+		}
 
 		return $html;
+	}
+
+	/**
+	 * Get the local path for the combined URL.
+	 *
+	 * @param string $url The combined URL.
+	 * @param string $filename The filename.
+	 * @return string The local path.
+	 */
+	private function get_local_path( string $url, string $filename ): string {
+		$parsed_url = parse_url( $url );
+
+		$path       = ( ! empty( $parsed_url['path'] ) ) ? $parsed_url['path'] : '';
+		$protocol   = ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443 ) ? 'https://' : 'http://';
+		$host       = $_SERVER['HTTP_HOST'];
+		$local_path = $protocol . $host . '/wp-content/cache/wp-rocket/fonts' . $path . '/' . $filename . '.css';
+
+		return $local_path;
+	}
+
+	/**
+	 * Get the current URL.
+	 *
+	 * @return string The current URL.
+	 */
+	private function get_current_url(): string {
+		$protocol    = ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443 ) ? 'https://' : 'http://';
+		$current_url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		return $current_url;
+	}
+
+	/**
+	 * Returns the optimized markup for Google Fonts
+	 *
+	 * @since 3.18
+	 *
+	 * @param string $url Google Fonts URL.
+	 *
+	 * @return string
+	 */
+	protected function get_optimized_markup( string $url ): string {
+		return sprintf(
+			'<link rel="preload" data-rocket-preload as="style" href="%1$s" /><link rel="stylesheet" href="%1$s" media="print" onload="this.media=\'all\'" /><noscript><link rel="stylesheet" href="%1$s" /></noscript>', // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet
+			$url
+		);
 	}
 }
