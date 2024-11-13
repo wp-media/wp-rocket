d2b3c6b0e9e8357268d64de8a65fe473cbbbcd84
Rémy Perona <remperona@gmail.com>
2024-11-13 07:52:11 -0500

    remove translations for title

--- inc/Engine/Optimization/DynamicLists/ServiceProvider.php
+++ inc/Engine/Optimization/DynamicLists/ServiceProvider.php
@@ -69,20 +69,19 @@ class ServiceProvider extends AbstractServiceProvider {
 				(object) [
 					'api_client'   => $this->getContainer()->get( 'dynamic_lists_defaultlists_api_client' ),
 					'data_manager' => $this->getContainer()->get( 'dynamic_lists_defaultlists_data_manager' ),
-					'title'        => __( 'Default Lists', 'rocket' ),
+					'title'        => 'default-lists',
 				],
 			'delayjslists'         =>
 				(object) [
 					'api_client'   => $this->getContainer()->get( 'dynamic_lists_delayjslists_api_client' ),
 					'data_manager' => $this->getContainer()->get( 'dynamic_lists_delayjslists_data_manager' ),
-					'title'        => __( 'Delay JavaScript Execution Exclusion Lists', 'rocket' ),
+					'title'        => 'delay-js-lists',
 				],
 			'incompatible_plugins' =>
 				(object) [
 					'api_client'   => $this->getContainer()->get( 'dynamic_lists_incompatible_plugins_lists_api_client' ),
 					'data_manager' => $this->getContainer()->get( 'dynamic_lists_incompatible_plugins_lists_data_manager' ),
-					'title'        => __( 'Incompatible plugins Lists', 'rocket' ),
-					'clear_cache'  => false,
+					'title'        => 'incompatible-plugins-lists',
 				],
 		];
 
