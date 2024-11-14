dc07f3e2730918a3ae3a1a2a036b4f362da11a99
Gaël Robin <robin.gael@gmail.com>
2024-11-14 08:57:12 +0100

    Closes #7104: Duplicated tag added to page (#7105)
    Co-authored-by: Rémy Perona <remperona@gmail.com>

---
+++ tests/Unit/inc/Engine/Media/Fonts/Frontend/Controller/rewriteFonts.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\Tests\Unit\inc\Engine\Media\Fonts\Frontend\Controller;
+
+use Brain\Monkey\Functions;
+use Mockery;
+use WP_Rocket\Engine\Media\Fonts\Frontend\Controller;
+use WP_Rocket\Engine\Media\Fonts\Context\Context;
+use WP_Rocket\Tests\Unit\FilesystemTestCase;
+
+/**
+ * @group HostFontsLocally
+ */
+class TestRewriteFonts extends FilesystemTestCase {
+	protected $path_to_test_data = '/inc/Engine/Media/Fonts/Frontend/Controller/rewriteFonts.php';
+	private $context;
+	private $controller;
+
+	public function set_up() {
+		parent::set_up();
+
+		Functions\when( 'get_current_blog_id' )->justReturn( 1 );
+
+		$this->context    = Mockery::mock( Context::class );
+		$this->controller = new Controller( $this->context );
+
+		$this->stubWpParseUrl();
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldDoExpected( $config, $original, $expected ) {
+		$this->context->shouldReceive('is_allowed')
+			->once()
+			->andReturn( $config['is_allowed'] );
+
+		$this->assertSame(
+			$this->format_the_html( $expected ),
+			$this->format_the_html( $this->controller->rewrite_fonts( $original ) )
+		);
+	}
+}
