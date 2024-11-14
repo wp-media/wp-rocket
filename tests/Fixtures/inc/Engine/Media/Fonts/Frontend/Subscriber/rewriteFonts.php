b9a03b985edde8e63d01bab5ceaff0a81d60046b
Gael Robin <robin.gael@gmail.com>
2024-11-14 14:09:06 +0100

    Add Integration Tests

---
+++ tests/Fixtures/inc/Engine/Media/Fonts/Frontend/Subscriber/rewriteFonts.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+	'test_data' => [
+		'testShouldReturnOriginalWhenNoGoogleFonts' => [
+			'config' => [
+				'html' => '<html><body></body></html>',
+				'host_fonts_locally' => true,
+				'internal_fonts_styling' => false,
+				'css_files' => []
+			],
+
+			'expected' => [
+				'html' => '<html><body></body></html>'
+			],
+		],
+		'testShouldRewriteV1Font' => [
+			'config' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/input_v1.php' ),
+				'host_fonts_locally' => true,
+				'internal_fonts_styling' => false,
+				'css_files' => []
+			],
+			'expected' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/expected_v1.php' ),
+			],
+		],
+		'testShouldRewriteV2Font' => [
+			'config' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/input_v2.php' ),
+				'host_fonts_locally' => true,
+				'internal_fonts_styling' => false,
+				'css_files' => []
+			],
+			'expected' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/expected_v2.php' ),
+			],
+		],
+		'testShouldRewriteV1AndV2Fonts' => [
+			'config' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/input_v1_v2.php' ),
+				'host_fonts_locally' => true,
+				'internal_fonts_styling' => false,
+				'css_files' => []
+				],
+			'expected' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/expected_v1_v2.php' ),
+			],
+		],
+		'testShouldRewriteFontV1PathInStyleTag' => [
+			'config' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/input_v1.php' ),
+				'host_fonts_locally' => true,
+				'css_files' => [
+					'wp-content/cache/fonts/1/e/b/c/173c0fc97eef86a6e51ada56c5a9a.css' => 'body { font-family: "Roboto"; }',
+					'wp-content/cache/fonts/1/5/9/5/cb6ccb56826a802ed411cef875f0e.css' => 'body { font-family: "Open-San"; }',
+				],
+				'internal_fonts_styling' => true,
+			],
+			'expected' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/expected_v1_style_tag.php' ),
+			],
+		]
+	],
+
+];
---
+++ tests/Integration/inc/Engine/Media/Fronts/Frontend/Subscriber/rewriteFonts.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Engine\Media\Fonts\Frontend\Subscriber;
+
+use WP_Rocket\Tests\Integration\FilesystemTestCase;
+
+
+/**
+ * Test class covering \WP_Rocket\Engine\Media\Fonts\Frontend\Subscriber::rewrite_fonts
+ * @group  Media
+ */
+class Test_RewriteFonts extends FilesystemTestCase
+{
+	protected $path_to_test_data = '/inc/Engine/Media/Fonts/Frontend/Subscriber/rewriteFonts.php';
+
+	protected $config;
+
+	public function set_up()
+	{
+		parent::set_up();
+
+
+		$this->unregisterAllCallbacksExcept('rocket_buffer', 'rewrite_fonts', 18);
+		add_filter( 'pre_get_rocket_option_host_fonts_locally', [ $this, 'host_fonts_locally' ] );
+		add_filter( 'rocket_internal_fonts_styling', [ $this, 'internal_fonts_styling' ] );
+
+
+	}
+
+	public function tear_down() {
+		remove_filter('pre_get_rocket_option_host_fonts_locally', [$this, 'host_fonts_locally']);
+		remove_filter('rocket_internal_fonts_styling', [$this, 'internal_fonts_styling']);
+		$this->restoreWpHook('rocket_buffer');
+		parent::tear_down();
+	}
+
+	/**
+	 * @dataProvider providerTestData
+	 */
+	public function testShouldReturnAsExpected( $config, $expected ) {
+		$this->config = $config;
+
+		foreach ($config['css_files'] as $path => $file) {
+			rocket_mkdir_p(dirname($path), $this->filesystem);
+			$this->filesystem->put_contents($path, $file);
+		}
+
+		$this->assertSame(
+			$expected['html'],
+			apply_filters('rocket_buffer', $config['html'])
+		);
+	}
+
+	public function host_fonts_locally() {
+		return $this->config['host_fonts_locally'];
+	}
+
+	public function internal_fonts_styling() {
+		return $this->config['internal_fonts_styling'];
+	}
+}
