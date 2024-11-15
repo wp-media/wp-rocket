a404fd2486619c6ca42a0d8a224fbdc819e812f5
Rémy Perona <remperona@gmail.com>
2024-11-15 16:38:29 -0500

    update fixture

--- tests/Fixtures/inc/Engine/Media/Fonts/Frontend/Controller/rewriteFonts.php
+++ tests/Fixtures/inc/Engine/Media/Fonts/Frontend/Controller/rewriteFonts.php
@@ -4,60 +4,48 @@ return [
 	'test_data' => [
 		'testShouldReturnOriginalWhenNotAllowed' => [
 			'config' => [
-				'is_allowed'    => false,
-				'download_font' => 0,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => false,
+				'is_allowed' => false,
+				'write'      => false,
 			],
 			'original' => '<html><body></body></html>',
 			'expected' => '<html><body></body></html>',
 		],
 		'testShouldReturnOriginalWhenNoGoogleFonts' => [
 			'config' => [
-				'is_allowed'    => true,
-				'download_font' => 0,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => false,
+				'is_allowed' => true,
+				'write'      => false,
 			],
 			'original' => '<html><body></body></html>',
 			'expected' => '<html><body></body></html>',
 		],
 		'testShouldReturnOriginalWhenWriteFailed' => [
 			'config' => [
-				'is_allowed'    => true,
-				'download_font' => 1,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => false,
+				'is_allowed' => true,
+				'write'      => false,
 			],
 			'original' => file_get_contents( __DIR__ . '/HTML/input_v1.php' ),
 			'expected' => file_get_contents( __DIR__ . '/HTML/input_v1.php' ),
 		],
 		'testShouldRewriteV1Font' => [
 			'config' => [
-				'is_allowed'    => true,
-				'download_font' => 1,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => true,
+				'is_allowed' => true,
+				'write'      => true,
 			],
 			'original' => file_get_contents( __DIR__ . '/HTML/input_v1.php' ),
 			'expected' => file_get_contents( __DIR__ . '/HTML/expected_v1.php' ),
 		],
 		'testShouldRewriteV2' => [
 			'config' => [
-				'is_allowed'    => true,
-				'download_font' => 1,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => true,
+				'is_allowed' => true,
+				'write'      => true,
 			],
 			'original' => file_get_contents( __DIR__ . '/HTML/input_v2.php' ),
 			'expected' =>  file_get_contents( __DIR__ . '/HTML/expected_v2.php' ),
 		],
 		'testShouldRewriteV1AndV2' => [
 			'config' => [
-				'is_allowed'    => true,
-				'download_font' => 1,
-				'base_url'      => 'http://example.org/wp-content/cache',
-				'write'         => true,
+				'is_allowed' => true,
+				'write'      => true,
 			],
 			'original' => file_get_contents( __DIR__ . '/HTML/input_v1_v2.php' ),
 			'expected' => file_get_contents( __DIR__ . '/HTML/expected_v1_v2.php' ),
