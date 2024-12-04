e28b971ad445946db2592afa871877222cb9f3a7
Rémy Perona <remperona@gmail.com>
2024-12-04 11:44:18 -0500

    update hook name

--- inc/Engine/Media/Fonts/Clean/Subscriber.php
+++ inc/Engine/Media/Fonts/Clean/Subscriber.php
@@ -29,7 +29,7 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public static function get_subscribed_events(): array {
 		return [
-			'rocket_clean_domain'              => 'clean_css_fonts',
+			'rocket_after_clean_domain'        => 'clean_css_fonts',
 			'update_option_wp_rocket_settings' => [ 'clean_on_option_change', 10, 2 ],
 		];
 	}
