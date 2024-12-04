895bfe4c25a9edb45a5baac89c28440011330064
Rémy Perona <remperona@gmail.com>
2024-12-04 11:43:53 -0500

    instantiate clean classes

--- inc/Engine/Media/Fonts/ServiceProvider.php
+++ inc/Engine/Media/Fonts/ServiceProvider.php
@@ -7,6 +7,8 @@ use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvi
 use WP_Rocket\Engine\Media\Fonts\Filesystem;
 use WP_Rocket\Engine\Media\Fonts\Admin\Settings;
 use WP_Rocket\Engine\Media\Fonts\Admin\Subscriber as AdminSubscriber;
+use WP_Rocket\Engine\Media\Fonts\Clean\Clean;
+use WP_Rocket\Engine\Media\Fonts\Clean\Subscriber as CleanSubscriber;
 use WP_Rocket\Engine\Media\Fonts\Context\Context;
 use WP_Rocket\Engine\Media\Fonts\Frontend\Controller as FrontendController;
 use WP_Rocket\Engine\Media\Fonts\Frontend\Subscriber as FrontendSubscriber;
@@ -31,6 +33,8 @@ class ServiceProvider extends AbstractServiceProvider {
 		'media_fonts_context',
 		'media_fonts_frontend_controller',
 		'media_fonts_frontend_subscriber',
+		'media_fonts_clean',
+		'media_fonts_clean_subscriber',
 	];
 
 	/**
@@ -58,19 +62,23 @@ class ServiceProvider extends AbstractServiceProvider {
 		$this->getContainer()->addShared( 'media_fonts_admin_subscriber', AdminSubscriber::class )
 			->addArgument( 'media_fonts_settings' );
 
+		$this->getContainer()->add( 'media_fonts_clean', Clean::class )
+			->addArgument( 'media_fonts_filesystem' );
+
+		$this->getContainer()->addShared( 'media_fonts_clean_subscriber', CleanSubscriber::class )
+			->addArgument( 'media_fonts_clean' );
+
 		$this->getContainer()->add( 'media_fonts_context', Context::class )
-			->addArgument( $this->getContainer()->get( 'options' ) );
+			->addArgument( 'options' );
 
 		$this->getContainer()->add( 'media_fonts_frontend_controller', FrontendController::class )
 			->addArguments(
 				[
-					$this->getContainer()->get( 'media_fonts_context' ),
-					$this->getContainer()->get( 'media_fonts_filesystem' ),
+					'media_fonts_context',
+					'media_fonts_filesystem',
 				]
 			);
-		$this->getContainer()->add( 'media_fonts_frontend_subscriber', FrontendSubscriber::class )
-			->addArgument(
-				$this->getContainer()->get( 'media_fonts_frontend_controller' )
-			);
+		$this->getContainer()->addShared( 'media_fonts_frontend_subscriber', FrontendSubscriber::class )
+			->addArgument( 'media_fonts_frontend_controller' );
 	}
 }
