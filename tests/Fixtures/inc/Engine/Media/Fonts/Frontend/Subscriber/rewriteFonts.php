42dc3871b32d000faebc0fc97b1486a91e43af23
Michael Lee <michaelleemichaellee408@gmail.com>
2024-12-12 09:53:14 +0100

    Updated test

--- tests/Fixtures/inc/Engine/Media/Fonts/Frontend/Subscriber/rewriteFonts.php
+++ tests/Fixtures/inc/Engine/Media/Fonts/Frontend/Subscriber/rewriteFonts.php
@@ -134,5 +134,29 @@ return [
 				'html' => file_get_contents( __DIR__ . '/HTML/expected_v2_from_combination.php' ),
 			],
 		],
+		'testShouldExcludeFontWithRegex' => [
+			'config' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/input_v1_v2_regex.php' ),
+				'host_fonts_locally' => true,
+				'locally_inline_css' => false,
+				'http' => [
+					'https://fonts.googleapis.com/css?family=Roboto|Open+Sans' => [
+						'body' => '.roboto { font-family: "Roboto"; } .open-san { font-family: "Open-San"; }',
+						'response' => ['code' => 200 ]
+					],
+					'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@400;700&display=swap' => [
+						'body' => '.lato { font-family: "Lato"; } .montserrat { font-family: "Montserrat"; }',
+						'response' => ['code' => 200 ]
+					],
+				],
+				'exclude_locally_host_fonts' => [
+					'family=Rob(.*)o',
+					'family(.*)Montserrat(.*)display=swap'
+				]
+			],
+			'expected' => [
+				'html' => file_get_contents( __DIR__ . '/HTML/expected_v1_v2_regex.php' ),
+			],
+		],
 	]
 ];
