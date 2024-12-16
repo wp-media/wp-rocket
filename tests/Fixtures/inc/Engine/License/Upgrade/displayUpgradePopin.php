e233557d2336fedc59fe4c61ddca4ca69a228819
WordPressFan <ahmed@wp-media.me>
2024-12-16 22:12:23 +0200

    remove not needed pricing methods in tests

--- tests/Fixtures/inc/Engine/License/Upgrade/displayUpgradePopin.php
+++ tests/Fixtures/inc/Engine/License/Upgrade/displayUpgradePopin.php
@@ -24,23 +24,6 @@ return [
 		'config'   => [
 			'license_account'    => 1,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 50,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 200,
-					'regular'     => 200,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => false,
 			'upgrades' => [
 				(object) [
@@ -70,23 +53,6 @@ return [
 		'config'   => [
 			'license_account'    => 1,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 40,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 160,
-					'regular'     => 200,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => true,
 			'upgrades' => [
 				(object) [
@@ -118,23 +84,6 @@ return [
 		'config'   => [
 			'license_account'    => 2,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 50,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 200,
-					'regular'     => 200,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => false,
 			'upgrades' => [
 				(object) [
@@ -164,23 +113,6 @@ return [
 		'config'   => [
 			'license_account'    => 2,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 40,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 160,
-					'regular'     => 200,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => true,
 			'upgrades' => [
 				(object) [
@@ -212,23 +144,6 @@ return [
 		'config'   => [
 			'license_account'    => 3,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 50,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 150,
-					'regular'     => 150,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => false,
 			'upgrades' => [
 				(object) [
@@ -258,23 +173,6 @@ return [
 		'config'   => [
 			'license_account'    => 3,
 			'licence_expiration' => false,
-			'pricing'            => [
-				'single'   => [
-					'websites' => 1,
-				],
-				'plus'     => [
-					'price'       => 40,
-					'regular'     => 50,
-					'websites'    => 3,
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/plus/',
-				],
-				'infinite' => [
-					'price'       => 120,
-					'regular'     => 150,
-					'websites'    => 'Unlimited',
-					'upgrade_url' => 'https://wp-rocket.me/checkout/upgrade/roger@wp-rocket.me/d89e18ee/infinite/',
-				],
-			],
 			'promo_active' => true,
 			'upgrades' => [
 				(object) [
