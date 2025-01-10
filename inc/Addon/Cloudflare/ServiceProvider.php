248b3f2511ada9b118cd764e00f75a0cf7768d44
Rémy Perona <remperona@gmail.com>
2025-01-10 09:30:39 -0500

    revert alias removal

--- inc/Addon/Cloudflare/ServiceProvider.php
+++ inc/Addon/Cloudflare/ServiceProvider.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 namespace WP_Rocket\Addon\Cloudflare;
 
-use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Addon\Cloudflare\Admin\Subscriber as CloudflareAdminSubscriber;
 use WP_Rocket\Addon\Cloudflare\API\{Client, Endpoints};
 use WP_Rocket\Addon\Cloudflare\Cloudflare;
 use WP_Rocket\Addon\Cloudflare\Subscriber as CloudflareSubscriber;
-use WP_Rocket\Admin\Options;
 use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
 use WPMedia\Cloudflare\Auth\APIKeyFactory;
 
@@ -22,10 +20,10 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * @var array
 	 */
 	protected $provides = [
-		APIKeyFactory::class,
-		Client::class,
-		Endpoints::class,
-		Cloudflare::class,
+		'cloudflare_api_key_factory',
+		'cloudflare_client',
+		'cloudflare_endpoints',
+		'cloudflare',
 		'cloudflare_subscriber',
 		'cloudflare_admin_subscriber',
 	];
@@ -45,27 +43,27 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * Registers items with the container
 	 */
 	public function register(): void {
-		$this->getContainer()->add( APIKeyFactory::class )->addArgument( Options_Data::class );
+		$this->getContainer()->add( 'cloudflare_api_key_factory', APIKeyFactory::class )->addArgument( 'options' );
 
-		$this->getContainer()->add( Client::class )
-			->addArgument( $this->getContainer()->get( APIKeyFactory::class )->create() );
-		$this->getContainer()->add( Endpoints::class )
-			->addArgument( Client::class );
+		$this->getContainer()->add( 'cloudflare_client', Client::class )
+			->addArgument( $this->getContainer()->get( 'cloudflare_api_key_factory' )->create() );
+		$this->getContainer()->add( 'cloudflare_endpoints', Endpoints::class )
+			->addArgument( 'cloudflare_client' );
 
-		$this->getContainer()->add( Cloudflare::class )
+		$this->getContainer()->add( 'cloudflare', Cloudflare::class )
 			->addArguments(
 				[
-					Options_Data::class,
-					Endpoints::class,
+					'options',
+					'cloudflare_endpoints',
 				]
 			);
 		$this->getContainer()->addShared( 'cloudflare_subscriber', CloudflareSubscriber::class )
 			->addArguments(
 				[
-					Cloudflare::class,
-					Options_Data::class,
-					Options::class,
-					APIKeyFactory::class,
+					'cloudflare',
+					'options',
+					'options_api',
+					'cloudflare_api_key_factory',
 				]
 			);
 		$this->getContainer()->addShared( 'cloudflare_admin_subscriber', CloudflareAdminSubscriber::class );
