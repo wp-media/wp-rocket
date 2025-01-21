7aff6de2d07be6310475f37bf8b6fa2612db4e74
Michael Lee <michaelleemichaellee408@gmail.com>
2025-01-21 09:36:20 +0100

    Removed unnecessary conditional

--- inc/Engine/Common/PerformanceHints/Frontend/Processor.php
+++ inc/Engine/Common/PerformanceHints/Frontend/Processor.php
@@ -189,13 +189,10 @@ class Processor {
 		$script_url = rocket_get_constant( 'WP_ROCKET_ASSETS_JS_URL' ) . 'wpr-beacon' . $min . '.js';
 
 		// Create the script tag.
-		$script_tag = "<script data-name=\"wpr-wpr-beacon\" src='{$script_url}' async></script>"; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$body_tag   = strrpos( $html, '</body>' );
-
-		if ( false !== $body_tag ) {
-			// Append the script tag just before the last closing body tag especially in cases where there's an iframe.
-			$html = substr_replace( $html, $inline_script . $script_tag . '</body>', $body_tag, 7 );
-		}
+		$script_tag             = "<script data-name=\"wpr-wpr-beacon\" src='{$script_url}' async></script>"; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
+		$last_body_tag_position = strrpos( $html, '</body>' );
+		// Append the script tag just before the last closing body tag especially in cases where there's an iframe.
+		$html = substr_replace( $html, $inline_script . $script_tag . '</body>', $last_body_tag_position, 7 );
 
 		return $html;
 	}
