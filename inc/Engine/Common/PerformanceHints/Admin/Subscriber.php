fe949986e0e56008b2e15fd90404d4aa10b04281
Khadreal <opeyemi.khadri@gmail.com>
2025-01-23 16:02:41 +0100

    :closes: #7073 Update test, move logic to controller form subscriber

--- inc/Engine/Common/PerformanceHints/Admin/Subscriber.php
+++ inc/Engine/Common/PerformanceHints/Admin/Subscriber.php
@@ -99,9 +99,6 @@ class Subscriber implements Subscriber_Interface {
 	 * @return void
 	 */
 	public function delete_post( int $post_id ): void {
-		if ( 'attachment' === get_post_type( $post_id ) ) {
-			return;
-		}
 		$this->controller->delete_post( $post_id );
 	}
 
