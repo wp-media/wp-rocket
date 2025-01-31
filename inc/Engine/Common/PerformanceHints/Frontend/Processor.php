10e4ea7a898d70e48a200b52a3129c78461aa6d5
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2025-01-31 13:38:11 +0100

    Release 3.18.1.3 (#7268)

--- inc/Engine/Common/PerformanceHints/Frontend/Processor.php
+++ inc/Engine/Common/PerformanceHints/Frontend/Processor.php
@@ -191,8 +191,14 @@ class Processor {
 		// Create the script tag.
 		$script_tag             = "<script data-name=\"wpr-wpr-beacon\" src='{$script_url}' async></script>"; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
 		$last_body_tag_position = strrpos( $html, '</body>' );
-		// Append the script tag just before the last closing body tag especially in cases where there's an iframe.
-		$html = substr_replace( $html, $inline_script . $script_tag . '</body>', $last_body_tag_position, 7 );
+
+		if ( false !== $last_body_tag_position ) {
+			// Append the script tag just before the last closing body tag especially in cases where there's an iframe.
+			$html = substr_replace( $html, $inline_script . $script_tag . '</body>', $last_body_tag_position, 7 );
+		} else {
+			// Append to the end of html if </body> is not found.
+			$html .= $inline_script . $script_tag;
+		}
 
 		return $html;
 	}
