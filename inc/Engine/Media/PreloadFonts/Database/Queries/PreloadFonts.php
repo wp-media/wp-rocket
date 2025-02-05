56f9157c232e2d15801f288d02290834deebd730
Khadreal <opeyemi.khadri@gmail.com>
2025-02-05 18:41:48 +0100

    create deletion filter method

--- inc/Engine/Media/PreloadFonts/Database/Queries/PreloadFonts.php
+++ inc/Engine/Media/PreloadFonts/Database/Queries/PreloadFonts.php
@@ -81,13 +81,7 @@ class PreloadFonts extends AbstractQueries implements QueriesInterface {
 			return false;
 		}
 
-		/**
-		 * Filters the interval (in months) to determine when a Preload Fonts(PLF) entry is considered 'old'.
-		 * Old PLF entries are eligible for deletion. By default, a PLF entry is considered old if it hasn't been accessed in the last month.
-		 *
-		 * @param int $delete_interval The interval in months after which an PLF entry is considered old. Default is 1 month.
-		 */
-		$delete_interval = wpm_apply_filters_typed( 'integer', 'rocket_preload_fonts_cleanup_interval', 1 );
+		$delete_interval = $this->deletion_interval();
 
 		if ( $delete_interval <= 0 ) {
 			return false;
@@ -97,5 +91,15 @@ class PreloadFonts extends AbstractQueries implements QueriesInterface {
 		$query               = "DELETE FROM `$prefixed_table_name` WHERE status = 'failed' OR `last_accessed` <= date_sub(now(), interval $delete_interval month)";
 
 		return $db->query( $query );
+	}
+
+	private function deletion_interval() {
+		/**
+		 * Filters the interval (in months) to determine when a Preload Fonts(PLF) entry is considered 'old'.
+		 * Old PLF entries are eligible for deletion. By default, a PLF entry is considered old if it hasn't been accessed in the last month.
+		 *
+		 * @param int $delete_interval The interval in months after which an PLF entry is considered old. Default is 1 month.
+		 */
+		return wpm_apply_filters_typed( 'integer', 'rocket_preload_fonts_cleanup_interval', 1 );
 	}
 }
