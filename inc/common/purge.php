32e54341f274425d113fbc3cb0de9405ae816474
Gaël Robin <robin.gael@gmail.com>
2025-02-10 11:45:48 +0100

    Minor condition change

--- inc/common/purge.php
+++ inc/common/purge.php
@@ -248,7 +248,7 @@ function rocket_clean_post( $post_id, $post = null ) {
 	// Purge all files.
 	rocket_clean_files( $purge_urls );
 
-	if ( wpm_apply_filters_typed( 'boolean', 'rocket_clean_home_after_clean_post', true, $post_id ) === true ) {
+	if ( wpm_apply_filters_typed( 'boolean', 'rocket_clean_home_after_clean_post', true, $post_id ) ) {
 		// Never forget to purge homepage and their pagination.
 		rocket_clean_home( $lang );
 	}
