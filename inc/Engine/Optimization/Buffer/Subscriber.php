fcd272156a81276f9b37547ec861f6cd997e8b2d
WordPressFan <ahmed@wp-media.me>
2025-02-18 12:09:34 +0200

    change the way we add rocket_head to be inside rocket_buffer

--- inc/Engine/Optimization/Buffer/Subscriber.php
+++ inc/Engine/Optimization/Buffer/Subscriber.php
@@ -33,6 +33,7 @@ class Subscriber implements Subscriber_Interface {
 	public static function get_subscribed_events() {
 		return [
 			'template_redirect' => [ 'start_content_process', 2 ],
+			'rocket_buffer' => [ 'insert_rocket_head', 100000 ],
 		];
 	}
 
@@ -43,5 +44,30 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public function start_content_process() {
 		return $this->optimizer->maybe_init_process();
+	}
+
+	/**
+	 * Insert rocket_head into the buffer HTML
+	 *
+	 * @param string $html Buffer HTML.
+	 * @return string
+	 */
+	public function insert_rocket_head( $html ) {
+		if ( empty( $html ) ) {
+			return $html;
+		}
+
+		$filtered_buffer = preg_replace(
+			'#</title>#iU',
+			'</title>' . wpm_apply_filters_typed( 'string', 'rocket_head', '' ),
+			$html,
+			1
+		);
+
+		if ( empty( $filtered_buffer ) ) {
+			return $html;
+		}
+
+		return $filtered_buffer;
 	}
 }
