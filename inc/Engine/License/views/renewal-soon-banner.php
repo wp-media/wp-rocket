35f3cddb4ac97e6be64aea1d42172c2956c4b239
Khadreal <opeyemi.khadri@gmail.com>
2025-02-27 13:27:33 +0100

    Modify view, css

--- inc/Engine/License/views/renewal-soon-banner.php
+++ inc/Engine/License/views/renewal-soon-banner.php
@@ -17,16 +17,6 @@ $data = isset( $data ) ? $data : []; // phpcs:ignore WordPress.NamingConventions
 		<li class="rocket-countdown-item"><span class="rocket-countdown-value rocket-countdown-seconds"><?php echo esc_html( $data['countdown']['seconds'] ); ?></span> <?php esc_html_e( 'Seconds', 'rocket' ); ?></li>
 	</ul>
 	<div class="rocket-renew-message">
-		<p>
-			<?php
-			printf(
-				// translators: %1$s = <strong>, %2$s = </strong>.
-				esc_html__( 'Your %1$sWP Rocket license is about to expire%2$s: you will soon lose access to product updates and support.', 'rocket' ),
-				'<strong>',
-				'</strong>'
-			);
-			?>
-		</p>
 		<p><?php echo $data['message']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 	</div>
 	<div class="rocket-renew-cta-container">
