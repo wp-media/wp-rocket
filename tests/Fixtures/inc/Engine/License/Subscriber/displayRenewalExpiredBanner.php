2b931384f6da12506bb43aae80c87da3266ffd32
Khadreal <opeyemi.khadri@gmail.com>
2025-03-03 15:45:32 +0100

    Fixed test

--- tests/Fixtures/inc/Engine/License/Subscriber/displayRenewalExpiredBanner.php
+++ tests/Fixtures/inc/Engine/License/Subscriber/displayRenewalExpiredBanner.php
@@ -84,7 +84,8 @@ return [
 			'pricing' => $pricing,
 			'transient' => false,
 		],
-		'expected' => '<section class="rocket-renewal-expired-banner" id="rocket-renewal-banner"><div class="banner-copy">
+		'expected' => '<section class="rocket-renewal-expired-banner" id="rocket-renewal-banner">
+		<div class="banner-copy">
 		<h3 class="rocket-expired-title">Your WP Rocket license is expired!</h3>
 		<div class="rocket-renewal-expired-banner-container">
 			<div class="rocket-expired-message">
@@ -94,10 +95,10 @@ return [
 				</p>
 			</div>
 			</div>
+			</div>
 			<div class="rocket-expired-cta-container">
 				<a href="https://wp-rocket.me/checkout/renew/roger@wp-rocket.me/da5891162a3bc2d8a9670267fd07c9eb/" class="rocket-renew-cta" target="_blank" rel="noopener noreferrer">Renew now</a>
 			</div>
-		</div>
 		<button class="wpr-notice-close wpr-icon-close" id="rocket-dismiss-renewal"><span class="screen-reader-text">Dismiss this notice</span></button>
 	</section>',
 	],
