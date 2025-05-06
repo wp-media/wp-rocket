f67de544229ab8b02bc05eeef50833fafaa46cbe
Michael Lee <michaelleemichaellee408@gmail.com>
2025-05-06 14:27:04 +0100

    Added exclusions logic to context for central exclusion management

--- inc/Engine/Media/PreloadFonts/Context/Context.php
+++ inc/Engine/Media/PreloadFonts/Context/Context.php
@@ -5,6 +5,7 @@ namespace WP_Rocket\Engine\Media\PreloadFonts\Context;
 
 use WP_Rocket\Admin\Options_Data;
 use WP_Rocket\Engine\Common\Context\ContextInterface;
+use WP_Rocket\Engine\Optimization\DynamicLists\DefaultLists\DataManager;
 
 class Context implements ContextInterface {
 	/**
@@ -15,12 +16,54 @@ class Context implements ContextInterface {
 	private $options;
 
 	/**
+	 * DataManager instance
+	 *
+	 * @var DataManager
+	 */
+	private $data_manager;
+
+	/**
+	 * Array of default fonts to exclude (mostly system).
+	 *
+	 * @var array
+	 */
+	private $exclusions  = [
+        'serif',
+        'sans-serif',
+        'monospace',
+        'cursive',
+        'fantasy',
+        'system-ui',
+        'ui-serif',
+        'ui-sans-serif',
+        'ui-monospace',
+        'ui-rounded',
+        'Arial',
+        'Helvetica',
+        'Times New Roman',
+        'Times',
+        'Courier New',
+        'Courier',
+        'Georgia',
+        'Palatino',
+        'Garamond',
+        'Bookman',
+        'Tahoma',
+        'Trebuchet MS',
+        'Arial Black',
+        'Impact',
+        'Comic Sans MS',
+    ];
+
+	/**
 	 * Constructor.
 	 *
 	 * @param Options_Data $options Instance of the Option_Data class.
+	 * @param DataManager  $data_manager DataManager instance.
 	 */
-	public function __construct( Options_Data $options ) {
+	public function __construct( Options_Data $options, DataManager $data_manager ) {
 		$this->options = $options;
+		$this->data_manager = $data_manager;
 	}
 
 	/**
@@ -47,4 +90,22 @@ class Context implements ContextInterface {
 			&& $this->options->get( 'do_caching_mobile_files', 0 )
 			&& wp_is_mobile();
 	}
+
+	/**
+     * Get array of fonts to be excluded.
+     *
+     * @return array
+     */
+    public function get_exclusions(): array {
+		$lists = $this->data_manager->get_lists();
+		$lists = isset( $lists->delay_js_exclusions ) ? $lists->delay_js_exclusions : [];
+        $exclusions = array_merge( $this->exclusions, $lists );
+
+        /**
+		 * Filters excluded fonts.
+
+		 * @param array $exclusions Array of fonts to exclude.
+		 */
+        return wpm_apply_filters_typed('array', 'rocket_preload_fonts_excluded_fonts', $exclusions );
+    }
 }
