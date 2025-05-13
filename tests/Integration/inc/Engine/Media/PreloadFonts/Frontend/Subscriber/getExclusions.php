a9b8fdb8af2f546232865f4499a00c745d1c346a
Michael Lee <michaelleemichaellee408@gmail.com>
2025-05-13 09:25:50 +0100

    Added new integration test

---
+++ tests/Integration/inc/Engine/Media/PreloadFonts/Frontend/Subscriber/getExclusions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WP_Rocket\Tests\Integration\inc\Media\PreloadFonts\Frontend\Subscriber;
+
+use WP_Rocket\Tests\Integration\TestCase;
+
+/**
+ * Test class covering \WP_Rocket\Engine\Media\PreloadFonts\Frontend\Subscriber::get_exclusions
+ *
+ * @group PerformanceHints
+ */
+class Test_GetExclusions extends TestCase {
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldReturnExpected( $config, $expected ) {
+		set_transient( 'wpr_dynamic_lists', $config['get_lists'] );
+
+		$this->assertSame(
+			$expected,
+			apply_filters( 'rocket_preload_fonts_excluded_fonts', [] )
+		);
+	}
+}
