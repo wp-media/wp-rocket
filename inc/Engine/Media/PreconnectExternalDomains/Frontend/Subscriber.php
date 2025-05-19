29f1414a71c4bdd1b24828e4c6036ea5659a415c
Michael Lee <michaelleemichaellee408@gmail.com>
2025-05-19 12:40:32 +0100

    Added data manager dependency and new method to merge both exclusions

--- inc/Engine/Media/PreconnectExternalDomains/Frontend/Subscriber.php
+++ inc/Engine/Media/PreconnectExternalDomains/Frontend/Subscriber.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace WP_Rocket\Engine\Media\PreconnectExternalDomains\Frontend;
 
 use WP_Rocket\Event_Management\Subscriber_Interface;
+use WP_Rocket\Engine\Optimization\DynamicLists\DefaultLists\DataManager;
 
 class Subscriber implements Subscriber_Interface {
 	/**
@@ -14,12 +15,21 @@ class Subscriber implements Subscriber_Interface {
 	private $controller;
 
 	/**
+	 * DataManager instance
+	 *
+	 * @var DataManager
+	 */
+	private $data_manager;
+
+	/**
 	 * Constructor.
 	 *
 	 * @param Controller $controller Controller instance.
+	 * @param DataManager  $data_manager DataManager instance.
 	 */
-	public function __construct( Controller $controller ) {
+	public function __construct( Controller $controller, DataManager $data_manager ) {
 		$this->controller = $controller;
+		$this->data_manager  = $data_manager;
 	}
 
 	/**
@@ -30,6 +40,7 @@ class Subscriber implements Subscriber_Interface {
 	public static function get_subscribed_events(): array {
 		return [
 			'rocket_head_items' => [ 'preconnect_domains', 10 ],
+			'preconnect_external_domain_exclusions' => 'get_exclusions',
 		];
 	}
 
@@ -41,5 +52,21 @@ class Subscriber implements Subscriber_Interface {
 	 */
 	public function preconnect_domains( array $items ) {
 		return $this->controller->add_preconnect_to_head( $items );
+	}
+
+	/**
+	 * Gets the exclusion patterns used to identify elements that should be excluded.
+	 * Merges any existing exclusions pattern with those from the dynamic lists.
+	 *
+	 * @param array $exclusions Array of exclusion patterns.
+	 * @return array
+	 */
+	public function get_exclusions( array $exclusions ): array {
+		$lists = $this->data_manager->get_lists()->preconnect_external_domains_exclusions ?? [];
+		/**
+		 * Merge exclusions and lists.
+		 * Handle empty arrays gracefully.
+		 */
+		return array_merge( $exclusions, (array) $lists );
 	}
 }
