bdbdabef9a4045d49030c3db2556fe28e07be492
Rémy Perona <remperona@gmail.com>
2025-06-06 16:02:23 -0400

    Migrate to new optin

--- inc/Engine/Tracking/Tracking.php
+++ inc/Engine/Tracking/Tracking.php
@@ -3,16 +3,26 @@ declare(strict_types=1);
 
 namespace WP_Rocket\Engine\Tracking;
 
+use WP_Rocket\Abstract_Render;
 use WP_Rocket\Admin\Options_Data;
+use WPMedia\Mixpanel\Optin;
 use WPMedia\Mixpanel\Tracking as MixpanelTracking;
 
-class Tracking {
+class Tracking extends Abstract_Render {
 	/**
 	 * Options Data instance.
 	 *
 	 * @var Options_Data
 	 */
 	private $options;
+
+	/**
+	 * Optin instance.
+	 *
+	 *
+	 * @var Optin
+	 */
+	private $optin;
 
 	/**
 	 * Mixpanel Tracking instance.
@@ -25,10 +35,15 @@ class Tracking {
 	 * Constructor.
 	 *
 	 * @param Options_Data     $options Options Data instance.
+	 * @param Optin            $optin Optin instance.
 	 * @param MixpanelTracking $mixpanel Mixpanel Tracking instance.
+	 * @param string		   $template_path Path to the template files.
 	 */
-	public function __construct( Options_Data $options, MixpanelTracking $mixpanel ) {
+	public function __construct( Options_Data $options, Optin $optin, MixpanelTracking $mixpanel, $template_path ) {
+		parent::__construct( $template_path );
+
 		$this->options  = $options;
+		$this->optin    = $optin;
 		$this->mixpanel = $mixpanel;
 
 		$this->mixpanel->identify( $this->options->get( 'consumer_email', '' ) );
@@ -41,8 +56,7 @@ class Tracking {
 	 * @param mixed $value     The new value of the option.
 	 */
 	public function track_option_change( $old_value, $value ) {
-		// TODO: Refactor this to use the Optin from the WP Mixpanel package.
-		if ( ! $this->options->get( 'analytics_enabled', false ) ) {
+		if ( ! $this->optin->is_enabled() ) {
 			return;
 		}
 
@@ -74,5 +88,68 @@ class Tracking {
 				]
 			);
 		}
+	}
+
+	/**
+	 * Migrate opt-in to new package on upgrade
+	 *
+	 * @param string $new_version The new version of the plugin.
+	 * @param string $old_version The old version of the plugin.
+	 *
+	 * @return void
+	 */
+	public function migrate_optin( string $new_version, string $old_version ): void {
+		if ( version_compare( $old_version, '3.19.1', '>=' ) ) {
+			return;
+		}
+
+		if ( ! $this->options->get( 'analytics_enabled', false ) ) {
+			return;
+		}
+
+		$this->optin->enable();
+	}
+
+	/**
+	 * Render the opt-in section.
+	 *
+	 * @return void
+	 */
+	public function render_optin(): void {
+		echo $this->generate(
+			'optin',
+			[
+				'current_value' => (int) $this->optin->is_enabled(),
+			]
+		);
+	}
+
+	/**
+	 * Handle AJAX request to toggle opt-in.
+	 *
+	 * @return void
+	 */
+	public function ajax_toggle_optin(): void {
+		check_ajax_referer( 'rocket-ajax' );
+
+		if ( ! current_user_can( 'rocket_manage_options' ) ) {
+			wp_send_json_error( 'Missing capability' );
+		}
+
+		if ( ! isset( $_POST['value'] ) ) {
+			wp_send_json_error( 'Missing value parameter' );
+		}
+
+		$value = sanitize_key( wp_unslash( $_POST['value'] ) );
+
+		if ( '1' === $value ) {
+			$this->optin->enable();
+			wp_send_json_success( 'Opt-in enabled.' );
+		} elseif ( '0' === $value ) {
+			$this->optin->disable();
+			wp_send_json_success( 'Opt-in disabled.' );
+		}
+
+		wp_send_json_error( 'Invalid value parameter.' );
 	}
 }
