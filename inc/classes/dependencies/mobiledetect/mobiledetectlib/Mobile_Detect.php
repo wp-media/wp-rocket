72407a4a921934bf0d8b01b5c1b0036efbd068b1
Gaël Robin <robin.gael@gmail.com>
2025-06-19 02:13:22 +0200

    Fix constructor parameter type hint for headers to allow null value

--- inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php
+++ inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php
@@ -886,7 +886,7 @@ class WP_Rocket_Mobile_Detect
      *                          from the $headers array instead.
      */
     public function __construct(
-        array $headers = null,
+        ?array $headers = null,
         $userAgent = null
     ) {
         $this->setHttpHeaders($headers);
