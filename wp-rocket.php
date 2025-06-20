8785376db07e3ea276e647ac9594fe543cedb60f
Michael Lee <michaelleemichaellee408@gmail.com>
2025-06-20 10:37:43 +0100

    Updated version number to 3.19.1.1-beta

--- wp-rocket.php
+++ wp-rocket.php
@@ -20,7 +20,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Rocket defines.
-define( 'WP_ROCKET_VERSION',               '3.19.1-beta' );
+define( 'WP_ROCKET_VERSION',               '3.19.1.1-beta' );
 define( 'WP_ROCKET_WP_VERSION',            '5.8' );
 define( 'WP_ROCKET_WP_VERSION_TESTED',     '6.3.1' );
 define( 'WP_ROCKET_PHP_VERSION',           '7.3' );
