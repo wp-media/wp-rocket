d5f712dbaab2815ef3cf3645b0d2afbb5dd37de3
Gaël Robin <robin.gael@gmail.com>
2025-07-04 02:09:30 +0200

    Add MixPanel tracked options handling in DynamicLists and Tracking classes

--- inc/Engine/Tracking/Tracking.php
+++ inc/Engine/Tracking/Tracking.php
@@ -59,9 +59,9 @@ class Tracking extends Abstract_Render {
 			return;
 		}
 
-		$options_to_track = [
+		$options_to_track = wpm_apply_filters_typed( 'string[]', 'rocket_mixpanel_tracked_options', [
 			'auto_preload_fonts',
-		];
+		] );
 
 		foreach ( $options_to_track as $option_tracked ) {
 			if ( ! isset( $old_value[ $option_tracked ], $value[ $option_tracked ] ) ) {
