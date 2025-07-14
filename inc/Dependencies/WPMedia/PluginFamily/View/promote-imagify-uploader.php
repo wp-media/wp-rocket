191d5c8c2000615416e214cc1a614492f2cc800b
WordPress Fan <146129302+wordpressfan@users.noreply.github.com>
2025-07-14 19:59:55 +0300

    Promote Imagify (#7487)

---
+++ inc/Dependencies/WPMedia/PluginFamily/View/promote-imagify-uploader.php
@@ -0,0 +1,17 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+?>
+<script type="text/template" id="pluginfamily_promote_imagify_uploader_template">
+	<div class="pluginfamily-promote-imagify">
+		<p>
+			<?php
+			printf(
+				// translators: %1$is = Plugin Name.
+				esc_html__( '%1$s recommends you to optimize your images for even better website performance.', 'rocket' ),
+				'WP Rocket'
+			);
+			?>
+		</p>
+		<button id="pluginfamily_install_imagify"><?php esc_html_e( 'Install Imagify Plugin', 'rocket' ); ?></button>
+	</div>
+</script>
