605d4f7d44118c6b8a592f5f154e28b7f1534c73
Michael Lee <michaelleemichaellee408@gmail.com>
2025-08-29 12:40:31 +0100

    Uncommented check for local env

--- inc/Engine/Admin/PerformanceMonitoring/AJAX/Controller.php
+++ inc/Engine/Admin/PerformanceMonitoring/AJAX/Controller.php
@@ -144,12 +144,12 @@ class Controller extends Abstract_Render {
 			'message' => '',
 		];
 
-		// if ( 'local' === wp_get_environment_type() ) {
-		// 	$payload['error']   = true;
-		// 	$payload['message'] = 'Performance monitoring is disabled for local environment';
+		if ( 'local' === wp_get_environment_type() ) {
+			$payload['error']   = true;
+			$payload['message'] = 'Performance monitoring is disabled for local environment';
 
-		// 	return $payload;
-		// }
+			return $payload;
+		}
 
 		// Validate that performance monitoring is not disabled.
 		if ( ! $this->context->is_allowed() ) {
