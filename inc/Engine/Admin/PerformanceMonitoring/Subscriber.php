bf194d6e02acbf02915e112dc606e80e134fb6d9
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2025-09-09 06:55:48 +0300

    use function
    Co-authored-by: WordPress Fan <146129302+wordpressfan@users.noreply.github.com>

--- inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
+++ inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
@@ -142,7 +142,6 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 	 * @return void
 	 */
 	public function render_global_score_widget(): void {
-		$global_score = $this->controller->get_global_score();
-		$this->render->render_global_score_widget( $global_score );
+		$this->render->render_global_score_widget( $this->controller->get_global_score() );
 	}
 }
