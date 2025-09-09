dec44256f957b36604224a03afa6c6db3d1bf9f0
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-09 17:51:46 +0100

    Updated widget to include target id

--- views/settings/partials/performance-monitoring/global-score-widget.php
+++ views/settings/partials/performance-monitoring/global-score-widget.php
@@ -5,47 +5,49 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<div class="wpr-optionHeader">
-	<h3 class="wpr-title2">
-		<?php echo esc_html__( 'RocketInsights', 'rocket' ); ?>
-	</h3>
-</div>
-<div class="wpr-fieldsContainer">
-	<fieldset class="wpr-fieldsContainer-fieldset">
-		<div class="wpr-field">
-			<div class="wpr-percentage-score-widget">
-				<div>
+<div id="wpr_global_score_widget">
+	<div class="wpr-optionHeader">
+		<h3 class="wpr-title2">
+			<?php echo esc_html__( 'RocketInsights', 'rocket' ); ?>
+		</h3>
+	</div>
+	<div class="wpr-fieldsContainer">
+		<fieldset class="wpr-fieldsContainer-fieldset">
+			<div class="wpr-field">
+				<div class="wpr-percentage-score-widget">
+					<div>
+						<?php
+						if ( isset( $data['status'] ) && 'no-url' !== $data['status'] ) :
+							$this->render_performance_score( $data );
+							?>
+						<?php else : ?>
+							<div class="wpr-score-no-urls"></div>
+						<?php endif; ?>
+					</div>
+					<p class="wpr-page-num-txt">
 					<?php
-					if ( isset( $data['status'] ) && 'no-url' !== $data['status'] ) :
-						$this->render_performance_score( $data );
-						?>
-					<?php else : ?>
-						<div class="wpr-score-no-urls"></div>
-					<?php endif; ?>
+					// translators: %1$s is the number of pages monitored.
+					printf( esc_html__( '%1$s page monitored.', 'rocket' ), intval( $data['pages_num'] ) );
+					?>
+					</p>
+					<?php
+					$this->render_action_button(
+						'link',
+						'',
+						[
+							'label'      => $data['pages_num'] ? __( 'ADD PAGE', 'rocket' ) : __( 'ADD HOME PAGE', 'rocket' ),
+							'parameters' => [
+								'type' => 'all',
+							],
+							'url'        => '#rocket_insights',
+							'attributes' => [
+								'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width',
+							],
+						]
+					);
+					?>
 				</div>
-				<p class="wpr-page-num-txt">
-				<?php
-				// translators: %1$s is the number of pages monitored.
-				printf( esc_html__( '%1$s page monitored.', 'rocket' ), intval( $data['pages_num'] ) );
-				?>
-				</p>
-				<?php
-				$this->render_action_button(
-					'link',
-					'',
-					[
-						'label'      => $data['pages_num'] ? __( 'ADD PAGE', 'rocket' ) : __( 'ADD HOME PAGE', 'rocket' ),
-						'parameters' => [
-							'type' => 'all',
-						],
-						'url'        => '#rocket_insights',
-						'attributes' => [
-							'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width',
-						],
-					]
-				);
-				?>
 			</div>
-		</div>
-	</fieldset>
+		</fieldset>
+	</div>
 </div>
