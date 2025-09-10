18ee1a0c1896406bfd9bdd337cf6bc34588714b7
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2025-09-10 13:38:08 +0300

    handle faild status in score

--- views/settings/partials/performance-monitoring/performance-score.php
+++ views/settings/partials/performance-monitoring/performance-score.php
@@ -10,6 +10,10 @@ defined( 'ABSPATH' ) || exit;
 		<div class="wpr-loading-container">
 			<img class="wpr-loading-img" src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL . 'orange-loading.gif' ); ?>"/>
 		</div>
+	<?php elseif ( isset( $data['status'] ) && 'failed' === $data['status'] ) : ?>
+		<div class="wpr-percentage-circle  status-red <?php echo isset( $data['status'] ) && 'blurred' === $data['status'] ? 'blurred' : ''; ?>">
+			!
+		</div>
 	<?php else : ?>
 		<div class="wpr-percentage-circle <?php echo esc_html( $data['status-color'] ?? '' ); ?> <?php echo isset( $data['status'] ) && 'blurred' === $data['status'] ? 'blurred' : ''; ?>">
 			<?php echo esc_html( $data['score'] ); ?>
