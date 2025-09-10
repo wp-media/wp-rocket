7a8d65258949433735acf3350fe45417f94b1391
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2025-09-10 14:10:13 +0300

    score tool-tip

--- views/settings/partials/performance-monitoring/performance-score.php
+++ views/settings/partials/performance-monitoring/performance-score.php
@@ -19,4 +19,14 @@ defined( 'ABSPATH' ) || exit;
 			<?php echo esc_html( $data['score'] ); ?>
 		</div>
 	<?php endif; ?>
+	<?php if (isset($data['status']) && ( 'failed' === $data['status'] || 'blurred' === $data['status'] ) ): ?>
+		<div class="wpr-tooltip">
+			<?php
+				$tool_tip_text = !empty( $data['error_message'] ) ? $data['error_message'] : __( 'Upgrade your plan to see your score.', 'rocket' );
+			?>
+			<div class="wpr-tooltip-content">
+				<?php esc_html_e($tool_tip_text)?>
+			</div>
+		</div>
+	<?php endif; ?>
 </div>
