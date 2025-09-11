dd78a32a67ce578f97efbc23e586b3bc06b11801
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-11 12:54:39 +0100

    Fixed linter error

--- inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
+++ inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
@@ -76,7 +76,7 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 			'rocket_pm_job_completed'             => 'reset_global_score',
 			'rocket_pm_job_failed'                => 'reset_global_score',
 			'rocket_pm_job_added'                 => 'reset_global_score',
-			'rocket_pm_job_retest'				  => 'reset_global_score',
+			'rocket_pm_job_retest'                => 'reset_global_score',
 			'rocket_pm_job_deleted'               => 'reset_global_score',
 			'rocket_dashboard_sidebar'            => 'render_global_score_widget',
 		];
