28a995216a8a212d9a82e0c3766f9b6c37152ac8
Gaël Robin <robin.gael@gmail.com>
2025-09-11 14:48:25 +0200

    Use `is_blurred`

--- inc/Engine/Admin/PerformanceMonitoring/GlobalScore.php
+++ inc/Engine/Admin/PerformanceMonitoring/GlobalScore.php
@@ -159,11 +159,12 @@ class GlobalScore {
 
 		// Check if any URLs are blurred.
 		$blurred_count = $this->query->query(
-			[
+	[
 				'count'      => true,
-				'status__in' => [ 'blurred' ],
+				'status__in' => [ 'completed' ],
+				'is_blurred' => 1,
 			]
-			);
+		);
 
 		if ( $blurred_count > 0 ) {
 			return 'blurred';
