a209c843f70dbdc22060eac26bbb0bf7c03dd783
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2025-09-12 16:20:59 +0300

    fix blurred golbal score

--- views/settings/partials/performance-monitoring/performance-score.php
+++ views/settings/partials/performance-monitoring/performance-score.php
@@ -4,6 +4,11 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+$rocket_pma_item_is_blurred = false;
+if ( isset( $data['is_blurred'] ) && $data['is_blurred'] || isset( $data['status'] ) && 'blurred' === $data['status'] ) {
+	$rocket_pma_item_is_blurred = true;
+}
 ?>
 <div class="wpr-percentage-indicator">
 	<?php if ( isset( $data['status'] ) && 'in-progress' === $data['status'] ) : ?>
@@ -11,15 +16,15 @@ defined( 'ABSPATH' ) || exit;
 			<img class="wpr-loading-img" src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL . 'orange-loading.gif' ); ?>"/>
 		</div>
 	<?php elseif ( isset( $data['status'] ) && 'failed' === $data['status'] ) : ?>
-		<div class="wpr-percentage-circle  status-red <?php echo isset( $data['is_blurred'] ) && $data['is_blurred'] ? 'blurred' : ''; ?>">
+		<div class="wpr-percentage-circle  status-red <?php echo $rocket_pma_item_is_blurred ? 'blurred' : ''; ?>">
 			!
 		</div>
 	<?php else : ?>
-		<div class="wpr-percentage-circle <?php echo esc_html( $data['status-color'] ?? '' ); ?> <?php echo isset( $data['is_blurred'] ) && $data['is_blurred'] ? 'blurred' : ''; ?>">
+		<div class="wpr-percentage-circle <?php echo esc_html( $data['status-color'] ?? '' ); ?> <?php echo $rocket_pma_item_is_blurred ? 'blurred' : ''; ?>">
 			<?php echo esc_html( $data['score'] ); ?>
 		</div>
 	<?php endif; ?>
-	<?php if ( ( isset( $data['status'] ) && 'failed' === $data['status'] ) || ( isset( $data['is_blurred'] ) && $data['is_blurred'] ) ) : ?>
+	<?php if ( ( isset( $data['status'] ) && 'failed' === $data['status'] ) || $rocket_pma_item_is_blurred ) : ?>
 		<div class="wpr-tooltip">
 			<div class="wpr-tooltip-content">
 				<?php echo ! empty( $data['error_message'] ) ? esc_html( $data['error_message'] ) : esc_html__( 'Upgrade your plan to see your score.', 'rocket' ); ?>
