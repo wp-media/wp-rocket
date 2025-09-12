479698d7bc66dbb402f909ed808d5669e7ab6877
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-12 13:03:30 +0100

    Hook new callback to admin_post event

--- inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
+++ inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
@@ -107,6 +107,7 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 			'rocket_pm_job_retest'                => 'reset_global_score',
 			'rocket_pm_job_deleted'               => 'reset_global_score',
 			'rocket_dashboard_sidebar'            => 'render_global_score_widget',
+			'admin_post_rocket_pm_add_homepage'	=> 'add_homepage_from_widget',
 		];
 	}
 
@@ -236,5 +237,14 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 	 */
 	public function render_global_score_widget(): void {
 		$this->render->render_global_score_widget( $this->controller->get_global_score() );
+	}
+
+	/**
+	 * Adds homepage for monitoring from the dashboard widget.
+	 *
+	 * @return void
+	 */
+	public function add_homepage_from_widget(): void {
+		$this->controller->add_homepage_from_widget();
 	}
 }
