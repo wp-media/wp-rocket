aabb568b4b150437c67c9dc7ad7536e050ba755c
Rémy Perona <remperona@gmail.com>
2025-09-15 16:08:15 -0400

    add translatable strings

--- views/settings/partials/performance-monitoring/global-score-row.php
+++ views/settings/partials/performance-monitoring/global-score-row.php
@@ -13,9 +13,9 @@ defined( 'ABSPATH' ) || exit;
 	</td>
 	<td class="wpr-pma-item-title">
 		<?php if ( 'in-progress' === $data['status'] ) : ?>
-			<span><?php printf( esc_html( 'Tracked pages: %s' ), esc_html( $data['pages_num'] ) ); ?></span>
+			<span><?php printf( esc_html__( 'Tracked pages: %s', 'rocket' ), esc_html( $data['pages_num'] ) ); ?></span>
 		<?php else : ?>
-			<span><?php printf( esc_html( '%s pages monitored' ), esc_html( $data['pages_num'] ) ); ?></span>
+			<span><?php printf( esc_html__( '%s pages monitored', 'rocket' ), esc_html( $data['pages_num'] ) ); ?></span>
 		<?php endif; ?>
 	</td>
 	<td class="wpr-pma-item-actions"></td>
