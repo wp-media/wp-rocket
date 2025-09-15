90e4cc5032e764373a32e380193251dcbd962716
WordPressFan <ahmed@wp-media.me>
2025-09-15 18:09:35 +0300

    fix phpcs

--- inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
+++ inc/Engine/Admin/PerformanceMonitoring/Subscriber.php
@@ -90,29 +90,29 @@ class Subscriber implements Subscriber_Interface, LoggerAwareInterface {
 	 */
 	public static function get_subscribed_events(): array {
 		return [
-			'wp_rocket_first_install'        => 'schedule_homepage_tests',
-			'wp_ajax_rocket_pm_add_new_page' => 'add_new_page',
-			'wp_ajax_rocket_pm_get_results'  => 'get_results',
-			'rocket_localize_admin_script'   => 'add_pending_ids',
-			'admin_post_delete_pm'           => 'delete_row',
-			'wp_ajax_rocket_pm_reset_page'   => 'reset_page',
-			'init'                           => 'schedule_reset_credit',
-			'rocket_pma_credit_reset'        => 'reset_credit_monthly',
-			'rocket_pm_job_completed'        => [
+			'wp_rocket_first_install'           => 'schedule_homepage_tests',
+			'wp_ajax_rocket_pm_add_new_page'    => 'add_new_page',
+			'wp_ajax_rocket_pm_get_results'     => 'get_results',
+			'rocket_localize_admin_script'      => 'add_pending_ids',
+			'admin_post_delete_pm'              => 'delete_row',
+			'wp_ajax_rocket_pm_reset_page'      => 'reset_page',
+			'init'                              => 'schedule_reset_credit',
+			'rocket_pma_credit_reset'           => 'reset_credit_monthly',
+			'rocket_pm_job_completed'           => [
 				[ 'validate_credit' ],
 				[ 'reset_global_score' ],
 			],
-			'rocket_pm_job_failed'           => 'reset_global_score',
-			'rocket_pm_job_added'            => 'reset_global_score',
-			'rocket_pm_job_retest'           => 'reset_global_score',
-			'rocket_pm_job_deleted'          => 'reset_global_score',
-			'rocket_dashboard_sidebar'       => 'render_global_score_widget',
-			'rocket_insights_tab_content'    => [
+			'rocket_pm_job_failed'              => 'reset_global_score',
+			'rocket_pm_job_added'               => 'reset_global_score',
+			'rocket_pm_job_retest'              => 'reset_global_score',
+			'rocket_pm_job_deleted'             => 'reset_global_score',
+			'rocket_dashboard_sidebar'          => 'render_global_score_widget',
+			'rocket_insights_tab_content'       => [
 				[ 'render_license_banner_section', 10 ],
 				[ 'render_performance_urls_table', 20 ],
 				[ 'render_settings_section', 30 ],
 			],
-			'admin_post_rocket_pm_add_homepage'   => 'add_homepage_from_widget',
+			'admin_post_rocket_pm_add_homepage' => 'add_homepage_from_widget',
 		];
 	}
 
