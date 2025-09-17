2a76a9e3506eb3bc222e8c8ca57ec46ad74d9575
Rémy Perona <remperona@gmail.com>
2025-09-17 17:21:16 -0400

    rename variable and remove unnecessary checks

--- views/settings/buttons/link.php
+++ views/settings/buttons/link.php
@@ -10,22 +10,19 @@
  *     @type string $label      Link text.
  *     @type string $url        URL for the href attribute.
  *     @type string $attributes String of attribute=value for the <a> tag, e.g. class, target, etc.
+ *     @type string $tooltip    Tooltip text.
  * }
  */
 
 defined( 'ABSPATH' ) || exit;
 
-$data['url'] = isset( $data['url'] ) ? esc_url( $data['url'] ) : ''; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-if ( ! empty( $data['disabled'] ) ) {
-	$data['url']         = 'javascript:void(0);'; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$data['attributes'] .= 'disabled ="disabled"'; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-}
+$data['url'] = ! empty( $data['url'] ) ? esc_url( $data['url'] ) : 'javascript:void(0);'; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 ?>
 <a href="<?php echo $data['url']; ?>" <?php echo $data['attributes']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- $data['attributes'] escaped with sanitize_key & esc_attr ?>><?php echo $data['label']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Dynamic content is properly escaped in the view. ?>
-	<?php if ( ! empty( $data['tool_tip'] ) ) : ?>
+	<?php if ( ! empty( $data['tooltip'] ) ) : ?>
 		<div class="wpr-tooltip">
 			<div class="wpr-tooltip-content">
-				<?php echo esc_html( $data['tool_tip'] ); ?>
+				<?php echo esc_html( $data['tooltip'] ); ?>
 			</div>
 		</div>
 	<?php endif; ?>
