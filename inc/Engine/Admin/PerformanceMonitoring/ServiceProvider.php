2d9c54ffc36de04d30f52a7da4f9255528f8fe6e
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-18 14:01:16 +0100

    Updated dependency for pm context

--- inc/Engine/Admin/PerformanceMonitoring/ServiceProvider.php
+++ inc/Engine/Admin/PerformanceMonitoring/ServiceProvider.php
@@ -90,6 +90,7 @@ class ServiceProvider extends AbstractServiceProvider {
 				[
 					new StringArgument( $this->getContainer()->get( 'template_path' ) . '/settings/' ),
 					'pm_credit_manager',
+					'pm_context',
 				]
 				);
 
