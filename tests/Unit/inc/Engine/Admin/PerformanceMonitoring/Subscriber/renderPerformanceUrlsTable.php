69ec2454ffc28bf1fc940baa6663765592a3ac65
COQUARD Cyrille <coquardcyr@gmail.com>
2025-09-18 11:08:09 +0200

    Updated `Subscriber` test: added `User` and `UserClient` mock dependencies to `renderPerformanceUrlsTable`.

--- tests/Unit/inc/Engine/Admin/PerformanceMonitoring/Subscriber/renderPerformanceUrlsTable.php
+++ tests/Unit/inc/Engine/Admin/PerformanceMonitoring/Subscriber/renderPerformanceUrlsTable.php
@@ -3,6 +3,8 @@ namespace WP_Rocket\Tests\Unit\inc\Engine\Admin\PerformanceMonitoring\Subscriber
 
 use WP_Rocket\Engine\Admin\PerformanceMonitoring\Context\PerformanceMonitoringContext;
 use WP_Rocket\Engine\Admin\PerformanceMonitoring\Queue\Queue;
+use WP_Rocket\Engine\License\API\User;
+use WP_Rocket\Engine\License\API\UserClient;
 use WP_Rocket\Tests\Unit\TestCase;
 use WP_Rocket\Engine\Admin\PerformanceMonitoring\Subscriber;
 use WP_Rocket\Engine\Admin\PerformanceMonitoring\Render;
@@ -41,8 +43,10 @@ class Test_renderPerformanceUrlsTable extends TestCase {
 		$mock_ajax_controller   = $this->createMock(AjaxController::class);
 		$mock_queue             = $this->createMock(Queue::class);
 		$pm_context = $this->createMock(PerformanceMonitoringContext::class);
+		$user = $this->createMock(User::class);
+		$user_client = $this->createMock(UserClient::class);
 
-		$subscriber = new Subscriber($mock_render, $mock_controller, $mock_ajax_controller, $mock_queue, $pm_context, $mock_global_score);
+		$subscriber = new Subscriber($mock_render, $mock_controller, $mock_ajax_controller, $mock_queue, $pm_context, $mock_global_score, $user_client, $user);
 		$subscriber->render_performance_urls_table();
 	}
 }
