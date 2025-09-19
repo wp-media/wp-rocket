e4041dbaad22fefa55dc48cec7af7a1cfd1a2bd3
Gaël Robin <robin.gael@gmail.com>
2025-09-20 00:33:47 +0200

    Fix #7663: 3.20: Free Plan – Adding (up to 3) URLs from Global Score widget when no credits left isn’t working (#7673)
    Co-authored-by: Rémy Perona <remperona@gmail.com>
    Co-authored-by: WordPress Fan <146129302+wordpressfan@users.noreply.github.com>

---
+++ tests/Fixtures/inc/Engine/Admin/PerformanceMonitoring/Subscriber/html/output_3_urls_disabled.php
@@ -0,0 +1,20 @@
+<div id="wpr_global_score_widget">
+<div class="wpr-optionHeader">
+<h3 class="wpr-title2">Rocket Insights Global Score</h3>
+</div>
+<div class="wpr-fieldsContainer">
+<fieldset class="wpr-fieldsContainer-fieldset">
+<div class="wpr-field">
+<div class="wpr-percentage-score-widget">
+<div>
+<div class="wpr-percentage-indicator">
+<div class="wpr-percentage-circle status-red ">0</div>
+</div>
+</div>
+<p class="wpr-page-num-txt">3 page monitored.</p>
+<a href="javascript:void(0);" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-pma-add-url-button wpr-pma-global-score-add-url-button wpr-btn-with-tool-tip disabled"disabled ="disabled">Add Pages <div class="wpr-tooltip"> <div class="wpr-tooltip-content"> Maximum number of URLs reached for your license. </div> </div> </a>
+</div>
+</div>
+</fieldset>
+</div>
+</div>
