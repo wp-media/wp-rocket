a563f884b09ecf68048e9839ebdd58a606a69753
WordPressFan <ahmed@wp-media.me>
2025-09-22 10:18:37 +0300

    fix tests

--- tests/Fixtures/inc/Engine/Admin/PerformanceMonitoring/Subscriber/html/output_3_urls_loading_disabled.php
+++ tests/Fixtures/inc/Engine/Admin/PerformanceMonitoring/Subscriber/html/output_3_urls_loading_disabled.php
@@ -14,7 +14,7 @@
 </div>
 </div>
 <p class="wpr-page-num-txt">3 page monitored.</p>
-<a href="javascript:void(0);" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-pma-add-url-button wpr-pma-global-score-add-url-button wpr-btn-with-tool-tip disabled"disabled ="disabled">Add Pages <div class="wpr-tooltip"> <div class="wpr-tooltip-content"> Maximum number of URLs reached for your license. </div> </div> </a>
+<a href="#rocket_insights" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-pma-add-url-button wpr-pma-global-score-add-url-button wpr-btn-with-tool-tip disabled">Add Pages <div class="wpr-tooltip"> <div class="wpr-tooltip-content"> You don&#039;t have enough credits </div> </div> </a>
 </div>
 </div>
 </fieldset>
