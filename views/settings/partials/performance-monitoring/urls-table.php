155920abc75c3192b907b5948e0f75848f949402
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-25 11:44:08 +0100

    Used new button template

--- views/settings/partials/performance-monitoring/urls-table.php
+++ views/settings/partials/performance-monitoring/urls-table.php
@@ -98,25 +98,10 @@ $wp_rocket_boxes = get_user_meta( get_current_user_id(), 'rocket_boxes', true );
 			class="wpr-speed-radar-input"
 			placeholder="<?php esc_attr_e( 'Enter a page URL to monitor', 'rocket' ); ?>"
 			id="wpr-speed-radar-url-input" />
-
-	<?php
-	$rocket_pma_add_page_button_args = [
-		'label'      => __( 'Add page +', 'rocket' ),
-		'attributes' => [
-			'class' => 'wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-pma-add-url-button',
-			'id'    => 'add_page_speed_radar',
-		],
-	];
-	if ( $data['reach_max_url'] ) {
-		$rocket_pma_add_page_button_args['attributes']['class'] .= ' wpr-btn-with-tool-tip disabled';
-		$rocket_pma_add_page_button_args['url']                  = '#';
-		$rocket_pma_add_page_button_args['tooltip']              = esc_html__( 'You have reached your maximum page limit', 'rocket' );
-		$rocket_pma_add_page_button_args['disabled']             = true;
-	}
-	$this->render_action_button(
-		'button',
-		'add_page_speed_radar',
-		$rocket_pma_add_page_button_args
-	);
-	?>
+	
+	<div id="wpr-pma-add-url-button-container">
+		<?php
+		$this->render_add_page_btn( 'rocket-insights', $data );
+		?>
+	</div>
 </div>
