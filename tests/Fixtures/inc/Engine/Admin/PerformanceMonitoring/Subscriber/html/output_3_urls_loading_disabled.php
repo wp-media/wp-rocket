526a1faee8edfdc1b6f7f7656f2fac7928ebe65e
Michael Lee <michaelleemichaellee408@gmail.com>
2025-09-25 12:58:29 +0100

    Updated test data

--- tests/Fixtures/inc/Engine/Admin/PerformanceMonitoring/Subscriber/html/output_3_urls_loading_disabled.php
+++ tests/Fixtures/inc/Engine/Admin/PerformanceMonitoring/Subscriber/html/output_3_urls_loading_disabled.php
@@ -14,7 +14,9 @@
 </div>
 </div>
 <p class="wpr-page-num-txt">3 page monitored.</p>
+<div id="wpr_global_score_widget_add_page_btn_wrapper">
 <a href="#rocket_insights" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-pma-add-url-button wpr-pma-global-score-add-url-button wpr-btn-with-tool-tip disabled">Add Pages <div class="wpr-tooltip"> <div class="wpr-tooltip-content"> You have reached your maximum page limit </div> </div> </a>
+</div>
 </div>
 </div>
 </fieldset>
