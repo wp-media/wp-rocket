696cde53a509ecf614d844b016e64c267d8a96a7
Rémy Perona <remperona@gmail.com>
2025-09-26 11:44:23 -0400

    remove update notice

--- inc/Engine/Admin/Settings/Page.php
+++ inc/Engine/Admin/Settings/Page.php
@@ -2231,48 +2231,4 @@ class Page extends Abstract_Render {
 		$this->options->set( 'do_caching_mobile_files', 1 );
 		update_option( rocket_get_constant( 'WP_ROCKET_SLUG', 'wp_rocket_settings' ), $this->options->get_options() );
 	}
-
-	/**
-	 * Display an update notice when the plugin is updated.
-	 *
-	 * @return void
-	 */
-	public function display_update_notice() {
-		if ( ! current_user_can( 'rocket_manage_options' ) ) {
-			return;
-		}
-
-		if ( 'settings_page_wprocket' !== get_current_screen()->id ) {
-			return;
-		}
-
-		$boxes = get_user_meta( get_current_user_id(), 'rocket_boxes', true );
-
-		if ( in_array( 'rocket_update_notice', (array) $boxes, true ) ) {
-			return;
-		}
-
-		$previous_version = $this->options->get( 'previous_version' );
-
-		// Bail-out if previous version is greater than or equal to 3.20.
-		if ( version_compare( $previous_version, '3.20', '>=' ) ) {
-			return;
-		}
-
-		rocket_notice_html(
-			[
-				'status'         => 'info',
-				'dismissible'    => '',
-				'message'        => sprintf(
-						// translators: %1$s: opening strong tag, %2$s: closing strong tag, %3$s: opening a tag, %4$s: closing a tag.
-						__( '%1$sWP Rocket:%2$s the plugin has been updated to the 3.20 version. New feature: %3$sRocket Insights%4$s. Check out our documentation to learn more about it.', 'rocket' ),
-						'<strong>',
-						'</strong>',
-						'<a href="" data-beacon-article="" target="_blank" rel="noopener noreferrer">',
-						'</a>'
-				),
-				'dismiss_button' => 'rocket_update_notice',
-			]
-		);
-	}
 }
