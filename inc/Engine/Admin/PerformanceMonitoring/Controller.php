a863b75e80104062257e3e4ef7b9fc6ff6aa6189
WordPressFan <ahmed@wp-media.me>
2025-09-30 15:50:28 +0300

    fix phpcs

--- inc/Engine/Admin/PerformanceMonitoring/Controller.php
+++ inc/Engine/Admin/PerformanceMonitoring/Controller.php
@@ -306,7 +306,7 @@ class Controller {
 			'currency'    => '$',
 			'page_number' => $limit,
 			'period'      => 'month',
-			'subtitle' => $this->user->get_pma_addon_subtitle( $upgrade ),
+			'subtitle'    => $this->user->get_pma_addon_subtitle( $upgrade ),
 			'billing'     => $this->user->get_pma_addon_billing( $upgrade ),
 			'highlights'  => $this->user->get_pma_addon_highlights( $upgrade ),
 		];
