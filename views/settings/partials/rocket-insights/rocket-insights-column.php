c6b753a2cf1257e6931ec8492348c8bc11f52bfa
Gael Robin <robin.gael@gmail.com>
2025-10-17 13:52:53 +0200

    add view of the column content

---
+++ views/settings/partials/rocket-insights/rocket-insights-column.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Rocket Insights column content for post listing pages.
+ *
+ * @since 3.20.1
+ *
+ * @var string $url The URL of the post.
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<div class="wpr-rocket-insights-column">
+	<?php
+	// Placeholder content for now.
+	// In the future, this will fetch and display the actual performance score.
+	echo esc_html__( 'Coming Soon', 'rocket' );
+	?>
+</div>
