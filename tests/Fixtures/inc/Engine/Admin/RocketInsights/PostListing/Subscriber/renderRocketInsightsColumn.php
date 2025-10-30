bf042485a7822cfc5b6f220c9fc6532fe0c2e787
Gael Robin <robin.gael@gmail.com>
2025-10-30 02:11:31 +0100

    add test

--- tests/Fixtures/inc/Engine/Admin/RocketInsights/PostListing/Subscriber/renderRocketInsightsColumn.php
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/PostListing/Subscriber/renderRocketInsightsColumn.php
@@ -58,5 +58,21 @@ return [
 				'html' => '<div class="wpr-ri-score-wrapper">',
 			]
 		],
+		'testShouldRenderFailedStateWithRetestButton' => [
+			'config' => [
+				'rows' => [
+					[
+						'url' 	  	 => 'https://example.com/page-to-test',
+						'status'     => 'failed',
+						'score'      => 0,
+						'is_blurred' => 0,
+						'report_url' => '',
+					]
+				]
+			],
+			'expected' => [
+				'html' => 'wpr-ri-retest-link',
+			]
+		],
 	],
 ];
