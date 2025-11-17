d7b761a65c0201fc4c33c00eaac2529f90242ca7
Gaël Robin <robin.gael@gmail.com>
2025-11-17 04:41:17 +0100

    Fix ArgumentCountError in purge_cloudflare_after_automatic_purge
    - Add 2 argument specification to hook registration
    - Hook callback now correctly receives both $deleted and $args parameters
    - Update corresponding unit test expectations
    
    This fixes CI failures where the method expected 2 arguments but WordPress
    was only passing 1 due to the default hook registration behavior.

--- tests/Unit/inc/ThirdParty/Plugins/CDN/Cloudflare/getSubscribedEvents.php
+++ tests/Unit/inc/ThirdParty/Plugins/CDN/Cloudflare/getSubscribedEvents.php
@@ -30,7 +30,7 @@ class Test_getSubscribedEvents extends TestCase {
             'rocket_after_clean_domain'           => 'purge_cloudflare',
             'after_rocket_clean_files'            => 'purge_cloudflare_partial',
             'after_rocket_clean_home'             => 'purge_cloudflare',
-            'rocket_after_automatic_cache_purge'  => 'purge_cloudflare_after_automatic_purge',
+            'rocket_after_automatic_cache_purge'  => [ 'purge_cloudflare_after_automatic_purge', 10, 2 ],
             'rocket_saas_complete_job_status'     => 'purge_cloudflare_after_usedcss',
             'rocket_rucss_after_clearing_usedcss' => 'purge_cloudflare_after_usedcss',
             'admin_post_rocket_enable_separate_mobile_cache' => 'enable_separate_mobile_cache',
