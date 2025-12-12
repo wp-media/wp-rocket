0cb8d62454936008ab5998acab0f20751a3486e6
Michael Lee <michaelleemichaellee408@gmail.com>
2025-12-12 14:50:23 +0100

    Added 3rd party compatibility class for generatepress

---
+++ inc/ThirdParty/Themes/GeneratePress.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WP_Rocket\ThirdParty\Themes;
+
+use WP_Rocket\Event_Management\Subscriber_Interface;
+
+class GeneratePress implements Subscriber_Interface {
+	/**
+	 * Return an array of events that this subscriber wants to listen to.
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events() {
+		return [
+			'generate_footer_class' => 'inject_exclusions_class',
+		];
+	}
+
+	/**
+	 * Injects exclusion class into GeneratePress footer classes.
+	 *
+	 * @since 3.20.3
+	 *
+	 * @param array $class Array of footer classes.
+	 *
+	 * @return array The modified array of footer classes.
+	 */
+	public function inject_exclusions_class( array $class ): array {
+        $class[] = 'no-wpr-lazyrender';
+
+        return $class;
+	}
+}
