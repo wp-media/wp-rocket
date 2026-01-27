0764eec69ba2c57481b97a2dec402c7892e54f6e
Gaël Robin <robin.gael@gmail.com>
2026-01-27 09:37:34 +0100

    Hide upgrade section and popup when license is banned
    - Add is_banned() check to display_upgrade_section()
    - Add is_banned() check to display_upgrade_popin()
    
    This prevents upgrade messages from being displayed when the website is revoked, even if there's an active promo or the license is valid/expiring.

--- inc/Engine/License/Upgrade.php
+++ inc/Engine/License/Upgrade.php
@@ -41,6 +41,10 @@ class Upgrade extends Abstract_Render {
 	 * @return void
 	 */
 	public function display_upgrade_section() {
+		if ( $this->user->is_banned() ) {
+			return;
+		}
+
 		if ( ! $this->can_upgrade() ) {
 			return;
 		}
@@ -55,6 +59,10 @@ class Upgrade extends Abstract_Render {
 	 */
 	public function display_upgrade_popin() {
 		if ( rocket_get_constant( 'WP_ROCKET_WHITE_LABEL_ACCOUNT' ) ) {
+			return;
+		}
+
+		if ( $this->user->is_banned() ) {
 			return;
 		}
 
