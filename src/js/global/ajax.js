9ddf56feb7da9bf2455cc6ffc6c68bd0cab35742
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-02-10 08:22:35 +0100

    Rework mixpanel tracking for see report and expanded metric view

--- src/js/global/ajax.js
+++ src/js/global/ajax.js
@@ -732,18 +732,31 @@ document.addEventListener('DOMContentLoaded', function() {
 			$details.show('fast');
 			$img.attr('src', carets.down);
 			
-			// Track expand only (not collapse) if Mixpanel is available.
-			if (window.rocket_mixpanel_data && window.rocket_mixpanel_data.optin_enabled && window.mixpanel) {
-				window.mixpanel.track('Rocket Insights Metrics Expanded', {
-					context: 'wp_plugin',
-					row_id: insightsId
-				});
-			}
+			// Track expand only expand metric action.
+			handleMetricActionTracking('expand', insightsId);
 			
 			// Manipulate styling for last elements when details cell is not visible.
 			if (isLast) {
 				updateRowStylingForLastItem($selectors);
 			}
+		}
+
+		// Tracks user interactions with metric actions in Rocket Insights via AJAX.
+		function handleMetricActionTracking(event, rowId) {
+			$.post(
+				ajaxurl,
+				{
+					action: 'rocket_insight_track_metric_actions',
+					_ajax_nonce: rocket_ajax_data.nonce,
+					event: event,
+					row_id: rowId
+				},
+				function(response) {
+					if (!response.success) {
+						console.error('Metric action tracking failed:', response?.data || response);
+					}
+				}
+			);
 		}
 
     	/**
@@ -803,17 +816,17 @@ document.addEventListener('DOMContentLoaded', function() {
 			$('.wpr-ri-item-toggle-single img').attr('src', carets.down);
 			updateRowStylingForLastItem($selectors);
 		});
-	}
 
-	// Track "See Report" clicks in Rocket Insights.
-	$(document).on('click', '.wpr-ri-report', function(e) {
-		// Only track if link is not disabled and mixpanel is available.
-		if (!$(this).hasClass('wpr-ri-action--disabled') && window.rocket_mixpanel_data && window.rocket_mixpanel_data.optin_enabled && window.mixpanel) {
-			var insightsId = $(this).closest('.wpr-ri-item').data('rocket-insights-id');
-			window.mixpanel.track('Rocket Insights See Report', {
-				context: 'wp_plugin',
-				row_id: insightsId
-			});
-		}
-	});
+		// Track "See Report" clicks in Rocket Insights.
+		$(document).on('click', '.wpr-ri-report', function(e) {
+			// Only track if link is not disabled and mixpanel is available.
+			if ($(this).hasClass('wpr-ri-action--disabled')) {
+				return;
+			}
+
+			var insightsId = $(this).data('rocket-insights-id');
+
+			handleMetricActionTracking('see_report', insightsId);
+		});
+	}
 });
