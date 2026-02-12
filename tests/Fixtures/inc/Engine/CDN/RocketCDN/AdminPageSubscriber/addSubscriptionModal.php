0ae4ee526b8e9f03f562213bc28e53b72f042c6c
Gaël Robin <robin.gael@gmail.com>
2026-02-12 09:19:11 +0100

    fix test

--- tests/Fixtures/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/addSubscriptionModal.php
+++ tests/Fixtures/inc/Engine/CDN/RocketCDN/AdminPageSubscriber/addSubscriptionModal.php
@@ -27,8 +27,7 @@ return [
 		<div class="wpr-loader" id="wpr-rocketcdn-modal-loader"></div>
 		<div class="wpr-rocketcdn-modal__container" role="dialog" aria-modal="true" aria-labelledby="wpr-rocketcdn-modal-title">
 			<div id="wpr-rocketcdn-modal-content">
-				<iframe id="rocketcdn-iframe" src="https://api.wp-rocket.me/cdn/iframe?website=http://example.org&#038;callback=http://example.org/index.php?rest_route=/wp-rocket/v1/rocketcdn/&#038;dashboard_url=http://example.org/wp-admin/options-general.php?page=wprocket&#038;rocketcdn_checkout=true&#038;source=plugin" loading="lazy" width="674" height="425"></iframe>
-			</div>
+				<iframe id="rocketcdn-iframe" src="https://api.wp-rocket.me/cdn/iframe?website=http://example.org&#038;callback=http://example.org/index.php?rest_route=/wp-rocket/v1/rocketcdn/&#038;dashboard_url=http://example.org/wp-admin/options-general.php?page=wprocket&#038;rocketcdn_checkout=true&#038;source=plugin" loading="lazy" width="674" height="425"></iframe>			</div>
 		</div>
 	</div>
 </div>
