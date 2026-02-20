85eb50b58a960aeac1e635ea3c927f73e8a71884
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2026-02-20 13:16:51 +0100

    RI advanced indicators - UI enhancements & fixes (#8079)
    Co-authored-by: WordPress Fan <146129302+wordpressfan@users.noreply.github.com>

--- views/settings/partials/rocket-insights/global-score-row.php
+++ views/settings/partials/rocket-insights/global-score-row.php
@@ -7,9 +7,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <tr class="wpr-ri-item wpr-global-score">
 	<td class="wpr-ri-item-toggle">
-		<div class="icon-frame wpr-ri-item-toggle-all">
-			<img src="<?php echo esc_url( WP_ROCKET_ASSETS_IMG_URL ); ?>ri-caret-up-down.svg" alt="">
-		</div>
+		<div class="icon-frame wpr-ri-item-toggle-all wpr-ri-item-toggle-all--expanded"></div>
 	</td>
 	<td class="wpr-ri-item-score">
 		<?php
