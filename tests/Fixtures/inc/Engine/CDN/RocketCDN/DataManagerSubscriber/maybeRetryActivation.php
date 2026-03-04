4a5b30a2b966e861207465cbe718ffd1eb69b278
Gaël Robin <robin.gael@gmail.com>
2026-03-04 11:59:55 +0100

    fix test

--- tests/Fixtures/inc/Engine/CDN/RocketCDN/DataManagerSubscriber/maybeRetryActivation.php
+++ tests/Fixtures/inc/Engine/CDN/RocketCDN/DataManagerSubscriber/maybeRetryActivation.php
@@ -131,35 +131,4 @@ return [
 			'cdn_enabled' => false,
 		],
 	],
-
-	'shouldRetryActivationWithTokenFromUserEndpoint' => [
-		'config'   => [
-			'user_role'                         => 'administrator',
-			// No token saved locally.
-			'user_data'                         => json_decode( wp_json_encode( [
-				'rocketcdn' => [
-					'cdn_token' => '1234567890123456789012345678901234567890',
-				],
-			] ) ),
-			'subscription_data'                 => [
-				'id'                            => 12345,
-				'is_active'                     => false,
-				'cdn_url'                       => 'https://abcd1234.delivery.rocketcdn.me',
-				'subscription_status'           => 'running',
-				'subscription_next_date_update' => '+30 days',
-			],
-			'activation_success'                => true,
-			'subscription_data_after_activation' => [
-				'id'                            => 12345,
-				'is_active'                     => true,
-				'cdn_url'                       => 'https://abcd1234.delivery.rocketcdn.me',
-				'subscription_status'           => 'running',
-				'subscription_next_date_update' => '+30 days',
-			],
-		],
-		'expected' => [
-			'cdn_enabled' => true,
-			'cdn_url'     => 'https://abcd1234.delivery.rocketcdn.me',
-		],
-	],
 ];
