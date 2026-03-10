8e28ab180c7de0ad1d8e92c52ff2d04b9b2c43af
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-10 17:12:16 +0100

    Updated failed state component to remove passed data

--- views/settings/partials/rocket-insights/recommendations/states/failed.php
+++ views/settings/partials/rocket-insights/recommendations/states/failed.php
@@ -6,30 +6,22 @@
  *
  * @since 3.21
  *
- * @param array $data {
- *     Data for failed state.
- *
- *     @type string $error_title   Optional error title to display.
- *     @type string $error_message Optional error message to display.
- * }
  */
 
 defined( 'ABSPATH' ) || exit;
-
-$error_title   = __( 'We’re sorry, recommendations are currently unavailable.', 'rocket' );
-$error_message = __( 'Please check the documentation for next steps', 'rocket' );
 ?>
+
 <div class="wpr-recommendations__failed">
 	<p class="wpr-recommendations__failed-title">
-		<?php echo esc_html( $error_title ); ?>
+		<?php esc_html_e( 'We’re sorry, recommendations are currently unavailable.', 'rocket' ); ?>
 	</p>
 	<p class="wpr-recommendations__failed-message">
-		<?php echo esc_html( $error_message ); ?>
+		<?php esc_html_e( 'Please check the documentation for next steps', 'rocket' ); ?>
 	</p>
 	<a href="#" 
-	   target="_blank" 
-	   rel="noopener noreferrer" 
-	   class="wpr-recommendations__failed-link">
+		target="_blank" 
+		rel="noopener noreferrer" 
+		class="wpr-recommendations__failed-link">
 		<?php esc_html_e( 'Read documentation', 'rocket' ); ?>
 		<span class="wpr-icon-external-link"></span>
 	</a>
