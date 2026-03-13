50562e956229098a1db82c7ad28a2b03e66d5210
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-13 18:24:56 +0100

    Updated test fixture

--- tests/Fixtures/inc/Engine/Admin/RocketInsights/Subscriber/html/output_2_urls_enabled.php
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/Subscriber/html/output_2_urls_enabled.php
@@ -5,13 +5,13 @@
 <div class="wpr-fieldsContainer">
 <fieldset class="wpr-fieldsContainer-fieldset">
 <div class="wpr-field wpr-global-score-widget">
-<div class="wpr-percentage-score-widget">
-<div>
+<div class="wpr-percentage-score-widget wpr-ri-score-widget">
+<div class="wpr-ri-score-widget__score-section">
 <div class="wpr-percentage-indicator">
 <a href="http://example.org/wp-admin/options-general.php?page=wprocket#rocket_insights"><div class="wpr-percentage-circle status-red ">0</div></a>
 </div>
+<p class="wpr-page-num-txt">Tracked Pages:<span>2</span></p>
 </div>
-<p class="wpr-page-num-txt">Tracked Pages: 2</p>
 <div class="wpr-global-score-widget-btn-wrapper">
 <a href="#rocket_insights" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-ri-add-url-button wpr-ri-global-score-add-url-button" data-source="dashboard">Add Pages </a>
 </div>
--- tests/Fixtures/inc/Engine/Admin/RocketInsights/Subscriber/html/output_3_urls_loading_disabled.php
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/Subscriber/html/output_3_urls_loading_disabled.php
@@ -5,15 +5,15 @@
 <div class="wpr-fieldsContainer">
 <fieldset class="wpr-fieldsContainer-fieldset">
 <div class="wpr-field wpr-global-score-widget">
-<div class="wpr-percentage-score-widget">
-<div>
+<div class="wpr-percentage-score-widget wpr-ri-score-widget">
+<div class="wpr-ri-score-widget__score-section">
 <div class="wpr-percentage-indicator">
 <div class="wpr-loading-container">
 <img class="wpr-loading-img" src="http://example.org/wp-content/plugins/wp-rocket/assets/img/orange-loading.svg"/>
 </div>
 </div>
+<p class="wpr-page-num-txt">Tracked Pages:<span>3</span></p>
 </div>
-<p class="wpr-page-num-txt">Tracked Pages: 3</p>
 <div class="wpr-global-score-widget-btn-wrapper">
 <a href="javascript:void(0);" class="wpr-button wpr-button--icon wpr-button--small wpr-button--purple wpr-icon-plus wpr-button--no-min-width wpr-ri-add-url-button wpr-ri-global-score-add-url-button wpr-btn-with-tool-tip disabled" data-source="dashboard" disabled="disabled">Add Pages <div class="wpr-tooltip"> <div class="wpr-tooltip-content"> You have reached your maximum page limit </div> </div> </a>
 </div>
