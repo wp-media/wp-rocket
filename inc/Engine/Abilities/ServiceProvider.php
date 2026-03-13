1ec26e524ae9bd03f2988e7a39efb2b58414c997
Rémy Perona <remperona@gmail.com>
2026-03-13 11:40:48 -0400

    add service provider

---
+++ inc/Engine/Abilities/ServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace WP_Rocket\Engine\Abilities;
+
+use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Array of services provided by this service provider
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'abilities_options',
+		'abilities_subscriber',
+	];
+
+	/**
+	 * Check if the service provider provides a specific service.
+	 *
+	 * @param string $id The id of the service.
+	 *
+	 * @return bool
+	 */
+	public function provides( string $id ): bool {
+		return in_array( $id, $this->provides, true );
+	}
+
+	public function register(): void {
+		$this->getContainer()->add( 'abilities_options', Options::class )
+			->addArgument( 'options' );
+		$this->getContainer()->addShared( 'abilities_subscriber', Subscriber::class )
+			->addArgument( 'abilities_options', );
+	}
+}
