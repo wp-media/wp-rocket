e673ae488e6bb95ec2330673d6d5c07d8e5686da
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-13 08:46:25 +0100

    Moved header into widget container

--- views/settings/partials/rocket-insights/global-score-widget.php
+++ views/settings/partials/rocket-insights/global-score-widget.php
@@ -6,15 +6,13 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<div class="wpr-optionHeader">
-	<h3 class="wpr-title2">
-		<?php echo esc_html__( 'Rocket Insights Score', 'rocket' ); ?>
-	</h3>
-</div>
-<div class="wpr-fieldsContainer">
-	<fieldset class="wpr-fieldsContainer-fieldset">
-		<div class="wpr-field" id="wpr_global_score_widget">
-			<?php $this->render_parts_with_data( 'rocket-insights/global-score-widget-content', $data ); ?>
-		</div>
-	</fieldset>
+<div class="wpr-ri-score-widget-container">
+	<div class="wpr-ri-score-widget-header">
+		<h3 class="wpr-ri-score-widget-header__title">
+			<?php echo esc_html__( 'Rocket Insights Score', 'rocket' ); ?>
+		</h3>
+	</div>
+	<div class="wpr-field" id="wpr_global_score_widget">
+		<?php $this->render_parts_with_data( 'rocket-insights/global-score-widget-content', $data ); ?>
+	</div>
 </div>
