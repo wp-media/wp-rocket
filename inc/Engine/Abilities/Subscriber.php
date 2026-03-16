a999ab089be701f3a1d354708cc45326255b2d31
Rémy Perona <remperona@gmail.com>
2026-03-16 15:16:10 -0400

    update code for Abilities options

--- inc/Engine/Abilities/Subscriber.php
+++ inc/Engine/Abilities/Subscriber.php
@@ -7,20 +7,45 @@ use WP\MCP\Core\McpAdapter;
 use WP_Rocket\Event_Management\Subscriber_Interface;
 
 class Subscriber implements Subscriber_Interface {
+	/**
+	 * Options ability instance.
+	 *
+	 * @var Options
+	 */
 	private $options;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param Options $options The options ability instance.
+	 */
 	public function __construct( Options $options ) {
 		$this->options = $options;
 	}
 
+	/**
+	 * Get the events to which this subscriber wants to listen.
+	 *
+	 * @return array The events and their corresponding callback methods.
+	 */
 	public static function get_subscribed_events(): array {
 		return [
-			'init' => 'init_mcp',
-			'wp_abilities_api_categories_init' => 'register_wpr_category',
+			'plugins_loaded'                   => [ 'init_mcp', 99 ],
+			'wp_abilities_api_categories_init' => [
+				[ 'register_options_category' ],
+			],
+			'wp_abilities_api_init'            => [
+				[ 'register_get_options_ability' ],
+			],
 		];
 	}
 
-	public function init_mcp() {
+	/**
+	 * Initialize the MCP adapter if the class exists.
+	 *
+	 * @return void
+	 */
+	public function init_mcp(): void {
 		if ( ! class_exists( McpAdapter::class ) ) {
 			return;
 		}
@@ -28,13 +53,21 @@ class Subscriber implements Subscriber_Interface {
 		McpAdapter::instance();
 	}
 
-	public function register_wpr_category() {
-		wp_register_ability_category(
-			'wp-rocket-options',
-			[
-				'label' => __( 'WP Rocket Options', 'rocket' ),
-				'description' => __( 'Abilities that retrieve or update WP Rocket options', 'rocket' ),
-			]
-		);
+	/**
+	 * Register the WP Rocket options ability category.
+	 *
+	 * @return void
+	 */
+	public function register_options_category(): void {
+		$this->options->register_options_category();
+	}
+
+	/**
+	 * Register the ability to get WP Rocket options.
+	 *
+	 * @return void
+	 */
+	public function register_get_options_ability(): void {
+		$this->options->register_get_options_ability();
 	}
 }
