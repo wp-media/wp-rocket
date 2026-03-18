e8d0e13d238b4f5537722034fc808c31b997cd87
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-18 18:59:09 +0100

    Removed lodash import

--- src/js/global/main.js
+++ src/js/global/main.js
@@ -1,5 +1,3 @@
-const { set } = require("lodash");
-
 var $ = jQuery;
 $(document).ready(function(){
 
@@ -289,7 +287,7 @@ $(document).ready(function(){
 		var recommendation = $(this).data('recommendation') || 'unknown';
 
 		// If the element is visible, scroll to the element with id matching the recommendation value.
-		// Delay scroll by 500ms to allow navigation to the section to complete and ensure the target element is in view.
+		// Delay scroll by 100ms to allow navigation to the section to complete and ensure the target element is in view.
 		setTimeout(function() {
 			var $target = $(`#${recommendation}`);
 			
