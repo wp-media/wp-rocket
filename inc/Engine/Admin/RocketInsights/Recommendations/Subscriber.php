ec6c4f1a65161e572ea4801536f864e8241c03de
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-18 11:34:31 +0100

    Added new wp_rocket_upgrade event with callback to force recalculate global score data on upgrade less than 3.21 but not less than 3.20

--- inc/Engine/Admin/RocketInsights/Recommendations/Subscriber.php
+++ inc/Engine/Admin/RocketInsights/Recommendations/Subscriber.php
@@ -58,6 +58,7 @@ class Subscriber implements Subscriber_Interface {
 			'rocket_sidebar'                              => 'render_recommendations_widget',
 			'rocket_insights_global_score_status_changed' => 'handle_status_change',
 			'rocket_insights_recommendations_rest_response' => 'output_recommendations_rest_response',
+			'wp_rocket_upgrade'                           => [ 'force_global_metrics_recalculation', 10, 2 ],
 		];
 	}
 
@@ -119,6 +120,23 @@ class Subscriber implements Subscriber_Interface {
 				// No action for other statuses.
 				break;
 		}
+	}
+
+	/**
+	 * Forces global metrics recalculation when upgrading from a version older than 3.21, but not older than 3.20.
+	 *
+	 * @since 3.21
+	 *
+	 * @param string $new_version New plugin version.
+	 * @param string $old_version Previously installed plugin version.
+	 * @return void
+	 */
+	public function force_global_metrics_recalculation( string $new_version, string $old_version ): void {
+		if ( version_compare( $old_version, '3.21', '>=' ) || version_compare( $old_version, '3.20', '<' ) ) {
+			return;
+		}
+
+		$this->data_manager->force_global_metrics_recalculation();
 	}
 
 	/**
