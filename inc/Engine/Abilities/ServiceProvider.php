f76ef937aa4fe89f2613e99016d0caaf0f25d47b
Rémy Perona <remperona@gmail.com>
2026-03-20 11:04:30 -0400

    add SetOption instantiation

--- inc/Engine/Abilities/ServiceProvider.php
+++ inc/Engine/Abilities/ServiceProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace WP_Rocket\Engine\Abilities;
 
 use WP_Rocket\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+use WP_Rocket\Engine\Abilities\Options\GetOptions;
+use WP_Rocket\Engine\Abilities\Options\SetOption;
+use WP_Rocket\Engine\Abilities\Options\Subscriber;
 
 class ServiceProvider extends AbstractServiceProvider {
 	/**
@@ -12,7 +15,8 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * @var array
 	 */
 	protected $provides = [
-		'abilities_options',
+		'abilities_get_options',
+		'abilities_set_option',
 		'abilities_subscriber',
 	];
 
@@ -33,9 +37,15 @@ class ServiceProvider extends AbstractServiceProvider {
 	 * @return void
 	 */
 	public function register(): void {
-		$this->getContainer()->add( 'abilities_options', Options::class )
+		$this->getContainer()->add( 'abilities_get_options', GetOptions::class )
 			->addArgument( 'options' );
+		$this->getContainer()->add( 'abilities_set_option', SetOption::class );
 		$this->getContainer()->addShared( 'abilities_subscriber', Subscriber::class )
-			->addArgument( 'abilities_options', );
+			->addArguments(
+				[
+					'abilities_get_options',
+					'abilities_set_option',
+				]
+			);
 	}
 }
