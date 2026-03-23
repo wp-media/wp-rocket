f42603b1ef3dbfe809f8f6b1fca6f7a667276212
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-03-23 17:43:52 +0100

    Updated test

---
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/Recommendations/Subscriber/ClearRecommendationsOnLangChange.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+	'test_data' => [
+		'shouldClearTransientWhenLocaleMetaUpdated' => [
+			'config'   => [
+				'meta_key'          => 'locale',
+				'initial_transient' => [
+					'status'          => 'success',
+					'recommendations' => [ 'some_recommendation' ],
+					'metadata'        => [],
+					'timestamp'       => time(),
+				],
+			],
+			'expected' => [
+				'should_clear'     => true,
+				'transient_after'  => null, // Not checked when should_clear is true.
+			],
+		],
+
+		'shouldNotClearTransientWhenOtherMetaUpdated' => [
+			'config'   => [
+				'meta_key'          => 'nickname',
+				'initial_transient' => [
+					'status'          => 'success',
+					'recommendations' => [ 'some_recommendation' ],
+					'metadata'        => [],
+					'timestamp'       => time(),
+				],
+			],
+			'expected' => [
+				'should_clear'     => false,
+				'transient_after'  => [
+					'status'          => 'success',
+					'recommendations' => [ 'some_recommendation' ],
+					'metadata'        => [],
+					'timestamp'       => time(),
+				],
+			],
+		],
+	],
+];
