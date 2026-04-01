74d8f8a3e9c99dd9ea011f6129510fe446995a2f
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-04-01 13:31:34 +0100

    Updated test case

--- tests/Fixtures/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParams.php
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParams.php
@@ -2,21 +2,7 @@
 
 return [
 	'test_data' => [
-		'shouldReturnDefaultParamsWhenImagifyNotActive' => [
-			'config' => [
-				'white_label_account'  => true,
-				'params'               => [
-					'enabled_options' => [ 'plugin_example' ],
-				],
-			],
-			'expected' => [
-				'params' => [
-					'enabled_options' => [ 'plugin_example' ],
-				],
-			],
-		],
-
-		'shouldReturnDefaultParamsWhenWhiteLabelNotActive' => [
+		'shouldReturnDefaultParamsWhenImagifyNotActiveOrWhiteLabelIsFalse' => [
 			'config' => [
 				'white_label_account'  => false,
 				'params'               => [
