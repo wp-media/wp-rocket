305a883197d22518f6a72f0d157b250edf37d4e0
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-04-01 11:03:50 +0100

    Updated test to remove isolated run

--- tests/Fixtures/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParams.php
+++ tests/Fixtures/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParams.php
@@ -19,7 +19,6 @@ return [
 
 		'shouldReturnDefaultParamsWhenWhiteLabelNotActive' => [
 			'config' => [
-				'has_imagify_api_key'  => false,
 				'white_label_account'  => false,
 				'params'               => [
 					'enabled_options' => [ 'plugin_example' ],
@@ -28,30 +27,6 @@ return [
 			'expected' => [
 				'params' => [
 					'enabled_options' => [ 'plugin_example' ],
-				],
-			],
-		],
-
-		'shouldAddImagifytoRecommendationsApiParamsWhenImagifyActiveAndWhiteLabelEnabled' => [
-			'config' => [
-				'has_imagify_api_key'  => true,
-				'white_label_account'  => true,
-				'params'               => [
-					'enabled_options' => [
-						'minify_css',
-						'minify_js',
-						'manual_preload'
-					],
-				],
-			],
-			'expected' => [
-				'params' => [
-					'enabled_options' => [
-						'minify_css',
-						'minify_js',
-						'manual_preload',
-						'plugin_imagify',
-					],
 				],
 			],
 		],
