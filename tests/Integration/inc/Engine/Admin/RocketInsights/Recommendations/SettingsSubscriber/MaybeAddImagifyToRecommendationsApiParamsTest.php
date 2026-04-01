9efe32a0c37ad599f6cba0320453faf3bd6545bf
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-04-01 09:36:58 +0100

    Refactored test to use hooks and update test class name

--- tests/Integration/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParamsTest.php
+++ tests/Integration/inc/Engine/Admin/RocketInsights/Recommendations/SettingsSubscriber/MaybeAddImagifyToRecommendationsApiParamsTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace WP_Rocket\Tests\Integration\inc\Engine\Admin\RocketInsights\Recommendations\SettingsSubscriber;
 
 use WP_Rocket\Tests\Integration\TestCase;
-use WP_Rocket\Engine\Admin\RocketInsights\Recommendations\SettingsSubscriber;
+use Mockery;
 
 /**
  * Test class for SettingsSubscriber::maybe_add_imagify_to_recommendations_api_params()
@@ -13,39 +13,45 @@ use WP_Rocket\Engine\Admin\RocketInsights\Recommendations\SettingsSubscriber;
  * Full integration testing with Imagify active requires the actual Imagify plugin to be installed.
  *
  * @group RocketInsights
- * @group Recommendations
  * @group AdminOnly
  */
-class MaybeAddImagifyToRecommendationsApiParamsTest extends TestCase {
-	private $subscriber;
+class Test_MaybeAddImagifyToRecommendationsApiParams extends TestCase {
 
 	public function set_up() {
 		parent::set_up();
 
-		// Get the SettingsSubscriber from the container.
-		$container = apply_filters( 'rocket_container', null );
-		if ( $container && $container->has( 'ri_recommendations_settings_subscriber' ) ) {
-			$this->subscriber = $container->get( 'ri_recommendations_settings_subscriber' );
-		}
+		 $this->unregisterAllCallbacksExcept( 'rocket_insights_api_recommendations_params', 'maybe_add_imagify_to_recommendations_api_params', 10 );
 	}
 
 	public function tear_down() {
+		$this->restoreWpHook( 'rocket_insights_api_recommendations_params' );
 		parent::tear_down();
 	}
 
 	/**
 	 * @dataProvider configTestData
+	 * 
+	 * @runInSeparateProcess Required because Mockery alias mocking creates a real class definition
+	 * for \Imagify_Partner at runtime. If the class is already loaded in the current process,
+	 * PHP will throw a fatal error for redeclaring it. Running in a separate process ensures
+	 * a clean state where the class hasn't been defined yet.
+	
+	 * @preserveGlobalState disabled Prevents PHPUnit from copying the parent process's global
+ 	 * state (loaded classes, constants, globals) into the new process, which would defeat the
+ 	 * purpose of @runInSeparateProcess and cause the alias mock to fail.
 	 */
-	public function testShouldHandleImagifyApiParams( $config, $expected ) {
+	public function testShouldDoAsExpected( $config, $expected ) {
 		// Set up white label constant.
 		$this->white_label = $config['white_label_account'];
 
-		// Test by calling the method directly.
-		if ( $this->subscriber && method_exists( $this->subscriber, 'maybe_add_imagify_to_recommendations_api_params' ) ) {
-			$result = $this->subscriber->maybe_add_imagify_to_recommendations_api_params( $config['params'] );
-			$this->assertSame( $expected['params'], $result );
-		} else {
-			$this->markTestIncomplete( 'SettingsSubscriber not available in container.' );
-		}
+		 Mockery::mock('alias:\Imagify_Partner')
+			->shouldReceive('has_imagify_api_key')
+			->once()
+			->andReturn($config['has_imagify_api_key']);
+
+		$this->assertSame( 
+			$expected['params'], 
+			wpm_apply_filters_typed( 'array', 'rocket_insights_api_recommendations_params', $config['params'] ) 
+		);
 	}
 }
