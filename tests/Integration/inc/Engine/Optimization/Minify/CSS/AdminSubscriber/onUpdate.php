6bd6b1bf1ff0e778133ccb5c2555773ac5384f87
Khadreal <opeyemi.khadri@gmail.com>
2026-04-26 12:25:23 +0100

    Fixed integration test

--- tests/Integration/inc/Engine/Optimization/Minify/CSS/AdminSubscriber/onUpdate.php
+++ tests/Integration/inc/Engine/Optimization/Minify/CSS/AdminSubscriber/onUpdate.php
@@ -12,17 +12,26 @@ use WP_Rocket\Tests\Integration\TestCase;
  * @group AdminOnly
  * @group MinifyAdmin
  */
-class TestOnUpdate extends TestCase {
+class OnUpdate extends TestCase {
+	protected $subscriber;
+
 	public function set_up() {
+
+		$container = apply_filters( 'rocket_container', null );
+
+		$this->subscriber = $container->get( 'cdn_subscriber' );
 		parent::set_up();
 
 		remove_filter( 'wp_rocket_upgrade', 'rocket_new_upgrade' );
+
+		remove_action( 'wp_rocket_upgrade', [ $this->subscriber, 'on_update_add_cdn_type_option' ] );
 		// Disable ATF optimization to prevent DB request (unrelated to the test).
 		add_filter( 'rocket_above_the_fold_optimization', '__return_false' );
 	}
 
 	public function tear_down() {
 		add_filter( 'wp_rocket_upgrade', 'rocket_new_upgrade', 10, 2 );
+		add_action( 'wp_rocket_upgrade', [ $this->subscriber, 'on_update_add_cdn_type_option' ], 10, 2 );
 		remove_filter( 'rocket_above_the_fold_optimization', '__return_false' );
 
 		parent::tear_down();
