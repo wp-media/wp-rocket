d56d11b8421e77aea5b233f61affae9b8c72a593
WordpressFan <ahmed@wp-media.me>
2026-04-27 21:00:40 +0300

    fix phpcs

--- inc/Engine/CDN/RocketCDN/NoticesSubscriber.php
+++ inc/Engine/CDN/RocketCDN/NoticesSubscriber.php
@@ -301,7 +301,7 @@ class NoticesSubscriber extends Abstract_Render implements Subscriber_Interface 
 				'button_url'            => $button_url,
 				'regular_price'         => $regular_price_monthly,
 				'current_price'         => $current_price_monthly,
-				'current_price_array'         => [
+				'current_price_array'   => [
 					'major' => ! empty( $current_price_array[0] ) ? $current_price_array[0] : 0,
 					'minor' => ! empty( $current_price_array[1] ) ? $current_price_array[1] : 0,
 				],
