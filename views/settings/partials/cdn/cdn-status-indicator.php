71a47cc21d73e176ad29a25509af08eea45c6715
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-04-27 17:25:02 +0100

    Applied phpcs fix

--- views/settings/partials/cdn/cdn-status-indicator.php
+++ views/settings/partials/cdn/cdn-status-indicator.php
@@ -11,14 +11,14 @@
  *     @type string $status_text Main status text.
  *     @type string $details     Details text (edge locations, pages covered).
  *     @type string $class       CSS class for the status indicator.
- * 	   @type bool   $is_pause_btn_disabled Whether the pause button should be disabled.
+ *     @type bool   $is_pause_btn_disabled Whether the pause button should be disabled.
  * }
  */
 
 defined( 'ABSPATH' ) || exit;
 
-$rocket_details = isset( $data['details'] ) ? $data['details'] : '';
-$rocket_class   = isset( $data['class'] ) ? $data['class'] : '';
+$rocket_details               = isset( $data['details'] ) ? $data['details'] : '';
+$rocket_class                 = isset( $data['class'] ) ? $data['class'] : '';
 $rocket_is_pause_btn_disabled = isset( $data['disable_pause_btn'] ) ? $data['disable_pause_btn'] : false;
 
 if ( ! $data['is_active'] ) {
@@ -40,8 +40,6 @@ if ( ! $data['is_active'] ) {
 			<?php endif; ?>
 		</div>
 	</div>
-	<?php
-	?>
 	<button type="button" class="wpr-cdn-pause" aria-pressed="false" <?php echo $rocket_is_pause_btn_disabled ? 'disabled' : ''; ?>>
 		<span class="wpr-cdn-pause__icon"></span>
 		<span class="wpr-cdn-pause__text"><?php esc_html_e( 'PAUSE CDN', 'rocket' ); ?></span>
