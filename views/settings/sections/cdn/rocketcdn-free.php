6c812d9559a52d9c0a50bf94bee047a323fce8ab
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-04-30 11:32:10 +0100

    Added new data param to track page count

--- views/settings/sections/cdn/rocketcdn-free.php
+++ views/settings/sections/cdn/rocketcdn-free.php
@@ -16,6 +16,7 @@
  *     @type string $help        Data to pass to beacon.
  *     @type string $page        Page section identifier.
  *     @type array  $status_indicator_data Data for the CDN status indicator partial.
+ *     @type int    $pages_count Number of pages added to RocketCDN.
  * }
  */
 
@@ -61,10 +62,12 @@ do_action( 'rocket_cdn_free_before_status_indicator' );
 	<div class="wpr-cdn-add-page">
 		<div class="wpr-cdn-add-page__input-wrap">
 			<input type="text" id="wpr_cdn_add_page_input" placeholder="<?php esc_attr_e( 'Enter a page URL to add to RocketCDN', 'rocket' ); ?>" />
+			<?php if ( 0 === $data['pages_count'] ) : ?>
 			<button type="button" class="wpr-cdn-add-page__homepage">
 				<span class="wpr-cdn-add-page__icon"></span>
 				<?php esc_html_e( 'ADD HOMEPAGE', 'rocket' ); ?>
 			</button>
+			<?php endif; ?>
 		</div>
 		<button type="button" class="wpr-cdn-add-page__button">
 			<span class="wpr-icon-plus"></span>
