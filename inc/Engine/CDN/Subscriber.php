55a8ed8e4cfd9fca4b208d8716886f90e43da82e
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-05-25 11:15:40 +0100

    Added condition to bail out for css url & properties rewriting

--- inc/Engine/CDN/Subscriber.php
+++ inc/Engine/CDN/Subscriber.php
@@ -142,6 +142,10 @@ class Subscriber implements Subscriber_Interface {
 	 * @return string
 	 */
 	public function rewrite_css_properties( $content ) {
+		if ( ! $this->cdn_driver_should_rewrite_url() ) {
+			return $content;
+		}
+
 		/**
 		 * Filters the application of the CDN on CSS properties
 		 *
@@ -226,6 +230,10 @@ class Subscriber implements Subscriber_Interface {
 	 * @return string
 	 */
 	public function add_cdn_url( $url, $original_url = '' ) {
+		if ( ! $this->is_allowed() ) {
+			return $url;
+		}
+
 		if ( ! empty( $original_url ) ) {
 			if ( $this->cdn->is_excluded( $original_url ) ) {
 				return $url;
@@ -372,7 +380,7 @@ class Subscriber implements Subscriber_Interface {
 			return false;
 		}
 
-		if ( $this->driver && ! $this->driver->should_rewrite_url( $this->get_current_url() ) ) {
+		if ( ! $this->cdn_driver_should_rewrite_url() ) {
 			return false;
 		}
 
@@ -455,5 +463,20 @@ class Subscriber implements Subscriber_Interface {
 		$current_options['cdn']      = 1;
 
 		$this->options_api->set( 'settings', $current_options );
+	}
+
+	/**
+	 * Determines if the CDN driver should rewrite the current URL.
+	 *
+	 * Checks if a CDN driver is set and whether it allows rewriting of the current URL.
+	 *
+	 * @return bool True if the URL should be rewritten by the CDN driver, false otherwise.
+	 */
+	private function cdn_driver_should_rewrite_url(): bool {
+		if ( $this->driver && ! $this->driver->should_rewrite_url( $this->get_current_url() ) ) {
+			return false;
+		}
+
+		return true;
 	}
 }
