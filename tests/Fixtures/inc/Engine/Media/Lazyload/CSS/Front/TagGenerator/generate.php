7c24212f2491680b6cc00ccb0a72c26a741257aa
Gaël Métais <gael.metais@gmail.com>
2026-06-06 01:06:40 +0200

    Run minifier + update test

--- tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Front/TagGenerator/generate.php
+++ tests/Fixtures/inc/Engine/Media/Lazyload/CSS/Front/TagGenerator/generate.php
@@ -5,7 +5,7 @@ $tags = <<<TAG
 <noscript>
 <style id="wpr-lazyload-bg-nostyle">:root{--wpr-bg-16ef9: url(https://domain.ext/path/to/background2.ext); }:root{--wpr-bg-17ef6: url(https://domain.ext/path/to/background3.ext); }:root{--wpr-bg-18ef6: url(https://domain.ext/path/to/background3.ext); }</style>
 </noscript>
-<script type="application/javascript">const rocket_pairs = [{"selector":"#section_2_hash","style":":root{--wpr-bg-16ef9: url(https:\/\/domain.ext\/path\/to\/background2.ext); }"},{"selector":"title~=\"wp-rocket\"","style":":root{--wpr-bg-17ef6: url(https:\/\/domain.ext\/path\/to\/background3.ext); }"},{"selector":"title~=\"wp-rocket-image\"","style":":root{--wpr-bg-18ef6: url(https:\/\/domain.ext\/path\/to\/background3.ext); }"}]; const rocket_excluded_pairs = [{"selector":".internal-css-background-image","style":":root{--wpr-bg-15ef8: url(https:\/\/domain.ext\/path\/to\/background.ext); }"}];</script>
+<script type="application/javascript">window.rocket_pairs = [{"selector":"#section_2_hash","style":":root{--wpr-bg-16ef9: url(https:\/\/domain.ext\/path\/to\/background2.ext); }"},{"selector":"title~=\"wp-rocket\"","style":":root{--wpr-bg-17ef6: url(https:\/\/domain.ext\/path\/to\/background3.ext); }"},{"selector":"title~=\"wp-rocket-image\"","style":":root{--wpr-bg-18ef6: url(https:\/\/domain.ext\/path\/to\/background3.ext); }"}]; const rocket_excluded_pairs = [{"selector":".internal-css-background-image","style":":root{--wpr-bg-15ef8: url(https:\/\/domain.ext\/path\/to\/background.ext); }"}];</script>
 TAG;
 
 
