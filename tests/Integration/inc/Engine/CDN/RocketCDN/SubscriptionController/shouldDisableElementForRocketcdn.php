4c92a6e0128735645d8c28083209ad5d7c78ceb0
Khadreal <opeyemi.khadri@gmail.com>
2026-06-28 19:07:34 +0100

    fixed tests

--- tests/Integration/inc/Engine/CDN/RocketCDN/SubscriptionController/shouldDisableElementForRocketcdn.php
+++ tests/Integration/inc/Engine/CDN/RocketCDN/SubscriptionController/shouldDisableElementForRocketcdn.php
@@ -3,6 +3,8 @@ declare( strict_types=1 );
 
 namespace WP_Rocket\Tests\Integration\inc\Engine\CDN\RocketCDN\SubscriptionController;
 
+use ReflectionProperty;
+
 /**
  * @group RocketCDN
  * @group CDN
@@ -15,7 +17,16 @@ class Test_ShouldDisableElementForRocketcdn extends AbstractSubscriptionControll
 	public function set_up() {
 		parent::set_up();
 
-		$this->render_controller = $this->getRocketContainer()->get( 'cdn_render_controller' );
+		$container = apply_filters( 'rocket_container', null );
+		$this->render_controller = $container->get( 'cdn_render_controller' );
+
+		$prop = new ReflectionProperty( $this->render_controller, 'options' );
+		$prop->setAccessible( true );
+		$prop->getValue( $this->render_controller )->set_values( [ 
+			'cdn'      => 1, 
+			'cdn_type' => 'rocketcdn' 
+			] 
+		);
 
 		$this->update_rocketcdn_settings( [ 'cdn' => 1, 'cdn_type' => 'rocketcdn' ] );
 		$this->set_rocketcdn_user_token();
