7603d49ec3e21a8fc64c986cf5a65d4bb5d3483d
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-07-07 21:10:22 +0100

    Added new test scenarios and updated existing

--- tests/Fixtures/inc/Engine/Cache/Abilities/PurgeUrl/RegisterPurgeUrlAbility.php
+++ tests/Fixtures/inc/Engine/Cache/Abilities/PurgeUrl/RegisterPurgeUrlAbility.php
@@ -46,7 +46,9 @@ return [
 			'expected' => [
 				'is_error' => false,
 				'success'  => false,
-				'error'    => [ 'http://example.org/not-cached-page/' ],
+				'error'    => [
+					'http://example.org/not-cached-page/' => 'No cache found for this URL, or a file permission issue prevented cache clearing.',
+				],
 			],
 		],
 
@@ -87,7 +89,9 @@ return [
 			'expected' => [
 				'is_error' => false,
 				'success'  => false,
-				'error'    => [ 'http://example.org/not-cached-page/' ],
+				'error'    => [
+					'http://example.org/not-cached-page/' => 'No cache found for this URL, or a file permission issue prevented cache clearing.',
+				],
 				'cleaned'  => [
 					'vfs://public/wp-content/cache/wp-rocket/example.org/lorem-ipsum/'                => null,
 					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/lorem-ipsum/' => null,
@@ -109,8 +113,127 @@ return [
 				'is_error' => false,
 				'success'  => false,
 				'error'    => [
-					'http://example.org/not-cached-page-1/',
-					'http://example.org/not-cached-page-2/',
+					'http://example.org/not-cached-page-1/' => 'No cache found for this URL, or a file permission issue prevented cache clearing.',
+					'http://example.org/not-cached-page-2/' => 'No cache found for this URL, or a file permission issue prevented cache clearing.',
+				],
+			],
+		],
+
+		'testShouldReturnErrorForInvalidUrl'                => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => 'not-a-valid-url',
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'not-a-valid-url' => 'URL format is invalid.',
+				],
+			],
+		],
+
+		'testShouldReturnMixedErrorsForInvalidAndUncachedUrls' => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => [
+						'http://example.org/lorem-ipsum/',
+						'http://example.org/not-cached-page/',
+						'not-a-valid-url',
+					],
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'not-a-valid-url'                     => 'URL format is invalid.',
+					'http://example.org/not-cached-page/' => 'No cache found for this URL, or a file permission issue prevented cache clearing.',
+				],
+				'cleaned'  => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org/lorem-ipsum/'                => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/lorem-ipsum/' => null,
+				],
+			],
+		],
+
+		'testShouldReturnErrorForExternalDomainUrl'         => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => 'https://external-domain.com/some-page/',
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'https://external-domain.com/some-page/' => 'URL does not belong to this site.',
+				],
+			],
+		],
+
+		'testShouldReturnMixedErrorsForExternalAndCachedUrls' => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => [
+						'http://example.org/lorem-ipsum/',
+						'https://external-domain.com/some-page/',
+					],
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'https://external-domain.com/some-page/' => 'URL does not belong to this site.',
+				],
+				'cleaned'  => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org/lorem-ipsum/'                => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/lorem-ipsum/' => null,
+				],
+			],
+		],
+
+		'testShouldReturnErrorForAdminUrl'                  => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => 'http://example.org/wp-admin/post.php?post=919&action=edit',
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'http://example.org/wp-admin/post.php?post=919&action=edit' => 'URL is an admin page.',
+				],
+			],
+		],
+
+		'testShouldReturnMixedErrorsForAdminAndCachedUrls'  => [
+			'config'   => [
+				'has_permission' => true,
+				'input'          => [
+					'url' => [
+						'http://example.org/lorem-ipsum/',
+						'http://example.org/wp-admin/post.php?post=919&action=edit',
+					],
+				],
+			],
+			'expected' => [
+				'is_error' => false,
+				'success'  => false,
+				'error'    => [
+					'http://example.org/wp-admin/post.php?post=919&action=edit' => 'URL is an admin page.',
+				],
+				'cleaned'  => [
+					'vfs://public/wp-content/cache/wp-rocket/example.org/lorem-ipsum/'                => null,
+					'vfs://public/wp-content/cache/wp-rocket/example.org-wpmedia-123456/lorem-ipsum/' => null,
 				],
 			],
 		],
