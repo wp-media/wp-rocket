38491c4da4444fdd99b79ece3ec398a60f971efc
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-07-14 09:53:22 +0100

    Added new test

---
+++ tests/Fixtures/inc/Addon/Cloudflare/Subscriber/purgeUrl.php
@@ -0,0 +1,103 @@
+<?php
+
+$page_rule_response = [
+	'headers'  => [],
+	'body'     => wp_json_encode(
+		(object) [
+			'success' => true,
+			'result'  => (object) [
+				'actions' => [
+					'id' => 'cache_everything',
+				],
+			],
+		]
+	),
+	'response' => '',
+	'cookies'  => [],
+];
+
+$no_page_rule_response = [
+	'headers'  => [],
+	'body'     => wp_json_encode(
+		(object) [
+			'success' => true,
+			'result'  => (object) [
+				'actions' => [
+					'id' => 'browser_check',
+				],
+			],
+		]
+	),
+	'response' => '',
+	'cookies'  => [],
+];
+
+$purge_response = [
+	'headers'  => [],
+	'body'     => wp_json_encode(
+		(object) [
+			'success' => true,
+			'result'  => '',
+		]
+	),
+	'response' => '',
+	'cookies'  => [],
+];
+
+return [
+	'testShouldDoNothingWhenNoCap'                      => [
+		'config'   => [
+			'cap'                => false,
+			'hook'               => 'after_rocket_clean_files',
+			'args'               => [
+				[ 'http://example.org/wp-content/cache/style.css' ],
+			],
+			'page_rule_response' => $page_rule_response,
+			'purge_response'     => $purge_response,
+		],
+		'expected' => null,
+	],
+	'testShouldDoNothingWhenNoPageRule'                 => [
+		'config'   => [
+			'cap'                => true,
+			'hook'               => 'after_rocket_clean_files',
+			'args'               => [
+				[ 'http://example.org/wp-content/cache/style.css' ],
+			],
+			'page_rule_response' => $no_page_rule_response,
+			'purge_response'     => $purge_response,
+		],
+		'expected' => null,
+	],
+	'testShouldPurgeHomeUrlWhenTriggeredByCleanHome'    => [
+		'config'   => [
+			'cap'                => true,
+			'hook'               => 'after_rocket_clean_home',
+			'args'               => [
+				'/tmp/wp-rocket-cache/example.org',
+				'',
+			],
+			'page_rule_response' => $page_rule_response,
+			'purge_response'     => $purge_response,
+		],
+		'expected' => [ home_url() ],
+	],
+	'testShouldPurgeFilesUrlsWhenTriggeredByCleanFiles' => [
+		'config'   => [
+			'cap'                => true,
+			'hook'               => 'after_rocket_clean_files',
+			'args'               => [
+				[
+					'http://example.org/wp-content/cache/style.css',
+					'http://example.org/wp-content/cache/script.js',
+				],
+			],
+			'page_rule_response' => $page_rule_response,
+			'purge_response'     => $purge_response,
+		],
+		'expected' => [
+			'http://example.org/wp-content/cache/style.css',
+			'http://example.org/wp-content/cache/script.js',
+		],
+	],
+];
