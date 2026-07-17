2e76452913ac99f65fc8a802f4d5f50d4feecf1f
Khadreal <opeyemi.khadri@gmail.com>
2026-07-17 14:31:29 +0100

    Add new tests for rocketcdn cta

--- tests/Fixtures/inc/Engine/CDN/RocketCDN/NoticesSubscriber/displayRocketcdnCta.php
+++ tests/Fixtures/inc/Engine/CDN/RocketCDN/NoticesSubscriber/displayRocketcdnCta.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-	'testShouldDisplayNothingWhenNotLiveSite' => [
+	'testShouldDisplayNothingWhenNotLiveSite'             => [
 		'rocketcdn_data' => [],
 
-		'expected' => [
-			'unit'        => null,
+		'expected'       => [
+			'unit' => null,
 		],
 
-		'config' => [
+		'config'         => [
 			'home_url'  => 'http://localhost',
 			'live_site' => false,
 		],
 	],
 
-	'testShouldNotDisplayNoticeWhenPlanIsPaid' => [
+	'testShouldNotDisplayNoticeWhenPlanIsPaid'            => [
 		'rocketcdn_data' => [
 			'rocketcdn_status'  => [
 				'subscription_status' => 'running',
@@ -23,17 +23,17 @@ return [
 			'rocketcdn_pricing' => [],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [],
 				'cta-big'   => [],
 			],
 		],
 
-		'config' => [],
+		'config'         => [],
 	],
 
-	'testShouldDisplayBigCTANoPromoWhenDefault' => [
+	'testShouldDisplayBigCTANoPromoWhenDefault'           => [
 
 		'rocketcdn_data' => [
 			'rocketcdn_status'  => [
@@ -48,7 +48,7 @@ return [
 				'monthly_price'            => 7.99,
 				'annual_price'             => 79.99,
 			],
-			'cta_data' => [
+			'cta_data'          => [
 				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
 				'cta_heading_max_limit' => '',
 				'cta_description'       => '',
@@ -56,25 +56,25 @@ return [
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => 'wpr-isHidden',
 				],
 				'cta-big'   => [
-					'container_class'    => '',
-					'promotion_campaign' => '',
-					'promotion_end_date' => '',
-					'nopromo_variant'    => '--no-promo',
-					'regular_price_monthly'      => '',
-					'regular_price_annual'       => '',
-					'current_price_monthly'      => 7.99,
-					'current_price_annual'      => 79.99,
+					'container_class'       => '',
+					'promotion_campaign'    => '',
+					'promotion_end_date'    => '',
+					'nopromo_variant'       => '--no-promo',
+					'regular_price_monthly' => '',
+					'regular_price_annual'  => '',
+					'current_price_monthly' => 7.99,
+					'current_price_annual'  => 79.99,
 				],
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => false,
 			'is_wp_error'                 => false,
 		],
@@ -95,7 +95,7 @@ return [
 				'monthly_price'            => 7.99,
 				'annual_price'             => 79.99,
 			],
-			'cta_data' => [
+			'cta_data'          => [
 				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
 				'cta_heading_max_limit' => '',
 				'cta_description'       => '',
@@ -103,31 +103,31 @@ return [
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => 'wpr-isHidden',
 				],
 				'cta-big'   => [
-					'container_class'    => '',
-					'promotion_campaign' => '',
-					'promotion_end_date' => '',
-					'nopromo_variant'    => '--no-promo',
-					'regular_price_monthly'      => '',
-					'regular_price_annual'       => '',
-					'current_price_monthly'      => 7.99,
-					'current_price_annual'      => 6.67,
+					'container_class'       => '',
+					'promotion_campaign'    => '',
+					'promotion_end_date'    => '',
+					'nopromo_variant'       => '--no-promo',
+					'regular_price_monthly' => '',
+					'regular_price_annual'  => '',
+					'current_price_monthly' => 7.99,
+					'current_price_annual'  => 6.67,
 				],
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => false,
 			'is_wp_error'                 => false,
 		],
 	],
 
-	'testShouldDisplayBigCTANoPromoWhenAfterEndDate' => [
+	'testShouldDisplayBigCTANoPromoWhenAfterEndDate'      => [
 
 		'rocketcdn_data' => [
 			'rocketcdn_status'  => [
@@ -142,7 +142,7 @@ return [
 				'monthly_price'            => 7.99,
 				'annual_price'             => 79.99,
 			],
-			'cta_data' => [
+			'cta_data'          => [
 				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
 				'cta_heading_max_limit' => '',
 				'cta_description'       => '',
@@ -150,31 +150,31 @@ return [
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => 'wpr-isHidden',
 				],
 				'cta-big'   => [
-					'container_class'    => '',
-					'promotion_campaign' => '',
-					'promotion_end_date' => '',
-					'nopromo_variant'    => '--no-promo',
-					'regular_price_monthly'      => '',
-					'regular_price_annual'       => '',
-					'current_price_monthly'      => 7.99,
-					'current_price_annual'      => 6.67,
+					'container_class'       => '',
+					'promotion_campaign'    => '',
+					'promotion_end_date'    => '',
+					'nopromo_variant'       => '--no-promo',
+					'regular_price_monthly' => '',
+					'regular_price_annual'  => '',
+					'current_price_monthly' => 7.99,
+					'current_price_annual'  => 6.67,
 				],
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => false,
 			'is_wp_error'                 => false,
 		],
 	],
 
-	'testShouldDisplaySmallCTAWhenBigHidden' => [
+	'testShouldDisplaySmallCTAWhenBigHidden'              => [
 
 		'rocketcdn_data' => [
 			'rocketcdn_status'  => [
@@ -189,7 +189,7 @@ return [
 				'monthly_price'            => 7.99,
 				'annual_price'             => 79.99,
 			],
-			'cta_data' => [
+			'cta_data'          => [
 				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
 				'cta_heading_max_limit' => '',
 				'cta_description'       => '',
@@ -197,31 +197,31 @@ return [
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => '',
 				],
 				'cta-big'   => [
-					'container_class'    => 'wpr-isHidden',
-					'promotion_campaign' => '',
-					'promotion_end_date' => '',
-					'nopromo_variant'    => '--no-promo',
-					'regular_price_monthly'      => '',
-					'regular_price_annual'       => '',
-					'current_price_monthly'      => 7.99,
-					'current_price_annual'      => 6.67,
+					'container_class'       => 'wpr-isHidden',
+					'promotion_campaign'    => '',
+					'promotion_end_date'    => '',
+					'nopromo_variant'       => '--no-promo',
+					'regular_price_monthly' => '',
+					'regular_price_annual'  => '',
+					'current_price_monthly' => 7.99,
+					'current_price_annual'  => 6.67,
 				],
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => true,
 			'is_wp_error'                 => false,
 		],
 	],
 
-	'testShouldDisplayBigCTAPromoWhenPromoActive' => [
+	'testShouldDisplayBigCTAPromoWhenPromoActive'         => [
 		'rocketcdn_data' => [
 			'rocketcdn_status'  => [
 				'subscription_status' => 'cancelled',
@@ -235,7 +235,7 @@ return [
 				'monthly_price'            => 7.99,
 				'annual_price'             => 79.99,
 			],
-			'cta_data' => [
+			'cta_data'          => [
 				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
 				'cta_heading_max_limit' => '',
 				'cta_description'       => '',
@@ -243,25 +243,25 @@ return [
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => 'wpr-isHidden',
 				],
 				'cta-big'   => [
-					'container_class'    => '',
-					'promotion_campaign' => 'Launch',
-					'promotion_end_date' => date( 'Y-m-d', strtotime( 'tomorrow', time() ) ),
-					'nopromo_variant'    => '',
-					'regular_price_monthly'      => 7.99,
-					'regular_price_annual'       => 6.67,
-					'current_price_monthly'      => 5.99,
-					'current_price_annual'      => 5.00,
+					'container_class'       => '',
+					'promotion_campaign'    => 'Launch',
+					'promotion_end_date'    => date( 'Y-m-d', strtotime( 'tomorrow', time() ) ),
+					'nopromo_variant'       => '',
+					'regular_price_monthly' => 7.99,
+					'regular_price_annual'  => 6.67,
+					'current_price_monthly' => 5.99,
+					'current_price_annual'  => 5.00,
 				],
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => false,
 			'is_wp_error'                 => false,
 		],
@@ -274,16 +274,16 @@ return [
 				'subscription_status' => 'cancelled',
 			],
 			'rocketcdn_pricing' => 'RocketCDN is not available at the moment. Please retry later.',
-			'cta_data' => [
-				'cta_heading'     => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
-				'cta_description' => '',
+			'cta_data'          => [
+				'cta_heading'           => '<strong>Want full-site Content Delivery coverage?</strong> Extend RocketCDN to all your pages with unlimited bandwidth.',
+				'cta_description'       => '',
 				'cta_heading_max_limit' => '',
 				'limit_reached'         => false,
 			],
 		],
 
-		'expected' => [
-			'unit'        => [
+		'expected'       => [
+			'unit' => [
 				'cta-small' => [
 					'container_class' => 'wpr-isHidden',
 				],
@@ -296,10 +296,10 @@ return [
 			],
 		],
 
-		'config' => [
+		'config'         => [
 			'rocket_rocketcdn_cta_hidden' => false,
 			'is_wp_error'                 => true,
 		],
-		'expected' => false,
+		'expected'       => false,
 	],
 ];
