d9763ca43cdb04f0adaebe7c235acc31dca1fd02
WordpressFan <ahmed@wp-media.me>
2026-07-21 11:48:44 +0300

    Move test data to a fixture following repo convention
    Addresses review feedback on PR #8642: use a configTestData
    dataProvider backed by a Fixtures/ file, matching the pattern
    used by the other SubscriptionController integration tests
    (isPaid, hasActiveSubscription, etc.) instead of hardcoded
    test methods.

--- tests/Integration/inc/Engine/CDN/RocketCDN/SubscriptionController/isSubscriptionCreationLoading.php
+++ tests/Integration/inc/Engine/CDN/RocketCDN/SubscriptionController/isSubscriptionCreationLoading.php
@@ -10,22 +10,18 @@ namespace WP_Rocket\Tests\Integration\inc\Engine\CDN\RocketCDN\SubscriptionContr
  */
 class Test_IsSubscriptionCreationLoading extends AbstractSubscriptionControllerTestCase {
 
-	public function testShouldReturnFalseWhenNoTokenEvenIfTransientSet(): void {
-		set_transient( 'rocket_cdn_subscription_creation_in_progress', time(), HOUR_IN_SECONDS );
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldDoAsExpected( array $config, bool $expected ): void {
+		if ( ! empty( $config['has_token'] ) ) {
+			$this->set_rocketcdn_user_token();
+		}
 
-		$this->assertFalse( $this->subscription_controller->is_subscription_creation_loading() );
-	}
+		if ( ! empty( $config['has_transient'] ) ) {
+			set_transient( 'rocket_cdn_subscription_creation_in_progress', time(), HOUR_IN_SECONDS );
+		}
 
-	public function testShouldReturnFalseWhenTokenExistsAndNoTransient(): void {
-		$this->set_rocketcdn_user_token();
-
-		$this->assertFalse( $this->subscription_controller->is_subscription_creation_loading() );
-	}
-
-	public function testShouldReturnTrueWhenTokenExistsAndTransientSet(): void {
-		$this->set_rocketcdn_user_token();
-		set_transient( 'rocket_cdn_subscription_creation_in_progress', time(), HOUR_IN_SECONDS );
-
-		$this->assertTrue( $this->subscription_controller->is_subscription_creation_loading() );
+		$this->assertSame( $expected, $this->subscription_controller->is_subscription_creation_loading() );
 	}
 }
