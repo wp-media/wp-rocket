acba79fd734ee735948545b3c483259a90b2ce23
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2026-08-04 13:08:08 +0100

    Enhancement/8589 expose wp rocket mcp server in tools (#8664)
    Co-authored-by: hanna-meda <93833759+hanna-meda@users.noreply.github.com>
    Co-authored-by: Opeyemi Ibrahim <opeyemi.khadri@gmail.com>

--- inc/Engine/Admin/Beacon/Beacon.php
+++ inc/Engine/Admin/Beacon/Beacon.php
@@ -885,6 +885,12 @@ class Beacon extends Abstract_Render implements Subscriber_Interface {
 					'url' => 'https://fr.docs.wp-rocket.me/article/1878-rocket-insights?utm_source=wp_plugin&utm_medium=wp_rocket',
 				],
 			],
+			'mcp_server'                 => [
+				'en' => [
+					'id'  => '6a5a7a2d8609704abe640d81',
+					'url' => 'https://docs.wp-rocket.me/article/1922-wp-rocket-mcp',
+				],
+			],
 		];
 
 		return isset( $suggest[ $doc_id ][ $this->get_user_locale() ] )
