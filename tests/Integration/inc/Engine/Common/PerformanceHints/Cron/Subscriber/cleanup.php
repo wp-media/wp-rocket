36296dc14d6a6e4ecfce1c227e7913df24776328
Rémy Perona <remperona@gmail.com>
2026-08-07 15:17:04 -0400

    remove unused param

--- tests/Integration/inc/Engine/Common/PerformanceHints/Cron/Subscriber/cleanup.php
+++ tests/Integration/inc/Engine/Common/PerformanceHints/Cron/Subscriber/cleanup.php
@@ -11,7 +11,6 @@ use WP_Rocket\Tests\Integration\TestCase;
  * @group PerformanceHints
  */
 class Test_Cleanup extends TestCase {
-	protected $path_to_test_data = '/inc/Engine/Common/PerformanceHints/Cron/Subscriber/cleanup.php';
 
 	/**
 	 * @var array
