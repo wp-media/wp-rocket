5061de2ce6640f4c5a09c6823dcf6a2c7d94504c
Rémy Perona <remperona@gmail.com>
2026-08-07 15:17:10 -0400

    remove uninstallAll

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -35,8 +35,6 @@ abstract class TestCase extends BaseTestCase {
 				self::$transients[ $transient ] = get_transient( $transient );
 			}
 		}
-
-		self::uninstallAll();
 	}
 
 	public static function tear_down_after_class() {
