7de94156f1223296bf905b021aea00c462464ead
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-08-21 12:58:18 +0100

    Bail out early when no paid subscription

--- inc/Engine/CDN/RocketCDN/CDNOptionsManager.php
+++ inc/Engine/CDN/RocketCDN/CDNOptionsManager.php
@@ -115,13 +115,16 @@ class CDNOptionsManager {
 	/**
 	 * Resolve the CDN state following first-install Pro detection.
 	 *
-	 * @param bool $is_subscription_running Whether a running paid subscription was detected.
+	 * @param bool $is_paid_subscription_running Whether a running paid subscription was detected.
 	 *
 	 * @return void
 	 */
-	public function resolve_auto_detect_data( bool $is_subscription_running ): void {
-		$this->options->set( 'cdn_state', $is_subscription_running ? Context::ROCKETCDN_PAID_TYPE : Context::CDN_STATE_NOTHING );
+	public function resolve_auto_detect_data( bool $is_paid_subscription_running ): void {
+		if ( ! $is_paid_subscription_running ) {
+			return;
+		}
 
+		$this->options->set( 'cdn_state', Context::ROCKETCDN_PAID_TYPE );
 		$this->options_api->set( 'settings', $this->options->get_options() );
 	}
 }
