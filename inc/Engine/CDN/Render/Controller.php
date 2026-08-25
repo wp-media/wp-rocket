642f6c3a6af7d4054b0a1f2a20dfad7177e3f14d
Michael Jeawhanlee <michaelleemichaellee408@gmail.com>
2026-08-25 08:40:57 +0100

    Display RocketCDN section based on subscription data

--- inc/Engine/CDN/Render/Controller.php
+++ inc/Engine/CDN/Render/Controller.php
@@ -126,7 +126,7 @@ class Controller extends Abstract_Render {
 	 * @return array
 	 */
 	public function add_rocketcdn_paid_section( array $sections ): array {
-		if ( Context::ROCKETCDN_PAID_TYPE !== $this->context->get_driver() ) {
+		if ( ! $this->subscription_controller->is_paid() ) {
 			return $sections;
 		}
 
@@ -160,7 +160,7 @@ class Controller extends Abstract_Render {
 	 * @return array
 	 */
 	public function add_rocketcdn_free_section( array $sections ): array {
-		if ( Context::ROCKETCDN_PAID_TYPE === $this->context->get_driver() ) {
+		if ( $this->subscription_controller->is_paid() ) {
 			return $sections;
 		}
 
