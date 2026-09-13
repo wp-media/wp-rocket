5e27bd6481f6593accc36abeb98e3078a58edc1c
WordpressFan <ahmed@wp-media.me>
2026-09-13 12:51:33 +0300

    fix(cdn): correct "WPRocket" to "WP Rocket" in licence-expired tooltip copy

--- inc/Engine/CDN/Render/Controller.php
+++ inc/Engine/CDN/Render/Controller.php
@@ -858,7 +858,7 @@ class Controller extends Abstract_Render {
 
 		if ( $this->subscription_controller->is_license_invalid() ) {
 			$texts['class']         .= ' wpr-cdn-status--expired';
-			$texts['paused_details'] = __( 'RocketCDN is currently paused because your WPRocket licence has expired.', 'rocket' );
+			$texts['paused_details'] = __( 'RocketCDN is currently paused because your WP Rocket licence has expired.', 'rocket' );
 		}
 
 		if ( $this->user->is_reseller_license_banned() ) {
